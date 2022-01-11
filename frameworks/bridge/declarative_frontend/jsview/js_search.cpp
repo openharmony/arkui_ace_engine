openharmony/ace_ace_engine

fix controll null bug of input area and search
Signed-off-by: yaoyuchi <yaoyuchi@huawei.com>
Change-Id: I349f363173a916158e7adbc64986d52dd7af9f68

frameworks/bridge/declarative_frontend/jsview/js_search.cpp
@@ -279,10 +279,14 @@ void JSSearch::Create(const JSCallbackInfo& info)
         textFieldComponent->SetPlaceholder(tip);
     }
 
-    auto controllerObj = param->GetProperty("controller");
-    JSSearchController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSSearchController>();
-    if (jsController) {
-        jsController->SetController(textFieldComponent->GetTextFieldController());
+    auto controllerObj = paramObject->GetProperty("controller");
+    if (!controllerObj->IsUndefined() && !controllerObj->IsNull()) {
+        JSTextAreaController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSTextAreaController>();
+        if (jsController) {
+            jsController->SetController(textAreaComponent->GetTextFieldController());
+        }
+    } else {
+        LOGI("controller is nullptr");
     }
 
     auto icon = param->GetProperty("icon");

frameworks/bridge/declarative_frontend/jsview/js_textinput.cpp
@@ -164,9 +164,13 @@ void JSTextInput::Create(const JSCallbackInfo& info)
     }
 
     auto controllerObj = paramObject->GetProperty("controller");
-    JSTextInputController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSTextInputController>();
-    if (jsController) {
-        jsController->SetController(textInputComponent->GetTextFieldController());
+    if (!controllerObj->IsUndefined() && !controllerObj->IsNull()) {
+        JSTextAreaController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSTextAreaController>();
+        if (jsController) {
+            jsController->SetController(textAreaComponent->GetTextFieldController());
+        }
+    } else {
+        LOGI("controller is nullptr");
     }
 
 }

openharmony/arkui_ace_engine

frameworks/bridge/declarative_frontend/jsview/js_search.cpp
@@ -268,25 +268,29 @@ void JSSearch::Create(const JSCallbackInfo& info)
 
     auto param = JSRef<JSObject>::Cast(info[0]);
     auto value = param->GetProperty("value");
-    if (!value->IsNull() && value->IsString()) {
+    if (!value->IsUndefined() && value->IsString()) {
         auto key = value->ToString();
         textFieldComponent->SetValue(key);
     }
 
     auto placeholde = param->GetProperty("placeholder");
-    if (!placeholde->IsNull() && placeholde->IsString()) {
+    if (!placeholde->IsUndefined() && placeholde->IsString()) {
         auto tip = placeholde->ToString();
         textFieldComponent->SetPlaceholder(tip);
     }
 
     auto controllerObj = param->GetProperty("controller");
-    JSSearchController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSSearchController>();
-    if (jsController) {
-        jsController->SetController(textFieldComponent->GetTextFieldController());
-    }
+    if (!controllerObj->IsUndefined() && !controllerObj->IsNull()) {
+        JSSearchController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSSearchController>();
+        if (jsController) {
+            jsController->SetController(textFieldComponent->GetTextFieldController());
+        }
+    } else {
+        LOGI("controller is nullptr");
+     }
 
     auto icon = param->GetProperty("icon");
-    if (!icon->IsNull() && icon->IsString()) {
+    if (!icon->IsUndefined() && icon->IsString()) {
         auto src = icon->ToString();
         textFieldComponent->SetIconImage(src);
     }

frameworks/bridge/declarative_frontend/jsview/js_textinput.cpp
@@ -140,7 +140,7 @@ void JSTextInput::JSBind(BindingTarget globalObj)
 void JSTextInput::Create(const JSCallbackInfo& info)
 {
     RefPtr<TextFieldComponent> textInputComponent = AceType::MakeRefPtr<TextFieldComponent>();
-
+    textInputComponent->SetTextFieldController(AceType::MakeRefPtr<TextFieldController>());
     // default type is text, default action is done.
     textInputComponent->SetTextInputType(TextInputType::TEXT);
     textInputComponent->SetAction(TextInputAction::DONE);
@@ -164,11 +164,14 @@ void JSTextInput::Create(const JSCallbackInfo& info)
     }
 
     auto controllerObj = paramObject->GetProperty("controller");
-    JSTextInputController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSTextInputController>();
-    if (jsController) {
-        jsController->SetController(textInputComponent->GetTextFieldController());
+    if (!controllerObj->IsUndefined() && !controllerObj->IsNull()) {
+        JSTextInputController* jsController = JSRef<JSObject>::Cast(controllerObj)->Unwrap<JSTextInputController>();
+        if (jsController) {
+            jsController->SetController(textInputComponent->GetTextFieldController());
+        }
+    } else {
+        LOGI("controller is nullptr");
     }
-
 }
 
 void JSTextInput::SetType(const JSCallbackInfo& info)
@@ -579,9 +582,8 @@ void JSTextInputController::Destructor(JSTextInputController* scroller)
 
 void JSTextInputController::CaretPosition(int32_t caretPosition)
 {
-    auto controller = controller_.Upgrade();
-    if (controller) {
-        controller->CaretPosition(caretPosition);
+    if (controller_) {
+        controller_->CaretPosition(caretPosition);
     }
 }
 