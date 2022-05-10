3bd7258e88939ac2dcefcc4becfa484dcad957ae
xiexiyun <xiexiyun@huawei.com>
2022-05-10 15:11:37 +0800

    rm redundant comment
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>

--- frameworks/core/components/clip/render_clip.cpp
+++ frameworks/core/components/clip/render_clip.cpp
@@ -74,7 +74,6 @@ FloatPropertyAnimatable::SetterMap RenderClip::GetFloatPropertySetterMap()
             return;
         }
         clip->SetWidth(value);
-        // clip->MarkNeedRender();
     };
     map[PropertyAnimatableType::PROPERTY_HEIGHT] = [weak](float value) {
         auto clip = weak.Upgrade();
@@ -83,7 +82,6 @@ FloatPropertyAnimatable::SetterMap RenderClip::GetFloatPropertySetterMap()
             return;
         }
         clip->SetHeight(value);
-        // clip->MarkNeedRender();
     };
     map[PropertyAnimatableType::PROPERTY_OFFSET_X] = [weak](float value) {
         auto clip = weak.Upgrade();
@@ -93,7 +91,6 @@ FloatPropertyAnimatable::SetterMap RenderClip::GetFloatPropertySetterMap()
         }
         clip->SetOffsetX(value);
         clip->UpdateBoxForShadowAnimation();
-        // clip->MarkNeedRender();
     };
     map[PropertyAnimatableType::PROPERTY_OFFSET_Y] = [weak](float value) {
         auto clip = weak.Upgrade();
@@ -103,7 +100,6 @@ FloatPropertyAnimatable::SetterMap RenderClip::GetFloatPropertySetterMap()
         }
         clip->SetOffsetY(value);
         clip->UpdateBoxForShadowAnimation();
-        // clip->MarkNeedRender();
     };
     map[PropertyAnimatableType::PROPERTY_BORDER_RADIUS] = [weak](float value) {
         auto clip = weak.Upgrade();
