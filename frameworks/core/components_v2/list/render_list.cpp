1b81427fe046a288aadbfcfb9ff0ee194f9a1aad
chenxuankai1 <chenxuankai1@huawei.com>
2022-05-26 16:12:34 +0800

    sticky item consume click event
    Signed-off-by: chenxuankai1 <chenxuankai1@huawei.com>
    Change-Id: I36b115227e7461200d8178f914292a52c95c5c6d

--- frameworks/core/components_v2/list/render_list.cpp
+++ frameworks/core/components_v2/list/render_list.cpp
@@ -616,7 +616,11 @@ bool RenderList::TouchTest(const Point& globalPoint, const Point& parentLocalPoi
     if (GetVisible() && fixedMainSize_ && scrollable_ && scrollable_->Available()) {
         result.emplace_back(scrollable_);
     }
-
+    // when click point is in sticky item, consume the touch event to avoid clicking on the list item underneath.
+    if (currentStickyItem_ && currentStickyItem_->GetPaintRect().IsInRegion(parentLocalPoint)) {
+        currentStickyItem_->TouchTest(globalPoint, parentLocalPoint, touchRestrict, result);
+        return true;
+    }
     return RenderNode::TouchTest(globalPoint, parentLocalPoint, touchRestrict, result);
 }
 
