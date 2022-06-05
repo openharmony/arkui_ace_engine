5b93c7bee457bc5c02117c772f54043fe506c96a
xiexiyun <xiexiyun@huawei.com>
2022-06-05 16:38:34 +0800

    fix header file not added
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>
    Change-Id: Icfe5335479bdec3cfa94b91ee387e86c7ee73026

--- frameworks/core/components/tab_bar/rosen_render_tab_bar_item.cpp
+++ frameworks/core/components/tab_bar/rosen_render_tab_bar_item.cpp
@@ -17,6 +17,7 @@
 
 #include "render_service_client/core/ui/rs_node.h"
 
+#include "core/components/common/painter/debug_boundary_painter.h"
 #include "core/components/common/painter/rosen_decoration_painter.h"
 #include "core/pipeline/base/rosen_render_context.h"
 
