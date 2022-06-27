0b7cff2242dffa817c2326991c668a561b696862
chenxuankai1 <chenxuankai1@huawei.com>
2022-06-27 16:10:49 +0800

    transform pass non-negative param
    Signed-off-by: chenxuankai1 <chenxuankai1@huawei.com>
    Change-Id: I772761200fdc190fba884f53da1a2799ac3e3139

--- frameworks/core/components/transform/render_transform.cpp
+++ frameworks/core/components/transform/render_transform.cpp
@@ -413,7 +413,7 @@ void RenderTransform::PerformLayout()
     Size layoutSize;
     LayoutParam innerLayout;
     Size maxLayoutSize = GetLayoutParam().GetMaxSize();
-    if (maxLayoutSize.IsValid()) {
+    if (!(maxLayoutSize < Size())) {
         innerLayout.SetMaxSize(maxLayoutSize);
         child->Layout(innerLayout);
         layoutSize = child->GetLayoutSize();
