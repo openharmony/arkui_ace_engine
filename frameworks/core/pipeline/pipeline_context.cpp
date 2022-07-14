700cc4d3727af0f2ce1bf2b7992dc3619823d0fa
caocan <caocan6@huawei.com>
2022-07-14 17:30:27 +0800

    fixed c81b79c from https://gitee.com/cc520bf/arkui_ace_engine/pulls/3363 修复气泡无法弹出问题
    Signed-off-by: caocan <caocan6@huawei.com>
    Change-Id: I327e7c0f3f0d108e9c08a10aca172965afaada3d

--- frameworks/core/pipeline/pipeline_context.cpp
+++ frameworks/core/pipeline/pipeline_context.cpp
@@ -1395,6 +1395,11 @@ bool PipelineContext::PopPageStackOverlay()
         return false;
     }
     LOGI("Pop page overlays");
+    // If last child is popup, use PopPopup to trigger state change event.
+    if (pageStack->PopPopupIfExist()) {
+        return true;
+    }
+
     pageStack->PopComponent();
     return true;
 }
