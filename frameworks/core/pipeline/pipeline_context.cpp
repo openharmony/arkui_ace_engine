eb81c83324863d41fce2fb8a276345fc1f59c8c3
caocan <caocan6@huawei.com>
2022-07-18 19:26:50 +0800

    fixed a06dcff from https://gitee.com/cc520bf/arkui_ace_engine/pulls/3430 修复recent后视频未暂停播放问题
    Signed-off-by: caocan <caocan6@huawei.com>
    Change-Id: I43dacf5ba10c45d3612a855f3c4d825bb972b575

--- frameworks/core/pipeline/pipeline_context.cpp
+++ frameworks/core/pipeline/pipeline_context.cpp
@@ -2566,6 +2566,7 @@ void PipelineContext::Destroy()
     window_->Destroy();
     touchPluginPipelineContext_.clear();
     webPaintCallback_.clear();
+    windowFocusChangedCallbackMap_.clear();
     LOGI("PipelineContext::Destroy end.");
 }
 
@@ -2993,6 +2994,12 @@ void PipelineContext::RootLostFocus() const
 
 void PipelineContext::WindowFocus(bool isFocus)
 {
+    for (const auto& [id, callback] : windowFocusChangedCallbackMap_) {
+        if (callback) {
+            callback(isFocus);
+        }
+    }
+
     if (!isFocus) {
         OnVirtualKeyboardAreaChange(Rect());
     }
--- frameworks/core/pipeline/pipeline_context.h
+++ frameworks/core/pipeline/pipeline_context.h
@@ -119,6 +119,7 @@ public:
     using SurfaceChangedCallbackMap =
         std::unordered_map<int32_t, std::function<void(int32_t, int32_t, int32_t, int32_t)>>;
     using PostRTTaskCallback = std::function<void(std::function<void()>&&)>;
+    using WindowFocusChangedCallbackMap = std::unordered_map<int32_t, std::function<void(bool)>>;
 
     PipelineContext(std::unique_ptr<Window> window, RefPtr<TaskExecutor> taskExecutor,
         RefPtr<AssetManager> assetManager, RefPtr<PlatformResRegister> platformResRegister,
@@ -1323,6 +1324,20 @@ public:
     {
         return usePartialupdate_;
     }
+
+    int32_t RegisterWindowFocusChangedCallback(std::function<void(bool)>&& callback)
+    {
+        if (callback) {
+            windowFocusChangedCallbackMap_.emplace(++callbackId_, std::move(callback));
+            return callbackId_;
+        }
+        return 0;
+    }
+
+    void UnregisterWindowFocusChangedCallback(int32_t callbackId)
+    {
+        windowFocusChangedCallbackMap_.erase(callbackId);
+    }
     
 protected:
     virtual void FlushVsync(uint64_t nanoTimestamp, uint32_t frameCount);
@@ -1545,6 +1560,7 @@ private:
 
     int32_t callbackId_ = 0;
     SurfaceChangedCallbackMap surfaceChangedCallbackMap_;
+    WindowFocusChangedCallbackMap windowFocusChangedCallbackMap_;
 
     std::vector<WeakPtr<PipelineContext>> touchPluginPipelineContext_;
     Offset pluginOffset_ { 0, 0 };
