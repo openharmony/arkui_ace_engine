de0b154bbf00dbed515f2d7e8e2dc92f122c8133
yangfan <yangfan229@huawei.com>
2022-07-26 21:57:18 +0800

    fixed cc0751f from https://gitee.com/yangfan229/ace_ace_engine/pulls/3696 add nullptr protect for GetMainSize
    Signed-off-by: yangfan <yangfan229@huawei.com>
    Change-Id: Ie63293e756e97a87f162088a7f077a2fd482c11f

--- frameworks/core/components/flex/render_flex.cpp
+++ frameworks/core/components/flex/render_flex.cpp
@@ -1018,7 +1018,10 @@ double RenderFlex::GetAvailableMainSize()
 
 double RenderFlex::GetMainSize(const RefPtr<RenderNode>& item) const
 {
-    double size;
+    double size = 0.0;
+    if (!item) {
+        return size;
+    }
     if (direction_ == FlexDirection::ROW || direction_ == FlexDirection::ROW_REVERSE) {
         size = item->GetLayoutSize().Width();
         if (item->GetPositionType() == PositionType::SEMI_RELATIVE) {
@@ -1033,7 +1036,10 @@ double RenderFlex::GetMainSize(const RefPtr<RenderNode>& item) const
 
 double RenderFlex::GetCrossSize(const RefPtr<RenderNode>& item) const
 {
-    double size;
+    double size = 0.0;
+    if (!item) {
+        return size;
+    }
     if (direction_ == FlexDirection::ROW || direction_ == FlexDirection::ROW_REVERSE) {
         size = item->GetLayoutSize().Height();
     } else {
