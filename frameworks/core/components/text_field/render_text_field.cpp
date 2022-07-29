832a72d825b98abb4ec120cd93006eca4b0b8e1f
bixuefeng <bixuefeng@huawei.com>
2022-07-29 10:38:49 +0800

    Bugfix: onClick issue
    Signed-off-by: bixuefeng <bixuefeng@huawei.com>
    Change-Id: Ic7566333f7f914c4cf52078668e67a1ffc6f4bd2

--- frameworks/core/components/text_field/render_text_field.cpp
+++ frameworks/core/components/text_field/render_text_field.cpp
@@ -433,7 +433,6 @@ void RenderTextField::OnTouchTestHit(
             }
         });
         clickRecognizer_->SetPriority(GesturePriority::Low);
-        clickRecognizer_->SetUseCatchMode(false);
     }
     clickRecognizer_->SetCoordinateOffset(coordinateOffset);
     result.emplace_back(clickRecognizer_);
