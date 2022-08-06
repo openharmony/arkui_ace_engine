cdd693e2ec26e7718898aa2a9fd55b45021735d5
jiangdayuan <jiangdayuan@huawei.com>
2022-08-06 11:13:10 +0800

    change console.log to info
    Signed-off-by: jiangdayuan <jiangdayuan@huawei.com>
    Change-Id: Iceb0c1a2449b2ba0dda4f062177f2222e9983c07

--- frameworks/bridge/js_frontend/engine/quickjs/qjs_engine.cpp
+++ frameworks/bridge/js_frontend/engine/quickjs/qjs_engine.cpp
@@ -2281,7 +2281,7 @@ JSValue AppLogPrint(JSContext* ctx, JsLogLevel level, JSValueConst value, int32_
 
 JSValue AppLogPrint(JSContext* ctx, JSValueConst value, int32_t argc, JSValueConst* argv)
 {
-    return AppLogPrint(ctx, JsLogLevel::DEBUG, value, argc, argv);
+    return AppLogPrint(ctx, JsLogLevel::INFO, value, argc, argv);
 }
 
 JSValue AppDebugLogPrint(JSContext* ctx, JSValueConst value, int32_t argc, JSValueConst* argv)
@@ -2500,7 +2500,7 @@ JSValue JsLogPrint(JSContext* ctx, JsLogLevel level, JSValueConst value, int32_t
 
 JSValue JsLogPrint(JSContext* ctx, JSValueConst value, int32_t argc, JSValueConst* argv)
 {
-    return JsLogPrint(ctx, JsLogLevel::DEBUG, value, argc, argv);
+    return JsLogPrint(ctx, JsLogLevel::INFO, value, argc, argv);
 }
 
 JSValue JsDebugLogPrint(JSContext* ctx, JSValueConst value, int32_t argc, JSValueConst* argv)
