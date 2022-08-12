8ec1590f9ce4687c20db59cbfad54d5c3c7513cd
laosan_ted <wangruichang@huawei.com>
2022-08-12 17:06:06 +0800

    update textZoomAtio to textZoomRatio
    Signed-off-by: laosan_ted <wangruichang@huawei.com>

--- frameworks/bridge/declarative_frontend/jsview/js_web.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_web.cpp
@@ -1003,7 +1003,8 @@ void JSWeb::JSBind(BindingTarget globalObj)
     JSClass<JSWeb>::StaticMethod("overviewModeAccess", &JSWeb::OverviewModeAccess);
     JSClass<JSWeb>::StaticMethod("fileFromUrlAccess", &JSWeb::FileFromUrlAccess);
     JSClass<JSWeb>::StaticMethod("databaseAccess", &JSWeb::DatabaseAccess);
-    JSClass<JSWeb>::StaticMethod("textZoomAtio", &JSWeb::TextZoomAtio);
+    JSClass<JSWeb>::StaticMethod("textZoomRatio", &JSWeb::TextZoomRatio);
+    JSClass<JSWeb>::StaticMethod("textZoomAtio", &JSWeb::TextZoomRatio);
     JSClass<JSWeb>::StaticMethod("webDebuggingAccess", &JSWeb::WebDebuggingAccessEnabled);
     JSClass<JSWeb>::StaticMethod("initialScale", &JSWeb::InitialScale);
     JSClass<JSWeb>::StaticMethod("backgroundColor", &JSWeb::BackgroundColor);
@@ -1919,7 +1920,7 @@ void JSWeb::DatabaseAccess(bool isDatabaseAccessEnabled)
     webComponent->SetDatabaseAccessEnabled(isDatabaseAccessEnabled);
 }
 
-void JSWeb::TextZoomAtio(int32_t textZoomAtioNum)
+void JSWeb::TextZoomRatio(int32_t textZoomRatioNum)
 {
     auto stack = ViewStackProcessor::GetInstance();
     auto webComponent = AceType::DynamicCast<WebComponent>(stack->GetMainComponent());
@@ -1927,7 +1928,7 @@ void JSWeb::TextZoomAtio(int32_t textZoomAtioNum)
         LOGE("JSWeb: MainComponent is null.");
         return;
     }
-    webComponent->SetTextZoomAtio(textZoomAtioNum);
+    webComponent->SetTextZoomRatio(textZoomRatioNum);
 }
 
 void JSWeb::WebDebuggingAccessEnabled(bool isWebDebuggingAccessEnabled)
