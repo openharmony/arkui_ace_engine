47965a98172fcd4ad1cd784fc4614cd4d92f70c7
wangxiaoding <wangxiaoding2@huawei.com>
2022-08-17 14:20:56 +0800

    modify rss push_page_complete stub position.
    Signed-off-by: wangxiaoding <wangxiaoding2@huawei.com>

--- frameworks/core/pipeline/pipeline_context.cpp
+++ frameworks/core/pipeline/pipeline_context.cpp
@@ -1100,7 +1100,7 @@ void PipelineContext::PushPage(const RefPtr<PageComponent>& pageComponent, const
     ACE_FUNCTION_TRACE();
     CHECK_RUN_ON(UI);
     std::unordered_map<std::string, std::string> params {{"pageUrl", pageComponent->GetPageUrl()}};
-    ResSchedReport::GetInstance().ResSchedDataReport("push_page_start", params);
+    ResSchedReportScope report("push_page", params);
     auto stageElement = stage;
     if (!stageElement) {
         // if not target stage, use root stage
@@ -1120,7 +1120,6 @@ void PipelineContext::PushPage(const RefPtr<PageComponent>& pageComponent, const
         RefPtr<DisplayComponent> display = AceType::MakeRefPtr<DisplayComponent>(pageComponent);
         stageElement->PushPage(display);
     }
-    ResSchedReport::GetInstance().ResSchedDataReport("push_page_complete", params);
 
 #if defined(ENABLE_NATIVE_VIEW) || defined(ENABLE_ROSEN_BACKEND)
     if (GetIsDeclarative()) {
