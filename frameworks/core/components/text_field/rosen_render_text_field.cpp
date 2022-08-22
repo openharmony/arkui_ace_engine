8d3ff716f8b2877d46021ef7a5c03ca5d8a2b46b
xiexiyun <xiexiyun@huawei.com>
2022-08-22 10:05:31 +0800

    修复输入框光标有时粗细不一致
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>
    Change-Id: I48a4deeb78a31b6741acfd0a6c5ec10773ea731e

--- frameworks/core/components/text_field/rosen_render_text_field.cpp
+++ frameworks/core/components/text_field/rosen_render_text_field.cpp
@@ -141,7 +141,7 @@ void RosenRenderTextField::PaintCaret(SkCanvas& canvas, const Rect& caretRect)
         cursorColor = style_.GetTextColor();
     }
     paint.setColor(Constants::ConvertSkColor(cursorColor));
-
+    paint.setAntiAlias(true);
     if (NearZero(cursorRadius_.Value())) {
         canvas.drawRect(
             SkRect::MakeLTRB(caretRect.Left(), caretRect.Top(), caretRect.Right(), caretRect.Bottom()), paint);
