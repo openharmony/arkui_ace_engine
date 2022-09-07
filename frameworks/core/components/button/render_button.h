99fdbf22b76fd5c7442c0e993e6e7fb3440b2056
peter <zhuzijia@huawei.com>
2022-09-07 15:21:29 +0800

    fix inspector bug
    Signed-off-by: peter <zhuzijia@huawei.com>
    Change-Id: Ibdbc104e8cdc208ce5660f8528f4c8e7ef02c356

--- frameworks/core/components/button/render_button.h
+++ frameworks/core/components/button/render_button.h
@@ -124,6 +124,11 @@ public:
         return buttonComponent_->GetBackgroundColor();
     }
 
+    double GetRadius() const
+    {
+        return rrectRadius_;
+    }
+
     void SendAccessibilityEvent();
 
 protected:
