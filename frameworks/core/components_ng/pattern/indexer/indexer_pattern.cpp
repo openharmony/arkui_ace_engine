b05b8acc7fc2a108043018d98fafe5b8b062b2b0
zhengqiyi <zhengqiyi2@huawei.com>
2022-09-21 12:00:07 +0800

    add indexer
    Signed-off-by: zhengqiyi <zhengqiyi2@huawei.com>

--- frameworks/core/components_ng/pattern/indexer/indexer_pattern.cpp
+++ frameworks/core/components_ng/pattern/indexer/indexer_pattern.cpp
@@ -16,7 +16,7 @@
 #include "core/components_ng/pattern/indexer/indexer_pattern.h"
 #include "core/components_ng/pattern/text/text_pattern.h"
 #include "core/components_ng/pattern/text/text_view.h"
-#include "core?components_ng/property/measure_utils.h"
+#include "core/components_ng/property/measure_utils.h"
 #include "core/pipeline_ng/pipeline_context.h"
 
 namespace OHOS::Ace::NG {
