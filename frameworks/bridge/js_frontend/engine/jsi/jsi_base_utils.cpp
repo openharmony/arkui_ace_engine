77bdd8b62f12dd57c1dca66f1d3d403f58b9319c
gongyuhang <gongyuhang7@huawei.com>
2022-10-08 16:43:52 +0800

    fixed 5b15d25 from https://gitee.com/maimaity/arkui_ace_engine/pulls/5367 Adjust the format of "[Engine Log]" for windows and mac
    Issue: https://gitee.com/openharmony/arkui_ace_engine/issues/I5UHL0
    Test: N/A
    Signed-off-by: gongyuhang <gongyuhang7@huawei.com>

--- frameworks/bridge/js_frontend/engine/jsi/jsi_base_utils.cpp
+++ frameworks/bridge/js_frontend/engine/jsi/jsi_base_utils.cpp
@@ -93,7 +93,7 @@ std::string JsiBaseUtils::GenerateSummaryBody(std::shared_ptr<JsValue> error, st
     }
 
 #if defined(WINDOWS_PLATFORM) || defined(MAC_PLATFORM)
-    std::string summaryBodyInsertedWithTagStr = "[Engine Log]";
+    std::string summaryBodyInsertedWithTagStr = "";
     size_t lastPosOfNextLine = -1;
     size_t currPosOfNextLine = 0;
     while (true) {
@@ -103,13 +103,10 @@ std::string JsiBaseUtils::GenerateSummaryBody(std::shared_ptr<JsValue> error, st
             break;
         }
         summaryBodyInsertedWithTagStr
-            .append(summaryBody.substr(lastPosOfNextLine, (currPosOfNextLine - lastPosOfNextLine) + 1))
-            .append("[Engine Log]");
+            .append("[Engine Log]")
+            .append(summaryBody.substr(lastPosOfNextLine, (currPosOfNextLine - lastPosOfNextLine) + 1));
         lastPosOfNextLine = currPosOfNextLine;
     }
-    summaryBodyInsertedWithTagStr
-        .append(summaryBody.substr(lastPosOfNextLine, summaryBody.length() - lastPosOfNextLine))
-        .append("\n");
     return summaryBodyInsertedWithTagStr;
 #else
     return summaryBody;
