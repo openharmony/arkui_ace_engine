2eda7aa7f8458593363b97a9e28c25a3136a6896
zhengqiyi <zhengqiyi2@huawei.com>
2022-10-11 11:18:10 +0800

    solve list warnning problem
    Signed-off-by: zhengqiyi <zhengqiyi2@huawei.com>

--- frameworks/core/components_ng/pattern/list/list_paint_method.cpp
+++ frameworks/core/components_ng/pattern/list/list_paint_method.cpp
@@ -33,8 +33,7 @@ CanvasDrawFunction ListPaintMethod::GetForegroundDrawFunction(PaintWrapper* pain
     int32_t endIndex = endIndex_;
     float startMargin = divider_.startMargin.ConvertToPx();
 
-    return [dividerPainter, startIndex, endIndex, itemPosition = std::move(itemPosition_), startMargin](
-               RSCanvas& canvas) { dividerPainter.DrawListLines(canvas, startIndex, endIndex,
-               itemPosition, startMargin); };
+    return [dividerPainter, startIndex, endIndex, itemPosition = std::move(itemPosition_), startMargin](RSCanvas& canvas)
+        { dividerPainter.DrawListLines(canvas, startIndex, endIndex, itemPosition, startMargin); };
 }
 } // namespace OHOS::Ace::NG
