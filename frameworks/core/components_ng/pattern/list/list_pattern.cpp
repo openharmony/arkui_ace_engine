c01c83fbd5a11d70df863cf8b1b05922f58617ac
zhengqiyi <zhengqiyi2@huawei.com>
2022-10-11 16:17:57 +0800

    close edgeEffect
    Signed-off-by: zhengqiyi <zhengqiyi2@huawei.com>

--- frameworks/core/components_ng/pattern/list/list_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_pattern.cpp
@@ -113,6 +113,7 @@ void ListPattern::OnModifyDone()
     initScrollable_ = true;
     
     auto edgeEffect = listLayoutProperty->GetEdgeEffect().value_or(EdgeEffect::SPRING);
+    edgeEffect = EdgeEffect::NONE;
     if (edgeEffect == EdgeEffect::SPRING) {
         auto scrollEdgeEffect = AceType::MakeRefPtr<NG::ScrollSpringEffect>();
         SetScrollEdgeEffect(scrollEdgeEffect);
