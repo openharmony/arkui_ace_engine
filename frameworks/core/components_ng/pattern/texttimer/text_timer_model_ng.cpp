5a347538341e2effdc93abe85a3797b4e8d03137
liukaii <liukai240@huawei.com>
2022-10-11 19:09:20 +0800

    [UI组件重构]新框架TextTimer组件js解耦
    Signed-off-by: liukaii <liukai240@huawei.com>
    Change-Id: Id857b3b309f64f1c4c907b2951708fbab2f1a5ad
    Signed-off-by: liukaii <liukai240@huawei.com>

--- frameworks/core/components_ng/pattern/texttimer/text_timer_model_ng.cpp
+++ frameworks/core/components_ng/pattern/texttimer/text_timer_model_ng.cpp
@@ -13,20 +13,14 @@
  * limitations under the License.
  */
 
-#include "core/components_ng/pattern/texttimer/text_timer_view.h"
+#include "core/components_ng/pattern/texttimer/text_timer_model_ng.h"
 
-#include <cstddef>
-
-#include "base/geometry/dimension.h"
-#include "core/common/frontend.h"
-#include "core/components_ng/base/frame_node.h"
 #include "core/components_ng/base/view_stack_processor.h"
 #include "core/components_ng/pattern/text/text_layout_property.h"
 #include "core/components_ng/pattern/texttimer/text_timer_pattern.h"
-#include "core/components_v2/inspector/inspector_constants.h"
 
 namespace OHOS::Ace::NG {
-RefPtr<TextTimerController> TextTimerView::Create()
+RefPtr<TextTimerController> TextTimerModelNG::Create()
 {
     auto* stack = ViewStackProcessor::GetInstance();
     auto nodeId = stack->ClaimNodeId();
@@ -37,47 +31,47 @@ RefPtr<TextTimerController> TextTimerView::Create()
     return pattern ? pattern->GetTextTimerController() : nullptr;
 }
 
-void TextTimerView::SetFormat(const std::string& format)
+void TextTimerModelNG::SetFormat(const std::string& format)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextTimerLayoutProperty, Format, format);
 }
 
-void TextTimerView::SetIsCountDown(bool isCountDown)
+void TextTimerModelNG::SetIsCountDown(bool isCountDown)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextTimerLayoutProperty, IsCountDown, isCountDown);
 }
 
-void TextTimerView::SetInputCount(double count)
+void TextTimerModelNG::SetInputCount(double count)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextTimerLayoutProperty, InputCount, count);
 }
 
-void TextTimerView::SetFontSize(const Dimension& value)
+void TextTimerModelNG::SetFontSize(const Dimension& value)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextLayoutProperty, FontSize, value);
 }
 
-void TextTimerView::SetTextColor(const Color& value)
+void TextTimerModelNG::SetTextColor(const Color& value)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextLayoutProperty, TextColor, value);
 }
 
-void TextTimerView::SetItalicFontStyle(Ace::FontStyle value)
+void TextTimerModelNG::SetItalicFontStyle(Ace::FontStyle value)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextLayoutProperty, ItalicFontStyle, value);
 }
 
-void TextTimerView::SetFontWeight(FontWeight value)
+void TextTimerModelNG::SetFontWeight(FontWeight value)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextLayoutProperty, FontWeight, value);
 }
 
-void TextTimerView::SetFontFamily(const std::vector<std::string>& value)
+void TextTimerModelNG::SetFontFamily(const std::vector<std::string>& value)
 {
     ACE_UPDATE_LAYOUT_PROPERTY(TextLayoutProperty, FontFamily, value);
 }
 
-void TextTimerView::SetOnTimer(ChangeEvent&& onChange)
+void TextTimerModelNG::SetOnTimer(std::function<void(const std::string, const std::string)>&& onChange)
 {
     auto frameNode = ViewStackProcessor::GetInstance()->GetMainFrameNode();
     CHECK_NULL_VOID(frameNode);
