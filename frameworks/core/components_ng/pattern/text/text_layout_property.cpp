6f53a808c574c982dd9e14d275ca276e3d971608
zcdqs <junfeng.lijunfeng@huawei.com>
2022-11-03 15:17:12 +0800

    ng xts
    Signed-off-by: zcdqs <junfeng.lijunfeng@huawei.com>
    Change-Id: Ica1a4bb6d2b45d7379fc6736473235d735d0fdb5

--- frameworks/core/components_ng/pattern/text/text_layout_property.cpp
+++ frameworks/core/components_ng/pattern/text/text_layout_property.cpp
@@ -26,39 +26,22 @@ void TextLayoutProperty::ToJsonValue(std::unique_ptr<JsonValue>& json) const
     json->Put("fontStyle", GetItalicFontStyle().value_or(Ace::FontStyle::NORMAL) == Ace::FontStyle::NORMAL
                                ? "FontStyle.Normal"
                                : "FontStyle.Italic");
-    static const std::array<std::string, 15> FONT_WEIGHT_TO_STRING = { "FontWeight.W100", "FontWeight.W200",
-        "FontWeight.W300", "FontWeight.W400", "FontWeight.W500", "FontWeight.W600", "FontWeight.W700",
-        "FontWeight.W800", "FontWeight.W900", "FontWeight.BOLD", "FontWeight.NORMAL", "FontWeight.BOLDER",
-        "FontWeight.LIGHTER", "FontWeight.MEDIUM", "FontWeight.REGULAR" };
-    json->Put("fontWeight",
-        FONT_WEIGHT_TO_STRING.at(static_cast<int32_t>(GetFontWeight().value_or(FontWeight::W400))).c_str());
+    json->Put("fontWeight", V2::ConvertWrapFontWeightToStirng(GetFontWeight().value_or(FontWeight::NORMAL)).c_str());
     std::vector<std::string> fontFamilyVector =
         GetFontFamily().value_or<std::vector<std::string>>({ "HarmonyOS Sans" });
-    std::string fontFamily = '{' + fontFamilyVector.at(0);
+    std::string fontFamily = fontFamilyVector.at(0);
     for (uint32_t i = 1; i < fontFamilyVector.size(); ++i) {
         fontFamily += ',' + fontFamilyVector.at(i);
     }
-    fontFamily += '}';
     json->Put("fontFamily", fontFamily.c_str());
-    static const std::array<std::string, 5> TEXT_DECORATION_TO_STRING = {
-        "TextDecorationType.None",
-        "TextDecorationType.Underline",
-        "TextDecorationType.Overline",
-        "TextDecorationType.LineThrough",
-        "TextDecorationType.Inherit",
-    };
-    std::string decoration =
-        "{type:" +
-        TEXT_DECORATION_TO_STRING.at(static_cast<int32_t>(GetTextDecoration().value_or(TextDecoration::NONE))) +
-        ",color:" + GetTextDecorationColor().value_or(Color::BLACK).ColorToString() + '}';
-    json->Put("decoration", decoration.c_str());
-    static const std::array<std::string, 3> TEXT_CASE_TO_STRING = {
-        "TextCase.Normal",
-        "TextCase.LowerCase",
-        "TextCase.UpperCase",
-    };
-    json->Put(
-        "textCase", TEXT_CASE_TO_STRING.at(static_cast<int32_t>(GetTextCase().value_or(TextCase::NORMAL))).c_str());
+
+    auto jsonDecoration = JsonUtil::Create(true);
+    std::string type = V2::ConvertWrapTextDecorationToStirng(GetTextDecoration().value_or(TextDecoration::NONE));
+    jsonDecoration->Put("type", type.c_str());
+    jsonDecoration->Put("color", GetTextDecorationColor().value_or(Color::BLACK).ColorToString().c_str());
+    json->Put("decoration", jsonDecoration->ToString().c_str());
+
+    json->Put("textCase", V2::ConvertWrapTextCaseToStirng(GetTextCase().value_or(TextCase::NORMAL)).c_str());
     if (HasAdaptMinFontSize()) {
         json->Put("minFontSize", GetAdaptMinFontSize().value().ToString().c_str());
     }
@@ -81,23 +64,9 @@ void TextLayoutProperty::ToJsonValue(std::unique_ptr<JsonValue>& json) const
         TEXT_BASE_LINE_TO_STRING.at(static_cast<int32_t>(GetTextBaseline().value_or(TextBaseline::ALPHABETIC)))
             .c_str());
     json->Put("baselineOffset", GetBaselineOffset().value_or(0.0_vp).ToString().c_str());
-    static const std::array<std::string, 6> TEXT_ALIGN_TO_STRING = {
-        "TextAlign.Left",
-        "TextAlign.Right",
-        "TextAlign.Center",
-        "TextAlign.Justify",
-        "TextAlign.Start",
-        "TextAlign.End",
-    };
+    json->Put("textAlign", V2::ConvertWrapTextAlignToString(GetTextAlign().value_or(TextAlign::START)).c_str());
     json->Put(
-        "textAlign", TEXT_ALIGN_TO_STRING.at(static_cast<int32_t>(GetTextAlign().value_or(TextAlign::START))).c_str());
-    static const std::array<std::string, 3> TEXT_OVERFLOW_TO_STRING = {
-        "TextOverflow.Clip",
-        "TextOverflow.Ellipsis",
-        "TextOverflow.None",
-    };
-    json->Put("textOverflow",
-        TEXT_OVERFLOW_TO_STRING.at(static_cast<int32_t>(GetTextOverflow().value_or(TextOverflow::CLIP))).c_str());
+        "textOverflow", V2::ConvertWrapTextOverflowToString(GetTextOverflow().value_or(TextOverflow::CLIP)).c_str());
     json->Put("maxLines", std::to_string(GetMaxLines().value_or(UINT32_MAX)).c_str());
 }
 
