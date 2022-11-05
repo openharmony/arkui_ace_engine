c895d6ded4effe666f0f6c50578d8f780a7a3470
kangchongtao <kangchongtao@huawei.com>
2022-11-05 17:12:38 +0800

    fixed 97d49a6 from https://gitee.com/kangchongtao/arkui_ace_engine/pulls/6399 补齐FUZZ测试用例
    Signed-off-by: kangchongtao <kangchongtao@huawei.com>
    Change-Id: I3f1434f6cb59e8a024047e9bbeeffd63bdbfe2b6

--- test/fuzztest/imageloader_fuzzer/imageloader_fuzzer.cpp
+++ test/fuzztest/imageloader_fuzzer/imageloader_fuzzer.cpp
@@ -31,8 +31,47 @@ namespace OHOS {
     {
         auto info = CreatImageSourceInfo(data, size);
         Ace::WeakPtr<Ace::PipelineBase> context = nullptr;
+        Ace::FileImageLoader fileImageLoader;
+        return fileImageLoader.LoadImageData(info, context) != nullptr;
+    }
+
+    bool DataProviderImageLoaderTest(const uint8_t* data, size_t size)
+    {
+        auto info = CreatImageSourceInfo(data, size);
+        Ace::WeakPtr<Ace::PipelineBase> context = nullptr;
         Ace::DataProviderImageLoader dataProviderImageLoader;
-        return dataProviderImageLoader.LoadImageData(info, context) != nullptr;
+        Ace::Base64ImageLoader base64ImageLoader;
+        auto base64Image = base64ImageLoader.LoadImageData(info, context) != nullptr;
+        return dataProviderImageLoader.LoadImageData(info, context) != nullptr && base64Image;
+    }
+
+    bool DecodedDataProviderImageLoaderTest(const uint8_t* data, size_t size)
+    {
+        auto info = CreatImageSourceInfo(data, size);
+        Ace::WeakPtr<Ace::PipelineBase> context = nullptr;
+        Ace::DecodedDataProviderImageLoader decodedDataProviderImageLoader;
+        auto skData = decodedDataProviderImageLoader.LoadImageData(info, context);
+        auto ngData = decodedDataProviderImageLoader.LoadDecodedImageData(info, context);
+        return skData != nullptr && ngData != nullptr;
+    }
+
+    bool PixelMapImageLoaderTest(const uint8_t* data, size_t size)
+    {
+        auto info = CreatImageSourceInfo(data, size);
+        Ace::WeakPtr<Ace::PipelineBase> context = nullptr;
+        Ace::PixelMapImageLoader pixelMapImageLoader;
+        auto skData = pixelMapImageLoader.LoadImageData(info, context);
+        auto ngData = pixelMapImageLoader.LoadDecodedImageData(info, context);
+        return skData != nullptr && ngData != nullptr;
+    }
+
+    bool ResourceImageLoaderTest(const uint8_t* data, size_t size)
+    {
+        auto info = CreatImageSourceInfo(data, size);
+        Ace::WeakPtr<Ace::PipelineBase> context = nullptr;
+        Ace::ResourceImageLoader resourceImageLoader;
+        auto skData = resourceImageLoader.LoadImageData(info, context);
+        return skData != nullptr;
     }
 
     bool AssetImageLoader(const uint8_t* data, size_t size)
@@ -40,7 +79,9 @@ namespace OHOS {
         auto info = CreatImageSourceInfo(data, size);
         Ace::WeakPtr<Ace::PipelineBase> context = nullptr;
         Ace::AssetImageLoader assetImageLoader;
-        return assetImageLoader.LoadImageData(info, context) != nullptr;
+        std::string randomString = reinterpret_cast<const char*>(data);
+        auto jsonString = assetImageLoader.LoadJsonData(randomString);
+        return assetImageLoader.LoadImageData(info, context) != nullptr && jsonString.empty();
     }
 
     bool NetworkImageLoader(const uint8_t* data, size_t size)
@@ -65,9 +106,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
     OHOS::FileImageLoaderTest(data, size);
+    OHOS::DataProviderImageLoaderTest(data, size);
+    OHOS::DecodedDataProviderImageLoaderTest(data, size);
     OHOS::AssetImageLoader(data, size);
     OHOS::NetworkImageLoader(data, size);
     OHOS::InternalImageLoader(data, size);
+    OHOS::ResourceImageLoaderTest(data, size);
+    OHOS::PixelMapImageLoaderTest(data, size);
     return 0;
 }
 
