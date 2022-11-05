182033ad3ec563fdec7b575a3dfbdf3af2121d11
zhaojian@huawei.com <zhaojian2@huawei.com>
2022-11-05 11:02:20 +0800

    格式化问题
    Signed-off-by: zhaojian@huawei.com <zhaojian2@huawei.com>
    Change-Id: I4113c8d56d9ec9b671d1e2996fbfe513e8e5e59d

--- test/fuzztest/util_fuzzer/util_fuzzer.cpp
+++ test/fuzztest/util_fuzzer/util_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,16 +14,21 @@
  */
 
 #include "util_fuzzer.h"
-
+#include "resource_configuration.h"
+#include <string>
 #include <stddef.h>
 #include <stdint.h>
 
 
 namespace OHOS {
+    using namespace OHOS::Ace;
     bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
     {
         bool result = false;
-
+        // ResourceConfiguration r;
+        // uint32_t update = 0;
+        // std::string s;
+        // r.UpdateFromJsonString(s,update);
         return result;
     }
 }
