65d52e8e32851592f05824f9975e340c9a6178d0
harmony_zhangjian <zhangjian112@huawei.com>
2022-11-11 16:06:08 +0800

    [新框架]refresh和abiliycomponent组件补充TDD用例
    Signed-off-by: harmony_zhangjian <zhangjian112@huawei.com>

--- frameworks/core/components_ng/test/pattern/refresh/refresh_pattern_test_ng.cpp
+++ frameworks/core/components_ng/test/pattern/refresh/refresh_pattern_test_ng.cpp
@@ -35,6 +35,9 @@ namespace {
 constexpr double DEFAULT_INDICATOR_OFFSET = 16.0;
 constexpr int32_t DEFAULT_FRICTION_RATIO = 42;
 constexpr int32_t DEFAULT_PARAM = 100;
+const float CONTAINER_WIDTH = 300.0f;
+const float CONTAINER_HEIGHT = 300.0f;
+const SizeF CONTAINER_SIZE(CONTAINER_WIDTH, CONTAINER_HEIGHT);
 } // namespace
 class RefreshPatternTestNg : public testing::Test {
 public:
@@ -54,6 +57,23 @@ HWTEST_F(RefreshPatternTestNg, RefreshObjectTest001, TestSize.Level1)
     modelNG.SetIsShowLastTime(false);
     auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
     EXPECT_TRUE(frameNode != nullptr && frameNode->GetTag() == V2::REFRESH_ETS_TAG);
+    RefPtr<RefreshEventHub> eventHub = frameNode->GetEventHub<RefreshEventHub>();
+    EXPECT_NE(eventHub, nullptr);
+    std::string changeEventValue;
+    auto onChangeEvent = [&changeEventValue](const std::string& param) { changeEventValue = param; };
+    eventHub->SetChangeEvent(std::move(onChangeEvent));
+    int32_t stateChangeValue = -1;
+    auto onStateChangeEvent = [&stateChangeValue](const int32_t param) { stateChangeValue = param; };
+    eventHub->SetOnStateChange(std::move(onStateChangeEvent));
+    bool refreshingValue = false;
+    auto onRefreshingEvent = [&refreshingValue]() { refreshingValue = true; };
+    eventHub->SetOnRefreshing(std::move(onRefreshingEvent));
+    eventHub->FireOnRefreshing();
+    EXPECT_EQ(refreshingValue, true);
+    eventHub->FireChangeEvent("true");
+    EXPECT_EQ(changeEventValue, "true");
+    eventHub->FireOnStateChange(0);
+    EXPECT_EQ(stateChangeValue, 0);
 }
 
 /**
@@ -75,8 +95,23 @@ HWTEST_F(RefreshPatternTestNg, RefreshObjectTest002, TestSize.Level1)
     EXPECT_NE(paintProperty, nullptr);
     RefPtr<RefreshEventHub> eventHub = frameNode->GetEventHub<RefreshEventHub>();
     EXPECT_NE(eventHub, nullptr);
+    std::string changeEventValue;
+    auto onChangeEvent = [&changeEventValue](const std::string& param) { changeEventValue = param; };
+    eventHub->SetChangeEvent(std::move(onChangeEvent));
+    int32_t stateChangeValue = -1;
+    auto onStateChangeEvent = [&stateChangeValue](const int32_t param) { stateChangeValue = param; };
+    eventHub->SetOnStateChange(std::move(onStateChangeEvent));
+    bool refreshingValue = false;
+    auto onRefreshingEvent = [&refreshingValue]() { refreshingValue = true; };
+    eventHub->SetOnRefreshing(std::move(onRefreshingEvent));
     RefPtr<RefreshPattern> pattern = frameNode->GetPattern<RefreshPattern>();
     EXPECT_NE(pattern, nullptr);
+    pattern->FireRefreshing();
+    EXPECT_EQ(refreshingValue, true);
+    pattern->FireChangeEvent("true");
+    EXPECT_EQ(changeEventValue, "true");
+    pattern->FireStateChange(0);
+    EXPECT_EQ(stateChangeValue, 0);
 }
 
 /**
@@ -102,43 +137,145 @@ HWTEST_F(RefreshPatternTestNg, RefreshModelTest001, TestSize.Level1)
  * @tc.desc: Test setting of refresh.
  * @tc.type: FUNC
  */
+HWTEST_F(RefreshPatternTestNg, RefreshModelTest002, TestSize.Level1)
+{
+    RefreshModelNG modelNG;
+    modelNG.Create();
+    modelNG.SetIsShowLastTime(false);
+    Dimension refreshDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
+    modelNG.SetRefreshDistance(refreshDistance);
+    Dimension loadingDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
+    modelNG.SetLoadingDistance(loadingDistance);
+    Dimension progressDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
+    modelNG.SetProgressDistance(progressDistance);
+    Dimension progressDiameter = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
+    modelNG.SetProgressDiameter(progressDiameter);
+    Dimension maxDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
+    modelNG.SetMaxDistance(maxDistance);
+    Dimension showTimeDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
+    modelNG.SetShowTimeDistance(showTimeDistance);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    EXPECT_NE(frameNode, nullptr);
+    RefPtr<RefreshLayoutProperty> layoutProperty = frameNode->GetLayoutProperty<RefreshLayoutProperty>();
+    EXPECT_NE(layoutProperty, nullptr);
+    EXPECT_EQ(layoutProperty->GetLoadingDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
+        loadingDistance.ConvertToPx());
+    EXPECT_EQ(layoutProperty->GetRefreshDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
+        refreshDistance.ConvertToPx());
+    EXPECT_EQ(layoutProperty->GetProgressDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
+        progressDistance.ConvertToPx());
+    EXPECT_EQ(layoutProperty->GetProgressDiameterValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
+        progressDiameter.ConvertToPx());
+    EXPECT_EQ(
+        layoutProperty->GetMaxDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(), maxDistance.ConvertToPx());
+    EXPECT_EQ(layoutProperty->GetShowTimeDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
+        showTimeDistance.ConvertToPx());
+}
+
+/**
+ * @tc.name: RefreshModelTest003
+ * @tc.desc: Test setting of refresh.
+ * @tc.type: FUNC
+ */
 HWTEST_F(RefreshPatternTestNg, RefreshModelTest003, TestSize.Level1)
 {
     RefreshModelNG modelNG;
     modelNG.Create();
     modelNG.SetIsShowLastTime(false);
+    std::string changeEventValue;
+    auto onChangeEvent = [&changeEventValue](const std::string& param) { changeEventValue = param; };
+    modelNG.SetChangeEvent(std::move(onChangeEvent));
+    int32_t stateChangeValue = -1;
+    auto onStateChangeEvent = [&stateChangeValue](const int32_t param) { stateChangeValue = param; };
+    modelNG.SetOnStateChange(std::move(onStateChangeEvent));
+
+    bool refreshingValue = false;
+    auto onRefreshingEvent = [&refreshingValue]() { refreshingValue = true; };
+    modelNG.SetOnRefreshing(std::move(onRefreshingEvent));
     auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    EXPECT_TRUE(frameNode != nullptr && frameNode->GetTag() == V2::REFRESH_ETS_TAG);
     EXPECT_NE(frameNode, nullptr);
-    RefPtr<RefreshLayoutProperty> layoutProperty = frameNode->GetLayoutProperty<RefreshLayoutProperty>();
-    EXPECT_NE(layoutProperty, nullptr);
-    Dimension loadingDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
-    modelNG.SetLoadingDistance(loadingDistance);
-    EXPECT_EQ(layoutProperty->GetLoadingDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
-        loadingDistance.ConvertToPx());
 
-    Dimension refreshDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
-    modelNG.SetRefreshDistance(refreshDistance);
-    EXPECT_EQ(layoutProperty->GetRefreshDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
-        refreshDistance.ConvertToPx());
+    RefPtr<RefreshPattern> pattern = frameNode->GetPattern<RefreshPattern>();
+    EXPECT_NE(pattern, nullptr);
+    pattern->FireRefreshing();
+    EXPECT_EQ(refreshingValue, true);
+    pattern->FireChangeEvent("true");
+    EXPECT_EQ(changeEventValue, "true");
+    pattern->FireStateChange(0);
+    EXPECT_EQ(stateChangeValue, 0);
+}
 
-    Dimension progressDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
-    modelNG.SetProgressDistance(progressDistance);
-    EXPECT_EQ(layoutProperty->GetProgressDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
-        progressDistance.ConvertToPx());
+/**
+ * @tc.name: RefreshPatternTest001
+ * @tc.desc: Test setting of refresh.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RefreshPatternTestNg, RefreshPatternTest001, TestSize.Level1)
+{
+    RefreshModelNG modelNG;
+    modelNG.Create();
+    modelNG.SetIsShowLastTime(false);
+    std::string changeEventValue;
+    auto onChangeEvent = [&changeEventValue](const std::string& param) { changeEventValue = param; };
+    modelNG.SetChangeEvent(std::move(onChangeEvent));
+    int32_t stateChangeValue = -1;
+    auto onStateChangeEvent = [&stateChangeValue](const int32_t param) { stateChangeValue = param; };
+    modelNG.SetOnStateChange(std::move(onStateChangeEvent));
 
-    Dimension progressDiameter = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
-    modelNG.SetProgressDiameter(progressDiameter);
-    EXPECT_EQ(layoutProperty->GetProgressDiameterValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
-        progressDiameter.ConvertToPx());
+    bool refreshingValue = false;
+    auto onRefreshingEvent = [&refreshingValue]() { refreshingValue = true; };
+    modelNG.SetOnRefreshing(std::move(onRefreshingEvent));
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    EXPECT_TRUE(frameNode != nullptr && frameNode->GetTag() == V2::REFRESH_ETS_TAG);
+    EXPECT_NE(frameNode, nullptr);
 
-    Dimension maxDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
-    modelNG.SetMaxDistance(maxDistance);
-    EXPECT_EQ(
-        layoutProperty->GetMaxDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(), maxDistance.ConvertToPx());
+    RefPtr<RefreshPattern> pattern = frameNode->GetPattern<RefreshPattern>();
+    EXPECT_NE(pattern, nullptr);
+    auto atomicNode = pattern->IsAtomicNode();
+    EXPECT_EQ(atomicNode, false);
+}
 
-    Dimension showTimeDistance = Dimension(DEFAULT_PARAM, DimensionUnit::VP);
-    modelNG.SetShowTimeDistance(showTimeDistance);
-    EXPECT_EQ(layoutProperty->GetShowTimeDistanceValue(Dimension(0, DimensionUnit::VP)).ConvertToPx(),
-        showTimeDistance.ConvertToPx());
+/**
+ * @tc.name: RefreshPatternTest001
+ * @tc.desc: Test setting of refresh.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RefreshPatternTestNg, RefreshPatternTest002, TestSize.Level1)
+{
+    RefreshModelNG modelNG;
+    modelNG.Create();
+    modelNG.SetIsShowLastTime(false);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    EXPECT_TRUE(frameNode != nullptr && frameNode->GetTag() == V2::REFRESH_ETS_TAG);
+
+    // Create LayoutWrapper and set ratingLayoutAlgorithm.
+    RefPtr<GeometryNode> geometryNode = AceType::MakeRefPtr<GeometryNode>();
+    EXPECT_FALSE(geometryNode == nullptr);
+    LayoutWrapper layoutWrapper = LayoutWrapper(frameNode, geometryNode, frameNode->GetLayoutProperty());
+    auto refreshLayoutProperty = frameNode->GetLayoutProperty<RefreshLayoutProperty>();
+    auto refreshPattern = frameNode->GetPattern<RefreshPattern>();
+    EXPECT_FALSE(refreshPattern == nullptr);
+    auto refreshLayoutAlgorithm = refreshPattern->CreateLayoutAlgorithm();
+    EXPECT_FALSE(refreshLayoutAlgorithm == nullptr);
+    layoutWrapper.SetLayoutAlgorithm(AceType::MakeRefPtr<LayoutAlgorithmWrapper>(refreshLayoutAlgorithm));
+
+    // Refresh without setting height and width.
+    const LayoutConstraintF layoutConstraint;
+    layoutWrapper.GetLayoutProperty()->UpdateLayoutConstraint(layoutConstraint);
+    layoutWrapper.GetLayoutProperty()->UpdateContentConstraint();
+    // Calculate the size and offset.
+    refreshLayoutAlgorithm->Measure(&layoutWrapper);
+    refreshLayoutAlgorithm->Layout(&layoutWrapper);
+    // Test the default size set in theme and the offset.
+    EXPECT_EQ(layoutWrapper.GetGeometryNode()->GetFrameSize(), SizeF(0, 0));
+    EXPECT_EQ(layoutWrapper.GetGeometryNode()->GetFrameOffset(), OffsetF(0.0, 0.0));
+    LayoutConstraintF layoutConstraintSize;
+    layoutConstraintSize.selfIdealSize.SetSize(CONTAINER_SIZE);
+    layoutWrapper.GetLayoutProperty()->UpdateLayoutConstraint(layoutConstraintSize);
+    layoutWrapper.GetLayoutProperty()->UpdateContentConstraint();
+    refreshLayoutAlgorithm->Measure(&layoutWrapper);
+    // Test the size set by codes.
+    EXPECT_EQ(layoutWrapper.GetGeometryNode()->GetFrameSize(), SizeF(CONTAINER_SIZE));
 }
 } // namespace OHOS::Ace::NG
