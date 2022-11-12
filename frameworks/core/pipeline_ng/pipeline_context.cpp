534b162a3491489dc6338fd7268a795352ea4b49
yan-shuifeng <yanshuifeng@huawei.com>
2022-11-12 10:41:23 +0800

    fix the problem of id leadk in partial update
    Signed-off-by: yan-shuifeng <yanshuifeng@huawei.com>
    Change-Id: Ib1e5bf0b12442d5af4970b47b5bb28ca21250df0

--- frameworks/core/pipeline_ng/pipeline_context.cpp
+++ frameworks/core/pipeline_ng/pipeline_context.cpp
@@ -826,7 +826,6 @@ void PipelineContext::Destroy()
     drawDelegate_.reset();
     touchEvents_.clear();
     buildFinishCallbacks_.clear();
-    ElementRegister::GetInstance()->ClearInstance();
 }
 
 void PipelineContext::AddBuildFinishCallBack(std::function<void()>&& callback)
