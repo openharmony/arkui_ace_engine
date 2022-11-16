53724b9efd2cb6850979146fabbbacc6c9d78937
redjie <hongshaojie@h-partners.com>
2022-11-16 18:11:02 +0800

    fixed 6f1680e from https://gitee.com/redjie/arkui_ace_engine/pulls/6866 补充source_map和utils的TDD
    Signed-off-by: redjie <hongshaojie@h-partners.com>
    Change-Id: Iec4cc594c9921de04ddcdf98149f1d7db21a89f9

--- frameworks/bridge/test/unittest/jsfrontend/source_map/source_map_test.cpp
+++ frameworks/bridge/test/unittest/jsfrontend/source_map/source_map_test.cpp
@@ -25,6 +25,8 @@ namespace {
 const std::string FILESTR = "./pages/dfxtest.js";
 const std::string SOURCESTR = "source.json";
 const std::string NULLSTR = "";
+const std::string ORIGINAL_NAME_ONE = "SourceCode:\n   RedjieRedjie002^";
+const std::string ORIGINAL_NAME_TWO = "SourceCode:\n001001Wau^002";
 } // namespace
 
 class SourceMapTest : public testing::Test {
@@ -68,28 +70,84 @@ HWTEST_F(SourceMapTest, GetOriginalNames001, TestSize.Level1)
 
 /**
  * @tc.name: GetOriginalNames002
- * @tc.desc: Test get original names from sourceCode which doesn't have "SourceCode:\n".
+ * @tc.desc: Test get original names from sourceCode which has "SourceCode:\n".
  * @tc.type: FUNC
  * @tc.require: issueI5IZXS
  */
 HWTEST_F(SourceMapTest, GetOriginalNames002, TestSize.Level1)
 {
     /**
-     * @tc.steps: step1. Input a string which doesn't have "SourceCode:\n" as sourceCode to get original names.
+     * @tc.steps: step1. Input a string which has "SourceCode:\n" as sourceCode to get original names.
+     * NameMap has one elements.
      * @tc.expected: step1. Output is the same as the input string.
      */
     std::string pagemapStr = "{\"version\":3,"
                              "\"file\":\"./pages/dfxtest.js\","
                              "\"mappings\":\";\","
                              "\"sources\":[],"
+                             "\"nameMap\":\"Redjie\","
                              "\"names\":[\"_ohos_router_1\",\"router\",\"_ohos_process_1\",\"process\"]}";
-    std::string sourceCode = "sourceCode";
+    std::string sourceCode = "SourceCode:\n";
     uint32_t errorPos = 0;
 
     RevSourceMap pageMap;
     pageMap.Init(pagemapStr);
     std::string result = pageMap.GetOriginalNames(sourceCode, errorPos);
     ASSERT_EQ(result, sourceCode);
+}
+
+/**
+ * @tc.name: GetOriginalNames003
+ * @tc.desc: Test get original names from sourceCode which has "SourceCode:\n".
+ * @tc.type: FUNC
+ */
+HWTEST_F(SourceMapTest, GetOriginalNames003, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. Input a string which has "SourceCode:\n" as sourceCode to get original names.
+     * NameMap has two elements.
+     * @tc.expected: step1. Output is the same as the input string.
+     */
+    std::string pagemapStr = "{\"version\":3,"
+                             "\"file\":\"./pages/dfxtest.js\","
+                             "\"mappings\":\";\","
+                             "\"sources\":[],"
+                             "\"nameMap\":[\"001\",\"Redjie\"],"
+                             "\"names\":[\"_ohos_router_1\",\"router\",\"_ohos_process_1\",\"process\"]}";
+    std::string sourceCode = "SourceCode:\n001Redjie002^";
+    uint32_t errorPos = 16;
+
+    RevSourceMap pageMap;
+    pageMap.Init(pagemapStr);
+    std::string result = pageMap.GetOriginalNames(sourceCode, errorPos);
+    ASSERT_EQ(result, ORIGINAL_NAME_ONE);
+}
+
+/**
+ * @tc.name: GetOriginalNames004
+ * @tc.desc: Test get original names from sourceCode which has "SourceCode:\n".
+ * @tc.type: FUNC
+ */
+HWTEST_F(SourceMapTest, GetOriginalNames004, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. Input a string which has "SourceCode:\n" as sourceCode to get original names.
+     * NameMap has two elements.
+     * @tc.expected: step1. Get the original names as expected.
+     */
+    std::string pagemapStr = "{\"version\":3,"
+                             "\"file\":\"./pages/dfxtest.js\","
+                             "\"mappings\":\";\","
+                             "\"sources\":[],"
+                             "\"nameMap\":[\"Redjie\",\"001\"],"
+                             "\"names\":[\"_ohos_router_1\",\"router\",\"_ohos_process_1\",\"process\"]}";
+    std::string sourceCode = "SourceCode:\nRedjie001Wangwu^002";
+    uint32_t errorPos = 13;
+
+    RevSourceMap pageMap;
+    pageMap.Init(pagemapStr);
+    std::string result = pageMap.GetOriginalNames(sourceCode, errorPos);
+    ASSERT_EQ(result, ORIGINAL_NAME_TWO);
 }
 
 /**
@@ -117,6 +175,17 @@ HWTEST_F(SourceMapTest, FindSourcesString001, TestSize.Level1)
     ASSERT_EQ(pageMapInfo.row, row);
     ASSERT_EQ(pageMapInfo.col, col);
     ASSERT_EQ(pageMapInfo.sources, FILESTR);
+
+    /**
+     * @tc.steps: step2. Initialize a refPtr to find the content of string
+     * @tc.expected: step2. Get the content of file
+     */
+    auto curMapData = Referenced::MakeRefPtr<RevSourceMap>();
+    pageMap.MergeInit(pagemapStr, curMapData);
+    pageMapInfo = curMapData->Find(row, col);
+    ASSERT_EQ(pageMapInfo.row, row);
+    ASSERT_EQ(pageMapInfo.col, col);
+    ASSERT_EQ(pageMapInfo.sources, FILESTR);
 }
 
 /**
@@ -141,6 +210,17 @@ HWTEST_F(SourceMapTest, FindSourcesString002, TestSize.Level1)
     RevSourceMap pageMap;
     pageMap.Init(pagemapStr);
     auto pageMapInfo = pageMap.Find(row, col);
+    ASSERT_EQ(pageMapInfo.row, row);
+    ASSERT_EQ(pageMapInfo.col, col);
+    ASSERT_EQ(pageMapInfo.sources, SOURCESTR);
+
+    /**
+     * @tc.steps: step2. Initialize a refPtr to find the content of string
+     * @tc.expected: step2. Get the content of source
+     */
+    auto curMapData = Referenced::MakeRefPtr<RevSourceMap>();
+    pageMap.MergeInit(pagemapStr, curMapData);
+    pageMapInfo = curMapData->Find(row, col);
     ASSERT_EQ(pageMapInfo.row, row);
     ASSERT_EQ(pageMapInfo.col, col);
     ASSERT_EQ(pageMapInfo.sources, SOURCESTR);
