fbb27a42ffcca980d611f80ccc3621493d2e5ea3
zhaojian@huawei.com <zhaojian2@huawei.com>
2022-11-22 10:51:10 +0800

    fixed 3090ced from https://gitee.com/zhaojian2021/arkui_ace_engine/pulls/7178 修正out of ragion,mark热区提前退出问题
    Signed-off-by: zhaojian@huawei.com <zhaojian2@huawei.com>
    Change-Id: I7a2880c32fe0935d1f769c076d51edb1bf3007f4

--- frameworks/core/components_ng/base/frame_node.cpp
+++ frameworks/core/components_ng/base/frame_node.cpp
@@ -203,13 +203,6 @@ void FrameNode::OnAttachToMainTree()
     UINode::OnAttachToMainTree();
     eventHub_->FireOnAppear();
     renderContext_->OnNodeAppear();
-    if (!hasPendingRequest_) {
-        return;
-    }
-    auto context = GetContext();
-    CHECK_NULL_VOID(context);
-    context->RequestFrame();
-    hasPendingRequest_ = false;
     if (IsResponseRegion() || renderContext_->HasPosition() || renderContext_->HasOffset() ||
         renderContext_->HasAnchor()) {
         auto parent = GetParent();
@@ -221,6 +214,13 @@ void FrameNode::OnAttachToMainTree()
             parent = parent->GetParent();
         }
     }
+    if (!hasPendingRequest_) {
+        return;
+    }
+    auto context = GetContext();
+    CHECK_NULL_VOID(context);
+    context->RequestFrame();
+    hasPendingRequest_ = false;
 }
 
 void FrameNode::OnDetachFromMainTree()
