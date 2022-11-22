4f92eb115f2b021e22ff1479d3f2c9af8f564a6c
dubj <dubingjian@huawei.com>
2022-11-22 10:21:11 +0800

    解决圆角显示问题
    Signed-off-by: dubj <dubingjian@huawei.com>
    Change-Id: I904c01adcbde5bc25c1ed9c34b38385c330a3cfd

--- frameworks/core/pipeline_ng/pipeline_context.h
+++ frameworks/core/pipeline_ng/pipeline_context.h
@@ -231,6 +231,7 @@ public:
     void RemoveNodesToNotifyMemoryLevel(int32_t nodeId);
     void NotifyMemoryLevel(int32_t level) override;
     void FlushMessages() override;
+    void SetContainerWindow(bool isShow) override;
 
     void FlushUITasks() override
     {
