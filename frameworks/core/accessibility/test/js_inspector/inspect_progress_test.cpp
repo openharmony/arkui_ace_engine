19ab40154340c439358365ee6ec6b6aa35fdcc0a
zhangbingce <zhangbingce@huawei.com>
2022-11-23 00:12:13 +0800

    [TDD]js_inspector bugfix and increase coverage rate 1122&&1123
    Signed-off-by: zhangbingce <zhangbingce@huawei.com>
    Change-Id: I9131332a7544c19cb7d0a6f68f50fb3d4cead4ec

--- frameworks/core/accessibility/test/js_inspector/inspect_progress_test.cpp
+++ frameworks/core/accessibility/test/js_inspector/inspect_progress_test.cpp
@@ -28,6 +28,34 @@ class InspectProgressTest : public testing::Test {
 public:
     static void SetUpTestSuite() {};
     static void TearDownTestSuite() {};
+
+    void CheckProgressAttrAndStyle(InspectProgress& inspectProgress, uint16_t attrsSize, uint16_t stylesSize)
+    {
+        uint16_t attrsSizeInsert = 5;
+        uint16_t stylesSizeInsert = 1;
+        DeviceType deviceType = SystemProperties::GetDeviceType();
+        EXPECT_EQ(inspectProgress.attrs_["type"], "horizontal");
+        EXPECT_EQ(inspectProgress.attrs_["focusable"], "true");
+        EXPECT_EQ(inspectProgress.attrs_["percent"], "0");
+        EXPECT_EQ(inspectProgress.attrs_["secondarypercent"], "0");
+        EXPECT_EQ(inspectProgress.attrs_["clockwise"], "true");
+        if (deviceType == DeviceType::PHONE) {
+            EXPECT_EQ(inspectProgress.styles_["color"], "#ff007dff");
+            stylesSizeInsert++;
+        } else if (deviceType == DeviceType::TV) {
+            EXPECT_EQ(inspectProgress.styles_["color"], "#e5ffffff");
+            stylesSizeInsert++;
+        } else if (deviceType == DeviceType::WATCH) {
+            EXPECT_EQ(inspectProgress.styles_["color"], "#ff45a5ff");
+            stylesSizeInsert++;
+        }
+        EXPECT_EQ(inspectProgress.styles_["stroke-width"], "4px");
+        EXPECT_EQ(inspectProgress.styles_.find("background-size"), inspectProgress.styles_.end());
+        EXPECT_EQ(inspectProgress.styles_.find("background-repeat"), inspectProgress.styles_.end());
+        EXPECT_EQ(inspectProgress.styles_.find("background-position"), inspectProgress.styles_.end());
+        EXPECT_EQ(inspectProgress.attrs_.size(), attrsSize + attrsSizeInsert);
+        EXPECT_EQ(inspectProgress.styles_.size(), stylesSize + stylesSizeInsert);
+    }
 };
 
 /**
@@ -46,7 +74,7 @@ HWTEST_F(InspectProgressTest, InspectProgressTest001, TestSize.Level1)
 
 /**
  * @tc.name: InspectProgressTest002
- * @tc.desc: InspectProgress::PackAttrAndStyle
+ * @tc.desc: InspectProgress::PackAttrAndStyle-PHONE
  * @tc.type: FUNC
  */
 HWTEST_F(InspectProgressTest, InspectProgressTest002, TestSize.Level1)
@@ -56,21 +84,111 @@ HWTEST_F(InspectProgressTest, InspectProgressTest002, TestSize.Level1)
     InspectProgress inspectProgress(nodeId, tag);
     auto attrsSize = inspectProgress.attrs_.size();
     auto stylesSize = inspectProgress.styles_.size();
-    uint16_t attrsSizeInsert = 5;
-    uint16_t stylesSizeInsert = 2;
 
+    DeviceType deviceType = SystemProperties::GetDeviceType();
+    SystemProperties::SetDeviceType(DeviceType::PHONE);
     inspectProgress.PackAttrAndStyle();
-    EXPECT_EQ(inspectProgress.attrs_.size(), attrsSize + attrsSizeInsert);
-    EXPECT_EQ(inspectProgress.attrs_["type"], "horizontal");
-    EXPECT_EQ(inspectProgress.attrs_["focusable"], "true");
-    EXPECT_EQ(inspectProgress.attrs_["percent"], "0");
-    EXPECT_EQ(inspectProgress.attrs_["secondarypercent"], "0");
-    EXPECT_EQ(inspectProgress.attrs_["clockwise"], "true");
-    EXPECT_EQ(inspectProgress.styles_.size(), stylesSize + stylesSizeInsert);
-    EXPECT_EQ(inspectProgress.styles_["color"], "#ff007dff");
-    EXPECT_EQ(inspectProgress.styles_["stroke-width"], "4px");
-    EXPECT_EQ(inspectProgress.styles_.find("background-size"), inspectProgress.styles_.end());
-    EXPECT_EQ(inspectProgress.styles_.find("background-repeat"), inspectProgress.styles_.end());
-    EXPECT_EQ(inspectProgress.styles_.find("background-position"), inspectProgress.styles_.end());
+    CheckProgressAttrAndStyle(inspectProgress, attrsSize, stylesSize);
+    SystemProperties::SetDeviceType(deviceType);
+}
+
+/**
+ * @tc.name: InspectProgressTest003
+ * @tc.desc: InspectProgress::PackAttrAndStyle-TV
+ * @tc.type: FUNC
+ */
+HWTEST_F(InspectProgressTest, InspectProgressTest003, TestSize.Level1)
+{
+    NodeId nodeId = -1;
+    std::string tag = "tagTest";
+    InspectProgress inspectProgress(nodeId, tag);
+    auto attrsSize = inspectProgress.attrs_.size();
+    auto stylesSize = inspectProgress.styles_.size();
+
+    DeviceType deviceType = SystemProperties::GetDeviceType();
+    SystemProperties::SetDeviceType(DeviceType::TV);
+    inspectProgress.PackAttrAndStyle();
+    CheckProgressAttrAndStyle(inspectProgress, attrsSize, stylesSize);
+    SystemProperties::SetDeviceType(deviceType);
+}
+
+/**
+ * @tc.name: InspectProgressTest004
+ * @tc.desc: InspectProgress::PackAttrAndStyle-WATCH
+ * @tc.type: FUNC
+ */
+HWTEST_F(InspectProgressTest, InspectProgressTest004, TestSize.Level1)
+{
+    NodeId nodeId = -1;
+    std::string tag = "tagTest";
+    InspectProgress inspectProgress(nodeId, tag);
+    auto attrsSize = inspectProgress.attrs_.size();
+    auto stylesSize = inspectProgress.styles_.size();
+
+    DeviceType deviceType = SystemProperties::GetDeviceType();
+    SystemProperties::SetDeviceType(DeviceType::WATCH);
+    inspectProgress.PackAttrAndStyle();
+    CheckProgressAttrAndStyle(inspectProgress, attrsSize, stylesSize);
+    SystemProperties::SetDeviceType(deviceType);
+}
+
+/**
+ * @tc.name: InspectProgressTest005
+ * @tc.desc: InspectProgress::PackAttrAndStyle-CAR
+ * @tc.type: FUNC
+ */
+HWTEST_F(InspectProgressTest, InspectProgressTest005, TestSize.Level1)
+{
+    NodeId nodeId = -1;
+    std::string tag = "tagTest";
+    InspectProgress inspectProgress(nodeId, tag);
+    auto attrsSize = inspectProgress.attrs_.size();
+    auto stylesSize = inspectProgress.styles_.size();
+
+    DeviceType deviceType = SystemProperties::GetDeviceType();
+    SystemProperties::SetDeviceType(DeviceType::CAR);
+    inspectProgress.PackAttrAndStyle();
+    CheckProgressAttrAndStyle(inspectProgress, attrsSize, stylesSize);
+    SystemProperties::SetDeviceType(deviceType);
+}
+
+/**
+ * @tc.name: InspectProgressTest006
+ * @tc.desc: InspectProgress::PackAttrAndStyle-TABLET
+ * @tc.type: FUNC
+ */
+HWTEST_F(InspectProgressTest, InspectProgressTest006, TestSize.Level1)
+{
+    NodeId nodeId = -1;
+    std::string tag = "tagTest";
+    InspectProgress inspectProgress(nodeId, tag);
+    auto attrsSize = inspectProgress.attrs_.size();
+    auto stylesSize = inspectProgress.styles_.size();
+
+    DeviceType deviceType = SystemProperties::GetDeviceType();
+    SystemProperties::SetDeviceType(DeviceType::TABLET);
+    inspectProgress.PackAttrAndStyle();
+    CheckProgressAttrAndStyle(inspectProgress, attrsSize, stylesSize);
+    SystemProperties::SetDeviceType(deviceType);
+}
+
+/**
+ * @tc.name: InspectProgressTest007
+ * @tc.desc: InspectProgress::PackAttrAndStyle-UNKNOWN
+ * @tc.type: FUNC
+ */
+HWTEST_F(InspectProgressTest, InspectProgressTest007, TestSize.Level1)
+{
+    NodeId nodeId = -1;
+    std::string tag = "tagTest";
+    InspectProgress inspectProgress(nodeId, tag);
+    auto attrsSize = inspectProgress.attrs_.size();
+    auto stylesSize = inspectProgress.styles_.size();
+
+    DeviceType deviceType = SystemProperties::GetDeviceType();
+    SystemProperties::SetDeviceType(DeviceType::UNKNOWN);
+    inspectProgress.PackAttrAndStyle();
+    CheckProgressAttrAndStyle(inspectProgress, attrsSize, stylesSize);
+    SystemProperties::SetDeviceType(deviceType);
 }
 } // namespace OHOS::Ace::Framework
