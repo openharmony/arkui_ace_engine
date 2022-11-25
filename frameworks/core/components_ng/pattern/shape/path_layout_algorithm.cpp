a0c98af6041daba4a9a805b865b0cfe87dde29bd
sunbees <sunqinjia@huawei.com>
2022-11-25 16:06:47 +0800

    fixed 146116d from https://gitee.com/sunbees/ace_ace_engine/pulls/7478 fix path
    Signed-off-by: sunbees <sunqinjia@huawei.com>

---
+++ frameworks/core/components_ng/pattern/shape/path_layout_algorithm.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "core/components_ng/pattern/shape/path_layout_algorithm.h"
+
+#include "include/utils/SkParsePath.h"
+
+#include "base/geometry/dimension.h"
+#include "base/geometry/ng/size_t.h"
+#include "base/utils/utils.h"
+#include "core/components_ng/base/frame_node.h"
+#include "core/components_ng/pattern/shape/path_paint_property.h"
+
+namespace OHOS::Ace::NG {
+std::optional<SizeF> PathLayoutAlgorithm::MeasureContent(
+    const LayoutConstraintF& contentConstraint, LayoutWrapper* layoutWrapper)
+{
+    if (contentConstraint.selfIdealSize.IsValid()) {
+        return contentConstraint.selfIdealSize.ConvertToSizeT();
+    }
+
+    auto host = layoutWrapper->GetHostNode();
+    CHECK_NULL_RETURN(host, std::nullopt);
+    auto paintProperty = host->GetPaintProperty<PathPaintProperty>();
+    CHECK_NULL_RETURN(paintProperty, std::nullopt);
+
+    auto pathCommands = paintProperty->GetCommandsValue("");
+    CHECK_NULL_RETURN(!pathCommands.empty(), SizeF());
+    SkPath skPath;
+    bool ret = SkParsePath::FromSVGString(pathCommands.c_str(), &skPath);
+    CHECK_NULL_RETURN(ret, SizeF());
+    auto skRect = skPath.getBounds();
+    auto width = skRect.width();
+    auto height = skRect.height();
+    if (NearZero(width) && NearZero(height)) {
+        return SizeF();
+    }
+    auto lineWidth = static_cast<float>(paintProperty->GetStrokeWidthValue().ConvertToPx());
+    if (NearZero(width)) {
+        width += lineWidth;
+    }
+    if (NearZero(height)) {
+        height += lineWidth;
+    }
+    return SizeF(width, height);
+}
+} // namespace OHOS::Ace::NG
