5931959e944fcc9046f27aa90f8e576f0ace5f0d
limeng <limeng208@huawei.com>
2022-12-01 02:38:03 +0000

    update adapter/ohos/entrance/subwindow/subwindow_ohos.h. update code
    Signed-off-by: limeng <limeng208@huawei.com>

--- adapter/ohos/entrance/subwindow/subwindow_ohos.h
+++ adapter/ohos/entrance/subwindow/subwindow_ohos.h
@@ -115,8 +115,8 @@ private:
     std::shared_ptr<AppExecFwk::EventRunner> eventLoop_;
     std::shared_ptr<AppExecFwk::EventHandler> handler_;
     int32_t targetId_ = -1;
-    int32_t popupTargetId_ = -1;
     bool isToastWindow_ = false;
+    int32_t popupTargetId_ = -1;
     bool isShowed_ = false;
 };
 
