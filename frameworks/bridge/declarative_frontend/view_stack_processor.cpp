919d4f13012f81776d57bb437a40b08f9b5ea055
zhuzijia <zhuzijia@h-partners.com>
2023-01-03 09:08:28 +0800

    fixed 95655a2 from https://gitee.com/zhuzijia/ace_ace_engine/pulls/8705 fix indexer fast preview bug
    Signed-off-by: zhuzijia <zhuzijia@h-partners.com>
    Change-Id: I8109ed6c89e497f92dcb0a41d2a56c321e8ca333

--- frameworks/bridge/declarative_frontend/view_stack_processor.cpp
+++ frameworks/bridge/declarative_frontend/view_stack_processor.cpp
@@ -519,7 +519,7 @@ void ViewStackProcessor::PopContainer()
 
     while ((!componentGroup && !multiComposedComponent && !soleChildComponent) ||
            strcmp(type, AceType::TypeName<TextSpanComponent>()) == 0) {
-        if (componentsStack_.size() == 1) {
+        if (componentsStack_.size() <= 1) {
             break;
         }
         Pop();
