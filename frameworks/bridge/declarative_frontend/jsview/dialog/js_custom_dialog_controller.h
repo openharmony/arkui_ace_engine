0375a876018a34e41dd409f110d86ebc4b09432b
jinweiliu <hw.liujinwei@huawei.com>
2023-01-09 13:59:18 +0000

    Add acetype
    Signed-off-by: jinweiliu <hw.liujinwei@huawei.com>
    Change-Id: I9e6b7b16f5411dc8500ecdee4d0b4c8b3878e987

--- frameworks/bridge/declarative_frontend/jsview/dialog/js_custom_dialog_controller.h
+++ frameworks/bridge/declarative_frontend/jsview/dialog/js_custom_dialog_controller.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,7 +27,9 @@ enum class DialogOperation {
     DIALOG_CLOSE,
 };
 
-class JSCustomDialogController {
+class JSCustomDialogController : public virtual AceType {
+    DECLARE_ACE_TYPE(JSCustomDialogController, AceType)
+
 public:
     explicit JSCustomDialogController(JSView* ownerView) : ownerView_(ownerView) {}
     ~JSCustomDialogController() = default;
