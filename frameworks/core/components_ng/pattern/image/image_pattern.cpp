e67f34df9fca9693d68ceec7c1d3adbe32a77ec0
nickyjd <yujiadong3@huawei.com>
2023-01-17 10:47:10 +0800

    fixed 90a6fd4 from https://gitee.com/nickyjd/arkui_ace_engine/pulls/9104 修复浮层文本在图片区域外不显示的问题
    Signed-off-by: nickyjd <yujiadong3@huawei.com>
    Change-Id: I489fe602e2d51b72922c3bce156a110489b2fd7e

--- frameworks/core/components_ng/pattern/image/image_pattern.cpp
+++ frameworks/core/components_ng/pattern/image/image_pattern.cpp
@@ -372,7 +372,7 @@ void ImagePattern::OnAttachToFrameNode()
 {
     auto host = GetHost();
     CHECK_NULL_VOID(host);
-    host->GetRenderContext()->SetClipToBounds(true);
+    host->GetRenderContext()->SetClipToBounds(false);
 }
 
 } // namespace OHOS::Ace::NG
