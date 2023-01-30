251edd7b5c5a37d5d8fb7f0ad67e0def3a130924
bixuefeng <bixuefeng@huawei.com>
2023-01-30 15:07:58 +0800

    Bugfix: NG request main pipeline node's focus failed while on sub pipeline
    Signed-off-by: bixuefeng <bixuefeng@huawei.com>
    Change-Id: Ib49885d3e570c39144d08844457dc4c35f64ae3c

--- frameworks/core/pipeline/pipeline_context.h
+++ frameworks/core/pipeline/pipeline_context.h
@@ -776,16 +776,6 @@ public:
 
     std::string GetRestoreInfo(int32_t restoreId);
 
-    void SetIsSubPipeline(bool isSubPipeline)
-    {
-        isSubPipeline_ = isSubPipeline;
-    }
-
-    bool IsSubPipeline() const
-    {
-        return isSubPipeline_;
-    }
-
     bool GetIsDragStart() const
     {
         return isDragStart_;
@@ -822,11 +812,6 @@ public:
     void SetRootRect(double width, double height, double offset = 0.0) override
     {
         SetRootSizeWithWidthHeight(width, height, offset);
-    }
-
-    void SetParentPipeline(const WeakPtr<PipelineBase>& pipeline)
-    {
-        parentPipeline_ = pipeline;
     }
 
     void SetContainerWindow(bool isShow) override;
@@ -1037,9 +1022,6 @@ private:
 
     std::unordered_map<int32_t, WeakPtr<RenderElement>> storeNode_;
     std::unordered_map<int32_t, std::string> restoreNodeInfo_;
-
-    bool isSubPipeline_ = false;
-    WeakPtr<PipelineBase> parentPipeline_;
 
     std::unordered_map<ComposeId, std::list<VisibleCallbackInfo>> visibleAreaChangeNodes_;
 
