302c8d0189e88d5053fb08ba63431bfaee2a3345
zhengqiyi <zhengqiyi2@huawei.com>
2023-02-02 11:46:28 +0000

    cherry pick frameNode TouchTest
    Signed-off-by: zhengqiyi <zhengqiyi2@huawei.com>

--- frameworks/core/components_ng/base/frame_node.h
+++ frameworks/core/components_ng/base/frame_node.h
@@ -299,6 +299,8 @@ public:
 
     void AddHotZoneRect(const DimensionRect& hotZoneRect) const;
 
+    bool IsOutOfTouchTestRegion(const PointF& parentLocalPoint);
+    
 private:
     void MarkNeedRender(bool isRenderBoundary);
     bool IsNeedRequestParentMeasure() const;
