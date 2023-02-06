8005c4e5dbb080fdd12f98c3a9c3b532a2738cee
huzeqi <huzeqi@huawei.com>
2023-02-06 21:36:46 +0800

    stage old module need not notify first show
    Signed-off-by: huzeqi <huzeqi@huawei.com>
    Change-Id: I332ba140b6740fad3e1e7b67244a91c8a1d3eaff

--- frameworks/bridge/js_frontend/frontend_delegate_impl.cpp
+++ frameworks/bridge/js_frontend/frontend_delegate_impl.cpp
@@ -1305,11 +1305,7 @@ void FrontendDelegateImpl::PushPageTransitionListener(
     const TransitionEvent& event, const RefPtr<JsAcePage>& page)
 {
     if (event == TransitionEvent::PUSH_END) {
-        if (isMainPage_) {
-            isMainPage_ = false;
-        } else {
-            OnPageShow();
-        }
+        OnPageShow();
     }
 }
 
