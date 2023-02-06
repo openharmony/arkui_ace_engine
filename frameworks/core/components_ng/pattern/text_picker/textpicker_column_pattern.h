6f2bd4e415d3e71f172332846cd890bfa0a5f635
zhangxiao72 <zhangxiao72@huawei.com>
2023-02-06 15:18:46 +0800

    【monthly】picker 按压时会闪，由于按压时触发了MOVE的状态 和selected 时间限制
    Change-Id: I4f4ff1bd4e3b009c4849203d30f923a2687942e2
    Signed-off-by: zhangxiao72 <zhangxiao72@huawei.com>

--- frameworks/core/components_ng/pattern/text_picker/textpicker_column_pattern.h
+++ frameworks/core/components_ng/pattern/text_picker/textpicker_column_pattern.h
@@ -176,6 +176,16 @@ public:
     {
         EventCallback_ = value;
     }
+  
+    void SetLocalDownDistance(float value)
+    {
+        localDownDistance_ = value;
+    }
+
+    float GetLocalDownDistance() const
+    {
+        return localDownDistance_;
+    }
 
     void UpdateToss(double offsetY);
 
@@ -206,6 +216,7 @@ private:
     void SetButtonBackgroundColor(const Color& pressColor);
     void PlayPressAnimation(const Color& pressColor);
 
+    float localDownDistance_ = 0.0f;
     Color pressColor_;
     Color hoverColor_;
     EventCallback EventCallback_;
