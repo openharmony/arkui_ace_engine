2ac846b948781cf4a7afffde09da2ef66c4c226f
hehongyang9 <hehongyang@huawei.com>
2023-02-13 04:22:32 +0000

    fix no picture from camera
    Signed-off-by: hehongyang9 <hehongyang@huawei.com>
    Change-Id: I6d1f7c66bae805d63c7cf8728785f00137c9c183

--- frameworks/core/pipeline/pipeline_context.cpp
+++ frameworks/core/pipeline/pipeline_context.cpp
@@ -1136,6 +1136,12 @@ void PipelineContext::PushPage(const RefPtr<PageComponent>& pageComponent, const
         stageElement->PushPage(display);
     }
 
+#if defined(ENABLE_ROSEN_BACKEND)
+    if (GetIsDeclarative()) {
+        FlushBuild();
+        return;
+    }
+#endif
 #if defined(ENABLE_NATIVE_VIEW)
     if (GetIsDeclarative()) {
         // if not use flutter scheduler, can flush pipeline immediately.
