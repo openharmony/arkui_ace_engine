a99494c7ce97dd7c9c54895e2000afe4eb4e3184
zhaoxinyu <zhaoxinyu20@huawei.com>
2023-02-13 07:12:28 +0000

    fixed 138ddd0 from https://gitee.com/zhao-xinyu007/arkui_ace_engine/pulls/9889 fix textInput mouse event
    Signed-off-by: zhaoxinyu <zhaoxinyu20@huawei.com>
    Change-Id: I781b28dc113bcd2f5ec044c803c5e2d36316efea

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -1862,6 +1862,7 @@ void TextFieldPattern::HandleMouseEvent(MouseInfo& info)
     auto pipeline = PipelineContext::GetCurrentContext();
     CHECK_NULL_VOID(pipeline);
 
+    info.SetStopPropagation(true);
     auto focusHub = GetHost()->GetOrCreateFocusHub();
     if (info.GetButton() == MouseButton::RIGHT_BUTTON) {
         LOGI("Handle mouse right click");
@@ -1883,7 +1884,6 @@ void TextFieldPattern::HandleMouseEvent(MouseInfo& info)
             mouseStatus_ = MouseStatus::RELEASED;
             GetHost()->MarkDirtyNode(PROPERTY_UPDATE_MEASURE_SELF);
         }
-        info.SetStopPropagation(true);
         return;
     }
     if (info.GetAction() == MouseAction::PRESS) {
@@ -1935,7 +1935,6 @@ void TextFieldPattern::HandleMouseEvent(MouseInfo& info)
         mouseStatus_ = MouseStatus::MOVE;
         GetHost()->MarkDirtyNode(PROPERTY_UPDATE_MEASURE_SELF);
     }
-    info.SetStopPropagation(true);
 }
 
 void TextFieldPattern::UpdatePositionOfParagraph(int32_t position)
