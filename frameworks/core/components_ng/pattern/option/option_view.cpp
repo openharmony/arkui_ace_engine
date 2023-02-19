8ec88f22aba1d48bbd1fde3b4466fb465f6f29c9
zhaoxinyu <zhaoxinyu20@huawei.com>
2023-02-19 02:06:19 +0000

    fix select and menu TDD
    Signed-off-by: zhaoxinyu <zhaoxinyu20@huawei.com>
    Change-Id: If92f7ea67ba2adfc922e77f207f1ead2dbc35cf0

--- frameworks/core/components_ng/pattern/option/option_view.cpp
+++ frameworks/core/components_ng/pattern/option/option_view.cpp
@@ -19,7 +19,6 @@
 #include "base/memory/referenced.h"
 #include "base/utils/utils.h"
 #include "core/components/select/select_theme.h"
-#include "core/components/text_field/textfield_theme.h"
 #include "core/components_ng/base/frame_node.h"
 #include "core/components_ng/base/view_stack_processor.h"
 #include "core/components_ng/pattern/image/image_model_ng.h"
