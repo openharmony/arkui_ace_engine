f005e6407dde0eabb95b4ecbe5d9037e705f4ec3
xiexiyun <xiexiyun@huawei.com>
2023-02-26 17:41:55 +0800

    allow paste on any type of copy option
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>
    Change-Id: I70ae70dea47f79c3b2a40ddc0d7eeafd8b1844c7

--- frameworks/core/components_ng/pattern/text_field/text_editing_value_ng.h
+++ frameworks/core/components_ng/pattern/text_field/text_editing_value_ng.h
@@ -92,12 +92,8 @@ struct TextEditingValueNG {
     std::string GetSelectedText(int32_t start, int32_t end) const
     {
         auto wideText = GetWideText();
-        if (start < 0 || end > static_cast<int32_t>(wideText.length()) || start >= end) {
-            LOGE("Get selected boundary is invalid");
-            return "";
-        }
-        auto min = std::min(start, end);
-        auto max = std::max(start, end);
+        auto min = std::max(std::min(start, end), 0);
+        auto max = std::min(std::max(start, end), static_cast<int32_t>(wideText.length()));
         return StringUtils::ToString(wideText.substr(min, max - min));
     }
 
