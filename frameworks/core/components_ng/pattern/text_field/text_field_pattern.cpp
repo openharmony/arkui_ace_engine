47879213c8079c2cd142c9bc80ccfce223c25f57
xiexiyun <xiexiyun@huawei.com>
2023-02-27 07:03:10 +0000

    preview input
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>
    Change-Id: Ic5300a12b5d145cdb4b2f6b66fe7c7350ccaa4f4

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -959,7 +959,9 @@ void TextFieldPattern::HandleDirectionalKey(const KeyEvent& keyEvent)
 
 bool TextFieldPattern::HandleKeyEvent(const KeyEvent& keyEvent)
 {
+    LOGI("HandleKeyEvent, action %{public}d, code %{public}d", keyEvent.action, keyEvent.code);
     if (keyEvent.action == KeyAction::DOWN) {
+        std::string appendElement;
         if (keyEvent.code == KeyCode::KEY_ENTER || keyEvent.code == KeyCode::KEY_NUMPAD_ENTER ||
             keyEvent.code == KeyCode::KEY_DPAD_CENTER) {
             if (keyboard_ != TextInputType::MULTILINE) {
@@ -967,6 +969,8 @@ bool TextFieldPattern::HandleKeyEvent(const KeyEvent& keyEvent)
             }
         } else if (keyEvent.IsDirectionalKey()) {
             HandleDirectionalKey(keyEvent);
+        } else if (keyEvent.IsNumberKey()) {
+            appendElement = keyEvent.ConvertCodeToString();
         } else if (keyEvent.IsLetterKey()) {
             if (keyEvent.IsKey({ KeyCode::KEY_CTRL_LEFT, KeyCode::KEY_SHIFT_LEFT, KeyCode::KEY_Z }) ||
                 keyEvent.IsKey({ KeyCode::KEY_CTRL_LEFT, KeyCode::KEY_SHIFT_RIGHT, KeyCode::KEY_Z }) ||
@@ -990,10 +994,43 @@ bool TextFieldPattern::HandleKeyEvent(const KeyEvent& keyEvent)
             } else if (keyEvent.IsKey({ KeyCode::KEY_CTRL_LEFT, KeyCode::KEY_X }) ||
                        keyEvent.IsKey({ KeyCode::KEY_CTRL_RIGHT, KeyCode::KEY_X })) {
                 HandleOnCut();
+            } else {
+                appendElement = keyEvent.ConvertCodeToString();
             }
+        }
+        if (keyEvent.code == KeyCode::KEY_DEL) {
+#if defined (PREVIEW)
+            DeleteBackward(1);
+#else
+            DeleteForward(1);
+#endif
+            return true;
+        }
+        if (keyEvent.code == KeyCode::KEY_FORWARD_DEL) {
+#if defined (PREVIEW)
+            DeleteForward(1);
+#else
+            DeleteBackward(1);
+#endif
+            return true;
+        }
+        ParseAppendValue(keyEvent.code, appendElement);
+        if (!appendElement.empty()) {
+            InsertValue(appendElement);
         }
     }
     return true;
+}
+
+void TextFieldPattern::ParseAppendValue(KeyCode keycode, std::string& appendElement)
+{
+    switch (keycode) {
+        case KeyCode::KEY_SPACE:
+            appendElement = " ";
+            break;
+        default:
+            break;
+    }
 }
 
 void TextFieldPattern::HandleOnUndoAction()
@@ -2169,6 +2206,7 @@ void TextFieldPattern::OnTextInputActionUpdate(TextInputAction value) {}
 
 void TextFieldPattern::InsertValue(const std::string& insertValue)
 {
+    LOGD("Insert value '%{public}s'", insertValue.c_str());
     auto originLength = static_cast<uint32_t>(textEditingValue_.GetWideText().length());
     if (originLength >= GetMaxLength()) {
         LOGW("Max length reached");
