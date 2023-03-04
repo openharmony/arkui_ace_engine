8fa5212b3e18b316a99c8c9e91b578856ccec3ac
seaside_wu <wuhaibin5@huawei.com>
2023-03-04 19:16:03 +0800

    fix memory leaks
    Signed-off-by: seaside_wu <wuhaibin5@huawei.com>

--- frameworks/core/components_ng/pattern/list/list_item_group_layout_property.h
+++ frameworks/core/components_ng/pattern/list/list_item_group_layout_property.h
@@ -53,14 +53,14 @@ public:
         LayoutProperty::ToJsonValue(json);
         json->Put("space", propSpace_.value_or(Dimension(0, DimensionUnit::VP)).ToString().c_str());
         if (propDivider_.has_value()) {
-            auto divider = JsonUtil::Create(false);
+            auto divider = JsonUtil::Create(true);
             divider->Put("strokeWidth", propDivider_.value().strokeWidth.ToString().c_str());
             divider->Put("startMargin", propDivider_.value().startMargin.ToString().c_str());
             divider->Put("endMargin", propDivider_.value().endMargin.ToString().c_str());
             divider->Put("color", propDivider_.value().color.ColorToString().c_str());
             json->Put("divider", divider);
         } else {
-            auto divider = JsonUtil::Create(false);
+            auto divider = JsonUtil::Create(true);
             json->Put("divider", divider);
         }
     }
