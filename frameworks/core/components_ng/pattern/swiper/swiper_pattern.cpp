9d6e97e7fa2c80b846a6d576b4a41ed4da07bf4b
xuzhidan <xuzhidan1@huawei.com>
2023-03-08 20:53:03 +0800

    fixed 7ce639b from https://gitee.com/xuzhidan/ace_ace_engine/pulls/10759 调整swiper触发onChange回调时机
    Signed-off-by: xuzhidan <xuzhidan1@huawei.com>
    Change-Id: I033dd5be2cb244eb158e85ab92db86107db6a220

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
@@ -77,9 +77,9 @@ void SwiperPattern::OnIndexChange() const
         return;
     }
 
-    auto currentIndex = (currentIndex_ + totalCount) % totalCount;
+    auto oldIndex = (oldIndex_ + totalCount) % totalCount;
     auto targetIndex = (CurrentIndex() + totalCount) % totalCount;
-    if (currentIndex != targetIndex) {
+    if (oldIndex != targetIndex) {
         auto swiperEventHub = GetEventHub<SwiperEventHub>();
         CHECK_NULL_VOID(swiperEventHub);
         swiperEventHub->FireChangeEvent(targetIndex);
@@ -97,12 +97,8 @@ void SwiperPattern::OnModifyDone()
     auto layoutProperty = GetLayoutProperty<SwiperLayoutProperty>();
     CHECK_NULL_VOID(layoutProperty);
 
+    oldIndex_ = currentIndex_;
     InitSwiperIndicator();
-    if (isInit_) {
-        isInit_ = false;
-    } else {
-        OnIndexChange();
-    }
 
     auto childrenSize = TotalCount();
     if (layoutProperty->GetIndex().has_value() && CurrentIndex() >= 0) {
@@ -238,6 +234,12 @@ WeakPtr<FocusHub> SwiperPattern::GetNextFocusNode(FocusStep step, const WeakPtr<
 
 bool SwiperPattern::OnDirtyLayoutWrapperSwap(const RefPtr<LayoutWrapper>& dirty, const DirtySwapConfig& config)
 {
+    if (isInit_) {
+        isInit_ = false;
+    } else {
+        OnIndexChange();
+    }
+
     auto curChild = dirty->GetOrCreateChildByIndex(currentIndex_);
     CHECK_NULL_RETURN(curChild, false);
     auto curChildFrame = curChild->GetHostNode();
