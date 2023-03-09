95e5f992e746b880120ed9ede8b669a4c8a63a6c
yeyinglong <yeyinglong@live.com>
2023-03-09 16:36:07 +0800

    ShadowStyle XTS接口适配
    Signed-off-by: yeyinglong <yeyinglong@live.com>

--- frameworks/core/components/common/properties/shadow.cpp
+++ frameworks/core/components/common/properties/shadow.cpp
@@ -48,8 +48,9 @@ Shadow Shadow::CreateShadow(ShadowStyle style)
                 return ShadowConfig::FloatingShadowSDark;
             case ShadowStyle::OuterFloatingMD:
                 return ShadowConfig::FloatingShadowMDark;
+            default:
+                return ShadowConfig::NoneShadow;
         }
-        return ShadowConfig::NoneShadow;
     }
     switch (style) {
         case ShadowStyle::OuterDefaultXS:
@@ -64,6 +65,8 @@ Shadow Shadow::CreateShadow(ShadowStyle style)
             return ShadowConfig::FloatingShadowS;
         case ShadowStyle::OuterFloatingMD:
             return ShadowConfig::FloatingShadowM;
+        default:
+            return ShadowConfig::NoneShadow;
     }
     return ShadowConfig::NoneShadow;
 }
