fbc21a45f5bf7bdce7ee9fd19fd113015569f664
aryawang <wangcaihui@huawei.com>
2023-03-11 07:40:51 +0000

    indexer点击
    Signed-off-by: aryawang <wangcaihui@huawei.com>
    Change-Id: If210b0bf3f794ca382cda609df6fb4c6fbcd1b54

--- frameworks/core/components_ng/pattern/indexer/indexer_pattern.cpp
+++ frameworks/core/components_ng/pattern/indexer/indexer_pattern.cpp
@@ -165,12 +165,7 @@ void IndexerPattern::OnHover(bool isHover)
 void IndexerPattern::OnChildHover(int32_t index, bool isHover)
 {
     childHoverIndex_ = isHover ? index : -1;
-    auto refreshBubble = false;
-    if (selected_ != childFocusIndex_) {
-        selected_ = childFocusIndex_;
-        refreshBubble = true;
-    }
-    ApplyIndexChanged(refreshBubble);
+    ApplyIndexChanged(childHoverIndex_ >= 0);
 }
 
 void IndexerPattern::InitInputEvent()
@@ -228,11 +223,9 @@ void IndexerPattern::OnTouchUp(const TouchEventInfo& info)
     }
     auto nextSelectIndex = GetSelectChildIndex(info.GetTouches().front().GetLocalLocation());
     auto refreshBubble = false;
-    if (nextSelectIndex != selected_) {
-        selected_ = nextSelectIndex;
-        refreshBubble = true;
-        ResetStatus();
-    }
+    selected_ = nextSelectIndex;
+    refreshBubble = true;
+    ResetStatus();
     ApplyIndexChanged(refreshBubble, true);
     OnSelect(refreshBubble);
 }
@@ -254,14 +247,9 @@ void IndexerPattern::MoveIndexByOffset(const Offset& offset)
     if (isHover_ && childPressIndex_ >= 0) {
         IndexerPressInAnimation();
     }
-    auto refreshBubble = false;
-    if (selected_ != childPressIndex_) {
-        selected_ = childPressIndex_;
-        refreshBubble = true;
-    }
     childFocusIndex_ = -1;
     childHoverIndex_ = -1;
-    ApplyIndexChanged(refreshBubble);
+    ApplyIndexChanged(true);
 }
 
 int32_t IndexerPattern::GetSelectChildIndex(const Offset& offset)
@@ -382,22 +370,18 @@ void IndexerPattern::OnSelect(bool changed)
     if (onSelected && (selected_ >= 0) && (selected_ < itemCount_)) {
         onSelected(selected_);
     }
-    if (changed) {
-        auto host = GetHost();
-        CHECK_NULL_VOID(host);
-        animateSelected_ = selected_;
-        if (animateSelected_ >= 0) {
-            auto selectedFrameNode = DynamicCast<FrameNode>(host->GetChildAtIndex(animateSelected_));
-            CHECK_NULL_VOID(selectedFrameNode);
-            ItemSelectedInAnimation(selectedFrameNode);
-        }
-        if (lastSelected_ >= 0) {
-            auto lastFrameNode = DynamicCast<FrameNode>(host->GetChildAtIndex(lastSelected_));
-            CHECK_NULL_VOID(lastFrameNode);
-            ItemSelectedOutAnimation(lastFrameNode);
-        }
-        lastSelected_ = selected_;
+    animateSelected_ = selected_;
+    if (animateSelected_ >= 0) {
+        auto selectedFrameNode = DynamicCast<FrameNode>(host->GetChildAtIndex(animateSelected_));
+        CHECK_NULL_VOID(selectedFrameNode);
+        ItemSelectedInAnimation(selectedFrameNode);
     }
+    if (lastSelected_ >= 0 && lastSelected_ != animateSelected_) {
+        auto lastFrameNode = DynamicCast<FrameNode>(host->GetChildAtIndex(lastSelected_));
+        CHECK_NULL_VOID(lastFrameNode);
+        ItemSelectedOutAnimation(lastFrameNode);
+    }
+    lastSelected_ = selected_;
 }
 
 void IndexerPattern::ApplyIndexChanged(bool selectChanged, bool fromTouchUp)
@@ -749,12 +733,14 @@ void IndexerPattern::AddListItemClickListener(RefPtr<FrameNode>& listItemNode, i
     CHECK_NULL_VOID(listItemNode);
     auto gestureHub = listItemNode->GetOrCreateGestureEventHub();
     CHECK_NULL_VOID(gestureHub);
-    auto clickEventFunc = [weak = WeakClaim(this), index](GestureEvent& gestureEvent) {
+    auto touchCallback = [weak = WeakClaim(this), index](const TouchEventInfo& info) {
         auto indexerPattern = weak.Upgrade();
         CHECK_NULL_VOID(indexerPattern);
-        indexerPattern->OnListItemClick(index);
+        if (info.GetTouches().front().GetTouchType() == TouchType::DOWN) {
+            indexerPattern->OnListItemClick(index);
+        }
     };
-    gestureHub->SetUserOnClick(std::move(clickEventFunc));
+    gestureHub->AddTouchEvent(MakeRefPtr<TouchEventImpl>(std::move(touchCallback)));
 }
 
 void IndexerPattern::OnListItemClick(int32_t index)
