53f335ea1830539d11e0eadafacbef6796b281a9
xuzhidan <xuzhidan1@huawei.com>
2023-04-07 20:02:48 +0800

    挑单3.2Release 修复swiper最后两页空白以及自动轮播卡死问题
    Signed-off-by: xuzhidan <xuzhidan1@huawei.com>
    Change-Id: I85a13f142087e21662234acac0ccbd81d2e5c5b0

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
@@ -767,6 +767,7 @@ void SwiperPattern::HandleDragStart()
 {
     StopTranslateAnimation();
     StopSpringAnimation();
+    StopAutoPlay();
 
     const auto& tabBarFinishCallback = swiperController_->GetTabBarFinishCallback();
     if (tabBarFinishCallback) {
@@ -905,6 +906,7 @@ void SwiperPattern::PlayTranslateAnimation(float startPos, float endPos, int32_t
 
     // If animation is still running, stop it before play new animation.
     StopTranslateAnimation();
+    StopAutoPlay();
 
     auto translate = AceType::MakeRefPtr<CurveAnimation<double>>(startPos, endPos, curve);
     auto weak = AceType::WeakClaim(this);
