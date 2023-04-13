42e836f8156305a1ee0d8500e45602998f7eae5d
dujingcheng <dujingcheng@huawei.com>
2023-04-13 12:13:17 +0000

    resource
    Signed-off-by: dujingcheng <dujingcheng@huawei.com>
    Change-Id: I357c552c8250822300b23d3aa5d48fd0453e20b0

--- adapter/ohos/entrance/ui_content_impl.cpp
+++ adapter/ohos/entrance/ui_content_impl.cpp
@@ -47,6 +47,7 @@
 #include "adapter/ohos/entrance/utils.h"
 #include "adapter/ohos/osal/page_url_checker_ohos.h"
 #include "base/geometry/rect.h"
+#include "base/i18n/localization.h"
 #include "base/log/ace_trace.h"
 #include "base/log/log.h"
 #include "base/subwindow/subwindow_manager.h"
@@ -252,6 +253,7 @@ UIContentImpl::UIContentImpl(OHOS::AbilityRuntime::Context* context,
     CHECK_NULL_VOID(applicationInfo);
     minCompatibleVersionCode_ = applicationInfo->minCompatibleVersionCode;
     isBundle_ = (hapModuleInfo->compileMode == AppExecFwk::CompileMode::JS_BUNDLE);
+    SetConfiguration(context->GetConfiguration());
     const auto& obj = context->GetBindingObject();
     CHECK_NULL_VOID(obj);
     auto ref = obj->Get<NativeReference>();
@@ -417,9 +419,8 @@ void UIContentImpl::CommonInitializeForm(OHOS::Rosen::Window* window,
         LOGI("UIContent: deviceWidth: %{public}d, deviceHeight: %{public}d, default density: %{public}f", deviceWidth,
             deviceHeight, density);
     }
-    SystemProperties::InitDeviceInfo(deviceWidth, deviceHeight, deviceHeight >= deviceWidth ? 0 : 1, density, false);
-    SystemProperties::SetColorMode(ColorMode::LIGHT);
 
+    SystemProperties::InitDeviceInfo(deviceWidth, deviceHeight, deviceHeight >= deviceWidth ? 0 : 1, density, false);
     std::unique_ptr<Global::Resource::ResConfig> resConfig(Global::Resource::CreateResConfig());
     if (context) {
         auto resourceManager = context->GetResourceManager();
@@ -444,12 +445,6 @@ void UIContentImpl::CommonInitializeForm(OHOS::Rosen::Window* window,
             SystemProperties::SetDeviceAccess(
                 resConfig->GetInputDevice() == Global::Resource::InputDevice::INPUTDEVICE_POINTINGDEVICE);
         }
-    } else {
-        LOGI("Context is nullptr, set localeInfo to default");
-        UErrorCode status = U_ZERO_ERROR;
-        icu::Locale locale = icu::Locale::forLanguageTag(Global::I18n::LocaleConfig::GetSystemLanguage(), status);
-        AceApplicationInfo::GetInstance().SetLocale(locale.getLanguage(), locale.getCountry(), locale.getScript(), "");
-        SystemProperties::SetColorMode(ColorMode::LIGHT);
     }
 
     auto abilityContext = OHOS::AbilityRuntime::Context::ConvertTo<OHOS::AbilityRuntime::AbilityContext>(context);
@@ -805,6 +800,48 @@ void UIContentImpl::CommonInitializeForm(OHOS::Rosen::Window* window,
         }
     }
     LayoutInspector::SetCallback(instanceId_);
+}
+
+void UIContentImpl::SetConfiguration(const std::shared_ptr<OHOS::AppExecFwk::Configuration>& config)
+{
+    if (config == nullptr) {
+        LOGI("config is nullptr, set localeInfo to default");
+        UErrorCode status = U_ZERO_ERROR;
+        icu::Locale locale = icu::Locale::forLanguageTag(Global::I18n::LocaleConfig::GetSystemLanguage(), status);
+        AceApplicationInfo::GetInstance().SetLocale(locale.getLanguage(), locale.getCountry(), locale.getScript(), "");
+        SystemProperties::SetColorMode(ColorMode::LIGHT);
+        return;
+    }
+
+    LOGI("SetConfiguration");
+    auto colorMode = config->GetItem(OHOS::AAFwk::GlobalConfigurationKey::SYSTEM_COLORMODE);
+    auto deviceAccess = config->GetItem(OHOS::AAFwk::GlobalConfigurationKey::INPUT_POINTER_DEVICE);
+    auto languageTag = config->GetItem(OHOS::AAFwk::GlobalConfigurationKey::SYSTEM_LANGUAGE);
+    if (!colorMode.empty()) {
+        LOGI("SetConfiguration colorMode: %{public}s", colorMode.c_str());
+        if (colorMode == "dark") {
+            SystemProperties::SetColorMode(ColorMode::DARK);
+        } else {
+            SystemProperties::SetColorMode(ColorMode::LIGHT);
+        }
+    }
+
+    if (!deviceAccess.empty()) {
+        // Event of accessing mouse or keyboard
+        LOGI("SetConfiguration deviceAccess: %{public}s", deviceAccess.c_str());
+        SystemProperties::SetDeviceAccess(deviceAccess == "true");
+    }
+
+    if (!languageTag.empty()) {
+        LOGI("SetConfiguration languageTag: %{public}s", languageTag.c_str());
+        std::string language;
+        std::string script;
+        std::string region;
+        Localization::ParseLocaleTag(languageTag, language, script, region, false);
+        if (!language.empty() || !script.empty() || !region.empty()) {
+            AceApplicationInfo::GetInstance().SetLocale(language, region, script, "");
+        }
+    }
 }
 
 std::shared_ptr<Rosen::RSSurfaceNode> UIContentImpl::GetFormRootNode()
