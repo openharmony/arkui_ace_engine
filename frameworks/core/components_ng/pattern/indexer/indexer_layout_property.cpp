b1c24e0552746a89f00b6a05cbcdb224076a5ecc
aryawang <wangcaihui@huawei.com>
2023-04-17 08:17:16 +0000

    inspector大小写修改
    Signed-off-by: aryawang <wangcaihui@huawei.com>
    Change-Id: Ie1c7c8ee236e96fe5bf3646e9042e0a56b512f34

--- frameworks/core/components_ng/pattern/indexer/indexer_layout_property.cpp
+++ frameworks/core/components_ng/pattern/indexer/indexer_layout_property.cpp
@@ -29,25 +29,25 @@ const std::string DEFAULT_FAMILY = "HarmonyOS Sans";
 void IndexerLayoutProperty::ToJsonValue(std::unique_ptr<JsonValue>& json) const
 {
     LayoutProperty::ToJsonValue(json);
-    json->Put("Selected", std::to_string(propSelected_.value_or(0)).c_str());
-    json->Put("Color", propColor_.value_or(Color::WHITE).ColorToString().c_str());
-    json->Put("SelectedColor", propSelectedColor_.value_or(Color::WHITE).ColorToString().c_str());
-    json->Put("PopupColor", propPopupColor_.value_or(Color::WHITE).ColorToString().c_str());
-    json->Put("UsingPopup", propUsingPopup_.value_or(false) ? "true" : "false");
-    json->Put("ItemSize", propItemSize_.value_or(Dimension(0, DimensionUnit::VP)).ToString().c_str());
-    json->Put("AlignStyle",
+    json->Put("selected", std::to_string(propSelected_.value_or(0)).c_str());
+    json->Put("color", propColor_.value_or(Color::WHITE).ColorToString().c_str());
+    json->Put("selectedColor", propSelectedColor_.value_or(Color::WHITE).ColorToString().c_str());
+    json->Put("popupColor", propPopupColor_.value_or(Color::WHITE).ColorToString().c_str());
+    json->Put("usingPopup", propUsingPopup_.value_or(false) ? "true" : "false");
+    json->Put("itemSize", propItemSize_.value_or(Dimension(0, DimensionUnit::VP)).ToString().c_str());
+    json->Put("alignStyle",
         propAlignStyle_.value_or(AlignStyle::LEFT) == AlignStyle::LEFT ? "AlignStyle::LEFT" : "AlignStyle::RIGHT");
     auto PopupPositionJsonObject = JsonUtil::Create(true);
-    PopupPositionJsonObject->Put("PopupPositionX", propPopupPositionX_.value_or(Dimension()).ToString().c_str());
-    PopupPositionJsonObject->Put("PopupPositionY", propPopupPositionY_.value_or(Dimension()).ToString().c_str());
-    json->Put("PopupPosition", PopupPositionJsonObject);
+    PopupPositionJsonObject->Put("popupPositionX", propPopupPositionX_.value_or(Dimension()).ToString().c_str());
+    PopupPositionJsonObject->Put("popupPositionY", propPopupPositionY_.value_or(Dimension()).ToString().c_str());
+    json->Put("popupPosition", PopupPositionJsonObject);
     auto jsonArrayValue = JsonUtil::CreateArray(true);
     auto arrayValue = propArrayValue_.value_or(std::vector<std::string>());
     for (uint32_t i = 0; i < arrayValue.size(); i++) {
         auto index = std::to_string(i);
         jsonArrayValue->Put(index.c_str(), arrayValue[i].c_str());
     }
-    json->Put("ArrayValue", jsonArrayValue);
+    json->Put("arrayValue", jsonArrayValue);
     auto defaultFont = TextStyle();
     defaultFont.SetFontStyle(FontStyle::NORMAL);
     defaultFont.SetFontSize(Dimension(DEFAULT_SIZE, DimensionUnit::FP));
@@ -58,9 +58,9 @@ void IndexerLayoutProperty::ToJsonValue(std::unique_ptr<JsonValue>& json) const
     auto fontJsonObject = ToJsonObjectValue(propFont_.value_or(defaultFont));
     json->Put("font", fontJsonObject);
     auto selectFontJsonObject = ToJsonObjectValue(propSelectedFont_.value_or(defaultFont));
-    json->Put("SelectFont", selectFontJsonObject);
+    json->Put("selectFont", selectFontJsonObject);
     auto popupFontJsonObject = ToJsonObjectValue(propPopupFont_.value_or(defaultFont));
-    json->Put("PopupFont", popupFontJsonObject);
+    json->Put("popupFont", popupFontJsonObject);
     auto pipeline = PipelineContext::GetCurrentContext();
     CHECK_NULL_VOID(pipeline);
     auto indexerTheme = pipeline->GetTheme<IndexerTheme>();
@@ -75,10 +75,10 @@ void IndexerLayoutProperty::ToJsonValue(std::unique_ptr<JsonValue>& json) const
 std::unique_ptr<JsonValue> IndexerLayoutProperty::ToJsonObjectValue(const TextStyle& textStyle)
 {
     auto fontJsonObject = JsonUtil::Create(true);
-    fontJsonObject->Put("FontSize", textStyle.GetFontSize().ToString().c_str());
+    fontJsonObject->Put("fontSize", textStyle.GetFontSize().ToString().c_str());
     fontJsonObject->Put(
-        "FontStyle", textStyle.GetFontStyle() == FontStyle::NORMAL ? "FontStyle::NORMAL" : "FontStyle::ITALIC");
-    fontJsonObject->Put("FontWeight", V2::ConvertWrapFontWeightToStirng(textStyle.GetFontWeight()).c_str());
+        "fontStyle", textStyle.GetFontStyle() == FontStyle::NORMAL ? "FontStyle::NORMAL" : "FontStyle::ITALIC");
+    fontJsonObject->Put("fontWeight", V2::ConvertWrapFontWeightToStirng(textStyle.GetFontWeight()).c_str());
     auto fontFamilyVector = textStyle.GetFontFamilies();
     std::string fontFamily;
     for (uint32_t i = 0; i < fontFamilyVector.size(); i++) {
@@ -88,7 +88,7 @@ std::unique_ptr<JsonValue> IndexerLayoutProperty::ToJsonObjectValue(const TextSt
         }
         fontFamily += fontFamilyVector.at(i) + ",";
     }
-    fontJsonObject->Put("FontFamily", fontFamily.c_str());
+    fontJsonObject->Put("fontFamily", fontFamily.c_str());
     return fontJsonObject;
 }
 
