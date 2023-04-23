c751b4ec1d2322cfaa24c607e760bfdc9a020c8a
张凯 <zhangkai351@huawei.com>
2023-04-23 09:10:10 +0000

    update frameworks/core/components_ng/gestures/gesture_referee.h.
    Signed-off-by: 张凯 <zhangkai351@huawei.com>

--- frameworks/core/components_ng/gestures/gesture_referee.h
+++ frameworks/core/components_ng/gestures/gesture_referee.h
@@ -115,7 +115,7 @@ public:
         queryStateFunc_ = queryStateFunc;
     }
     bool QueryAllDone(size_t touchId);
-    bool CheckDeviceChange(size_t id);
+    bool CheckSourceTypeChange(SourceType type, bool isAxis = false);
     void CleanAll();
 private:
     void HandleAcceptDisposal(const RefPtr<NGGestureRecognizer>& recognizer);
@@ -126,6 +126,8 @@ private:
     std::unordered_map<size_t, RefPtr<GestureScope>> gestureScopes_;
 
     std::function<void(size_t)> queryStateFunc_;
+    SourceType lastSourceType_ = SourceType::NONE;
+    bool lastIsAxis_ = false;
 };
 
 } // namespace OHOS::Ace::NG
