7b5844516d16fa83eebbb8df6e9c1b0ae5322117
sunfei <sunfei.sun@huawei.com>
2023-04-27 23:55:27 +0800

    fix undefined symbol in cross platform
    Signed-off-by: sunfei <sunfei.sun@huawei.com>
    Change-Id: Ib73f3a34564b4ce8f5378593c691fd5bc0a6a778

--- frameworks/bridge/js_frontend/engine/jsi/jsi_engine.cpp
+++ frameworks/bridge/js_frontend/engine/jsi/jsi_engine.cpp
@@ -60,8 +60,10 @@
 #ifndef OHOS_PLATFORM
 extern const uint8_t _binary_strip_native_min_abc_start[];
 extern const uint8_t* _binary_strip_native_min_abc_end;
+#if defined(PREVIEW)
 extern const uint8_t _binary_jsMockSystemPlugin_abc_start[];
 extern const uint8_t* _binary_jsMockSystemPlugin_abc_end;
+#endif
 #endif
 
 namespace OHOS::Ace::Framework {
@@ -2993,12 +2995,14 @@ bool JsiEngineInstance::InitJsEnv(bool debugger_mode, const std::unordered_map<s
         return false;
     }
 #else
+#if defined(PREVIEW)
     bool mockResult = runtime_->EvaluateJsCode((uint8_t*)_binary_jsMockSystemPlugin_abc_start,
         _binary_strip_native_min_abc_end - _binary_jsMockSystemPlugin_abc_start);
     if (!mockResult) {
         LOGE("Failed to load js mock api!");
         return false;
     }
+#endif
     bool jsfwkResult = runtime_->EvaluateJsCode((uint8_t*)_binary_strip_native_min_abc_start,
         _binary_strip_native_min_abc_end - _binary_strip_native_min_abc_start);
     if (!jsfwkResult) {
