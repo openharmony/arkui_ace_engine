d77480e433c3707a56d4d65d4527be75ca89e3fb
limeng <limeng208@huawei.com>
2023-05-08 19:45:23 +0800

    initial commit for the bugs of setTextSelection
    Signed-off-by: limeng <limeng208@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_overlay_modifier.h
+++ frameworks/core/components_ng/pattern/text_field/text_field_overlay_modifier.h
@@ -53,6 +53,7 @@ public:
     void SetUnderlineColor(const Color& value);
     void SetUnderlineWidth(float underlineWidth);
     void SetShowCounter(bool value);
+    void SetSelectedAreaRedraw(bool value);
 
 private:
     void PaintSelection(DrawingContext& context) const;
@@ -71,6 +72,7 @@ private:
     RefPtr<PropertySizeF> contentSize_;
     RefPtr<PropertyOffsetF> contentOffset_;
     RefPtr<PropertyFloat> currentOffset_;
+    RefPtr<PropertyBool> isSelectedAreaRedraw_;
     RefPtr<PropertyFloat> underlineWidth_;
     RefPtr<PropertyColor> underlineColor_;
     InputStyle inputStyle_ = InputStyle::DEFAULT;
