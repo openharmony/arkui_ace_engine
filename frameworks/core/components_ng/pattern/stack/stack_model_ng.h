c20565bf8acb6da627c4cd4b1eabf6a9c97346b9
chengfeiwang <chengfeiwang@huawei.com>
2023-05-10 02:33:09 +0000

    cherry-pick stack layout algorithm
    Signed-off-by: chengfeiwang <chengfeiwang@huawei.com>
    Change-Id: Ibc31b1fb67c2969bfa5d726428881fefbe1c51c2

--- frameworks/core/components_ng/pattern/stack/stack_model_ng.h
+++ frameworks/core/components_ng/pattern/stack/stack_model_ng.h
@@ -27,11 +27,12 @@ public:
     void SetStackFit(StackFit fit) override {}
     void SetOverflow(Overflow overflow) override {}
     void SetAlignment(Alignment align) override;
+    void SetAlignmentContent(Alignment align) override;
     void SetHasHeight() override {}
     void SetHasWidth() override {}
 
 private:
-    void Create();
+    void Create() override;
 };
 
 } // namespace OHOS::Ace::NG
