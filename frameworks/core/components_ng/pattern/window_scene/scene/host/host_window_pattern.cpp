6b06868e6af60cd7f16613e89d9ee883945c00f8
qpzeng <zengqingpeng6@huawei.com>
2023-05-11 16:40:24 +0800

    bridge to window interface
    Signed-off-by: qpzeng <zengqingpeng6@huawei.com>
    Change-Id: I49091edaac47e12d8082d03e9e1a41c687090a2e

--- frameworks/core/components_ng/pattern/window_scene/scene/host/host_window_pattern.cpp
+++ frameworks/core/components_ng/pattern/window_scene/scene/host/host_window_pattern.cpp
@@ -212,7 +212,7 @@ bool HostWindowPattern::OnDirtyLayoutWrapperSwap(const RefPtr<LayoutWrapper>& di
     };
 
     CHECK_NULL_RETURN(session_, false);
-    session_->UpdateRect(rect, Rosen::SizeChangeReason::SHOW);
+    session_->UpdateRect(rect, Rosen::SizeChangeReason::UNDEFINED);
     return false;
 }
 
