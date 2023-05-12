ce62321defcefc8fb1c50912c55a00a92911f121
yangguangzhao <yangguangzhao1@huawei.com>
2023-05-12 17:17:56 +0800

    web dtor delayed
    Signed-off-by: yangguangzhao <yangguangzhao1@huawei.com>

--- frameworks/core/components/web/resource/web_delegate.cpp
+++ frameworks/core/components/web/resource/web_delegate.cpp
@@ -85,6 +85,8 @@ const std::string RESOURCE_VIDEO_CAPTURE = "TYPE_VIDEO_CAPTURE";
 const std::string RESOURCE_AUDIO_CAPTURE = "TYPE_AUDIO_CAPTURE";
 const std::string RESOURCE_PROTECTED_MEDIA_ID = "TYPE_PROTECTED_MEDIA_ID";
 const std::string RESOURCE_MIDI_SYSEX = "TYPE_MIDI_SYSEX";
+
+constexpr uint32_t DESTRUCT_DELAY_MILLISECONDS = 1000;
 } // namespace
 
 #define EGLCONFIG_VERSION 3
@@ -558,6 +560,29 @@ int FaviconReceivedOhos::GetColorType()
 int FaviconReceivedOhos::GetAlphaType()
 {
     return static_cast<int>(alphaType_);
+}
+
+WebDelegateObserver::~WebDelegateObserver()
+{
+    LOGI("WebDelegateObserver::~WebDelegateObserver");
+}
+
+void WebDelegateObserver::NotifyDestory()
+{
+    LOGI("WebDelegateObserver::NotifyDestory");
+    auto context = context_.Upgrade();
+    CHECK_NULL_VOID(context);
+    auto taskExecutor = context->GetTaskExecutor();
+    CHECK_NULL_VOID(taskExecutor);
+    taskExecutor->PostDelayedTask(
+        [weak = WeakClaim(this)]() {
+            auto observer = weak.Upgrade();
+            CHECK_NULL_VOID(observer);
+            if (observer->delegate_) {
+                observer->delegate_.Reset();
+            }
+        },
+        TaskExecutor::TaskType::UI, DESTRUCT_DELAY_MILLISECONDS);
 }
 
 WebDelegate::~WebDelegate()
