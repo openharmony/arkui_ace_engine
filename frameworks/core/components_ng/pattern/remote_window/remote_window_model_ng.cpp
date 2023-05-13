ab6a3f0a7ec387790d19548e2c466d2d070956e7
quguiren <quguiren@huawei.com>
2023-05-13 10:17:52 +0800

    remote_window decoupling
    Signed-off-by: quguiren <quguiren@huawei.com>

--- frameworks/core/components_ng/pattern/remote_window/remote_window_model_ng.cpp
+++ frameworks/core/components_ng/pattern/remote_window/remote_window_model_ng.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-#include "core/components_ng/pattern/remote_window/remote_window_view.h"
+#include "core/components_ng/pattern/remote_window/remote_window_model_ng.h"
 
 #include "core/components/common/layout/constants.h"
 #include "core/components_ng/base/frame_node.h"
@@ -23,8 +23,7 @@
 #include "core/components_v2/inspector/inspector_constants.h"
 
 namespace OHOS::Ace::NG {
-
-void RemoteWindowView::Create(const std::shared_ptr<OHOS::Rosen::RSNode>& rsNode)
+void RemoteWindowModelNG::Create(const std::shared_ptr<OHOS::Rosen::RSNode>& rsNode)
 {
     auto* stack = ViewStackProcessor::GetInstance();
     auto nodeId = stack->ClaimNodeId();
@@ -37,5 +36,4 @@ void RemoteWindowView::Create(const std::shared_ptr<OHOS::Rosen::RSNode>& rsNode
     context->SetRSNode(rsNode);
 #endif
 }
-
 } // namespace OHOS::Ace::NG
