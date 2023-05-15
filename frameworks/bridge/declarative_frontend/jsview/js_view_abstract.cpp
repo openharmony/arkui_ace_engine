f4bf192c00bd394ba6da45112d911a24dff03c35
xiexiyun <xiexiyun@huawei.com>
2023-05-15 07:52:11 +0000

    flex basis undefined
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>
    Change-Id: I5d360a005708424ac953208f2fbee70b2c0061b8

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
@@ -1644,7 +1644,7 @@ void JSViewAbstract::JsFlexBasis(const JSCallbackInfo& info)
     }
     CalcDimension value;
     if (!ParseJsDimensionVp(info[0], value)) {
-        return;
+        value.SetUnit(DimensionUnit::AUTO);
     }
     // flexbasis don't support percent case.
     if (value.Unit() == DimensionUnit::PERCENT) {
