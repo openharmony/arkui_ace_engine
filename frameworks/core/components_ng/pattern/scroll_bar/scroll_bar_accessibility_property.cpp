d6250243b818b56d3af55c7b5f29738cb1422bb0
zhangrongjie <zhangrongjie4@huawei.com>
2023-05-16 12:45:10 +0000

    fixed ce25954 from https://gitee.com/rongShao-Z/arkui_ace_engine/pulls/13170 fix the bug with incorrect IsScrollable attribute in scrollBar
    Signed-off-by: zhangrongjie <zhangrongjie4@huawei.com>
    Change-Id: I9dd7e1b4eb91255e0dc6923d2707fb2d4e73bf01

--- frameworks/core/components_ng/pattern/scroll_bar/scroll_bar_accessibility_property.cpp
+++ frameworks/core/components_ng/pattern/scroll_bar/scroll_bar_accessibility_property.cpp
@@ -26,7 +26,7 @@ bool ScrollBarAccessibilityProperty::IsScrollable() const
     CHECK_NULL_RETURN(frameNode, false);
     auto scrollBarPattern = frameNode->GetPattern<ScrollBarPattern>();
     CHECK_NULL_RETURN(scrollBarPattern, false);
-    if (scrollBarPattern->GetAxis() != Axis::NONE && scrollBarPattern->GetScrollableDistance() != 0.0) {
+    if (scrollBarPattern->GetAxis() != Axis::NONE && Positive(scrollBarPattern->GetControlDistance())) {
         return true;
     }
     return false;
