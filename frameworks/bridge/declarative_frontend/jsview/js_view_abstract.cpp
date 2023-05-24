f56a20f4838c8e4cc7bd29cd30dd70ab6d181bd3
yeyinglong <yeyinglong@hisilicon.com>
2023-05-24 20:22:40 +0800

    修复blur单位和inspector问题
    Signed-off-by: yeyinglong <yeyinglong@hisilicon.com>

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
@@ -5207,7 +5207,7 @@ void JSViewAbstract::SetPaddingRight(const JSCallbackInfo& info)
 
 void JSViewAbstract::SetBlur(float radius)
 {
-    CalcDimension dimensionRadius(radius, DimensionUnit::PX);
+    CalcDimension dimensionRadius(radius, DimensionUnit::VP);
     ViewAbstractModel::GetInstance()->SetFrontBlur(dimensionRadius);
 }
 
@@ -5218,7 +5218,7 @@ void JSViewAbstract::SetColorBlend(Color color)
 
 void JSViewAbstract::SetBackdropBlur(float radius)
 {
-    CalcDimension dimensionRadius(radius, DimensionUnit::PX);
+    CalcDimension dimensionRadius(radius, DimensionUnit::VP);
     ViewAbstractModel::GetInstance()->SetBackdropBlur(dimensionRadius);
 }
 
