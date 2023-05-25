c116ece887663d8b97ae5e995b003e44536143ec
sunjiakun <sunjiakun3@huawei.com>
2023-05-25 10:12:38 +0800

    fixed 4261c27 from https://gitee.com/sunjiakun/arkui_ace_engine/pulls/13629 datapanel bug 修复
    Signed-off-by: sunjiakun <sunjiakun3@huawei.com>

--- frameworks/bridge/declarative_frontend/jsview/js_data_panel.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_data_panel.cpp
@@ -213,6 +213,13 @@ void JSDataPanel::ShadowOption(const JSCallbackInfo& info)
         return;
     }
 
+    OHOS::Ace::NG::DataPanelShadow shadow;
+    if (info[0]->IsNull()) {
+        shadow.isShadowVisible = false;
+        DataPanelModel::GetInstance()->SetShadowOption(shadow);
+        return;
+    }
+
     auto paramObject = JSRef<JSObject>::Cast(info[0]);
     JSRef<JSVal> jsRadius = paramObject->GetProperty("radius");
     JSRef<JSVal> jsOffsetX = paramObject->GetProperty("offsetX");
@@ -250,7 +257,6 @@ void JSDataPanel::ShadowOption(const JSCallbackInfo& info)
         }
     }
 
-    OHOS::Ace::NG::DataPanelShadow shadow;
     shadow.radius = radius;
     shadow.offsetX = offsetX;
     shadow.offsetY = offsetY;
