621d3e01a684f1931c687a74a16e3c1073d24510
xiexiyun <xiexiyun@huawei.com>
2023-05-26 12:41:29 +0000

    font zero not showing placeholder
    Signed-off-by: xiexiyun <xiexiyun@huawei.com>
    Change-Id: I641a0c328e37c489d14db72d28e7f22b16083e31

--- frameworks/core/components_ng/pattern/text_field/text_field_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_layout_algorithm.cpp
@@ -43,7 +43,7 @@ namespace {
 constexpr uint32_t COUNTER_TEXT_MAXLINE = 1;
 constexpr float ERROR_TEXT_UNDERLINE_MARGIN = 27.0f;
 constexpr float ERROR_TEXT_CAPSULE_MARGIN = 33.0f;
-}
+} // namespace
 
 void TextFieldLayoutAlgorithm::Measure(LayoutWrapper* layoutWrapper)
 {
@@ -232,13 +232,14 @@ std::optional<SizeF> TextFieldLayoutAlgorithm::MeasureContent(
         }
     }
     auto preferredHeight = static_cast<float>(paragraph_->GetHeight());
-    if (textContent.empty()) {
+    if (textContent.empty() || showPlaceHolder) {
         preferredHeight = pattern->PreferredLineHeight();
     }
     if (pattern->IsTextArea()) {
-        auto useHeight = counterParagraph_ ?
-            static_cast<float>(paragraph_->GetHeight() + counterParagraph_->GetHeight()) :
-            static_cast<float>(paragraph_->GetHeight());
+        auto paragraphHeight =
+            (textContent.empty() || showPlaceHolder) ? preferredHeight : static_cast<float>(paragraph_->GetHeight());
+        auto useHeight =
+            static_cast<float>(paragraphHeight + (counterParagraph_ ? counterParagraph_->GetHeight() : 0.0f));
         const auto& calcLayoutConstraint = textFieldLayoutProperty->GetCalcLayoutConstraint();
         if (calcLayoutConstraint && calcLayoutConstraint->maxSize.has_value() &&
             calcLayoutConstraint->maxSize.value().Height().has_value()) {
@@ -403,7 +404,7 @@ void TextFieldLayoutAlgorithm::UpdateTextStyle(const RefPtr<FrameNode>& frameNod
     textStyle.SetFontFamilies(layoutProperty->GetFontFamilyValue(defaultFontFamily));
 
     Dimension fontSize;
-    if (layoutProperty->HasFontSize() && layoutProperty->GetFontSizeValue(Dimension()).IsNonNegative()) {
+    if (layoutProperty->HasFontSize() && layoutProperty->GetFontSize().value_or(Dimension()).IsNonNegative()) {
         fontSize = layoutProperty->GetFontSizeValue(Dimension());
     } else {
         fontSize = theme ? theme->GetFontSize() : textStyle.GetFontSize();
@@ -446,7 +447,7 @@ void TextFieldLayoutAlgorithm::UpdatePlaceholderTextStyle(const RefPtr<TextField
     textStyle.SetFontFamilies(layoutProperty->GetFontFamilyValue(defaultFontFamily));
     Dimension fontSize;
     if (layoutProperty->HasPlaceholderFontSize() &&
-        layoutProperty->GetPlaceholderFontSizeValue(Dimension()).IsNonNegative()) {
+        layoutProperty->GetPlaceholderFontSize().value_or(Dimension()).IsNonNegative()) {
         fontSize = layoutProperty->GetPlaceholderFontSizeValue(Dimension());
     } else {
         fontSize = theme ? theme->GetFontSize() : textStyle.GetFontSize();
