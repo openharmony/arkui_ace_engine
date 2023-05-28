85f5aa9db6b2eb93b9331f9a7212c878f0a6d435
zhouchaobo <zhouchaobo@huawei.com>
2023-05-28 15:17:10 +0800

    Tabs set clip bug fix
    Signed-off-by: zhouchaobo <zhouchaobo@huawei.com>
    Change-Id: I05522fb370d6a3ff311acf35e8c7c6e642a50225

--- frameworks/core/components_ng/pattern/tabs/tabs_model_ng.h
+++ frameworks/core/components_ng/pattern/tabs/tabs_model_ng.h
@@ -50,6 +50,7 @@ public:
     void SetBarOverlap(bool barOverlap) override;
     void SetOnChangeEvent(std::function<void(const BaseEventInfo*)>&& onChangeEvent) override;
     void SetBarBackgroundColor(const Color& backgroundColor) override;
+    void SetClipEdge(bool clipEdge) override;
     static RefPtr<TabsNode> GetOrCreateTabsNode(
         const std::string& tag, int32_t nodeId, const std::function<RefPtr<Pattern>(void)>& patternCreator);
 
