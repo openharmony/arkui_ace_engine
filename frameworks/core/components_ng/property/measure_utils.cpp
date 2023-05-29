bcaf950109fe8814aaa7029737b56288ee5d9b37
xiexiyun <xiexiyun@huawei.com>
2023-05-29 08:27:02 +0000

    回退 'Pull Request !12895 : use percent reference to create ideal size '

--- frameworks/core/components_ng/property/measure_utils.cpp
+++ frameworks/core/components_ng/property/measure_utils.cpp
@@ -253,16 +253,14 @@ void SetMainAxisSize(float value, Axis axis, OptionalSizeF& size)
     size.SetWidth(value);
 }
 
-SizeF CreateIdealSize(
-    const LayoutConstraintF& layoutConstraint, Axis axis, MeasureType measureType, bool usingPercentRef)
+SizeF CreateIdealSize(const LayoutConstraintF& layoutConstraint, Axis axis, MeasureType measureType, bool usingMaxSize)
 {
     auto optional = CreateIdealSize(layoutConstraint, axis, measureType);
-    if (usingPercentRef) {
-        optional.UpdateIllegalSizeWithCheck(layoutConstraint.percentReference);
+    if (usingMaxSize) {
+        optional.UpdateIllegalSizeWithCheck(layoutConstraint.maxSize);
     } else {
         optional.UpdateIllegalSizeWithCheck(layoutConstraint.minSize);
     }
-    optional.Constrain(layoutConstraint.minSize, layoutConstraint.maxSize);
     return optional.ConvertToSizeT();
 }
 
@@ -278,7 +276,7 @@ OptionalSizeF CreateIdealSize(const LayoutConstraintF& layoutConstraint, Axis ax
 
         if (measureType == MeasureType::MATCH_PARENT) {
             idealSize.UpdateIllegalSizeWithCheck(layoutConstraint.parentIdealSize);
-            idealSize.UpdateIllegalSizeWithCheck(layoutConstraint.percentReference);
+            idealSize.UpdateIllegalSizeWithCheck(layoutConstraint.maxSize);
             break;
         }
 
@@ -289,7 +287,7 @@ OptionalSizeF CreateIdealSize(const LayoutConstraintF& layoutConstraint, Axis ax
                 if (parentCrossSize) {
                     SetCrossAxisSize(parentCrossSize.value(), axis, idealSize);
                 } else {
-                    parentCrossSize = GetCrossAxisSize(layoutConstraint.percentReference, axis);
+                    parentCrossSize = GetCrossAxisSize(layoutConstraint.maxSize, axis);
                     SetCrossAxisSize(parentCrossSize.value(), axis, idealSize);
                 }
             }
@@ -303,14 +301,13 @@ OptionalSizeF CreateIdealSize(const LayoutConstraintF& layoutConstraint, Axis ax
                 if (parentMainSize) {
                     SetMainAxisSize(parentMainSize.value(), axis, idealSize);
                 } else {
-                    parentMainSize = GetMainAxisSize(layoutConstraint.percentReference, axis);
+                    parentMainSize = GetMainAxisSize(layoutConstraint.maxSize, axis);
                     SetMainAxisSize(parentMainSize.value(), axis, idealSize);
                 }
             }
             break;
         }
     } while (false);
-    idealSize.Constrain(layoutConstraint.minSize, layoutConstraint.maxSize);
     return idealSize;
 }
 
