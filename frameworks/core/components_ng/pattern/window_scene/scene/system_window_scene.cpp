b72f7f36c1418009e75bb0c4a3da272257869c71
qpzeng <zengqingpeng6@huawei.com>
2023-05-31 18:44:20 +0800

    system scene
    Signed-off-by: qpzeng <zengqingpeng6@huawei.com>
    Change-Id: I957e0391bb8f5d789c234a76bbd9608ddfffa591

---
+++ frameworks/core/components_ng/pattern/window_scene/scene/system_window_scene.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "core/components_ng/pattern/window_scene/scene/system_window_scene.h"
+
+#include "ui/rs_surface_node.h"
+
+#include "core/components_ng/render/adapter/rosen_render_context.h"
+
+namespace OHOS::Ace::NG {
+SystemWindowScene::SystemWindowScene(const sptr<Rosen::SceneSession>& session) : session_(session)
+{
+    CHECK_NULL_VOID(session_);
+    auto sessionInfo = session_->GetSessionInfo();
+    LOGD("SystemWindowScene: bundleName: %{public}s, abilityName: %{public}s",
+        sessionInfo.bundleName_.c_str(), sessionInfo.abilityName_.c_str());
+}
+
+SystemWindowScene::~SystemWindowScene()
+{
+    CHECK_NULL_VOID(session_);
+    auto sessionInfo = session_->GetSessionInfo();
+    LOGD("~SystemWindowScene: bundleName: %{public}s, abilityName: %{public}s",
+        sessionInfo.bundleName_.c_str(), sessionInfo.abilityName_.c_str());
+}
+
+void SystemWindowScene::OnAttachToFrameNode()
+{
+    CHECK_NULL_VOID(session_);
+    auto sessionInfo = session_->GetSessionInfo();
+    auto name = sessionInfo.bundleName_;
+    auto pos = name.find_last_of('.');
+    name = (pos == std::string::npos) ? name : name.substr(pos + 1); // skip '.'
+
+    struct Rosen::RSSurfaceNodeConfig rsSurfaceNodeConfig;
+    rsSurfaceNodeConfig.SurfaceNodeName = name;
+    auto surfaceNode = Rosen::RSSurfaceNode::Create(rsSurfaceNodeConfig, Rosen::RSSurfaceNodeType::SELF_DRAWING_NODE);
+
+    auto host = GetHost();
+    CHECK_NULL_VOID(host);
+    auto context = AceType::DynamicCast<NG::RosenRenderContext>(host->GetRenderContext());
+    CHECK_NULL_VOID(context);
+    context->SetRSNode(surfaceNode);
+}
+} // namespace OHOS::Ace::NG
