9cc81added62c8c5c434731c01bcf601b06d0f22
yujiadong <yujiadong3@huawei.com>
2023-06-03 03:56:18 +0000

    fix menu_text_ng
    Signed-off-by: yujiadong <yujiadong3@huawei.com>
    Change-Id: Ic20a046961ad889749869d476e752fc52490b425

--- frameworks/core/components_ng/test/pattern/menu/menu_pattern_test_ng.cpp
+++ frameworks/core/components_ng/test/pattern/menu/menu_pattern_test_ng.cpp
@@ -30,6 +30,7 @@
 #include "core/components_ng/pattern/menu/menu_view.h"
 #include "core/components_ng/pattern/menu/multi_menu_layout_algorithm.h"
 #include "core/components_ng/pattern/pattern.h"
+#include "core/components_ng/pattern/scroll/scroll_pattern.h"
 #include "core/components_ng/pattern/text/text_layout_property.h"
 #include "core/components_ng/pattern/text/text_pattern.h"
 #include "core/components_ng/property/measure_property.h"
