1a7dadae85183099efb3e7be30297dd8efb2b217
chenchangjian <chenchangjian@huawei.com>
2023-06-07 14:47:57 +0800

    TextClock规格整改
    Signed-off-by: chenchangjian <chenchangjian@huawei.com>
    Change-Id: I09a4d798e6bc0b102ed2cb72ac637a45be345211

--- frameworks/core/components_ng/pattern/text_clock/text_clock_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_clock/text_clock_pattern.cpp
@@ -15,7 +15,6 @@
 
 #include "core/components_ng/pattern/text_clock/text_clock_pattern.h"
 
-#include <ctime>
 #include <string>
 #include <sys/time.h>
 
@@ -27,7 +26,6 @@
 
 namespace OHOS::Ace::NG {
 namespace {
-constexpr int32_t TOTAL_MINUTE_OF_HOUR = 60;
 constexpr int32_t TOTAL_SECONDS_OF_HOUR = 60 * 60;
 constexpr int32_t BASE_YEAR = 1900;
 constexpr int32_t INTERVAL_OF_U_SECOND = 1000000;
@@ -36,10 +34,8 @@ const std::string DEFAULT_FORMAT = "hms";
 
 int32_t GetSystemTimeZone()
 {
-    struct timeval currentTime {};
-    struct timezone timeZone {};
-    gettimeofday(&currentTime, &timeZone);
-    int32_t hoursWest = timeZone.tz_minuteswest / TOTAL_MINUTE_OF_HOUR;
+    // timezone is a global variable defined in time.h, in seconds
+    int32_t hoursWest = timezone / TOTAL_SECONDS_OF_HOUR;
     return hoursWest;
 }
 } // namespace
@@ -160,8 +156,8 @@ void TextClockPattern::UpdateTimeTextCallBack()
 std::string TextClockPattern::GetCurrentFormatDateTime()
 {
     time_t utc = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    time_t localTime = (hourWest_ == GetSystemTimeZone()) ? utc : utc - (hourWest_ * TOTAL_SECONDS_OF_HOUR);
-
+    auto tz = GetSystemTimeZone();
+    time_t localTime = (hourWest_ == tz) ? utc : utc - ((hourWest_ - tz) * TOTAL_SECONDS_OF_HOUR);
     auto* timeZoneTime = std::localtime(&localTime);
     CHECK_NULL_RETURN(timeZoneTime, "");
     // This is for i18n date time.
