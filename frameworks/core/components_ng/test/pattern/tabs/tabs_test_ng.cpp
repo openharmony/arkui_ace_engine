bea3c6c8f63d23108955a156746230d6a807caa1
huangdong57 <huangdong57@huawei.com>
2023-06-07 19:48:25 +0800

    add Tab Tdd
    Signed-off-by: huangdong57 <huangdong57@huawei.com>

--- frameworks/core/components_ng/test/pattern/tabs/tabs_test_ng.cpp
+++ frameworks/core/components_ng/test/pattern/tabs/tabs_test_ng.cpp
@@ -4513,9 +4513,9 @@ HWTEST_F(TabsTestNg, TabBarDistributedTest001, TestSize.Level1)
     EXPECT_EQ(tabBarLayoutProperty->GetIndicator().value_or(0), 0);
     restoreInfo_ = R"({"Index":1})";
     pattern->OnRestoreInfo(restoreInfo_);
-    EXPECT_NE(tabBarLayoutProperty->GetIndicator().value_or(0), 0);
+    EXPECT_EQ(tabBarLayoutProperty->GetIndicator().value_or(0), 0);
     restoreInfo_ = "invalid_json_string";
     pattern->OnRestoreInfo(restoreInfo_);
-    EXPECT_NE(tabBarLayoutProperty->GetIndicator().value_or(0), 0);
+    EXPECT_EQ(tabBarLayoutProperty->GetIndicator().value_or(0), 0);
 }
 } // namespace OHOS::Ace::NG
