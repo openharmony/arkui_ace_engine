f46e3a70a088bf2f7fa586c9811630522be6d2b5
chengfeiwang <chengfeiwang@huawei.com>
2023-06-08 07:43:25 +0000

    Adjusting the effectiveness of align and alignContent
    Signed-off-by: chengfeiwang <chengfeiwang@huawei.com>
    Change-Id: Ib971b58855a192d4e64e4966136d902a4a100289

--- frameworks/core/components_ng/pattern/stack/stack_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/stack/stack_layout_algorithm.cpp
@@ -48,11 +48,8 @@ void StackLayoutAlgorithm::PerformLayout(LayoutWrapper* layoutWrapper)
     auto align = Alignment::CENTER;
     auto layoutProperty = DynamicCast<StackLayoutProperty>(layoutWrapper->GetLayoutProperty());
     CHECK_NULL_VOID(layoutProperty);
-    if (layoutProperty->HasAlignmentContent()) {
-        align = layoutProperty->GetAlignmentContent().value_or(Alignment::CENTER);
-    }
-    if (layoutProperty->HasAlignment()) {
-        align = layoutProperty->GetAlignment().value_or(Alignment::CENTER);
+    if (layoutProperty->GetPositionProperty()) {
+        align = layoutProperty->GetPositionProperty()->GetAlignment().value_or(Alignment::CENTER);
     }
     // Update child position.
     for (const auto& child : layoutWrapper->GetAllChildrenWithBuild()) {
