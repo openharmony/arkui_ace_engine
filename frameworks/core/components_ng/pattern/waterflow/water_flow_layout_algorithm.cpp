2321f98bf0e94f4d34eec41bba87e35314560944
zcdqs <junfeng.lijunfeng@huawei.com>
2023-06-10 12:44:00 +0800

    waterflow padding clip
    Signed-off-by: zcdqs <junfeng.lijunfeng@huawei.com>
    Change-Id: I3a383859ad984129495838bf8f80fab0262d98c2

--- frameworks/core/components_ng/pattern/waterflow/water_flow_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/waterflow/water_flow_layout_algorithm.cpp
@@ -118,7 +118,6 @@ void WaterFlowLayoutAlgorithm::Measure(LayoutWrapper* layoutWrapper)
 
     InitialItemsCrossSize(layoutProperty, idealSize, layoutWrapper->GetTotalChildCount());
     mainSize_ = GetMainAxisSize(idealSize, axis);
-
     if (layoutInfo_.jumpIndex_ >= 0 && layoutInfo_.jumpIndex_ < layoutWrapper->GetTotalChildCount()) {
         auto crossIndex = layoutInfo_.GetCrossIndex(layoutInfo_.jumpIndex_);
         if (crossIndex == -1) {
@@ -173,10 +172,6 @@ void WaterFlowLayoutAlgorithm::Layout(LayoutWrapper* layoutWrapper)
                 currentOffset += OffsetF(crossOffset, mainOffset);
             } else {
                 currentOffset += OffsetF(mainOffset, crossOffset);
-            }
-            if (GetMainAxisOffset(currentOffset, axis_) + item.second.second < 0 ||
-                GetMainAxisOffset(currentOffset, axis_) > size.MainSize(axis_)) {
-                continue;
             }
             auto wrapper = layoutWrapper->GetOrCreateChildByIndex(GetChildIndexWithFooter(item.first));
             if (!wrapper) {
