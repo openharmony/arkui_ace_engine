45ce05b9d19dbdb2b9b026c222eec7bf8669047b
jyj-0306 <jiangyingjie2@huawei.com>
2023-07-01 16:26:13 +0800

    解决offset更新时，选中框和menu漂移问题
    Signed-off-by: jyj-0306 <jiangyingjie2@huawei.com>
    Change-Id: If84f739c5f7f87aad521207c7012d866fc78f89e

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
@@ -1025,6 +1025,7 @@ private:
     Offset lastTouchOffset_;
     PaddingPropertyF utilPadding_;
     OffsetF rightClickOffset_;
+    OffsetF offsetDifference_;
 
     bool setBorderFlag_ = true;
     BorderWidthProperty lastDiffBorderWidth_;
