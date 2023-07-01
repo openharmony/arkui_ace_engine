3982af1582a0cdce76123816e8cd05cae67143b7
openharmony_ci <120357966@qq.com>
2023-07-01 02:16:24 +0000

    回退 'Pull Request !15190 : 解决offset更新时，选中框和menu漂移问题'

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
@@ -1025,7 +1025,6 @@ private:
     Offset lastTouchOffset_;
     PaddingPropertyF utilPadding_;
     OffsetF rightClickOffset_;
-    OffsetF offsetDifference_;
 
     bool setBorderFlag_ = true;
     BorderWidthProperty lastDiffBorderWidth_;
--- frameworks/core/components_ng/property/measure_property.h
+++ frameworks/core/components_ng/property/measure_property.h
@@ -307,24 +307,14 @@ struct PaddingPropertyT {
 
     bool UpdateWithCheck(const PaddingPropertyT& value)
     {
-        bool isModified = false;
-        if (value.left.has_value() && (left != value.left)) {
+        if (*this != value) {
             left = value.left;
-            isModified = true;
-        }
-        if (value.right.has_value() && (right != value.right)) {
             right = value.right;
-            isModified = true;
-        }
-        if (value.top.has_value() && (top != value.top)) {
             top = value.top;
-            isModified = true;
-        }
-        if (value.bottom.has_value() && (bottom != value.bottom)) {
             bottom = value.bottom;
-            isModified = true;
+            return true;
         }
-        return isModified;
+        return false;
     }
 
     std::string ToString() const
