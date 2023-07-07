3aba5a2bb00dc85fd7ce72ed9355890ba68ccbfd
huangzejia1 <huangzejia1@huawei.com>
2023-07-07 12:42:27 +0800

    add drawing_decoration_painter-3
    Change-Id: I35e20f34099c8b667bd88d65a1a97147753c5033
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>

--- frameworks/core/components_ng/render/adapter/rosen/drawing_decoration_painter.cpp
+++ frameworks/core/components_ng/render/adapter/rosen/drawing_decoration_painter.cpp
@@ -270,45 +270,289 @@ void DrawingDecorationPainter::DrawingCreatePath(
 void DrawingDecorationPainter::DrawingCreateRect(
     const RefPtr<BasicShape>& basicShape, const SizeF& size, const OffsetF& position, RSPath& rsPath)
 {
+    const auto& rect = AceType::DynamicCast<ShapeRect>(basicShape);
+    CHECK_NULL_VOID(rect);
+    double left = DrawingDimensionToPx(rect->GetOffset().GetX(), size, LengthMode::HORIZONTAL) + position.GetX();
+    double top = DrawingDimensionToPx(rect->GetOffset().GetY(), size, LengthMode::VERTICAL) + position.GetY();
+    double width = DrawingDimensionToPx(rect->GetWidth(), size, LengthMode::HORIZONTAL);
+    double height = DrawingDimensionToPx(rect->GetHeight(), size, LengthMode::VERTICAL);
+    RSRect rsRect = RSRect(left, top, left + width, top + height);
+    auto radiusSize = SizeF(width, height);
+    float topLeftRadiusX = DrawingGetFloatRadiusValue(
+        rect->GetTopLeftRadius().GetX(), rect->GetTopLeftRadius().GetY(), radiusSize, LengthMode::HORIZONTAL);
+    float topLeftRadiusY = DrawingGetFloatRadiusValue(
+        rect->GetTopLeftRadius().GetY(), rect->GetTopLeftRadius().GetX(), radiusSize, LengthMode::VERTICAL);
+    float topRightRadiusX = DrawingGetFloatRadiusValue(
+        rect->GetTopRightRadius().GetX(), rect->GetTopRightRadius().GetY(), radiusSize, LengthMode::HORIZONTAL);
+    float topRightRadiusY = DrawingGetFloatRadiusValue(
+        rect->GetTopRightRadius().GetY(), rect->GetTopRightRadius().GetX(), radiusSize, LengthMode::VERTICAL);
+    float bottomRightRadiusX = DrawingGetFloatRadiusValue(
+        rect->GetBottomRightRadius().GetX(), rect->GetBottomRightRadius().GetY(), radiusSize, LengthMode::HORIZONTAL);
+    float bottomRightRadiusY = DrawingGetFloatRadiusValue(
+        rect->GetBottomRightRadius().GetY(), rect->GetBottomRightRadius().GetX(), radiusSize, LengthMode::VERTICAL);
+    float bottomLeftRadiusX = DrawingGetFloatRadiusValue(
+        rect->GetBottomLeftRadius().GetX(), rect->GetBottomLeftRadius().GetY(), radiusSize, LengthMode::HORIZONTAL);
+    float bottomLeftRadiusY = DrawingGetFloatRadiusValue(
+        rect->GetBottomLeftRadius().GetY(), rect->GetBottomLeftRadius().GetX(), radiusSize, LengthMode::VERTICAL);
+    std::vector<RSPoint> fRadii = { { topLeftRadiusX, topLeftRadiusY }, { topRightRadiusX, topRightRadiusY },
+        { bottomRightRadiusX, bottomRightRadiusY }, { bottomLeftRadiusX, bottomLeftRadiusY } };
+    RSRoundRect roundRect(rsRect, fRadii);
+    rsPath.AddRoundRect(roundRect);
 }
 
 void DrawingDecorationPainter::PaintGrayScale(const RSRoundRect& rRect, RSCanvas* canvas, float scale)
 {
+    if (GreatNotEqual(scale, 0.0)) {
+        if (canvas) {
+            RSAutoCanvasRestore acr(*canvas, true);
+            canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+            RSBrush brush;
+            brush.SetAntiAlias(true);
+            float matrix[20] = { 0.0f };
+            matrix[0] = matrix[5] = matrix[10] = 0.2126f * scale;
+            matrix[1] = matrix[6] = matrix[11] = 0.7152f * scale;
+            matrix[2] = matrix[7] = matrix[12] = 0.0722f * scale;
+            matrix[18] = 1.0f * scale;
+            RSColorMatrix colorMatrix;
+            colorMatrix.SetArray(matrix);
+            RSFilter filter;
+            filter.SetColorFilter(RSColorFilter::CreateMatrixColorFilter(colorMatrix));
+            brush.SetFilter(filter);
+            RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+            canvas->SaveLayer(slr);
+        }
+    }
 }
 
 void DrawingDecorationPainter::PaintBrightness(const RSRoundRect& rRect, RSCanvas* canvas, float bright)
 {
+    // brightness range = (0, 2)
+    // skip painting when brightness is normal
+    CHECK_NULL_VOID_NOLOG(!NearEqual(bright, 1.0));
+    if (canvas) {
+        RSAutoCanvasRestore acr(*canvas, true);
+        canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+        RSBrush brush;
+        brush.SetAntiAlias(true);
+        float matrix[20] = { 0.0f };
+        // shift brightness to (-1, 1)
+        bright = bright - 1;
+        matrix[0] = matrix[6] = matrix[12] = matrix[18] = 1.0f;
+        matrix[4] = matrix[9] = matrix[14] = bright;
+        RSColorMatrix colorMatrix;
+        colorMatrix.SetArray(matrix);
+        RSFilter filter;
+        filter.SetColorFilter(RSColorFilter::CreateMatrixColorFilter(colorMatrix));
+        brush.SetFilter(filter);
+        RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+        canvas->SaveLayer(slr);
+    }
 }
 
 void DrawingDecorationPainter::PaintContrast(const RSRoundRect& rRect, RSCanvas* canvas, float contrasts)
 {
+    // skip painting if contrast is normal
+    CHECK_NULL_VOID_NOLOG(!NearEqual(contrasts, 1.0));
+    if (canvas) {
+        RSAutoCanvasRestore acr(*canvas, true);
+        canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+        RSBrush brush;
+        brush.SetAntiAlias(true);
+        float matrix[20] = { 0.0f };
+        matrix[0] = matrix[6] = matrix[12] = contrasts;
+        matrix[4] = matrix[9] = matrix[14] = 128 * (1 - contrasts) / 255;
+        matrix[18] = 1.0f;
+        RSColorMatrix colorMatrix;
+        colorMatrix.SetArray(matrix);
+        RSFilter filter;
+        filter.SetColorFilter(RSColorFilter::CreateMatrixColorFilter(colorMatrix));
+        brush.SetFilter(filter);
+        RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+        canvas->SaveLayer(slr);
+    }
 }
 
 void DrawingDecorationPainter::PaintColorBlend(const RSRoundRect& rRect, RSCanvas* canvas, const Color& colorBlend)
 {
+    if (colorBlend.GetValue() != COLOR_MASK) {
+        if (canvas) {
+            RSAutoCanvasRestore acr(*canvas, true);
+            canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+            RSBrush brush;
+            brush.SetAntiAlias(true);
+            RSFilter filter;
+            filter.SetColorFilter(RSColorFilter::CreateBlendModeColorFilter(RSColor::ColorQuadSetARGB(
+                colorBlend.GetRed(), colorBlend.GetGreen(), colorBlend.GetBlue(), colorBlend.GetAlpha()),
+                RSBlendMode::PLUS));
+            brush.SetFilter(filter);
+            RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+            canvas->SaveLayer(slr);
+        }
+    }
 }
 
 void DrawingDecorationPainter::PaintSaturate(const RSRoundRect& rRect, RSCanvas* canvas, float saturates)
 {
+    if (!NearEqual(saturates, 1.0) && GreatOrEqual(saturates, 0.0)) {
+        if (canvas) {
+            RSAutoCanvasRestore acr(*canvas, true);
+            canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+            RSBrush brush;
+            brush.SetAntiAlias(true);
+            float matrix[20] = { 0.0f };
+            matrix[0] = 0.3086f * (1 - saturates) + saturates;
+            matrix[1] = matrix[11] = 0.6094f * (1 - saturates);
+            matrix[2] = matrix[7] = 0.0820f * (1 - saturates);
+            matrix[5] = matrix[10] = 0.3086f * (1 - saturates);
+            matrix[6] = 0.6094f * (1 - saturates) + saturates;
+            matrix[12] = 0.0820f * (1 - saturates) + saturates;
+            matrix[18] = 1.0f;
+            RSColorMatrix colorMatrix;
+            colorMatrix.SetArray(matrix);
+            RSFilter filter;
+            filter.SetColorFilter(RSColorFilter::CreateMatrixColorFilter(colorMatrix));
+            brush.SetFilter(filter);
+            RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+            canvas->SaveLayer(slr);
+        }
+    }
 }
 
 void DrawingDecorationPainter::PaintSepia(const RSRoundRect& rRect, RSCanvas* canvas, float sepias)
 {
+    if (sepias > 1.0) {
+        sepias = 1.0;
+    }
+    if (GreatNotEqual(sepias, 0.0)) {
+        if (canvas) {
+            RSAutoCanvasRestore acr(*canvas, true);
+            canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+            RSBrush brush;
+            brush.SetAntiAlias(true);
+            float matrix[20] = { 0.0f };
+            matrix[0] = 0.393f * sepias;
+            matrix[1] = 0.769f * sepias;
+            matrix[2] = 0.189f * sepias;
+
+            matrix[5] = 0.349f * sepias;
+            matrix[6] = 0.686f * sepias;
+            matrix[7] = 0.168f * sepias;
+
+            matrix[10] = 0.272f * sepias;
+            matrix[11] = 0.534f * sepias;
+            matrix[12] = 0.131f * sepias;
+            matrix[18] = 1.0f * sepias;
+            RSColorMatrix colorMatrix;
+            colorMatrix.SetArray(matrix);
+            RSFilter filter;
+            filter.SetColorFilter(RSColorFilter::CreateMatrixColorFilter(colorMatrix));
+            brush.SetFilter(filter);
+            RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+            canvas->SaveLayer(slr);
+        }
+    }
 }
 
 void DrawingDecorationPainter::PaintInvert(const RSRoundRect& rRect, RSCanvas* canvas, float inverts)
 {
+    if (GreatNotEqual(inverts, 0.0)) {
+        if (canvas) {
+            RSAutoCanvasRestore acr(*canvas, true);
+            canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+            RSBrush brush;
+            brush.SetAntiAlias(true);
+            float matrix[20] = { 0.0f };
+            if (inverts > 1.0) {
+                inverts = 1.0;
+            }
+            // complete color invert when dstRGB = 1 - srcRGB
+            // map (0, 1) to (1, -1)
+            matrix[0] = matrix[6] = matrix[12] = 1.0 - 2.0 * inverts;
+            matrix[18] = 1.0f;
+            // inverts = 0.5 -> RGB = (0.5, 0.5, 0.5) -> image completely gray
+            matrix[4] = matrix[9] = matrix[14] = inverts;
+            RSColorMatrix colorMatrix;
+            colorMatrix.SetArray(matrix);
+            RSFilter filter;
+            filter.SetColorFilter(RSColorFilter::CreateMatrixColorFilter(colorMatrix));
+            brush.SetFilter(filter);
+            RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+            canvas->SaveLayer(slr);
+        }
+    }
 }
 
 void DrawingDecorationPainter::PaintHueRotate(const RSRoundRect& rRect, RSCanvas* canvas, float hueRotate)
 {
+    if (GreatNotEqual(hueRotate, 0.0)) {
+        if (canvas) {
+            RSAutoCanvasRestore acr(*canvas, true);
+            canvas->ClipRoundRect(rRect, RSClipOp::INTERSECT, true);
+            RSBrush brush;
+            brush.SetAntiAlias(true);
+            while (GreatOrEqual(hueRotate, 360)) {
+                hueRotate -= 360;
+            }
+            float matrix[20] = { 0.0f };
+            int32_t type = hueRotate / 120;
+            float N = (hueRotate - 120 * type) / 120;
+            switch (type) {
+                case 0:
+                    // color change = R->G, G->B, B->R
+                    matrix[2] = matrix[5] = matrix[11] = N;
+                    matrix[0] = matrix[6] = matrix[12] = 1 - N;
+                    matrix[18] = 1.0f;
+                    break;
+                case 1:
+                    // compare to original: R->B, G->R, B->G
+                    matrix[1] = matrix[7] = matrix[10] = N;
+                    matrix[2] = matrix[5] = matrix[11] = 1 - N;
+                    matrix[18] = 1.0f;
+                    break;
+                case 2:
+                    // back to normal color
+                    matrix[0] = matrix[6] = matrix[12] = N;
+                    matrix[1] = matrix[7] = matrix[10] = 1 - N;
+                    matrix[18] = 1.0f;
+                    break;
+                default:
+                    break;
+            }
+            RSColorMatrix colorMatrix;
+            colorMatrix.SetArray(matrix);
+            RSFilter filter;
+            filter.SetColorFilter(RSColorFilter::CreateMatrixColorFilter(colorMatrix));
+            brush.SetFilter(filter);
+            RSSaveLayerOps slr(nullptr, &brush, RSSaveLayerOps::Flags::INIT_WITH_PREVIOUS);
+            canvas->SaveLayer(slr);
+        }
+    }
 }
 
 RSBrush DrawingDecorationPainter::CreateMaskDrawingBrush(const RefPtr<BasicShape>& basicShape)
 {
+    RSBrush brush;
+    brush.SetAntiAlias(true);
+    brush.SetColor(basicShape->GetColor().GetValue());
+    return brush;
 }
 
 RSImage DrawingDecorationPainter::CreateBorderImageGradient(const NG::Gradient& gradient, const SizeF& paintSize)
 {
+    auto shader = DrawingDecorationPainter::CreateGradientShader(gradient, paintSize);
+    RSBrush brush;
+    brush.SetAntiAlias(true);
+    brush.SetShaderEffect(std::move(shader));
+
+    RSBitmapFormat format { RSColorType::COLORTYPE_RGBA_8888, RSAlphaType::ALPHATYPE_OPAQUE };
+    RSBitmap rsBitmap;
+    rsBitmap.Build(paintSize.Width(), paintSize.Height(), format);
+
+    auto canvas = std::make_unique<RSCanvas>();
+    canvas->Bind(rsBitmap);
+    canvas->DrawBackground(brush);
+    RSImage rsImage;
+    rsImage.BuildFromBitmap(rsBitmap);
+    return rsImage;
 }
 } // namespace OHOS::Ace::NG
