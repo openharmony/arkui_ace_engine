a2873b1e3ae50999628957b3d2c07cc3285b1b6f
Tianer Zhou <zhoutianer@huawei.com>
2023-07-11 18:40:12 +0800

    update safeArea when new safeArea is null
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: Ic9b713f3c7085da1743457166c848d1b7e15f630

--- frameworks/core/components_ng/pattern/overlay/popup_base_pattern.cpp
+++ frameworks/core/components_ng/pattern/overlay/popup_base_pattern.cpp
@@ -25,9 +25,11 @@ void PopupBasePattern::BeforeCreateLayoutWrapper()
     auto inset = pipeline->GetSafeArea();
     auto manager = pipeline->GetSafeAreaManager();
     inset.bottom_ = inset.bottom_.Combine(manager->GetKeyboardInset());
-    CHECK_NULL_VOID_NOLOG(inset.IsValid());
     auto host = GetHost();
     CHECK_NULL_VOID(host);
-    host->GetLayoutProperty()->UpdateSafeAreaInsets(inset);
+    auto props = host->GetLayoutProperty();
+    if (inset.IsValid() || props->GetSafeAreaInsets()) {
+        props->UpdateSafeAreaInsets(inset);
+    }
 }
 } // namespace OHOS::Ace::NG
