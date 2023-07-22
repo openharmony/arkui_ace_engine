e74692d7bb3f5515f6cd4c0601eb64dea7186115
zhangjiayong1 <zhangjiayong3@huawei.com>
2023-07-22 08:25:28 +0000

    修复(displayCount-1)*itemSpace大于可视区，子组件不显示问题
    Signed-off-by: zhangjiayong1 <zhangjiayong3@huawei.com>

--- frameworks/core/components_ng/pattern/swiper/swiper_utils.h
+++ frameworks/core/components_ng/pattern/swiper/swiper_utils.h
@@ -71,31 +71,26 @@ public:
         auto childSelfIdealSize = idealSize;
         float childCalcIdealLength = 0.0f;
 
-        if (axis == Axis::HORIZONTAL) {
-            if (idealSize.Width().has_value()) {
-                childCalcIdealLength = (idealSize.Width().value() - itemSpace * itemSpaceCount -
+        if ((axis == Axis::HORIZONTAL && idealSize.Width().has_value()) ||
+            (axis == Axis::VERTICAL && idealSize.Height().has_value())) {
+            auto length = axis == Axis::HORIZONTAL ? idealSize.Width().value() :
+                idealSize.Height().value();
+            childCalcIdealLength = (length - itemSpace * itemSpaceCount -
                                         static_cast<float>(prevMargin + nextMargin)) / displayCount;
-                if (CheckMarginPropertyExceed(property, childCalcIdealLength)) {
-                    prevMargin = 0.0;
-                    nextMargin = 0.0;
-                    itemSpaceCount = CaculateDisplayItemSpaceCount(property, prevMargin, nextMargin);
-                    childCalcIdealLength = (idealSize.Width().value() - itemSpace * itemSpaceCount) / displayCount;
-                }
-                childSelfIdealSize.SetWidth(childCalcIdealLength);
+            if (LessNotEqual(childCalcIdealLength, 0.0)) {
+                itemSpace = 0.0f;
+                childCalcIdealLength = (length - itemSpace * itemSpaceCount -
+                                    static_cast<float>(prevMargin + nextMargin)) / displayCount;
+                property->UpdateItemSpace(Dimension(0.0));
             }
-        } else if (axis == Axis::VERTICAL) {
-            if (idealSize.Height().has_value()) {
-                childCalcIdealLength = (idealSize.Height().value() - itemSpace * itemSpaceCount -
-                                        static_cast<float>(prevMargin + nextMargin)) / displayCount;
-                if (CheckMarginPropertyExceed(property, childCalcIdealLength)) {
-                    prevMargin = 0.0;
-                    nextMargin = 0.0;
-                    itemSpaceCount = CaculateDisplayItemSpaceCount(property, prevMargin, nextMargin);
-                    childCalcIdealLength = (idealSize.Height().value() -
-                                            itemSpace * itemSpaceCount) / displayCount;
-                }
+            if (CheckMarginPropertyExceed(property, childCalcIdealLength)) {
+                prevMargin = 0.0;
+                nextMargin = 0.0;
+                itemSpaceCount = CaculateDisplayItemSpaceCount(property, prevMargin, nextMargin);
+                childCalcIdealLength = (length - itemSpace * itemSpaceCount) / displayCount;
+            }
+            axis == Axis::HORIZONTAL ? childSelfIdealSize.SetWidth(childCalcIdealLength) :
                 childSelfIdealSize.SetHeight(childCalcIdealLength);
-            }
         }
 
         layoutConstraint.selfIdealSize = childSelfIdealSize;
