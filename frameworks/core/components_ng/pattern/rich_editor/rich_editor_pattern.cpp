502ff493e67c28a4e51cc186e439f10eeaf81667
huangdong57 <huangdong57@huawei.com>
2023-07-25 11:49:23 +0800

    get focus when press tab
    Signed-off-by: huangdong57 <huangdong57@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -1581,6 +1581,9 @@ void RichEditorPattern::DeleteByDeleteValueInfo(const RichEditorDeleteValue& inf
 bool RichEditorPattern::OnKeyEvent(const KeyEvent& keyEvent)
 {
     if (keyEvent.action == KeyAction::DOWN) {
+        if (keyEvent.code == KeyCode::KEY_TAB) {
+            return false;
+        }
         if (keyEvent.code == KeyCode::KEY_DPAD_LEFT) {
             return CursorMoveLeft();
         }
