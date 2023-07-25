8e8c63af56c0001271bb68ec8e14b551bfa9aaab
zhangrongjie <zhangrongjie4@huawei.com>
2023-07-25 14:14:42 +0000

    fixed ff14a88 from https://gitee.com/rongShao-Z/arkui_ace_engine/pulls/16676 modify the srolling threshold of scrollable
    Signed-off-by: zhangrongjie <zhangrongjie4@huawei.com>
    Change-Id: I488244392c855a5fc6488a85c09aec102aab7734

--- frameworks/core/components/scroll/scrollable.cpp
+++ frameworks/core/components/scroll/scrollable.cpp
@@ -178,8 +178,13 @@ void Scrollable::Initialize(const WeakPtr<PipelineBase>& context)
     };
 
     if (Container::IsCurrentUseNewPipeline()) {
-        panRecognizerNG_ =
-            AceType::MakeRefPtr<NG::PanRecognizer>(DEFAULT_PAN_FINGER, panDirection, DEFAULT_PAN_DISTANCE);
+        const static int32_t PLATFORM_VERSION_TEN = 10;
+        float distance = DEFAULT_PAN_DISTANCE;
+        auto context = context_.Upgrade();
+        if (context && (context->GetMinPlatformVersion() >= PLATFORM_VERSION_TEN)) {
+            distance = static_cast<float>(Dimension(DEFAULT_PAN_DISTANCE, DimensionUnit::VP).ConvertToPx());
+        }
+        panRecognizerNG_ = AceType::MakeRefPtr<NG::PanRecognizer>(DEFAULT_PAN_FINGER, panDirection, distance);
 
         panRecognizerNG_->SetOnActionStart(actionStart);
         panRecognizerNG_->SetOnActionUpdate(actionUpdate);
