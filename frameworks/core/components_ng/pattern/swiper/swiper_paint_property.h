a8ead20514861d12a2a9f9c78515246bd4036e0b
xuzhidan <xuzhidan1@huawei.com>
2023-07-25 20:32:23 +0800

    修复Swiper组件loop属性动态修改后，未触发重新布局问题
    Signed-off-by: xuzhidan <xuzhidan1@huawei.com>
    Change-Id: I87b7d67e652f05709e90ad18911e6fa907383d19

--- frameworks/core/components_ng/pattern/swiper/swiper_paint_property.h
+++ frameworks/core/components_ng/pattern/swiper/swiper_paint_property.h
@@ -45,7 +45,6 @@ public:
     {
         auto paintProperty = MakeRefPtr<SwiperPaintProperty>();
         paintProperty->UpdatePaintProperty(this);
-        paintProperty->propLoop_ = CloneLoop();
         paintProperty->propSwiperAnimationStyle_ = CloneSwiperAnimationStyle();
         paintProperty->propEnabled_ = CloneEnabled();
         paintProperty->propDisableSwipe_ = CloneDisableSwipe();
@@ -57,7 +56,6 @@ public:
     void Reset() override
     {
         PaintProperty::Reset();
-        ResetLoop();
         ResetSwiperAnimationStyle();
         ResetEnabled();
         ResetDisableSwipe();
@@ -75,7 +73,6 @@ public:
     ACE_DEFINE_PROPERTY_ITEM_WITH_GROUP(SwiperAnimationStyle, Duration, int32_t, PROPERTY_UPDATE_RENDER);
     ACE_DEFINE_PROPERTY_ITEM_WITH_GROUP(SwiperAnimationStyle, Curve, RefPtr<Curve>, PROPERTY_UPDATE_RENDER);
 
-    ACE_DEFINE_PROPERTY_ITEM_WITHOUT_GROUP(Loop, bool, PROPERTY_UPDATE_RENDER);
     ACE_DEFINE_PROPERTY_ITEM_WITHOUT_GROUP(Enabled, bool, PROPERTY_UPDATE_RENDER);
     ACE_DEFINE_PROPERTY_ITEM_WITHOUT_GROUP(DisableSwipe, bool, PROPERTY_UPDATE_RENDER);
     ACE_DEFINE_PROPERTY_ITEM_WITHOUT_GROUP(EdgeEffect, EdgeEffect, PROPERTY_UPDATE_RENDER);
