3ad9a882a0cd8545e4828a723b6dc3aeba1c6755
sunjiakun <sunjiakun3@huawei.com>
2023-07-25 17:28:01 +0800

    fix inline
    Signed-off-by: sunjiakun <sunjiakun3@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -2196,6 +2196,9 @@ void TextFieldPattern::OnModifyDone()
         }
         HandleCounterBorder();
     }
+    if (!IsTextArea()) {
+        isTextInput_ = true;
+    }
     auto inputStyle = paintProperty->GetInputStyleValue(InputStyle::DEFAULT);
     if ((!IsSelected() && inputStyle == InputStyle::INLINE) ||
         ((inputStyle == InputStyle::DEFAULT) && preInputStyle_ != InputStyle::INLINE)) {
