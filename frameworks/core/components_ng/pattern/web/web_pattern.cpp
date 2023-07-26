505d515468e06673aa18b56781927c3d2834d919
echoorchid <huweidong2@huawei.com>
2023-07-26 19:34:23 +0800

    revert padding to fix position
    Signed-off-by: echoorchid <huweidong2@huawei.com>

--- frameworks/core/components_ng/pattern/web/web_pattern.cpp
+++ frameworks/core/components_ng/pattern/web/web_pattern.cpp
@@ -980,7 +980,6 @@ void WebPattern::UpdateContentOffset(const RefPtr<LayoutWrapper>& dirty)
 
 bool WebPattern::OnDirtyLayoutWrapperSwap(const RefPtr<LayoutWrapper>& dirty, const DirtySwapConfig& config)
 {
-    UpdateContentOffset(dirty);
     if (!config.contentSizeChange || isInWindowDrag_) {
         return false;
     }
