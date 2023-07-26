91344b8595401722b3fea8737380002ffe9aa995
Zhang-Dong-hui <zhangdonghui5@huawei.com>
2023-07-26 10:25:34 +0000

    fixed a0a03f8 from https://gitee.com/Zhang-Dong-hui/arkui_ace_engine/pulls/16773 clip the part that goes beyond the boundary
    Signed-off-by: Zhang-Dong-hui <zhangdonghui5@huawei.com>
    Change-Id: I87faaa7f31bdcfed81a0652cc9597c278faacd90

--- frameworks/core/components_ng/pattern/time_picker/timepicker_column_pattern.cpp
+++ frameworks/core/components_ng/pattern/time_picker/timepicker_column_pattern.cpp
@@ -64,6 +64,7 @@ void TimePickerColumnPattern::OnAttachToFrameNode()
     jumpInterval_ = pickerTheme->GetJumpInterval().ConvertToPx();
     CreateAnimation();
     InitPanEvent(gestureHub);
+    host->GetRenderContext()->SetClipToFrame(true);
 }
 
 void TimePickerColumnPattern::OnModifyDone()
