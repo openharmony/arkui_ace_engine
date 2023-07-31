0f1f4d896c1755e3559b1d34b99277b9c037f6f9
tengfan <tengfan3@huawei.com>
2023-07-31 17:56:24 +0800

    modify piacker title hover style
    Signed-off-by: tengfan <tengfan3@huawei.com>

--- frameworks/core/components_ng/pattern/picker/datepicker_dialog_view.h
+++ frameworks/core/components_ng/pattern/picker/datepicker_dialog_view.h
@@ -69,8 +69,8 @@ private:
     static void CreateLunarswitchNode(
         const RefPtr<FrameNode>& contentColumn, std::function<void(const bool)>&& changeEvent, bool isLunar);
     static void SetTitleMouseHoverEvent(const RefPtr<FrameNode>& titleRow);
-    static void HandleMouseEvent(const RefPtr<FrameNode>& titleRow, bool isHover);
-    static void PlayHoverAnimation(const RefPtr<FrameNode>& titleRow, const Color& color);
+    static void HandleMouseEvent(const RefPtr<FrameNode>& titleButton, bool isHover);
+    static void PlayHoverAnimation(const RefPtr<FrameNode>& titleButton, const Color& color);
 };
 } // namespace OHOS::Ace::NG
 
