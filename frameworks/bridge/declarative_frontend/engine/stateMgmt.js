1b4bf95ba7b0441208da09fd8e78adb4ea83cebf
LiWenzhen <liwenzhen3@huawei.com>
2023-07-31 08:56:35 +0000

    回退 'Pull Request !16899 : remove to record the uncessary element id for the sync source'

--- frameworks/bridge/declarative_frontend/engine/stateMgmt.js
+++ frameworks/bridge/declarative_frontend/engine/stateMgmt.js
@@ -3958,7 +3958,7 @@ class SynchedPropertyTwoWayPU extends ObservedPropertyAbstractPU {
             // register to the parent property
             this.source_.addSubscriber(this);
             // register to the ObservedObject
-            ObservedObject.addOwningProperty(this.source_.getUnmonitored(), this);
+            ObservedObject.addOwningProperty(this.source_.get(), this);
         }
         else {
             throw new SyntaxError(`SynchedPropertyObjectTwoWayPU[${this.id__()}, '${this.info() || "unknown"}']: constructor @Link/@Consume source variable in parent/ancestor @ Component must be defined. Application error!`);
@@ -4579,7 +4579,7 @@ class ViewPU extends NativeViewPartialUpdate {
         }
         const factory = (source) => {
             const result = ((source instanceof ObservedPropertySimple) || (source instanceof ObservedPropertySimplePU))
-                ? new SynchedPropertySimpleTwoWayPU(source, this, consumeVarName)
+                ? new SynchedPropertyObjectTwoWayPU(source, this, consumeVarName)
                 : new SynchedPropertyObjectTwoWayPU(source, this, consumeVarName);
             stateMgmtConsole.error(`The @Consume is instance of ${result.constructor.name}`);
             return result;
