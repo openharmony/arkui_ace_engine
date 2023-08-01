c1d525c138d245b0bcc2bfcdf84195dd8c291719
lijuan <lijuan124@huawei.com>
2023-08-01 09:49:29 +0000

    修复text告警
    Signed-off-by: lijuan <lijuan124@huawei.com>

--- frameworks/core/components_ng/pattern/text/text_pattern.cpp
+++ frameworks/core/components_ng/pattern/text/text_pattern.cpp
@@ -266,7 +266,8 @@ void TextPattern::OnHandleMoveDone(const RectF& handleRect, bool isFirstHandle)
 
 bool TextPattern::IsSelectAll()
 {
-    return textSelector_.GetTextStart() == 0 && textSelector_.GetTextEnd() == GetWideText().length() + imageCount_;
+    return textSelector_.GetTextStart() == 0 &&
+           textSelector_.GetTextEnd() == static_cast<int32_t>(GetWideText().length()) + imageCount_;
 }
 std::wstring TextPattern::GetWideText() const
 {
