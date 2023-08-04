86882c91b7c9709d9cc1a821facbd13461be5320
lvzikai <lvzikai@huawei.com>
2023-08-04 01:43:25 +0000

    Repair video pause problem
    Signed-off-by: lvzikai <lvzikai@huawei.com>
    Change-Id: Id0692b733a634ace333e63538a1c6eb75cadc879

--- frameworks/core/components_ng/pattern/video/video_pattern.cpp
+++ frameworks/core/components_ng/pattern/video/video_pattern.cpp
@@ -1134,12 +1134,6 @@ void VideoPattern::Start()
         LOGE("Player has not prepared");
         return;
     }
-
-    if (isPlaying_) {
-        LOGI("Already isPlaying");
-        return;
-    }
-
     auto context = PipelineContext::GetCurrentContext();
     CHECK_NULL_VOID(context);
     auto host = GetHost();
@@ -1168,12 +1162,6 @@ void VideoPattern::Pause()
         LOGE("media player is invalid.");
         return;
     }
-
-    if (!isPlaying_) {
-        LOGI("Already pause");
-        return;
-    }
-
     LOGD("Video Pause");
     mediaPlayer_->Pause();
 }
