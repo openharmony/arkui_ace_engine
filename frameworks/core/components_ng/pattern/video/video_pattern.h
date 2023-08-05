719d9cf0bcc1acde3a897c3b4c50f81a683eb310
lvzikai <lvzikai@huawei.com>
2023-08-05 10:48:26 +0000

    Video create time information according to controls property
    Signed-off-by: lvzikai <lvzikai@huawei.com>
    Change-Id: Iad266eb9cbfd9002834dd09b79a4d76be1e074ff

--- frameworks/core/components_ng/pattern/video/video_pattern.h
+++ frameworks/core/components_ng/pattern/video/video_pattern.h
@@ -264,7 +264,7 @@ private:
     void UpdateVideoProperty();
     
     static RefPtr<FrameNode> CreateSVG();
-    static RefPtr<FrameNode> CreateText(uint32_t time);
+    RefPtr<FrameNode> CreateText(uint32_t time);
     RefPtr<FrameNode> CreateSlider();
     void ChangePlayButtonTag();
     void ChangePlayButtonTag(RefPtr<FrameNode>& playBtn);
