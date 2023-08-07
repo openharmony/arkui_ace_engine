d4adf66110889292b566a404baf436eaf5ffb80f
“huangdong57” <huangdong57@huawei.com>
2023-08-07 10:33:01 +0800

    richeditor支持避障(beta2)
    Signed-off-by: “huangdong57” <huangdong57@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.h
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.h
@@ -173,6 +173,9 @@ private:
     void HandleMouseEvent(const MouseInfo& info);
     void HandleTouchEvent(const TouchEventInfo& info);
     void InitLongPressEvent(const RefPtr<GestureEventHub>& gestureHub);
+    void UseHostToUpdateTextFieldManager();
+    void UpdateTextFieldManager(const Offset& offset, float height);
+    bool HasFocus() const;
 #ifdef ENABLE_DRAG_FRAMEWORK
     void InitDragDropEvent();
     void UpdateSpanItemDragStatus(const std::list<ResultObject>& resultObjects, bool IsDragging);
@@ -244,6 +247,7 @@ private:
     RefPtr<RichEditorContentModifier> richEditorContentModifier_;
     RefPtr<RichEditorOverlayModifier> richEditorOverlayModifier_;
     MoveDirection moveDirection_ = MoveDirection::FORWARD;
+    RectF frameRect_;
 #ifdef ENABLE_DRAG_FRAMEWORK
     std::list<ResultObject> dragResultObjects_;
 #endif // ENABLE_DRAG_FRAMEWORK
