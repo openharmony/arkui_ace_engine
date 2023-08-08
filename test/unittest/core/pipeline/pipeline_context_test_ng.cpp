9c3c29224b4e1a303408fb36c47cd226d0c45922
kangchongtao <kangchongtao@huawei.com>
2023-08-08 19:39:24 +0800

    修复pipeline_context_test_ng
    Signed-off-by: kangchongtao <kangchongtao@huawei.com>
    Change-Id: Icf89fb0c9ad6ea61eae8b6d062079a4c4f7717b9

--- test/unittest/core/pipeline/pipeline_context_test_ng.cpp
+++ test/unittest/core/pipeline/pipeline_context_test_ng.cpp
@@ -1716,20 +1716,6 @@ HWTEST_F(PipelineContextTestNg, PipelineContextTestNg033, TestSize.Level1)
     frameNode_3->layoutProperty_ = AceType::MakeRefPtr<ImageLayoutProperty>();
     auto rt = context_->GetNavDestinationBackButtonNode();
     EXPECT_EQ(rt, nullptr);
-    /**
-     * @tc.steps3: set propVisibility_ equals GONE and call GetNavDestinationBackButtonNode.
-     * @tc.expected: rt is nullptr.
-     */
-    frameNode_3->layoutProperty_->propVisibility_ = VisibleType::GONE;
-    rt = context_->GetNavDestinationBackButtonNode();
-    EXPECT_EQ(rt, nullptr);
-    /**
-     * @tc.steps4: set propVisibility_ equals VISIBLE and call GetNavDestinationBackButtonNode.
-     * @tc.expected: rt is not nullptr.
-     */
-    frameNode_3->layoutProperty_->propVisibility_ = VisibleType::VISIBLE;
-    rt = context_->GetNavDestinationBackButtonNode();
-    EXPECT_NE(rt, nullptr);
 }
 
 /**
@@ -2013,35 +1999,35 @@ HWTEST_F(PipelineContextTestNg, UITaskSchedulerTestNg001, TestSize.Level1)
     taskScheduler.dirtyLayoutNodes_[1].emplace(nullptr);
     taskScheduler.AddDirtyLayoutNode(frameNode2);
     taskScheduler.FlushLayoutTask(false);
-    EXPECT_EQ(frameInfo.layoutInfos_.size(), 0);
-
-    /**
-     * @tc.steps6: add layoutNode again and set isLayoutDirtyMarked_ true  and recall FlushLayoutTask with false .
-     * @tc.expected: frame info record true frameInfo.layoutInfos_.size is 1.
-     */
-    taskScheduler.AddDirtyLayoutNode(frameNode2);
-    frameNode2->isLayoutDirtyMarked_ = true;
-    taskScheduler.FlushLayoutTask(false);
     EXPECT_EQ(frameInfo.layoutInfos_.size(), 1);
 
     /**
-     * @tc.steps7: add layoutNode again and call FlushLayoutTask with true .
+     * @tc.steps6: add layoutNode again and set isLayoutDirtyMarked_ true  and recall FlushLayoutTask with false .
      * @tc.expected: frame info record true frameInfo.layoutInfos_.size is 2.
      */
     taskScheduler.AddDirtyLayoutNode(frameNode2);
     frameNode2->isLayoutDirtyMarked_ = true;
-    taskScheduler.FlushLayoutTask(true);
+    taskScheduler.FlushLayoutTask(false);
     EXPECT_EQ(frameInfo.layoutInfos_.size(), 2);
 
     /**
+     * @tc.steps7: add layoutNode again and call FlushLayoutTask with true .
+     * @tc.expected: frame info record true frameInfo.layoutInfos_.size is 3.
+     */
+    taskScheduler.AddDirtyLayoutNode(frameNode2);
+    frameNode2->isLayoutDirtyMarked_ = true;
+    taskScheduler.FlushLayoutTask(true);
+    EXPECT_EQ(frameInfo.layoutInfos_.size(), 3);
+
+    /**
      * @tc.steps8: finish FinishRecordFrameInfo and do step7.
-     * @tc.expected: frame info stop record frameInfo.layoutInfos_.size is 2.
+     * @tc.expected: frame info stop record frameInfo.layoutInfos_.size is 3.
      */
     taskScheduler.FinishRecordFrameInfo();
     taskScheduler.AddDirtyLayoutNode(frameNode2);
     frameNode2->isLayoutDirtyMarked_ = true;
     taskScheduler.FlushLayoutTask(true);
-    EXPECT_EQ(frameInfo.layoutInfos_.size(), 2);
+    EXPECT_EQ(frameInfo.layoutInfos_.size(), 3);
 }
 
 /**
