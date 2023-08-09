daca2fe7c47d7ac75f5597a8c586d021098a248c
hw_wyx <wuyinxiao@huawei.com>
2023-08-09 18:50:19 +0800

    fix LetterSpacing PerData.
    Signed-off-by: hw_wyx <wuyinxiao@huawei.com>
    Change-Id: Ifa3524b542052e1f5ddd95cc91a1fa7d3d515aef

--- frameworks/bridge/declarative_frontend/jsview/js_text.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_text.cpp
@@ -352,6 +352,18 @@ void JSText::SetLetterSpacing(const JSCallbackInfo& info)
         LOGI("The argv is wrong, it is supposed to have at least 1 argument");
         return;
     }
+    if (info[0]->IsString()) {
+        auto value = info[0]->ToString();
+        if (!value.empty() && value.back() == '%') {
+            auto pipelineContext = PipelineBase::GetCurrentContext();
+            CHECK_NULL_VOID_NOLOG(pipelineContext);
+            auto theme = pipelineContext->GetTheme<TextTheme>();
+            CHECK_NULL_VOID_NOLOG(theme);
+            CalcDimension defaultValue = theme->GetTextStyle().GetLetterSpacing();
+            TextModel::GetInstance()->SetLetterSpacing(defaultValue);
+            return;
+        }
+    }
     CalcDimension value;
     if (!ParseJsDimensionFp(info[0], value)) {
         return;
