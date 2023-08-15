6ef881a1143cf86d199ba960e23acbeff866d179
yuhaoge <yuhaoge1@huawei.com>
2023-08-15 17:01:30 +0800

    --amend
    Signed-off-by: yuhaoge <yuhaoge1@huawei.com>

--- frameworks/core/components_ng/pattern/web/web_pattern.cpp
+++ frameworks/core/components_ng/pattern/web/web_pattern.cpp
@@ -1447,11 +1447,11 @@ bool WebPattern::ProcessVirtualKeyBoard(int32_t width, int32_t height, double ke
     if (NearZero(keyboard)) {
         drawSize_.SetSize(drawSizeCache_);
         UpdateWebLayoutSize(width, height);
-        isVirtualKeyBoardShow_ = VkState::VK_HIDE;
         if (isVirtualKeyBoardShow_ == VkState::VK_HIDE) {
             LOGI("Web ProcessVirtualKeyBoard no need to process");
             return false;
         }
+        isVirtualKeyBoardShow_ = VkState::VK_HIDE;
     } else if (isVirtualKeyBoardShow_ != VkState::VK_SHOW) {
         drawSizeCache_.SetSize(drawSize_);
         if (drawSize_.Height() <= (height - keyboard - GetCoordinatePoint()->GetY())) {
