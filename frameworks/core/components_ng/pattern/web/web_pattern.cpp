49241a6f620cd276d935fbd4941322125875c401
周礼亭 <zhouliting5@huawei.com>
2023-08-18 07:31:13 +0000

    add 2
    Signed-off-by: 周礼亭 <zhouliting5@huawei.com>

--- frameworks/core/components_ng/pattern/web/web_pattern.cpp
+++ frameworks/core/components_ng/pattern/web/web_pattern.cpp
@@ -141,6 +141,8 @@ constexpr int32_t DOUBLE_CLICK_NUM = 2;
 constexpr double DEFAULT_DBCLICK_INTERVAL = 0.5;
 constexpr double DEFAULT_DBCLICK_OFFSET = 2.0;
 constexpr double DEFAULT_AXIS_RATIO = -0.06;
+constexpr double DEFAULT_WEB_WIDTH = 100;
+constexpr double DEFAULT_WEB_HEIGHT = 80;
 // web feature params
 const std::string VISIBLE_ACTIVE_ENABLE = "persist.web.visible_active_enable";
 const std::string MEMORY_LEVEL_ENABEL = "persist.web.memory_level_enable";
@@ -993,6 +995,15 @@ bool WebPattern::OnDirtyLayoutWrapperSwap(const RefPtr<LayoutWrapper>& dirty, co
     if (drawSize.IsInfinite() || drawSize.IsEmpty()) {
         LOGE("resize invalid %{public}f, %{public}f", drawSize.Width(), drawSize.Height());
         return false;
+    }
+
+    if (GreatOrEqual(drawSize.Width(), Infinity<double>())) {
+        drawSize.SetWidth(DEFAULT_WEB_WIDTH);
+        LOGE("SetWidth to 100");
+    }
+    if (GreatOrEqual(drawSize.Height(), Infinity<double>())) {
+        drawSize.SetHeight(DEFAULT_WEB_HEIGHT);
+        LOGE("SetHeight to 80");
     }
 
     drawSize_ = drawSize;
