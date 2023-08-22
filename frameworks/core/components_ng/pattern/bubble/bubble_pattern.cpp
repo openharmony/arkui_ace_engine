90cbfdb6fc282e0e1bff26dcace75b9121effd5a
fengjituo111 <fengjituo@huawei.com>
2023-08-22 12:23:06 +0000

    【popup】拆分UpdatePopupNode、修复动效缺失、修复键盘走焦
    Signed-off-by: fengjituo111 <fengjituo@huawei.com>
    Change-Id: I6c2dbd6dc812b6e6ab39edf20e55c0f2173b7abd

--- frameworks/core/components_ng/pattern/bubble/bubble_pattern.cpp
+++ frameworks/core/components_ng/pattern/bubble/bubble_pattern.cpp
@@ -320,16 +320,11 @@ void BubblePattern::PopBubble()
     auto layoutProp = host->GetLayoutProperty<BubbleLayoutProperty>();
     CHECK_NULL_VOID(layoutProp);
     auto showInSubWindow = layoutProp->GetShowInSubWindow().value_or(false);
-    StartExitingAnimation([showInSubWindow, targetId = targetNodeId_, popupInfo,
-                              weakOverlayManger = AceType::WeakClaim(AceType::RawPtr(overlayManager))]() {
-        if (showInSubWindow) {
-            SubwindowManager::GetInstance()->HidePopupNG(targetId);
-        } else {
-            auto overlay = weakOverlayManger.Upgrade();
-            CHECK_NULL_VOID(overlay);
-            overlay->UpdatePopupNode(targetId, popupInfo);
-        }
-    });
+    if (showInSubWindow) {
+        SubwindowManager::GetInstance()->HidePopupNG(targetNodeId_);
+    } else {
+        overlayManager->HidePopup(targetNodeId_, popupInfo);
+    }
 }
 
 RefPtr<PopupTheme> BubblePattern::GetPopupTheme()
@@ -431,10 +426,6 @@ void BubblePattern::StartAlphaEnteringAnimation(std::function<void()> finish)
 
 void BubblePattern::StartExitingAnimation(std::function<void()> finish)
 {
-    if (!IsOnShow()) {
-        return;
-    }
-
     StartOffsetExitingAnimation();
     StartAlphaExitingAnimation(finish);
 }
