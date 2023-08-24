a758decbfe19e9f16208543c3d93843ab44b870b
w00445410 <wangmingming15@huawei.com>
2023-08-24 01:39:00 +0000

    Modify the spring curve to a Bézier curve
    Signed-off-by: w00445410 <wangmingming15@huawei.com>
    Change-Id: Id96131973eb0c85a0e25f2a14e6638fefbfac7b0

--- frameworks/core/components_ng/pattern/navigation/navigation_group_node.cpp
+++ frameworks/core/components_ng/pattern/navigation/navigation_group_node.cpp
@@ -59,9 +59,8 @@ constexpr int32_t OPACITY_TITLE_DURATION = 150;
 constexpr int32_t OPACITY_BACKBUTTON_IN_DELAY = 150;
 constexpr int32_t OPACITY_BACKBUTTON_IN_DURATION = 200;
 constexpr int32_t OPACITY_BACKBUTTON_OUT_DURATION = 67;
-constexpr int32_t DEFAULT_ANIMATION_DURATION = 400;
-const RefPtr<InterpolatingSpring> interpolatingSpringCurve =
-    AceType::MakeRefPtr<InterpolatingSpring>(18.0f, 1.0f, 324.0f, 36.0f);
+constexpr int32_t DEFAULT_ANIMATION_DURATION = 450;
+const RefPtr<CubicCurve> bezierCurve = AceType::MakeRefPtr<CubicCurve>(0.23f, 0.07f, 0.0f, 1.0f);
 } // namespace
 RefPtr<NavigationGroupNode> NavigationGroupNode::GetOrCreateGroupNode(
     const std::string& tag, int32_t nodeId, const std::function<RefPtr<Pattern>(void)>& patternCreator)
@@ -321,7 +320,7 @@ void NavigationGroupNode::ExitTransitionWithPop(const RefPtr<FrameNode>& node)
 {
     CHECK_NULL_VOID(node);
     AnimationOption option;
-    option.SetCurve(interpolatingSpringCurve);
+    option.SetCurve(bezierCurve);
     option.SetFillMode(FillMode::FORWARDS);
     option.SetDuration(DEFAULT_ANIMATION_DURATION);
     auto size = GetGeometryNode()->GetFrameSize();
@@ -418,7 +417,7 @@ void NavigationGroupNode::ExitTransitionWithPush(const RefPtr<FrameNode>& node, 
 {
     CHECK_NULL_VOID(node);
     AnimationOption option;
-    option.SetCurve(interpolatingSpringCurve);
+    option.SetCurve(bezierCurve);
     option.SetFillMode(FillMode::FORWARDS);
     option.SetDuration(DEFAULT_ANIMATION_DURATION);
     auto size = GetGeometryNode()->GetFrameSize();
@@ -495,7 +494,7 @@ void NavigationGroupNode::EnterTransitionWithPush(const RefPtr<FrameNode>& node,
 {
     CHECK_NULL_VOID(node);
     AnimationOption option;
-    option.SetCurve(interpolatingSpringCurve);
+    option.SetCurve(bezierCurve);
     option.SetFillMode(FillMode::FORWARDS);
     option.SetDuration(DEFAULT_ANIMATION_DURATION);
     auto size = GetGeometryNode()->GetFrameSize();
@@ -572,7 +571,7 @@ void NavigationGroupNode::EnterTransitionWithPop(const RefPtr<FrameNode>& node, 
 {
     CHECK_NULL_VOID(node);
     AnimationOption option;
-    option.SetCurve(interpolatingSpringCurve);
+    option.SetCurve(bezierCurve);
     option.SetFillMode(FillMode::FORWARDS);
     option.SetDuration(DEFAULT_ANIMATION_DURATION);
     auto size = GetGeometryNode()->GetFrameSize();
