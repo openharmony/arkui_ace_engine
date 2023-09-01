11b2ca16b886fe9b768d95302e500a4178508bca
zhouchaobo <zhouchaobo@huawei.com>
2023-09-01 10:07:12 +0800

    fixed 0e5bcf8 from https://gitee.com/zhou-chaobo/arkui_ace_engine/pulls/18889 Tabs rotate bug fix
    Signed-off-by: zhouchaobo <zhouchaobo@huawei.com>
    Change-Id: Iae62b42e62abad3b7bfdce08eea9b25a6ac03e20

--- frameworks/core/components_ng/pattern/tabs/tab_bar_pattern.cpp
+++ frameworks/core/components_ng/pattern/tabs/tab_bar_pattern.cpp
@@ -232,7 +232,7 @@ void TabBarPattern::HandleMouseEvent(const MouseInfo& info)
     if (totalCount < 0) {
         return;
     }
-    auto index = CalculateSelectedIndex(info.GetGlobalLocation());
+    auto index = CalculateSelectedIndex(info.GetLocalLocation());
     if (index < 0 || index >= totalCount) {
         if (hoverIndex_.has_value() && !touchingIndex_.has_value()) {
             HandleMoveAway(hoverIndex_.value());
@@ -620,7 +620,7 @@ void TabBarPattern::HandleClick(const GestureEvent& info)
         return;
     }
 
-    auto index = CalculateSelectedIndex(info.GetGlobalLocation());
+    auto index = CalculateSelectedIndex(info.GetLocalLocation());
     if (index < 0 || index >= totalCount || !swiperController_ ||
         indicator_ >= static_cast<int32_t>(tabBarStyles_.size())) {
         return;
@@ -960,7 +960,7 @@ void TabBarPattern::HandleTouchEvent(const TouchLocationInfo& info)
         return;
     }
     auto touchType = info.GetTouchType();
-    auto index = CalculateSelectedIndex(info.GetGlobalLocation());
+    auto index = CalculateSelectedIndex(info.GetLocalLocation());
     if (touchType == TouchType::DOWN && index >= 0 && index < totalCount) {
         HandleTouchDown(index);
         touchingIndex_ = index;
@@ -985,8 +985,7 @@ int32_t TabBarPattern::CalculateSelectedIndex(const Offset& info)
     auto layoutProperty = host->GetLayoutProperty<TabBarLayoutProperty>();
     CHECK_NULL_RETURN(layoutProperty, -1);
     auto axis = layoutProperty->GetAxis().value_or(Axis::HORIZONTAL);
-    auto local = OffsetF(info.GetX() - host->GetPaintRectGlobalOffsetWithTranslate().GetX(),
-        info.GetY() - host->GetPaintRectGlobalOffsetWithTranslate().GetY());
+    auto local = OffsetF(info.GetX(), info.GetY());
     if (axis == Axis::VERTICAL) {
         auto clickRange = std::make_pair(tabItemOffsets_[0].GetY(), tabItemOffsets_[tabItemOffsets_.size() - 1].GetY());
         if (LessNotEqual(local.GetY(), clickRange.first) || GreatNotEqual(local.GetY(), clickRange.second)) {
