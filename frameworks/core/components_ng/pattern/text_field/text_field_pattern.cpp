eb61f6ed1641b5dbbef0004f61955964fd7f7fdf
lijuan <lijuan124@huawei.com>
2023-09-02 10:49:23 +0000

    fixed d6f9521 from https://gitee.com/lijuan124/arkui_ace_engine/pulls/18993 修复动态切换时输入框内容被清空以及预览动态切换maxlen失效问题
    Signed-off-by: lijuan <lijuan124@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -2275,7 +2275,6 @@ void TextFieldPattern::OnModifyDone()
         operationRecords_.clear();
         redoOperationRecords_.clear();
     }
-#if defined(ENABLE_STANDARD_INPUT)
     auto maxLength = GetMaxLength();
     if (GreatNotEqual(textWidth, maxLength)) {
         textEditingValue_.text = StringUtils::ToString(textEditingValue_.GetWideText().substr(0, maxLength));
@@ -2285,7 +2284,6 @@ void TextFieldPattern::OnModifyDone()
         CHECK_NULL_VOID(layoutProperty);
         layoutProperty->UpdateNeedFireOnChange(true);
     }
-#endif
     FireOnChangeIfNeeded();
     if (IsTextArea() || IsNormalInlineState()) {
         SetAxis(Axis::VERTICAL);
