16c9cbdd55c717e8dd5dd1cdaa8b37a26a12fc51
tengfan66 <tengfan3@huawei.com>
2023-09-04 14:34:53 +0800

    tabs int warning
    Signed-off-by: tengfan66 <tengfan3@huawei.com>

--- frameworks/core/components_ng/pattern/tabs/tab_bar_pattern.cpp
+++ frameworks/core/components_ng/pattern/tabs/tab_bar_pattern.cpp
@@ -1569,7 +1569,7 @@ float TabBarPattern::CalculateBackChildrenMainSize(int32_t indicator)
         auto childFrameSize = childGeometryNode->GetMarginFrameSize();
         backChildrenMainSize += childFrameSize.MainSize(axis_);
     }
-    return indicator == childCount - 1 ? 0.0f : backChildrenMainSize;
+    return indicator == static_cast<int32_t>(childCount - 1) ? 0.0f : backChildrenMainSize;
 }
 
 void TabBarPattern::SetEdgeEffect(const RefPtr<GestureEventHub>& gestureHub)
