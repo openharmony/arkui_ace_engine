58396184e7c6622de704062724ef9eab90a1192b
lijuan <lijuan124@huawei.com>
2023-09-05 09:03:59 +0000

    fixed 6b89739 from https://gitee.com/lijuan124/arkui_ace_engine/pulls/19136 修复拖拽后释放光标位置不对问题
    Signed-off-by: lijuan <lijuan124@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -3165,8 +3165,6 @@ void TextFieldPattern::HandleMouseEvent(MouseInfo& info)
     if (info.GetAction() == MouseAction::RELEASE) {
         LOGI("Handle mouse left button release");
         if (blockPress_) {
-            caretUpdateType_ = CaretUpdateType::PRESSED;
-            UpdateCaretPositionByPressOffset();
             blockPress_ = false;
         }
         CloseSelectOverlay(true);
