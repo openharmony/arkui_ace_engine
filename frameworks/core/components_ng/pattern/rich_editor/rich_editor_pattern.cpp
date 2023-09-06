c075bcb35ae932c3b6a58e209c5d2483cea8da50
wangtao <wangtao487@huawei.com>
2023-09-06 12:41:16 +0800

    fixed ae20b15 from https://gitee.com/luckclover/arkui_ace_engine/pulls/19220 富文本空行输入法候选框不跟焦
    Signed-off-by: wangtao <wangtao487@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -1366,6 +1366,9 @@ std::optional<MiscServices::TextConfig> RichEditorPattern::GetMiscTextConfig()
     auto windowRect = pipeline->GetCurrentWindowRect();
     float caretHeight = 0.0f;
     OffsetF caretOffset = CalcCursorOffsetByPosition(GetCaretPosition(), caretHeight);
+    if (NearZero(caretHeight)) {
+        caretHeight = richEditorOverlayModifier_ ? richEditorOverlayModifier_->GetCareHeight() : DEFAULT_CARET_HEIGHT;
+    }
     MiscServices::CursorInfo cursorInfo { .left = caretOffset.GetX() + windowRect.Left() + parentGlobalOffset_.GetX(),
         .top = caretOffset.GetY() + windowRect.Top() + parentGlobalOffset_.GetY(),
         .width = CARET_WIDTH,
@@ -1419,14 +1422,13 @@ bool RichEditorPattern::UnableStandardInput(bool isFocusViewChanged)
 void RichEditorPattern::UpdateCaretInfoToController()
 {
     CHECK_NULL_VOID_NOLOG(HasFocus());
-    auto caretPosition = GetCaretPosition();
-    auto selectionResult = GetSpansInfo(caretPosition, caretPosition, GetSpansMethod::GETSPANS);
+    auto selectionResult = GetSpansInfo(0, GetTextContentLength(), GetSpansMethod::ONSELECT);
     auto resultObjects = selectionResult.GetSelection().resultObjects;
     std::string text = "";
     if (!resultObjects.empty()) {
         for (const auto& resultObj : resultObjects) {
             if (resultObj.type == RichEditorSpanType::TYPESPAN) {
-                text.append(resultObj.valueString);
+                text += resultObj.valueString;
             }
         }
     }
