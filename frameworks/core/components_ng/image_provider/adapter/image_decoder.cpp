f95ba217f58fd54325a9012be6601a1b79691417
lushi1202 <lushi@huawei.com>
2023-10-09 17:08:40 +0800

    add resize trace
    Signed-off-by: lushi1202 <lushi@huawei.com>
    Change-Id: I954a9fe1d8a0444098267a9183ac3faac519dcc3

--- frameworks/core/components_ng/image_provider/adapter/image_decoder.cpp
+++ frameworks/core/components_ng/image_provider/adapter/image_decoder.cpp
@@ -106,8 +106,6 @@ RefPtr<CanvasImage> ImageDecoder::MakeDrawingImage()
 RefPtr<CanvasImage> ImageDecoder::MakePixmapImage()
 {
     CHECK_NULL_RETURN(obj_ && data_, nullptr);
-    ACE_SCOPED_TRACE("MakePixmapImage %s", obj_->GetSourceInfo().ToString().c_str());
-
 #ifndef USE_ROSEN_DRAWING
     auto source = ImageSource::Create(data_->bytes(), data_->size());
 #else
@@ -117,6 +115,11 @@ RefPtr<CanvasImage> ImageDecoder::MakePixmapImage()
 
     auto width = std::lround(desiredSize_.Width());
     auto height = std::lround(desiredSize_.Height());
+    std::pair<int32_t, int32_t> sourceSize = source->GetImageSize();
+    ACE_SCOPED_TRACE("CreateImagePixelMap %s, sourceSize: [ %d, %d ], targetSize: [ %d, %d ]",
+        obj_->GetSourceInfo().ToString().c_str(), sourceSize.first, sourceSize.second,
+        static_cast<int32_t>(width),
+        static_cast<int32_t>(height));
     auto pixmap = source->CreatePixelMap({ width, height });
     CHECK_NULL_RETURN(pixmap, nullptr);
     auto image = PixelMapImage::Create(pixmap);
@@ -165,6 +168,10 @@ sk_sp<SkImage> ImageDecoder::ResizeSkImage()
 #endif
     CHECK_NULL_RETURN(codec, {});
     auto info = codec->getInfo();
+
+    ACE_SCOPED_TRACE(
+        "ImageResize %s, sourceSize: [ %d, %d ], targetSize: [ %d, %d ]", obj_->GetSourceInfo().ToString().c_str(),
+        info.width(), info.height(), static_cast<int32_t>(width), static_cast<int32_t>(height));
 
     // sourceSize is set by developer, then we will force scaling to [TargetSize] using SkImage::scalePixels,
     // this method would succeed even if the codec doesn't support that size.
