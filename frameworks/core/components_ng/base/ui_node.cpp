6970a53dc5eaeb29d8709c5d5a09e81305d347c9
zhouke <zhouke35@huawei.com>
2023-10-25 10:55:56 +0800

    modify.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhouke <zhouke35@huawei.com>

--- frameworks/core/components_ng/base/ui_node.cpp
+++ frameworks/core/components_ng/base/ui_node.cpp
@@ -446,9 +446,9 @@ void UINode::OnAttachToMainTree(bool)
 void UINode::DumpTree(int32_t depth)
 {
     if (DumpLog::GetInstance().GetDumpFile()) {
-        DumpLog::GetInstance().AddDesc("AccessibilityId: " + std::to_string(accessibilityId_));
         DumpLog::GetInstance().AddDesc("ID: " + std::to_string(nodeId_));
         DumpLog::GetInstance().AddDesc(std::string("Depth: ").append(std::to_string(GetDepth())));
+        DumpLog::GetInstance().AddDesc("AccessibilityId: " + std::to_string(accessibilityId_));
         DumpLog::GetInstance().AddDesc(std::string("IsDisappearing: ").append(std::to_string(IsDisappearing())));
         DumpInfo();
         DumpLog::GetInstance().Print(depth, tag_, static_cast<int32_t>(GetChildren().size()));
