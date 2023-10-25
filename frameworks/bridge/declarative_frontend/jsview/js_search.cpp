1e1d1e7fce6d0c8d6544fc6f638c4028b86846ad
zhoukechen <zhoukechen2@huawei.com>
2023-10-25 09:55:30 +0800

    搜索框清除按钮大小为0时默认不显示
    Signed-off-by: zhoukechen <zhoukechen2@huawei.com>
    Change-Id: I5e9bb11dfb337c0f30932984fd62ed6b9c23fd5f

--- frameworks/bridge/declarative_frontend/jsview/js_search.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_search.cpp
@@ -309,7 +309,7 @@ void JSSearch::SetIconStyle(const JSCallbackInfo& info)
     auto iconSizeProp = iconParam->GetProperty("size");
     auto theme = GetTheme<SearchTheme>();
     if (!iconSizeProp->IsUndefined() && !iconSizeProp->IsNull() && ParseJsDimensionVpNG(iconSizeProp, iconSize)) {
-        if (LessOrEqual(iconSize.Value(), 0.0) || iconSize.Unit() == DimensionUnit::PERCENT) {
+        if (LessNotEqual(iconSize.Value(), 0.0) || iconSize.Unit() == DimensionUnit::PERCENT) {
             iconSize = theme->GetIconHeight();
         }
     } else {
