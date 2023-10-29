d13dc3959d80806668d4b88e8c2fb89207c9b3ef
hw_wyx <wuyinxiao@huawei.com>
2023-10-29 09:16:15 +0000

    fixIMEInputCompletePro
    Signed-off-by: hw_wyx <wuyinxiao@huawei.com>
    Change-Id: I7814fc9326d12ae9dc746120a6849c8e8ff15e1b

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -1975,8 +1975,8 @@ void RichEditorPattern::AfterIMEInsertValue(const RefPtr<SpanNode>& spanNode, in
     retInfo.SetValue(spanNode->GetSpanItem()->content);
     auto contentLength = StringUtils::ToWstring(spanNode->GetSpanItem()->content).length();
     if (isCreate) {
-        auto spanEnd = GetCaretPosition() + 1;
-        auto spanStart = spanEnd - static_cast<int32_t>(contentLength);
+        auto spanStart = 0;
+        auto spanEnd = static_cast<int32_t>(contentLength);
         retInfo.SetSpanRangeStart(spanStart);
         retInfo.SetSpanRangeEnd(spanEnd);
         retInfo.SetOffsetInSpan(0);
