e57af88e2ce9c2d09f8e7066992693cf5450719a
jyj-0306 <jiangyingjie2@huawei.com>
2023-10-30 14:07:51 +0800

    增加光标位置上报事件
    Signed-off-by: jyj-0306 <jiangyingjie2@huawei.com>
    Change-Id: Ic04a486de844f220d137c4a4cca32f13c69703c5

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
@@ -939,6 +939,7 @@ public:
     void OnHandleMoveDone(const RectF& handleRect, bool isFirstHandle) override;
     void OnHandleClosed(bool closedByGlobalEvent) override;
     bool CheckHandleVisible(const RectF& paintRect) override;
+    void UpdateCaretInfoToController() const;
     bool OnPreShowSelectOverlay(
         SelectOverlayInfo& overlayInfo, const ClientOverlayInfo& clientInfo, bool isSelectOverlayOn) override;
     void OnObscuredChanged(bool isObscured);
@@ -1024,7 +1025,6 @@ private:
     void ShowSelectOverlay(const ShowSelectOverlayParams& params);
 
     void CursorMoveOnClick(const Offset& offset);
-    void UpdateCaretInfoToController() const;
 
     void ProcessOverlay(bool isUpdateMenu = true, bool animation = false, bool isShowMenu = true);
     void DelayProcessOverlay(bool isUpdateMenu = true, bool animation = false, bool isShowMenu = true);
