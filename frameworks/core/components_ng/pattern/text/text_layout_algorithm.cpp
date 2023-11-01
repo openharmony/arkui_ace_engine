4a1d4b28302652496246db6290875dd5e2cad790
fengjituo111 <fengjituo@huawei.com>
2023-11-01 13:35:59 +0000

    Revert "【Text】解决contentConstraint传入maxSize为0 未生效的问题"
    This reverts commit 9e793fe0286dc35cd9df6192688320b083418d79.
    
    Signed-off-by: fengjituo111 <fengjituo@huawei.com>

--- frameworks/core/components_ng/pattern/text/text_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/text/text_layout_algorithm.cpp
@@ -61,7 +61,7 @@ void TextLayoutAlgorithm::OnReset() {}
 std::optional<SizeF> TextLayoutAlgorithm::MeasureContent(
     const LayoutConstraintF& contentConstraint, LayoutWrapper* layoutWrapper)
 {
-    if (!contentConstraint.maxSize.IsNonNegative()) {
+    if (!contentConstraint.maxSize.IsPositive()) {
         return std::nullopt;
     }
 
