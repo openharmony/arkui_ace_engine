f30dbfc842ac9e9f23b419fc6f1c040c23c7c081
huangxiaolinabc123 <huangxiaolin12@huawei.com>
2023-11-02 22:12:56 +0800

    fix bug
    Signed-off-by: huangxiaolinabc123 <huangxiaolin12@huawei.com>
    Change-Id: I9bce29c6c8ced5052863db19d99395baa285fe81

--- frameworks/core/components_ng/pattern/navigation/title_bar_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/navigation/title_bar_layout_algorithm.cpp
@@ -516,6 +516,7 @@ void TitleBarLayoutAlgorithm::LayoutTitle(LayoutWrapper* layoutWrapper, const Re
         titleWrapper->Layout();
         return;
     }
+    initialTitleOffsetY_ = menuHeight_ + offsetY;
     auto overDragOffset = titlePattern->GetOverDragOffset();
     auto titleOffset = OffsetF(static_cast<float>(maxPaddingStart_.ConvertToPx()),
         titlePattern->GetTempTitleOffsetY() + overDragOffset / 6.0f);
