f2d31e64303461ca834548472914a2ad3c5d08c1
Tianer Zhou <zhoutianer@huawei.com>
2023-11-05 10:10:13 +0800

    fix multi-level nested scroll
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: If87afd03f69440de87c6af01abda2d59caf0339a

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
@@ -3235,13 +3235,26 @@ ScrollResult SwiperPattern::HandleScroll(float offset, int32_t source, NestedSta
 
 ScrollResult SwiperPattern::HandleScrollSelfFirst(float offset, int32_t source, NestedState state)
 {
+    // priority: self scroll > parent scroll > parent overScroll > self overScroll
     if (IsOutOfBoundary(offset)) {
+        // skip CHECK_NULL, already checked in HandleScroll
+        auto parent = parent_.Upgrade();
+
+        // reached edge, pass offset to parent
+        auto res = parent->HandleScroll(offset, source, NestedState::CHILD_SCROLL);
+        if (res.remain == 0.0f) {
+            return { 0.0f, true };
+        }
         // parent handle overScroll first
-        auto res = parent_.Upgrade()->HandleScroll(offset, source, NestedState::CHILD_OVER_SCROLL);
+        if (res.reachEdge) {
+            res = parent->HandleScroll(res.remain, source, NestedState::CHILD_OVER_SCROLL);
+        }
+
         if (ChildFirst(state)) {
             return { res.remain, true };
         }
         if (res.remain != 0.0f) {
+            // self overScroll
             UpdateCurrentOffset(res.remain);
         }
     } else {
@@ -3253,7 +3266,7 @@ ScrollResult SwiperPattern::HandleScrollSelfFirst(float offset, int32_t source, 
 
 inline bool SwiperPattern::ChildFirst(NestedState state)
 {
-    // priority: self scroll > child scroll > self overScroll > child overScroll
+    // SELF/CHILD priority: self scroll > child scroll > self overScroll > child overScroll
     return state == NestedState::CHILD_SCROLL // child hasn't reach edge
            || GetEdgeEffect() == EdgeEffect::NONE;
 }
