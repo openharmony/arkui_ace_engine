73348829a2ebadec2dda7e416c26dd151c219e05
chenjiafeng <chenjiafeng10@huawei.com>
2023-11-08 09:29:31 +0800

    Description: textfield setTextSelection 缺少异常值检查 IssueNo: https://gitee.com/openharmony/arkui_ace_engine/issues/I8ERLU Feature or Bugfix: Bugfix Binary Source:No Signed-off-by: chenjiafeng <chenjiafeng10@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_controller.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_controller.cpp
@@ -64,8 +64,26 @@ NG::OffsetF TextFieldController::GetCaretPosition()
     return OffsetF(ERROR, ERROR);
 }
 
+bool TextFieldController::IsTextSelectionIllegal(int32_t& selectionStart, int32_t& selectionEnd)
+{
+    if (selectionEnd < selectionStart) {
+        return false;
+    }
+    auto textFieldPattern = AceType::DynamicCast<TextFieldPattern>(pattern_.Upgrade());
+    CHECK_NULL_RETURN(textFieldPattern, false);
+    auto wideText = textFieldPattern->GetWideText();
+    auto startIndex = std::clamp(selectionStart, 0, static_cast<int32_t>(wideText.length()));
+    auto endIndex = std::clamp(selectionEnd, 0, static_cast<int32_t>(wideText.length()));
+    selectionStart = startIndex;
+    selectionEnd = endIndex;
+    return true;
+}
+
 void TextFieldController::SetTextSelection(int32_t selectionStart, int32_t selectionEnd)
 {
+    if (!IsTextSelectionIllegal(selectionStart, selectionEnd)) {
+        return;
+    }
     auto textFieldPattern = AceType::DynamicCast<TextFieldPattern>(pattern_.Upgrade());
     textFieldPattern->SetSelectionFlag(selectionStart, selectionEnd);
 }
