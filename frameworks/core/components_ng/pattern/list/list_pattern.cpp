dbef65d6f482966b83611e987a199f348784c955
Tianer Zhou <zhoutianer@huawei.com>
2023-11-14 10:37:56 +0800

    list adapt to safeArea
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: I284a28a9b6cc5894d99b587bbf272b0166160ad7

--- frameworks/core/components_ng/pattern/list/list_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_pattern.cpp
@@ -499,8 +499,6 @@ RefPtr<LayoutAlgorithm> ListPattern::CreateLayoutAlgorithm()
     listLayoutAlgorithm->SetCurrentDelta(currentDelta_);
     listLayoutAlgorithm->SetItemsPosition(itemPosition_);
     listLayoutAlgorithm->SetPrevContentMainSize(contentMainSize_);
-    listLayoutAlgorithm->SetContentStartOffset(contentStartOffset_);
-    listLayoutAlgorithm->SetContentEndOffset(contentEndOffset_);
     if (IsOutOfBoundary(false) && GetScrollSource() != SCROLL_FROM_AXIS) {
         listLayoutAlgorithm->SetOverScrollFeature();
     }
