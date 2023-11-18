4ccb0d2aa1262fa4ebef0755ae0c4a629fbbe876
Xing guang <xingguang1@huawei.com>
2023-11-18 18:58:11 +0800

    无障碍执行部分I事件部分抛给主线程
    Signed-off-by: Xing guang <xingguang1@huawei.com>

--- adapter/ohos/osal/js_accessibility_manager.cpp
+++ adapter/ohos/osal/js_accessibility_manager.cpp
@@ -2802,7 +2802,18 @@ bool JsAccessibilityManager::ExecuteExtensionActionNG(int32_t elementId,
     const std::map<std::string, std::string>& actionArguments, int32_t action, const RefPtr<PipelineBase>& context,
     int32_t uiExtensionOffset)
 {
-    return ExecuteActionNG(elementId, actionArguments, static_cast<ActionType>(action), context, uiExtensionOffset);
+    bool isExecuted = false;
+    auto pipeline = context_.Upgrade();
+    CHECK_NULL_RETURN(pipeline, isExecuted);
+    pipeline->GetTaskExecutor()->PostSyncTask(
+        [weak = WeakClaim(this), elementId, actionArguments, action, context, uiExtensionOffset, &isExecuted]() {
+            auto jsAccessibilityManager = weak.Upgrade();
+            CHECK_NULL_VOID(jsAccessibilityManager);
+            isExecuted = jsAccessibilityManager->ExecuteActionNG(
+                elementId, actionArguments, static_cast<ActionType>(action), context, uiExtensionOffset);
+        },
+        TaskExecutor::TaskType::UI);
+    return isExecuted;
 }
 
 bool JsAccessibilityManager::ExecuteActionNG(int32_t elementId,
@@ -2834,7 +2845,7 @@ bool JsAccessibilityManager::ExecuteActionNG(int32_t elementId,
     if (!enabled) {
         return result;
     }
-    result = ConvertActionTypeToBoolen(action, frameNode, elementId, ngPipeline, result);
+    result = ConvertActionTypeToBoolen(action, frameNode, elementId, ngPipeline);
     if (!result) {
         auto accessibilityProperty = frameNode->GetAccessibilityProperty<NG::AccessibilityProperty>();
         CHECK_NULL_RETURN(accessibilityProperty, false);
@@ -2844,8 +2855,9 @@ bool JsAccessibilityManager::ExecuteActionNG(int32_t elementId,
 }
 
 bool JsAccessibilityManager::ConvertActionTypeToBoolen(ActionType action, RefPtr<NG::FrameNode>& frameNode,
-    int32_t elementId, RefPtr<NG::PipelineContext>& context, bool result)
+    int32_t elementId, RefPtr<NG::PipelineContext>& context)
 {
+    bool result = false;
     switch (action) {
         case ActionType::ACCESSIBILITY_ACTION_FOCUS: {
             result = RequestFocus(frameNode);
