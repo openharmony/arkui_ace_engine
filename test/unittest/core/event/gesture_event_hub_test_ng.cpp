a049fbddf86ec0cf252cde8319fa108e59f8f065
xushaoyue <xushaoyue1@huawei.com>
2023-11-20 11:08:06 +0000

    自定义手势判定tdd用例补充
    Signed-off-by: xushaoyue <xushaoyue1@huawei.com>

--- test/unittest/core/event/gesture_event_hub_test_ng.cpp
+++ test/unittest/core/event/gesture_event_hub_test_ng.cpp
@@ -1051,4 +1051,61 @@ HWTEST_F(GestureEventHubTestNg, GestureEventHubTest016, TestSize.Level1)
     guestureEventHub->ClearUserOnTouch();
     ASSERT_FALSE(guestureEventHub->clickEventActuator_->userCallback_);
 }
+
+/**
+ * @tc.name: GestureEventHubTest017
+ * @tc.desc: Test ProcessTouchTestHit
+ * @tc.type: FUNC
+ */
+HWTEST_F(GestureEventHubTestNg, GestureEventHubTest017, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. Create GestureEventHub.
+     * @tc.expected: gestureEventHub is not null.
+     */
+    auto frameNode = FrameNode::CreateFrameNode("myButton", 100, AceType::MakeRefPtr<Pattern>());
+    auto guestureEventHub = frameNode->GetOrCreateGestureEventHub();
+    ASSERT_NE(guestureEventHub, nullptr);
+    OffsetF coordinateOffset;
+    TouchRestrict touchRestrict;
+    TouchTestResult innerTargets;
+    TouchTestResult finalResult;
+    PointF localPoint;
+
+    PanDirection panDirection;
+    guestureEventHub->panEventActuator_ = AceType::MakeRefPtr<PanEventActuator>(
+        AceType::WeakClaim(AceType::RawPtr(guestureEventHub)), panDirection, 1, 50.0f);
+    /**
+     * @tc.steps: step2. call ProcessTouchTestHit , recognizer is not instance of recognizer group
+     * @tc.expected: result is false
+     */
+    guestureEventHub->longPressEventActuator_ =
+        AceType::MakeRefPtr<LongPressEventActuator>(AceType::WeakClaim(AceType::RawPtr(guestureEventHub)));
+    guestureEventHub->dragEventActuator_ = nullptr;
+
+    auto result = guestureEventHub->ProcessTouchTestHit(
+        coordinateOffset, touchRestrict, innerTargets, finalResult, 2, localPoint, nullptr);
+    EXPECT_FALSE(result);
+    /**
+     * @tc.steps: step3. call ProcessTouchTestHit , recognizer is instance of recognizer group.
+     * @tc.expected: result is false
+     */
+    GestureEventFunc callback = [](GestureEvent& info) {};
+    auto longPressEvent = AceType::MakeRefPtr<LongPressEvent>(std::move(callback));
+    guestureEventHub->longPressEventActuator_->SetLongPressEvent(longPressEvent);
+
+    guestureEventHub->dragEventActuator_ = AceType::MakeRefPtr<DragEventActuator>(
+        AceType::WeakClaim(AceType::RawPtr(guestureEventHub)), panDirection, 1, 50.0f);
+
+    auto dragActionStart = [](GestureEvent& info) {};
+    auto dragActionUpdate = [](GestureEvent& info) {};
+    auto dragActionEnd = [](GestureEvent& info) {};
+    auto dragActionCancel = []() {};
+    auto dragEvent = AceType::MakeRefPtr<DragEvent>(
+        std::move(dragActionStart), std::move(dragActionUpdate), std::move(dragActionEnd), std::move(dragActionCancel));
+    guestureEventHub->dragEventActuator_->userCallback_ = dragEvent;
+    result = guestureEventHub->ProcessTouchTestHit(
+        coordinateOffset, touchRestrict, innerTargets, finalResult, 2, localPoint, nullptr);
+    EXPECT_FALSE(result);
+}
 } // namespace OHOS::Ace::NG
