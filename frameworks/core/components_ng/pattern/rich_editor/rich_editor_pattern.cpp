5fd1683cc5e53e27fb0c94d1de50b347764360b9
conewang <wangdantong@huawei.com>
2023-11-20 10:20:38 +0800

    fix_ctrl组合键
    Signed-off-by: conewang <wangdantong@huawei.com>
    Change-Id: I91924ed1f9dec1b999ea8f0999668b6612e6dcaa

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -761,8 +761,8 @@ bool RichEditorPattern::SetCaretOffset(int32_t caretPosition)
     if (focusHub->IsCurrentFocus()) {
         StartTwinkling();
     }
-    textSelector_.Update(-1);
     CloseSelectOverlay();
+    ResetSelection();
     return success;
 }
 
@@ -975,8 +975,8 @@ void RichEditorPattern::UpdateSpanStyle(
             break;
         }
     }
-    textSelector_.Update(-1);
     CloseSelectOverlay();
+    ResetSelection();
 }
 
 std::vector<ParagraphInfo> RichEditorPattern::GetParagraphInfo(int32_t start, int32_t end)
@@ -2274,8 +2274,8 @@ void RichEditorPattern::SetInputMethodStatus(bool keyboardShown)
 
 bool RichEditorPattern::CursorMoveLeft()
 {
-    textSelector_.Update(-1);
     CloseSelectOverlay();
+    ResetSelection();
     auto caretPosition = std::clamp((caretPosition_ - 1), 0, static_cast<int32_t>(GetTextContentLength()));
     if (caretPosition_ == caretPosition) {
         return false;
@@ -2289,8 +2289,8 @@ bool RichEditorPattern::CursorMoveLeft()
 
 bool RichEditorPattern::CursorMoveRight()
 {
-    textSelector_.Update(-1);
     CloseSelectOverlay();
+    ResetSelection();
     auto caretPosition = std::clamp((caretPosition_ + 1), 0, static_cast<int32_t>(GetTextContentLength()));
     if (caretPosition_ == caretPosition) {
         return false;
@@ -2304,8 +2304,8 @@ bool RichEditorPattern::CursorMoveRight()
 
 bool RichEditorPattern::CursorMoveUp()
 {
-    textSelector_.Update(-1);
     CloseSelectOverlay();
+    ResetSelection();
     if (static_cast<int32_t>(GetTextContentLength()) > 1) {
         float caretHeight = 0.0f;
         OffsetF caretOffset = CalcCursorOffsetByPosition(GetCaretPosition(), caretHeight);
@@ -2326,8 +2326,8 @@ bool RichEditorPattern::CursorMoveUp()
 
 bool RichEditorPattern::CursorMoveDown()
 {
-    textSelector_.Update(-1);
     CloseSelectOverlay();
+    ResetSelection();
     if (static_cast<int32_t>(GetTextContentLength()) > 1) {
         float caretHeight = 0.0f;
         OffsetF caretOffset = CalcCursorOffsetByPosition(GetCaretPosition(), caretHeight);
