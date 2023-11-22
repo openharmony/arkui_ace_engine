474acce75f645ae3c72733e913aafd5f43c39e8b
conewang <wangdantong@huawei.com>
2023-11-22 22:12:04 +0800

    code_fix
    Signed-off-by: conewang <wangdantong@huawei.com>
    Change-Id: Id7657c0b9f5d16d33885ba4225fb84e12745c792

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -2225,6 +2225,7 @@ void RichEditorPattern::DeleteBackward(int32_t length)
     }
     if (length == spans_.back()->position) {
         ResetFirstNodeStyle();
+        textForDisplay_.clear();
     }
     info.SetOffset(caretPosition_ - 1);
     info.SetLength(length);
