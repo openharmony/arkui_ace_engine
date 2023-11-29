e1e9179bd2b438fb6ee6ec848fcdb46445a8ef08
mayaolll <mayao17@huawei.com>
2023-11-29 09:50:54 +0000

    Prohibit page response events during animation
    Signed-off-by: mayaolll <mayao17@huawei.com>
    Change-Id: Ia855f9311c16f86b105e61d51109359730aed9e0

--- frameworks/core/components_ng/pattern/navigation/navigation_group_node.cpp
+++ frameworks/core/components_ng/pattern/navigation/navigation_group_node.cpp
@@ -129,7 +129,6 @@ void NavigationGroupNode::UpdateNavDestinationNodeWithoutMarkDirty(const RefPtr<
             // process shallow builder
             navDestination->ProcessShallowBuilder();
             navDestination->GetLayoutProperty()->UpdateVisibility(VisibleType::VISIBLE);
-            navDestination->GetEventHub<EventHub>()->SetEnabledInternal(true);
             // for the navDestination at the top, FireChangeEvent
             eventHub->FireChangeEvent(true);
             hasChanged = CheckNeedMeasure(navDestination->GetLayoutProperty()->GetPropertyChangeFlag());
@@ -550,7 +549,8 @@ void NavigationGroupNode::EnterTransitionWithPush(const RefPtr<FrameNode>& node,
     }
     CHECK_NULL_VOID(titleNode);
 
-    option.SetOnFinishEvent([weakNavigation = WeakClaim(this), id = Container::CurrentId()] {
+    option.SetOnFinishEvent([weakNode = WeakPtr<FrameNode>(node), weakNavigation = WeakClaim(this),
+                                id = Container::CurrentId()] {
         ContainerScope scope(id);
         auto context = PipelineContext::GetCurrentContext();
         CHECK_NULL_VOID(context);
@@ -558,18 +558,22 @@ void NavigationGroupNode::EnterTransitionWithPush(const RefPtr<FrameNode>& node,
         CHECK_NULL_VOID(taskExecutor);
         // animation finish event should be posted to UI thread.
         taskExecutor->PostTask(
-            [weakNavigation]() {
+            [weakNode, weakNavigation]() {
                 PerfMonitor::GetPerfMonitor()->End(PerfConstants::ABILITY_OR_PAGE_SWITCH, true);
                 TAG_LOGD(AceLogTag::ACE_NAVIGATION, "navigation animation end");
                 auto navigation = weakNavigation.Upgrade();
                 CHECK_NULL_VOID(navigation);
                 navigation->isOnAnimation_ = false;
                 navigation->OnAccessibilityEvent(AccessibilityEventType::PAGE_CHANGE);
+                auto node = weakNode.Upgrade();
+                CHECK_NULL_VOID(node);
+                node->GetEventHub<EventHub>()->SetEnabledInternal(true);
             },
             TaskExecutor::TaskType::UI);
     });
 
     // content
+    node->GetEventHub<EventHub>()->SetEnabledInternal(false);
     node->GetRenderContext()->ClipWithRRect(
         RectF(nodeWidth * HALF, 0.0f, nodeWidth, nodeHeight), RadiusF(EdgeF(0.0f, 0.0f)));
     node->GetRenderContext()->UpdateTranslateInXY({ nodeWidth * HALF, 0.0f });
@@ -640,13 +644,16 @@ void NavigationGroupNode::EnterTransitionWithPop(const RefPtr<FrameNode>& node, 
         CHECK_NULL_VOID(taskExecutor);
         // animation finish event should be posted to UI thread.
         taskExecutor->PostTask(
-            [weakNavigation]() {
+            [weakNode, weakNavigation]() {
                 PerfMonitor::GetPerfMonitor()->End(PerfConstants::ABILITY_OR_PAGE_SWITCH, true);
                 TAG_LOGD(AceLogTag::ACE_NAVIGATION, "navigation animation end");
                 auto navigation = weakNavigation.Upgrade();
                 CHECK_NULL_VOID(navigation);
                 navigation->isOnAnimation_ = false;
                 navigation->OnAccessibilityEvent(AccessibilityEventType::PAGE_CHANGE);
+                auto node = weakNode.Upgrade();
+                CHECK_NULL_VOID(node);
+                node->GetEventHub<EventHub>()->SetEnabledInternal(true);
             },
             TaskExecutor::TaskType::UI);
     });
@@ -655,6 +662,7 @@ void NavigationGroupNode::EnterTransitionWithPop(const RefPtr<FrameNode>& node, 
             RectF(nodeWidth * PARENT_PAGE_OFFSET, 0.0f, nodeWidth, nodeHeight), RadiusF(EdgeF(0.0f, 0.0f)));
     }
     // content
+    node->GetEventHub<EventHub>()->SetEnabledInternal(false);
     node->GetRenderContext()->UpdateTranslateInXY({ -nodeWidth * PARENT_PAGE_OFFSET, 0.0f });
     // title
     titleNode->GetRenderContext()->UpdateTranslateInXY({ nodeWidth * PARENT_TITLE_OFFSET, 0.0f });
