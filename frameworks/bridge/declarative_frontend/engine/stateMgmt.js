40932f939885520a946647d6b2411bc363457389
liwenzhen <liwenzhen3@huawei.com>
2023-11-30 21:37:45 +0800

    add log for obtainDeletedElmtIds is not callable in stability test
    Signed-off-by: liwenzhen <liwenzhen3@huawei.com>
    Change-Id: Ib2df12e76d05c2ee28d61218a2882550aed047a4

--- frameworks/bridge/declarative_frontend/engine/stateMgmt.js
+++ frameworks/bridge/declarative_frontend/engine/stateMgmt.js
@@ -4310,7 +4310,11 @@ class UINodeRegisterProxy {
     }
     static obtainDeletedElmtIds() {
         
-        UINodeRegisterProxy.instance_.obtainDeletedElmtIds();
+        if ((!UINodeRegisterProxy.instance_.obtainDeletedElmtIds) || typeof UINodeRegisterProxy.instance_.obtainDeletedElmtIds != "function") {
+            stateMgmtConsole.error(`UINodeRegisterProxy obtainDeletedElmtIds is not a function: ${UINodeRegisterProxy.instance_.obtainDeletedElmtIds}.` );
+        } else {
+            UINodeRegisterProxy.instance_.obtainDeletedElmtIds();UINodeRegisterProxy.instance_.obtainDeletedElmtIds();
+        }
     }
     static unregisterElmtIdsFromViewPUs() {
         
@@ -4340,11 +4344,11 @@ class UINodeRegisterProxy {
                     owningView.purgeDeleteElmtId(rmElmtInfo.elmtId);
                 }
                 else {
-                    stateMgmtConsole.warn(`elmtIds ${rmElmtInfo.elmtId} tag: ${rmElmtInfo.tag} has not been removed because of failure of updating the weakptr of viewpu. Internal error!.`);
+                    
                 }
             }
             else {
-                stateMgmtConsole.warn(`elmtIds ${rmElmtInfo.elmtId} tag: ${rmElmtInfo.tag} cannot find its owning viewpu, maybe this viewpu has already been abouttobedeleted. Internal error!`);
+                
             }
         });
         this.removeElementsInfo_.length = 0;
@@ -4470,7 +4474,6 @@ class ViewPU extends NativeViewPartialUpdate {
         if (extraInfo) {
             this.extraInfo_ = extraInfo;
         }
-
         if (parent) {
             // this View is not a top-level View
             this.setCardId(parent.getCardId());
