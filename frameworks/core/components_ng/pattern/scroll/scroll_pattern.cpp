4d6ff5faf2229f037a34ae76f84ce969e9431b36
zhangrongjie <zhangrongjie4@huawei.com>
2023-12-04 19:09:09 +0000

    fix the bug that OnScrollStop is not triggered
    Signed-off-by: zhangrongjie <zhangrongjie4@huawei.com>
    Change-Id: Ia071395fc8004d14b17a657c963eafdeb8fa02b9

--- frameworks/core/components_ng/pattern/scroll/scroll_pattern.cpp
+++ frameworks/core/components_ng/pattern/scroll/scroll_pattern.cpp
@@ -513,6 +513,7 @@ bool ScrollPattern::ScrollPage(bool reverse, bool smooth, const std::function<vo
 void ScrollPattern::JumpToPosition(float position, int32_t source)
 {
     // If an animation is playing, stop it.
+    auto lastAnimateRunning = AnimateRunning();
     StopAnimate();
     float cachePosition = currentOffset_;
     DoJump(position, source);
@@ -520,6 +521,9 @@ void ScrollPattern::JumpToPosition(float position, int32_t source)
         auto host = GetHost();
         CHECK_NULL_VOID(host);
         host->OnAccessibilityEvent(AccessibilityEventType::SCROLL_END);
+    }
+    if (lastAnimateRunning) {
+        SetScrollAbort(false);
     }
 }
 
