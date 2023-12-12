50190d6b5d9303e7b189f6c4624bd83c9f232ff0
Xing guang <xingguang1@huawei.com>
2023-12-12 10:39:17 +0800

    Fix Polyline ellipse circle no shepe arrt issue
    Signed-off-by: Xing guang <xingguang1@huawei.com>

--- frameworks/bridge/declarative_frontend/ark_component/src/ArkPolygon.ts
+++ frameworks/bridge/declarative_frontend/ark_component/src/ArkPolygon.ts
@@ -1,42 +1,6 @@
 /// <reference path='./import.ts' />
-class ArkPolygonComponent extends ArkComponent implements PolygonAttribute {
-  constructor(nativePtr: KNode) {
-    super(nativePtr);
-  }
+class ArkPolygonComponent extends ArkCommonShapeComponent implements PolygonAttribute {
   points(value: any[]): this {
-    throw new Error('Method not implemented.');
-  }
-  stroke(value: any): this {
-    throw new Error('Method not implemented.');
-  }
-  fill(value: any): this {
-    throw new Error('Method not implemented.');
-  }
-  strokeDashOffset(value: string | number): this {
-    throw new Error('Method not implemented.');
-  }
-  strokeLineCap(value: LineCapStyle): this {
-    throw new Error('Method not implemented.');
-  }
-  strokeLineJoin(value: LineJoinStyle): this {
-    throw new Error('Method not implemented.');
-  }
-  strokeMiterLimit(value: string | number): this {
-    throw new Error('Method not implemented.');
-  }
-  strokeOpacity(value: any): this {
-    throw new Error('Method not implemented.');
-  }
-  fillOpacity(value: any): this {
-    throw new Error('Method not implemented.');
-  }
-  strokeWidth(value: any): this {
-    throw new Error('Method not implemented.');
-  }
-  antiAlias(value: boolean): this {
-    throw new Error('Method not implemented.');
-  }
-  strokeDashArray(value: any[]): this {
     throw new Error('Method not implemented.');
   }
   monopolizeEvents(monopolize: boolean): this {
