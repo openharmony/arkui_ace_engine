7540c0d085c949850754adeb0a985fdd3d4c06cc
liugan <liugan8@huawei.com>
2023-12-14 17:23:54 +0800

    修复Navigation的navBarWidth自动变化的问题
    Signed-off-by: liugan <liugan8@huawei.com>
    Change-Id: I410f7e130e2a79497b010c7e924df5e750517482
    Signed-off-by: liugan <liugan8@huawei.com>

--- frameworks/core/components_ng/pattern/navigation/navigation_pattern.h
+++ frameworks/core/components_ng/pattern/navigation/navigation_pattern.h
@@ -244,6 +244,12 @@ public:
     void SetInitNavBarWidth(const Dimension& initNavBarWidth)
     {
         realNavBarWidth_ = static_cast<float>(initNavBarWidth.ConvertToPx());
+        initNavBarWidthValue_ = initNavBarWidth;
+    }
+
+    Dimension GetInitNavBarWidth() const
+    {
+        return initNavBarWidthValue_;
     }
     
     void SetIfNeedInit(bool ifNeedInit)
@@ -322,6 +328,7 @@ private:
     bool userSetMinContentFlag_ = false;
     bool userSetNavBarWidthFlag_ = false;
     bool isChanged_ = false; // check navigation top page is change
+    Dimension initNavBarWidthValue_ = DEFAULT_NAV_BAR_WIDTH;
     Dimension minNavBarWidthValue_ = 0.0_vp;
     Dimension maxNavBarWidthValue_ = 0.0_vp;
     Dimension minContentWidthValue_ = 0.0_vp;
