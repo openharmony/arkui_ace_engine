8c67b051228857894649d42d5a4d899fa9a456a3
yuzhicheng <yuzhicheng4@huawei.com>
2023-12-19 08:51:48 +0000

    navStack clear fail bug fix
    Signed-off-by: yuzhicheng <yuzhicheng4@huawei.com>
    Change-Id: Ica4511fd0fb6dec357a1dd05c686ad49ec6b9599

--- frameworks/core/components_ng/pattern/navigation/navigation_pattern.cpp
+++ frameworks/core/components_ng/pattern/navigation/navigation_pattern.cpp
@@ -508,6 +508,8 @@ void NavigationPattern::TransitionWithAnimation(const RefPtr<NavDestinationGroup
     CHECK_NULL_VOID(navigationNode);
     auto navBarNode = AceType::DynamicCast<NavBarNode>(navigationNode->GetNavBarNode());
     CHECK_NULL_VOID(navBarNode);
+    auto pipeline = PipelineContext::GetCurrentContext();
+    CHECK_NULL_VOID(pipeline);
 
     // replace
     auto replaceValue = navigationStack_->GetReplaceValue();
@@ -536,6 +538,16 @@ void NavigationPattern::TransitionWithAnimation(const RefPtr<NavDestinationGroup
     auto layoutProperty = navigationNode->GetLayoutProperty<NavigationLayoutProperty>();
     CHECK_NULL_VOID(layoutProperty);
     if (layoutProperty->GetHideNavBarValue(false)) {
+        if (preTopNavDestination) {
+            auto parent = preTopNavDestination->GetParent();
+            CHECK_NULL_VOID(parent);
+            if (preTopNavDestination->GetContentNode()) {
+                preTopNavDestination->GetContentNode()->Clean();
+            }
+            parent->RemoveChild(preTopNavDestination);
+            parent->RebuildRenderContextTree();
+            pipeline->RequestFrame();
+        }
         return;
     }
 
