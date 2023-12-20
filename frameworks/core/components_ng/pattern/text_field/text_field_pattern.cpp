ee30ca9915ca60e14e55c3572af59fa96e737b71
lijuan <lijuan124@huawei.com>
2023-12-20 06:23:03 +0000

    修复showerror后有删除两次出现组件大小变动
    Signed-off-by: lijuan <lijuan124@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -1918,6 +1918,9 @@ void TextFieldPattern::OnModifyDone()
         HasFocus() && IsNormalInlineState()) {
         lastTextRectY_ = textRect_.GetY();
     }
+    if (!IsDisabled()) {
+        SetShowError();
+    }
     ProcessInnerPadding();
     // The textRect position can't be changed by only redraw.
     if (CheckNeedMeasure(layoutProperty->GetPropertyChangeFlag()) && !HasInputOperation()) {
@@ -1966,9 +1969,6 @@ void TextFieldPattern::OnModifyDone()
             AddScrollEvent();
             SetScrollEnable(false);
         }
-    }
-    if (!IsDisabled()) {
-        SetShowError();
     }
     if (IsTextArea()) {
         if (setBorderFlag_ && layoutProperty->HasMaxLength()) {
