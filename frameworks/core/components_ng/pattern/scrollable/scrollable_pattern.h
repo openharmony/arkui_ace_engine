34d927b6e1dbc2fb626f11d43f15973909d7eb40
443802242@qq.com <zhanghaodong12@huawei.com>
2023-12-21 11:05:17 +0800

    fix:fix animation interrupt problem
    Signed-off-by: 443802242@qq.com <zhanghaodong12@huawei.com>

--- frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.h
+++ frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.h
@@ -629,6 +629,8 @@ private:
     float currentVelocity_ = 0.0f;
     float lastPosition_ = 0.0f;
     float finalPosition_ = 0.0f;
+    uint32_t runningAnimationCount_ = 0;
+
     RefPtr<Animator> hotzoneAnimator_;
     float lastHonezoneOffsetPct_ = 0.0f;
     RefPtr<BezierVariableVelocityMotion> velocityMotion_;
