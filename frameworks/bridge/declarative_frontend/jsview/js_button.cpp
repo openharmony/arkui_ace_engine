e6e6f6fae5691269e380ee42269f419968249dde
18168667290 <huangpeng107@huawei.com>
2023-12-21 19:47:49 +0800

    修复button组件中labelStyle里的fontweight为数值型不生效
    Signed-off-by: 18168667290 <huangpeng107@huawei.com>
    Change-Id: I052856ecda0f51c1af37eb28e04cf7e844f45693

--- frameworks/bridge/declarative_frontend/jsview/js_button.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_button.cpp
@@ -171,7 +171,7 @@ void JSButton::GetFontContent(JSRef<JSVal>& font, ButtonParameters& buttonParame
     }
 
     JSRef<JSVal> weight = obj->GetProperty("weight");
-    if (weight->IsString()) {
+    if (weight->IsString() || weight->IsNumber()) {
         buttonParameters.fontWeight = ConvertStrToFontWeight(weight->ToString());
     }
 
