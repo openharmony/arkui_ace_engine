fb98136e9d24f12aab1b568ab6bda266c9f18f25
x00503640 <xufang21@huawei.com>
2023-12-25 09:07:37 +0800

    exit aminator ui and rs hisysevent order
    Signed-off-by: x00503640 <xufang21@huawei.com>

--- frameworks/base/perfmonitor/perf_monitor.cpp
+++ frameworks/base/perfmonitor/perf_monitor.cpp
@@ -144,12 +144,14 @@ void ReportPerfEventToUI(DataBase data)
 {
     switch (data.eventType) {
         case EVENT_COMPLETE:
-            if (!data.isDisplayAnimator) {
+            if (!data.needReportRs) {
                 EventReport::ReportEventComplete(data);
             }
             break;
         case EVENT_JANK_FRAME:
-            EventReport::ReportEventJankFrame(data);
+            if (data.totalFrames > 0) {
+                EventReport::ReportEventJankFrame(data);
+            }
             break;
         default :
             break;
@@ -165,6 +167,7 @@ void SceneRecord::InitRecord(const std::string& sId, PerfActionType aType, PerfS
     note = nt;
     inputTime = time;
     beginVsyncTime = GetCurrentRealTimeNs();
+    isDisplayAnimator = IsDisplayAnimator(sceneId);
 }
 
 bool SceneRecord::IsTimeOut(int64_t nowTime)
@@ -212,12 +215,20 @@ void SceneRecord::Report(const std::string& sceneId, int64_t vsyncTime, bool isR
     } else {
         endVsyncTime = vsyncTime;
     }
-    isDisplayAnimator = !isRsRender;
+    needReportRs = !isRsRender;
 }
 
 bool SceneRecord::IsFirstFrame()
 {
     return isFirstFrame;
+}
+
+bool SceneRecord::IsDisplayAnimator(const std::string& sceneId)
+{
+    if (sceneId == PerfConstants::APP_LIST_FLING) {
+        return true;
+    }
+    return false;
 }
 
 void SceneRecord::Reset()
@@ -247,19 +258,16 @@ PerfMonitor* PerfMonitor::GetPerfMonitor()
 
 void PerfMonitor::Start(const std::string& sceneId, PerfActionType type, const std::string& note)
 {
-    AceAsyncTraceBegin(0, sceneId.c_str());
     std::lock_guard<std::mutex> Lock(mMutex);
     int64_t inputTime = GetInputTime(sceneId, type, note);
     SceneRecord* record = GetRecord(sceneId);
-    if (record != nullptr) {
-        record->Reset();
-        record->InitRecord(sceneId, type, mSourceType, note, inputTime);
-    } else {
+    if (record == nullptr) {
         record = new SceneRecord();
         record->InitRecord(sceneId, type, mSourceType, note, inputTime);
         mRecords.insert(std::pair<std::string, SceneRecord*> (sceneId, record));
+        RecordBaseInfo(record);
+        AceAsyncTraceBegin(0, sceneId.c_str());
     }
-    RecordBaseInfo(record);
 }
 
 void PerfMonitor::End(const std::string& sceneId, bool isRsRender)
@@ -442,6 +450,7 @@ void PerfMonitor::FlushDataBase(SceneRecord* record, DataBase& data)
     data.maxSuccessiveFrames = record->maxSuccessiveFrames;
     data.totalMissed = record->totalMissed;
     data.totalFrames = record->totalFrames;
+    data.needReportRs = record->needReportRs;
     data.isDisplayAnimator = record->isDisplayAnimator;
     data.sourceType = record->sourceType;
     data.actionType = record->actionType;
@@ -463,8 +472,8 @@ void PerfMonitor::ReportPerfEvent(PerfEventType type, DataBase& data)
         default :
             break;
     }
-    ReportPerfEventToRS(data);
     ReportPerfEventToUI(data);
+    ReportPerfEventToRS(data);
 }
 
 bool PerfMonitor::IsExceptResponseTime(int64_t time, const std::string& sceneId)
