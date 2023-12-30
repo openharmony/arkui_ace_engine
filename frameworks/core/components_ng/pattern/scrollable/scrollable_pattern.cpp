8fc92db7c238e3891babfeb1257732b5b9a2a002
shihongjie <shihongjie@huawei.com>
2023-12-30 18:21:56 +0800

    修改scrollable组速度增益和摩擦系数的默认参数
    Signed-off-by: shihongjie <shihongjie@huawei.com>
    Change-Id: I86e7e8e4fdd7d14830319f1ca76f60271e0e1878

--- frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
+++ frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
@@ -44,6 +44,12 @@ const std::string SCROLLABLE_MULTI_TASK_SCENE = "scrollable_multi_task_scene";
 using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
 
+ScrollablePattern::ScrollablePattern(EdgeEffect edgeEffect, bool alwaysEnabled)
+    : edgeEffect_(edgeEffect), edgeEffectAlwaysEnabled_(alwaysEnabled)
+{
+    friction_ = Container::GreatOrEqualAPIVersion(PlatformVersion::VERSION_ELEVEN) ? NEW_FRICTION : FRICTION;
+}
+
 RefPtr<PaintProperty> ScrollablePattern::CreatePaintProperty()
 {
     auto defaultDisplayMode = GetDefaultScrollBarDisplayMode();
@@ -765,7 +771,7 @@ void ScrollablePattern::SetNestedScroll(const NestedScrollOptions& nestedOpt)
 void ScrollablePattern::SetFriction(double friction)
 {
     if (LessOrEqual(friction, 0.0)) {
-        friction = FRICTION;
+        friction = Container::GreatOrEqualAPIVersion(PlatformVersion::VERSION_ELEVEN) ? NEW_FRICTION : FRICTION;
     }
     friction_ = friction;
     CHECK_NULL_VOID(scrollableEvent_);
