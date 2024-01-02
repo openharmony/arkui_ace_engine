307c1918113dabddd50913fbf3dd6f813768541d
shihongjie <shihongjie@huawei.com>
2024-01-02 17:26:44 +0800

    fixed 4f7ea3b from https://gitee.com/shi-hongjie/arkui_ace_engine/pulls/24869 Revert "修改scrollable组速度增益和摩擦系数的默认参数"
    This reverts commit 8fc92db7c238e3891babfeb1257732b5b9a2a002.
    
    Signed-off-by: shihongjie <shihongjie@huawei.com>
    Change-Id: I9dfd822f0db419f337bd3860a034465a0c30e47a

--- frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.h
+++ frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.h
@@ -43,7 +43,6 @@
 namespace OHOS::Ace::NG {
 #ifndef WEARABLE_PRODUCT
 constexpr double FRICTION = 0.6;
-constexpr double NEW_FRICTION = 0.7;
 constexpr double MAX_VELOCITY = 800000.0;
 #else
 constexpr double FRICTION = 0.9;
@@ -59,7 +58,9 @@ class ScrollablePattern : public NestableScrollContainer {
 
 public:
     ScrollablePattern() = default;
-    ScrollablePattern(EdgeEffect edgeEffect, bool alwaysEnabled);
+    ScrollablePattern(EdgeEffect edgeEffect, bool alwaysEnabled)
+        : edgeEffect_(edgeEffect), edgeEffectAlwaysEnabled_(alwaysEnabled)
+    {}
 
     ~ScrollablePattern()
     {
@@ -617,7 +618,7 @@ private:
     bool isSheetInReactive_ = false;
     bool isCoordEventNeedSpring_ = true;
     double scrollBarOutBoundaryExtent_ = 0.0;
-    double friction_ = 0.0;
+    double friction_ = FRICTION;
     double maxFlingVelocity_ = MAX_VELOCITY;
     // scroller
     RefPtr<Animator> animator_;
