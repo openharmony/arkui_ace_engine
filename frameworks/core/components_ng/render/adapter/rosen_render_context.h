88b9a2c1aa588724e12e7a352aac7e88b572731b
wanglili12 <wanglili65@huawei.com>
2024-01-02 10:20:47 +0800

    修改转场退场动画问题
    Signed-off-by: wanglili12 <wanglili65@huawei.com>
    Change-Id: Ifdfd03ccc259a8346eca802471f234ec32c3ec38

--- frameworks/core/components_ng/render/adapter/rosen_render_context.h
+++ frameworks/core/components_ng/render/adapter/rosen_render_context.h
@@ -402,7 +402,7 @@ private:
     }
     bool HasDisappearTransition() const override
     {
-        return transitionEffect_ != nullptr && !transitionEffect_->HasDisappearTransition();
+        return transitionEffect_ != nullptr && transitionEffect_->HasDisappearTransition();
     }
     void OnTransitionInFinish();
     void OnTransitionOutFinish();
