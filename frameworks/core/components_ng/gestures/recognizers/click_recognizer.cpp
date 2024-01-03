80ed4ba67d33b944ab173708cd02567175765751
zhouchaobo <zhouchaobo@huawei.com>
2024-01-03 17:29:14 +0800

    click compute focus points bug fix
    Signed-off-by: zhouchaobo <zhouchaobo@huawei.com>
    Change-Id: I57cefc7d4f4b682ac699f058f75e2e0998fff522

--- frameworks/core/components_ng/gestures/recognizers/click_recognizer.cpp
+++ frameworks/core/components_ng/gestures/recognizers/click_recognizer.cpp
@@ -318,10 +318,15 @@ void ClickRecognizer::DeadlineTimer(CancelableCallback<void()>& deadlineTimer, i
 Offset ClickRecognizer::ComputeFocusPoint()
 {
     Offset sumOfPoints;
+    int32_t count = 0;
     for (auto& element : touchPoints_) {
+        if (count >= fingers_) {
+            break;
+        }
         sumOfPoints = sumOfPoints + element.second.GetOffset();
+        count++;
     }
-    Offset focusPoint = sumOfPoints / touchPoints_.size();
+    Offset focusPoint = sumOfPoints / count;
     return focusPoint;
 }
 
