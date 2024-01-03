c030a2ea4de27ed9b622500639237eae06e07ceb
zhoukechen <zhoukechen2@huawei.com>
2024-01-03 22:28:10 +0800

    inline不支持拖入 && 修复小数点模式在前面输小数点后面的小数点会消失
    Signed-off-by: zhoukechen <zhoukechen2@huawei.com>
    Change-Id: Icc6e47b17832fdb52eae039e86072d062b2c987b

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -1478,7 +1478,7 @@ std::function<void(const RefPtr<OHOS::Ace::DragEvent>&, const std::string&)> Tex
         CHECK_NULL_VOID(host);
         auto layoutProperty = host->GetLayoutProperty<TextFieldLayoutProperty>();
         CHECK_NULL_VOID(layoutProperty);
-        if (layoutProperty->GetIsDisabledValue(false)) {
+        if (layoutProperty->GetIsDisabledValue(false) || pattern->IsNormalInlineState()) {
             return;
         }
         if (extraParams.empty()) {
@@ -6163,6 +6163,9 @@ void TextFieldPattern::ShowMenu()
 
 void TextFieldPattern::HandleCursorOnDragMoved(const RefPtr<NotifyDragEvent>& notifyDragEvent)
 {
+    if (IsNormalInlineState()) {
+        return;
+    }
     auto host = GetHost();
     CHECK_NULL_VOID(host);
     if (isCursorAlwaysDisplayed_) {
