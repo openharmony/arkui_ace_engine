950ab5e04ed6b2a71640879fd408d7ac54b21ad0
hw_wyx <wuyinxiao@huawei.com>
2024-01-04 21:23:07 +0800

    fixChangeSystemColor
    Signed-off-by: hw_wyx <wuyinxiao@huawei.com>
    Change-Id: I53a7f2e74b7df01eea31665cd5622601329264c2

--- test/mock/core/image_provider/mock_image_loading_context.cpp
+++ test/mock/core/image_provider/mock_image_loading_context.cpp
@@ -119,16 +119,6 @@ void ImageLoadingContext::SetAutoResize(bool needResize)
     autoResize_ = needResize;
 }
 
-bool ImageLoadingContext::GetIsOnSystemColorChange() const
-{
-    return isSystemColorChange_;
-}
-
-void ImageLoadingContext::SetIsSystemColorChange(bool isSystemColorChange)
-{
-    isSystemColorChange_ = isSystemColorChange;
-}
-
 const SizeF& ImageLoadingContext::GetDstSize() const
 {
     return dstSize_;
