23d18854c667e82a309abb697a394108f9defd59
郗鹤琼 <xiheqiong@huawei.com>
2024-01-06 10:00:06 +0000

    update frameworks/core/components_ng/pattern/navrouter/navdestination_model_ng.cpp.
    Signed-off-by: 郗鹤琼 <xiheqiong@huawei.com>

--- frameworks/core/components_ng/pattern/navrouter/navdestination_model_ng.cpp
+++ frameworks/core/components_ng/pattern/navrouter/navdestination_model_ng.cpp
@@ -188,7 +188,7 @@ void NavDestinationModelNG::Create(std::function<void()>&& deepRenderFunc)
         navDestinationNode->AddChild(contentNode);
         navDestinationNode->SetContentNode(contentNode);
 
-        SafeAreaExpandOpts opts = {.type = SAFE_AREA_TYPE_SYSTEM, .edges = SAFE_AREA_EDGE_ALL};
+        SafeAreaExpandOpts opts = {.type = SAFE_AREA_TYPE_SYSTEM, .edges = SAFE_AREA_EDGE_BOTTOM};
         contentNode->GetLayoutProperty()->UpdateSafeAreaExpandOpts(opts);
     }
     stack->Push(navDestinationNode);
