36b36c28e66ca59b5b0e6308cf57e984de12aa30
SQ <sunqi57@huawei.com>
2024-01-08 11:34:16 +0000

    修复List居中限位时滑到底部滚动条变小的问题
    Signed-off-by: SQ <sunqi57@huawei.com>

--- frameworks/core/components_ng/pattern/list/list_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_pattern.cpp
@@ -754,7 +754,7 @@ bool ListPattern::IsOutOfBoundary(bool useCurrentDelta)
     bool outOfEnd = (endIndex_ == maxListItemIndex_) && LessNotEqual(endPos, contentEndPos) &&
         LessNotEqual(startPos, contentStartOffset_);
     if (IsScrollSnapAlignCenter()) {
-        auto itemHeight = itemPosition_.begin()->second.endPos - itemPosition_.begin()->second.startPos;
+        float itemHeight = itemPosition_[centerIndex_].endPos - itemPosition_[centerIndex_].startPos;
         outOfStart = (startIndex_ == 0) && Positive(startPos + itemHeight / 2.0f - contentMainSize_ / 2.0f);
         outOfEnd =
             (endIndex_ == maxListItemIndex_) && LessNotEqual(endPos - itemHeight / 2.0f, contentMainSize_ / 2.0f);
