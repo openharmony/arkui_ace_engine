944eab4eb5ca95b578fe76396e00d22abeb262d0
wu_zhende <wuzhende666@163.com>
2024-01-09 17:03:45 +0800

    copyOption设置为None时不弹出复制剪切选项
    Signed-off-by: wu_zhende <wuzhende666@163.com>

--- frameworks/core/components/web/resource/web_delegate.h
+++ frameworks/core/components/web/resource/web_delegate.h
@@ -663,6 +663,7 @@ public:
         int32_t accessibilityId, int32_t direction, OHOS::NWeb::NWebAccessibilityNodeInfo& nodeInfo) const;
     void SetAccessibilityState(bool state);
     void UpdateAccessibilityState(bool state);
+    OHOS::NWeb::NWebPreference::CopyOptionMode GetCopyOptionMode() const;
 private:
     void InitWebEvent();
     void RegisterWebEvent();
