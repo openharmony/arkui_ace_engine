ded39e0af1baa9917244e97630da3f7029748a7d
g00649947 <gaoweihua2@huawei.com>
2024-01-10 13:40:55 +0800

    change sequence
    Signed-off-by: g00649947 <gaoweihua2@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
+++ frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
@@ -270,8 +270,8 @@ var Alignment;
 var BlendMode;
 (function (BlendMode) {
   BlendMode[BlendMode["NORMAL"] = 0] = "NORMAL";
-  BlendMode[BlendMode["DESTINATION_IN"] = 1] = "DESTINATION_IN";
-  BlendMode[BlendMode["SOURCE_IN"] = 2] = "SOURCE_IN";
+  BlendMode[BlendMode["DESTINATION_IN"] = 7] = "DESTINATION_IN";
+  BlendMode[BlendMode["SOURCE_IN"] = 6] = "SOURCE_IN";
   BlendMode[BlendMode["NONE"] = 0] = "NONE";
   BlendMode[BlendMode["CLEAR"] = 1] = "CLEAR";
   BlendMode[BlendMode["SRC"] = 2] = "SRC";
