58021cb4af78601bb8c2a8780374c425c193a8cb
zhangrongjie <zhangrongjie4@huawei.com>
2024-01-11 11:31:54 +0000

    fix the bug of IsOutOfBoundary in List
    Signed-off-by: zhangrongjie <zhangrongjie4@huawei.com>
    Change-Id: Ib96a8a902ee10ecf53ef770cc5adacdc96da7a94

--- frameworks/core/components_ng/pattern/list/list_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_pattern.cpp
@@ -753,12 +753,6 @@ bool ListPattern::IsOutOfBoundary(bool useCurrentDelta)
 
     bool outOfEnd = (endIndex_ == maxListItemIndex_) && LessNotEqual(endPos, contentEndPos) &&
         LessNotEqual(startPos, contentStartOffset_);
-    if (IsScrollSnapAlignCenter()) {
-        float itemHeight = itemPosition_[centerIndex_].endPos - itemPosition_[centerIndex_].startPos;
-        outOfStart = (startIndex_ == 0) && Positive(startPos + itemHeight / 2.0f - contentMainSize_ / 2.0f);
-        outOfEnd =
-            (endIndex_ == maxListItemIndex_) && LessNotEqual(endPos - itemHeight / 2.0f, contentMainSize_ / 2.0f);
-    }
     return outOfStart || outOfEnd;
 }
 
