fc3e458820d7e1acb130f19470d9754c91eb7a50
19863813978 <19863813978@163.com>
2024-01-15 15:05:07 +0800

    update
    Signed-off-by: 19863813978 <19863813978@163.com>

--- frameworks/core/components_ng/pattern/text/text_styles.cpp
+++ frameworks/core/components_ng/pattern/text/text_styles.cpp
@@ -100,7 +100,7 @@ std::string GetFontFamilyInJson(const std::optional<std::vector<std::string>>& v
 std::string GetSymbolRenderingStrategyInJson(const std::optional<uint32_t>& value)
 {
     std::string text;
-    switch( value.value_or(0)) {
+    switch (value.value_or(0)) {
         case 0:
             text = "RenderingStrategy.SINGLE";
             break;
@@ -118,7 +118,7 @@ std::string GetSymbolRenderingStrategyInJson(const std::optional<uint32_t>& valu
 std::string GetSymbolEffectStrategyInJson(const std::optional<uint32_t>& value)
 {
     std::string text;
-    switch( value.value_or(0)) {
+    switch (value.value_or(0)) {
         case 0:
             text = "EffectStrategy.NONE";
             break;
