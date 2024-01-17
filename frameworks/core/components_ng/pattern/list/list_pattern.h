7113a87afad4ce27daf364f49a451540f4d788ba
yeyinglong <yeyinglong@hisilicon.com>
2024-01-17 11:55:40 +0000

    回退 'Pull Request !25178 : 滚动组件鼠标框选起始位置纠正为鼠标按下位置'

--- frameworks/core/components_ng/pattern/list/list_pattern.h
+++ frameworks/core/components_ng/pattern/list/list_pattern.h
@@ -271,7 +271,7 @@ private:
 
     // multiSelectable
     void ClearMultiSelect() override;
-    bool IsItemSelected() override;
+    bool IsItemSelected(const GestureEvent& info) override;
     void MultiSelectWithoutKeyboard(const RectF& selectedZone) override;
     void HandleCardModeSelectedEvent(
         const RectF& selectedZone, const RefPtr<FrameNode>& itemGroupNode, float itemGroupTop);
