8f631aa7885c67688cd11c50fe6dea11d0dbc135
donglin <donglin9@huawei.com>
2024-01-20 04:11:10 +0000

    codecheck
    Signed-off-by: donglin <donglin9@huawei.com>
    Change-Id: I36be870e7a4fe86637c1a07bcf9205b562fd91d4

--- frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
+++ frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
@@ -1733,7 +1733,7 @@ class NavPathStack {
     let pathInfo = this.pathArray.pop();
     this.changeFlag = this.changeFlag + 1;
     this.isReplace = 0;
-    if (result !== undefined && typeof result !== 'boolean' && currentPathInfo.onPop != undefined) {
+    if (result !== undefined && typeof result !== 'boolean' && currentPathInfo.onPop !== undefined) {
       let popInfo = {
         info: currentPathInfo,
         result: result,
@@ -1763,7 +1763,7 @@ class NavPathStack {
     this.pathArray.splice(index + 1);
     this.changeFlag = this.changeFlag + 1;
     this.isReplace = 0;
-    if (result !== undefined && typeof result !== 'boolean' && currentPathInfo.onPop != undefined) {
+    if (result !== undefined && typeof result !== 'boolean' && currentPathInfo.onPop !== undefined) {
       let popInfo = {
         info: currentPathInfo,
         result: result,
@@ -1789,7 +1789,7 @@ class NavPathStack {
     this.pathArray.splice(index + 1);
     this.changeFlag = this.changeFlag + 1;
     this.isReplace = 0;
-    if (result !== undefined && typeof result !== 'boolean' && currentPathInfo.onPop != undefined) {
+    if (result !== undefined && typeof result !== 'boolean' && currentPathInfo.onPop !== undefined) {
       let popInfo = {
         info: currentPathInfo,
         result: result,
