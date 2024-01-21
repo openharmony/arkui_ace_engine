938bd3171f843ca1169f296675309f13402a3cf4
shihao <wangshihao26@huawei.com>
2024-01-21 09:34:49 +0000

    tabbar 适配pixelround
    Signed-off-by: shihao <wangshihao26@huawei.com>

--- frameworks/core/components_ng/pattern/tabs/tab_content_model_ng.cpp
+++ frameworks/core/components_ng/pattern/tabs/tab_content_model_ng.cpp
@@ -33,6 +33,9 @@
 #include "core/components_v2/inspector/inspector_constants.h"
 
 namespace OHOS::Ace::NG {
+namespace {
+const uint8_t PIXEL_ROUND = 18;
+}
 
 void TabContentModelNG::Create(std::function<void()>&& deepRenderFunc)
 {
@@ -133,6 +136,7 @@ void TabContentModelNG::AddTabBarItem(const RefPtr<UINode>& tabContent, int32_t 
     linearLayoutProperty->UpdateMainAxisAlign(FlexAlign::CENTER);
     linearLayoutProperty->UpdateCrossAxisAlign(FlexAlign::CENTER);
     linearLayoutProperty->UpdateSpace(tabTheme->GetBottomTabBarSpace());
+    linearLayoutProperty->UpdatePixelRound(PIXEL_ROUND);
     auto columnRenderContext = columnNode->GetRenderContext();
     CHECK_NULL_VOID(columnRenderContext);
     columnRenderContext->UpdateClipEdge(true);
