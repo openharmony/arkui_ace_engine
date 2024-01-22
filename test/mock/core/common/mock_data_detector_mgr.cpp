13f2f5541f64b44b9dc6e4d524d771dc5e75c4e2
fan_pan <fanpan10@huawei.com>
2024-01-22 09:26:02 +0800

    text的AI相关属性与方法整改
    Signed-off-by: fan_pan <fanpan10@huawei.com>
    Change-Id: I180a1207260f61193052f69b0f128e44c8b21d52

--- test/mock/core/common/mock_data_detector_mgr.cpp
+++ test/mock/core/common/mock_data_detector_mgr.cpp
@@ -29,28 +29,6 @@ bool DataDetectorMgr::IsDataDetectorSupported()
 
 void DataDetectorMgr::DataDetect(const TextDataDetectInfo& info, const TextDetectResultFunc& func) {}
 
-RefPtr<NG::FrameNode> DataDetectorMgr::CreateUIExtensionMenu(
-    const std::map<std::string, std::string>& paramaters, std::function<void(const std::string&)> onClickMenu)
-{
-    return nullptr;
-}
-
-bool DataDetectorMgr::ShowUIExtensionMenu(const std::map<std::string, std::string>& paramaters, NG::RectF safeArea,
-    std::function<void(const std::string&)> onClickMenu, const RefPtr<NG::FrameNode>& targetNode)
-{
-    return true;
-}
-
-void DataDetectorMgr::ResponseBestMatchItem(const std::map<std::string, std::string>& paramaters, const AISpan& aiSpan)
-{}
-
-std::function<void(const AAFwk::WantParams&)> DataDetectorMgr::GetOnReceive(
-    const RefPtr<NG::FrameNode>& uiExtNode, NG::RectF aiRect,
-    std::function<void(const std::string&)> onClickMenu, const RefPtr<NG::FrameNode>& targetNode)
-{
-    return [](const AAFwk::WantParams& wantParams) {};
-}
-
 void DataDetectorMgr::AdjustCursorPosition(
     int32_t& caretPos, const std::string& content, TimeStamp& lastAiPosTimeStamp, const TimeStamp& lastClickTimeStamp)
 {
