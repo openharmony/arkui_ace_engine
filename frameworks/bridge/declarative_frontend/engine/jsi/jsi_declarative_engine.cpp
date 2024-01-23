011cae327d5fa42f4e20189fa44c910bb4062577
kangchongtao <kangchongtao@huawei.com>
2024-01-23 17:27:17 +0800

    fixed 2a1f8e5 from https://gitee.com/kangchongtao/arkui_ace_engine/pulls/26262 localstorage native memory leak
    Signed-off-by: kangchongtao <kangchongtao@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
+++ frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
@@ -2060,10 +2060,9 @@ void JsiDeclarativeEngine::SetLocalStorage(int32_t instanceId, NativeReference* 
     if (jsValue->IsObject()) {
         auto storage = JSRef<JSObject>::Cast(jsValue);
         JSLocalStorage::AddStorage(instanceId, storage);
-    } else {
-        delete nativeValue;
-        nativeValue = nullptr;
     }
+    delete nativeValue;
+    nativeValue = nullptr;
 #endif
 }
 
