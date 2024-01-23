b6ec4b61cbf301ac78fb2f6b08dd49cbc9bc2062
fengjituo111 <fengjituo@huawei.com>
2024-01-23 12:40:54 +0000

    增加synatxItem Tag判断
    Signed-off-by: fengjituo111 <fengjituo@huawei.com>
    Change-Id: Id54576165491cd565db5ba582f19b2f4ba8cdd98

--- frameworks/core/components_ng/layout/layout_wrapper.cpp
+++ frameworks/core/components_ng/layout/layout_wrapper.cpp
@@ -37,7 +37,7 @@ namespace {
 bool IsSyntaxNode(const std::string& tag)
 {
     return tag == V2::JS_VIEW_ETS_TAG || tag == V2::JS_IF_ELSE_ETS_TAG || tag == V2::JS_FOR_EACH_ETS_TAG ||
-           tag == V2::JS_LAZY_FOR_EACH_ETS_TAG;
+           tag == V2::JS_LAZY_FOR_EACH_ETS_TAG || tag == V2::JS_SYNTAX_ITEM_ETS_TAG;
 }
 } // namespace
 
