d8e270a6be46742bd1be919b94713ae8398b36f0
txzhang94 <zhangtianxiang4@huawei.com>
2024-01-24 14:10:35 +0800

    fix arkui-x build failed
    Signed-off-by: txzhang94 <zhangtianxiang4@huawei.com>
    Change-Id: Iafe5b2a11a18f665adcaa0339a9d42c2276dc99f

--- frameworks/core/components_ng/pattern/web/web_model_ng.cpp
+++ frameworks/core/components_ng/pattern/web/web_model_ng.cpp
@@ -450,7 +450,6 @@ void WebModelNG::SetCopyOptionMode(CopyOptions mode)
 {
     auto webPattern = ViewStackProcessor::GetInstance()->GetMainFrameNodePattern<WebPattern>();
     CHECK_NULL_VOID(webPattern);
-    webPattern->UpdateCopyOptionMode(static_cast<int32_t>(mode));
 }
 
 void WebModelNG::SetOverviewModeAccessEnabled(bool isOverviewModeAccessEnabled)
@@ -834,7 +833,6 @@ void WebModelNG::SetNativeEmbedModeEnabled(bool isEmbedModeEnabled)
 {
     auto webPattern = ViewStackProcessor::GetInstance()->GetMainFrameNodePattern<WebPattern>();
     CHECK_NULL_VOID(webPattern);
-    webPattern->UpdateNativeEmbedModeEnabled(isEmbedModeEnabled);
 }
 
 void WebModelNG::SetOnControllerAttached(std::function<void()>&& callback)
@@ -941,7 +939,5 @@ void WebModelNG::SetPermissionClipboard(std::function<void(const std::shared_ptr
 {
     auto webPattern = ViewStackProcessor::GetInstance()->GetMainFrameNodePattern<WebPattern>();
     CHECK_NULL_VOID(webPattern);
-    
-    webPattern->SetPermissionClipboardCallback(std::move(jsCallback));
 }
 } // namespace OHOS::Ace::NG
