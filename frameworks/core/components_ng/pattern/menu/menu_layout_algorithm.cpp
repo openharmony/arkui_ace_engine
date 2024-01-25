e3d1d65ae493bb62f1c35447f3b08fa0c44099b7
lijuan <lijuan124@huawei.com>
2024-01-25 12:05:08 +0000

    fixed 5d72181 from https://gitee.com/lijuan124/arkui_ace_engine/pulls/26412 支持输入框右键菜单避让输入法，以及滚动时右键菜单关闭
    Signed-off-by: lijuan <lijuan124@huawei.com>

--- frameworks/core/components_ng/pattern/menu/menu_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/menu/menu_layout_algorithm.cpp
@@ -1268,6 +1268,9 @@ void MenuLayoutAlgorithm::Layout(LayoutWrapper* layoutWrapper)
             position_ += offset;
         }
         auto menuPosition = MenuLayoutAvoidAlgorithm(menuProp, menuPattern, size, didNeedArrow);
+        if (menuPattern->IsSelectOverlayRightClickMenu()) {
+            AdjustSelectOverlayMenuPosition(menuPosition, geometryNode);
+        }
         SetMenuPlacementForAnimation(layoutWrapper);
         arrowPosition_ = GetArrowPositionWithPlacement(size);
         if (didNeedArrow && arrowPlacement_ != Placement::NONE) {
@@ -1300,6 +1303,23 @@ void MenuLayoutAlgorithm::Layout(LayoutWrapper* layoutWrapper)
         child->GetGeometryNode()->SetMarginFrameOffset(translate);
         child->Layout();
         translate += OffsetF(0, child->GetGeometryNode()->GetFrameSize().Height());
+    }
+}
+
+void MenuLayoutAlgorithm::AdjustSelectOverlayMenuPosition(
+    OffsetF& menuPosition, const RefPtr<GeometryNode>& geometryNode)
+{
+    auto pipelineContext = GetCurrentPipelineContext();
+    CHECK_NULL_VOID(pipelineContext);
+    auto safeAreaManager = pipelineContext->GetSafeAreaManager();
+    auto keyboardInsert = safeAreaManager->GetKeyboardInset();
+    auto size = geometryNode->GetFrameSize();
+    auto start = static_cast<float>(keyboardInsert.start);
+    if (GreatNotEqual(menuPosition.GetY() + size.Height(), start) && GreatOrEqual(start, size.Height())) {
+        menuPosition.SetY(menuPosition.GetY() - margin_ - size.Height());
+    } else if (GreatNotEqual(menuPosition.GetY() + size.Height(), start) && LessNotEqual(start, size.Height()) &&
+               GreatNotEqual(start, 0)) {
+        menuPosition.SetY(menuPosition.GetY() - margin_ - size.Height() / 2);
     }
 }
 
