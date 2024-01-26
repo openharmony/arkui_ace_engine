24b12630ef24cea11fef54281c687036d0af5c40
SQ <sunqi57@huawei.com>
2024-01-26 03:47:57 +0000

    list调整大距离跳转优化只对拖动滚动条场景生效
    Signed-off-by: SQ <sunqi57@huawei.com>

--- frameworks/core/components_ng/pattern/list/list_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_pattern.cpp
@@ -181,6 +181,7 @@ bool ListPattern::OnDirtyLayoutWrapperSwap(const RefPtr<LayoutWrapper>& dirty, c
         isScrollEnd_ = false;
     }
     currentDelta_ = 0.0f;
+    isNeedCheckOffset_ = false;
     float prevStartOffset = startMainPos_;
     float prevEndOffset = endMainPos_ - contentMainSize_ + contentEndOffset_;
     contentMainSize_ = listLayoutAlgorithm->GetContentMainSize();
@@ -484,6 +485,7 @@ RefPtr<LayoutAlgorithm> ListPattern::CreateLayoutAlgorithm()
     }
     listLayoutAlgorithm->SetTotalOffset(GetTotalOffset());
     listLayoutAlgorithm->SetCurrentDelta(currentDelta_);
+    listLayoutAlgorithm->SetIsNeedCheckOffset(isNeedCheckOffset_);
     listLayoutAlgorithm->SetItemsPosition(itemPosition_);
     listLayoutAlgorithm->SetPrevContentMainSize(contentMainSize_);
     if (IsOutOfBoundary(false) && GetScrollSource() != SCROLL_FROM_AXIS) {
@@ -700,6 +702,9 @@ bool ListPattern::UpdateCurrentOffset(float offset, int32_t source)
     SetScrollSource(source);
     FireAndCleanScrollingListener();
     currentDelta_ = currentDelta_ - offset;
+    if (source == SCROLL_FROM_BAR || source == SCROLL_FROM_BAR_FLING) {
+        isNeedCheckOffset_ = true;
+    }
     MarkDirtyNodeSelf();
     if (!IsOutOfBoundary() || !scrollable_) {
         return true;
