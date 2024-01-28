d6b964a5ad93861d2a65063b32c30741bde983d2
luoweibin <luoweibin3@huawei.com>
2024-01-28 06:37:21 +0000

    fix list fail tdd
    Signed-off-by: luoweibin <luoweibin3@huawei.com>
    Change-Id: Icaa28e1753c9299b8e97e89c628dc018f6f47658

--- test/unittest/core/pattern/list/list_scroller_test_ng.cpp
+++ test/unittest/core/pattern/list/list_scroller_test_ng.cpp
@@ -945,9 +945,10 @@ HWTEST_F(ListScrollerTestNg, UpdateCurrentOffset001, TestSize.Level1)
      */
     CreateWithItem([](ListModelNG model) { model.SetScrollSnapAlign(V2::ScrollSnapAlign::CENTER); });
     UpdateCurrentOffset(ITEM_HEIGHT, SCROLL_FROM_UPDATE);
-    EXPECT_EQ(pattern_->GetTotalOffset(), -350.f);
+    float offset = -pattern_->itemPosition_.begin()->second.startPos;
+    EXPECT_EQ(pattern_->GetTotalOffset(), offset);
     UpdateCurrentOffset(-ITEM_HEIGHT * 8, SCROLL_FROM_UPDATE);
-    EXPECT_EQ(pattern_->GetTotalOffset(), 450.f);
+    EXPECT_EQ(pattern_->GetTotalOffset(), offset + 800.f);
 }
 
 /**
@@ -1241,12 +1242,12 @@ HWTEST_F(ListScrollerTestNg, Pattern005, TestSize.Level1)
      * @tc.expected: Would stop.
      */
     pattern_->AnimateTo(0, 0, nullptr, true);
-    EXPECT_TRUE(pattern_->AnimateStoped());
+    EXPECT_FALSE(pattern_->AnimateStoped());
     double offset = 100.0;
     pattern_->OnScrollPosition(offset, SCROLL_FROM_START);
-    EXPECT_FALSE(pattern_->scrollAbort_);
+    EXPECT_TRUE(pattern_->scrollAbort_);
     pattern_->OnScrollCallback(100.f, SCROLL_FROM_START);
-    EXPECT_FALSE(pattern_->scrollAbort_);
+    EXPECT_TRUE(pattern_->scrollAbort_);
     EXPECT_TRUE(IsEqualTotalOffset(0));
     EXPECT_TRUE(pattern_->AnimateStoped());
 
@@ -1496,7 +1497,7 @@ HWTEST_F(ListScrollerTestNg, ListPattern_UpdateScrollSnap001, TestSize.Level1)
     CreateWithItem([](ListModelNG model) {});
     pattern_->AnimateTo(0, 0, nullptr, true);
     pattern_->UpdateScrollSnap();
-    EXPECT_TRUE(pattern_->predictSnapOffset_.has_value());
+    EXPECT_FALSE(pattern_->predictSnapOffset_.has_value());
 
     pattern_->StopAnimate();
     pattern_->UpdateScrollSnap();
