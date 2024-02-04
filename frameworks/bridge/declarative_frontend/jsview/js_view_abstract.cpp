e2fd26f637b959d33e34ec54538134e036465a88
zhouchaobo <zhouchaobo@huawei.com>
2024-02-04 10:12:21 +0800

    fixed babf713 from https://gitee.com/zhou-chaobo/arkui_ace_engine/pulls/27228 response region default value bug fix
    Signed-off-by: zhouchaobo <zhouchaobo@huawei.com>
    Change-Id: I5d83d619d43a8cf56690a2b487dff281904433c8

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
@@ -1562,6 +1562,7 @@ void JSViewAbstract::JsResponseRegion(const JSCallbackInfo& info)
 {
     std::vector<DimensionRect> result;
     if (!JSViewAbstract::ParseJsResponseRegionArray(info[0], result)) {
+        ViewAbstractModel::GetInstance()->SetResponseRegion({});
         return;
     }
 
@@ -1572,6 +1573,7 @@ void JSViewAbstract::JsMouseResponseRegion(const JSCallbackInfo& info)
 {
     std::vector<DimensionRect> result;
     if (!JSViewAbstract::ParseJsResponseRegionArray(info[0], result)) {
+        ViewAbstractModel::GetInstance()->SetMouseResponseRegion({});
         return;
     }
     ViewAbstractModel::GetInstance()->SetMouseResponseRegion(result);
