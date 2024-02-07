33af465e3e415113c60a5294bcb99e059c8eda94
sunbees <sunqinjia@huawei.com>
2024-02-07 14:04:31 +0800

    surface-xcomponent support background color
    Signed-off-by: sunbees <sunqinjia@huawei.com>
    Change-Id: Ibcba1a48b906d4f4da89f1890056570a25c5b3db

--- frameworks/core/interfaces/native/node/node_xcomponent_modifier.cpp
+++ frameworks/core/interfaces/native/node/node_xcomponent_modifier.cpp
@@ -25,9 +25,10 @@ namespace OHOS::Ace::NG {
 namespace {
 void SetXComponentBackgroundColor(ArkUINodeHandle node, uint32_t color)
 {
-    auto *frameNode = reinterpret_cast<FrameNode *>(node);
+    auto* frameNode = reinterpret_cast<FrameNode*>(node);
     CHECK_NULL_VOID(frameNode);
-    if (!XComponentModelNG::IsTexture(frameNode)) {
+    auto type = XComponentModelNG::GetType(frameNode);
+    if (!XComponentModel::IsBackGroundColorAvailable(type)) {
         return;
     }
     ViewAbstract::SetBackgroundColor(frameNode, Color(color));
@@ -35,12 +36,13 @@ void SetXComponentBackgroundColor(ArkUINodeHandle node, uint32_t color)
 
 void ResetXComponentBackgroundColor(ArkUINodeHandle node)
 {
-    auto *frameNode = reinterpret_cast<FrameNode *>(node);
+    auto* frameNode = reinterpret_cast<FrameNode*>(node);
     CHECK_NULL_VOID(frameNode);
-    if (!XComponentModelNG::IsTexture(frameNode)) {
+    auto type = XComponentModelNG::GetType(frameNode);
+    if (!XComponentModel::IsBackGroundColorAvailable(type)) {
         return;
     }
-    ViewAbstract::SetBackgroundColor(frameNode, Color(Color::TRANSPARENT));
+    ViewAbstract::SetBackgroundColor(frameNode, (type == XComponentType::SURFACE) ? Color::BLACK : Color::TRANSPARENT);
 }
 
 void SetXComponentOpacity(ArkUINodeHandle node, ArkUI_Float32 opacity)
