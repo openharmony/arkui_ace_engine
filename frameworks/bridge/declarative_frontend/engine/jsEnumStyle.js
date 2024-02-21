00c4a6a84afc67de90052d12ff4ab0148fb63a55
xiongjun_gitee <xiongjun28@huawei.com>
2024-02-21 10:01:16 +0800

    fixed 00028a0 from https://gitee.com/xiongjun_gitee/arkui_ace_engine/pulls/27737 解决ProtectedResourceType枚举值为undefined
    Signed-off-by: xiongjun_gitee <xiongjun28@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
+++ frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
@@ -1106,6 +1106,8 @@ var FileSelectorMode;
 var ProtectedResourceType;
 (function (ProtectedResourceType) {
   ProtectedResourceType["MidiSysex"] = "TYPE_MIDI_SYSEX";
+  ProtectedResourceType["VIDEO_CAPTURE"] = "TYPE_VIDEO_CAPTURE";
+  ProtectedResourceType["AUDIO_CAPTURE"] = "TYPE_AUDIO_CAPTURE";
 })(ProtectedResourceType || (ProtectedResourceType = {}));
 
 var ProgressType;
