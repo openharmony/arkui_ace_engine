d7d4557470f8647405a06f46d4bcb767fe44076c
姚明 <yaoming14@huawei.com>
2024-02-22 10:09:01 +0800

    修复RichEditor的AI取词相关初始化时还未完成进行滑动异常
    Signed-off-by: 姚明 <yaoming14@huawei.com>
    Change-Id: Id8d1cfcf21e786259bf0f10ad85ba6e28f8f80ae

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -1674,14 +1674,14 @@ void RichEditorPattern::InitFocusEvent(const RefPtr<FocusHub>& focusHub)
 {
     CHECK_NULL_VOID(!focusEventInitialized_);
     auto focusTask = [weak = WeakClaim(this)]() {
-        TAG_LOGI(AceLogTag::ACE_RICH_TEXT, "rich editor in focus");
+        TAG_LOGD(AceLogTag::ACE_RICH_TEXT, "rich editor in focus");
         auto pattern = weak.Upgrade();
         CHECK_NULL_VOID(pattern);
         pattern->HandleFocusEvent();
     };
     focusHub->SetOnFocusInternal(focusTask);
     auto blurTask = [weak = WeakClaim(this)]() {
-        TAG_LOGI(AceLogTag::ACE_RICH_TEXT, "rich editor in blur");
+        TAG_LOGD(AceLogTag::ACE_RICH_TEXT, "rich editor in blur");
         auto pattern = weak.Upgrade();
         CHECK_NULL_VOID(pattern);
         pattern->HandleBlurEvent();
