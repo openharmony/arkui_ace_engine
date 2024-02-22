8a72528fe48b4c67655d4d9f34cba5db4aa5bf2c
s30052571 <sunhanyang@huawei.com>
2024-02-22 21:45:46 +0800

    support getting appbar rect.
    Signed-off-by: s30052571 <sunhanyang@huawei.com>

--- frameworks/core/components_ng/pattern/app_bar/app_bar_theme.h
+++ frameworks/core/components_ng/pattern/app_bar/app_bar_theme.h
@@ -24,6 +24,7 @@
 #include "core/components/theme/theme_attributes.h"
 #include "core/components/theme/theme_constants.h"
 #include "core/components/theme/theme_style.h"
+#include "core/pipeline_ng/pipeline_context.h"
 
 namespace OHOS::Ace::NG {
 
@@ -69,6 +70,71 @@ public:
         return appBarFontSize_;
     }
 
+    const Dimension& GetMenuBarMarginLeft() const
+    {
+        return menuBarMarginLeft_;
+    }
+
+    const Dimension& GetMenuBarMarginRight() const
+    {
+        return menuBarMarginRight_;
+    }
+
+    const Dimension& GetMenuBarMarginTop() const
+    {
+        return menuBarMarginTop_;
+    }
+
+    const Dimension& GetMenuBarWidth() const
+    {
+        return menuBarWidth_;
+    }
+
+    const Dimension& GetMenuBarHeight() const
+    {
+        return menuBarHeight_;
+    }
+
+    const Dimension& GetShareMarginLeft() const
+    {
+        return shareMarginLeft_;
+    }
+
+    const Dimension& GetShareMarginRight() const
+    {
+        return shareMarginRight_;
+    }
+
+    const Dimension& GetCloseMarginLeft() const
+    {
+        return closeMarginLeft_;
+    }
+
+    const Dimension& GetCloseMarginRight() const
+    {
+        return closeMarginRight_;
+    }
+
+    const Dimension& GetIconMarginVertical() const
+    {
+        return iconMarginVertical_;
+    }
+
+    const Dimension& GetDividerWidth() const
+    {
+        return dividerWidth_;
+    }
+
+    const Dimension& GetDividerHeight() const
+    {
+        return dividerHeight_;
+    }
+
+    const Dimension& GetIconNewSize() const
+    {
+        return iconNewSize_;
+    }
+
     const Color& GetTextColor() const
     {
         return textColorPrimary_;
@@ -99,6 +165,21 @@ public:
         return stageAbilityName_;
     }
 
+    std::optional<RectF> GetAppBarRect() const
+    {
+        auto pipeline = PipelineContext::GetCurrentContext();
+        if (!pipeline || !pipeline->GetInstallationFree()) {
+            return std::nullopt;
+        }
+        Dimension screenWidthByPx(SystemProperties::GetDeviceWidth(), DimensionUnit::PX);
+        Dimension screenWidthByVp(screenWidthByPx.ConvertToVp(), DimensionUnit::VP);
+        Dimension width = menuBarMarginLeft_ + menuBarWidth_ + menuBarMarginRight_;
+        Dimension height = menuBarHeight_;
+        Dimension left = screenWidthByVp - width;
+        Dimension top = menuBarMarginTop_;
+        RectF rect(left.Value(), top.Value(), width.Value(), height.Value());
+        return rect;
+    }
 private:
     AppBarTheme() = default;
     static void ParsePattern(const RefPtr<ThemeStyle>& themeStyle, const RefPtr<AppBarTheme>& theme)
@@ -121,12 +202,38 @@ private:
         theme->bundleName_ = appBarPattern->GetAttr<std::string>("app_bar_bundle_name", "com.ohos.hag.famanager");
         theme->abilityName_ = appBarPattern->GetAttr<std::string>("app_bar_ability_name", "FaPanelAbility");
         theme->stageAbilityName_ = appBarPattern->GetAttr<std::string>("app_bar_stage_ability_name", "PanelAbility");
+        theme->menuBarMarginLeft_ = appBarPattern->GetAttr<Dimension>("menu_bar_margin_left", 12.0_vp);
+        theme->menuBarMarginRight_ = appBarPattern->GetAttr<Dimension>("menu_bar_margin_right", 8.0_vp);
+        theme->menuBarMarginTop_ = appBarPattern->GetAttr<Dimension>("menu_bar_margin_top", 10.0_vp);
+        theme->menuBarWidth_ = appBarPattern->GetAttr<Dimension>("menu_bar_width", 80.0_vp);
+        theme->menuBarHeight_ = appBarPattern->GetAttr<Dimension>("menu_bar_height", 36.0_vp);
+        theme->shareMarginLeft_ = appBarPattern->GetAttr<Dimension>("share_margin_left", 12.0_vp);
+        theme->shareMarginRight_ = appBarPattern->GetAttr<Dimension>("share_margin_right", 8.0_vp);
+        theme->closeMarginLeft_ = appBarPattern->GetAttr<Dimension>("close_margin_left", 8.0_vp);
+        theme->closeMarginRight_ = appBarPattern->GetAttr<Dimension>("close_margin_right", 12.0_vp);
+        theme->iconMarginVertical_ = appBarPattern->GetAttr<Dimension>("icon_margin_vertical", 8.0_vp);
+        theme->dividerWidth_ = appBarPattern->GetAttr<Dimension>("divider_width", 1.0_px);
+        theme->dividerHeight_ = appBarPattern->GetAttr<Dimension>("divider_height", 16.0_vp);
+        theme->iconNewSize_ = appBarPattern->GetAttr<Dimension>("icon_new_size", 20.0_vp);
     }
 
     Dimension appBarHeight_;
     Dimension iconSize_;
     Dimension iconCornerRadius_;
     Dimension appBarFontSize_;
+    Dimension menuBarMarginLeft_;
+    Dimension menuBarMarginRight_;
+    Dimension menuBarMarginTop_;
+    Dimension menuBarWidth_;
+    Dimension menuBarHeight_;
+    Dimension shareMarginLeft_;
+    Dimension shareMarginRight_;
+    Dimension closeMarginLeft_;
+    Dimension closeMarginRight_;
+    Dimension iconMarginVertical_;
+    Dimension dividerWidth_;
+    Dimension dividerHeight_;
+    Dimension iconNewSize_;
     Color textColorPrimary_;
     Color clickEffectColor_;
     Color appBarBgColor_;
