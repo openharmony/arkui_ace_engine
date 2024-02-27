446f62acce6838a40670a1847dc9bdf86bbdcb53
Kirill Kirichenko <kirichenko.kirill@huawei.com>
2024-02-27 10:30:11 +0800

    Use strtol instead of std::stoi
    Signed-off-by: Kirill Kirichenko <kirichenko.kirill@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/jsi/nativeModule/arkts_native_search_bridge.cpp
+++ frameworks/bridge/declarative_frontend/engine/jsi/nativeModule/arkts_native_search_bridge.cpp
@@ -29,6 +29,7 @@ constexpr int NUM_2 = 2;
 constexpr int NUM_3 = 3;
 constexpr int NUM_4 = 4;
 const std::vector<TextAlign> TEXT_ALIGNS = { TextAlign::START, TextAlign::CENTER, TextAlign::END };
+constexpr FontWeight DEFAULT_FONT_WEIGHT = FontWeight::NORMAL;
 
 ArkUINativeModuleValue SearchBridge::SetTextFont(ArkUIRuntimeCallInfo* runtimeCallInfo)
 {
@@ -64,8 +65,12 @@ ArkUINativeModuleValue SearchBridge::SetTextFont(ArkUIRuntimeCallInfo* runtimeCa
 
     if (threeArg->IsString() || threeArg->IsNumber()) {
         if (threeArg->IsString()) {
-            auto weightStr = threeArg->ToString(vm)->ToString();
-            value.fontWeight = std::stoi(weightStr);
+            auto strPtr = threeArg->ToString(vm)->ToString().c_str();
+            char* end;
+            value.fontWeight = strtol(strPtr, &end, 10);
+            if (end == strPtr || *end != '\0' || errno == ERANGE) {
+                value.fontWeight = static_cast<ArkUI_Int32>(DEFAULT_FONT_WEIGHT);
+            }
         }
 
         if (threeArg->IsNumber()) {
@@ -351,8 +356,12 @@ ArkUINativeModuleValue SearchBridge::SetPlaceholderFont(ArkUIRuntimeCallInfo* ru
 
     if (threeArg->IsString() || threeArg->IsNumber()) {
         if (threeArg->IsString()) {
-            auto weightStr = threeArg->ToString(vm)->ToString();
-            value.fontWeight = std::stoi(weightStr);
+            auto strPtr = threeArg->ToString(vm)->ToString().c_str();
+            char* end;
+            value.fontWeight = strtol(strPtr, &end, 10);
+            if (end == strPtr || *end != '\0' || errno == ERANGE) {
+                value.fontWeight = static_cast<ArkUI_Int32>(DEFAULT_FONT_WEIGHT);
+            }
         }
 
         if (threeArg->IsNumber()) {
