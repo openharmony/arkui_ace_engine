fdf0c0f154b0a11d86543e7617a906ee0af0d119
yan-shuifeng <yanshuifeng@huawei.com>
2024-02-28 22:46:02 +0800

    update arkts builderNode bridge
    Signed-off-by: yan-shuifeng <yanshuifeng@huawei.com>
    Change-Id: Ia373826c62b66807fd873b6dfc22e7059d3828fd

---
+++ interfaces/native/native_node_napi.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @addtogroup ArkUI_NativeModule
+ * @{
+ *
+ * @brief Provides UI capabilities of ArkUI on the native side, such as UI component creation and destruction,
+ * tree node operations, attribute setting, and event listening.
+ *
+ * @since 12
+ */
+
+/**
+ * @file native_node_napi.h
+ *
+ * @brief 提供ArkTS侧的FrameNode转换NodeHandle的方式。
+ *
+ * @library libace_ndk.z.so
+ * @syscap SystemCapability.ArkUI.ArkUI.Full
+ * @since 12
+ */
+
+#ifndef ARKUI_NATIVE_NODE_NAPI_H
+#define ARKUI_NATIVE_NODE_NAPI_H
+
+#include "napi/native_api.h"
+#include "native_type.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int32_t OH_ArkUI_GetNodeHandleFromNapiValue(napi_env env, napi_value frameNode, ArkUI_NodeHandle* handle);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif // ARKUI_NATIVE_NODE_NAPI_H
+/** @} */
---
+++ interfaces/native/node/native_node_napi.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "native_node_napi.h"
+
+#include <cstdint>
+
+#include "js_native_api.h"
+#include "js_native_api_types.h"
+#include "native_type.h"
+#include "node/node_model.h"
+
+#include "base/error/error_code.h"
+#include "core/components_ng/base/ui_node.h"
+
+extern "C" {
+
+int32_t OH_ArkUI_GetNodeHandleFromNapiValue(napi_env env, napi_value frameNode, ArkUI_NodeHandle* handle)
+{
+    napi_value frameNodePtr = nullptr;
+    auto result = napi_get_named_property(env, frameNode, "nodePtr_", &frameNodePtr);
+    if (result != napi_ok) {
+        return OHOS::Ace::ERROR_CODE_PARAM_INVALID;
+    }
+    void* nativePtr = 0;
+    result = napi_get_value_external(env, frameNodePtr, &nativePtr);
+    if (result != napi_ok) {
+        return OHOS::Ace::ERROR_CODE_PARAM_INVALID;
+    }
+    auto* uiNodePtr = reinterpret_cast<OHOS::Ace::NG::UINode*>(nativePtr);
+    uiNodePtr->IncRefCount();
+    *handle = new ArkUI_Node({ .type = -1, .uiNodeHandle = reinterpret_cast<ArkUINodeHandle>(nativePtr) });
+    return OHOS::Ace::ERROR_CODE_NO_ERROR;
+}
+
+}
