2cbf1bc44ae7f9eda63496f9a1e92c738ff929e8
yangziyong <nsyangziyong@huawei.com>
2024-02-29 21:42:12 +0800

    TextPicker 支持自定义渐隐效果高度
    Signed-off-by: yangziyong <nsyangziyong@huawei.com>

--- frameworks/bridge/declarative_frontend/jsview/js_textpicker.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_textpicker.cpp
@@ -106,6 +106,7 @@ void JSTextPicker::JSBind(BindingTarget globalObj)
     JSClass<JSTextPicker>::StaticMethod("onCancel", &JSTextPicker::OnCancel);
     JSClass<JSTextPicker>::StaticMethod("onChange", &JSTextPicker::OnChange);
     JSClass<JSTextPicker>::StaticMethod("backgroundColor", &JSTextPicker::PickerBackgroundColor);
+    JSClass<JSTextPicker>::StaticMethod("gradientHeight", &JSTextPicker::SetGradientHeight);
     JSClass<JSTextPicker>::StaticMethod("onClick", &JSInteractableView::JsOnClick);
     JSClass<JSTextPicker>::StaticMethod("onTouch", &JSInteractableView::JsOnTouch);
     JSClass<JSTextPicker>::StaticMethod("onKeyEvent", &JSInteractableView::JsOnKey);
@@ -805,6 +806,32 @@ void JSTextPicker::SetDefaultPickerItemHeight(const JSCallbackInfo& info)
     }
     TextPickerModel::GetInstance()->SetDefaultPickerItemHeight(height);
 }
+
+void JSTextPicker::SetGradientHeight(const JSCallbackInfo& info)
+{
+    CalcDimension height;
+    auto pickerTheme = GetTheme<PickerTheme>();
+    if (info[0]->IsNull() || info[0]->IsUndefined()) {
+        if (pickerTheme) {
+            height = pickerTheme->GetGradientHeight();
+        } else {
+            height = 0.0_vp;
+        }
+    }
+    if (info.Length() >= 1) {
+        ConvertFromJSValueNG(info[0], height);
+        if ((height.Unit() == DimensionUnit::PERCENT) &&
+            ((height.Value() > 1.0f) || (height.Value() < 0.0f))) {
+            if (pickerTheme) {
+                height = pickerTheme->GetGradientHeight();
+            } else {
+                height = 0.0_vp;
+            }
+        }
+    }
+    TextPickerModel::GetInstance()->SetGradientHeight(height);
+}
+
 void JSTextPicker::SetCanLoop(const JSCallbackInfo& info)
 {
     bool value = true;
