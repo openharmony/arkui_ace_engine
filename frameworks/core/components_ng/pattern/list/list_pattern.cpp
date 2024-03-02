c2591e983bf3e14d517f9fe197dec7c574602a6c
lijianing <lijianing7@huawei.com>
2024-03-02 11:53:19 +0800

    修复List嵌套滚动时判断是否到底问题
    Signed-off-by: lijianing <lijianing7@huawei.com>
    Change-Id: Ib7573b3d9c4eb7b5db53d12b607a9ba653bc1c07

--- frameworks/core/components_ng/pattern/list/list_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_pattern.cpp
@@ -722,7 +722,9 @@ bool ListPattern::UpdateCurrentOffset(float offset, int32_t source)
     if (source == SCROLL_FROM_BAR || source == SCROLL_FROM_BAR_FLING) {
         isNeedCheckOffset_ = true;
     }
-    MarkDirtyNodeSelf();
+    if (!NearZero(offset)) {
+        MarkDirtyNodeSelf();
+    }
     if (!IsOutOfBoundary() || !scrollable_) {
         FireOnWillScroll(currentDelta_ - lastDelta);
         return true;
