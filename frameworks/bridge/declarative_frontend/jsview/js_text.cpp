9e0a492918b4cfd89998fc2377d6fa0c8d950e5d
zhouyan <zhouyan162@huawei.com>
2024-03-07 12:07:36 +0000

    fixed ed450a0 from https://gitee.com/earnk/arkui_ace_engine/pulls/28451 fontSize属性接口减少耗时
    Signed-off-by: zhouyan <zhouyan162@huawei.com>
    Change-Id: Ia7748c9971666ba3388e1bcb44113e238d97678b

--- frameworks/bridge/declarative_frontend/jsview/js_text.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_text.cpp
@@ -149,18 +149,15 @@ void JSText::SetFontSize(const JSCallbackInfo& info)
     if (info.Length() < 1) {
         return;
     }
-    auto pipelineContext = PipelineBase::GetCurrentContext();
-    CHECK_NULL_VOID(pipelineContext);
-    auto theme = pipelineContext->GetTheme<TextTheme>();
-    CHECK_NULL_VOID(theme);
-    CalcDimension fontSize = theme->GetTextStyle().GetFontSize();
-    if (!ParseJsDimensionFpNG(info[0], fontSize, false)) {
+    CalcDimension fontSize;
+    if (!ParseJsDimensionFpNG(info[0], fontSize, false) || fontSize.IsNegative()) {
+        auto pipelineContext = PipelineBase::GetCurrentContext();
+        CHECK_NULL_VOID(pipelineContext);
+        auto theme = pipelineContext->GetTheme<TextTheme>();
+        CHECK_NULL_VOID(theme);
         fontSize = theme->GetTextStyle().GetFontSize();
         TextModel::GetInstance()->SetFontSize(fontSize);
         return;
-    }
-    if (fontSize.IsNegative()) {
-        fontSize = theme->GetTextStyle().GetFontSize();
     }
     TextModel::GetInstance()->SetFontSize(fontSize);
 }
