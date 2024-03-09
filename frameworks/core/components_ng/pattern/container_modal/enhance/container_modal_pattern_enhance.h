66d7685590f0fdf7aa7662771aff681a413e332c
chengzhen <chengzhen25@huawei.com>
2024-03-09 16:03:17 +0800

    Description:标题栏不显示 Sig: SIG_ApplicationFramework Feature or Bugfix:Feature Binary Source:No TDD:Pass XTS:不涉及 预测试:Pass
    Signed-off-by: chengzhen <chengzhen25@huawei.com>

--- frameworks/core/components_ng/pattern/container_modal/enhance/container_modal_pattern_enhance.h
+++ frameworks/core/components_ng/pattern/container_modal/enhance/container_modal_pattern_enhance.h
@@ -41,8 +41,6 @@ protected:
 
     void ChangeTitleButtonIcon(
         const RefPtr<FrameNode>& buttonNode, InternalResource::ResourceId icon, bool isFocus) override;
-
-    bool CanHideFloatingTitle() override;
 };
 } // namespace OHOS::Ace::NG
 #endif // FOUNDATION_ACE_FRAMEWORKS_CORE_COMPONENTS_NG_PATTERNS_CONTAINER_MODAL_CONTAINER_MODAL_PATTERN_ENHANCE_H
