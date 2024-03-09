6937e1c20f0ff082a211ebd1915363449b0be21b
huyanchao@huawei.com <huyanchao@huawei.com>
2024-03-09 17:43:27 +0800

    window buffer
    Signed-off-by: huyanchao@huawei.com <huyanchao@huawei.com>

--- frameworks/core/components_ng/pattern/xcomponent/xcomponent_pattern.cpp
+++ frameworks/core/components_ng/pattern/xcomponent/xcomponent_pattern.cpp
@@ -35,6 +35,8 @@
 #endif
 #ifdef ENABLE_ROSEN_BACKEND
 #include "transaction/rs_transaction_proxy.h"
+#include "ui/rs_surface_node_operation.h"
+#include "core/components_ng/render/adapter/rosen_render_context.h"
 #endif
 
 #include "core/components_ng/event/input_event.h"
@@ -281,6 +283,33 @@ void XComponentPattern::OnAreaChangedInner()
 #endif
 }
 
+void XComponentPattern::SetSurfaceNodeToGraphic()
+{
+#ifdef ENABLE_ROSEN_BACKEND
+    auto host = GetHost();
+    CHECK_NULL_VOID(host);
+    auto renderContext = host->GetRenderContext();
+    CHECK_NULL_VOID(renderContext);
+    auto rosenRenderContext = AceType::DynamicCast<RosenRenderContext>(renderContext);
+    CHECK_NULL_VOID(rosenRenderContext);
+    std::shared_ptr<Rosen::RSNode> parentNode = rosenRenderContext->GetRSNode();
+    CHECK_NULL_VOID(parentNode);
+    RectF canvasRect = rosenRenderContext->GetPropertyOfPosition();
+
+    CHECK_NULL_VOID(renderContextForSurface_);
+    auto context = AceType::DynamicCast<RosenRenderContext>(renderContextForSurface_);
+    CHECK_NULL_VOID(context);
+    std::shared_ptr<Rosen::RSNode> rsNode = context->GetRSNode();
+    CHECK_NULL_VOID(rsNode);
+    std::shared_ptr<Rosen::RSSurfaceNode> rsSurfaceNode =
+        std::static_pointer_cast<Rosen::RSSurfaceNode>(rsNode);
+    CHECK_NULL_VOID(rsSurfaceNode);
+
+    Rosen::RSSurfaceNodeOperation::GetInstance().ProcessRSSurfaceNode(GetId(), parentNode->GetId(),
+        canvasRect.GetX(), canvasRect.GetY(), rsSurfaceNode);
+#endif
+}
+
 void XComponentPattern::OnRebuildFrame()
 {
     if (type_ != XComponentType::SURFACE) {
@@ -295,6 +324,7 @@ void XComponentPattern::OnRebuildFrame()
     CHECK_NULL_VOID(renderContext);
     CHECK_NULL_VOID(handlingSurfaceRenderContext_);
     renderContext->AddChild(handlingSurfaceRenderContext_, 0);
+    SetSurfaceNodeToGraphic();
 }
 
 void XComponentPattern::OnDetachFromFrameNode(FrameNode* frameNode)
