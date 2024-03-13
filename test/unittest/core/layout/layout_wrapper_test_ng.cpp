8002648412184f87b4668d546ffe70bcf44fb1b4
bizhenhang <bizhenhang@huawei.com>
2024-03-13 07:33:35 +0000

    fix layoutwrapper test
    Signed-off-by: bizhenhang <bizhenhang@huawei.com>
    Change-Id: Idb1280e7e2232db0152b53825684f0723d3a3d63

--- test/unittest/core/layout/layout_wrapper_test_ng.cpp
+++ test/unittest/core/layout/layout_wrapper_test_ng.cpp
@@ -1532,7 +1532,7 @@ HWTEST_F(LayoutWrapperTestNg, LayoutWrapperTest040, TestSize.Level1)
     // recreate wrapper to simulate next layout
     auto wrapper2 = AceType::MakeRefPtr<LayoutWrapperNode>(host, wrapper->geometryNode_, host->GetLayoutProperty());
     wrapper2->RestoreGeoState();
-    EXPECT_EQ(manager->GetGeoRestoreNodes().size(), 0UL);
+    EXPECT_EQ(manager->GetGeoRestoreNodes().size(), 1UL);
     EXPECT_FALSE(wrapper2->geometryNode_->previousState_);
     EXPECT_EQ(wrapper2->geometryNode_->GetFrameOffset(), OffsetF(0, 0));
 }
