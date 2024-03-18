008a8b331b3147e424dc40abb16ead14b4888a50
lushi1202 <lushi@huawei.com>
2024-03-18 20:37:02 +0800

    ace_forward_compatibility hilog to HILOG_IMPL
    Signed-off-by: lushi1202 <lushi@huawei.com>
    Change-Id: I5b235e6069f3d0621ddf00eacc6b5e6f67569d8f

--- interfaces/inner_api/ace/arkui_log.h
+++ interfaces/inner_api/ace/arkui_log.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,24 +13,18 @@
  * limitations under the License.
  */
 
-#ifndef OHOS_ACE_UI_SERVICE_HILOG_H
-#define OHOS_ACE_UI_SERVICE_HILOG_H
+#ifndef INTERFACES_INNER_API_ACE_ARKUI_LOG_H
+#define INTERFACES_INNER_API_ACE_ARKUI_LOG_H
 
 #include "hilog/log.h"
 
-#define UISERVICE_LOG_DOMAIN 0xD003935
-#define UISERVICE_LOG_TAG "AceUIService"
-
-#define UISERVICE_FILENAME (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
-#define ACE_FMT_PREFIX "[%{public}s(%{public}s:%{public}d)]"
-
 #define PRINT_LOG(level, fmt, ...) \
-    HILOG_IMPL(LOG_CORE, LOG_##level, UISERVICE_LOG_DOMAIN, UISERVICE_LOG_TAG, \
-        ACE_FMT_PREFIX fmt, UISERVICE_FILENAME, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+    HILOG_IMPL(LOG_CORE, LOG_##level, 0xD003900, "ACE_UIContent", "[%{public}s:%{public}d]" fmt, \
+        __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #define LOGE(fmt, ...) PRINT_LOG(ERROR, fmt, ##__VA_ARGS__)
 #define LOGW(fmt, ...) PRINT_LOG(WARN, fmt, ##__VA_ARGS__)
 #define LOGI(fmt, ...) PRINT_LOG(INFO, fmt, ##__VA_ARGS__)
 #define LOGD(fmt, ...) PRINT_LOG(DEBUG, fmt, ##__VA_ARGS__)
 
-#endif
+#endif // INTERFACES_INNER_API_ACE_ARKUI_LOG_H
