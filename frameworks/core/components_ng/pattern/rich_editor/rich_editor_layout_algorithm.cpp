41e1f771db6a2d8a920e6b2b34496bd329787600
zyx <zhangyingxiang2@huawei.com>
2024-03-19 22:06:37 +0800

    修复placeholder超出content手柄不显示
    Signed-off-by: zyx <zhangyingxiang2@huawei.com>
    Change-Id: I7a0f1e0725863d32282e6c264185356ffc3d09e4

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_layout_algorithm.cpp
@@ -119,7 +119,7 @@ std::optional<SizeF> RichEditorLayoutAlgorithm::MeasureContent(
     if (!res.IsPositive()) {
         return std::nullopt;
     }
-    richTextRect_.SetSize(SizeF(res.Width(), textHeight));
+    UpdateRichTextRect(res, textHeight, layoutWrapper);
     auto contentHeight = res.Height();
     if (contentConstraint.selfIdealSize.Height().has_value()) {
         contentHeight = std::min(contentHeight, contentConstraint.selfIdealSize.Height().value());
@@ -127,6 +127,20 @@ std::optional<SizeF> RichEditorLayoutAlgorithm::MeasureContent(
         contentHeight = std::min(contentHeight, contentConstraint.maxSize.Height());
     }
     return SizeF(res.Width(), contentHeight);
+}
+
+void RichEditorLayoutAlgorithm::UpdateRichTextRect(
+    const SizeF& res, const float& textHeight, LayoutWrapper* layoutWrapper)
+{
+    auto host = layoutWrapper->GetHostNode();
+    CHECK_NULL_VOID(host);
+    auto pattern = host->GetPattern<RichEditorPattern>();
+    CHECK_NULL_VOID(pattern);
+    if (pattern->IsShowPlaceholder()) {
+        richTextRect_.SetSize(SizeF(0.0f, 0.0f));
+    } else {
+        richTextRect_.SetSize(SizeF(res.Width(), textHeight));
+    }
 }
 
 void RichEditorLayoutAlgorithm::SetPlaceholder(LayoutWrapper* layoutWrapper)
