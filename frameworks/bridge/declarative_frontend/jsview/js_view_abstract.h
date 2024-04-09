ea478b4cd3adf6f01791fa188d20badb7bb15b49
yangziyong <nsyangziyong@huawei.com>
2024-04-09 09:26:44 +0800

    组合键支持方向键及Tab键暴露
    Signed-off-by: yangziyong <nsyangziyong@huawei.com>

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.h
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.h
@@ -493,7 +493,12 @@ public:
             {FunctionKey::F9, "F9"},
             {FunctionKey::F10, "F10"},
             {FunctionKey::F11, "F11"},
-            {FunctionKey::F12, "F12"}
+            {FunctionKey::F12, "F12"},
+            {FunctionKey::TAB, "TAB"},
+            {FunctionKey::DPAD_UP, "DPAD_UP"},
+            {FunctionKey::DPAD_DOWN, "DPAD_DOWN"},
+            {FunctionKey::DPAD_LEFT, "DPAD_LEFT"},
+            {FunctionKey::DPAD_RIGHT, "DPAD_RIGHT"}
         };
         auto result = keyNameMap.find(functionkey);
         return (result != keyNameMap.end()) ? result->second : std::string();
