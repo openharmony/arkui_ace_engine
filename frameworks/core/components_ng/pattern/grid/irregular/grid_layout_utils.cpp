3fdb4d7b180032832bc807edb3213b6e557c2a9b
Tianer Zhou <zhoutianer@huawei.com>
2024-04-09 10:24:09 +0800

    handle negative size
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: Ibd993662a6c22949af8a06fb2d7410cb77fd3359

--- frameworks/core/components_ng/pattern/grid/irregular/grid_layout_utils.cpp
+++ frameworks/core/components_ng/pattern/grid/irregular/grid_layout_utils.cpp
@@ -40,6 +40,8 @@ GridItemSize GridLayoutUtils::GetItemSize(const GridLayoutInfo* info, const Layo
     if (size.columns > info->crossCount_) {
         size.columns = info->crossCount_;
     }
+    size.columns = std::min(1, size.columns);
+    size.rows = std::min(1, size.rows);
     return size;
 }
 } // namespace OHOS::Ace::NG
