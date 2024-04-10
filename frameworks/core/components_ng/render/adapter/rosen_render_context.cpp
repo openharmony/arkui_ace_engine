6cd01236a648ecdc3d82a04b9f2c1ef1088bcb72
DaTeng <fengdateng@huawei.com>
2024-04-10 12:12:03 +0000

    添加单位转换
    Signed-off-by: DaTeng <fengdateng@huawei.com>
    Change-Id: I728ade89259c0554c75a5160b9d9269206d007be

--- frameworks/core/components_ng/render/adapter/rosen_render_context.cpp
+++ frameworks/core/components_ng/render/adapter/rosen_render_context.cpp
@@ -37,6 +37,7 @@
 #include "render_service_client/core/ui/rs_surface_node.h"
 #include "rosen_render_context.h"
 
+#include "base/geometry/calc_dimension.h"
 #include "base/geometry/dimension.h"
 #include "base/geometry/matrix4.h"
 #include "base/geometry/ng/offset_t.h"
@@ -625,7 +626,17 @@ void RosenRenderContext::OnForegroundColorUpdate(const Color& value)
 void RosenRenderContext::OnForegroundEffectUpdate(float radius)
 {
     CHECK_NULL_VOID(rsNode_);
-    rsNode_->SetForegroundEffectRadius(radius);
+    auto context = PipelineBase::GetCurrentContext();
+    CHECK_NULL_VOID(context);
+    CalcDimension value;
+    value.SetValue(static_cast<double>(radius));
+    float radiusPx = context->NormalizeToPx(value);
+#ifndef USE_ROSEN_DRAWING
+    float foreRadius = SkiaDecorationPainter::ConvertRadiusToSigma(radiusPx);
+#else
+    float foreRadius = DrawingDecorationPainter::ConvertRadiusToSigma(radiusPx);
+#endif
+    rsNode_->SetForegroundEffectRadius(foreRadius);
     RequestNextFrame();
 }
 
