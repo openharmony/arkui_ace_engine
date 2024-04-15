9548b32f87abe172a899d4e95af8d66d12557c09
hongzexuan <hongzexuan@huawei.com>
2024-04-15 10:56:33 +0800

    scrollbar and scrollable add log
    Signed-off-by: hongzexuan <hongzexuan@huawei.com>
    Change-Id: Ifbf25111630568d0f4e4d8899ec193d4981b5bf4

--- adapter/ohos/osal/log_wrapper.cpp
+++ adapter/ohos/osal/log_wrapper.cpp
@@ -114,6 +114,7 @@ const std::unordered_map<AceLogTag, const char*> g_DOMAIN_CONTENTS_MAP = {
     { AceLogTag::ACE_STATE_MGMT, "AceStateMgmt" },
     { AceLogTag::ACE_SHEET, "AceSheet" },
     { AceLogTag::ACE_CANVAS_COMPONENT, "AceCanvasComponent" },
+    { AceLogTag::ACE_SCROLL_BAR, "AceScrollBar" },
 };
 // initial static member object
 LogLevel LogWrapper::level_ = LogLevel::DEBUG;
