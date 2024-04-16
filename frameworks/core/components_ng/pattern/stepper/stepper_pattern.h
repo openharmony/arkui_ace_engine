b444707db8521d92ea8c9e35d7034a12246b2952
sunjiakun <sunjiakun5@huawei.com>
2024-04-16 11:05:56 +0800

    stepper code
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

--- frameworks/core/components_ng/pattern/stepper/stepper_pattern.h
+++ frameworks/core/components_ng/pattern/stepper/stepper_pattern.h
@@ -103,7 +103,8 @@ public:
     void OnModifyDone() override;
 
     void OnColorConfigurationUpdate() override;
-
+    void OnLanguageConfigurationUpdate() override;
+  
 private:
     void OnAttachToFrameNode() override;
     int32_t TotalCount() const;
@@ -124,7 +125,9 @@ private:
     void SetAccessibilityAction();
     static void ButtonSkipColorConfigurationUpdate(const RefPtr<FrameNode>& buttonNode);
     WeakPtr<FocusHub> GetFocusNode(FocusStep step, const WeakPtr<FocusHub>& currentFocusNode);
-
+    bool isRightToLeft_ = false;
+    RefPtr<FrameNode> leftImage_;
+    RefPtr<FrameNode> rightImage_;
     int32_t index_ = 0;
     int32_t maxIndex_ = 0;
     std::shared_ptr<ChangeEvent> swiperChangeEvent_;
