a4d9c9cbdaa7e6fb7f634c03abc140d2b054639a
aryawang <wangcaihui@huawei.com>
2024-04-22 10:55:07 +0000

    fixed 9e69ab7 from https://gitee.com/aryawang/arkui_ace_engine/pulls/31259 NDK C-Api OH_ArkUI_GestureEvent_GetRawInputEvent 对接
    Signed-off-by: aryawang <wangcaihui@huawei.com>
    Change-Id: I80e37ca809b92f00157bcaa9a243a6de4a5212dc

--- frameworks/core/components_ng/gestures/recognizers/pinch_recognizer.cpp
+++ frameworks/core/components_ng/gestures/recognizers/pinch_recognizer.cpp
@@ -394,6 +394,7 @@ void PinchRecognizer::SendCallbackMsg(const std::unique_ptr<GestureEventFunc>& c
             info.SetTiltY(lastTouchEvent_.tiltY.value());
         }
         info.SetSourceTool(lastTouchEvent_.sourceTool);
+        info.SetPointerEvent(lastTouchEvent_.pointerEvent);
         // callback may be overwritten in its invoke so we copy it first
         auto callbackFunction = *callback;
         callbackFunction(info);
