d6d8b457c983627a33c4f06c03afd36dc52d2194
zhangrongjie <zhangrongjie4@huawei.com>
2024-04-23 07:27:17 +0000

    Scroll and Waterflow add gain
    Signed-off-by: zhangrongjie <zhangrongjie4@huawei.com>
    Change-Id: I47289b01b175d0bbfd166509aecb035beaf565b1

--- frameworks/core/components_ng/pattern/list/list_pattern.h
+++ frameworks/core/components_ng/pattern/list/list_pattern.h
@@ -102,7 +102,7 @@ public:
 
     bool IsScrollable() const override
     {
-        return scrollable_;
+        return isScrollable_;
     }
 
     bool IsAtTop() const override;
@@ -299,7 +299,6 @@ private:
     void CheckScrollable();
     bool IsOutOfBoundary(bool useCurrentDelta = true) override;
     bool OnScrollCallback(float offset, int32_t source) override;
-    void InitScrollableEvent();
     void SetEdgeEffectCallback(const RefPtr<ScrollEdgeEffect>& scrollEffect) override;
     void HandleScrollEffect(float offset);
     void StartDefaultOrCustomSpringMotion(float start, float end, const RefPtr<InterpolatingSpring>& curve);
@@ -366,7 +365,7 @@ private:
     std::optional<float> predictSnapOffset_;
     std::optional<float> predictSnapEndPos_;
     ScrollAlign scrollAlign_ = ScrollAlign::START;
-    bool scrollable_ = true;
+    bool isScrollable_ = true;
     bool paintStateFlag_ = false;
     bool isFramePaintStateValid_ = false;
     bool isNeedCheckOffset_ = false;
@@ -394,7 +393,7 @@ private:
 
     RefPtr<SpringMotion> scrollToIndexMotion_;
     RefPtr<SpringMotion> scrollSnapMotion_;
-    RefPtr<Scrollable> scrollableTouchEvent_;
+    RefPtr<Scrollable> scrollable_;
 
     bool isScrollEnd_ = false;
     bool needReEstimateOffset_ = false;
