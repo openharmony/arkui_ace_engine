f23dd4bb4203f7f4ed2571e6327d7d142d8aeb63
Klein-CK <chenke76@huawei.com>
2024-04-23 15:44:12 +0800

    Fix the appFreeze of center in control, the vsyncRate is equal zero. Signed-off-by: Klein-CK<chenke76@huawei.com>
    Change-Id: Icacdaf091f1b4ba67f0c0d68d714f527210070bb

--- frameworks/core/components_ng/manager/display_sync/ui_display_sync.cpp
+++ frameworks/core/components_ng/manager/display_sync/ui_display_sync.cpp
@@ -294,7 +294,7 @@ int32_t UIDisplaySync::SearchMatchedRate(int32_t vsyncRate, int32_t iterCount)
         return FindMatchedRefreshRate(vsyncRate, data_->rateRange_->preferred_);
     }
 
-    if (iterCount == 0) {
+    if (iterCount == 0 || vsyncRate == 0) {
         return vsyncRate;
     }
 
