daca118a3f3cd56cdf1f39ecf5dc4db488ccffa8
zhoulianhai <zhoulianhai@huawei.com>
2024-04-25 21:33:54 +0800

    Badge初始状态细线修复
    Signed-off-by: zhoulianhai <zhoulianhai@huawei.com>

--- frameworks/core/components_ng/pattern/badge/badge_pattern.h
+++ frameworks/core/components_ng/pattern/badge/badge_pattern.h
@@ -53,9 +53,9 @@ public:
 
 private:
     void OnModifyDone() override;
-    void BadgeAnimation(RefPtr<FrameNode>& frameNode, bool isShowBadge);
+    void BadgeAnimation(RefPtr<FrameNode>& frameNode, bool isShowBadge) const;
     int32_t textNodeId_ = -2;
-    bool lastState_ = false;
+    bool lastBadgeVisible_ = false;
     const int32_t duration_ = 200;
 };
 } // namespace OHOS::Ace::NG
