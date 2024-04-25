6c015687038d35c6433138467d0c592c971f8da3
zhouchaobo <zhouchaobo@huawei.com>
2024-04-25 11:18:06 +0800

    swipe speed bug fix
    Signed-off-by: zhouchaobo <zhouchaobo@huawei.com>
    Change-Id: I89a7d3cb30ffee055c67a30dc0ab3c5fc8112d59

--- frameworks/core/components_ng/gestures/recognizers/swipe_recognizer.cpp
+++ frameworks/core/components_ng/gestures/recognizers/swipe_recognizer.cpp
@@ -165,7 +165,7 @@ void SwipeRecognizer::HandleTouchUpEvent(const TouchEvent& event)
         // nanoseconds duration to seconds.
         std::chrono::duration<double> duration = event.time - touchDownTime_;
         auto seconds = duration.count();
-        resultSpeed_ = offset.GetDistance() / seconds;
+        resultSpeed_ = LessOrEqual(seconds, 0.0) ? 0.0 : offset.GetDistance() / seconds;
         if (resultSpeed_ < speed_) {
             if (currentFingers_ - 1 + static_cast<int32_t>(matchedTouch_.size()) < fingers_) {
                 TAG_LOGI(AceLogTag::ACE_GESTURE, "The result speed %{public}f is less than duration %{public}f",
@@ -211,7 +211,9 @@ void SwipeRecognizer::HandleTouchUpEvent(const AxisEvent& event)
         auto duration = event.time - touchDownTime_;
         auto duration_ms =
             std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1, RATIO_US_TO_MS>>>(duration);
-        resultSpeed_ = axisOffset_.GetDistance() / duration_ms.count() * RATIO_MS_TO_S;
+        resultSpeed_ = LessOrEqual(duration_ms.count(), 0.0)
+                           ? 0.0
+                           : axisOffset_.GetDistance() / duration_ms.count() * RATIO_MS_TO_S;
         if (resultSpeed_ < speed_) {
             Adjudicate(AceType::Claim(this), GestureDisposal::REJECT);
         } else {
