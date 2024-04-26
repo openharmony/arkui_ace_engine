f6e2c8cd6a5a2dcf5576afc825066d9daee7bead
wangtao <wangtao487@huawei.com>
2024-04-26 13:22:02 +0000

    TextArea 全选无法选择换行符
    Signed-off-by: wangtao <wangtao487@huawei.com>
    Change-Id: Id96bca241f83cfdf845e8975fc8fe359ad64c4ea

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -1214,7 +1214,7 @@ void TextFieldPattern::HandleOnSelectAll(bool isKeyEvent, bool inlineStyle)
         }
         return;
     }
-    ProcessOverlay({ .animation = true });
+    selectOverlay_->ProcessSelectAllOverlay({ .animation = true });
 }
 
 void TextFieldPattern::HandleOnCopy(bool isUsingExternalKeyboard)
