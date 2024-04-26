4bbeb9211834de354294ebe286bbbcac54e10c00
laoyitong <laoyitong@huawei.com>
2024-04-26 15:06:04 +0800

    test
    Signed-off-by: laoyitong <laoyitong@huawei.com>
    Change-Id: I2fba32499fd2f8ae4ee719e0811b2bc86114f34e

--- frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
+++ frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
@@ -4566,7 +4566,7 @@ void OverlayManager::CloseUIExtensionMenu(const std::function<void(const std::st
 
 void OverlayManager::CreateOverlayNode()
 {
-    TAG_LOGD(AceLogTag::ACE_OVERLAY, "create overlay node enter");
+    TAG_LOGD(AceLogTag::ACE_OVERLAY, "create overlay node enter lyt");
     if (overlayNode_) {
         return;
     }
