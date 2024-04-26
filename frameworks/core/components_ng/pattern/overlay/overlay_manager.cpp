d372edca290d92f10055a538bda004913cf8c8ff
miaoyu <miaoyu6@huawei.com>
2024-04-26 09:59:52 +0000

    [bugfix]:[修复menuMap没有判断导致报错问题]
    Signed-off-by: miaoyu <miaoyu6@huawei.com>
    Change-Id: I132bbb2cb578e1f7a6c91371ad19aa5c51d750c9

--- frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
+++ frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
@@ -1569,7 +1569,7 @@ void OverlayManager::DeleteMenu(int32_t targetId)
             RemoveMenuNotInSubWindow(WeakClaim(RawPtr(node)), rootNodeWeak_, WeakClaim(this));
         }
     }
-    menuMap_.erase(it);
+    EraseMenuInfo(targetId);
 }
 
 void OverlayManager::CleanMenuInSubWindowWithAnimation()
