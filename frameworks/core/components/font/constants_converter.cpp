88498febdee6f53416dc0063465dc8e1c648a3e1
zhao1d <zhaolidong4@huawei.com>
2024-04-28 10:28:44 +0800

    修复字重问题
    Signed-off-by: zhao1d <zhaolidong4@huawei.com>
    Change-Id: I5bf7a3ff0442509320c906efa12312ee4f86ed3f

--- frameworks/core/components/font/constants_converter.cpp
+++ frameworks/core/components/font/constants_converter.cpp
@@ -412,6 +412,7 @@ Rosen::TextDecorationStyle ConvertTxtTextDecorationStyle(TextDecorationStyle tex
 void ConvertTxtStyle(const TextStyle& textStyle, const WeakPtr<PipelineBase>& context, txt::TextStyle& txtStyle)
 {
     txtStyle.color = ConvertSkColor(textStyle.GetTextColor());
+    txtStyle.font_weight = ConvertTxtFontWeight(textStyle.GetFontWeight());
     auto fontWeightValue = (static_cast<int32_t>(
             ConvertTxtFontWeight(textStyle.GetFontWeight())) + 1) * DEFAULT_MULTIPLE;
     auto pipelineContext = context.Upgrade();
@@ -604,6 +605,7 @@ void ConvertTxtStyle(const TextStyle& textStyle, Rosen::TextStyle& txtStyle)
 void ConvertTxtStyle(const TextStyle& textStyle, const WeakPtr<PipelineBase>& context, Rosen::TextStyle& txtStyle)
 {
     txtStyle.color = ConvertSkColor(textStyle.GetTextColor());
+    txtStyle.fontWeight = ConvertTxtFontWeight(textStyle.GetFontWeight());
     auto fontWeightValue = (static_cast<int32_t>(
             ConvertTxtFontWeight(textStyle.GetFontWeight())) + 1) * DEFAULT_MULTIPLE;
     auto pipelineContext = context.Upgrade();
