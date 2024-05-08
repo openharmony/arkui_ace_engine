c00a71be9e5c8c040b92d66ad9c2c331f8d45d2e
jiangzhijun8 <jiangzhijun7@huawei.com>
2024-05-08 16:51:33 +0800

    toggle组件支持镜像
    Signed-off-by: jiangzhijun8 <jiangzhijun7@huawei.com>

--- frameworks/core/components_ng/pattern/toggle/switch_modifier.h
+++ frameworks/core/components_ng/pattern/toggle/switch_modifier.h
@@ -71,10 +71,12 @@ public:
         if (!actualSize_.IsPositive()) {
             return;
         }
+        bool isRtl = direction_ == TextDirection::AUTO ? AceApplicationInfo::GetInstance().IsRightToLeft()
+                                                   : direction_ == TextDirection::RTL;
         // Animation is not displayed when created for the first time.
         if (isFirstCreated_) {
             animatableBoardColor_->Set(isSelect_->Get() ? LinearColor(userActiveColor_) : LinearColor(inactiveColor_));
-            if (direction_ == TextDirection::RTL) {
+            if (isRtl) {
                 pointOffset_->Set(isSelect_->Get() ? 0.0f : actualSize_.Width() - actualSize_.Height());
             } else {
                 pointOffset_->Set(isSelect_->Get() ? actualSize_.Width() - actualSize_.Height() : 0.0f);
@@ -93,7 +95,7 @@ public:
         pointOption.SetCurve(Curves::FAST_OUT_SLOW_IN);
         float newPointOffset = 0.0f;
         if (!isDragEvent_) {
-            if (direction_ == TextDirection::RTL) {
+            if (isRtl) {
                 newPointOffset = isSelect_->Get() ? 0.0f : actualSize_.Width() - actualSize_.Height();
             } else {
                 newPointOffset = isSelect_->Get() ? actualSize_.Width() - actualSize_.Height() : 0.0f;
