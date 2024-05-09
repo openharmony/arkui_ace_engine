4a9dc3f2e78f34d9e96fd15a55eba142e0acb299
yangziyong <nsyangziyong@huawei.com>
2024-05-09 14:30:45 +0800

    删除冗余代码
    Signed-off-by: yangziyong <nsyangziyong@huawei.com>
    Change-Id: Ia6dc3d164d33bf1411c647c58bc3037655dd5da6

--- frameworks/core/components_ng/pattern/scroll_bar/scroll_bar_pattern.cpp
+++ frameworks/core/components_ng/pattern/scroll_bar/scroll_bar_pattern.cpp
@@ -204,14 +204,6 @@ void ScrollBarPattern::SetScrollBar(DisplayMode displayMode)
     }
 }
 
-void ScrollBarPattern::SetScrollProperties(const RefPtr<LayoutWrapper>& dirty)
-{
-    auto scrollBarPattern = AceType::DynamicCast<ScrollBarPattern>(dirty->GetHostNode()->GetPattern());
-    CHECK_NULL_VOID(scrollBarPattern);
-    scrollableNodeOffset_ = scrollBarPattern->GetScrollableNodeOffset();
-    scrollableDistance_ = scrollBarPattern->GetScrollableDistance();
-}
-
 void ScrollBarPattern::HandleScrollBarOutBoundary(float scrollBarOutBoundaryExtent)
 {
     CHECK_NULL_VOID(scrollBar_ && scrollBar_->NeedScrollBar());
@@ -304,7 +296,6 @@ bool ScrollBarPattern::OnDirtyLayoutWrapperSwap(const RefPtr<LayoutWrapper>& dir
     }
     bool updateFlag = false;
     if (!HasChild() && Container::GreatOrEqualAPITargetVersion(PlatformVersion::VERSION_TWELVE)) {
-        SetScrollProperties(dirty);
         UpdateScrollBarOffset();
         updateFlag = true;
     } else {
