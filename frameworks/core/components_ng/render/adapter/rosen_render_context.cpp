d5eedbff01f4d1a377574a1cb176ee1502494715
b30045777 <baoguoqiang3@huawei.com>
2024-05-09 21:23:14 +0800

    【RichEditor】RN文本二次布局接口适配
    Signed-off-by: b30045777 <baoguoqiang3@huawei.com>
    Change-Id: Ie7b352f618cc6d1317db0998f85c400e7ddbab2f

--- frameworks/core/components_ng/render/adapter/rosen_render_context.cpp
+++ frameworks/core/components_ng/render/adapter/rosen_render_context.cpp
@@ -3339,7 +3339,7 @@ void RosenRenderContext::FlushContentModifier(const RefPtr<Modifier>& modifier)
             std::make_shared<Rosen::RectF>(rect->GetX(), rect->GetY(), rect->Width(), rect->Height());
         UpdateDrawRegion(DRAW_REGION_CONTENT_MODIFIER_INDEX, overlayRect);
     }
-    rsNode_->SetIsCustomTextType(contentModifier->GetIsCustomFont());
+    rsNode_->SetIsCustomTypeface(contentModifier->GetIsCustomFont());
     rsNode_->AddModifier(modifierAdapter);
     modifierAdapter->AttachProperties();
 }
@@ -4485,7 +4485,7 @@ void RosenRenderContext::PaintOverlayText()
             overlayRect = std::make_shared<Rosen::RectF>(overlayOffset.GetX(), overlayOffset.GetY(),
                 std::max(paragraphSize.Width(), paintRect.Width()),
                 std::max(paragraphSize.Height(), paintRect.Height()));
-            rsNode_->SetIsCustomTextType(modifier_->IsCustomFont());
+            rsNode_->SetIsCustomTypeface(modifier_->IsCustomFont());
             UpdateDrawRegion(DRAW_REGION_OVERLAY_TEXT_MODIFIER_INDEX, overlayRect);
         } else {
             modifier_ = std::make_shared<OverlayTextModifier>();
@@ -4496,7 +4496,7 @@ void RosenRenderContext::PaintOverlayText()
             overlayRect = std::make_shared<Rosen::RectF>(overlayOffset.GetX(), overlayOffset.GetY(),
                 std::max(paragraphSize.Width(), paintRect.Width()),
                 std::max(paragraphSize.Height(), paintRect.Height()));
-            rsNode_->SetIsCustomTextType(modifier_->IsCustomFont());
+            rsNode_->SetIsCustomTypeface(modifier_->IsCustomFont());
             UpdateDrawRegion(DRAW_REGION_OVERLAY_TEXT_MODIFIER_INDEX, overlayRect);
         }
     }
