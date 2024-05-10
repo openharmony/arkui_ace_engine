97a857a6210bc4a7b7c119fe862dec222746b389
huqingyun <huqingyun1@huawei.com>
2024-05-10 07:26:01 +0000

    fixed e8d5d55 from https://gitee.com/Yun-xier/arkui_ace_engine/pulls/32574 C-API: 新增focusOnTouch通用属性
    Signed-off-by: huqingyun <huqingyun1@huawei.com>
    Change-Id: I9783e775f02f5930900d1d13b255c154a5f27511

--- frameworks/core/interfaces/arkoala/arkoala_api.h
+++ frameworks/core/interfaces/arkoala/arkoala_api.h
@@ -1402,6 +1402,7 @@ struct ArkUICommonModifier {
     void (*setLayoutRect)(ArkUINodeHandle node, ArkUI_Int32* values);
     void (*getLayoutRect)(ArkUINodeHandle node, ArkUI_Int32* values);
     void (*resetLayoutRect)(ArkUINodeHandle node);
+    ArkUI_Bool (*getFocusOnTouch)(ArkUINodeHandle node);
 };
 
 struct ArkUICommonShapeModifier {
