803613e48972056f3645770149738e1e0ede4175
z00797037 <zhangzecong@huawei.com>
2024-05-13 14:54:00 +0800

    [richEditor]图文混排TDD用例
    Signed-off-by: z00797037 <zhangzecong@huawei.com>
    Change-Id: Ie1c7fd1a4580de23da389e33d204d1cbd918de99

--- test/unittest/core/pattern/rich_editor/rich_editor_test_ng.cpp
+++ test/unittest/core/pattern/rich_editor/rich_editor_test_ng.cpp
@@ -6518,4 +6518,91 @@ HWTEST_F(RichEditorTestNg, onDraw001, TestSize.Level1)
     //Verify the insertion state symbol magnifying glass
     OnDrawVerify(SelectSpanType::TYPESYMBOLSPAN, INIT_VALUE_1, symbolSpanOptions, localOffset);
 }
+
+/**
+ * @tc.name: StyledString001
+ * @tc.desc: Test the styledString with image.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorTestNg, StyledString001, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create styledString with image
+     */
+    ImageSpanSize size { .width = 50.0_vp, .height = 50.0_vp };
+    BorderRadiusProperty borderRadius;
+    borderRadius.SetRadius(2.0_vp);
+    MarginProperty margins;
+    margins.SetEdges(CalcLength(10.0));
+    PaddingProperty paddings;
+    paddings.SetEdges(CalcLength(5.0));
+    ImageSpanAttribute attr { .size = size,
+        .paddingProp = paddings,
+        .marginProp = margins,
+        .borderRadius = borderRadius,
+        .objectFit = ImageFit::COVER,
+        .verticalAlign = VerticalAlign::BOTTOM };
+    ImageSpanOptions imageOption { .image = "src/icon-1.png", .imageAttribute = attr };
+    auto mutableStr = AceType::MakeRefPtr<MutableSpanString>(imageOption);
+    /**
+     * @tc.steps: step2. get richEditor pattern
+     */
+    ASSERT_NE(richEditorNode_, nullptr);
+    auto richEditorPattern = richEditorNode_->GetPattern<RichEditorPattern>();
+    ASSERT_NE(richEditorPattern, nullptr);
+    /**
+     * @tc.steps: step3. set styledString
+     */
+    richEditorPattern->SetStyledString(mutableStr);
+    EXPECT_EQ(static_cast<int32_t>(richEditorNode_->GetChildren().size()), 1);
+    auto child = richEditorPattern->GetChildByIndex(0);
+    auto imageNode = AceType::DynamicCast<ImageSpanNode>(child);
+    ASSERT_NE(imageNode, nullptr);
+    auto imageLayoutProperty = imageNode->GetLayoutProperty<ImageLayoutProperty>();
+    ASSERT_NE(imageLayoutProperty, nullptr);
+    EXPECT_EQ(imageLayoutProperty->calcLayoutConstraint_->selfIdealSize, size.GetSize());
+    EXPECT_EQ(imageLayoutProperty->GetVerticalAlignValue(), VerticalAlign::BOTTOM);
+    EXPECT_EQ(imageLayoutProperty->GetImageFitValue(), ImageFit::COVER);
+    auto&& padding = imageLayoutProperty->GetPaddingProperty();
+    ASSERT_NE(padding, nullptr);
+    EXPECT_EQ(padding->ToString(), paddings.ToString());
+    auto imageRenderCtx = imageNode->GetRenderContext();
+    ASSERT_NE(imageRenderCtx, nullptr);
+    EXPECT_EQ(imageRenderCtx->GetBorderRadius(), borderRadius);
+}
+
+/**
+ * @tc.name: StyledString002
+ * @tc.desc: Test the styledString with image and text.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorTestNg, StyledString002, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create styledString with image and text
+     */
+    ImageSpanOptions imageOption;
+    auto mutableStr = AceType::MakeRefPtr<MutableSpanString>(imageOption);
+    mutableStr->InsertString(0, "text");
+    /**
+     * @tc.steps: step2. get richEditor pattern
+     */
+    ASSERT_NE(richEditorNode_, nullptr);
+    auto richEditorPattern = richEditorNode_->GetPattern<RichEditorPattern>();
+    ASSERT_NE(richEditorPattern, nullptr);
+    /**
+     * @tc.steps: step3. set styledString
+     */
+    richEditorPattern->SetStyledString(mutableStr);
+    EXPECT_EQ(richEditorNode_->GetChildren().size(), 1);
+    EXPECT_EQ(richEditorPattern->spans_.size(), 2);
+    /**
+     * @tc.steps: step5. insert other image
+     */
+    auto imageSpan = AceType::MakeRefPtr<SpanString>(imageOption);
+    mutableStr->AppendSpanString(imageSpan);
+    richEditorPattern->SetStyledString(mutableStr);
+    EXPECT_EQ(richEditorNode_->GetChildren().size(), 2);
+    EXPECT_EQ(richEditorPattern->spans_.size(), 3);
+}
 } // namespace OHOS::Ace::NG
