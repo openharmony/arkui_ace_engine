685be13fda0342c1ff4f70428b3c0df54097d68e
benb365 <liuyongchang1@huawei.com>
2024-05-15 21:09:04 +0800

    Forbid to paint FORCE_NONE components' focus-box.
    Signed-off-by: benb365 <liuyongchang1@huawei.com>
    Change-Id: Id6d34dc3af482927f6970a45cf978ce74c03a1d9

--- frameworks/core/components_ng/event/focus_hub.cpp
+++ frameworks/core/components_ng/event/focus_hub.cpp
@@ -45,7 +45,9 @@
 namespace OHOS::Ace::NG {
 RefPtr<FocusManager> FocusHub::GetFocusManager() const
 {
-    auto context = NG::PipelineContext::GetCurrentContextSafely();
+    auto frameNode = GetFrameNode();
+    CHECK_NULL_RETURN(frameNode, nullptr);
+    auto context = frameNode->GetContextRefPtr();
     CHECK_NULL_RETURN(context, nullptr);
     auto focusManager = context->GetOrCreateFocusManager();
     return focusManager;
@@ -167,6 +169,10 @@ void FocusHub::DumpFocusNodeTree(int32_t depth)
         if (!focusScopeId_.empty() && (focusPriority_ == FocusPriority::PREVIOUS)) {
             information += (" previous-focus-in-" + focusScopeId_);
         }
+        auto focusMgr = GetFocusManager();
+        if (focusMgr && focusMgr == this) {
+            information += " [Painted]";
+        }
         DumpLog::GetInstance().Print(depth, information, 0);
     }
 }
@@ -195,6 +201,10 @@ void FocusHub::DumpFocusScopeTree(int32_t depth)
         if (!focusScopeId_.empty()) {
             information += GetIsFocusGroup() ? " GroupId:" : " ScopeId:";
             information += focusScopeId_;
+        }
+        auto focusMgr = GetFocusManager();
+        if (focusMgr && focusMgr == this) {
+            information += " [Painted]";
         }
         DumpLog::GetInstance().Print(depth, information, static_cast<int32_t>(focusNodes.size()));
     }
@@ -1431,6 +1441,9 @@ bool FocusHub::PaintAllFocusState()
     }
     if (onPaintFocusStateCallback_) {
         return onPaintFocusStateCallback_();
+    }
+    if (focusStyleType_ != FocusStyleType::NONE) {
+        return false;
     }
 
     // Force paint focus box for the component on the tail of focus-chain.
