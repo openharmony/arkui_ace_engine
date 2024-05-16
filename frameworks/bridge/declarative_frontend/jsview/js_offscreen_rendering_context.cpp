778cc51398c55e753ac68719be32174e214f11a2
WangJiazhen <wangjiazhen4@huawei.com>
2024-05-16 23:12:22 +0800

    修改nativeEngine获得方式
    Signed-off-by: WangJiazhen <wangjiazhen4@huawei.com>

--- frameworks/bridge/declarative_frontend/jsview/js_offscreen_rendering_context.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_offscreen_rendering_context.cpp
@@ -16,6 +16,7 @@
 #include "bridge/declarative_frontend/jsview/js_offscreen_rendering_context.h"
 
 #include "base/utils/utils.h"
+#include "bridge/common/utils/engine_helper.h"
 #include "bridge/declarative_frontend/engine/js_converter.h"
 #include "bridge/declarative_frontend/jsview/models/canvas/offscreen_canvas_rendering_context_2d_model_impl.h"
 #include "core/components_ng/pattern/canvas/offscreen_canvas_rendering_context_2d_model_ng.h"
@@ -191,9 +192,9 @@ void JSOffscreenRenderingContext::Destructor(JSOffscreenRenderingContext* contex
 
 void JSOffscreenRenderingContext::JsTransferToImageBitmap(const JSCallbackInfo& info)
 {
-    auto runtime = std::static_pointer_cast<ArkJSRuntime>(JsiDeclarativeEngineInstance::GetCurrentRuntime());
-    CHECK_NULL_VOID(runtime);
-    NativeEngine* nativeEngine = runtime->GetNativeEngine();
+    auto engine = EngineHelper::GetCurrentEngineSafely();
+    CHECK_NULL_VOID(engine);
+    NativeEngine* nativeEngine = engine->GetNativeEngine();
     CHECK_NULL_VOID(nativeEngine);
     napi_env env = reinterpret_cast<napi_env>(nativeEngine);
     napi_value global = nullptr;
