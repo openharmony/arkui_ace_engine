7f6c0483cb7925c5183f8141ba22fb2a5003ca7f
xi-heqiong <xiheqiong@huawei.com>
2024-05-17 17:18:13 +0800

    fixed b1c2994 from https://gitee.com/xi-heqiong/arkui_ace_engine/pulls/33287 0517_sheet
    Signed-off-by: xi-heqiong <xiheqiong@huawei.com>
    Change-Id: Ib65f04e70a16b85f90c2fda3e9b4ffb9dd9f54f6

--- frameworks/core/pipeline_ng/pipeline_context.cpp
+++ frameworks/core/pipeline_ng/pipeline_context.cpp
@@ -1490,11 +1490,15 @@ PipelineBase::SafeAreaInsets PipelineContext::GetSafeAreaWithoutProcess() const
 void PipelineContext::SyncSafeArea(bool onKeyboard)
 {
     CHECK_NULL_VOID(stageManager_);
-    auto page = stageManager_->GetLastPageWithTransition();
-    if (page) {
-        page->MarkDirtyNode(onKeyboard && !safeAreaManager_->KeyboardSafeAreaEnabled() ? PROPERTY_UPDATE_LAYOUT
+    auto lastPage = stageManager_->GetLastPageWithTransition();
+    auto prevPage = stageManager_->GetPrevPageWithTransition();
+    if (lastPage) {
+        lastPage->MarkDirtyNode(onKeyboard && !safeAreaManager_->KeyboardSafeAreaEnabled() ? PROPERTY_UPDATE_LAYOUT
                                                                                        : PROPERTY_UPDATE_MEASURE);
-        page->GetPattern<PagePattern>()->MarkDirtyOverlay();
+        lastPage->GetPattern<PagePattern>()->MarkDirtyOverlay();
+    }
+    if (prevPage) {
+        prevPage->GetPattern<PagePattern>()->MarkDirtyOverlay();
     }
     SubwindowManager::GetInstance()->MarkDirtyDialogSafeArea();
     if (overlayManager_) {
