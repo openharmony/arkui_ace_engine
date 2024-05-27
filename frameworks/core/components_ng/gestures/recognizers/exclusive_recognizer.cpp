08939d3fc37150f70ca147433cebba6c7390f7fe
zhengqiyi <zhengqiyi2@huawei.com>
2024-05-27 02:46:01 +0000

    enhance clean force
    Signed-off-by: zhengqiyi <zhengqiyi2@huawei.com>

--- frameworks/core/components_ng/gestures/recognizers/exclusive_recognizer.cpp
+++ frameworks/core/components_ng/gestures/recognizers/exclusive_recognizer.cpp
@@ -301,7 +301,9 @@ void ExclusiveRecognizer::CleanRecognizerState()
             child->CleanRecognizerState();
         }
     }
-    if ((refereeState_ == RefereeState::SUCCEED || refereeState_ == RefereeState::FAIL) &&
+    if ((refereeState_ == RefereeState::SUCCEED ||
+        refereeState_ == RefereeState::FAIL ||
+        refereeState_ == RefereeState::DETECTING) &&
         currentFingers_ == 0) {
         refereeState_ = RefereeState::READY;
         disposal_ = GestureDisposal::NONE;
