ad7829d27fa64e50d1143fb543a3ef0ecc2666aa
youqiuyu <youqiuyu1@huawei.com>
2024-05-30 14:10:48 +0800

    Menu 5.0 UX
    Signed-off-by: youqiuyu <youqiuyu1@huawei.com>

--- frameworks/core/components_ng/pattern/menu/menu_item/menu_item_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/menu/menu_item/menu_item_layout_algorithm.cpp
@@ -97,6 +97,7 @@ void MenuItemLayoutAlgorithm::Layout(LayoutWrapper* layoutWrapper)
     if (AceApplicationInfo::GetInstance().GreatOrEqualTargetAPIVersion(PlatformVersion::VERSION_TWELVE)) {
         verticalInteval = static_cast<float>(MENU_ITEM_VERTICAL_PADDING.ConvertToPx());
     }
+    
     const auto& padding = layoutWrapper->GetLayoutProperty()->CreatePaddingAndBorderWithDefault(horInterval_,
         verticalInteval, 0.0f, 0.0f);
 
