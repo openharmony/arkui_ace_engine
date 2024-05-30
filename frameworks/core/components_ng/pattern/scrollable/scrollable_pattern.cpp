47e72ef5fd0d61cd4eec48d44c3e20187930dee0
wind <lijianing7@huawei.com>
2024-05-30 14:15:42 +0000

    PlayCurveAnimation加判空
    Signed-off-by: wind <lijianing7@huawei.com>

--- frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
+++ frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
@@ -1000,6 +1000,7 @@ void ScrollablePattern::PlaySpringAnimation(float position, float velocity, floa
 {
     if (!springOffsetProperty_) {
         InitSpringOffsetProperty();
+        CHECK_NULL_VOID(springOffsetProperty_);
     }
     scrollableEvent_->SetAnimateVelocityCallback([weakScroll = AceType::WeakClaim(this)]() -> double {
         auto pattern = weakScroll.Upgrade();
@@ -1040,6 +1041,7 @@ void ScrollablePattern::PlayCurveAnimation(
     InitOption(option, duration, curve);
     if (!curveOffsetProperty_) {
         InitCurveOffsetProperty();
+        CHECK_NULL_VOID(curveOffsetProperty_);
     }
     scrollableEvent_->SetAnimateVelocityCallback([weakScroll = AceType::WeakClaim(this)]() -> double {
         auto pattern = weakScroll.Upgrade();
