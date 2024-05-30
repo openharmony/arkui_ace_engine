13acb75219761452dcc0cc4c4864bc1e29a18397
fangzhiyuan <fangzhiyuan5@huawei.com>
2024-05-30 09:18:01 +0800

    mcc/mnc适配
    Signed-off-by: fangzhiyuan <fangzhiyuan5@huawei.com>
    Change-Id: Id06cfb2e11635d2739fb11cb9a2b8abcf4f2361f

--- adapter/ohos/entrance/ace_container.h
+++ adapter/ohos/entrance/ace_container.h
@@ -67,11 +67,13 @@ struct ParsedConfig {
     std::string fontScale;
     std::string fontWeightScale;
     std::string colorModeIsSetByApp;
+    std::string mcc;
+    std::string mnc;
     bool IsValid() const
     {
         return !(colorMode.empty() && deviceAccess.empty() && languageTag.empty() && direction.empty() &&
                  densitydpi.empty() && themeTag.empty() && fontScale.empty() && fontWeightScale.empty() &&
-                 colorModeIsSetByApp.empty());
+                 colorModeIsSetByApp.empty() && mcc.empty() && mnc.empty());
     }
 };
 
