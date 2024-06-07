ccaa49eedcd18bfaf9c5591ccf6c50e98931b878
zhangjiayong1 <zhangjiayong3@huawei.com>
2024-06-07 03:11:43 +0000

    use task remove insteadof cancelTask
    Signed-off-by: zhangjiayong1 <zhangjiayong3@huawei.com>

--- frameworks/base/thread/task_executor.h
+++ frameworks/base/thread/task_executor.h
@@ -249,6 +249,7 @@ public:
     virtual void AddTaskObserver(Task&& callback) = 0;
     virtual void RemoveTaskObserver() = 0;
     virtual bool WillRunOnCurrentThread(TaskType type) const = 0;
+    virtual void RemoveTask(TaskType type, const std::string &name) = 0;
 
     virtual int32_t GetTid(TaskType type)
     {
