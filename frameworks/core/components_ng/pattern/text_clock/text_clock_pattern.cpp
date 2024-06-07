740868cce726f619a2afd2564ca24a0126a31087
Zhang-Dong-hui <zhangdonghui5@huawei.com>
2024-06-07 09:37:08 +0000

    subscribe time tick event while in form
    Signed-off-by: Zhang-Dong-hui <zhangdonghui5@huawei.com>
    Change-Id: I998261cf7a28dc215a0a4d7ac81f2326ab257b40

--- frameworks/core/components_ng/pattern/text_clock/text_clock_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_clock/text_clock_pattern.cpp
@@ -230,7 +230,9 @@ void TextClockPattern::UpdateTimeText(bool isTimeChange)
         return;
     }
     FireBuilder();
-    RequestUpdateForNextSecond();
+    if (!isForm_) {
+        RequestUpdateForNextSecond();
+    }
     std::string currentTime = GetCurrentFormatDateTime();
     if (currentTime.empty()) {
         return;
