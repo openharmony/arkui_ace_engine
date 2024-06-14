9a1b3a07e62f71901be8d3d8e8679d0a980d5274
lijuan <lijuan124@huawei.com>
2024-06-14 07:50:21 +0000

    属性字符串段落样式多段落设置相同Indent maxline时除首段外未生效
    Signed-off-by: lijuan <lijuan124@huawei.com>

--- frameworks/core/components_ng/pattern/text/text_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/text/text_layout_algorithm.cpp
@@ -92,7 +92,8 @@ void TextLayoutAlgorithm::ConstructParagraphSpanGroup(std::list<RefPtr<SpanItem>
                 break;
             }
             if (pStyle != nextSpanParagraphStyle ||
-                (pStyle.leadingMargin.has_value() && pStyle.leadingMargin->pixmap)) {
+                (pStyle.leadingMargin.has_value() && pStyle.leadingMargin->pixmap) || Positive(pStyle.indent.Value()) ||
+                pStyle.maxLines != UINT32_MAX) {
                 std::list<RefPtr<SpanItem>> newGroup;
                 spanItem->SetNeedRemoveNewLine(true);
                 newGroup.splice(newGroup.begin(), spans, spans.begin(), std::next(it));
