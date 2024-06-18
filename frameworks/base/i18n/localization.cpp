2ad2a701ca6da41a9b029b87e1589ae557988b71
yhpeng <pengyuhao1@huawei.com>
2024-06-18 16:25:19 +0800

    fix: error typing
    Signed-off-by: yhpeng <pengyuhao1@huawei.com>

--- frameworks/base/i18n/localization.cpp
+++ frameworks/base/i18n/localization.cpp
@@ -347,6 +347,7 @@ const std::string Localization::FormatDuration(uint32_t duration, bool needShowH
 std::string Localization::FormatDuration(uint32_t duration, const std::string& format)
 {
     WaitingForInit();
+    const char* engTimeFormat = format.c_str();
 
     auto simpleDateFormat = GetSimpleDateFormat(locale_->instance);
     CHECK_NULL_RETURN(simpleDateFormat, "");
