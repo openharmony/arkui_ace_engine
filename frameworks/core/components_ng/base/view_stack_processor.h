1fc0cdfda78d295f5c4fe8c7c451b92626eb7f0f
lizhan <lizhan11@huawei.com>
2024-06-26 09:59:15 +0800

    fix recycle_memory_leak
    Signed-off-by: lizhan <lizhan11@huawei.com>
    Change-Id: I3983eb39e0b57a967f29a81d8e0c79560798ef49

--- frameworks/core/components_ng/base/view_stack_processor.h
+++ frameworks/core/components_ng/base/view_stack_processor.h
@@ -355,6 +355,18 @@ public:
         return result;
     }
 
+    void SetRecycleNodeId(int32_t recycleNodeId)
+    {
+        recycleNodeId_ = recycleNodeId;
+    }
+
+    int32_t GetRecycleNodeId()
+    {
+        const auto result = recycleNodeId_;
+        recycleNodeId_ = ElementRegister::UndefinedElementId;
+        return result;
+    }
+
     /**
      * get the elmtId to which all get access should be accounted
      * ElementRegister::UndefinedElementId; means no get access recording enabled
@@ -485,6 +497,7 @@ private:
     int32_t containerId_ = OHOS::Ace::INSTANCE_ID_UNDEFINED;
     int32_t restoreInstanceId_ = OHOS::Ace::INSTANCE_ID_UNDEFINED;
 
+    ElementIdType recycleNodeId_ = ElementRegister::UndefinedElementId;
     // elmtId reserved for next component creation
     ElementIdType reservedNodeId_ = ElementRegister::UndefinedElementId;
 
