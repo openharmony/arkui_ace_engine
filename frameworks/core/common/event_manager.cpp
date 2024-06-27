d6b6a8ddfcbd4d35f9f7b10d45955f5720454564
zhouchaobo <zhouchaobo@huawei.com>
2024-06-27 19:14:52 +0800

    reissue cancel event when input device change
    Signed-off-by: zhouchaobo <zhouchaobo@huawei.com>
    Change-Id: Ib795d5ba82238742d14d4564ce28f5757072762d

--- frameworks/core/common/event_manager.cpp
+++ frameworks/core/common/event_manager.cpp
@@ -88,6 +88,8 @@ void EventManager::TouchTest(const TouchEvent& touchPoint, const RefPtr<NG::Fram
     TouchTestResult hitTestResult;
     const NG::PointF point { touchPoint.x, touchPoint.y };
     if (refereeNG_->CheckEventTypeChange(touchPoint.sourceType)) {
+        AxisEvent axisEvent;
+        MockCancelEventAndDispatch(axisEvent);
         refereeNG_->CleanAll(true);
         touchTestResults_.clear();
         axisTouchTestResults_.clear();
@@ -354,6 +356,8 @@ void EventManager::TouchTest(
     ContainerScope scope(instanceId_);
 
     if (refereeNG_->CheckSourceTypeChange(event.sourceType, true)) {
+        TouchEvent touchEvent;
+        MockCancelEventAndDispatch(touchEvent);
         refereeNG_->CleanAll(true);
         touchTestResults_.clear();
         axisTouchTestResults_.clear();
@@ -1933,5 +1937,16 @@ void EventManager::MockCancelEventAndDispatch(const TouchEvent& touchPoint)
         mockedEvent.id = iter;
         DispatchTouchEvent(mockedEvent);
     }
+}
+
+void EventManager::MockCancelEventAndDispatch(const AxisEvent& axisEvent)
+{
+    if (axisTouchTestResults_.empty()) {
+        return;
+    }
+    AxisEvent mockedEvent = axisEvent;
+    mockedEvent.action = AxisAction::CANCEL;
+    mockedEvent.id = static_cast<int32_t>(axisTouchTestResults_.begin()->first);
+    DispatchTouchEvent(mockedEvent);
 }
 } // namespace OHOS::Ace
