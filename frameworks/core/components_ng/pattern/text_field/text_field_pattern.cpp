c4415f0750098d0037b46013900651bedcdc4875
c30016310 <chenpeng242@huawei.com>
2024-06-27 16:04:49 +0800

    add onchange when previewRange change
    Signed-off-by: c30016310 <chenpeng242@huawei.com>
    Change-Id: I4e5d5b56123809bec6a997332c2fbd67e1b14df6

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -2747,7 +2747,9 @@ bool TextFieldPattern::FireOnTextChangeEvent()
         }
     }
     auto textCache = layoutProperty->GetValueValue("");
-    if (textCache == contentController_->GetTextValue()) {
+    auto rangeCache = layoutProperty->GetPreviewRangeValue({-1, -1});
+    TextRange curPreviewRange = {GetPreviewTextStart(), GetPreviewTextEnd()};
+    if (textCache == contentController_->GetTextValue() && rangeCache == curPreviewRange) {
         return false;
     }
     host->OnAccessibilityEvent(AccessibilityEventType::TEXT_CHANGE, textCache, contentController_->GetTextValue());
@@ -2797,6 +2799,7 @@ void TextFieldPattern::AddTextFireOnChange()
         TextRange range;
         range.start = pattern->GetPreviewTextStart();
         range.end = pattern->GetPreviewTextEnd();
+        layoutProperty->UpdatePreviewRange(range);
         eventHub->FireOnChange(pattern->GetTextContentController()->GetTextValue(), range);
     });
 }
