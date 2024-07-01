3423ff057cbe17238c8cff8fadd47cb693845ff9
carnivore233 <xuyue51@huawei.com>
2024-07-01 19:57:10 +0800

    【RichEditor】修改leadingmargin默认尺寸格式与文档一致
    Signed-off-by: carnivore233 <xuyue51@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/selection_info.h
+++ frameworks/core/components_ng/pattern/rich_editor/selection_info.h
@@ -114,7 +114,7 @@ struct TextStyleResult {
     int32_t textAlign = 0;
     int32_t wordBreak = static_cast<int32_t>(WordBreak::BREAK_WORD);
     int32_t lineBreakStrategy = static_cast<int32_t>(LineBreakStrategy::GREEDY);
-    std::string leadingMarginSize[2] = { "0.0", "0.0" };
+    std::string leadingMarginSize[2] = { "0.00px", "0.00px" };
 };
 
 struct ImageStyleResult {
@@ -206,7 +206,7 @@ public:
 struct ParagraphInfo {
     // style
     RefPtr<PixelMap> leadingMarginPixmap;
-    std::string leadingMarginSize[2] = { "0.0", "0.0" };
+    std::string leadingMarginSize[2] = { "0.00px", "0.00px" };
     int32_t textAlign = 0;
     int32_t wordBreak = static_cast<int32_t>(WordBreak::BREAK_WORD);
     int32_t lineBreakStrategy = static_cast<int32_t>(LineBreakStrategy::GREEDY);
