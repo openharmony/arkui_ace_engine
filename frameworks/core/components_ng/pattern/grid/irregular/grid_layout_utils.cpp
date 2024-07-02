841893b9a9b5ba649c88d1008ec8506595b5b490
Tianer Zhou <zhoutianer@huawei.com>
2024-07-02 19:34:52 +0800

    refactor grid preload items
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: I57f186c754aa940a7a585c9d136d098e98ac7849

--- frameworks/core/components_ng/pattern/grid/irregular/grid_layout_utils.cpp
+++ frameworks/core/components_ng/pattern/grid/irregular/grid_layout_utils.cpp
@@ -14,7 +14,10 @@
  */
 #include "core/components_ng/pattern/grid/irregular/grid_layout_utils.h"
 
+#include "core/components_ng/pattern/grid/grid_layout_info.h"
 #include "core/components_ng/pattern/grid/grid_layout_property.h"
+#include "core/components_ng/pattern/grid/grid_pattern.h"
+
 namespace OHOS::Ace::NG {
 GridItemSize GridLayoutUtils::GetItemSize(const GridLayoutInfo* info, const LayoutWrapper* wrapper, int32_t idx)
 {
@@ -43,5 +46,56 @@ GridItemSize GridLayoutUtils::GetItemSize(const GridLayoutInfo* info, const Layo
     size.columns = std::max(1, size.columns);
     size.rows = std::max(1, size.rows);
     return size;
+}
+
+void GridLayoutUtils::PreloadGridItems(
+    const RefPtr<GridPattern>& pattern, std::list<int32_t>&& items, const BuildGridItemCallback& buildCb)
+{
+    CHECK_NULL_VOID(pattern);
+    if (!pattern->GetPreloadItemList().empty()) {
+        // task already added to pipeline, only need to update item list
+        pattern->SetPreloadItemList(std::move(items));
+        return;
+    }
+    PreloadGridItemsHelper(pattern, buildCb);
+}
+
+void GridLayoutUtils::PreloadGridItemsHelper(const RefPtr<GridPattern>& pattern, const BuildGridItemCallback& buildCb)
+{
+    auto context = PipelineContext::GetCurrentContext();
+    CHECK_NULL_VOID(context);
+    context->AddPredictTask([weak = AceType::WeakClaim(AceType::RawPtr(pattern)), buildCb](int64_t deadline, bool _) {
+        ACE_SCOPED_TRACE("Grid preload items");
+        auto pattern = weak.Upgrade();
+        CHECK_NULL_VOID(pattern);
+        const auto& items = pattern->GetPreloadItemList();
+        if (items.empty()) {
+            return;
+        }
+        auto it = items.begin();
+        if (pattern->IsPredictOutOfRange(*it)) {
+            return;
+        }
+        bool needMarkDirty = false;
+        auto host = pattern->GetHost();
+        CHECK_NULL_VOID(host);
+        for (; it != items.end(); ++it) {
+            if (GetSysTimestamp() > deadline) {
+                break;
+            }
+            if (buildCb) {
+                needMarkDirty = buildCb(host, *it) || needMarkDirty;
+            }
+        }
+        if (needMarkDirty) {
+            host->MarkDirtyNode(PROPERTY_UPDATE_MEASURE_SELF);
+        }
+        if (it != items.end() && !needMarkDirty) {
+            pattern->SetPreloadItemList(std::list<int32_t>(it, items.end()));
+            PreloadGridItemsHelper(pattern, buildCb);
+        } else {
+            pattern->SetPreloadItemList({});
+        }
+    });
 }
 } // namespace OHOS::Ace::NG
