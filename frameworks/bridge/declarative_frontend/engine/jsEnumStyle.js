675c7adc5d180093693a08c1d85b22238c569943
tengfan3 <tengfan3@huawei.com>
2024-07-03 20:23:23 +0800

    传感器功能及权限对应
    Signed-off-by: tengfan3 <tengfan3@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
+++ frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
@@ -1217,6 +1217,7 @@ var ProtectedResourceType;
   ProtectedResourceType["MidiSysex"] = "TYPE_MIDI_SYSEX";
   ProtectedResourceType["VIDEO_CAPTURE"] = "TYPE_VIDEO_CAPTURE";
   ProtectedResourceType["AUDIO_CAPTURE"] = "TYPE_AUDIO_CAPTURE";
+  ProtectedResourceType["SENSORS"] = "TYPE_SENSORS";
 })(ProtectedResourceType || (ProtectedResourceType = {}));
 
 var ProgressType;
