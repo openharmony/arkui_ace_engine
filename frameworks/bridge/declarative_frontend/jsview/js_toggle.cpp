ddf9d3ba310e73c4bd76b87d48456ff2c3c44fbe
yangziyong <nsyangziyong@huawei.com>
2024-07-05 11:39:38 +0800

    代码格式整改
    Signed-off-by: yangziyong <nsyangziyong@huawei.com>
    Change-Id: I824e0cbcc9b94154967656b2f0a77b62d1b2f942

--- frameworks/bridge/declarative_frontend/jsview/js_toggle.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_toggle.cpp
@@ -330,8 +330,9 @@ NG::PaddingPropertyF JSToggle::GetOldPadding(const JSCallbackInfo& info)
 
 NG::PaddingProperty JSToggle::GetNewPadding(const JSCallbackInfo& info)
 {
-    NG::PaddingProperty padding(
-        { NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp) });
+    NG::PaddingProperty padding({
+        NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp)
+    });
     if (info[0]->IsObject()) {
         std::optional<CalcDimension> left;
         std::optional<CalcDimension> right;
@@ -373,8 +374,9 @@ NG::PaddingProperty JSToggle::GetPadding(const std::optional<CalcDimension>& top
     const std::optional<CalcDimension>& bottom, const std::optional<CalcDimension>& left,
     const std::optional<CalcDimension>& right)
 {
-    NG::PaddingProperty padding(
-        { NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp) });
+    NG::PaddingProperty padding({
+        NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp), NG::CalcLength(0.0_vp)
+    });
     if (left.has_value() && left.value().IsNonNegative()) {
         padding.left = NG::CalcLength(left.value());
     }
