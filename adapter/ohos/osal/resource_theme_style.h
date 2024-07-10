e69e9c8dd7e5fc2960a71eaa35ec521efd736b61
yhpeng <pengyuhao1@huawei.com>
2024-07-10 16:55:37 +0800

    add: code format fix
    Signed-off-by: yhpeng <pengyuhao1@huawei.com>

--- adapter/ohos/osal/resource_theme_style.h
+++ adapter/ohos/osal/resource_theme_style.h
@@ -37,7 +37,8 @@ public:
 
     void ParseContent() override;
     void CheckThemeStyleLoaded() override;
-    void SetPromiseValue() {
+    void SetPromiseValue() 
+    {
         promise_.set_value();
     }
 protected:
