7fc5f23db594519267371f6a9c2eac1552d72391
liugan <liugan8@huawei.com>
2024-07-12 16:24:21 +0800

    【一镜到底】新增节点配对异常维测日志
    Signed-off-by: liugan <liugan8@huawei.com>

--- frameworks/core/components_ng/layout/layout_property.cpp
+++ frameworks/core/components_ng/layout/layout_property.cpp
@@ -859,10 +859,13 @@ void LayoutProperty::UpdateGeometryTransition(const std::string& id,
         // unregister node from old geometry transition
         geometryTransitionOld->Update(host_, nullptr);
         // register node into new geometry transition
-        if (geometryTransitionNew) {
-            geometryTransitionNew->Update(nullptr, host_);
+        if (geometryTransitionNew && !geometryTransitionNew->Update(nullptr, host_)) {
+            TAG_LOGE(AceLogTag::ACE_GEOMETRY_TRANSITION, "redundant node%{public}d has same geoid", host->GetId());
         }
     } else if (geometryTransitionNew) {
+        if (geometryTransitionNew->IsInAndOutValid()) {
+            TAG_LOGE(AceLogTag::ACE_GEOMETRY_TRANSITION, "redundant node%{public}d has same geoid", host->GetId());
+        }
         geometryTransitionNew->Build(host_, true);
     }
     geometryTransition_ = geometryTransitionNew;
