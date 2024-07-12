3ab5dae20ddcdf521e79bf7f19f752e65cd6412f
mayaolll <mayao17@huawei.com>
2024-07-12 17:46:16 +0800

    update get context
    Signed-off-by: mayaolll <mayao17@huawei.com>

--- frameworks/core/components_ng/pattern/side_bar/side_bar_container_pattern.cpp
+++ frameworks/core/components_ng/pattern/side_bar/side_bar_container_pattern.cpp
@@ -91,7 +91,7 @@ void SideBarContainerPattern::OnAttachToFrameNode()
     auto layoutProperty = host->GetLayoutProperty<SideBarContainerLayoutProperty>();
     CHECK_NULL_VOID(layoutProperty);
     userSetSidebarWidth_ = layoutProperty->GetSideBarWidth().value_or(SIDEBAR_WIDTH_NEGATIVE);
-    auto pipelineContext = host->GetContextRefPtr();
+    auto pipelineContext = host->GetContext();
     CHECK_NULL_VOID(pipelineContext);
     pipelineContext->AddWindowSizeChangeCallback(host->GetId());
     auto sideBarTheme = pipelineContext->GetTheme<SideBarTheme>();
@@ -103,7 +103,7 @@ void SideBarContainerPattern::OnAttachToFrameNode()
 void SideBarContainerPattern::OnDetachFromFrameNode(FrameNode* frameNode)
 {
     CHECK_NULL_VOID(frameNode);
-    auto pipeline = frameNode->GetContextRefPtr();
+    auto pipeline = frameNode->GetContext();
     CHECK_NULL_VOID(pipeline);
     pipeline->RemoveWindowSizeChangeCallback(frameNode->GetId());
     pipeline->RemoveWindowFocusChangedCallback(frameNode->GetId());
