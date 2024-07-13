65dc3ea47a81d13a842b799e9be328c92b4ab5d8
yangbo <yangbo305@h-partners.com>
2024-07-13 14:58:28 +0800

    修改弧形列表预览配置
    Signed-off-by: yangbo <yangbo305@h-partners.com>

--- component_ext/arc_list/arc_list_napi.cpp
+++ component_ext/arc_list/arc_list_napi.cpp
@@ -28,15 +28,8 @@
 #include "core/components_ng/pattern/scroll_bar/proxy/scroll_bar_proxy.h"
 #include "core/components_ng/pattern/scrollable/scrollable_model_ng.h"
 
-extern const char _binary_arkui_arclist_js_start[];
 extern const char _binary_arkui_arclist_abc_start[];
-#if !defined(IOS_PLATFORM)
-extern const char _binary_arkui_arclist_js_end[];
 extern const char _binary_arkui_arclist_abc_end[];
-#else
-extern const char* _binary_arkui_arclist_js_end;
-extern const char* _binary_arkui_arclist_abc_end;
-#endif
 
 namespace OHOS::Ace {
 namespace {
@@ -98,9 +91,11 @@ napi_value JsCreate(napi_env env, napi_callback_info info)
 #ifndef PREVIEW
         napi_value jsScroller = ExtNapiUtils::GetNamedProperty(env, argv[0], "scroller");
         if (ExtNapiUtils::CheckTypeForNapiValue(env, jsScroller, napi_object)) {
-            Framework::JSScroller* scroller = nullptr;
-            napi_unwrap(env, jsScroller, (void**)&scroller);
-            SetScroller(env, scroller);
+            panda::Local<panda::ObjectRef> scrollerObject((uintptr_t)jsScroller);
+            if (scrollerObject->GetNativePointerFieldCount() > 0) {
+                Framework::JSScroller* scroller = (Framework::JSScroller*)scrollerObject->GetNativePointerField(0);
+                SetScroller(env, scroller);
+            }
         }
 #endif
 
@@ -431,19 +426,7 @@ napi_value ExportArcList(napi_env env, napi_value exports)
 
 } // namespace OHOS::Ace
 
-extern "C" __attribute__((visibility("default"))) void NAPI_arkui_ArcList_GetJSCode(const char** buf, int* bufLen)
-{
-    if (buf != nullptr) {
-        *buf = _binary_arkui_arclist_js_start;
-    }
-
-    if (bufLen != nullptr) {
-        *bufLen = _binary_arkui_arclist_js_end - _binary_arkui_arclist_js_start;
-    }
-}
-
-// arkui_arclist JS register
-extern "C" __attribute__((visibility("default"))) void NAPI_arkui_ArcList_GetABCCode(const char** buf, int* buflen)
+extern "C" ACE_FORCE_EXPORT void NAPI_arkui_ArcList_GetABCCode(const char** buf, int* buflen)
 {
     if (buf != nullptr) {
         *buf = _binary_arkui_arclist_abc_start;
