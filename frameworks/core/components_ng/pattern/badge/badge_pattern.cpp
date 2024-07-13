8377cb45ee0364eaab56f42132272304f3d10af6
xuqinfeng1@huawei.com <xuqinfeng1@huawei.com>
2024-07-13 13:59:12 +0800

    badge的dump信息补充
    Signed-off-by: xuqinfeng1@huawei.com <xuqinfeng1@huawei.com>
    Change-Id: I2aef4d8a85a5e248419f0ff020a04106f4db0184

--- frameworks/core/components_ng/pattern/badge/badge_pattern.cpp
+++ frameworks/core/components_ng/pattern/badge/badge_pattern.cpp
@@ -13,6 +13,7 @@
  * limitations under the License.
  */
 
+#include "base/log/dump_log.h"
 #include "core/components_ng/pattern/badge/badge_pattern.h"
 
 #include "core/components/badge/badge_theme.h"
@@ -154,5 +155,27 @@ void BadgePattern::BadgeAnimation(RefPtr<FrameNode>& frameNode, bool isShowBadge
                 textLayoutProperty->ResetContent();
             }
     });
+}
+
+void BadgePattern::DumpInfo()
+{
+    auto layoutProperty = GetLayoutProperty<BadgeLayoutProperty>();
+    auto badgeCount = layoutProperty->GetBadgeCount();
+    auto badgeValue = layoutProperty->GetBadgeValue();
+    auto circleSize = layoutProperty->GetBadgeCircleSize();
+    auto badgeTextColor = layoutProperty->GetBadgeTextColor();
+    auto badgeFontSize = layoutProperty->GetBadgeFontSize();
+    if (badgeCount.has_value()) {
+        DumpLog::GetInstance().AddDesc(std::string("badgeCount: ").append(std::to_string(badgeCount.value())));
+    }
+    if (badgeValue.has_value()) {
+        if (badgeValue.value().empty()) {
+            DumpLog::GetInstance().AddDesc(std::string("badgeValue is empty"));
+        }
+        DumpLog::GetInstance().AddDesc(std::string("badgeValue: ").append(badgeValue.value()));
+    }
+    DumpLog::GetInstance().AddDesc(std::string("badgeTextColor: ").append(badgeTextColor.value().ToString()));
+    DumpLog::GetInstance().AddDesc(std::string("circleSize: ").append(std::to_string(circleSize->ConvertToPx())));
+    DumpLog::GetInstance().AddDesc(std::string("badgeFontSize: ").append(badgeFontSize.value().ToString()));
 }
 } // namespace OHOS::Ace::NG
