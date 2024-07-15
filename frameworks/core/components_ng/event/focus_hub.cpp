2de7ffb5f9964dfca228f11134e29b4eff125032
yihao_lin <linyh@zju.edu.cn>
2024-07-15 15:07:08 +0800

    onBlur回调在节点销毁时改为同步
    Signed-off-by: yihao_lin <linyh@zju.edu.cn>
    Change-Id: I41d4b4cba7e54baed902ac158b0465d17d34ab2f

--- frameworks/core/components_ng/event/focus_hub.cpp
+++ frameworks/core/components_ng/event/focus_hub.cpp
@@ -1183,25 +1183,35 @@ void FocusHub::OnBlurNode()
     CHECK_NULL_VOID(frameNode);
     auto* pipeline = frameNode->GetContext();
     CHECK_NULL_VOID(pipeline);
-    pipeline->AddAfterLayoutTask([weak = WeakClaim(this)]() {
-        auto focusHub = weak.Upgrade();
-        CHECK_NULL_VOID(focusHub);
-        auto onBlurCallback = focusHub->GetOnBlurCallback();
+    
+    if (blurReason_ == BlurReason::FRAME_DESTROY) {
+        auto onBlurCallback = GetOnBlurCallback();
         if (onBlurCallback) {
             onBlurCallback();
         }
-        auto onJSFrameNodeBlurCallback_ = focusHub->GetOnJSFrameNodeBlurCallback();
+        auto onJSFrameNodeBlurCallback_ = GetOnJSFrameNodeBlurCallback();
         if (onJSFrameNodeBlurCallback_) {
             onJSFrameNodeBlurCallback_();
         }
-    });
-    if (blurReason_ != BlurReason::FRAME_DESTROY) {
+    } else {
+        pipeline->AddAfterLayoutTask([weak = WeakClaim(this)]() {
+            auto focusHub = weak.Upgrade();
+            CHECK_NULL_VOID(focusHub);
+            auto onBlurCallback = focusHub->GetOnBlurCallback();
+            if (onBlurCallback) {
+                onBlurCallback();
+            }
+            auto onJSFrameNodeBlurCallback_ = focusHub->GetOnJSFrameNodeBlurCallback();
+            if (onJSFrameNodeBlurCallback_) {
+                onJSFrameNodeBlurCallback_();
+            }
+        });
         ClearFocusState();
     }
+
     auto focusManager = pipeline->GetOrCreateFocusManager();
     CHECK_NULL_VOID(focusManager);
     focusManager->PaintFocusState();
-
     pipeline->RequestFrame();
 }
 
