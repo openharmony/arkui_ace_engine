b020b18cfdd932dcf7436fd1d3b00bbb50367f83
shihao <wangshihao26@huawei.com>
2024-07-15 16:02:38 +0800

    修复swiper内嵌套swiper, 滚动父swiper时同时滚动子swiper, 造成父swiper不复位
    Signed-off-by: shihao <wangshihao26@huawei.com>
    Change-Id: I535047bb7dbe5a321fd9198bcba4d541b61cfabf

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
@@ -4911,7 +4911,8 @@ void SwiperPattern::NotifyParentScrollStart(float position)
     }
     auto parent = GetNestedScrollParent();
     CHECK_NULL_VOID(parent);
-    if (parent) {
+    const auto& nestedScroll = GetNestedScroll();
+    if (parent && nestedScroll.NeedParent()) {
         parent->OnScrollStartRecursive(position);
     }
 }
@@ -4933,7 +4934,7 @@ void SwiperPattern::OnScrollEndRecursive(const std::optional<float>& velocity)
 void SwiperPattern::NotifyParentScrollEnd()
 {
     auto parent = GetNestedScrollParent();
-    auto nestedScroll = GetNestedScroll();
+    const auto& nestedScroll = GetNestedScroll();
     if (parent && (nestedScroll.NeedParent() || GetIsNestedInterrupt())) {
         parent->OnScrollEndRecursive(std::nullopt);
     }
