c08d109369ff9ebf56230e0fecc6685bfe0df48d
zhouke <zhouke35@huawei.com>
2024-07-16 21:21:34 +0800

    modify.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhouke <zhouke35@huawei.com>

--- adapter/ohos/osal/js_accessibility_manager.cpp
+++ adapter/ohos/osal/js_accessibility_manager.cpp
@@ -1168,6 +1168,20 @@ void JsAccessibilityManager::UpdateAccessibilityElementInfo(
     nodeInfo.SetTextType(accessibilityProperty->GetTextType());
     nodeInfo.SetTextLengthLimit(accessibilityProperty->GetTextLengthLimit());
     nodeInfo.SetOffset(accessibilityProperty->GetScrollOffSet());
+    if (node->GetRenderContext() != nullptr) {
+        nodeInfo.SetZIndex(node->GetRenderContext()->GetZIndex().value_or(0));
+        nodeInfo.SetOpacity(node->GetRenderContext()->GetOpacity().value_or(1));
+        nodeInfo.SetBackgroundColor(node->GetRenderContext()->GetBackgroundColor().value_or(Color::TRANSPARENT).ToString());
+        nodeInfo.SetBackgroundImage(node->GetRenderContext()->GetBackgroundImage().value_or(ImageSourceInfo("")).ToString());
+        if (node->GetRenderContext()->GetForeground() != nullptr) {
+            nodeInfo.SetBlur(node->GetRenderContext()->GetForeground()->propBlurRadius.value_or(Dimension(0)).ToString());
+        }
+    }
+    auto eventHub = node->GetEventHub<NG::EventHub>();
+    CHECK_NULL_VOID(eventHub);
+    auto gestureEventHub = eventHub->GetGestureEventHub();
+    CHECK_NULL_VOID(gestureEventHub);
+    nodeInfo.SetHitTestBehavior(gestureEventHub->GetHitTestModeStr());
     if (accessibilityProperty->HasUserDisabled()) {
         nodeInfo.SetEnabled(!accessibilityProperty->IsUserDisabled());
     }
