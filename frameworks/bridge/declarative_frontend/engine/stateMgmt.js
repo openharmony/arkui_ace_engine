885c31f999f3acc5540bcabd969f7ddf004e4ad0
jiyujia926 <jiyujia3@huawei.com>
2024-07-16 15:49:18 +0800

    implement UIUtils.getTarget
    Signed-off-by: jiyujia926 <jiyujia3@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/stateMgmt.js
+++ frameworks/bridge/declarative_frontend/engine/stateMgmt.js
@@ -10583,6 +10583,47 @@ PersistenceV2Impl.MAX_DATA_LENGTH_ = 8000;
 PersistenceV2Impl.NOT_SUPPORT_TYPES_ = [Array, Set, Map, WeakSet, WeakMap, Date, Boolean, Number, String, Symbol, BigInt, RegExp, Function, Promise, ArrayBuffer];
 PersistenceV2Impl.instance_ = undefined;
 /*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class UIUtilsImpl {
+    static getTarget(source) {
+        if (!source || typeof source !== 'object') {
+            return source;
+        }
+        if (ObservedObject.IsObservedObject(source)) {
+            // V1 Proxy object
+            return ObservedObject.GetRawObject(source);
+        }
+        else if (source[ObserveV2.SYMBOL_PROXY_GET_TARGET]) {
+            // V2 Proxy object
+            return source[ObserveV2.SYMBOL_PROXY_GET_TARGET];
+        }
+        else {
+            // other situation, not handle
+            return source;
+        }
+    }
+    static instance() {
+        if (UIUtilsImpl.instance_) {
+            return UIUtilsImpl.instance_;
+        }
+        UIUtilsImpl.instance_ = new UIUtilsImpl();
+        return UIUtilsImpl.instance_;
+    }
+}
+UIUtilsImpl.instance_ = undefined;
+/*
  * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
