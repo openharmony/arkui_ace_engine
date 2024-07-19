ef16b09ffd8308a1f06f8a3615940e5fb07822b8
liuwei <liuwei846@h-partners.com>
2024-07-19 16:36:36 +0800

    Auto模式下默认不显示滚动条
    Signed-off-by: liuwei <liuwei846@h-partners.com>

--- frameworks/core/components_ng/pattern/arc_scroll/inner/arc_scroll_bar.cpp
+++ frameworks/core/components_ng/pattern/arc_scroll/inner/arc_scroll_bar.cpp
@@ -53,7 +53,7 @@ void ArcScrollBar::SetBarRegion(const Offset& offset, const Size& size)
     double sweepAngle = GetNormaMaxOffsetAngle();
     double width = NormalizeToPx(GetNormalBackgroundWidth());
     centerDeviation_ = width * FACTOR_HALF;
-    if (normalWidth == NormalizeToPx(GetActiveWidth())) {
+    if (GetDisplayMode() == DisplayMode::AUTO || normalWidth == NormalizeToPx(GetActiveWidth())) {
         startAngle = GetActiveStartAngle();
         sweepAngle = GetActiveMaxOffsetAngle();
         width = NormalizeToPx(GetActiveBackgroundWidth());
@@ -70,7 +70,7 @@ void ArcScrollBar::SetRoundTrickRegion(double estimatedHeight, double barRegionS
         width_ = NormalizeToPx(GetNormalScrollBarWidth());
         centerDeviation_ = NormalizeToPx(GetNormalBackgroundWidth()) * FACTOR_HALF;
         double maxAngle = GetNormaMaxOffsetAngle();
-        if (normalWidth == NormalizeToPx(GetActiveWidth())) {
+        if (GetDisplayMode() == DisplayMode::AUTO || normalWidth == NormalizeToPx(GetActiveWidth())) {
             width_ = NormalizeToPx(GetActiveScrollBarWidth());
             centerDeviation_ = NormalizeToPx(GetActiveBackgroundWidth()) * FACTOR_HALF;
             maxAngle = GetActiveMaxOffsetAngle();
