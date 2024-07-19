01e4b44c62f61cc6aac812e8a0b962e6fe9f5579
yhpeng <pengyuhao1@huawei.com>
2024-07-19 11:52:50 +0800

    Revert "add: theme load speed improve"
    This reverts commit ed0b20595ad79d47a3328d95d6ad05fded1b2b78.
    
    Signed-off-by: yhpeng <pengyuhao1@huawei.com>

--- adapter/ohos/osal/resource_theme_style.h
+++ adapter/ohos/osal/resource_theme_style.h
@@ -17,7 +17,6 @@
 #define FOUNDATION_ACE_ADAPTER_OHOS_OSAL_RESOURCE_THEME_STYLE_H
 
 #include <map>
-#include <future>
 
 #include "core/components/theme/theme_style.h"
 #include "core/components/theme/resource_adapter.h"
@@ -36,17 +35,11 @@ public:
     ~ResourceThemeStyle() override = default;
 
     void ParseContent() override;
-    void CheckThemeStyleLoaded() override;
-    void SetPromiseValue()
-    {
-        promise_.set_value();
-    }
+
 protected:
     void OnParseStyle();
     void OnParseResourceMedia(const std::string& attrName, const std::string& attrValue);
-    std::promise<void> promise_;
-    std::shared_future<void> future_ = promise_.get_future();
-    std::mutex checkThemeStyleLoadedMutex_;
+
 private:
     RawAttrMap rawAttrs_; // key and value read from global resource api.
     RawPatternMap patternAttrs_;
