5c0084445030cead38ea27b95835597aaa388c3f
zhangqing <zhangqing50@h-partners.com>
2024-07-20 16:06:09 +0800

    arkui-x update master OH to weekly_20240715 and OH fix build bug
    Signed-off-by: zhangqing <zhangqing50@h-partners.com>
    Change-Id: Ia76e6d7746af178690ae5b94a8d020e0f1180762

--- frameworks/base/network/download_manager_impl.cpp
+++ frameworks/base/network/download_manager_impl.cpp
@@ -96,9 +96,6 @@ public:
         // Some servers don't like requests that are made without a user-agent field, so we provide one
         ACE_CURL_EASY_SET_OPTION(handle.get(), CURLOPT_USERAGENT, "libcurl-agent/1.0");
         ACE_CURL_EASY_SET_OPTION(handle.get(), CURLOPT_URL, url.c_str());
-#if !defined(PREVIEW)
-        ACE_CURL_EASY_SET_OPTION(handle.get(), CURLOPT_CAINFO, "/etc/security/certificates");
-#endif
         ACE_CURL_EASY_SET_OPTION(handle.get(), CURLOPT_VERBOSE, 1L);
         ACE_CURL_EASY_SET_OPTION(handle.get(), CURLOPT_ERRORBUFFER, errorStr.data());
 
