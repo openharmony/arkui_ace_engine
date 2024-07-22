1fe86571a1f89de5e3f19a07d76f73223c9b1eb1
Tianer Zhou <zhoutianer@huawei.com>
2024-07-22 14:37:03 +0800

    fix childScrolling state
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: Ifb12e848e87c1d940f2a8dbec6eda07bb803442b

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
@@ -4985,11 +4985,13 @@ ScrollResult SwiperPattern::HandleScroll(float offset, int32_t source, NestedSta
     if (!CheckSwiperPanEvent(offset)) {
         return { offset, true };
     }
-    // handle situations when multiple children are notifying scrollStart / scrollEnd
-    // reset flag and animations to correct states when scroll happens
-    childScrolling_ = true;
-    if (DuringTranslateAnimation()) {
-        StopAnimationOnScrollStart(false);
+    if (state != NestedState::GESTURE) {
+        // handle situations when multiple children are notifying scrollStart / scrollEnd
+        // reset flag and animations to correct states when scroll happens
+        childScrolling_ = true;
+        if (DuringTranslateAnimation()) {
+            StopAnimationOnScrollStart(false);
+        }
     }
     // mouse scroll triggers showNext / showPrev instead of updating offset
     if (source == SCROLL_FROM_AXIS) {
