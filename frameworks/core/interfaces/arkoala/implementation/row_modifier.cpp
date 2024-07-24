4e02098bec1fb7891b04c748cf92ec1596fe1d01
Vladimir Stepanov <stepanov.vladimir@huawei.com>
2024-07-24 12:23:22 +0300

    Row Modifier

--- frameworks/core/interfaces/arkoala/implementation/row_modifier.cpp
+++ frameworks/core/interfaces/arkoala/implementation/row_modifier.cpp
@@ -13,29 +13,53 @@
  * limitations under the License.
  */
 
+#include "core/interfaces/native/node/node_api.h"
 #include "arkoala_api_generated.h"
+#include "core/interfaces/arkoala/utility/converter.h"
+#include "core/common/container.h"
+#include "core/components_ng/pattern/linear_layout/row_model_ng.h"
+#include "core/components_ng/base/view_stack_processor.h"
 
 namespace OHOS::Ace::NG::GeneratedModifier {
-    namespace RowInterfaceModifier {
-        void _setRowOptionsImpl(Ark_NativePointer node, const Opt_Type_RowInterface__setRowOptions_Arg0* value) {
-        }
-    } // RowInterfaceModifier
-    namespace RowAttributeModifier {
-        void AlignItemsImpl(Ark_NativePointer node, Ark_Int32 value) {
-        }
-        void JustifyContentImpl(Ark_NativePointer node, Ark_Int32 value) {
-        }
-        void PointLightImpl(Ark_NativePointer node, const Ark_PointLightStyle* value) {
-        }
-    } // RowAttributeModifier
-    const GENERATED_ArkUIRowModifier* GetRowModifier() {
-        static const GENERATED_ArkUIRowModifier ArkUIRowModifierImpl {
-            RowInterfaceModifier::_setRowOptionsImpl,
-            RowAttributeModifier::AlignItemsImpl,
-            RowAttributeModifier::JustifyContentImpl,
-            RowAttributeModifier::PointLightImpl,
-        };
-        return &ArkUIRowModifierImpl;
-    }
-
+namespace RowInterface {
+void _setRowOptionsImpl(Ark_NativePointer node, const Opt_Type_RowInterface__setRowOptions_Arg0* value) {
+  auto frameNode = reinterpret_cast<FrameNode *>(node);
+  std::tuple<Ark_Float32, Ark_Int32> space = Converter::ConvertOrDefault(
+      *value, std::make_tuple(0.0f, (int)DimensionUnit::PX));
+  RowModelNG::SetSpace(
+      frameNode,
+      CalcDimension(std::get<0>(space), (DimensionUnit)std::get<1>(space)));
 }
+} // RowInterface
+
+namespace RowAttribute {
+void AlignItemsImpl(Ark_NativePointer node, Ark_Int32 value) {
+  auto frameNode = reinterpret_cast<FrameNode *>(node);
+  RowModelNG::SetAlignItems(frameNode, static_cast<FlexAlign>(value + 1));
+}
+void JustifyContentImpl(Ark_NativePointer node, Ark_Int32 value) {
+  auto frameNode = reinterpret_cast<FrameNode *>(node);
+  RowModelNG::SetJustifyContent(frameNode, static_cast<FlexAlign>(value));
+}
+void PointLightImpl(Ark_NativePointer node, const Ark_PointLightStyle* value) {
+    LOGE("ARKOALA RowAttribute_PointLightImpl -> Method is not FULLY implemented.");
+    auto frameNode = reinterpret_cast<FrameNode *>(node);
+    ACE_UPDATE_NODE_RENDER_CONTEXT(
+        LightIlluminated, (float)Converter::ConvertOrDefault(value->illuminated, 0),
+        frameNode);
+    ACE_UPDATE_NODE_RENDER_CONTEXT(
+        Bloom, (float)Converter::ConvertOrDefault(value->bloom, 0), frameNode);
+}
+} // RowAttribute
+
+const GENERATED_ArkUIRowModifier* GetRowModifier() {
+    static const GENERATED_ArkUIRowModifier ArkUIRowModifierImpl {
+        RowInterface::_setRowOptionsImpl,
+        RowAttribute::AlignItemsImpl,
+        RowAttribute::JustifyContentImpl,
+        RowAttribute::PointLightImpl,
+    };
+    return &ArkUIRowModifierImpl;
+}
+
+} // namespace OHOS::Ace::NG::GeneratedModifier
