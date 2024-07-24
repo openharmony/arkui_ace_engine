21682a0d479a1c1d024ea2d2221da93129472b0c
panliujun <panliujun3@h-partners.com>
2024-07-24 21:51:34 +0800

    手势返回添加日志 Signed-off-by: panliujun <panliujun3@h-partners.com>

--- frameworks/core/components_ng/pattern/web/web_pattern.cpp
+++ frameworks/core/components_ng/pattern/web/web_pattern.cpp
@@ -4453,6 +4453,7 @@ bool WebPattern::OnBackPressedForFullScreen() const
         return false;
     }
 
+    TAG_LOGI(AceLogTag::ACE_WEB, "Web %{public}d receives back press event", host->GetId());
     CHECK_NULL_RETURN(fullScreenExitHandler_, false);
     auto webFullScreenExitHandler = fullScreenExitHandler_->GetHandler();
     CHECK_NULL_RETURN(webFullScreenExitHandler, false);
