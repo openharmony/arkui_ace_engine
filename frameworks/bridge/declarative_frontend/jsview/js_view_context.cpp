8519576100876a1efa6fc26101f78b618dbe537d
hehongyang9 <hehongyang@huawei.com>
2024-07-25 15:14:53 +0800

    notifyContainers by container id order
    Signed-off-by: hehongyang9 <hehongyang@huawei.com>
    Change-Id: I22e8563dd998cc50736c48c4ff21ceb74157c295

--- frameworks/bridge/declarative_frontend/jsview/js_view_context.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_view_context.cpp
@@ -178,7 +178,7 @@ void AnimateToForStageMode(const RefPtr<PipelineBase>& pipelineContext, Animatio
             option.GetDuration(), option.GetCurve()->ToString().c_str(), option.GetIteration());
     }
     NG::ScopedViewStackProcessor scopedProcessor;
-    AceEngine::Get().NotifyContainers([triggerId](const RefPtr<Container>& container) {
+    AceEngine::Get().NotifyContainersOrderly([triggerId](const RefPtr<Container>& container) {
         if (!CheckContainer(container)) {
             return;
         }
@@ -195,7 +195,7 @@ void AnimateToForStageMode(const RefPtr<PipelineBase>& pipelineContext, Animatio
     // Execute the function.
     jsAnimateToFunc->Call(jsAnimateToFunc);
     pipelineContext->FlushOnceVsyncTask();
-    AceEngine::Get().NotifyContainers([triggerId](const RefPtr<Container>& container) {
+    AceEngine::Get().NotifyContainersOrderly([triggerId](const RefPtr<Container>& container) {
         if (!CheckContainer(container)) {
             return;
         }
