322a1812af07e689014ad23dab916cc33a745f45
zhangjinyu101 <zhangjinyu101@huawei.com>
2024-07-25 15:18:43 +0800

    解决滚动条切换效果没有实时表现
    Signed-off-by: zhangjinyu101 <zhangjinyu101@huawei.com>

--- frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
+++ frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
@@ -732,6 +732,7 @@ void ScrollablePattern::SetScrollBar(DisplayMode displayMode)
             scrollBarOverlayModifier_->SetOpacity(UINT8_MAX);
         }
         scrollBar_->ScheduleDisappearDelayTask();
+        UpdateScrollBarOffset();
     }
     UpdateBorderRadius();
 }
