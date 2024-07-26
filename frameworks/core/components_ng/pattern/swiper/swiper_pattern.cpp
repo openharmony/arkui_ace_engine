ccd9f877070efcb2c75ddd731b537d6c09a36be0
jiangzhijun8 <jiangzhijun7@huawei.com>
2024-07-26 14:15:06 +0800

    list组件切换维吾尔语父组件联动滑动问题
    Signed-off-by: jiangzhijun8 <jiangzhijun7@huawei.com>

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
@@ -4910,7 +4910,9 @@ bool SwiperPattern::HandleScrollVelocity(float velocity, const RefPtr<NestableSc
     if (IsDisableSwipe()) {
         return false;
     }
-
+    if (IsHorizontalAndRightToLeft()) {
+        velocity = -velocity;
+    }
     DestructSetter<bool> scope(childScrolling_, false);
     // haven't reached edge
     if (GetDistanceToEdge() > 0.0f || IsLoop()) {
@@ -4933,6 +4935,9 @@ bool SwiperPattern::HandleScrollVelocity(float velocity, const RefPtr<NestableSc
 
 ScrollResult SwiperPattern::HandleScroll(float offset, int32_t source, NestedState state, float velocity)
 {
+    if (IsHorizontalAndRightToLeft() && state != NestedState::GESTURE) {
+        offset = -offset;
+    }
     if (IsDisableSwipe()) {
         return { offset, true };
     }
