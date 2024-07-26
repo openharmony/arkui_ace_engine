c3f7362ee7ec0105fb7bad76dcfbd384e9c6b14b
zhoukechen <zhoukechen2@huawei.com>
2024-07-26 19:36:51 +0800

    add more log
    Signed-off-by: zhoukechen <zhoukechen2@huawei.com>
    Change-Id: I14054b8ae04a7a81b8b17bc2301cb70b55edff82

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -3638,6 +3638,7 @@ void TextFieldPattern::KeyboardContentTypeToInputType()
 
 bool TextFieldPattern::RequestKeyboard(bool isFocusViewChanged, bool needStartTwinkling, bool needShowSoftKeyboard)
 {
+    TAG_LOGI(AceLogTag::ACE_TEXT_FIELD, "textfield ready to request keyboard");
     if (!showKeyBoardOnFocus_ || !HasFocus()) {
         return false;
     }
@@ -4790,6 +4791,7 @@ void TextFieldPattern::RequestKeyboardOnFocus()
     auto textFieldManager = DynamicCast<TextFieldManagerNG>(pipeline->GetTextFieldManager());
     CHECK_NULL_VOID(textFieldManager);
     textFieldManager->SetNeedToRequestKeyboard(true);
+    TAG_LOGI(AceLogTag::ACE_TEXT_FIELD, "textfield %{public}d add requestkeyboard task", host->GetId());
     pipeline->AddAfterLayoutTask([weak = WeakClaim(this), manager = WeakPtr<TextFieldManagerNG>(textFieldManager)]() {
         auto textField = weak.Upgrade();
         CHECK_NULL_VOID(textField);
