6e0463deae645d8c100308711fe45d4771cab38f
xuzhidan <xuzhidan1@huawei.com>
2024-07-26 03:17:06 +0000

    回退 'Pull Request !38672 : 修复Swiper圆点导航点超长显示时，在首页来回跟手滑动，选中导航点显示异常问题'

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.h
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.h
@@ -825,7 +825,7 @@ private:
 
     inline bool ChildFirst(NestedState state);
     void HandleTouchBottomLoop();
-    GestureState CalculateGestureState(float additionalOffset, int32_t currentIndex, int32_t currentFirstIndex) const;
+    void CalculateGestureState(float additionalOffset, float currentTurnPageRate, int32_t preFirstIndex);
     std::pair<float, float> CalcCurrentPageStatus(float additionalOffset) const;
     std::pair<float, float> CalcCurrentPageStatusOnRTL(float additionalOffset) const;
     void StopIndicatorAnimation(bool ifImmediately = false);
@@ -975,6 +975,7 @@ private:
     bool indicatorIsBoolean_ = true;
     bool isAtHotRegion_ = false;
     bool isDragging_ = false;
+    bool needTurn_ = false;
     bool isParentHiddenChange_ = false;
     /**
      * @brief Indicates whether the child NestableScrollContainer is currently scrolling and affecting Swiper.
