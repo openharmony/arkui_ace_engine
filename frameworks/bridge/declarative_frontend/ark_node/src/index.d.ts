9b85f8a6f0f2f658a12d47856f812e8c08cdb217
wangchensu <wangchensu@huawei.com>
2024-07-28 17:25:26 +0800

    修复不支持嵌套使用入参不一致的builderNode
    Signed-off-by: wangchensu <wangchensu@huawei.com>
    Change-Id: Idf6c7e9af3ca1f208f0a12f89f4e47e52600e0c3

--- frameworks/bridge/declarative_frontend/ark_node/src/index.d.ts
+++ frameworks/bridge/declarative_frontend/ark_node/src/index.d.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -57,7 +57,9 @@ declare enum XComponentType {
 }
 
 declare interface XComponentController { }
-
+declare interface BuildOptions {
+  nestingBuilderSupported?: boolean;
+}
 declare abstract class ViewPU {
   id__(): number;
   aboutToUpdate?: (updateParams: Object) => void;
