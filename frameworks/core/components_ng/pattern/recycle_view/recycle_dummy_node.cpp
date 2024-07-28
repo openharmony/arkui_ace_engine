443e0cf195e18997d49fae6eb05e60243af05f61
hwx1119949 <huyisuo@huawei.com>
2024-07-28 17:42:16 +0800

    删除多余头文件
    Signed-off-by: hwx1119949 <huyisuo@huawei.com>

--- frameworks/core/components_ng/pattern/recycle_view/recycle_dummy_node.cpp
+++ frameworks/core/components_ng/pattern/recycle_view/recycle_dummy_node.cpp
@@ -20,7 +20,6 @@
 #include "core/components_ng/pattern/custom/custom_node_base.h"
 #include "core/components_v2/inspector/inspector_constants.h"
 #include "core/pipeline/base/element_register.h"
-#include "core/pipeline_ng/pipeline_context.h"
 
 namespace OHOS::Ace::NG {
 RefPtr<RecycleDummyNode> RecycleDummyNode::CreateRecycleDummyNode(int32_t nodeId)
