bb520a0302267c7b7d9a873f674598c113d4b67e
zhanghangkai@huawei.com <zhanghangkai@huawei.com>
2024-07-30 23:16:09 +0800

    fix fake accessibility manager cpp compile
    Signed-off-by: zhanghangkai@huawei.com <zhanghangkai@huawei.com>
    Change-Id: I5cb286ceb958a297a49255d01dc368c9909dca1b

--- adapter/ohos/osal/fake_accessibility_manager.cpp
+++ adapter/ohos/osal/fake_accessibility_manager.cpp
@@ -16,8 +16,53 @@
 #include "frameworks/bridge/common/accessibility/accessibility_node_manager.h"
 
 namespace OHOS::Ace::Framework {
+class FakeAccessibilityManager : public AccessibilityNodeManager {
+    void SearchElementInfoByAccessibilityIdNG(int64_t elementId, int32_t mode,
+        std::list<Accessibility::AccessibilityElementInfo>& infos, const RefPtr<PipelineBase>& context,
+        int64_t uiExtensionOffset) override;
+    void SearchElementInfosByTextNG(int64_t elementId, const std::string& text,
+        std::list<Accessibility::AccessibilityElementInfo>& infos, const RefPtr<PipelineBase>& context,
+        const int64_t uiExtensionOffset = 0) override;
+    void FindFocusedElementInfoNG(int64_t elementId, int32_t focusType,
+        Accessibility::AccessibilityElementInfo& info, const RefPtr<PipelineBase>& context,
+        const int64_t uiExtensionOffset = 0) override;
+    void FocusMoveSearchNG(int64_t elementId, int32_t direction, Accessibility::AccessibilityElementInfo& info,
+        const RefPtr<PipelineBase>& context, const int64_t uiExtensionOffset = 0) override;
+    bool ExecuteExtensionActionNG(int64_t elementId, const std::map<std::string, std::string>& actionArguments,
+        int32_t action, const RefPtr<PipelineBase>& context, int64_t uiExtensionOffset) override;
+};
+
+void FakeAccessibilityManager::SearchElementInfoByAccessibilityIdNG(int64_t elementId, int32_t mode,
+        std::list<Accessibility::AccessibilityElementInfo>& infos, const RefPtr<PipelineBase>& context,
+        int64_t uiExtensionOffset)
+{
+}
+
+void FakeAccessibilityManager::SearchElementInfosByTextNG(int64_t elementId, const std::string& text,
+    std::list<Accessibility::AccessibilityElementInfo>& infos, const RefPtr<PipelineBase>& context,
+    const int64_t uiExtensionOffset)
+{
+}
+
+void FakeAccessibilityManager::FindFocusedElementInfoNG(int64_t elementId, int32_t focusType,
+        Accessibility::AccessibilityElementInfo& info, const RefPtr<PipelineBase>& context,
+        const int64_t uiExtensionOffset)
+{
+}
+
+void FakeAccessibilityManager::FocusMoveSearchNG(int64_t elementId, int32_t direction, Accessibility::AccessibilityElementInfo& info,
+        const RefPtr<PipelineBase>& context, const int64_t uiExtensionOffset)
+{
+}
+
+bool FakeAccessibilityManager::ExecuteExtensionActionNG(int64_t elementId, const std::map<std::string, std::string>& actionArguments,
+        int32_t action, const RefPtr<PipelineBase>& context, int64_t uiExtensionOffset)
+{
+    return false;
+}
+
 RefPtr<AccessibilityNodeManager> AccessibilityNodeManager::Create()
 {
-    return AceType::MakeRefPtr<AccessibilityNodeManager>();
+    return AceType::MakeRefPtr<FakeAccessibilityManager>();
 }
 } // namespace OHOS::Ace::Framework
