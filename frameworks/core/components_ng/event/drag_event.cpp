b44318ff60576b00c0a8cba54c12e1b72f4ce0ad
hct95 <hechentao@huawei.com>
2024-07-31 11:44:01 +0000

    修复拖拽场景异常菜单未消失的问题
    Signed-off-by: hct95 <hechentao@huawei.com>
    Change-Id: Icb936e3fa0dfb9c400f8d055d57366bcb3cd3982

--- frameworks/core/components_ng/event/drag_event.cpp
+++ frameworks/core/components_ng/event/drag_event.cpp
@@ -260,7 +260,7 @@ void DragEventActuator::OnCollectTouchTarget(const OffsetF& coordinateOffset, co
                 } else {
                     gestureHub->SetPixelMap(nullptr);
                 }
-            } else if (!isNotInPreviewState_) {
+            } else {
                 HideEventColumn();
                 if (gestureHub->GetTextDraggable()) {
                     HideTextAnimation(true, info.GetGlobalLocation().GetX(), info.GetGlobalLocation().GetY());
@@ -292,16 +292,11 @@ void DragEventActuator::OnCollectTouchTarget(const OffsetF& coordinateOffset, co
                 } else if (!gestureHub->GetIsTextDraggable()) {
                     gestureHub->SetPixelMap(nullptr);
                 }
-            } else if (!isNotInPreviewState_) {
+            } else {
                 HideEventColumn();
                 HidePixelMap(true, info.GetGlobalLocation().GetX(), info.GetGlobalLocation().GetY());
                 HideFilter();
                 SubwindowManager::GetInstance()->HideMenuNG(false, true);
-                actuator->UpdatePreviewOptionFromModifier(frameNode);
-            } else {
-                // For the drag initiacating from mouse, there is no chance to execute the modifier in the floating
-                // pharse, so need to update here to make sure the preview option is available to pass through to drag
-                // framwork later.
                 actuator->UpdatePreviewOptionFromModifier(frameNode);
             }
         }
