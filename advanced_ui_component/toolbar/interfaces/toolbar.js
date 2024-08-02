91d48d21090023290ba3781645ef850b8cd2cc7c
shashaH <huangshasha6@h-partners.com>
2024-08-02 17:14:40 +0800

    高级组件ToolBar基础能力增强
    Signed-off-by: shashaH <huangshasha6@h-partners.com>

--- advanced_ui_component/toolbar/interfaces/toolbar.js
+++ advanced_ui_component/toolbar/interfaces/toolbar.js
@@ -1,17 +1,18 @@
 /*
- * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the 'License');
+ * Copyright (c) 2023-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 let __decorate = (this && this.__decorate) || function (m4, n4, o4, p4) {
   let q4 = arguments.length, r4 = q4 < 3 ? n4 : p4 === null ? p4 = Object.getOwnPropertyDescriptor(n4, o4) : p4, s4;
   if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
