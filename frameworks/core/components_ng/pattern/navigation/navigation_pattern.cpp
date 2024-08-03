57a43a3a50159c6c841cc0de29a75a183b5a6ef3
huangxiaolinabc123 <huangxiaolin12@huawei.com>
2024-08-03 09:43:15 +0000

    modify code
    Signed-off-by: huangxiaolinabc123 <huangxiaolin12@huawei.com>
    Change-Id: Id897a7cf3862efd07e18d5a43a592dbd5ce07474

--- frameworks/core/components_ng/pattern/navigation/navigation_pattern.cpp
+++ frameworks/core/components_ng/pattern/navigation/navigation_pattern.cpp
@@ -1004,6 +1004,7 @@ void NavigationPattern::TransitionWithOutAnimation(const RefPtr<NavDestinationGr
     // navDestination push/pop navDestination
     if (newTopNavDestination && preTopNavDestination) {
         if (isPopPage) {
+            newTopNavDestination->SetTransitionType(PageTransitionType::ENTER_POP);
             auto parent = preTopNavDestination->GetParent();
             CHECK_NULL_VOID(parent);
             auto preTopNavDestinationPattern = preTopNavDestination->GetPattern<NavDestinationPattern>();
@@ -1017,6 +1018,8 @@ void NavigationPattern::TransitionWithOutAnimation(const RefPtr<NavDestinationGr
             parent->RemoveChild(preTopNavDestination, true);
             parent->MarkDirtyNode(PROPERTY_UPDATE_MEASURE);
         } else {
+            preTopNavDestination->SetTransitionType(PageTransitionType::EXIT_PUSH);
+            newTopNavDestination->SetTransitionType(PageTransitionType::ENTER_PUSH);
             DealTransitionVisibility(preTopNavDestination, needVisible, false);
         }
         navigationNode->RemoveDialogDestination();
@@ -1027,6 +1030,11 @@ void NavigationPattern::TransitionWithOutAnimation(const RefPtr<NavDestinationGr
     // navBar push navDestination
     if (newTopNavDestination && newTopNavDestination->GetNavDestinationMode() == NavDestinationMode::STANDARD &&
         navigationMode_ == NavigationMode::STACK) {
+        auto navBar = AceType::DynamicCast<NavBarNode>(navBarNode);
+        if (navBar) {
+            navBar->SetTransitionType(PageTransitionType::EXIT_PUSH);
+        }
+        newTopNavDestination->SetTransitionType(PageTransitionType::ENTER_PUSH);
         DealTransitionVisibility(navBarNode, false, true);
         navigationNode->SetNeedSetInvisible(true);
     }
@@ -1045,6 +1053,10 @@ void NavigationPattern::TransitionWithOutAnimation(const RefPtr<NavDestinationGr
         }
         parent->RemoveChild(preTopNavDestination, true);
         navigationNode->SetNeedSetInvisible(false);
+        auto navBar = AceType::DynamicCast<NavBarNode>(navBarNode);
+        if (navBar) {
+            navBar->SetTransitionType(PageTransitionType::ENTER_POP);
+        }
         parent->MarkDirtyNode(PROPERTY_UPDATE_MEASURE);
     }
     navigationNode->RemoveDialogDestination();
@@ -1990,6 +2002,17 @@ void NavigationPattern::DealTransitionVisibility(const RefPtr<FrameNode>& node, 
     renderContext->SetTransitionOutCallback([weakNode = WeakPtr<FrameNode>(node), isVisible, isNavBar] {
         auto curNode = weakNode.Upgrade();
         CHECK_NULL_VOID(curNode);
+        if (isNavBar) {
+            auto navBarNode = AceType::DynamicCast<NavBarNode>(curNode);
+            if (navBarNode->GetTransitionType() != PageTransitionType::EXIT_PUSH) {
+                return;
+            }
+        } else {
+            auto navDestinationNode = AceType::DynamicCast<NavDestinationGroupNode>(curNode);
+            if (navDestinationNode->GetTransitionType() != PageTransitionType::EXIT_PUSH) {
+                return;
+            }
+        }
         curNode->SetJSViewActive(isVisible);
     });
 }
