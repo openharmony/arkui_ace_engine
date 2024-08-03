852f0e770fb3d812c5ed4a2c1115dbcd97103f0a
zhang-quanchao <zhangquanchao@huawei.com>
2024-08-03 12:08:07 +0800

    增加LinearIndicator TDD覆盖率
    Signed-off-by: zhang-quanchao <zhangquanchao@huawei.com>

--- test/mock/core/pipeline/mock_pipeline_context.cpp
+++ test/mock/core/pipeline/mock_pipeline_context.cpp
@@ -783,6 +783,11 @@ RefPtr<PipelineBase> PipelineBase::GetCurrentContextSafely()
     return NG::MockPipelineContext::GetCurrent();
 }
 
+RefPtr<PipelineBase> PipelineBase::GetCurrentContextSafelyWithCheck()
+{
+    return NG::MockPipelineContext::GetCurrent();
+}
+
 double PipelineBase::GetCurrentDensity()
 {
     auto pipelineContext = NG::MockPipelineContext::GetCurrentContext();
