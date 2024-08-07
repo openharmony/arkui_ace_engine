5f36e5588bd949d549b83f0da844abd72e2e1d12
13650230359 <qinxin2@huawei.com>
2024-08-07 11:29:19 +0800

    同步
    Signed-off-by: c30041016 <chenjunxiang4@huawei.com>
    Change-Id: I8e3c3e586ed7cc12f12135b5c2e710b220f30374

--- adapter/ohos/osal/js_accessibility_manager.cpp
+++ adapter/ohos/osal/js_accessibility_manager.cpp
@@ -2434,11 +2434,18 @@ bool JsAccessibilityManager::TransferAccessibilityAsyncEvent(
     auto uiExtensionManager = ngPipeline->GetUIExtensionManager();
     CHECK_NULL_RETURN(uiExtensionManager, false);
     auto container = Container::GetContainer(ngPipeline->GetInstanceId());
-    if (!IsRegister() && !(container && container->IsDynamicRender())) {
+    bool isDynamicRender = container && container->IsDynamicRender();
+    if (!IsRegister() && !isDynamicRender) {
         return false;
     }
     AccessibilityEventInfo eventInfoNew = eventInfo;
-    eventInfoNew.SetSource(uiExtensionOffset * GetUiextensionId() + eventInfo.GetViewId());
+    if (isDynamicRender) {
+        auto focusedContainer = Container::GetFoucsed();
+        if (focusedContainer) {
+            eventInfoNew.SetWindowId(focusedContainer->GetWindowId());
+        }
+    }
+    eventInfoNew.SetSource(uiExtensionOffset + eventInfo.GetViewId());
     AccessibilityElementInfo elementInfo;
     FillElementInfo(eventInfoNew.GetAccessibilityId(), elementInfo, pipeline, Claim(this),
         FillEventInfoParam { eventInfoNew.GetAccessibilityId(), -1, eventInfoNew.GetWindowId() });
