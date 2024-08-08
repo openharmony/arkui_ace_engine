3b5aa450d98cedbd7ff9585e5a69ad915011b0a0
j21p3 <yanjun59@huawei.com>
2024-08-08 16:07:54 +0800

    security component cover check fix
    Change-Id: I6cff55880300303fd55a21034e35c720f420d9f1
    Signed-off-by: j21p3 <yanjun59@huawei.com>

--- frameworks/core/components_ng/pattern/security_component/security_component_handler.cpp
+++ frameworks/core/components_ng/pattern/security_component/security_component_handler.cpp
@@ -366,12 +366,12 @@ bool SecurityComponentHandler::InitBaseInfo(OHOS::Security::SecurityComponent::S
         layoutProperty->GetTextIconSpace().value_or(theme->GetTextIconSpace()).ConvertToVp();
 
     if (!GetDisplayOffset(node, buttonInfo.rect_.x_, buttonInfo.rect_.y_)) {
-        LOGW("Get display offset failed");
+        LOGW("InitBaseInfoWarning: Get display offset failed");
         return false;
     }
 
     if (!GetWindowRect(node, buttonInfo.windowRect_)) {
-        LOGW("Get window rect failed");
+        LOGW("InitBaseInfoWarning: Get window rect failed");
         return false;
     }
     auto render = node->GetRenderContext();
@@ -612,7 +612,7 @@ bool SecurityComponentHandler::CheckRectIntersect(const RectF& dest, int32_t sec
     std::unordered_map<int32_t, int32_t>& nodeId2Zindex)
 {
     for (const auto& originRect : nodeId2Rect) {
-        if (originRect.second.IsInnerIntersectWith(dest) &&
+        if (originRect.second.IsInnerIntersectWithRound(dest) &&
             (nodeId2Zindex[secNodeId] <= nodeId2Zindex[originRect.first])) {
             return true;
         }
@@ -705,7 +705,7 @@ int32_t SecurityComponentHandler::ReportSecurityComponentClickEvent(int32_t& scI
     uint8_t defaultData = 0;
     std::vector<uint8_t> dataBuffer;
     if (pointerEvent == nullptr) {
-        LOGW("Receive a NULL pointerEvent, set default data.");
+        LOGW("SecurityComponentClickEventWarning: Receive a NULL pointerEvent, set default data.");
         secEvent.extraInfo.data = &defaultData;
         secEvent.extraInfo.dataSize = 1;
         secEvent.point.timestamp = 0;
@@ -728,7 +728,7 @@ int32_t SecurityComponentHandler::ReportSecurityComponentClickEvent(int32_t& scI
         return -1;
     }
     if (CheckComponentCoveredStatus(node->GetId())) {
-        LOGW("Security component is covered by another component.");
+        LOGW("SecurityComponentCheckFail: Security component is covered by another component.");
         return -1;
     }
 
@@ -757,7 +757,7 @@ int32_t SecurityComponentHandler::ReportSecurityComponentClickEvent(int32_t& scI
         return -1;
     }
     if (CheckComponentCoveredStatus(node->GetId())) {
-        LOGW("Security component is covered by another component.");
+        LOGW("SecurityComponentCheckFail: Security component is covered by another component.");
         return -1;
     }
     return ReportSecurityComponentClickEventInner(scId, node, secEvent, std::move(callback));
