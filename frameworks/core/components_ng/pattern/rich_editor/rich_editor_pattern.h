8b5b9c0beb854cf0170324f92b9d78b079b7a9f7
carnivore233 <xuyue51@huawei.com>
2024-08-08 22:04:35 +0800

    【RichEditor】修改深浅色模式切换GetTypingStyle接口返回值未跟随问题
    Signed-off-by: carnivore233 <xuyue51@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.h
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.h
@@ -73,6 +73,20 @@ struct TextConfig;
     } while (false)
 #define CONTENT_MODIFY_LOCK(patternPtr) ContentModifyLock contentModifyLock(patternPtr)
 
+#define IF_TRUE(cond, func) \
+    do {                    \
+        if (cond) {         \
+            func;           \
+        }                   \
+    } while (false)
+
+#define IF_PRESENT(opt, func) \
+    do {                      \
+        if (opt) {            \
+            opt->func;        \
+        }                     \
+    } while (false)
+
 namespace OHOS::Ace::NG {
 class InspectorFilter;
 
@@ -474,6 +488,7 @@ public:
     void UpdateParagraphStyle(RefPtr<SpanNode> spanNode, const struct UpdateParagraphStyle& style);
     std::vector<ParagraphInfo> GetParagraphInfo(int32_t start, int32_t end);
     void SetTypingStyle(std::optional<struct UpdateSpanStyle> typingStyle, std::optional<TextStyle> textStyle);
+    std::optional<struct UpdateSpanStyle> GetTypingStyle();
     int32_t AddImageSpan(const ImageSpanOptions& options, bool isPaste = false, int32_t index = -1,
         bool updateCaret = true);
     void DisableDrag(RefPtr<ImageSpanNode> imageNode);
@@ -1238,6 +1253,7 @@ private:
     ACE_DISALLOW_COPY_AND_MOVE(RichEditorPattern);
     bool keyboardAvoidance_ = false;
     int32_t richEditorInstanceId_ = -1;
+    int32_t frameId_ = -1;
     bool contentChange_ = false;
     PreviewTextRecord previewTextRecord_;
     bool isTextPreviewSupported_ = true;
