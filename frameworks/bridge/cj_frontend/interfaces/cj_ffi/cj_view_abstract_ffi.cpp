d9eafd8897ae5ec49e9aef3224075d9053ee4ec0
余思远 <1164818708@qq.com>
2024-08-09 16:13:28 +0800

    bugfix image position
    Signed-off-by: 余思远 <1164818708@qq.com>
    Change-Id: Ibf4e66fbe20bb7f77e9ff5bb4f3f9fba471897c5

--- frameworks/bridge/cj_frontend/interfaces/cj_ffi/cj_view_abstract_ffi.cpp
+++ frameworks/bridge/cj_frontend/interfaces/cj_ffi/cj_view_abstract_ffi.cpp
@@ -578,6 +578,7 @@ void FfiOHOSAceFrameworkViewAbstractSetBackgroundImagePositionAlign(int32_t alig
         return;
     }
     BackgroundImagePosition bgImgPosition;
+    bgImgPosition.SetIsAlign(true);
     UpdateBackgroundImagePosition(static_cast<Align>(align), bgImgPosition);
     ViewAbstractModel::GetInstance()->SetBackgroundImagePosition(bgImgPosition);
 }
