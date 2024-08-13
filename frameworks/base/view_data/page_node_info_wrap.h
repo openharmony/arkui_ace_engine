695a7825dee2c1a7e4a5110d44d37115e569eed0
sysulzf <976502029@qq.com>
2024-08-13 20:05:42 +0800

    处理代码冲突
    Signed-off-by: sysulzf <976502029@qq.com>

--- frameworks/base/view_data/page_node_info_wrap.h
+++ frameworks/base/view_data/page_node_info_wrap.h
@@ -60,10 +60,7 @@ public:
     virtual const std::string& GetPlaceholder() const = 0;
 
     virtual void SetMetadata(const std::string& metadata) {}
-    virtual const std::string& GetMetadata() const
-    {
-        return "";
-    }
+    virtual const std::string& GetMetadata() const = 0;
 
     virtual void SetPasswordRules(const std::string& passwordRules) {}
     virtual const std::string& GetPasswordRules() const = 0;
