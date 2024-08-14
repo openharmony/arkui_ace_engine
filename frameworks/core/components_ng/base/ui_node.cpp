1f0fffdb13fdbffd3e5d16fee3dd32302f329c6f
zoulinken <zoulinken1@huawei.com>
2024-08-14 16:45:56 +0800

    3900日志整改
    Signed-off-by: zoulinken <zoulinken1@huawei.com>

--- frameworks/core/components_ng/base/ui_node.cpp
+++ frameworks/core/components_ng/base/ui_node.cpp
@@ -956,12 +956,6 @@ PipelineContext* UINode::GetContext() const
             context = PipelineContext::GetCurrentContextPtrSafely();
         }
     }
-
-    if (context && context->IsDestroyed()) {
-        LOGW("Get context from node when the context is destroyed. The context_ of node is%{public}s nullptr",
-            context_? " not" : "");
-    }
-
     return context;
 }
 
