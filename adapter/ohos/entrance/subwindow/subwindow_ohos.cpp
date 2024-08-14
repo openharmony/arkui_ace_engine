a0cb6655f5027d577c7897fb263f92a201fcdf4b
zhangyafei-echo <zhangyafei12@huawei.com>
2024-08-14 14:32:17 +0800

    Revert "Support recovery when background and killed by resource_schedule."
    This reverts commit a78a33ecd2d9f250d632451c354d0e7555b80a4c.
    
    Signed-off-by: zhangyafei-echo <zhangyafei12@huawei.com>
    Change-Id: Ia86fc6d50cba5a442acc80d34b8589573232cc6f

--- adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
+++ adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
@@ -144,7 +144,7 @@ bool SubwindowOhos::InitContainer()
         return false;
     }
     subSurface->SetShadowElevation(0.0f);
-    window_->NapiSetUIContent(url, nullptr, nullptr, Rosen::BackupAndRestoreType::NONE);
+    window_->NapiSetUIContent(url, nullptr, nullptr, false);
     childContainerId_ = SubwindowManager::GetInstance()->GetContainerId(window_->GetWindowId());
     TAG_LOGD(AceLogTag::ACE_SUB_WINDOW, "Window child containerId : %{public}d", childContainerId_);
     SubwindowManager::GetInstance()->AddParentContainerId(childContainerId_, parentContainerId_);
@@ -1043,7 +1043,7 @@ bool SubwindowOhos::InitToastDialogView(int32_t width, int32_t height, float den
 {
     TAG_LOGD(AceLogTag::ACE_SUB_WINDOW, "init toast dialog view enter");
 #ifndef NG_BUILD
-    dialogWindow_->NapiSetUIContent("", nullptr, nullptr, Rosen::BackupAndRestoreType::NONE);
+    dialogWindow_->NapiSetUIContent("", nullptr, nullptr, false);
     childContainerId_ = SubwindowManager::GetInstance()->GetContainerId(dialogWindow_->GetWindowId());
     SubwindowManager::GetInstance()->AddParentContainerId(childContainerId_, parentContainerId_);
     ContainerScope scope(childContainerId_);
