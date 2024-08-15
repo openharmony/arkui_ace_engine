e330349d086a2ffe486d9f5478879d5c1c908440
sunziyuan <sunziyuan1@huawei.com>
2024-08-15 03:10:37 +0000

    fix menu适配深色模式时系统svg图标判断逻辑问题
    Signed-off-by: sunziyuan <sunziyuan1@huawei.com>
    Change-Id: Ifd24db817d6dd73a82bdd2a069ad72f816e365c3

--- frameworks/core/components_ng/pattern/menu/menu_item/menu_item_pattern.cpp
+++ frameworks/core/components_ng/pattern/menu/menu_item/menu_item_pattern.cpp
@@ -56,9 +56,9 @@ constexpr double DAMPING = 33.0f;
 constexpr double SEMI_CIRCLE_ANGEL = 180.0f;
 constexpr float OPACITY_EFFECT = 0.99;
 const std::string SYSTEM_RESOURCE_PREFIX = std::string("resource:///");
-// id of system resource start from 0x07000000, included
+// id of system resource start from 0x07000000
 constexpr unsigned long MIN_SYSTEM_RESOURCE_ID = 0x07000000;
-// id of system resource end to 0x07FFFFFF, excluded
+// id of system resource end to 0x07FFFFFF
 constexpr unsigned long MAX_SYSTEM_RESOURCE_ID = 0x07FFFFFF;
 
 void UpdateFontSize(RefPtr<TextLayoutProperty>& textProperty, RefPtr<MenuLayoutProperty>& menuProperty,
@@ -1287,7 +1287,7 @@ bool MenuItemPattern::UseDefaultThemeIcon(const ImageSourceInfo& imageSourceInfo
         return (srcId.find("ohos_") != std::string::npos)
             || ((std::all_of(srcId.begin(), srcId.end(), ::isdigit))
                 && (std::stoul(srcId) >= MIN_SYSTEM_RESOURCE_ID)
-                && (std::stoul(srcId) < MAX_SYSTEM_RESOURCE_ID));
+                && (std::stoul(srcId) <= MAX_SYSTEM_RESOURCE_ID));
     }
     return false;
 }
