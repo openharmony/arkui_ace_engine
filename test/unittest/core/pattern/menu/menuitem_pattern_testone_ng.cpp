acea8890a5f81505deb0c8cfd05188b63c715452
jiangzhijun8 <jiangzhijun7@huawei.com>
2024-08-16 17:11:32 +0800

    menu tdd覆盖率提升
    Signed-off-by: jiangzhijun8 <jiangzhijun7@huawei.com>

---
+++ test/unittest/core/pattern/menu/menuitem_pattern_testone_ng.cpp
@@ -0,0 +1,391 @@
+/*
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <type_traits>
+
+#include "gtest/gtest.h"
+
+#define private public
+#define protected public
+
+#include "test/mock/core/common/mock_theme_manager.h"
+#include "test/mock/core/pipeline/mock_pipeline_context.h"
+#include "test/mock/core/render/mock_render_context.h"
+#include "test/mock/core/rosen/mock_canvas.h"
+#include "test/mock/core/rosen/testing_canvas.h"
+
+#include "core/components/common/layout/constants.h"
+#include "core/components/common/layout/grid_system_manager.h"
+#include "core/components/common/properties/shadow_config.h"
+#include "core/components/container_modal/container_modal_constants.h"
+#include "core/components/select/select_theme.h"
+#include "core/components/theme/shadow_theme.h"
+#include "core/components_ng/base/view_stack_processor.h"
+#include "core/components_ng/pattern/image/image_layout_property.h"
+#include "core/components_ng/pattern/image/image_pattern.h"
+#include "core/components_ng/pattern/menu/menu_item/menu_item_model_ng.h"
+#include "core/components_ng/pattern/menu/menu_item/menu_item_pattern.h"
+#include "core/components_ng/pattern/menu/menu_item_group/menu_item_group_pattern.h"
+#include "core/components_ng/pattern/menu/menu_item_group/menu_item_group_view.h"
+#include "core/components_ng/pattern/menu/menu_model_ng.h"
+#include "core/components_ng/pattern/menu/menu_pattern.h"
+#include "core/components_ng/pattern/menu/menu_theme.h"
+#include "core/components_ng/pattern/menu/menu_view.h"
+#include "core/components_ng/pattern/menu/multi_menu_layout_algorithm.h"
+#include "core/components_ng/pattern/menu/preview/menu_preview_layout_algorithm.h"
+#include "core/components_ng/pattern/menu/preview/menu_preview_pattern.h"
+#include "core/components_ng/pattern/menu/sub_menu_layout_algorithm.h"
+#include "core/components_ng/pattern/menu/wrapper/menu_wrapper_pattern.h"
+#include "core/components_ng/pattern/pattern.h"
+#include "core/components_ng/pattern/root/root_pattern.h"
+#include "core/components_ng/pattern/scroll/scroll_pattern.h"
+#include "core/components_ng/pattern/text/text_layout_property.h"
+#include "core/components_ng/pattern/text/text_pattern.h"
+#include "core/components_ng/property/border_property.h"
+#include "core/components_ng/property/measure_property.h"
+#include "core/components_ng/syntax/lazy_for_each_model.h"
+#include "core/components_ng/syntax/lazy_layout_wrapper_builder.h"
+#include "core/event/touch_event.h"
+
+using namespace testing;
+using namespace testing::ext;
+using namespace OHOS::Ace::Framework;
+
+namespace OHOS::Ace::NG {
+namespace {} // namespace
+class MenuItemPatternTestOneNg : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+    void InitMenuItemPatternTestOneNg();
+    void InitMenuItemTestNg();
+    void MockPipelineContextGetTheme();
+    PaintWrapper* GetPaintWrapper(RefPtr<MenuPaintProperty> paintProperty);
+    RefPtr<FrameNode> GetPreviewMenuWrapper(
+        SizeF itemSize = SizeF(0.0f, 0.0f), std::optional<MenuPreviewAnimationOptions> scaleOptions = std::nullopt);
+    RefPtr<FrameNode> menuFrameNode_;
+    RefPtr<MenuAccessibilityProperty> menuAccessibilityProperty_;
+    RefPtr<FrameNode> menuItemFrameNode_;
+    RefPtr<MenuItemPattern> menuItemPattern_;
+    RefPtr<MenuItemAccessibilityProperty> menuItemAccessibilityProperty_;
+};
+
+void MenuItemPatternTestOneNg::SetUpTestCase() {}
+
+void MenuItemPatternTestOneNg::TearDownTestCase() {}
+
+void MenuItemPatternTestOneNg::SetUp()
+{
+    MockPipelineContext::SetUp();
+    auto themeManager = AceType::MakeRefPtr<MockThemeManager>();
+    MockPipelineContext::GetCurrent()->SetThemeManager(themeManager);
+    EXPECT_CALL(*themeManager, GetTheme(_)).WillRepeatedly(Return(AceType::MakeRefPtr<SelectTheme>()));
+}
+
+void MenuItemPatternTestOneNg::MockPipelineContextGetTheme()
+{
+    auto themeManager = AceType::MakeRefPtr<MockThemeManager>();
+    MockPipelineContext::GetCurrent()->SetThemeManager(themeManager);
+    EXPECT_CALL(*themeManager, GetTheme(_)).WillRepeatedly([](ThemeType type) -> RefPtr<Theme> {
+        if (type == TextTheme::TypeId()) {
+            return AceType::MakeRefPtr<TextTheme>();
+        } else if (type == IconTheme::TypeId()) {
+            return AceType::MakeRefPtr<IconTheme>();
+        } else if (type == SelectTheme::TypeId()) {
+            return AceType::MakeRefPtr<SelectTheme>();
+        } else {
+            return AceType::MakeRefPtr<MenuTheme>();
+        }
+    });
+}
+
+void MenuItemPatternTestOneNg::TearDown()
+{
+    MockPipelineContext::TearDown();
+    menuFrameNode_ = nullptr;
+    menuAccessibilityProperty_ = nullptr;
+    menuItemFrameNode_ = nullptr;
+    menuItemPattern_ = nullptr;
+    menuItemAccessibilityProperty_ = nullptr;
+    SystemProperties::SetDeviceType(DeviceType::PHONE);
+    ScreenSystemManager::GetInstance().dipScale_ = 1.0;
+    SystemProperties::orientation_ = DeviceOrientation::PORTRAIT;
+}
+
+void MenuItemPatternTestOneNg::InitMenuItemPatternTestOneNg()
+{
+    menuFrameNode_ = FrameNode::GetOrCreateFrameNode(V2::MENU_TAG, ViewStackProcessor::GetInstance()->ClaimNodeId(),
+        []() { return AceType::MakeRefPtr<MenuPattern>(3, "", MenuType::MENU); });
+    ASSERT_NE(menuFrameNode_, nullptr);
+
+    menuAccessibilityProperty_ = menuFrameNode_->GetAccessibilityProperty<MenuAccessibilityProperty>();
+    ASSERT_NE(menuAccessibilityProperty_, nullptr);
+}
+
+void MenuItemPatternTestOneNg::InitMenuItemTestNg()
+{
+    menuItemFrameNode_ = FrameNode::GetOrCreateFrameNode(V2::MENU_ITEM_ETS_TAG,
+        ViewStackProcessor::GetInstance()->ClaimNodeId(), []() { return AceType::MakeRefPtr<MenuItemPattern>(); });
+    ASSERT_NE(menuItemFrameNode_, nullptr);
+
+    menuItemPattern_ = menuItemFrameNode_->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern_, nullptr);
+
+    menuItemAccessibilityProperty_ = menuItemFrameNode_->GetAccessibilityProperty<MenuItemAccessibilityProperty>();
+    ASSERT_NE(menuItemAccessibilityProperty_, nullptr);
+}
+
+PaintWrapper* MenuItemPatternTestOneNg::GetPaintWrapper(RefPtr<MenuPaintProperty> paintProperty)
+{
+    WeakPtr<RenderContext> renderContext;
+    RefPtr<GeometryNode> geometryNode = AceType::MakeRefPtr<GeometryNode>();
+    PaintWrapper* paintWrapper = new PaintWrapper(renderContext, geometryNode, paintProperty);
+    return paintWrapper;
+}
+
+RefPtr<FrameNode> MenuItemPatternTestOneNg::GetPreviewMenuWrapper(
+    SizeF itemSize, std::optional<MenuPreviewAnimationOptions> scaleOptions)
+{
+    auto rootNode = FrameNode::CreateFrameNode(
+        V2::ROOT_ETS_TAG, ElementRegister::GetInstance()->MakeUniqueId(), AceType::MakeRefPtr<RootPattern>());
+    CHECK_NULL_RETURN(rootNode, nullptr);
+    auto targetNode = FrameNode::CreateFrameNode(
+        V2::TEXT_ETS_TAG, ElementRegister::GetInstance()->MakeUniqueId(), AceType::MakeRefPtr<TextPattern>());
+    CHECK_NULL_RETURN(targetNode, nullptr);
+    auto textNode = FrameNode::CreateFrameNode(
+        V2::TEXT_ETS_TAG, ElementRegister::GetInstance()->MakeUniqueId(), AceType::MakeRefPtr<TextPattern>());
+    CHECK_NULL_RETURN(textNode, nullptr);
+    if (!(LessOrEqual(itemSize.Width(), 0.0) || LessOrEqual(itemSize.Height(), 0.0))) {
+        auto itemGeometryNode = textNode->GetGeometryNode();
+        CHECK_NULL_RETURN(itemGeometryNode, nullptr);
+        itemGeometryNode->SetFrameSize(itemSize);
+    }
+    targetNode->MountToParent(rootNode);
+    MenuParam menuParam;
+    menuParam.type = MenuType::CONTEXT_MENU;
+    menuParam.previewMode = MenuPreviewMode::CUSTOM;
+    if (scaleOptions != std::nullopt) {
+        menuParam.previewAnimationOptions = scaleOptions.value();
+    }
+    auto customNode = FrameNode::CreateFrameNode(
+        V2::TEXT_ETS_TAG, ElementRegister::GetInstance()->MakeUniqueId(), AceType::MakeRefPtr<TextPattern>());
+    CHECK_NULL_RETURN(customNode, nullptr);
+    auto customGeometryNode = customNode->GetGeometryNode();
+    CHECK_NULL_RETURN(customGeometryNode, nullptr);
+    customGeometryNode->SetFrameSize(SizeF(100.0f, 100.0f));
+    auto menuWrapperNode =
+        MenuView::Create(textNode, targetNode->GetId(), V2::TEXT_ETS_TAG, menuParam, true, customNode);
+    return menuWrapperNode;
+}
+
+/**
+ * @tc.name: OnAfterModifyDone001
+ * @tc.desc: Verify OnAfterModifyDone().
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, OnAfterModifyDone001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    menuItemPattern->OnAfterModifyDone();
+    auto host = menuItemPattern->GetHost();
+    ASSERT_NE(host, nullptr);
+}
+
+/**
+ * @tc.name: RecordChangeEvent001
+ * @tc.desc: Verify RecordChangeEvent().
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, RecordChangeEvent001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    Recorder::EventRecorder::Get().componentEnable_ = true;
+    Recorder::EventRecorder::Get().eventSwitch_.componentEnable = true;
+    menuItemPattern->RecordChangeEvent();
+
+    ASSERT_EQ(!Recorder::EventRecorder::Get().IsComponentRecordEnable(), false);
+}
+
+/**
+ * @tc.name: UpdateSubmenuExpandingMode001
+ * @tc.desc: Verify UpdateSubmenuExpandingMode().
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, UpdateSubmenuExpandingMode001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    RefPtr<UINode> node = menuItemPattern->GetHost();
+    node->tag_ = V2::MENU_ITEM_ETS_TAG;
+    menuItemPattern->UpdateSubmenuExpandingMode(node);
+
+    ASSERT_NE(node->GetTag() == V2::MENU_ETS_TAG, true);
+}
+
+/**
+ * @tc.name: OnExpandChanged001
+ * @tc.desc: Verify OnExpandChanged
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, OnExpandChanged001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    menuItemPattern->isExpanded_ = true;
+    menuItemPattern->OnExpandChanged(frameNode);
+
+    ASSERT_EQ(menuItemPattern->embeddedMenu_, nullptr);
+}
+
+/**
+ * @tc.name: RegisterOnClick001
+ * @tc.desc: Verify RegisterOnClick
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, RegisterOnClick001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    menuItemPattern->expandingMode_ = SubMenuExpandingMode::EMBEDDED;
+    menuItemPattern->clickableArea_ = nullptr;
+    menuItemPattern->RegisterOnClick();
+
+    menuItemPattern->clickableArea_ = frameNode;
+    menuItemPattern->RegisterOnClick();
+
+    auto clickableAreaGestureHub = menuItemPattern->clickableArea_->GetOrCreateGestureEventHub();
+    ASSERT_NE(clickableAreaGestureHub, nullptr);
+}
+
+/**
+ * @tc.name: RegisterOnTouch001
+ * @tc.desc: Verify RegisterOnTouch
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, RegisterOnTouch001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    menuItemPattern->expandingMode_ = SubMenuExpandingMode::EMBEDDED;
+    menuItemPattern->clickableArea_ = nullptr;
+    menuItemPattern->RegisterOnTouch();
+
+    menuItemPattern->clickableArea_ = frameNode;
+    menuItemPattern->RegisterOnTouch();
+
+    auto clickableAreaGestureHub = menuItemPattern->clickableArea_->GetOrCreateGestureEventHub();
+    ASSERT_NE(clickableAreaGestureHub, nullptr);
+}
+
+/**
+ * @tc.name: RegisterOnHover001
+ * @tc.desc: Verify RegisterOnHover
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, RegisterOnHover001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    menuItemPattern->expandingMode_ = SubMenuExpandingMode::EMBEDDED;
+    menuItemPattern->clickableArea_ = nullptr;
+    menuItemPattern->RegisterOnHover();
+
+    menuItemPattern->clickableArea_ = frameNode;
+    menuItemPattern->RegisterOnHover();
+
+    auto clickableAreaGestureHub = menuItemPattern->clickableArea_->GetOrCreateGestureEventHub();
+    ASSERT_NE(clickableAreaGestureHub, nullptr);
+}
+
+/**
+ * @tc.name: RegisterOnKeyEvent001
+ * @tc.desc: Verify RegisterOnKeyEvent
+ * @tc.type: FUNC
+ */
+HWTEST_F(MenuItemPatternTestOneNg, RegisterOnKeyEvent001, TestSize.Level1)
+{
+    MenuItemProperties itemOption;
+    itemOption.content = "content";
+    MenuItemModelNG MneuItemModelInstance;
+    MneuItemModelInstance.Create(itemOption);
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    ASSERT_NE(frameNode, nullptr);
+    auto menuItemPattern = frameNode->GetPattern<MenuItemPattern>();
+    ASSERT_NE(menuItemPattern, nullptr);
+
+    menuItemPattern->expandingMode_ = SubMenuExpandingMode::EMBEDDED;
+    menuItemPattern->clickableArea_ = nullptr;
+    menuItemPattern->RegisterOnKeyEvent();
+
+    menuItemPattern->clickableArea_ = frameNode;
+    menuItemPattern->RegisterOnKeyEvent();
+
+    auto clickableAreaGestureHub = menuItemPattern->clickableArea_->GetOrCreateGestureEventHub();
+    ASSERT_NE(clickableAreaGestureHub, nullptr);
+}
+} // namespace OHOS::Ace::NG
