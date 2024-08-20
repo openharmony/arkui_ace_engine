47aeee4d361aea7e85a7473aef3344fa9e3e1270
carnivore233 <xuyue51@huawei.com>
2024-08-20 22:18:35 +0800

    【RichEditor】扩展BindContextMenu接口
    Signed-off-by: carnivore233 <xuyue51@huawei.com>

--- frameworks/core/components_ng/base/view_abstract_model.h
+++ frameworks/core/components_ng/base/view_abstract_model.h
@@ -353,6 +353,8 @@ public:
         std::vector<NG::OptionParam>&& params, std::function<void()>&& buildFunc, const NG::MenuParam& menuParam) = 0;
     virtual void BindContextMenu(ResponseType type, std::function<void()>& buildFunc, const NG::MenuParam& menuParam,
         std::function<void()>& previewBuildFunc) = 0;
+    virtual void BindContextMenu(const RefPtr<NG::FrameNode>& targetNode, ResponseType type,
+        std::function<void()>& buildFunc, const NG::MenuParam& menuParam, std::function<void()>& previewBuildFunc) {};
     virtual void BindDragWithContextMenuParams(const NG::MenuParam& menuParam) = 0;
     virtual void BindContentCover(bool isShow, std::function<void(const std::string&)>&& callback,
         std::function<void()>&& buildFunc, NG::ModalStyle& modalStyle, std::function<void()>&& onAppear,
