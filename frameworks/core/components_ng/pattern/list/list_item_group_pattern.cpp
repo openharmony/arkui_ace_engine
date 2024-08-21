f56ebde1df9e12ea35c02c88e83a8025ba7e365c
zhangjinyu101 <zhangjinyu101@huawei.com>
2024-08-21 21:51:09 +0800

    修复使用lazyForEach加载ListItemGroup时导致Header/Footer闪烁问题 Signed-off-by: zhangjinyu101 <zhangjinyu101@huawei.com>

--- frameworks/core/components_ng/pattern/list/list_item_group_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_item_group_pattern.cpp
@@ -410,8 +410,8 @@ int32_t ListItemGroupPattern::UpdateBackwardCachedIndex(int32_t cacheCount, bool
     return backwardCachedIndex_;
 }
 
-void ListItemGroupPattern::LayoutCache(const LayoutConstraintF& constraint,
-    bool forward, int64_t deadline, int32_t cached)
+void ListItemGroupPattern::LayoutCache(const LayoutConstraintF& constraint, bool forward, int64_t deadline,
+    int32_t cached, ListMainSizeValues listSizeValues)
 {
     ACE_SCOPED_TRACE("Group LayoutCache:%d,%d", forward, cached);
     auto listNode = GetListFrameNode();
@@ -437,6 +437,8 @@ void ListItemGroupPattern::LayoutCache(const LayoutConstraintF& constraint,
     };
     itemGroup->SetCacheParam(param);
     itemGroup->SetListLayoutProperty(listLayoutProperty);
+    itemGroup->SetListMainSize(listSizeValues.startPos, listSizeValues.endPos, listSizeValues.referencePos,
+        listSizeValues.prevContentMainSize, forward);
     host->GetLayoutProperty()->UpdatePropertyChangeFlag(PROPERTY_UPDATE_MEASURE);
     host->GetGeometryNode()->SetParentLayoutConstraint(constraint);
     FrameNode::ProcessOffscreenNode(host);
--- frameworks/core/components_ng/pattern/list/list_item_group_pattern.h
+++ frameworks/core/components_ng/pattern/list/list_item_group_pattern.h
@@ -52,6 +52,13 @@ struct VisibleContentInfo {
     int32_t indexInGroup = -1;
 };
 
+struct ListMainSizeValues {
+    float startPos = 0.0f;
+    float endPos = 0.0f;
+    float referencePos = 0.0f;
+    float prevContentMainSize = 0.0f;
+};
+
 class ACE_EXPORT ListItemGroupPattern : public Pattern {
     DECLARE_ACE_TYPE(ListItemGroupPattern, Pattern);
 
@@ -210,7 +217,9 @@ public:
     void UpdateActiveChildRange(bool forward, int32_t cacheCount);
     int32_t UpdateForwardCachedIndex(int32_t cacheCount, bool outOfView);
     int32_t UpdateBackwardCachedIndex(int32_t cacheCount, bool outOfView);
-    void LayoutCache(const LayoutConstraintF& constraint, bool forward, int64_t deadline, int32_t cached);
+    void LayoutCache(const LayoutConstraintF& constraint, bool forward, int64_t deadline, int32_t cached,
+        ListMainSizeValues listSizeValues);
+
 private:
     bool IsNeedInitClickEventRecorder() const override
     {
