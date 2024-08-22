3d0cd05cfec4804b4bb3208e6df7cc9bfe69aec5
yeyinglong_admin <yeyinglong@hisilicon.com>
2024-08-22 20:47:35 +0800

    修复List链式动效无限刷帧问题
    Signed-off-by: yeyinglong_admin <yeyinglong@hisilicon.com>

--- frameworks/core/components_ng/pattern/list/list_pattern.cpp
+++ frameworks/core/components_ng/pattern/list/list_pattern.cpp
@@ -876,6 +876,9 @@ void ListPattern::OnScrollEndCallback()
     if (AnimateStoped()) {
         scrollStop_ = true;
         MarkDirtyNodeSelf();
+        if (chainAnimation_) {
+            chainAnimation_->SetOverDrag(false);
+        }
     }
 }
 
@@ -1982,8 +1985,7 @@ void ListPattern::ProcessDragStart(float startPosition)
 void ListPattern::ProcessDragUpdate(float dragOffset, int32_t source)
 {
     CHECK_NULL_VOID(chainAnimation_);
-    if (NearZero(dragOffset) || source == SCROLL_FROM_BAR || source == SCROLL_FROM_AXIS ||
-        source == SCROLL_FROM_BAR_FLING) {
+    if (source == SCROLL_FROM_BAR || source == SCROLL_FROM_AXIS || source == SCROLL_FROM_BAR_FLING) {
         bool overDrag = (source == SCROLL_FROM_UPDATE) && (IsAtTop() || IsAtBottom());
         chainAnimation_->SetOverDrag(overDrag);
         return;
