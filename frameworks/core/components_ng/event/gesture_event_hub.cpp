6ce547b1c30e63bee1b55a427af172b2e1f82fd0
zenix_zxy <zhengxingyou1@huawei.com>
2024-08-22 14:32:57 +0000

    修复子窗上的拖拽异常
    Signed-off-by: zenix_zxy <zhengxingyou1@huawei.com>
    Change-Id: I416a1de2856d37c55b928d7f807dc0a1cd2509cf

--- frameworks/core/components_ng/event/gesture_event_hub.cpp
+++ frameworks/core/components_ng/event/gesture_event_hub.cpp
@@ -935,9 +935,9 @@ void GestureEventHub::OnDragStart(const GestureEvent& info, const RefPtr<Pipelin
         info.GetInputEventType() == InputEventType::MOUSE_BUTTON ? 1.0f : DEFALUT_DRAG_PPIXELMAP_SCALE;
     auto windowScale = dragDropManager->GetWindowScale();
     float scale = windowScale * defaultPixelMapScale;
-    auto mainPipeline = PipelineContext::GetMainPipelineContext();
-    CHECK_NULL_VOID(mainPipeline);
-    auto overlayManager = mainPipeline->GetOverlayManager();
+    auto dragNodePipeline = frameNode->GetContextRefPtr();
+    CHECK_NULL_VOID(dragNodePipeline);
+    auto overlayManager = dragNodePipeline->GetOverlayManager();
     bool isSwitchToSubWindow = false;
     RefPtr<FrameNode> imageNode = nullptr;
     RefPtr<FrameNode> textNode = nullptr;
