77b7ced5144b425a9ee831e77f8e6ce28638bdf6
kangchongtao <kangchongtao@huawei.com>
2024-08-27 09:24:24 +0000

    update frameworks/core/components_ng/syntax/lazy_for_each_node.cpp.
    Signed-off-by: kangchongtao <kangchongtao@huawei.com>

--- frameworks/core/components_ng/syntax/lazy_for_each_node.cpp
+++ frameworks/core/components_ng/syntax/lazy_for_each_node.cpp
@@ -15,6 +15,7 @@
 
 #include "core/components_ng/syntax/lazy_for_each_node.h"
 
+#include "core/components_ng/pattern/list/list_item_pattern.h"
 #include "core/components_ng/syntax/lazy_layout_wrapper_builder.h"
 #include "core/components_v2/inspector/inspector_constants.h"
 #include "core/pipeline/base/element_register.h"
