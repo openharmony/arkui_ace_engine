14443951c6aae8e3bd6cae29b0d059e248235027
zhang-quanchao <zhangquanchao@huawei.com>
2024-08-28 15:16:03 +0800

    ichEditor快捷键能力补齐
    Signed-off-by: zhang-quanchao <zhangquanchao@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -5152,7 +5152,10 @@ bool RichEditorPattern::CursorMoveLeftWord()
 {
     CloseSelectOverlay();
     ResetSelection();
-    auto newPos = GetLeftWordPosition(caretPosition_);
+    int32_t newPos = 0;
+    int32_t index = GetCaretPosition();
+    auto aiContentStart = std::clamp(index - RICH_DEFAULT_AI_WORD, 0, GetTextContentLength());
+    AIDeleteComb(aiContentStart, index, newPos, true);
     if (newPos == caretPosition_) {
         return false;
     }
@@ -5169,7 +5172,10 @@ bool RichEditorPattern::CursorMoveRightWord()
 {
     CloseSelectOverlay();
     ResetSelection();
-    auto newPos = GetRightWordPosition(caretPosition_);
+    int32_t newPos = 0;
+    int32_t index = GetCaretPosition();
+    auto aiContentEnd = std::clamp(index + RICH_DEFAULT_AI_WORD, 0, GetTextContentLength());
+    AIDeleteComb(index, aiContentEnd, newPos, false);
     if (newPos == caretPosition_) {
         return false;
     }
