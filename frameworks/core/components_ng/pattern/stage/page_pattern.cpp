61b494695335c79c7848db580bef27724a59aedf
zhanghaoshen <zhanghaoshen1@huawei.com>
2024-08-28 18:48:20 +0800

    pageurl adapter
    Signed-off-by: zhanghaoshen <zhanghaoshen1@huawei.com>

--- frameworks/core/components_ng/pattern/stage/page_pattern.cpp
+++ frameworks/core/components_ng/pattern/stage/page_pattern.cpp
@@ -225,7 +225,7 @@ void PagePattern::OnShow()
         return;
     }
     std::string bundleName = container->GetBundleName();
-    NotifyPerfMonitorPageMsg(pageInfo_->GetPageUrl(), bundleName);
+    NotifyPerfMonitorPageMsg(pageInfo_->GetFullPath(), bundleName);
     if (pageInfo_) {
         context->FirePageChanged(pageInfo_->GetPageId(), true);
     }
@@ -242,7 +242,7 @@ void PagePattern::OnShow()
     state_ = RouterPageState::ON_PAGE_SHOW;
     UIObserverHandler::GetInstance().NotifyRouterPageStateChange(GetPageInfo(), state_);
 #endif
-    JankFrameReport::GetInstance().StartRecord(pageInfo_->GetPageUrl());
+    JankFrameReport::GetInstance().StartRecord(pageInfo_->GetFullPath());
     auto pageUrlChecker = container->GetPageUrlChecker();
     if (pageUrlChecker != nullptr) {
         pageUrlChecker->NotifyPageShow(pageInfo_->GetPageUrl());
