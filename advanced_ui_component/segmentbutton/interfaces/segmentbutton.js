cec1f6d1159771e7c22d3d50e0ca0211c2aafcbc
z30053720 <zhaoyong70@huawei.com>
2024-08-29 22:55:41 +0800

    cherry pick 67121c0 from https://gitee.com/zhaoyong70/arkui_ace_engine/pulls/41923 add check undefined
    Signed-off-by: z30053720 <zhaoyong70@huawei.com>

--- advanced_ui_component/segmentbutton/interfaces/segmentbutton.js
+++ advanced_ui_component/segmentbutton/interfaces/segmentbutton.js
@@ -1792,7 +1792,7 @@ export class SegmentButton extends ViewPU {
         this.__zoomScaleArray.aboutToBeDeleted();
         this.__pressArray.aboutToBeDeleted();
         this.__hoverColorArray.aboutToBeDeleted();
-        this.__shouldMirror.aboutToBeDeleted();
+        this.__shouldMirror?.aboutToBeDeleted();
         SubscriberManager.Get().delete(this.id__());
         this.aboutToBeDeletedInternal();
     }
