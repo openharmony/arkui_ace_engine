3f71ce749703f480a0fa19f4eed9c4452a370da8
zhangwenbin00001 <zhangwenbin35@h-partners.com>
2024-09-04 13:56:46 +0800

    【收编】【popup】默认样式支持产品差异化配置 -- 背景模糊主题颜色参数
    Signed-off-by: zhangwenbin00001 <zhangwenbin35@h-partners.com>

--- frameworks/core/components/popup/popup_theme.h
+++ frameworks/core/components/popup/popup_theme.h
@@ -104,6 +104,7 @@ public:
             theme->borderWidth_ = pattern->GetAttr<Dimension>("popup_border_width", 0.0_vp);
             theme->minHeight_ = pattern->GetAttr<Dimension>("popup_min_height", 0.0_vp);
             theme->popupMaxColumns_ = static_cast<uint32_t>(pattern->GetAttr<double>("popup_max_columns", 0));
+            theme->bgThemeColorMode_ = static_cast<uint32_t>(pattern->GetAttr<double>("popup_bg_theme_color_mode", 0));
         }
     };
 
@@ -347,6 +348,11 @@ public:
         return popupMaxColumns_;
     }
 
+    uint32_t GetBgThemeColorMode() const
+    {
+        return bgThemeColorMode_;
+    }
+
 protected:
     PopupTheme() = default;
 
@@ -400,6 +406,7 @@ private:
     Dimension borderWidth_ = 0.0_vp;
     Dimension minHeight_ = 0.0_vp;
     uint32_t popupMaxColumns_ = 0;
+    uint32_t bgThemeColorMode_ = 0;
 };
 
 } // namespace OHOS::Ace
