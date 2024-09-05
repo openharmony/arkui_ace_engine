6924b537f969305ea12fde06422b0d37738304db
liubihao <liubihao1@huawei.com>
2024-09-05 17:34:08 +0800

    sync release branch code diff.
    Signed-off-by: liubihao <liubihao1@huawei.com>

--- frameworks/core/components_ng/syntax/if_else_model.h
+++ frameworks/core/components_ng/syntax/if_else_model.h
@@ -19,6 +19,7 @@
 #include <cstdint>
 #include <memory>
 #include <list>
+#include <mutex>
 
 #include "base/utils/macros.h"
 
@@ -37,6 +38,7 @@ public:
 
 private:
     static std::unique_ptr<IfElseModel> instance;
+    static std::mutex mutex_;
 };
 } // namespace OHOS::Ace
 
