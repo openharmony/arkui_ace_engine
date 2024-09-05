4a9b985a2e792f313a9dcc765379c32e76708fef
jiyujia926 <jiyujia3@huawei.com>
2024-09-05 23:37:11 +0800

    add _arkUIUncaughtPromiseError to crash when application throw error in Promise
    Signed-off-by: jiyujia926 <jiyujia3@huawei.com>

--- frameworks/bridge/declarative_frontend/state_mgmt/src/lib/v2/v2_change_observation.ts
+++ frameworks/bridge/declarative_frontend/state_mgmt/src/lib/v2/v2_change_observation.ts
@@ -427,7 +427,7 @@ class ObserveV2 {
         .then(this.updateDirty.bind(this))
         .catch(error => {
           stateMgmtConsole.applicationError(`Exception occurred during the update process involving @Computed properties, @Monitor functions or UINode re-rendering`, error);
-          throw error;
+          _arkUIUncaughtPromiseError(error);
         });
       }
 
