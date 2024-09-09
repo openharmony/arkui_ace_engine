78c416e61f2248d407e6268a2d3b5ebb7986e84e
lancer <haoshuo@huawei.com>
2024-09-09 20:51:28 +0800

    删除冗余变量 Signed-off-by: haoshuo <haoshuo@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
@@ -1788,7 +1788,6 @@ private:
     RectF textRect_;
     RefPtr<Paragraph> paragraph_;
     RefPtr<Paragraph> errorParagraph_;
-    RefPtr<Paragraph> dragParagraph_;
     InlineMeasureItem inlineMeasureItem_;
     TextStyle nextLineUtilTextStyle_;
 
