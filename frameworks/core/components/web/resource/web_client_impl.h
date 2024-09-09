265a244054b4483222bcda5f0fc12748b3c7e8f1
tengfan <tengfan3@huawei.com>
2024-09-09 14:37:54 +0800

    deleate defference api from release5.0
    Signed-off-by: tengfan <tengfan3@huawei.com>

--- frameworks/core/components/web/resource/web_client_impl.h
+++ frameworks/core/components/web/resource/web_client_impl.h
@@ -163,7 +163,6 @@ public:
     bool OnFocus(OHOS::NWeb::NWebFocusSource source) override;
     void OnResourceLoadError(std::shared_ptr<OHOS::NWeb::NWebUrlResourceRequest> request,
         std::shared_ptr<OHOS::NWeb::NWebUrlResourceError> error) override;
-    void ReportDynamicFrameLossEvent(const std::string& sceneId, bool isStart) override;
     void OnHttpError(std::shared_ptr<OHOS::NWeb::NWebUrlResourceRequest> request,
         std::shared_ptr<OHOS::NWeb::NWebUrlResourceResponse> response) override;
     void OnRenderExited(OHOS::NWeb::RenderExitReason reason) override;
