154679b565f7f6dd14c482ab810c425a3f3e0f47
xi-heqiong <xiheqiong@huawei.com>
2024-09-10 19:16:13 +0800

    0910_animation
    Signed-off-by: xi-heqiong <xiheqiong@huawei.com>
    Change-Id: Icd0ca0bbf0f01e972a0c6ecff3bd8e06db35cf89

--- frameworks/core/components_ng/pattern/navrouter/navdestination_group_node.h
+++ frameworks/core/components_ng/pattern/navrouter/navdestination_group_node.h
@@ -184,12 +184,15 @@ public:
         return AceApplicationInfo::GetInstance().IsRightToLeft() ? -1.0f : 1.0f;
     }
 
+    std::shared_ptr<AnimationUtils::Animation> BackButtonAnimation(bool isTransitionIn);
+    std::shared_ptr<AnimationUtils::Animation> TitleOpacityAnimation(bool isTransitionOut);
+
     void InitSystemTransitionPush(bool transitionIn);
     void StartSystemTransitionPush(bool transitionIn);
     void SystemTransitionPushCallback(bool transitionIn);
     void InitSystemTransitionPop(bool isTransitionIn);
     void StartSystemTransitionPop(bool transitionIn);
-    bool SystemTransitionPopCallback(bool transitionIn);
+    bool SystemTransitionPopCallback();
     void InitDialogTransition(bool isZeroY);
 
     void SetRecoverable(bool recoverable)
