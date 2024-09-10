851e5990896bd2f423a96d3cfff8d1e0ceb40b79
jiangkuaixue <jiangchenzhou@huawei.com>
2024-09-10 16:58:03 +0800

    add web closeImageAnanlyzerOverlay
    Signed-off-by: jiangkuaixue <jiangchenzhou@huawei.com>

--- frameworks/core/components/web/resource/web_delegate.cpp
+++ frameworks/core/components/web/resource/web_delegate.cpp
@@ -7253,4 +7253,11 @@ void WebDelegate::StartVibraFeedback(const std::string& vibratorType)
     CHECK_NULL_VOID(webPattern);
     webPattern->StartVibraFeedback(vibratorType);
 }
+
+bool WebDelegate::CloseImageOverlaySelection()
+{
+    auto webPattern = webPattern_.Upgrade();
+    CHECK_NULL_RETURN(webPattern, false);
+    return webPattern->CloseImageOverlaySelection();
+}
 } // namespace OHOS::Ace
