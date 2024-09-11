89604cfcfb50f3e11c197b5e4b8c78453b84a71e
fengjituo111 <fengjituo@huawei.com>
2024-09-11 06:31:18 +0000

    【5.0-release】【mediaQuery】解决on回调中重入on或off引起appfreeze的问题 & 添加维测日志
    Signed-off-by: fengjituo111 <fengjituo@huawei.com>
    Change-Id: I6394fa240c80c1fc640934fa54272d18b6ad09a3

--- frameworks/base/log/log_wrapper.h
+++ frameworks/base/log/log_wrapper.h
@@ -200,6 +200,7 @@ enum AceLogTag : uint8_t {
     ACE_APPBAR,               // C03952
     ACE_SELECT_OVERLAY,       // C03953
     ACE_SECURITY_COMPONENT,   // C03956
+    ACE_MEDIA_QUERY,          // C03957
 
     FORM_RENDER = 255, // C039FF FormRenderer, last domain, do not add
 };
