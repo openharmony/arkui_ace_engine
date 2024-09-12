40afe7588c57e9d836ad9f571fc7b4ea43fafc4b
vincent <wangtao487@huawei.com>
2024-09-12 20:11:35 +0800

    未获焦时不可拖动光标
    Signed-off-by: vincent <wangtao487@huawei.com>
    Change-Id: Ib020d4991edd491d0542d9ca59d5da1c38b46d0b

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -1708,7 +1708,7 @@ void TextFieldPattern::HandleTouchDown(const Offset& offset)
 
     if (enableTouchAndHoverEffect_ && !isMousePressed_) {
         auto lastCaretRect = selectController_->GetCaretRect();
-        moveCaretState_.isTouchCaret = !IsSelected() && RepeatClickCaret(offset, lastCaretRect);
+        moveCaretState_.isTouchCaret = HasFocus() && !IsSelected() && RepeatClickCaret(offset, lastCaretRect);
         isTouchPreviewText_ = GetTouchInnerPreviewText(offset);
     }
 }
