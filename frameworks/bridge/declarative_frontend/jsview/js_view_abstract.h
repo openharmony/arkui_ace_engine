ae193a51b390d97e449661e2e78f81184b842b15
qianqiuhong <qianqiuhong@huawei.com>
2024-09-14 17:44:30 +0800

    代码差异同步
    Signed-off-by: qianqiuhong <qianqiuhong@huawei.com>

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.h
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.h
@@ -177,8 +177,10 @@ public:
         std::function<void()>& onDisappear, std::function<void()>& shouldDismiss,
         std::function<void(const int32_t info)>& onWillDismiss, std::function<void()>& onWillAppear,
         std::function<void()>& onWillDisappear, std::function<void(const float)>& onHeightDidChange,
-        std::function<void(const float)>& onDetentsDidChange, std::function<void(const float)>& onWidthDidChange,
-        std::function<void(const float)>& onTypeDidChange, std::function<void()>& sheetSpringBack);
+        std::function<void(const float)>& onDetentsDidChange,
+        std::function<void(const float)>& onWidthDidChange,
+        std::function<void(const float)>& onTypeDidChange,
+        std::function<void()>& sheetSpringBack);
     static void ParseSheetTitle(const JSRef<JSObject>& paramObj, NG::SheetStyle& sheetStyle,
         std::function<void()>& titleBuilderFunction);
     static panda::Local<panda::JSValueRef> JsDismissSheet(panda::JsiRuntimeCallInfo* runtimeCallInfo);
@@ -201,11 +203,6 @@ public:
     static void ParseCommonMarginOrPaddingCorner(
         const JSRef<JSObject>& object, CommonCalcDimension& commonCalcDimension);
     static void GetBorderRadiusByLengthMetrics(const char* key, JSRef<JSObject>& object, CalcDimension& radius);
-    static void JsOutline(const JSCallbackInfo& info);
-    static void JsOutlineWidth(const JSCallbackInfo& info);
-    static void JsOutlineColor(const JSCallbackInfo& info);
-    static void JsOutlineStyle(const JSCallbackInfo& info);
-    static void JsOutlineRadius(const JSCallbackInfo& info);
     static void JsBorder(const JSCallbackInfo& info);
     static void JsBorderWidth(const JSCallbackInfo& info);
     static void ParseBorderWidth(const JSRef<JSVal>& args);
@@ -216,6 +213,11 @@ public:
     static void ParseDashGap(const JSRef<JSVal>& args);
     static void ParseDashWidth(const JSRef<JSVal>& args);
     static void JsBorderImage(const JSCallbackInfo& info);
+    static void JsOutline(const JSCallbackInfo& info);
+    static void JsOutlineWidth(const JSCallbackInfo& info);
+    static void JsOutlineColor(const JSCallbackInfo& info);
+    static void JsOutlineStyle(const JSCallbackInfo& info);
+    static void JsOutlineRadius(const JSCallbackInfo& info);
     static void ParseBorderImageRepeat(const JSRef<JSVal>& args, RefPtr<BorderImage>& borderImage);
     static void ParseBorderImageOutset(const JSRef<JSVal>& args, RefPtr<BorderImage>& borderImage);
     static void ParseBorderImageSlice(const JSRef<JSVal>& args, RefPtr<BorderImage>& borderImage);
@@ -590,6 +592,8 @@ public:
     static bool ParseJsSymbolId(
         const JSRef<JSVal>& jsValue, uint32_t& symbolId, RefPtr<ResourceObject>& symbolResourceObject);
     static bool ParseJsSymbolColor(const JSRef<JSVal>& jsValue, std::vector<Color>& result);
+    static std::function<void(NG::DrawingContext& context)> GetDrawCallback(
+        const RefPtr<JsFunction>& jsDraw, const JSExecutionContext& execCtx);
     static bool ParseBorderWidthProps(const JSRef<JSVal>& args, NG::BorderWidthProperty& borderWidthProperty);
     static bool ParseBorderColorProps(const JSRef<JSVal>& args, NG::BorderColorProperty& colorProperty);
     static bool ParseBorderStyleProps(const JSRef<JSVal>& args, NG::BorderStyleProperty& borderStyleProperty);
@@ -599,9 +603,6 @@ public:
     static bool ParseEditMenuOptions(const JSCallbackInfo& info, NG::OnCreateMenuCallback& onCreateMenuCallback,
         NG::OnMenuItemClickCallback& onMenuItemClick);
     static void SetDialogProperties(const JSRef<JSObject>& obj, DialogProperties& properties);
-    static std::function<void(NG::DrawingContext& context)> GetDrawCallback(
-        const RefPtr<JsFunction>& jsDraw, const JSExecutionContext& execCtx);
-
     static RefPtr<NG::ChainedTransitionEffect> ParseNapiChainedTransition(
         const JSRef<JSObject>& object, const JSExecutionContext& context);
     static void JsFocusScopeId(const JSCallbackInfo& info);
