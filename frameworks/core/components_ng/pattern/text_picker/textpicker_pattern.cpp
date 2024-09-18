79ade0653f8afd7c05679e2a7040476ebcc3ce5e
sunjiakun <sunjiakun5@huawei.com>
2024-09-18 14:39:53 +0800

    pickerPaintLine
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

--- frameworks/core/components_ng/pattern/text_picker/textpicker_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_picker/textpicker_pattern.cpp
@@ -558,6 +558,7 @@ RectF TextPickerPattern::CalculatePaintRect(int32_t currentFocusIndex,
         centerY = centerY + DIALOG_OFFSET_LENGTH.ConvertToPx();
         centerX = centerX + FOUCS_WIDTH.ConvertToPx();
     }
+
     if (piantRectWidth > columnWidth) {
         if (!GetIsShowInDialog()) {
             piantRectWidth = columnWidth - FOUCS_WIDTH.ConvertToPx() - PRESS_INTERVAL.ConvertToPx();
@@ -1082,7 +1083,7 @@ void TextPickerPattern::OnDirectionConfigurationUpdate()
     isNeedUpdateSelectedIndex_ = false;
 }
 
-void TextPickerPattern::CheckAndUpdateColumnSize(SizeF& size)
+void TextPickerPattern::CheckAndUpdateColumnSize(SizeF& size, bool isNeedAdaptForAging)
 {
     auto host = GetHost();
     CHECK_NULL_VOID(host);
@@ -1117,7 +1118,11 @@ void TextPickerPattern::CheckAndUpdateColumnSize(SizeF& size)
     auto version10OrLarger = context->GetMinPlatformVersion() > 9;
     pickerContentSize.Constrain(minSize, stackLayoutConstraint->maxSize, version10OrLarger);
 
-    size.SetWidth(pickerContentSize.Width() / std::max(childCount, 1.0f));
+    if (isNeedAdaptForAging && GetIsShowInDialog()) {
+        size.SetWidth(pickerContentSize.Width());
+    } else {
+        size.SetWidth(pickerContentSize.Width() / std::max(childCount, 1.0f));
+    }
     size.SetHeight(std::min(pickerContentSize.Height(), size.Height()));
 }
 
@@ -1138,5 +1143,18 @@ void TextPickerPattern::SetCanLoop(bool isLoop)
         CHECK_NULL_VOID(pickerColumnPattern);
         pickerColumnPattern->SetCanLoop(isLoop);
     }
+}
+
+bool TextPickerPattern::NeedAdaptForAging()
+{
+    auto pipeline = PipelineContext::GetCurrentContextSafelyWithCheck();
+    CHECK_NULL_RETURN(pipeline, false);
+    auto pickerTheme = pipeline->GetTheme<PickerTheme>();
+    CHECK_NULL_RETURN(pickerTheme, false);
+
+    if (GreatOrEqual(pipeline->GetFontScale(), pickerTheme->GetMaxOneFontScale())) {
+        return true;
+    }
+    return false;
 }
 } // namespace OHOS::Ace::NG
