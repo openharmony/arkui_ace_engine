7cda4c5e473c447943ea2bd7a07e531eb378d9f7
zcdqs <junfeng.lijunfeng@huawei.com>
2024-09-18 15:45:29 +0800

    change onItemDrag preview from window to overlay
    Signed-off-by: zcdqs <junfeng.lijunfeng@huawei.com>
    Change-Id: I1b32e75fbe6de05287a9d820eed1813e7c14c3c9

--- frameworks/core/components_ng/pattern/grid/grid_event_hub.cpp
+++ frameworks/core/components_ng/pattern/grid/grid_event_hub.cpp
@@ -187,7 +187,7 @@ void GridEventHub::HandleOnItemDragStart(const GestureEvent& info)
                 CHECK_NULL_VOID(eventHub);
                 auto manager = pipeline->GetDragDropManager();
                 CHECK_NULL_VOID(manager);
-                eventHub->dragDropProxy_ = manager->CreateAndShowDragWindow(pixelMap, info);
+                eventHub->dragDropProxy_ = manager->CreateAndShowItemDragOverlay(pixelMap, info, eventHub);
                 CHECK_NULL_VOID(eventHub->dragDropProxy_);
                 eventHub->dragDropProxy_->OnItemDragStart(info, host);
                 gridItem->GetLayoutProperty()->UpdateVisibility(VisibleType::INVISIBLE);
@@ -204,7 +204,7 @@ void GridEventHub::HandleOnItemDragStart(const GestureEvent& info)
 #else
     auto manager = pipeline->GetDragDropManager();
     CHECK_NULL_VOID(manager);
-    dragDropProxy_ = manager->CreateAndShowDragWindow(customNode, info);
+    dragDropProxy_ = manager->CreateAndShowItemDragOverlay(customNode, info, AceType::Claim(this));
     CHECK_NULL_VOID(dragDropProxy_);
     dragDropProxy_->OnItemDragStart(info, host);
     gridItem->GetLayoutProperty()->UpdateVisibility(VisibleType::INVISIBLE);
