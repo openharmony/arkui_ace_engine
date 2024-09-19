07d0ea70c86a89fea122a807b7c15ca1f9f36708
zzc0127 <zhangzecong@huawei.com>
2024-09-19 03:05:51 +0000

    update frameworks/core/components_ng/pattern/text/text_pattern.h.
    Signed-off-by: zzc0127 <zhangzecong@huawei.com>

--- frameworks/core/components_ng/pattern/text/text_pattern.h
+++ frameworks/core/components_ng/pattern/text/text_pattern.h
@@ -676,6 +676,10 @@ public:
 
     void UpdateFontColor(const Color& value);
     void BeforeCreatePaintWrapper() override;
+    void ChangeHandleHeight(const GestureEvent& event, bool isFirst);
+    void ChangeFirstHandleHeight(const Offset& touchOffset, RectF& handleRect);
+    void ChangeSecondHandleHeight(const Offset& touchOffset, RectF& handleRect);
+    virtual void CalculateDefaultHandleHeight(float& height);
     uint64_t GetSystemTimestamp()
     {
         return static_cast<uint64_t>(
@@ -683,10 +687,6 @@ public:
                 .count());
     }
 
-    void ChangeHandleHeight(const GestureEvent& event, bool isFirst);
-    void ChangeFirstHandleHeight(const Offset& touchOffset, RectF& handleRect);
-    void ChangeSecondHandleHeight(const Offset& touchOffset, RectF& handleRect);
-    virtual void CalculateDefaultHandleHeight(float& height);
     void OnTextOverflowChanged();
 
 protected:
