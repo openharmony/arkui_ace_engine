f3ec0144a7e443c3caf5fd78ffa69707ee4f25ee
eager1 <chenyige2@huawei.com>
2024-09-19 11:31:27 +0800

    Fix: napi interface use hsp  can't get resource Signed-off-by: eager1 <chenyige2@huawei.com> （cherry picked commit from <gitee.com//openharmony/arkui_ace_engine/commit/285e964480e938de542dc86871885da403e0fbb9>
    Change-Id: I2156da10fe6f97b4e8bfa2c53ec8c499fbc39029

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
@@ -2786,6 +2786,7 @@ void JSViewAbstract::JsBackgroundColor(const JSCallbackInfo& info)
 
 void JSViewAbstract::JsBackgroundImage(const JSCallbackInfo& info)
 {
+    int32_t resId = 0;
     std::string src;
     std::string bundle;
     std::string module;
@@ -2796,7 +2797,7 @@ void JSViewAbstract::JsBackgroundImage(const JSCallbackInfo& info)
         src = jsBackgroundImage->ToString();
         ViewAbstractModel::GetInstance()->SetBackgroundImage(
             ImageSourceInfo { src, bundle, module }, GetThemeConstants());
-    } else if (ParseJsMedia(jsBackgroundImage, src)) {
+    } else if (ParseJsMediaWithBundleName(jsBackgroundImage, src, bundle, module, resId)) {
         ViewAbstractModel::GetInstance()->SetBackgroundImage(ImageSourceInfo { src, bundle, module }, nullptr);
     } else {
 #if defined(PIXEL_MAP_SUPPORTED)
