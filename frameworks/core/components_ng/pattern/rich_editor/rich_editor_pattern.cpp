677e3cced1cc0d1ff748689be917d367a654a3a6
zyx0121 <zhangyingxiang2@huawei.com>
2024-09-20 15:09:24 +0800

    修复三击光标和手柄同时存在
    Signed-off-by: zyx0121 <zhangyingxiang2@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -274,6 +274,10 @@ void RichEditorPattern::InsertValueInStyledString(const std::string& insertValue
     if (changeLength > 0) {
         DeleteForwardInStyledString(changeLength, false);
     }
+    if (textSelector_.IsValid()) {
+        CloseSelectOverlay();
+        ResetSelection();
+    }
     if (insertStyledString) {
         styledString_->InsertSpanString(changeStart, insertStyledString);
     } else {
@@ -9151,9 +9155,6 @@ void RichEditorPattern::TripleClickSection(GestureEvent& info, int32_t start, in
     }
     if (textSelector_.SelectNothing()) {
         textSelector_.Update(pos, pos);
-        if (!caretTwinkling_) {
-            StartTwinkling();
-        }
     } else {
         StopTwinkling();
     }
