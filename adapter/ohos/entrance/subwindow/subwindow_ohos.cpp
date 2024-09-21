12903f3aeaecfeb4b5cbbe6b9e28e0b1ce3d8a15
zhangjinyu101 <zhangjinyu101@huawei.com>
2024-09-21 16:58:23 +0800

    fix:修复toastService适老化适配问题
    Signed-off-by: zhangjinyu101 <zhangjinyu101@huawei.com>

--- adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
+++ adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
@@ -41,6 +41,7 @@
 #include "base/log/frame_report.h"
 #include "base/utils/system_properties.h"
 #include "base/utils/utils.h"
+#include "bundlemgr/bundle_mgr_interface.h"
 #include "core/common/connect_server_manager.h"
 #include "core/common/container_scope.h"
 #include "core/common/frontend.h"
@@ -55,6 +56,11 @@
 #include "core/components_ng/render/adapter/rosen_window.h"
 #include "frameworks/bridge/common/utils/engine_helper.h"
 #include "frameworks/bridge/declarative_frontend/declarative_frontend.h"
+#include "iservice_registry.h"
+#ifdef OS_ACCOUNT_EXISTS
+#include "os_account_manager.h"
+#endif
+#include "system_ability_definition.h"
 
 namespace OHOS::Ace {
 namespace {
@@ -62,6 +68,22 @@ const Rect MIN_WINDOW_HOT_AREA = Rect(0.0f, 0.0f, 1.0f, 1.0f);
 #ifndef NG_BUILD
 constexpr int32_t PLATFORM_VERSION_TEN = 10;
 #endif
+constexpr float FONTSIZE_MAXSCALE = 3.2f;
+const std::string  CONFIGURATION = "configuration";
+const std::string  FONTSIZE_SCALE_KEY = "fontSizeScale";
+const std::string  FOLLOW_SYSTEM = "followSystem";
+const std::string  FONTSIZE_MAXSCALE_KEY = "fontSizeMaxScale";
+ErrCode GetActiveAccountIds(std::vector<int32_t>& userIds)
+{
+    userIds.clear();
+#ifdef OS_ACCOUNT_EXISTS
+    return AccountSA::OsAccountManager::QueryActiveOsAccountIds(userIds);
+#else
+    TAG_LOGW(AceLogTag::ACE_PLUGIN_COMPONENT, "os account part doesn't exist, use default id.");
+    userIds.push_back(DEFAULT_OS_ACCOUNT_ID);
+    return ERR_OK;
+#endif
+}
 } // namespace
 
 int32_t SubwindowOhos::id_ = 0;
@@ -244,6 +266,53 @@ RefPtr<PipelineBase> SubwindowOhos::GetChildPipelineContext() const
     auto aceContainer = Platform::AceContainer::GetContainer(childContainerId_);
     CHECK_NULL_RETURN(aceContainer, nullptr);
     return aceContainer->GetPipelineContext();
+}
+
+std::function<void()> SubwindowOhos::GetInitToastDelayTask(const NG::ToastInfo& toastInfo)
+{
+    return [toastInfo]() {
+        int32_t posX = 0;
+        int32_t posY = 0;
+        int32_t width = 0;
+        int32_t height = 0;
+        float density = 1.0f;
+        auto subwindowOhos =
+            AceType::DynamicCast<SubwindowOhos>(SubwindowManager::GetInstance()->GetCurrentDialogWindow());
+        CHECK_NULL_VOID(subwindowOhos);
+        subwindowOhos->GetToastDialogWindowProperty(width, height, posX, posY, density);
+        auto childContainerId = subwindowOhos->GetChildContainerId();
+        auto window = Platform::DialogContainer::GetUIWindow(childContainerId);
+        auto dialogWindow = subwindowOhos->GetDialogWindow();
+        if (!dialogWindow || !window || !subwindowOhos->IsToastWindow()) {
+            bool ret = subwindowOhos->InitToastDialogWindow(width, height, posX, posY, true);
+            if (!ret) {
+                TAG_LOGW(AceLogTag::ACE_SUB_WINDOW, "init toast dialog window failed");
+                return;
+            }
+            ret = subwindowOhos->InitToastDialogView(width, height, density);
+            if (!ret) {
+                TAG_LOGW(AceLogTag::ACE_SUB_WINDOW, "init toast dialog view failed");
+                return;
+            }
+            ret = subwindowOhos->InitToastServiceConfig();
+            if (!ret) {
+                TAG_LOGW(AceLogTag::ACE_SUB_WINDOW, "init toast service conf failed");
+            }
+            subwindowOhos->SetIsToastWindow(true);
+        }
+        childContainerId = subwindowOhos->GetChildContainerId();
+        ContainerScope scope(childContainerId);
+        subwindowOhos->UpdateAceView(width, height, density, childContainerId);
+        TAG_LOGD(AceLogTag::ACE_SUB_WINDOW,
+            "update ace view width : %{public}d,  height : %{public}d, density : %{public}f,childContainerId : "
+            "%{public}d",
+            width, height, density, childContainerId);
+        auto container = Platform::DialogContainer::GetContainer(childContainerId);
+        CHECK_NULL_VOID(container);
+        container->SetFontScaleAndWeightScale(childContainerId);
+        Platform::DialogContainer::ShowToastDialogWindow(childContainerId, posX, posY, width, height, true);
+        Platform::DialogContainer::ShowToast(childContainerId, toastInfo.message, toastInfo.duration, toastInfo.bottom);
+    };
 }
 
 void SubwindowOhos::ResizeWindow()
@@ -1111,6 +1180,57 @@ bool SubwindowOhos::InitToastDialogView(int32_t width, int32_t height, float den
 #endif
 }
 
+bool SubwindowOhos::InitToastServiceConfig()
+{
+    int32_t creatorUid = AceApplicationInfo::GetInstance().GetUid();
+    std::string strBundleName;
+    sptr<ISystemAbilityManager> systemAbilityManager =
+        SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    CHECK_NULL_RETURN(systemAbilityManager, false);
+
+    sptr<IRemoteObject> remoteObject = systemAbilityManager->GetSystemAbility(BUNDLE_MGR_SERVICE_SYS_ABILITY_ID);
+    CHECK_NULL_RETURN(remoteObject, false);
+
+    sptr<AppExecFwk::IBundleMgr> bundleMgrProxy = iface_cast<AppExecFwk::IBundleMgr>(remoteObject);
+    CHECK_NULL_RETURN(bundleMgrProxy, false);
+
+    AppExecFwk::BundleInfo bundleInfo;
+    int32_t bundleInfoFlag = static_cast<int32_t>(AppExecFwk::GetBundleInfoFlag::GET_BUNDLE_INFO_WITH_APPLICATION);
+    bundleMgrProxy->GetBundleNameForUid(creatorUid, strBundleName);
+    std::vector<int32_t> userIds;
+    ErrCode errCode = GetActiveAccountIds(userIds);
+    CHECK_NULL_RETURN(errCode == ERR_OK, false);
+
+    errCode = bundleMgrProxy->GetBundleInfoV9(strBundleName, bundleInfoFlag, bundleInfo, userIds[0]);
+    CHECK_NULL_RETURN(errCode == ERR_OK, false);
+
+    auto maxScale = FONTSIZE_MAXSCALE;
+    auto isFollowSystem = false;
+    auto rootJson = JsonUtil::ParseJsonString(bundleInfo.applicationInfo.configuration);
+    if (rootJson != nullptr) {
+        auto configuration = rootJson->GetObject(CONFIGURATION);
+        if (configuration != nullptr) {
+            auto followSystem = configuration->GetValue(FONTSIZE_SCALE_KEY);
+            auto fontSizeMaxScale = configuration->GetValue(FONTSIZE_MAXSCALE_KEY);
+            if (followSystem != nullptr) {
+                isFollowSystem = followSystem->IsString() ? followSystem->GetString() == FOLLOW_SYSTEM : false;
+            }
+            if (fontSizeMaxScale != nullptr) {
+                maxScale = fontSizeMaxScale->IsString() ?
+                    StringUtils::StringToFloat(fontSizeMaxScale->GetString()) : FONTSIZE_MAXSCALE;
+            }
+            maxScale = maxScale == 0.0f ? FONTSIZE_MAXSCALE : maxScale;
+        }
+    }
+    auto container = Platform::DialogContainer::GetContainer(childContainerId_);
+    CHECK_NULL_RETURN(container, false);
+    auto pipelineContext = container->GetPipelineContext();
+    CHECK_NULL_RETURN(pipelineContext, false);
+    pipelineContext->SetMaxAppFontScale(maxScale);
+    pipelineContext->SetFollowSystem(isFollowSystem);
+    return true;
+}
+
 bool SubwindowOhos::CreateEventRunner()
 {
     if (!eventLoop_) {
@@ -1184,46 +1304,7 @@ void SubwindowOhos::ShowToastForService(const NG::ToastInfo& toastInfo)
     }
 
     SubwindowManager::GetInstance()->SetCurrentDialogSubwindow(AceType::Claim(this));
-    auto showDialogCallback = [toastInfo]() {
-        int32_t posX = 0;
-        int32_t posY = 0;
-        int32_t width = 0;
-        int32_t height = 0;
-        float density = 1.0f;
-        auto subwindowOhos =
-            AceType::DynamicCast<SubwindowOhos>(SubwindowManager::GetInstance()->GetCurrentDialogWindow());
-        CHECK_NULL_VOID(subwindowOhos);
-        subwindowOhos->GetToastDialogWindowProperty(width, height, posX, posY, density);
-        auto childContainerId = subwindowOhos->GetChildContainerId();
-        auto window = Platform::DialogContainer::GetUIWindow(childContainerId);
-        auto dialogWindow = subwindowOhos->GetDialogWindow();
-        if (!dialogWindow || !window || !subwindowOhos->IsToastWindow()) {
-            bool ret = subwindowOhos->InitToastDialogWindow(width, height, posX, posY, true);
-            if (!ret) {
-                TAG_LOGW(AceLogTag::ACE_SUB_WINDOW, "init toast dialog window failed");
-                return;
-            }
-            ret = subwindowOhos->InitToastDialogView(width, height, density);
-            if (!ret) {
-                TAG_LOGW(AceLogTag::ACE_SUB_WINDOW, "init toast dialog view failed");
-                return;
-            }
-            subwindowOhos->SetIsToastWindow(true);
-        }
-        childContainerId = subwindowOhos->GetChildContainerId();
-        ContainerScope scope(childContainerId);
-        subwindowOhos->UpdateAceView(width, height, density, childContainerId);
-        TAG_LOGD(AceLogTag::ACE_SUB_WINDOW,
-            "update ace view width: %{public}d,  height : %{public}d, density : %{public}f,childContainerId : "
-            "%{public}d",
-            width, height, density, childContainerId);
-        auto container = Platform::DialogContainer::GetContainer(childContainerId);
-        CHECK_NULL_VOID(container);
-        container->SetFontScaleAndWeightScale(childContainerId);
-        Platform::DialogContainer::ShowToastDialogWindow(childContainerId, posX, posY, width, height, true);
-        Platform::DialogContainer::ShowToast(childContainerId, toastInfo.message, toastInfo.duration, toastInfo.bottom);
-    };
-    if (!handler_->PostTask(showDialogCallback)) {
+    if (!handler_->PostTask(GetInitToastDelayTask(toastInfo))) {
         TAG_LOGW(AceLogTag::ACE_SUB_WINDOW, "create show dialog callback failed");
         return;
     }
