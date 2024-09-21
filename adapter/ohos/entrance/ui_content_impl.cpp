44bd329789349392c91862177c8f3fbbf9e3dcb6
wlh2624 <1968860844@qq.com>
2024-09-21 18:12:17 +0800

    蓝黄同步修改
    Signed-off-by: wlh2624 <1968860844@qq.com>
    Change-Id: I3e06ea6ce9e5d265f96d9c4c3c4ba9eb8ce7c8ba

--- adapter/ohos/entrance/ui_content_impl.cpp
+++ adapter/ohos/entrance/ui_content_impl.cpp
@@ -1278,7 +1278,7 @@ void UIContentImpl::SetConfiguration(const std::shared_ptr<OHOS::AppExecFwk::Con
 
 void UIContentImpl::StoreConfiguration(const std::shared_ptr<OHOS::AppExecFwk::Configuration>& config)
 {
-    if (config == nullptr) {
+    if (!config) {
         return;
     }
     TAG_LOGD(AceLogTag::ACE_WINDOW, "StoreConfiguration %{public}s", config->GetName().c_str());
@@ -1292,11 +1292,7 @@ void UIContentImpl::StoreConfiguration(const std::shared_ptr<OHOS::AppExecFwk::C
     }
 
     auto string2float = [](const std::string& str) {
-        try {
-            return std::stof(str);
-        } catch (...) {
-            return (float)1.0;
-        }
+        return std::stof(str);
     };
     auto fontScale = config->GetItem(OHOS::AAFwk::GlobalConfigurationKey::SYSTEM_FONT_SIZE_SCALE);
     if (!fontScale.empty()) {
