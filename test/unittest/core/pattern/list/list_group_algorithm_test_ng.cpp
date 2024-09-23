173fa2b750f8f7cfd26dc77d2e42baf18f1e3a81
yeyinglong_admin <yeyinglong@hisilicon.com>
2024-09-23 21:42:21 +0800

    修复ListItemGroup布局跳变问题
    Signed-off-by: yeyinglong_admin <yeyinglong@hisilicon.com>

--- test/unittest/core/pattern/list/list_group_algorithm_test_ng.cpp
+++ test/unittest/core/pattern/list/list_group_algorithm_test_ng.cpp
@@ -66,57 +66,23 @@ void ListGroupAlgTestNg::CreateGroupWithFooter(
  */
 HWTEST_F(ListGroupAlgTestNg, ListItemGroupLayoutAlgorithm_NeedMeasureItem001, TestSize.Level1)
 {
-    /* *
-     * @tc.steps: step1. create List/ListItemGroup and Set Space
+    /**
+     * @tc.steps: step1. Create ListItemGroup with header
      */
-    int32_t groupNumber = 2;
-    int32_t itemNumber = 3;
-    ListModelNG model = CreateList();
-    model.SetSpace(Dimension(SPACE));
-    // head + ListItem + SPACE + ListItem + SPACE + ListItem + Foot
-    // FrameNode = 2 * 3
-    CreateGroupWithSetting(groupNumber, V2::ListItemGroupStyle::NONE, itemNumber);
+    auto model = CreateList();
+    model.SetInitialIndex(1);
+    CreateGroupWithFooter(4, V2::ListItemGroupStyle::NONE);
     CreateDone(frameNode_);
 
-    /* *
-     * @tc.steps: step2. get child frame node from index
+    /**
+     * @tc.steps: step2. Scroll -25px and 0px
+     * @tc.expected: Group1 Offset is 25
      */
-    auto groupFrameNode = GetChildFrameNode(frameNode_, 0);
-    auto groupPattern = groupFrameNode->GetPattern<ListItemGroupPattern>();
-    RefPtr<LayoutAlgorithm> layoutAl = groupPattern->CreateLayoutAlgorithm();
-
-    /* *
-     * @tc.steps: step3. build a object about ListItemGroupLayoutAlgorithm
-     */
-    RefPtr<ListItemGroupLayoutAlgorithm> listItemGroupAl = AceType::DynamicCast<ListItemGroupLayoutAlgorithm>(layoutAl);
-    RefPtr<LayoutWrapper> layoutWrapper = AceType::DynamicCast<LayoutWrapper>(groupFrameNode);
-    LayoutWrapper* wrapper = AceType::RawPtr(layoutWrapper);
-    EXPECT_TRUE(listItemGroupAl->forwardLayout_ && listItemGroupAl->headerIndex_ >= 0);
-
-    bool resTypeInit = listItemGroupAl->NeedMeasureItem(wrapper);
-    EXPECT_TRUE(resTypeInit);
-
-    listItemGroupAl->headerMainSize_ = 3.1;
-    listItemGroupAl->endPos_ = 1.1;
-
-    bool resType = listItemGroupAl->NeedMeasureItem(wrapper);
-    EXPECT_FALSE(resType);
-
-    // init data
-    listItemGroupAl->headerMainSize_ = 0.0f;
-    listItemGroupAl->startPos_ = 0.0f;
-    listItemGroupAl->endPos_ = 0.0f;
-    listItemGroupAl->referencePos_ = 0.0f;
-    listItemGroupAl->totalMainSize_ = 0.0f;
-    listItemGroupAl->footerMainSize_ = 0.0f;
-
-    // LessNotEqual check
-    listItemGroupAl->totalMainSize_ = 10.1;
-    listItemGroupAl->footerMainSize_ = 3.1;
-    listItemGroupAl->startPos_ = 8.1;
-
-    resType = listItemGroupAl->NeedMeasureItem(wrapper);
-    EXPECT_FALSE(resType);
+    pattern_->UpdateCurrentOffset(25, SCROLL_FROM_UPDATE);
+    FlushLayoutTask(frameNode_);
+    EXPECT_TRUE(IsEqual(GetChildOffset(frameNode_, 1), OffsetF(0, 25)));
+    FlushLayoutTask(frameNode_, true);
+    EXPECT_TRUE(IsEqual(GetChildOffset(frameNode_, 1), OffsetF(0, 25)));
 }
 
 /**
@@ -126,65 +92,20 @@ HWTEST_F(ListGroupAlgTestNg, ListItemGroupLayoutAlgorithm_NeedMeasureItem001, Te
  */
 HWTEST_F(ListGroupAlgTestNg, ListItemGroupLayoutAlgorithm_NeedMeasureItem002, TestSize.Level1)
 {
-    /* *
-     * @tc.steps: step1. create List/ListItemGroup and Set Space
+    /**
+     * @tc.steps: step1. Create ListItemGroup with header
      */
-    int32_t groupNumber = 2;
-    int32_t itemNumber = 3;
-    ListModelNG model = CreateList();
-    model.SetSpace(Dimension(SPACE));
-    // head + ListItem + SPACE + ListItem + SPACE + ListItem + Foot
-    // FrameNode = 2 * 3
-    CreateGroupWithSetting(groupNumber, V2::ListItemGroupStyle::NONE, itemNumber);
+    auto model = CreateList();
+    CreateGroupWithHeader(3, V2::ListItemGroupStyle::NONE, 3);
     CreateDone(frameNode_);
 
-    /* *
-     * @tc.steps: step2. get child frame node from index
+    /**
+     * @tc.steps: step2. Scroll -25px and 0px
+     * @tc.expected: Group1 Offset is 25
      */
-    auto groupFrameNode = GetChildFrameNode(frameNode_, 0);
-    auto groupPattern = groupFrameNode->GetPattern<ListItemGroupPattern>();
-
-    RefPtr<LayoutAlgorithm> layoutAl = groupPattern->CreateLayoutAlgorithm();
-    RefPtr<LayoutWrapper> layoutWrapper = AceType::DynamicCast<LayoutWrapper>(groupFrameNode);
-    LayoutWrapper* wrapper = AceType::RawPtr(layoutWrapper);
-
-    /* *
-     * @tc.steps: step3. build a object about ListItemGroupLayoutAlgorithm
-     */
-    RefPtr<ListItemGroupLayoutAlgorithm> listItemGroupAl = AceType::DynamicCast<ListItemGroupLayoutAlgorithm>(layoutAl);
-    EXPECT_TRUE(listItemGroupAl->forwardLayout_ && listItemGroupAl->headerIndex_ >= 0);
-
-    // forwardLayout equ false, GreatNotEqual
-    listItemGroupAl->forwardLayout_ = false;
-    listItemGroupAl->headerMainSize_ = 3.1;
-    listItemGroupAl->referencePos_ = 10.1;
-    listItemGroupAl->totalMainSize_ = 10.1;
-    listItemGroupAl->endPos_ = 1.1;
-
-    bool resType = listItemGroupAl->NeedMeasureItem(wrapper);
-    EXPECT_FALSE(resType);
-
-    // init data
-    listItemGroupAl->headerMainSize_ = 0.0f;
-    listItemGroupAl->startPos_ = 0.0f;
-    listItemGroupAl->endPos_ = 0.0f;
-    listItemGroupAl->referencePos_ = 0.0f;
-    listItemGroupAl->totalMainSize_ = 0.0f;
-    listItemGroupAl->footerMainSize_ = 0.0f;
-
-    // forwardLayout false
-    listItemGroupAl->forwardLayout_ = false;
-    listItemGroupAl->totalMainSize_ = 10.1;
-    listItemGroupAl->footerMainSize_ = 3.1;
-    listItemGroupAl->startPos_ = 8.1;
-    listItemGroupAl->referencePos_ = 10.1;
-
-    resType = listItemGroupAl->NeedMeasureItem(wrapper);
-    EXPECT_FALSE(resType);
-
-    listItemGroupAl->startPos_ = 6.1;
-    resType = listItemGroupAl->NeedMeasureItem(wrapper);
-    EXPECT_TRUE(resType);
+    pattern_->UpdateCurrentOffset(-450, SCROLL_FROM_UPDATE);
+    FlushLayoutTask(frameNode_);
+    EXPECT_TRUE(IsEqual(GetChildOffset(frameNode_, 2), OffsetF(0, 290)));
 }
 
 /**
