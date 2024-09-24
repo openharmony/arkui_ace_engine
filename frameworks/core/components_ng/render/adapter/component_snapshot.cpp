af931f95b2f813d58a301ebabae0b18ba52bdb2a
huqingyun <huqingyun1@huawei.com>
2024-09-24 02:15:25 +0000

    【5.0Release】组件截图：修复进入应用第一次对Gif资源的image截图，截图是透明的问题。
    Signed-off-by: huqingyun <huqingyun1@huawei.com>

--- frameworks/core/components_ng/render/adapter/component_snapshot.cpp
+++ frameworks/core/components_ng/render/adapter/component_snapshot.cpp
@@ -138,6 +138,7 @@ void ProcessImageNode(const RefPtr<UINode>& node)
         auto imageNode = AceType::DynamicCast<FrameNode>(node);
         if (imageNode && AceType::DynamicCast<ImagePattern>(imageNode->GetPattern())) {
             auto imagePattern = AceType::DynamicCast<ImagePattern>(imageNode->GetPattern());
+            imagePattern->SetIsComponentSnapshotNode();
             imagePattern->OnVisibleAreaChange(true);
         }
     }
