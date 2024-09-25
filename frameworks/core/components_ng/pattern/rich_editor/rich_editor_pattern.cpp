950f934919a611da17a12df7f07369dde8444e3d
xuyue <xuyue51@huawei.com>
2024-09-25 01:49:05 +0000

    update frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp.
    Signed-off-by: xuyue <xuyue51@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -8612,7 +8612,7 @@ void RichEditorPattern::FillPreviewMenuInJson(std::unique_ptr<JsonValue> jsonVal
     auto jsonItem = JsonUtil::Create(true);
     jsonItem->Put("spanType", static_cast<int32_t>(TextSpanType::IMAGE));
     jsonItem->Put("responseType", static_cast<int32_t>(TextResponseType::LONG_PRESS));
-    jsonItem->Put("menuType", static_cast<int32_t>(SelectionMenuType.PREVIEW_MENU));
+    jsonItem->Put("menuType", static_cast<int32_t>(SelectionMenuType::PREVIEW_MENU));
     jsonValue->Put(jsonItem);
 }
 
