d3d62c8679e74d5115fcac7b6e1640cb221d2688
zhengqiyi <zhengqiyi2@huawei.com>
2024-09-25 03:05:35 +0000

    cherry pick 5a12c96 from https://gitee.com/zheng-qiyi/arkui_ace_engine/pulls/44458 fix double click problem
    Signed-off-by: zhengqiyi <zhengqiyi2@huawei.com>

--- frameworks/core/components_ng/gestures/recognizers/click_recognizer.cpp
+++ frameworks/core/components_ng/gestures/recognizers/click_recognizer.cpp
@@ -318,6 +318,7 @@ void ClickRecognizer::HandleTouchUpEvent(const TouchEvent& event)
     if (currentTouchPointsNum_ == 0) {
         responseRegionBuffer_.clear();
     }
+    bool fingersNumberSatisfied = equalsToFingers_;
     // Check whether multi-finger taps are completed in count_ times
     if (equalsToFingers_ && (currentTouchPointsNum_ == 0) && isUpInRegion) {
         // Turn off the multi-finger lift deadline timer
@@ -332,7 +333,7 @@ void ClickRecognizer::HandleTouchUpEvent(const TouchEvent& event)
         DeadlineTimer(tapDeadlineTimer_, MULTI_TAP_TIMEOUT);
     }
     if (refereeState_ != RefereeState::PENDING && refereeState_ != RefereeState::FAIL) {
-        if (equalsToFingers_) {
+        if (fingersNumberSatisfied) {
             Adjudicate(AceType::Claim(this), GestureDisposal::PENDING);
         } else {
             Adjudicate(AceType::Claim(this), GestureDisposal::REJECT);
