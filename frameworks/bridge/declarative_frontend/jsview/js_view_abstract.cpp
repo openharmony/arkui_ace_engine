6d2b8ef5544988f5d0ae96ae5ac80768c971cf30
c30053238 <chenxiaoxue11@huawei.com>
2024-09-28 18:03:14 +0800

    add log
    Signed-off-by: c30053238 <chenxiaoxue11@huawei.com>

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
@@ -7719,6 +7719,7 @@ bool ParseBindContentCoverIsShow(const JSCallbackInfo& info)
         auto isShowObj = callbackObj->GetProperty("value");
         isShow = isShowObj->IsBoolean() ? isShowObj->ToBoolean() : false;
     }
+    TAG_LOGD(AceLogTag::ACE_SHEET, "ContentCover get isShow is: %{public}d", isShow);
     return isShow;
 }
 
