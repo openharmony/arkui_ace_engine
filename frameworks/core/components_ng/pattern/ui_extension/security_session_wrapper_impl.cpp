988b1c0cde8e6ed3fcbf2e816fd201d320f71d0f
dujingcheng <dujingcheng@huawei.com>
2024-09-29 02:12:51 +0000

    suec log
    Signed-off-by: dujingcheng <dujingcheng@huawei.com>
    Change-Id: Ia0c82dd5eb24a4f89e44bcc489315c52096bb3dd

--- frameworks/core/components_ng/pattern/ui_extension/security_session_wrapper_impl.cpp
+++ frameworks/core/components_ng/pattern/ui_extension/security_session_wrapper_impl.cpp
@@ -624,7 +624,7 @@ void SecuritySessionWrapperImpl::NotifyOriginAvoidArea(
     const Rosen::AvoidArea& avoidArea, uint32_t type) const
 {
     CHECK_NULL_VOID(session_);
-    PLATFORM_LOGD("The avoid area is notified to the provider.");
+    PLATFORM_LOGI("The avoid area is notified to the provider.");
     session_->UpdateAvoidArea(
         sptr<Rosen::AvoidArea>::MakeSptr(avoidArea), static_cast<Rosen::AvoidAreaType>(type));
 }
@@ -647,7 +647,7 @@ bool SecuritySessionWrapperImpl::NotifyOccupiedAreaChangeInfo(
     sptr<Rosen::OccupiedAreaChangeInfo> newInfo = new Rosen::OccupiedAreaChangeInfo(
         info->type_, info->rect_, info->safeHeight_, info->textFieldPositionY_, info->textFieldHeight_);
     newInfo->rect_.height_ = static_cast<uint32_t>(keyboardHeight);
-    PLATFORM_LOGD("The occcupied area with 'keyboardHeight = %{public}d' is notified to the provider.",
+    PLATFORM_LOGI("The occcupied area with 'keyboardHeight = %{public}d' is notified to the provider.",
         keyboardHeight);
     session_->NotifyOccupiedAreaChangeInfo(newInfo);
     return true;
