584c666b91977742a4bb780501aded11106c956c
zhangwenbin <zhangwenbin35@h-partners.com>
2024-10-10 15:06:06 +0800

    文本按钮字体大小异于普通按钮和强调按钮
    Signed-off-by: zhangwenbin <zhangwenbin35@h-partners.com>

--- frameworks/core/components_ng/pattern/button/button_pattern.cpp
+++ frameworks/core/components_ng/pattern/button/button_pattern.cpp
@@ -201,6 +201,17 @@ void ButtonPattern::InitButtonLabel()
     auto textRenderContext = textNode->GetRenderContext();
     CHECK_NULL_VOID(textRenderContext);
     textRenderContext->UpdateClipEdge(buttonRenderContext->GetClipEdgeValue(true));
+    auto context = host->GetContext();
+    CHECK_NULL_VOID(context);
+    auto buttonTheme = context->GetTheme<ButtonTheme>();
+    CHECK_NULL_VOID(buttonTheme);
+    if (buttonTheme->GetIsApplyTextFontSize()) {
+        ControlSize controlSize = layoutProperty->GetControlSize().value_or(ControlSize::NORMAL);
+        ButtonStyleMode buttonStyle = layoutProperty->GetButtonStyle().value_or(ButtonStyleMode::EMPHASIZE);
+        Dimension fontSize = buttonStyle == ButtonStyleMode::TEXT ?
+            buttonTheme->GetTextButtonFontSize() : buttonTheme->GetTextSize(controlSize);
+        textLayoutProperty->UpdateFontSize(fontSize);
+    }
     textNode->MarkModifyDone();
     textNode->MarkDirtyNode();
 
