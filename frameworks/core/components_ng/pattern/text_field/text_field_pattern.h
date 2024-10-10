45223c14c8e5350c7dbc31bc583bc6d3cb877712
wanjining <wanjining@huawei.com>
2024-10-10 18:00:20 +0800

    蓝黄同步
    Signed-off-by: wanjining <wanjining@huawei.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.h
@@ -1631,6 +1631,7 @@ private:
     void HandleParentGlobalOffsetChange();
     HintToTypeWrap GetHintType();
     HintToTypeWrap GetAutoFillTypeAndMetaData(bool isNeedToHitType = true);
+    PaddingProperty GetPaddingByUserValue();
     void SetThemeAttr();
     void SetThemeBorderAttr();
     void ProcessInlinePaddingAndMargin();
