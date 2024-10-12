cda26e1907ce0b96905f95628141a8fe898622cc
lvan_UwU <linzuoquan@huawei.com>
2024-10-12 11:24:57 +0800

    调整缩进
    Signed-off-by: lvan_UwU <linzuoquan@huawei.com>
    Change-Id: I9f61c3d9d3933c1c140f462b616608a4c323b084

--- adapter/ohos/osal/js_accessibility_manager.cpp
+++ adapter/ohos/osal/js_accessibility_manager.cpp
@@ -1820,9 +1820,9 @@ void JsAccessibilityManager::UpdateChildrenNodeInCache(std::list<AccessibilityEl
             continue;
         }
         if (!((frameNodeParent->GetUiExtensionId() > NG::UI_EXTENSION_UNKNOW_ID) &&
-                (((frameNodeParent->GetUiExtensionId() <= NG::UI_EXTENSION_ID_FIRST_MAX) &&
-                    (NG::UI_EXTENSION_OFFSET_MAX == searchParam.uiExtensionOffset)) ||
-                        (frameNodeParent->GetUiExtensionId() <= NG::UI_EXTENSION_ID_OTHER_MAX)))) {
+            (((frameNodeParent->GetUiExtensionId() <= NG::UI_EXTENSION_ID_FIRST_MAX) &&
+            (NG::UI_EXTENSION_OFFSET_MAX == searchParam.uiExtensionOffset)) ||
+            (frameNodeParent->GetUiExtensionId() <= NG::UI_EXTENSION_ID_OTHER_MAX)))) {
             continue;
         }
         auto transferParam = searchParam;
