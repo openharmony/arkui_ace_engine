322e65d927f6fcfc259c548724229b321ef8e556
yangcan <yangcan18@huawei.com>
2024-10-15 08:34:24 +0000

    update frameworks/core/components_ng/pattern/tabs/tabs_model_ng.cpp.
    Signed-off-by: yangcan <yangcan18@huawei.com>

--- frameworks/core/components_ng/pattern/tabs/tabs_model_ng.cpp
+++ frameworks/core/components_ng/pattern/tabs/tabs_model_ng.cpp
@@ -46,8 +46,6 @@
 
 namespace OHOS::Ace::NG {
 namespace {
-constexpr Dimension BAR_BLUR_RADIUS = 200.0_vp;
-constexpr Dimension BAR_SATURATE = 1.3_vp;
 constexpr uint8_t PIXEL_ROUND = static_cast<uint8_t>(PixelRoundPolicy::FORCE_FLOOR_START) |
                                 static_cast<uint8_t>(PixelRoundPolicy::FORCE_FLOOR_TOP) |
                                 static_cast<uint8_t>(PixelRoundPolicy::FORCE_CEIL_END) |
