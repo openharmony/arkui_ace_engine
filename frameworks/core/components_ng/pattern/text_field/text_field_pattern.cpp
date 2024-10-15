a566e189119df22b9a9bb971154694d74f10d64a
kangshihui <kangshihui@huawei.com>
2024-10-15 11:08:24 +0800

    AutoFill检视意见修复 Signed-off-by:kangshihui<kangshihui@huawei.com>
    Change-Id: Ica3b58eca6048d0b99eb68e1cfca7057c58cd57d

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -7079,8 +7079,8 @@ void TextFieldPattern::NotifyFillRequestSuccess(RefPtr<ViewDataWrap> viewDataWra
         DoProcessAutoFill();
     }
     auto type = GetAutoFillType();
-    bool formOtherAccount = viewDataWrap->GetOtherAccount();
-    if (!(type == AceAutoFillType::ACE_NEW_PASSWORD && type == autoFillType) && !formOtherAccount) {
+    bool fromOtherAccount = viewDataWrap->GetOtherAccount();
+    if (!(type == AceAutoFillType::ACE_NEW_PASSWORD && type == autoFillType) && !fromOtherAccount) {
         TAG_LOGI(AceLogTag::ACE_AUTO_FILL, "Set last auto fill text value.");
         lastAutoFillTextValue_ = nodeWrap->GetValue();
     }
