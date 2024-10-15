3ac398aef06fb4fbc6604b5aaf442ec105114bc2
小小熊睡觉觉 <790343309@qq.com>
2024-10-15 15:14:11 +0800

    BB代码同步
    Signed-off-by: 小小熊睡觉觉 <790343309@qq.com>

--- adapter/ohos/osal/log_wrapper.cpp
+++ adapter/ohos/osal/log_wrapper.cpp
@@ -129,6 +129,8 @@ const std::unordered_map<AceLogTag, const char*> g_DOMAIN_CONTENTS_MAP = {
     { AceLogTag::ACE_CLIPBOARD, "AceClipBoard"},
     { AceLogTag::ACE_SECURITY_COMPONENT, "AceSecurityComponent"},
     { AceLogTag::ACE_MEDIA_QUERY, "AceMediaQuery" },
+    { AceLogTag::ACE_LAYOUT_INSPECTOR, "AceLayoutInspector" },
+    { AceLogTag::ACE_LAYOUT, "AceLayout" },
 };
 // initial static member object
 LogLevel LogWrapper::level_ = LogLevel::DEBUG;
