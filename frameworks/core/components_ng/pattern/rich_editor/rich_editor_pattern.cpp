def4c447f5366ffe69baa95daf4aa2a184a742c3
carnivore233 <xuyue51@huawei.com>
2024-10-17 17:15:03 +0800

    【RichEditor】修改预上屏输入异常
    Signed-off-by: carnivore233 <xuyue51@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -6340,7 +6340,7 @@ void RichEditorPattern::DeleteByDeleteValueInfo(const RichEditorDeleteValue& inf
     CHECK_NULL_VOID(host);
     ProcessDeleteNodes(deleteSpans);
     UpdateSpanPosition();
-    SetCaretPosition(info.GetOffset());
+    SetCaretPosition(info.GetOffset(), false);
     host->MarkDirtyNode(PROPERTY_UPDATE_MEASURE);
     OnModifyDone();
 }
