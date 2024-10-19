c85a525034d6a387b71199c841c11f9c9eef444c
z30053720 <zhaoyong70@huawei.com>
2024-10-19 17:53:06 +0800

    fix picker focusbox
    Signed-off-by: z30053720 <zhaoyong70@huawei.com>

--- test/unittest/core/pattern/picker/date_picker_test_ng.cpp
+++ test/unittest/core/pattern/picker/date_picker_test_ng.cpp
@@ -1409,8 +1409,9 @@ HWTEST_F(DatePickerTestNg, DatePickerPatternTest005, TestSize.Level1)
     RoundRect paintRect;
     datePickerPattern->GetInnerFocusPaintRect(paintRect);
     auto rect = paintRect.GetRect();
-    EXPECT_EQ(rect.GetX(), 0);
-    EXPECT_EQ(rect.Width(), pickerChild->GetGeometryNode()->GetFrameSize().Width());
+    Dimension offset = 2.0_vp;
+    EXPECT_EQ(rect.GetX(), offset.ConvertToPx());
+    EXPECT_EQ(rect.Width(), pickerChild->GetGeometryNode()->GetFrameSize().Width() - offset.ConvertToPx() * 2);
 }
 
 /**
