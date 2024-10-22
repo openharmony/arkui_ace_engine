799e17f2e724caa2ead415368cbf22ef8bf93c9e
voronov.vadim <voronov.vadim@huawei-partners.com>
2024-10-22 12:02:44 +0300

    canvas modifier review update
    Signed-off-by: voronov.vadim <voronov.vadim@huawei-partners.com>

--- frameworks/core/interfaces/arkoala/implementation/canvas_modifier.cpp
+++ frameworks/core/interfaces/arkoala/implementation/canvas_modifier.cpp
@@ -25,13 +25,13 @@ namespace CanvasInterfaceModifier {
 void SetCanvasOptions0Impl(Ark_NativePointer node,
                            const Opt_Union_CanvasRenderingContext2D_DrawingRenderingContext* context)
 {
-    LOGE("CanvasInterfaceModifier::Canvas.SetCanvasOptions0Impl - method not implemented");
+    LOGE("CanvasInterfaceModifier::SetCanvasOptions0Impl - method not implemented");
 }
 void SetCanvasOptions1Impl(Ark_NativePointer node,
                            const Ark_Union_CanvasRenderingContext2D_DrawingRenderingContext* context,
                            const Ark_ImageAIOptions* imageAIOptions)
 {
-    LOGE("CanvasInterfaceModifier::Canvas.SetCanvasOptions1Impl - method not implemented");
+    LOGE("CanvasInterfaceModifier::SetCanvasOptions1Impl - method not implemented");
 }
 } // CanvasInterfaceModifier
 namespace CanvasAttributeModifier {
