724b31502dd1dbe2855abb61d2774be65ab83f79
lzr <liuzengrui1@huawei.com>
2024-10-23 10:23:04 +0800

    fix wild pointer
    Signed-off-by: lzr <liuzengrui1@huawei.com>
    Change-Id: Ibad2370e277fe93daa696d841ec0bcb2ddfeb8aa

--- frameworks/bridge/declarative_frontend/engine/arkComponent.js
+++ frameworks/bridge/declarative_frontend/engine/arkComponent.js
@@ -3105,6 +3105,7 @@ class ArkComponent {
   }
   setNodePtr(nodePtr) {
     this.nativePtr = nodePtr;
+    this._weakPtr = getUINativeModule().nativeUtils.createNativeWeakRef(nativePtr);
   }
   setInstanceId(instanceId) {
     this._instanceId = instanceId;
@@ -3140,7 +3141,7 @@ class ArkComponent {
   applyModifierPatch() {
     let expiringItemsWithKeys = [];
     this._modifiersWithKeys.forEach((value, key) => {
-      if (value.applyStage(this.nativePtr, this)) {
+      if (!this._weakPtr?.invalid() && value.applyStage(this.nativePtr, this)) {
         expiringItemsWithKeys.push(key);
       }
     });
