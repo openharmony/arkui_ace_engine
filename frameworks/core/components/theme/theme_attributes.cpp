8086e91086304901751cf263c4a4560cc4631133
liyaotong <liyaotong2@huawei.com>
2024-10-24 15:13:20 +0800

    支持动态焦点动效
    Signed-off-by: liyaotong <liyaotong2@huawei.com>
    Change-Id: I72a7aa6a06e740689ecd8f1c9940d9ec7186110f

--- frameworks/core/components/theme/theme_attributes.cpp
+++ frameworks/core/components/theme/theme_attributes.cpp
@@ -84,6 +84,7 @@ const char THEME_BLUR_STYLE_COMMON[] = "blur_style_common";
 const char THEME_PATTERN_SHADOW[] = "shadow_pattern";
 const char THEME_PATTERN_CONTAINER_MODAL[] = "container_modal_pattern";
 const char THEME_PATTERN_SCROLLABLE[] = "scrollable_pattern";
+const char THEME_PATTERN_APP[] = "app_theme_pattern";
 
 // pattern general attributes
 const char PATTERN_FG_COLOR[] = "fg_color";
