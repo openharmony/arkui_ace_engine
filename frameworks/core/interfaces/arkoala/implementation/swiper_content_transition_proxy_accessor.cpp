fe40148f064c7c58e2d54ac77e20282f1029748f
gleb_skroba <skroba.gleb1@huawei.com>
2024-10-27 22:19:47 +0300

    SwiperContentTransitionProxyAccessor with UT
    Signed-off-by: gleb_skroba <skroba.gleb1@huawei.com>
    Change-Id: I6a35a71b1e3bb51cdea064e741b8dfbca732f5c4

--- frameworks/core/interfaces/arkoala/implementation/swiper_content_transition_proxy_accessor.cpp
+++ frameworks/core/interfaces/arkoala/implementation/swiper_content_transition_proxy_accessor.cpp
@@ -13,54 +13,108 @@
  * limitations under the License.
  */
 
-#include "core/components_ng/base/frame_node.h"
+#include "core/interfaces/arkoala/implementation/swiper_content_transition_proxy_peer_impl.h"
 #include "core/interfaces/arkoala/utility/converter.h"
-#include "arkoala_api_generated.h"
+#include "core/interfaces/arkoala/utility/reverse_converter.h"
 
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace SwiperContentTransitionProxyAccessor {
-SwiperContentTransitionProxyPeer* CtorImpl()
+static void DestroyPeer(SwiperContentTransitionProxyPeerImpl *peerImpl)
 {
-    return nullptr;
+    if (peerImpl) {
+        if (peerImpl->handler) {
+            peerImpl->handler = nullptr;
+        }
+        delete peerImpl;
+    }
+}
+
+Ark_NativePointer CtorImpl()
+{
+    return reinterpret_cast<Ark_NativePointer>(new SwiperContentTransitionProxyPeerImpl());
 }
 Ark_NativePointer GetFinalizerImpl()
 {
-    return 0;
+    return reinterpret_cast<Ark_NativePointer>(&DestroyPeer);
 }
 void FinishTransitionImpl(SwiperContentTransitionProxyPeer* peer)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        peerImpl->handler->FinishTransition();
+    }
 }
 Ark_Int32 GetSelectedIndexImpl(SwiperContentTransitionProxyPeer* peer)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        return Converter::ArkValue<Ark_Int32>(peerImpl->handler->GetSelectedIndex());
+    }
     return 0;
 }
 void SetSelectedIndexImpl(SwiperContentTransitionProxyPeer* peer,
                           const Ark_Number* selectedIndex)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        if (selectedIndex) {
+            peerImpl->handler->SetSelectedIndex(Converter::Convert<int32_t>(*selectedIndex));
+        }
+    }
 }
 Ark_Int32 GetIndexImpl(SwiperContentTransitionProxyPeer* peer)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        return Converter::ArkValue<Ark_Int32>(peerImpl->handler->GetIndex());
+    }
     return 0;
 }
 void SetIndexImpl(SwiperContentTransitionProxyPeer* peer,
                   const Ark_Number* index)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        if (index) {
+            peerImpl->handler->SetIndex(Converter::Convert<int32_t>(*index));
+        }
+    }
 }
 Ark_Int32 GetPositionImpl(SwiperContentTransitionProxyPeer* peer)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        return static_cast<Ark_Int32>(peerImpl->handler->GetPosition());
+    }
     return 0;
 }
 void SetPositionImpl(SwiperContentTransitionProxyPeer* peer,
                      const Ark_Number* position)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        if (position) {
+            peerImpl->handler->SetPosition(Converter::Convert<float>(*position));
+        }
+    }
 }
 Ark_Int32 GetMainAxisLengthImpl(SwiperContentTransitionProxyPeer* peer)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        return static_cast<Ark_Int32>(peerImpl->handler->GetMainAxisLength());
+    }
     return 0;
 }
 void SetMainAxisLengthImpl(SwiperContentTransitionProxyPeer* peer,
                            const Ark_Number* mainAxisLength)
 {
+    auto peerImpl = reinterpret_cast<SwiperContentTransitionProxyPeerImpl *>(peer);
+    if (peerImpl && peerImpl->handler) {
+        if (mainAxisLength) {
+            peerImpl->handler->SetMainAxisLength(Converter::Convert<float>(*mainAxisLength));
+        }
+    }
 }
 } // SwiperContentTransitionProxyAccessor
 const GENERATED_ArkUISwiperContentTransitionProxyAccessor* GetSwiperContentTransitionProxyAccessor()
