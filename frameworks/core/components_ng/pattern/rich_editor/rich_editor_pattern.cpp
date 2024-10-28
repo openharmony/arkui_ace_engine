d13d7c5ae700e13d169ba6606192cffc2da832ef
zzc0127 <zhangzecong@huawei.com>
2024-10-28 02:04:20 +0000

    update frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp.
    Signed-off-by: zzc0127 <zhangzecong@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -2341,6 +2341,7 @@ bool RichEditorPattern::HandleClickSelection(const OHOS::Ace::GestureEvent& info
 {
     CHECK_NULL_RETURN(!selectOverlay_->GetIsHandleMoving(), true);
     if (SelectOverlayIsOn()) {
+        selectOverlay_->SwitchToOverlayMode();
         selectOverlay_->ToggleMenu();
     } else {
         CalculateHandleOffsetAndShowOverlay();
