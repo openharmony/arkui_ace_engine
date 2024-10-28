ee6ee20581de3d6afa9b674df56bf45d27a58799
liubihao <liubihao1@huawei.com>
2024-10-28 22:05:26 +0800

    RepeatVirtualScroll. Trigger rerender when totalCount is 0. code format fix.
    Signed-off-by: liubihao <liubihao1@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/stateMgmt.js
+++ frameworks/bridge/declarative_frontend/engine/stateMgmt.js
@@ -10762,7 +10762,7 @@ class __RepeatVirtualScrollImpl {
     reRender() {
         
         // When this.totalCount_ == 0 need render to clear visible items
-        if (this.hasVisibleItemsChanged() || this.totalCount_ == 0) {
+        if (this.hasVisibleItemsChanged() || this.totalCount_ === 0) {
             this.purgeKeyCache();
             RepeatVirtualScrollNative.updateRenderState(this.totalCount_, true);
             
