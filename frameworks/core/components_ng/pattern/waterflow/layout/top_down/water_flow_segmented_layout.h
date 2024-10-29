5fc91394670059f5be93743577de0f34e5aeee7a
Tianer Zhou <zhoutianer@huawei.com>
2024-10-29 14:20:00 +0800

    fix FlowItem sync preload
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: I9eeae15d71fb6fd39a9cad1b614f3ee79dd540bb

--- frameworks/core/components_ng/pattern/waterflow/layout/top_down/water_flow_segmented_layout.h
+++ frameworks/core/components_ng/pattern/waterflow/layout/top_down/water_flow_segmented_layout.h
@@ -126,8 +126,9 @@ private:
      * @param targetIdx index of the last FlowItem to measure.
      * @param cacheDeadline when called during a cache layout, always measure the items and return early if deadline is
      * reached.
+     * @param force explicitly measure items even if their heights are user-defined.
      */
-    void MeasureToTarget(int32_t targetIdx, std::optional<int64_t> cacheDeadline);
+    void MeasureToTarget(int32_t targetIdx, std::optional<int64_t> cacheDeadline, bool force = false);
 
     /**
      * @brief Helper to measure a single FlowItems.
