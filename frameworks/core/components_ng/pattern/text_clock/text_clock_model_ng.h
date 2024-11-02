17a2997ce6895273f003b3c13473b4bbce861ab8
pavelyevivan <pavelyev.ivan@huawei-partners.com>
2024-11-02 14:01:14 +0300

    Implementing attributes for TextCloclModifire
    Signed-off-by: pavelyevivan <pavelyev.ivan@huawei-partners.com>
    Change-Id: I1d20689f90d69a230c3c1f6446cb2ff225f1d593

--- frameworks/core/components_ng/pattern/text_clock/text_clock_model_ng.h
+++ frameworks/core/components_ng/pattern/text_clock/text_clock_model_ng.h
@@ -49,12 +49,12 @@ public:
 
     static RefPtr<FrameNode> CreateFrameNode(int32_t nodeId);
     static RefPtr<TextClockController> InitTextController(FrameNode* frameNode);
-    static void SetFormat(FrameNode* frameNode, const std::string& format);
+    static void SetFormat(FrameNode* frameNode, const std::optional<std::string>& format);
     static void SetHoursWest(FrameNode* frameNode, float hoursWest);
-    static void SetFontColor(FrameNode* frameNode, const Color& value);
-    static void SetFontSize(FrameNode* frameNode, const Dimension& value);
-    static void SetFontStyle(FrameNode* frameNode, Ace::FontStyle value);
-    static void SetFontWeight(FrameNode* frameNode, FontWeight value);
+    static void SetFontColor(FrameNode* frameNode, const std::optional<Color>& color);
+    static void SetFontSize(FrameNode* frameNode, const std::optional<Dimension>& size);
+    static void SetFontStyle(FrameNode* frameNode, const std::optional<Ace::FontStyle>& value);
+    static void SetFontWeight(FrameNode* frameNode, const std::optional<Ace::FontWeight>& value);
     static void SetFontFamily(FrameNode* frameNode, const std::vector<std::string>& value);
     static void SetTextShadow(FrameNode* frameNode, const std::vector<Shadow>& value);
     static void SetFontFeature(FrameNode* frameNode, const FONT_FEATURES_LIST& value);
