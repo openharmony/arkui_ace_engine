c21a1a0c31d5da4dfd8639a4fe9086bb6282a3a4
Zjsxstar <yuguanbin@huawei.com>
2024-11-02 12:06:25 +0800

    修复代码错误问题
    Signed-off-by: Zjsxstar <yuguanbin@huawei.com>
    Change-Id: I674b0c26629d535a298221baf911f86b993a2504

--- frameworks/bridge/declarative_frontend/ark_component/src/ArkComponent.ts
+++ frameworks/bridge/declarative_frontend/ark_component/src/ArkComponent.ts
@@ -4302,7 +4302,7 @@ class ArkComponent implements CommonMethod<CommonAttribute> {
 
   onDragMove(event: (event?: DragEvent, extraParams?: string) => void): this {
     modifierWithKey(this._modifiersWithKeys, DragMoveModifier.identity, DragMoveModifier, event);
-    return this;throw new Error('Method not implemented.');
+    return this;
   }
 
   onDragLeave(event: (event?: DragEvent, extraParams?: string) => void): this {
