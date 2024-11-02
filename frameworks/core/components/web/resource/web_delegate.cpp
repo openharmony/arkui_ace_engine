f388dda11347dc5946bf8e37b7fbf67d3dec4d8b
liuzeyu <liuzeyu27@huawei.com>
2024-11-02 20:03:08 +0800

    修改requestFocusImmediately获焦
    Signed-off-by: liuzeyu <liuzeyu27@huawei.com>

--- frameworks/core/components/web/resource/web_delegate.cpp
+++ frameworks/core/components/web/resource/web_delegate.cpp
@@ -1360,7 +1360,10 @@ bool WebDelegate::RequestFocus(OHOS::NWeb::NWebFocusSource source)
                     result = false;
                     return;
                 }
-                if (focusHub->IsOnRootTree()) {
+
+                auto host = webPattern->GetHost();
+                CHECK_NULL_VOID(host);
+                if (host->IsOnMainTree()) {
                     focusHub->RequestFocus();
                     result = false;
                 }
