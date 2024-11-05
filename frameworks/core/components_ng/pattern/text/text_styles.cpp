0ca2d332f90caf6530bc526f7ad009610854f7ec
朱维峰 <zhuweifeng1@huawei.com>
2024-11-05 07:29:57 +0000

    update frameworks/core/components_ng/pattern/text/text_styles.cpp.
    Signed-off-by: 朱维峰 <zhuweifeng1@huawei.com>

--- frameworks/core/components_ng/pattern/text/text_styles.cpp
+++ frameworks/core/components_ng/pattern/text/text_styles.cpp
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -200,5 +199,23 @@ std::string GetSymbolEffectOptionsInJson(const std::optional<SymbolEffectOptions
         text = value.value().ToString();
     }
     return text;
+}
+
+void FontStyle::UpdateColorByResourceId()
+{
+    if (propTextColor) {
+        propTextColor->UpdateColorByResourceId();
+    }
+    if (propTextDecorationColor) {
+        propTextDecorationColor->UpdateColorByResourceId();
+    }
+    if (propTextShadow) {
+        auto& shadows = propTextShadow.value();
+        std::for_each(shadows.begin(), shadows.end(), [](Shadow& sd) { sd.UpdateColorByResourceId(); });
+    }
+    if (propSymbolColorList) {
+        auto& colors = propSymbolColorList.value();
+        std::for_each(colors.begin(), colors.end(), [](Color& cl) { cl.UpdateColorByResourceId(); });
+    }
 }
 } // namespace OHOS::Ace::NG
