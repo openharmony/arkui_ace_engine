5d04afebc91a5cae13c58b5d54da9e8ada207747
朱维峰 <zhuweifeng1@huawei.com>
2024-11-05 07:29:17 +0000

    update frameworks/core/components/common/properties/text_style.h.
    Signed-off-by: 朱维峰 <zhuweifeng1@huawei.com>

--- frameworks/core/components/common/properties/text_style.h
+++ frameworks/core/components/common/properties/text_style.h
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -378,11 +377,6 @@ public:
         textColor_ = textColor;
     }
 
-    void SetTextColor(const DynamicColor& textColor)
-    {
-        textColor_ = textColor;
-    }
-
     TextDecoration GetTextDecoration() const
     {
         return textDecoration_;
@@ -405,15 +399,10 @@ public:
 
     const Color GetTextDecorationColor() const
     {
-        return textDecorationColor_.ToColor();
-    }
-
-    const DynamicColor GetDynamicTextDecorationColor() const
-    {
         return textDecorationColor_;
     }
 
-    void SetTextDecorationColor(const DynamicColor& textDecorationColor)
+    void SetTextDecorationColor(const Color& textDecorationColor)
     {
         textDecorationColor_ = textDecorationColor;
     }
@@ -807,8 +796,8 @@ private:
     TextCase textCase_ { TextCase::NORMAL };
     EllipsisMode ellipsisMode_ = EllipsisMode::TAIL;
     LineBreakStrategy lineBreakStrategy_ { LineBreakStrategy::GREEDY };
-    DynamicColor textColor_ { Color::BLACK };
-    DynamicColor textDecorationColor_ { Color::BLACK };
+    Color textColor_ { Color::BLACK };
+    Color textDecorationColor_ { Color::BLACK };
     uint32_t maxLines_ = UINT32_MAX;
     int32_t variableFontWeight_ = 0;
     bool hasHeightOverride_ = false;
