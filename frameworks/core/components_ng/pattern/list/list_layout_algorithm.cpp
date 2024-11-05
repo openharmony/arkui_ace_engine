13390b5076c630c470455c01a69c8b56f710d40e
shihongjie <shihongjie@huawei.com>
2024-11-05 15:23:28 +0800

    修复底部过界回弹时前向大距离跳转异常的问题
    Signed-off-by: shihongjie <shihongjie@huawei.com>
    Change-Id: I6f8f3cdbd4e77293fab6e6a2292af70800ce64af

--- frameworks/core/components_ng/pattern/list/list_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/list/list_layout_algorithm.cpp
@@ -1069,11 +1069,11 @@ void ListLayoutAlgorithm::LayoutForward(LayoutWrapper* layoutWrapper, int32_t st
     } while (LessOrEqual(currentEndPos + chainOffset, endMainPos) || forwardFeature_);
     currentEndPos += chainOffset;
 
-    while (!itemPosition_.empty() && !targetIndex_) {
+    while (itemPosition_.size() > 1 && !targetIndex_) {
         auto pos = itemPosition_.rbegin();
         float chainDelta = chainOffsetFunc_ ? chainOffsetFunc_(pos->first) : 0.0f;
         if (GreatNotEqual(pos->second.endPos + chainDelta, endMainPos) &&
-            GreatOrEqual(pos->second.startPos + chainDelta, endMainPos) && pos->first != 0) {
+            GreatOrEqual(pos->second.startPos + chainDelta, endMainPos)) {
             recycledItemPosition_.emplace(pos->first, pos->second);
             itemPosition_.erase(pos->first);
         } else {
