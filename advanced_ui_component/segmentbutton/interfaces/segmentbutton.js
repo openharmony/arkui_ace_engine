3b82c6cd6632d896cb47f4cc5dda06dfb745cb79
Zhang Jinyu <zhangjinyu101@huawei.com>
2024-11-06 11:27:24 +0800

    【挑单5.0.1】修改SegmentButton获焦后按enter后获焦框消失
    Signed-off-by: Zhang Jinyu <zhangjinyu101@huawei.com>

--- advanced_ui_component/segmentbutton/interfaces/segmentbutton.js
+++ advanced_ui_component/segmentbutton/interfaces/segmentbutton.js
@@ -1531,7 +1531,6 @@ class m1 extends ViewPU {
                                             Gesture.create(GesturePriority.Low);
                                             TapGesture.create();
                                             TapGesture.onAction(() => {
-                                                this.focusIndex = -1;
                                                 if (this.options.type === 'capsule' &&
                                                     (this.options.multiply ?? false)) {
                                                     if (this.selectedIndexes.indexOf(index) === -1) {
