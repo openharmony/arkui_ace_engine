be8a706b25f9f2ce7465038f3a5dbe68572e1bed
wiiinston <pangwendong@huawei.com>
2024-11-12 11:53:39 +0800

    modify depth_
    Signed-off-by: wiiinston <pangwendong@huawei.com>

--- frameworks/core/components_ng/base/ui_node.h
+++ frameworks/core/components_ng/base/ui_node.h
@@ -882,7 +882,7 @@ private:
     std::unique_ptr<PerformanceCheckNode> nodeInfo_;
     WeakPtr<UINode> parent_;
     std::string tag_ = "UINode";
-    int32_t depth_ = INT32_MAX;
+    int32_t depth_ = Infinity<int32_t>();
     int32_t hostRootId_ = 0;
     int32_t hostPageId_ = 0;
     int32_t nodeId_ = 0;
