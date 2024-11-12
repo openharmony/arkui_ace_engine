0080483337e8864628a74033aa6bf43a82e1f4f9
Nikita Maksimov <maksimov.nikita@huawei.com>
2024-11-12 10:06:34 +0300

    The generated tests has been moved
    Signed-off-by: Nikita Maksimov <maksimov.nikita@huawei.com>
    Change-Id: I8465ae27b87e393622f99496c021312fb0d377f3

--- test/unittest/capi/modifiers/calendar_modifier_test.cpp
+++ test/unittest/capi/modifiers/calendar_modifier_test.cpp
@@ -32,6 +32,10 @@ class CalendarModifierTest : public ModifierTestBase<
 namespace {
 constexpr auto OPTIONS_NAME = "value";
 constexpr auto OPTIONS_VALUE = ""; // There is no field in JSON.
+constexpr auto ATTRIBUTE_NEED_SLIDE_NAME = "needSlide";
+constexpr auto ATTRIBUTE_NEED_SLIDE_DEFAULT_VALUE = "false";
+constexpr auto ATTRIBUTE_DIRECTION_NAME = "direction";
+constexpr auto ATTRIBUTE_DIRECTION_DEFAULT_VALUE = "1";
 
 const std::vector<Ark_CalendarDay> currentDayArray {
     {
@@ -107,6 +111,21 @@ const Ark_Type_CalendarInterface_value calendarOptions {
     },
     .controller = Converter::ArkValue<Opt_CalendarController>(Ark_Empty()),
 };
+
+const std::vector<std::tuple<std::string, Ark_Boolean, std::string>> testFixtureBooleanValidValues = {
+    { "true", Converter::ArkValue<Ark_Boolean>(true), "true" },
+    { "false", Converter::ArkValue<Ark_Boolean>(false), "false" },
+};
+
+const std::vector<std::tuple<std::string, Ark_Axis, std::string>> testFixtureIntAxisValidValues = {
+    { "ARK_AXIS_VERTICAL", Converter::ArkValue<Ark_Axis>(ARK_AXIS_VERTICAL), "0" },
+    { "ARK_AXIS_HORIZONTAL", Converter::ArkValue<Ark_Axis>(ARK_AXIS_HORIZONTAL), "1" },
+};
+
+const std::vector<std::tuple<std::string, Ark_Axis>> testFixtureIntAxisInvalidValues = {
+    { "static_cast<Ark_Axis>(-1)", Converter::ArkValue<Ark_Axis>(static_cast<Ark_Axis>(-1)) },
+    { "static_cast<Ark_Axis>(INT_MAX)", Converter::ArkValue<Ark_Axis>(static_cast<Ark_Axis>(INT_MAX)) },
+};
 } // namespace
 
 /*
@@ -120,6 +139,121 @@ HWTEST_F(CalendarModifierTest, DISABLED_setCalendarOptionsTestValidValues1, Test
     std::unique_ptr<JsonValue> jsonValue = GetJsonValue(node_);
 
     EXPECT_EQ(GetAttrValue<std::string>(jsonValue, OPTIONS_NAME), OPTIONS_VALUE);
+}
+
+/*
+ * @tc.name: setNeedSlideTestDefaultValues
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(CalendarModifierTest, setNeedSlideTestDefaultValues, TestSize.Level1)
+{
+    std::unique_ptr<JsonValue> jsonValue = GetJsonValue(node_);
+    std::string resultStr;
+
+    resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_NEED_SLIDE_NAME);
+    EXPECT_EQ(resultStr, ATTRIBUTE_NEED_SLIDE_DEFAULT_VALUE) << "Default value for attribute 'needSlide'";
+}
+
+/*
+ * @tc.name: setNeedSlideTestNeedSlideValidValues
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(CalendarModifierTest, setNeedSlideTestNeedSlideValidValues, TestSize.Level1)
+{
+    Ark_Boolean initValueNeedSlide;
+
+    // Initial setup
+    initValueNeedSlide = std::get<1>(testFixtureBooleanValidValues[0]);
+
+    auto checkValue = [this, &initValueNeedSlide](
+                          const std::string& input, const Ark_Boolean& value, const std::string& expectedStr) {
+        Ark_Boolean inputValueNeedSlide = initValueNeedSlide;
+
+        inputValueNeedSlide = value;
+        modifier_->setNeedSlide(node_, inputValueNeedSlide);
+        auto jsonValue = GetJsonValue(node_);
+        auto resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_NEED_SLIDE_NAME);
+        EXPECT_EQ(resultStr, expectedStr)
+            << "Input value is: " << input << ", method: setNeedSlide, attribute: needSlide";
+    };
+
+    for (auto& [input, value, expected] : testFixtureBooleanValidValues) {
+        checkValue(input, value, expected);
+    }
+}
+
+/*
+ * @tc.name: setDirectionTestDefaultValues
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(CalendarModifierTest, DISABLED_setDirectionTestDefaultValues, TestSize.Level1)
+{
+    std::unique_ptr<JsonValue> jsonValue = GetJsonValue(node_);
+    std::string resultStr;
+
+    resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_DIRECTION_NAME);
+    EXPECT_EQ(resultStr, ATTRIBUTE_DIRECTION_DEFAULT_VALUE) << "Default value for attribute 'direction'";
+}
+
+/*
+ * @tc.name: setDirectionTestDirectionValidValues
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(CalendarModifierTest, DISABLED_setDirectionTestDirectionValidValues, TestSize.Level1)
+{
+    Ark_Axis initValueDirection;
+
+    // Initial setup
+    initValueDirection = std::get<1>(testFixtureIntAxisValidValues[0]);
+
+    auto checkValue = [this, &initValueDirection](
+                          const std::string& input, const Ark_Axis& value, const std::string& expectedStr) {
+        Ark_Axis inputValueDirection = initValueDirection;
+
+        inputValueDirection = value;
+        modifier_->setDirection(node_, inputValueDirection);
+        auto jsonValue = GetJsonValue(node_);
+        auto resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_DIRECTION_NAME);
+        EXPECT_EQ(resultStr, expectedStr)
+            << "Input value is: " << input << ", method: setDirection, attribute: direction";
+    };
+
+    for (auto& [input, value, expected] : testFixtureIntAxisValidValues) {
+        checkValue(input, value, expected);
+    }
+}
+
+/*
+ * @tc.name: setDirectionTestDirectionInvalidValues
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(CalendarModifierTest, DISABLED_setDirectionTestDirectionInvalidValues, TestSize.Level1)
+{
+    Ark_Axis initValueDirection;
+
+    // Initial setup
+    initValueDirection = std::get<1>(testFixtureIntAxisValidValues[0]);
+
+    auto checkValue = [this, &initValueDirection](const std::string& input, const Ark_Axis& value) {
+        Ark_Axis inputValueDirection = initValueDirection;
+
+        modifier_->setDirection(node_, inputValueDirection);
+        inputValueDirection = value;
+        modifier_->setDirection(node_, inputValueDirection);
+        auto jsonValue = GetJsonValue(node_);
+        auto resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_DIRECTION_NAME);
+        EXPECT_EQ(resultStr, ATTRIBUTE_DIRECTION_DEFAULT_VALUE)
+            << "Input value is: " << input << ", method: setDirection, attribute: direction";
+    };
+
+    for (auto& [input, value] : testFixtureIntAxisInvalidValues) {
+        checkValue(input, value);
+    }
 }
 
 } // namespace OHOS::Ace::NG
