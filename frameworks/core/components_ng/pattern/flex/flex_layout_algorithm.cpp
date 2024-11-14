6ee094cfa1dc37f1de4bc0025452e030dd8dc8dd
Shi Bofan <shibofan1@huawei.com>
2024-11-14 19:35:57 +0800

    flex布局维测增强
    Signed-off-by: Shi Bofan <shibofan1@huawei.com>

--- frameworks/core/components_ng/pattern/flex/flex_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/flex/flex_layout_algorithm.cpp
@@ -990,29 +990,11 @@ void FlexLayoutAlgorithm::Measure(LayoutWrapper* layoutWrapper)
     SetFinalRealSize(layoutWrapper, realSize);
 
     layoutWrapper->GetGeometryNode()->SetFrameSize(realSize);
-    UpdateMeasureResultToPattern(layoutWrapper);
+    ApplyPatternOperation(layoutWrapper, FlexOperatorType::UPDATE_MEASURE_RESULT, reinterpret_cast<uintptr_t>(this));
 }
 
-void FlexLayoutAlgorithm::UpdateMeasureResultToPattern(LayoutWrapper* layoutWrapper)
-{
-    CHECK_NULL_VOID(layoutWrapper);
-    FlexMeasureResult measureResult { .allocatedSize = allocatedSize_, .validSizeCount = validSizeCount_ };
-    auto host = layoutWrapper->GetHostNode();
-    CHECK_NULL_VOID(host);
-    auto pattern = host->GetPattern();
-    CHECK_NULL_VOID(pattern);
-    if (AceType::InstanceOf<LinearLayoutPattern>(pattern)) {
-        auto linearPattern = DynamicCast<LinearLayoutPattern>(pattern);
-        CHECK_NULL_VOID(linearPattern);
-        linearPattern->SetFlexMeasureResult(measureResult);
-    } else {
-        auto flexPattern = DynamicCast<FlexLayoutPattern>(pattern);
-        CHECK_NULL_VOID(flexPattern);
-        flexPattern->SetFlexMeasureResult(measureResult);
-    }
-}
-
-void FlexLayoutAlgorithm::RestoreMeasureResultFromPattern(LayoutWrapper* layoutWrapper)
+void FlexLayoutAlgorithm::ApplyPatternOperation(
+    LayoutWrapper* layoutWrapper, FlexOperatorType operation, uintptr_t addr, FlexLayoutResult layoutResult)
 {
     CHECK_NULL_VOID(layoutWrapper);
     auto host = layoutWrapper->GetHostNode();
@@ -1023,14 +1005,17 @@ void FlexLayoutAlgorithm::RestoreMeasureResultFromPattern(LayoutWrapper* layoutW
     if (AceType::InstanceOf<LinearLayoutPattern>(pattern)) {
         auto linearPattern = DynamicCast<LinearLayoutPattern>(pattern);
         CHECK_NULL_VOID(linearPattern);
-        measureResult = linearPattern->GetFlexMeasureResult();
+        PatternOperator(linearPattern, operation, measureResult, layoutResult, addr);
     } else {
         auto flexPattern = DynamicCast<FlexLayoutPattern>(pattern);
         CHECK_NULL_VOID(flexPattern);
-        measureResult = flexPattern->GetFlexMeasureResult();
+        PatternOperator(flexPattern, operation, measureResult, layoutResult, addr);
     }
-    allocatedSize_ = measureResult.allocatedSize;
-    validSizeCount_ = measureResult.validSizeCount;
+    
+    if (operation == FlexOperatorType::RESTORE_MEASURE_RESULT) {
+        allocatedSize_ = measureResult.allocatedSize;
+        validSizeCount_ = measureResult.validSizeCount;
+    }
 }
 
 void FlexLayoutAlgorithm::AdjustTotalAllocatedSize(LayoutWrapper* layoutWrapper)
@@ -1072,7 +1057,7 @@ void FlexLayoutAlgorithm::Layout(LayoutWrapper* layoutWrapper)
         return;
     }
     if (!hasMeasured_) {
-        RestoreMeasureResultFromPattern(layoutWrapper);
+        ApplyPatternOperation(layoutWrapper, FlexOperatorType::RESTORE_MEASURE_RESULT);
     }
     auto layoutProperty = AceType::DynamicCast<FlexLayoutProperty>(layoutWrapper->GetLayoutProperty());
     CHECK_NULL_VOID(layoutProperty);
@@ -1108,6 +1093,8 @@ void FlexLayoutAlgorithm::Layout(LayoutWrapper* layoutWrapper)
             child->Layout();
         }
     }
+    ApplyPatternOperation(layoutWrapper, FlexOperatorType::UPDATE_LAYOUT_RESULT, reinterpret_cast<uintptr_t>(this),
+        { .frontSpace = frontSpace, .betweenSpace = betweenSpace });
 }
 
 void FlexLayoutAlgorithm::CalculateSpace(float remainSpace, float& frontSpace, float& betweenSpace) const
