5e0a3575a66fdeb031fef985aa9ad578f190df16
Tianer Zhou <zhoutianer@huawei.com>
2024-11-15 11:47:26 +0800

    fix test
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: Ibf4807f17e799d7a526c19604354ada4872af647

--- test/unittest/core/pattern/waterflow/water_flow_segment_layout_test.cpp
+++ test/unittest/core/pattern/waterflow/water_flow_segment_layout_test.cpp
@@ -181,7 +181,7 @@ HWTEST_F(WaterFlowSegmentTest, MeasureOnOffset001, TestSize.Level1)
     EXPECT_EQ(info->startIndex_, 0);
     EXPECT_EQ(info->endIndex_, 10);
 
-    algo->overScroll_ = true;
+    algo->SetCanOverScroll(true);
     info->currentOffset_ = -200.0f;
     algo->Measure(AceType::RawPtr(frameNode_));
     EXPECT_EQ(info->currentOffset_, -200.0f);
@@ -331,7 +331,7 @@ HWTEST_F(WaterFlowSegmentTest, Layout002, TestSize.Level1)
     info->footerIndex_ = 0;
 
     info->currentOffset_ = -100.0f;
-    algo->overScroll_ = true;
+    algo->SetCanOverScroll(true);
     algo->Measure(AceType::RawPtr(frameNode_));
     algo->Layout(AceType::RawPtr(frameNode_));
     EXPECT_EQ(info->startIndex_, 1);
