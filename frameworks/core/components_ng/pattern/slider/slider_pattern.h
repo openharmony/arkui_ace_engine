5d30af69a856d21730f5c5842155ca757e06d10a
Zhang Jinyu <zhangjinyu101@huawei.com>
2024-11-19 16:55:29 +0800

    Add slider accessibility enabled register
    Signed-off-by: Zhang Jinyu <zhangjinyu101@huawei.com>

--- frameworks/core/components_ng/pattern/slider/slider_pattern.h
+++ frameworks/core/components_ng/pattern/slider/slider_pattern.h
@@ -153,6 +153,7 @@ public:
     }
 
     void SetSliderValue(double value, int32_t mode);
+    void InitAccessibilityVirtualNodeTask();
 
 private:
     void OnAttachToFrameNode() override;
@@ -232,8 +233,8 @@ private:
     RefPtr<FrameNode> BuildContentModifierNode();
     float GetValueInValidRange(const RefPtr<SliderPaintProperty>& paintProperty, float value, float min, float max);
     void UpdateToValidValue();
+    void InitSliderAccessibilityEnabledRegister();
     void AccessibilityVirtualNodeRenderTask();
-    void InitAccessibilityVirtualNodeTask();
     void InitAccessibilityHoverEvent();
     void HandleAccessibilityHoverEvent(bool state, const AccessibilityHoverInfo& info);
     bool InitAccessibilityVirtualNode();
@@ -321,6 +322,7 @@ private:
     std::function<void(bool)> isFocusActiveUpdateEvent_;
     bool isFocusActive_ = false;
 
+    std::shared_ptr<AccessibilitySAObserverCallback> accessibilitySAObserverCallback_;
     RefPtr<FrameNode> parentAccessibilityNode_;
     std::vector<RefPtr<FrameNode>> pointAccessibilityNodeVec_;
     std::vector<GestureEventFunc> pointAccessibilityNodeEventVec_;
