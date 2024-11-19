84fd216ffd14161de7ddf80d328d47c677fda3aa
voronov.vadim <voronov.vadim@huawei-partners.com>
2024-11-19 23:04:33 +0300

    s_developer_datepicker_part2 init
    Signed-off-by: voronov.vadim <voronov.vadim@huawei-partners.com>
    Change-Id: I3350efc01b96c1a81701d311cf69de9f2f1bda7e

--- frameworks/core/interfaces/arkoala/implementation/date_picker_modifier.cpp
+++ frameworks/core/interfaces/arkoala/implementation/date_picker_modifier.cpp
@@ -20,7 +20,9 @@
 #include "core/components_ng/base/frame_node.h"
 #include "core/interfaces/arkoala/utility/reverse_converter.h"
 #include "arkoala_api_generated.h"
+#include "frameworks/base/utils/time_util.h"
 
+namespace OHOS::Ace::NG {
 namespace {
 const int32_t YEAR_MIN = 1900;
 const int32_t YEAR_MAX = 2100;
@@ -31,8 +33,14 @@ const int32_t DAY_MAX = 31;
 const std::string YEAR = "year";
 const std::string MONTH = "month";
 const std::string DAY = "day";
+const int64_t SEC_TO_MILLISEC = 1000;
+struct DatePickerOptions {
+    std::optional<PickerDate> start;
+    std::optional<PickerDate> end;
+    std::optional<PickerDate> selected;
+}
 
-bool g_checkValidDateValues(std::unique_ptr<OHOS::Ace::JsonValue>& sourceJson)
+bool CheckValidDateValues(std::unique_ptr<OHOS::Ace::JsonValue>& sourceJson)
 {
     if (!sourceJson || sourceJson->IsNull()) {
         return false;
@@ -53,7 +61,44 @@ bool g_checkValidDateValues(std::unique_ptr<OHOS::Ace::JsonValue>& sourceJson)
     return true;
 }
 } // namespace
+namespace Converter {
 
+void AssignArkValue(Ark_Date& dst, const PickerDate& src)
+{
+
+    std::tm tm{};
+    tm.tm_year = src.GetYear();
+    tm.tm_mon = src.GetMonth()-1;
+    tm.tm_mday = src.GetDay();
+    std::time_t time = std::mktime(&tm);
+    int64_t timemillis = staitc_cast<int64_t>(time*SEC_TO_MILLISEC);
+    dst = reinterpret_cast<Ark_Date>(timemillis);
+}
+
+template<>
+void AssignCast(std::optional<PickerDate>& dst, const Ark_Date& src)
+{
+    auto timestamp = reinterpret_cast<int64_t>(src);
+    auto t = static_cast<std::time_t>(timestamp / SEC_TO_MILLISEC);
+    auto local = std::localtime(&t);
+    if (!local) {
+        return;
+    }
+    dst = new PickerDate(local->tm_year, local->tm_mon + 1, local->tm_mday);
+}
+
+template<>
+DatePickerOptions Convert(const Ark_DatePickerOptions& src)
+{
+    DatePickerOptions dst;
+    dst->start = Converter::OptConvert<PickerDate>(src.start);
+    dst->end = Converter::OptConvert<PickerDate>(src.end);
+    dst->selected = Converter::OptConvert<PickerDate>(src.selected);
+}
+
+
+} // namespace  OHOS::Ace:NG:Converter
+} // namespace  OHOS::Ace:NG
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace DatePickerInterfaceModifier {
 void SetDatePickerOptionsImpl(Ark_NativePointer node,
@@ -61,8 +106,19 @@ void SetDatePickerOptionsImpl(Ark_NativePointer node,
 {
     auto frameNode = reinterpret_cast<FrameNode *>(node);
     CHECK_NULL_VOID(frameNode);
-    LOGE("ARKOALA DatePickerInterface::SetDatePickerOptionsImpl -> Method is not "
-        "implemented.");
+    CHECK_NULL_VOID(options);
+
+    auto datePickerOptions = Converter::Convert<DatePickerOptions>(*options);
+
+    if(datePickerOptions.start){
+        DatePickerModelNG::SetStartDate(*datePickerOptions.start);    
+    }    
+    if(datePickerOptions.end){
+        DatePickerModelNG::SetStartDate(*datePickerOptions.end);    
+    }    
+    if(datePickerOptions.selected){
+        DatePickerModelNG::SetStartDate(*datePickerOptions.selected);    
+    }    
 }
 } // DatePickerInterfaceModifier
 namespace DatePickerAttributeModifier {
@@ -116,7 +172,7 @@ void OnChangeImpl(Ark_NativePointer node,
     CHECK_NULL_VOID(frameNode);
     CHECK_NULL_VOID(value);
 
-    auto onChange = [frameNode](const BaseEventInfo* event) {
+    auto onChange = [arkCallback = CallbackHelper(*value)](const BaseEventInfo* event) {
         CHECK_NULL_VOID(event);
         const auto* eventInfo = TypeInfoHelper::DynamicCast<DatePickerChangeEvent>(event);
         CHECK_NULL_VOID(eventInfo);
@@ -126,7 +182,7 @@ void OnChangeImpl(Ark_NativePointer node,
         auto year = YEAR_MIN;
         auto month = MONTH_MIN;
         auto day = DAY_MIN;
-        if (g_checkValidDateValues(sourceJson)) {
+        if (CheckValidDateValues(sourceJson)) {
             year = sourceJson->GetValue(::YEAR)->GetInt();
             month = sourceJson->GetValue(::MONTH)->GetInt();
             day = sourceJson->GetValue(::DAY)->GetInt();
@@ -136,19 +192,39 @@ void OnChangeImpl(Ark_NativePointer node,
             .month = Converter::ArkValue<Opt_Number>(month),
             .day = Converter::ArkValue<Opt_Number>(day),
         };
-        GetFullAPI()->getEventsAPI()->getDatePickerEventsReceiver()->onChange(
-            frameNode->GetId(), result);
+         arkCallback.Invoke(result);
     };
     DatePickerModelNG::SetOnChange(frameNode, std::move(onChange));
 }
 void OnDateChangeImpl(Ark_NativePointer node,
                       const Callback_Date_Void* value)
 {
+
     auto frameNode = reinterpret_cast<FrameNode *>(node);
     CHECK_NULL_VOID(frameNode);
     CHECK_NULL_VOID(value);
-    LOGE("ARKOALA DatePickerInterface::SetDatePickerOptionsImp ->"
-        "Ark_CustomObject is not implemented yet");
+
+    auto onChange = [arkCallback = CallbackHelper(*value)](const BaseEventInfo* event) {
+        CHECK_NULL_VOID(event);
+        const auto* eventInfo = TypeInfoHelper::DynamicCast<DatePickerChangeEvent>(event);
+        CHECK_NULL_VOID(eventInfo);
+        auto selectedStr = eventInfo->GetSelectedStr();
+        auto sourceJson = JsonUtil::ParseJsonString(selectedStr);
+
+        auto year = YEAR_MIN;
+        auto month = MONTH_MIN;
+        auto day = DAY_MIN;
+        if (CheckValidDateValues(sourceJson)) {
+            year = sourceJson->GetValue(::YEAR)->GetInt();
+            month = sourceJson->GetValue(::MONTH)->GetInt();
+            day = sourceJson->GetValue(::DAY)->GetInt();
+        }
+        auto pickerDate = new PickerDate(year, month, day);
+        auto result = Converter::ArkValue<Ark_Date>(pickerDate);
+        arkCallback.Invoke(result);
+    };
+
+    DatePickerModelNG::SetOnDateChange(frameNode, std::move(onChange))
 }
 } // DatePickerAttributeModifier
 const GENERATED_ArkUIDatePickerModifier* GetDatePickerModifier()
