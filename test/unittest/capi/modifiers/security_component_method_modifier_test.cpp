2aa54d4aeb1f61885c2a00ad33c3109cffb46973
Sergey Samarin <samarin.sergey@huawei.com>
2024-11-20 13:51:42 +0300

    Implement SecurityCommonMethod modifier - part 5
    Signed-off-by: Sergey Samarin <samarin.sergey@huawei.com>
    Change-Id: I88392088a98b14209b2d7d0546f5df38c7f7c9bc

--- test/unittest/capi/modifiers/security_component_method_modifier_test.cpp
+++ test/unittest/capi/modifiers/security_component_method_modifier_test.cpp
@@ -13,215 +13,61 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
-
-#include "generated/test_fixtures.h"
-#include "modifier_test_base.h"
-#include "modifiers_test_utils.h"
-#include "core/components_ng/pattern/security_component/save_button/save_button_model_ng.h"
-#include "core/components_ng/pattern/security_component/security_component_model_ng.h"
-#include "core/components_ng/pattern/security_component/security_component_pattern.h"
-#include "core/components_ng/pattern/security_component/security_component_theme.h"
-#include "core/interfaces/arkoala/utility/reverse_converter.h"
-#include "arkoala_api_generated.h"
+#include "security_component_method_modifier_test.h"
 
 using namespace testing;
 using namespace testing::ext;
 
 namespace OHOS::Ace::NG {
-using namespace Converter;
+using namespace TestConst::SecurityComponentMethod;
 
-namespace  {
-    const auto ATTRIBUTE_POSITION_NAME = "position";
-    const auto ATTRIBUTE_MARK_ANCHOR_NAME = "markAnchor";
-    const auto ATTRIBUTE_SIZE_NAME = "size";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_NAME = "constraintSize";
-    const auto ATTRIBUTE_ICON_SIZE_NAME = "iconSize";
-    const auto ATTRIBUTE_ICON_SIZE_DEFAULT_VALUE = "16.00vp";
-    const auto ATTRIBUTE_LAYOUT_DIRECTION_NAME = "layoutDirection";
-    const auto ATTRIBUTE_LAYOUT_DIRECTION_DEFAULT_VALUE = SecurityComponentLayoutDirection::HORIZONTAL;
-    const auto ATTRIBUTE_POSITION_X_NAME = "x";
-    const auto ATTRIBUTE_POSITION_X_DEFAULT_VALUE = "0.00px";
-    const auto ATTRIBUTE_POSITION_Y_NAME = "y";
-    const auto ATTRIBUTE_POSITION_Y_DEFAULT_VALUE = "0.00px";
-    const auto ATTRIBUTE_FONT_SIZE_NAME = "fontSize";
-    const auto ATTRIBUTE_FONT_SIZE_DEFAULT_VALUE = "16.00fp";
-    const auto ATTRIBUTE_FONT_STYLE_NAME = "fontStyle";
-    const auto ATTRIBUTE_FONT_STYLE_DEFAULT_VALUE = Ace::FontStyle::NORMAL;
-    const auto ATTRIBUTE_FONT_WEIGHT_NAME = "fontWeight";
-    const auto ATTRIBUTE_FONT_WEIGHT_DEFAULT_VALUE = "FontWeight.Medium";
-    const auto ATTRIBUTE_FONT_COLOR_NAME = "fontColor";
-    const auto ATTRIBUTE_FONT_COLOR_DEFAULT_VALUE = "#FFFFFFA0";
-    const auto ATTRIBUTE_ICON_COLOR_NAME = "iconColor";
-    const auto ATTRIBUTE_ICON_COLOR_DEFAULT_VALUE = "#FFFFFFA1";
-    const auto ATTRIBUTE_BACKGROUND_COLOR_NAME = "backgroundColor";
-    const auto ATTRIBUTE_BACKGROUND_COLOR_DEFAULT_VALUE = "#FFFFFFA2";
-    const auto ATTRIBUTE_BORDER_STYLE_NAME = "borderStyle";
-    const auto ATTRIBUTE_BORDER_STYLE_DEFAULT_VALUE = BorderStyle::NONE;
-    const auto ATTRIBUTE_BORDER_WIDTH_NAME = "borderWidth";
-    const auto ATTRIBUTE_BORDER_WIDTH_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_BORDER_COLOR_NAME = "borderColor";
-    const auto ATTRIBUTE_BORDER_COLOR_DEFAULT_VALUE = "#FFFFFFA3";
-    const auto ATTRIBUTE_BORDER_RADIUS_NAME = "borderRadius";
-    const auto ATTRIBUTE_BORDER_RADIUS_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_TEXT_ICON_SPACE_NAME = "textIconSpace";
-    const auto ATTRIBUTE_TEXT_ICON_SPACE_DEFAULT_VALUE = "4vp";
-    const auto ATTRIBUTE_KEY_NAME = "id";
-    const auto ATTRIBUTE_KEY_DEFAULT_VALUE = "";
-    const auto ATTRIBUTE_WIDTH_NAME = "width";
-    const auto ATTRIBUTE_WIDTH_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_HEIGHT_NAME = "height";
-    const auto ATTRIBUTE_HEIGHT_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MIN_WIDTH_NAME = "minWidth";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MIN_WIDTH_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MAX_WIDTH_NAME = "maxWidth";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MAX_WIDTH_DEFAULT_VALUE = "Infinityvp";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MIN_HEIGHT_NAME = "minHeight";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MIN_HEIGHT_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MAX_HEIGHT_NAME = "maxHeight";
-    const auto ATTRIBUTE_CONSTRAINT_SIZE_MAX_HEIGHT_DEFAULT_VALUE = "Infinityvp";
+// Invalid values for attribute 'x' of position
+std::vector<std::tuple<std::string, Opt_Length>> positionXInvalidValues = {
+    {"Ark_Empty()", Converter::ArkValue<Opt_Length>(Ark_Empty())},
+};
 
-    const auto ATTRIBUTE_MARK_ANCHOR_X_NAME = "x";
-    const auto ATTRIBUTE_MARK_ANCHOR_X_DEFAULT_VALUE = "0.00px";
-    const auto ATTRIBUTE_MARK_ANCHOR_Y_NAME = "y";
-    const auto ATTRIBUTE_MARK_ANCHOR_Y_DEFAULT_VALUE = "0.00px";
-    const auto ATTRIBUTE_SIZE_HEIGHT_NAME = "height";
-    const auto ATTRIBUTE_SIZE_HEIGHT_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_SIZE_WIDTH_NAME = "width";
-    const auto ATTRIBUTE_SIZE_WIDTH_DEFAULT_VALUE = "0.00vp";
-    const auto ATTRIBUTE_PADDING_NAME = "padding";
-    const auto ATTRIBUTE_PADDING_LEFT_NAME = "left";
-    const auto ATTRIBUTE_PADDING_LEFT_DEFAULT_VALUE = "24.00vp";
-    const auto ATTRIBUTE_PADDING_RIGHT_NAME = "right";
-    const auto ATTRIBUTE_PADDING_RIGHT_DEFAULT_VALUE = "24.00vp";
-    const auto ATTRIBUTE_PADDING_TOP_NAME = "top";
-    const auto ATTRIBUTE_PADDING_TOP_DEFAULT_VALUE = "12.00vp";
-    const auto ATTRIBUTE_PADDING_BOTTOM_NAME = "bottom";
-    const auto ATTRIBUTE_PADDING_BOTTOM_DEFAULT_VALUE = "12.00vp";
-    const auto ATTRIBUTE_FONT_FAMILY_NAME = "fontFamily";
+// Invalid values for attribute 'y' of position
+std::vector<std::tuple<std::string, Opt_Length>> positionYInvalidValues = {
+    {"Ark_Empty()", Converter::ArkValue<Opt_Length>(Ark_Empty())},
+};
 
-    const auto THEME_BACKGROUND_COLOR = Color(0xFFFFFFA2);
-    const auto THEME_BORDER_COLOR = Color(0xFFFFFFA3);
-    const auto THEME_FONT_COLOR = Color(0xFFFFFFA0);
-    const auto THEME_FONT_SIZE = Dimension(16., DimensionUnit::FP);
-    const auto THEME_ICON_COLOR = Color(0xFFFFFFA1);
-    const auto THEME_ICON_SIZE = Dimension(16., DimensionUnit::VP);
-    const auto THEME_TEXT_ICON_SPACE = Dimension(4., DimensionUnit::VP);
-
-    const auto DEFAULT_JSON_INT = -1;
-
-    const uint32_t FLOAT_RES_0_ID = 100;
-    const float FLOAT_RES_0_VALUE = 70.5f;
-    const auto FLOAT_RES_0_STORED_VALUE = Dimension(FLOAT_RES_0_VALUE, DimensionUnit::PX);
-    const Ark_Resource FLOAT_RES_0 = CreateResource(FLOAT_RES_0_ID, NodeModifier::ResourceType::FLOAT);
-
-    const uint32_t FLOAT_RES_1_ID = 101;
-    const float FLOAT_RES_1_VALUE = 19.5f;
-    const auto FLOAT_RES_1_STORED_VALUE = Dimension(FLOAT_RES_1_VALUE, DimensionUnit::FP);
-    const Ark_Resource FLOAT_RES_1 = CreateResource(FLOAT_RES_1_ID, NodeModifier::ResourceType::FLOAT);
-
-    const uint32_t STRARRAY_RES_2_ID = 102;
-    const std::string STRARRAY_RES_2_VALUE = "ResourceFontFamily";
-    const Ark_Resource STRARRAY_RES_2 = CreateResource(STRARRAY_RES_2_ID, NodeModifier::ResourceType::STRARRAY);
-
-    // Invalid values for attribute 'x' of position
-    std::vector<std::tuple<std::string, Opt_Length>> positionXInvalidValues = {
-        {"Ark_Empty()", Converter::ArkValue<Opt_Length>(Ark_Empty())},
-    };
-
-    // Invalid values for attribute 'y' of position
-    std::vector<std::tuple<std::string, Opt_Length>> positionYInvalidValues = {
-        {"Ark_Empty()", Converter::ArkValue<Opt_Length>(Ark_Empty())},
-    };
-
-    // Valid values for attribute 'x' of position
-    std::vector<std::tuple<std::string, Opt_Length, std::string>> positionXValidValues = {
-        { "23", ArkValue<Opt_Length>(23), "23.00px" },
-        { "60.25f", ArkValue<Opt_Length>(60.25f), "60.25vp" },
-        { "-40.5f", ArkValue<Opt_Length>(-40.5f), "-40.50vp" },
-        {
-            "ResId:FLOAT_RES_0_ID",
-            { .tag = ARK_TAG_RESOURCE, .value = { .type = ARK_TAG_RESOURCE, .resource = FLOAT_RES_0_ID }},
-            "70.50px"
-        }
-    };
-
-    // Valid values for attribute 'y' of position
-    std::vector<std::tuple<std::string, Opt_Length, std::string>> positionYValidValues = {
-        { "23", ArkValue<Opt_Length>(23), "23.00px" },
-        { "60.25f", ArkValue<Opt_Length>(60.25f), "60.25vp" },
-        { "-40.5f", ArkValue<Opt_Length>(-40.5f), "-40.50vp" },
-        {
-            "ResId:FLOAT_RES_0_ID",
-            { .tag = ARK_TAG_RESOURCE, .value = { .type = ARK_TAG_RESOURCE, .resource = FLOAT_RES_0_ID }},
-            "70.50px"
-        }
-    };
-
-    // Valid values for attribute 'width'
-    static std::vector<std::tuple<std::string, Ark_Length, std::string>> widthValidValues = {
-        { "23", ArkValue<Ark_Length>(23), "23.00px" },
-        { "60.25f", ArkValue<Ark_Length>(60.25f), "60.25vp" },
-        {
-            "ResId:FLOAT_RES_0_ID",
-            { .type = ARK_TAG_RESOURCE, .resource = FLOAT_RES_0_ID },
-            "70.50px"
-        }
-    };
-
-    static std::vector<std::tuple<std::string, Ark_Length>> widthInvalidValues = {
-        {"-10", Converter::ArkValue<Ark_Length>(-10)},
-    };
-} // namespace
-
-class SecurityComponentMethodModifierTest : public ModifierTestBase<GENERATED_ArkUISecurityComponentMethodModifier,
-    &GENERATED_ArkUINodeModifiers::getSecurityComponentMethodModifier, GENERATED_ARKUI_SAVE_BUTTON> {
-public:
-    static void SetUpTestCase()
+// Valid values for attribute 'x' of position
+std::vector<std::tuple<std::string, Opt_Length, std::string>> positionXValidValues = {
+    { "23", ArkValue<Opt_Length>(23), "23.00px" },
+    { "60.25f", ArkValue<Opt_Length>(60.25f), "60.25vp" },
+    { "-40.5f", ArkValue<Opt_Length>(-40.5f), "-40.50vp" },
     {
-        ModifierTestBase::SetUpTestCase();
-
-        // set test values to Theme Pattern as data for the Theme building
-        auto themeStyle = SetupThemeStyle(THEME_PATTERN_SECURITY_COMPONENT);
-        themeStyle->SetAttr("background_color", { .value = THEME_BACKGROUND_COLOR });
-        themeStyle->SetAttr("border_color", { .value = THEME_BORDER_COLOR });
-        themeStyle->SetAttr("icon_color", { .value = THEME_ICON_COLOR });
-        themeStyle->SetAttr("icon_size", { .value = THEME_ICON_SIZE });
-        themeStyle->SetAttr("font_color", { .value = THEME_FONT_COLOR });
-        themeStyle->SetAttr("font_size", { .value = THEME_FONT_SIZE });
-        themeStyle->SetAttr("text_icon_pading", { .value = THEME_TEXT_ICON_SPACE });
-
-        SetupTheme<SecurityComponentTheme>();
-
-        AddResource(FLOAT_RES_0_ID, FLOAT_RES_0_STORED_VALUE);
-        AddResource(FLOAT_RES_1_ID, FLOAT_RES_1_STORED_VALUE);
-        AddResource(STRARRAY_RES_2_ID, STRARRAY_RES_2_VALUE);
-
-        for (const auto &[resId, resName, value] : Fixtures::resourceInitTable) {
-            AddResource(resId, value);
-            AddResource(resName, value);
-        }
+        "ResId:FLOAT_RES_0_ID",
+        { .tag = ARK_TAG_RESOURCE, .value = { .type = ARK_TAG_RESOURCE, .resource = FLOAT_RES_0_ID }},
+        "70.50px"
     }
+};
 
-    void SetUp(void) override
+// Valid values for attribute 'y' of position
+std::vector<std::tuple<std::string, Opt_Length, std::string>> positionYValidValues = {
+    { "23", ArkValue<Opt_Length>(23), "23.00px" },
+    { "60.25f", ArkValue<Opt_Length>(60.25f), "60.25vp" },
+    { "-40.5f", ArkValue<Opt_Length>(-40.5f), "-40.50vp" },
     {
-        ModifierTestBase::SetUp();
-
-        auto frameNode = reinterpret_cast<FrameNode*>(node_);
-        ASSERT_NE(frameNode, nullptr);
-        SaveButtonModelNG::InitSaveButton(frameNode, SaveButtonStyle(), false);
-
-        OnModifyDone();
+        "ResId:FLOAT_RES_0_ID",
+        { .tag = ARK_TAG_RESOURCE, .value = { .type = ARK_TAG_RESOURCE, .resource = FLOAT_RES_0_ID }},
+        "70.50px"
     }
+};
 
-    void OnModifyDone()
+// Valid values for attribute 'width'
+static std::vector<std::tuple<std::string, Ark_Length, std::string>> widthValidValues = {
+    { "23", ArkValue<Ark_Length>(23), "23.00px" },
+    { "60.25f", ArkValue<Ark_Length>(60.25f), "60.25vp" },
     {
-        auto frameNode = reinterpret_cast<FrameNode*>(node_);
-        ASSERT_NE(frameNode, nullptr);
-        auto pattern = frameNode->GetPattern();
-        ASSERT_TRUE(pattern);
-        pattern->OnModifyDone();
+        "ResId:FLOAT_RES_0_ID",
+        { .type = ARK_TAG_RESOURCE, .resource = FLOAT_RES_0_ID },
+        "70.50px"
     }
+};
+
+static std::vector<std::tuple<std::string, Ark_Length>> widthInvalidValues = {
+    {"-10", Converter::ArkValue<Ark_Length>(-10)},
 };
 
 /*
@@ -2307,36 +2153,6 @@ HWTEST_F(SecurityComponentMethodModifierTest, setConstraintSizeTestInvalidHeight
 }
 
 /*
- * @tc.name: setOffsetTestDefaultValues
- * @tc.desc:
- * @tc.type: FUNC
- */
-HWTEST_F(SecurityComponentMethodModifierTest, DISABLED_setOffsetTestDefaultValues, TestSize.Level1)
-{
-    ASSERT_TRUE(false);
-}
-
-/*
- * @tc.name: setOffsetTestValidValues
- * @tc.desc:
- * @tc.type: FUNC
- */
-HWTEST_F(SecurityComponentMethodModifierTest, DISABLED_setOffsetTestValidValues, TestSize.Level1)
-{
-    ASSERT_TRUE(false);
-}
-
-/*
- * @tc.name: setOffsetTestInvalidValues
- * @tc.desc:
- * @tc.type: FUNC
- */
-HWTEST_F(SecurityComponentMethodModifierTest, DISABLED_setOffsetTestInvalidValues, TestSize.Level1)
-{
-    ASSERT_TRUE(false);
-}
-
-/*
  * @tc.name: setPaddingTestDefaultValues
  * @tc.desc:
  * @tc.type: FUNC
@@ -2696,16 +2512,6 @@ HWTEST_F(SecurityComponentMethodModifierTest, DISABLED_setFontFamilyTestDefaultV
     ASSERT_TRUE(false);
 }
 
-std::vector<std::tuple<std::string, Ark_String, std::string>> fontFamilyStrValidValues = {
-    { "\"testFamily\"", ArkValue<Ark_String>("testFamily"), "testFamily" },
-    { "\"testFamily1,testFamily2\"", ArkValue<Ark_String>("testFamily1,testFamily2"), "testFamily1,testFamily2" },
-    { "\"testFamily1, testFamily2\"", ArkValue<Ark_String>("testFamily1, testFamily2"), "testFamily1, testFamily2" },
-};
-
-std::vector<std::tuple<std::string, Ark_Resource, std::string>> fontFamilyResValidValues = {
-    { "ResId:STRARRAY_RES_2_ID", ArkValue<Ark_Resource>(STRARRAY_RES_2), STRARRAY_RES_2_VALUE },
-};
-
 /*
  * @tc.name: setFontFamilyTestValidValues
  * @tc.desc:
@@ -2723,11 +2529,11 @@ HWTEST_F(SecurityComponentMethodModifierTest, DISABLED_setFontFamilyTestValidVal
         EXPECT_EQ(resultStr, expectedStr) << "Passed value is: " << input;
     };
 
-    for (const auto &[input, value, expectedStr]: fontFamilyStrValidValues) {
+    for (const auto &[input, value, expectedStr]: Fixtures::testFixtureFontFamilyStringValidValues) {
         checkValue(input, Converter::ArkUnion<Ark_Union_String_Resource, Ark_String>(value), expectedStr);
     }
 
-    for (const auto &[input, value, expectedStr]: fontFamilyResValidValues) {
+    for (const auto &[input, value, expectedStr]: Fixtures::testFixtureFontFamilyResourceValidValues) {
         checkValue(input, Converter::ArkUnion<Ark_Union_String_Resource, Ark_Resource>(value), expectedStr);
     }
 }
@@ -2741,5 +2547,4 @@ HWTEST_F(SecurityComponentMethodModifierTest, DISABLED_setFontFamilyTestInvalidV
 {
     ASSERT_TRUE(false);
 }
-
 } // namespace OHOS::Ace::NG
