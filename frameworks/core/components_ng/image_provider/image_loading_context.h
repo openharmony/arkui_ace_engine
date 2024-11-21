c59c2c4d0ca9bf036f7ac8b152aa9fd12abfbf5b
pavelyevivan <pavelyev.ivan@huawei-partners.com>
2024-11-21 15:49:37 +0300

    function position has been returned
    Signed-off-by: pavelyevivan <pavelyev.ivan@huawei-partners.com>
    Change-Id: I523b6bf9a73558b533bb0f0a7fe7bdfb202c645d

--- frameworks/core/components_ng/image_provider/image_loading_context.h
+++ frameworks/core/components_ng/image_provider/image_loading_context.h
@@ -172,8 +172,8 @@ private:
     DEFINE_SET_NOTIFY_TASK(LoadFail);
 
     // tasks that run when entering a new state
-    void OnDataLoading();
     void OnUnloaded();
+    void OnDataLoading();
     void OnMakeCanvasImage();
     void OnLoadSuccess();
     void OnLoadFail();
