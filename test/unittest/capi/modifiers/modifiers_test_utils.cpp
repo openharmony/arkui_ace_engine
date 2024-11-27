9286e191206d36fde33d736b2a5121309c370ffb
Nikolay Pisanov <Pisanov.Nikolay@huawei.com>
2024-11-27 21:52:22 +0300

    Initial C API commit.
    Signed-off-by: Nikolay Pisanov <Pisanov.Nikolay@huawei.com>
    Change-Id: If164b94a6f556ca51902e4f9e113439826bff948

---
+++ test/unittest/capi/modifiers/modifiers_test_utils.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fstream>
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+#include "modifiers_test_utils.h"
+
+namespace OHOS::Ace::NG {
+
+std::string GetStringAttribute(Ark_NodeHandle node, const std::string &name)
+{
+    static const InspectorFilter inspector;
+    if (auto fnode = reinterpret_cast<FrameNode *>(node); fnode) {
+        if (auto jsonVal = JsonUtil::Create(true); jsonVal) {
+            fnode->ToJsonValue(jsonVal, inspector);
+            auto val = jsonVal->GetValue(name);
+            return (val &&
+                (val->IsObject() || val->IsArray() || val->IsNumber() || val->IsBool()))
+                    ? val->ToString() : jsonVal->GetString(name);
+        }
+    }
+    return {};
+}
+
+std::unique_ptr<JsonValue> GetJsonValue(Ark_NodeHandle node)
+{
+    static const InspectorFilter inspector;
+
+    if (auto fnode = reinterpret_cast<FrameNode *>(node); fnode) {
+        if (auto jsonVal = JsonUtil::Create(true); jsonVal) {
+            fnode->ToJsonValue(jsonVal, inspector);
+            return jsonVal;
+        }
+    }
+    return nullptr;
+}
+
+std::unique_ptr<JsonValue> GetLayoutJsonValue(Ark_NodeHandle node)
+{
+    auto frameNode = reinterpret_cast<FrameNode*>(node);
+    CHECK_NULL_RETURN(frameNode, nullptr);
+    auto layout = frameNode->GetLayoutProperty();
+    CHECK_NULL_RETURN(layout, nullptr);
+
+    static const InspectorFilter inspector;
+
+    if (auto jsonVal = JsonUtil::Create(true); jsonVal) {
+        layout->ToJsonValue(jsonVal, inspector);
+        return jsonVal;
+    }
+    return nullptr;
+}
+
+std::unique_ptr<JsonValue> GetPatternJsonValue(Ark_NodeHandle node)
+{
+    auto frameNode = reinterpret_cast<FrameNode*>(node);
+    CHECK_NULL_RETURN(frameNode, nullptr);
+    auto pattern = frameNode->GetPattern();
+    CHECK_NULL_RETURN(pattern, nullptr);
+
+    static const InspectorFilter inspector;
+
+    if (auto jsonVal = JsonUtil::Create(true); jsonVal) {
+        pattern->ToJsonValue(jsonVal, inspector);
+        return jsonVal;
+    }
+    return nullptr;
+}
+
+template<>
+std::string GetAttrValue(const std::unique_ptr<JsonValue> &jsonVal, const std::string &attrKey)
+{
+    if (jsonVal) {
+        auto result = jsonVal->GetValue(attrKey);
+        if (result->IsObject() || result->IsArray() || result->IsBool() || result->IsNumber()) {
+            return result->ToString();
+        }
+        return jsonVal->GetString(attrKey);
+    }
+    return std::string();
+}
+
+Ark_Resource CreateResource(uint32_t id, OHOS::Ace::NG::NodeModifier::ResourceType type)
+{
+    return {
+        .id = Converter::ArkValue<Ark_Number>(id),
+        .type = Converter::ArkValue<Opt_Number>(static_cast<uint32_t>(type)),
+        .moduleName = Converter::ArkValue<Ark_String>(""),
+        .bundleName = Converter::ArkValue<Ark_String>(""),
+        .params = Converter::ArkValue<Opt_Array_String>(),
+    };
+}
+
+Ark_Resource CreateResource(const char *name, OHOS::Ace::NG::NodeModifier::ResourceType type)
+{
+    static std::vector<std::unique_ptr<Ark_String>> s_strCache;
+    s_strCache.emplace_back(std::make_unique<Ark_String>(Converter::ArkValue<Ark_String>(name)));
+    Array_String params = {.length = 1, .array = s_strCache.back().get()};
+    return {
+        .id = Converter::ArkValue<Ark_Number>(-1),
+        .type = Converter::ArkValue<Opt_Number>(static_cast<uint32_t>(type)),
+        .moduleName = Converter::ArkValue<Ark_String>(""),
+        .bundleName = Converter::ArkValue<Ark_String>(""),
+        .params = Converter::ArkValue<Opt_Array_String>(params),
+    };
+}
+
+void DumpJsonToFile(Ark_NodeHandle node, int index)
+{
+    if (!::testing::Test::HasFailure()) {
+        return;
+    }
+    std::stringstream fname;
+    fname << ::testing::UnitTest::GetInstance()->current_test_info()->name();
+    if (index >= 0) {
+        fname << index;
+    }
+    fname << ".json";
+    std::fstream file(fname.str(), file.out);
+    if (file.is_open()) {
+        auto jsonVal = GetJsonValue(node);
+        file << jsonVal->ToString();
+    }
+}
+} // namespace OHOS::Ace::NG
---
+++ test/unittest/capi/utils/converter_test.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <algorithm>
+
+#include "gtest/gtest.h"
+
+#ifdef ARKUI_GENERATED
+#include "core/interfaces/native/utility/converter.h"
+#include "core/interfaces/native/utility/reverse_converter.h"
+#endif
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Ace::NG {
+
+class ConvertorTest : public testing::Test {
+};
+
+HWTEST_F(ConvertorTest, DummyTest, TestSize.Level1)
+{
+    EXPECT_TRUE(true);
+}
+
+#ifdef ARKUI_GENERATED
+/**
+ * @tc.name:
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(ConvertorTest, ArrayConversion, TestSize.Level1)
+{
+    std::vector<std::string> testArray = { "123", "abc", "%^&" };
+    Converter::ArkArrayHolder<Array_String> holder(testArray);
+
+    auto input = holder.ArkValue();
+    auto result = Converter::Convert<std::vector<std::string>>(input);
+    EXPECT_EQ(result, testArray);
+
+    auto optInput = holder.OptValue<Opt_Array_String>();
+    auto optResult = Converter::OptConvert<std::vector<std::string>>(optInput);
+    EXPECT_TRUE(optResult.has_value());
+    EXPECT_EQ(optResult.value(), testArray);
+}
+
+/**
+ * @tc.name:
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(ConvertorTest, OptArrayConversion, TestSize.Level1)
+{
+    std::vector<uint32_t> inputArray = { 0xFF000123, 0xFF000456, 0xFF000789 };
+    std::vector<std::optional<Color>> testArray;
+    std::transform(inputArray.begin(), inputArray.end(), std::back_inserter(testArray),
+        [](auto val) {return Color(val);});
+    Converter::ArkArrayHolder<Array_ResourceColor, Ark_Number> holder(inputArray);
+
+    auto input = holder.ArkValue();
+    auto result = Converter::Convert<std::vector<std::optional<Color>>>(input);
+    EXPECT_EQ(result, testArray);
+
+    auto optInput = holder.OptValue<Opt_Array_ResourceColor>();
+    auto optResult = Converter::OptConvert<std::vector<std::optional<Color>>>(optInput);
+    EXPECT_TRUE(optResult.has_value());
+    EXPECT_EQ(optResult.value(), testArray);
+}
+#endif
+} // namespace OHOS::Ace::NG
