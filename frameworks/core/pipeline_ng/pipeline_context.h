fa953a7c7c169bed3a0436979f777c04c0419cab
kangshihui <kangshihui@huawei.com>
2024-11-28 15:55:47 +0800

    键盘关闭时显示Textinput清除按钮，清除按钮会有残影 Signed-off-by:kangshihui<kangshihui@huawei.com>
    Change-Id: Idf777a85e0928a17cad1a122de0db00d432b05f6

--- frameworks/core/pipeline_ng/pipeline_context.h
+++ frameworks/core/pipeline_ng/pipeline_context.h
@@ -1027,6 +1027,7 @@ protected:
         const bool supportAvoidance = false, bool forceChange = false) override;
     void OnVirtualKeyboardHeightChange(float keyboardHeight, double positionY, double height,
         const std::shared_ptr<Rosen::RSTransaction>& rsTransaction = nullptr, bool forceChange = false) override;
+    void FlushDirtyPropertyNodesWhenExist();
 
     void SetIsLayouting(bool layouting)
     {
