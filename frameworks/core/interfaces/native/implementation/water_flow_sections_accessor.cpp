741453e2b20c0e90915da1f700be1194d0825e7d
kovalevsergey <kovalev.sergey5@huawei-partners.com>
2024-11-29 10:43:52 +0300

    modifier update
    Signed-off-by: kovalevsergey <kovalev.sergey5@huawei-partners.com>
    Change-Id: I0ecbf2d0c313be18f09ec94b73ded1e7a66b502c

--- frameworks/core/interfaces/native/implementation/water_flow_sections_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/water_flow_sections_accessor.cpp
@@ -15,14 +15,44 @@
 
 #include "core/components_ng/base/frame_node.h"
 #include "core/interfaces/native/utility/converter.h"
+#include "core/interfaces/native/utility/reverse_converter.h"
+#include "water_flow_sections_accessor_peer_impl.h"
 #include "arkoala_api_generated.h"
 
-struct WaterFlowSectionsPeer {};
+ namespace OHOS::Ace::NG::Converter {
+    template<>
+ WaterFlowSections::Section Convert(const Ark_SectionOptions& src)
+ {
+    WaterFlowSections::Section dst;
+    dst.itemsCount = Converter::Convert<int32_t>(src.itemsCount);
+    dst.crossCount = Converter::OptConvert<int32_t>(src.crossCount);
+
+    dst.columnsGap = Converter::OptConvert<Dimension>(src.columnsGap);
+    dst.rowsGap = Converter::OptConvert<Dimension>(src.rowsGap);
+    dst.margin = Converter::OptConvert<MarginProperty>(src.margin);
+    return dst;
+ }
+
+ template<>
+ std::vector<WaterFlowSections::Section> Convert(const Array_SectionOptions& src)
+ {
+     std::vector<WaterFlowSections::Section> dst;
+    auto length = Converter::Convert<int>(src.length);
+    for (int i = 0; i < length; i++) {
+        WaterFlowSections::Section newSession = Converter::Convert<WaterFlowSections::Section>(src.array[i]);
+        dst.push_back(newSession);
+    }
+    return dst;
+ }
+ }
 
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace WaterFlowSectionsAccessor {
-void DestroyPeerImpl(WaterFlowSectionsPeer* peer)
+static void DestroyPeerImpl(WaterFlowSectionsPeer *peerImpl)
 {
+    if (peerImpl) {
+        delete peerImpl;
+    }
 }
 WaterFlowSectionsPeer* CtorImpl()
 {
@@ -37,25 +67,57 @@ Ark_Boolean SpliceImpl(WaterFlowSectionsPeer* peer,
                        const Opt_Number* deleteCount,
                        const Opt_Array_SectionOptions* sections)
 {
-    return 0;
+    LOGE("ARKOALA WaterFlowSectionAccessor.SpliceImpl -> Method is not fully implemented. "
+          "Synchronous method is needed for onGetItemMainSizeByIndex");
+    CHECK_NULL_RETURN(peer && peer->GetController(), false);
+    CHECK_NULL_RETURN(start, false);
+    auto deleteCountOpt = Converter::OptConvert<int32_t>(*deleteCount);
+    auto delCnt = deleteCountOpt ? deleteCountOpt.value() : 0;
+    auto sectionsOpt = sections ? Converter::OptConvert<Array_SectionOptions>(*sections) : std::nullopt;
+    if (sectionsOpt) {
+        auto sections = Converter::OptConvert<std::vector<WaterFlowSections::Section>>(*sectionsOpt);
+        peer->GetController()->ChangeData(Converter::Convert<int32_t>(*start), delCnt, sections.value());
+        return true;
+    } else {
+        return false;
+    }
 }
 Ark_Boolean PushImpl(WaterFlowSectionsPeer* peer,
                      const Ark_SectionOptions* section)
 {
-    return 0;
+    CHECK_NULL_RETURN(peer && peer->GetController(), false);
+    LOGE("ARKOALA WaterFlowSectionAccessor.PushImpl -> Method is not fully implemented. "
+          "Synchronous method is needed for onGetItemMainSizeByIndex");
+    auto start = peer->GetController()->GetSectionInfo().size();
+    std::vector<WaterFlowSections::Section> sections;
+    auto newSection = Converter::OptConvert<WaterFlowSections::Section>(*section);
+    sections.push_back(newSection.value());
+    peer->GetController()->ChangeData(start, 0, sections);
+    return true;
 }
 Ark_Boolean UpdateImpl(WaterFlowSectionsPeer* peer,
                        const Ark_Number* sectionIndex,
                        const Ark_SectionOptions* section)
 {
-    return 0;
+    LOGE("ARKOALA WaterFlowSectionAccessor.UpdateImpl -> Method is not fully implemented. "
+          "Synchronous method is needed for onGetItemMainSizeByIndex");
+    CHECK_NULL_RETURN(peer && peer->GetController(), false);
+    std::vector<WaterFlowSections::Section> sections;
+    auto newSection = Converter::OptConvert<WaterFlowSections::Section>(*section);
+    sections.push_back(newSection.value());
+    peer->GetController()->ChangeData(Converter::Convert<int32_t>(*sectionIndex), 0, sections);
+    return true;
 }
 void ValuesImpl(WaterFlowSectionsPeer* peer)
 {
+    LOGE("ARKOALA WaterFlowSectionAccessor.ValuesImpl -> Incorrect return value, "
+         "should be Array<SectionOptions>");
+    CHECK_NULL_VOID(peer && peer->GetController());
 }
 Ark_Int32 LengthImpl(WaterFlowSectionsPeer* peer)
 {
-    return 0;
+    CHECK_NULL_RETURN(peer && peer->GetController(), false);
+    return peer->GetController()->GetSectionInfo().size();
 }
 } // WaterFlowSectionsAccessor
 const GENERATED_ArkUIWaterFlowSectionsAccessor* GetWaterFlowSectionsAccessor()
@@ -72,5 +134,4 @@ const GENERATED_ArkUIWaterFlowSectionsAccessor* GetWaterFlowSectionsAccessor()
     };
     return &WaterFlowSectionsAccessorImpl;
 }
-
 }
---
+++ test/unittest/capi/accessors/water_flow_section_accessor_test.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "core/interfaces/native/implementation/water_flow_sections_accessor_peer_impl.h"
+#include "core/components_ng/pattern/waterflow/water_flow_sections.h"
+#include "accessor_test_base.h"
+#include "node_api.h"
+#include "core/interfaces/native/utility/reverse_converter.h"
+#include "gmock/gmock.h"
+#include "base/memory/ace_type.h"
+
+namespace OHOS::Ace::NG {
+
+using namespace testing;
+using namespace testing::ext;
+
+class MockWaterFlowSections : public WaterFlowSections {
+public:
+    MockWaterFlowSections() = default;
+    ~MockWaterFlowSections() override = default;
+
+    const std::vector<Section>& GetSectionInfo() const {
+        counter++;
+        return vector;
+   }
+public:
+    mutable int counter = 0;
+    std::vector<Section> vector;
+};
+
+class WaterFlowSectionAccessorTest : public AccessorTestBase<GENERATED_ArkUIWaterFlowSectionsAccessor,
+    &GENERATED_ArkUIAccessors::getWaterFlowSectionsAccessor, WaterFlowSectionsPeer> {
+public:
+    void SetUp(void) override
+    {
+        AccessorTestBase::SetUp();
+        mockWaterFlowSections_ = new MockWaterFlowSections();
+        mockWaterFlowSectionsKeeper_ = AceType::Claim(mockWaterFlowSections_);
+        ASSERT_NE(mockWaterFlowSectionsKeeper_, nullptr);        
+        auto peerImpl = reinterpret_cast<WaterFlowSectionsPeer*>(peer_);
+        ASSERT_NE(peerImpl, nullptr);
+        peerImpl->SetController(mockWaterFlowSectionsKeeper_);
+        ASSERT_NE(mockWaterFlowSections_, nullptr);
+    }
+    void TearDown() override
+    {
+        AccessorTestBase::TearDown();
+        mockWaterFlowSectionsKeeper_ = nullptr;
+        mockWaterFlowSections_ = nullptr;
+    }
+
+    MockWaterFlowSections *mockWaterFlowSections_ = nullptr;
+    RefPtr<MockWaterFlowSections> mockWaterFlowSectionsKeeper_ = nullptr;
+};
+
+/**
+ * @tc.name: LengthTest
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(WaterFlowSectionAccessorTest, LengthTest, TestSize.Level1)
+{
+    void * refPtr = reinterpret_cast<void *>(&(*(peer_->GetController())));
+    MockWaterFlowSections* mockSections = reinterpret_cast<MockWaterFlowSections*>(refPtr);
+    
+    accessor_->length(peer_);
+    accessor_->length(peer_);
+    accessor_->length(nullptr);
+    accessor_->length(peer_);
+
+    EXPECT_EQ(mockSections->counter, 3);
+}
+} // namespace OHOS::Ace::NG
