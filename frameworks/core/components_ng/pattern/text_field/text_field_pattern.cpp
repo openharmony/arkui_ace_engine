516a5ccf6f2e6338ec21fe5b4bd49a7b70d0291a
wanjining <wanjining@huawei.com>
2024-11-29 11:46:47 +0800

    修复FN+左键没有跳转至行首的问题
    Signed-off-by: wanjining <wanjining@huawei.com>
    Change-Id: Ic69c06d56849a0eeec5f727e293c83ee9524407e

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -4694,12 +4694,12 @@ int32_t TextFieldPattern::GetLineBeginPosition(int32_t originCaretPosition, bool
         moveLineBeginOffset++;
         strIndex--;
         // stop moving caret if reaches \n, text head or caret line changed
-    } while (((strIndex > 0) && (wideTextValue[strIndex] != u'\n')) ||
-             (needToCheckLineChanged && !CharLineChanged(strIndex)));
+    } while (((strIndex > 0) && (wideTextValue[strIndex] != u'\n')) &&
+             (needToCheckLineChanged ? !CharLineChanged(strIndex) : true));
     if (strIndex < 0 || strIndex >= static_cast<int32_t>(wideTextValue.length())) {
         return 0;
     }
-    if (wideTextValue[strIndex] == u'\n') {
+    if (strIndex > 0) {
         moveLineBeginOffset--;
     }
     if (moveLineBeginOffset > originCaretPosition) {
@@ -4723,8 +4723,8 @@ int32_t TextFieldPattern::GetLineEndPosition(int32_t originCaretPosition, bool n
     }
     int32_t moveLineEndOffset = 0;
     int32_t strIndex = 0;
-    for (strIndex = originCaretPosition; (strIndex <= textLength && wideTextValue[strIndex] != u'\n') ||
-                                         (needToCheckLineChanged && !CharLineChanged(strIndex));
+    for (strIndex = originCaretPosition; (strIndex <= textLength && wideTextValue[strIndex] != u'\n') &&
+                                         (needToCheckLineChanged ? !CharLineChanged(strIndex) : true);
          strIndex++) {
         moveLineEndOffset++;
     }
@@ -4740,7 +4740,7 @@ bool TextFieldPattern::CharLineChanged(int32_t caretPosition)
         return true;
     }
     CaretMetricsF caretMetrics;
-    CalcCaretMetricsByPosition(selectController_->GetStartIndex(), caretMetrics);
+    CalcCaretMetricsByPosition(caretPosition, caretMetrics);
     return !NearEqual(caretMetrics.offset.GetY(), selectController_->GetCaretRect().GetY());
 }
 
@@ -4812,7 +4812,7 @@ bool TextFieldPattern::CursorMoveLineBegin()
     } else {
         UpdateCaretPositionWithClamp(0);
     }
-    OnCursorMoveDone();
+    OnCursorMoveDone(TextAffinity::DOWNSTREAM);
     return originCaretPosition != selectController_->GetCaretIndex();
 }
 
