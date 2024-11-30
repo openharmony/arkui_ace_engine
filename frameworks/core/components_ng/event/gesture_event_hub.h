28be180d15d38b8bd1513d37d4150e06a8f6e5c4
zenix_zxy <zhengxingyou1@huawei.com>
2024-11-30 11:24:19 +0800

    Code Review Optimize
    Signed-off-by: zenix_zxy <zhengxingyou1@huawei.com>
    Change-Id: I257536b7fd54d2aeea3024dd3d0dbae713c0f5e3

--- frameworks/core/components_ng/event/gesture_event_hub.h
+++ frameworks/core/components_ng/event/gesture_event_hub.h
@@ -255,6 +255,10 @@ public:
     RefPtr<PixelMap> GetPreScaledPixelMapIfExist(float targetScale, RefPtr<PixelMap> defaultPixelMap);
     float GetPixelMapScale(const int32_t height, const int32_t width) const;
     bool IsPixelMapNeedScale() const;
+    bool CheckAllowDrag(const GestureEvent& info, const RefPtr<PipelineBase>& context,
+        const RefPtr<FrameNode>& frameNode);
+    RefPtr<OHOS::Ace::DragEvent> CreateDragEvent(const GestureEvent& info, const RefPtr<PipelineBase>& context,
+        const RefPtr<FrameNode>& frameNode);
     void InitDragDropEvent();
     void HandleOnDragStart(const GestureEvent& info);
     void HandleOnDragUpdate(const GestureEvent& info);
@@ -267,7 +271,7 @@ public:
     void OnModifyDone();
     bool KeyBoardShortCutClick(const KeyEvent& event, const WeakPtr<NG::FrameNode>& node);
     bool IsAllowedDrag(RefPtr<EventHub> eventHub);
-    void HandleNotallowDrag(const GestureEvent& info);
+    void HandleNotAllowDrag(const GestureEvent& info);
     RefPtr<DragEventActuator> GetDragEventActuator();
     bool GetMonopolizeEvents() const;
     void SetMonopolizeEvents(bool monopolizeEvents);
