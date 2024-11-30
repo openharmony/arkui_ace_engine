490ba0632af0d736465d7d843ecb4e325b623b7d
姚明 <yaoming14@huawei.com>
2024-11-30 09:11:09 +0800

    修复RichEditor弹出AI菜单时布局发生改变导致遮挡问题
    Signed-off-by: 姚明 <yaoming14@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.h
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.h
@@ -549,6 +549,7 @@ public:
     int32_t GetHandleIndex(const Offset& offset) const override;
     void OnAreaChangedInner() override;
     void UpdateParentOffsetAndOverlay();
+    void OnParentOffsetChange();
     void CreateHandles() override;
     void ShowHandles(const bool isNeedShowHandles) override;
     void ShowHandles() override;
