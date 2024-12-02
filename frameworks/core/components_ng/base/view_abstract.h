f9dc7bcb1ae9ed131cf187b11b43204321d532d9
pengzhiwen <pengzhiwen3@huawei.com>
2024-12-02 11:31:29 +0800

    inspector新增capi接口
    Signed-off-by: pengzhiwen <pengzhiwen3@huawei.com>
    Change-Id: Iddd5c3a67cff37374866635296a5785c9a29a22e

--- frameworks/core/components_ng/base/view_abstract.h
+++ frameworks/core/components_ng/base/view_abstract.h
@@ -649,6 +649,8 @@ public:
         FrameNode* frameNode, NG::ShouldBuiltInRecognizerParallelWithFunc&& shouldBuiltInRecognizerParallelWithFunc);
     static void SetSystemColorModeChangeEvent(FrameNode* frameNode, std::function<void(int32_t)>&& onColorModeChange);
     static void SetSystemFontChangeEvent(FrameNode* frameNode, std::function<void(float, float)>&& onFontChange);
+    static void SetDrawCompleteEvent(FrameNode* frameNode, std::function<void()>&& onDraw);
+    static void SetLayoutEvent(FrameNode* frameNode, std::function<void()>&& onLayout);
     static void SetFocusBoxStyle(FrameNode* frameNode, const NG::FocusBoxStyle& style);
 
     static bool GetFocusable(FrameNode* frameNode);
