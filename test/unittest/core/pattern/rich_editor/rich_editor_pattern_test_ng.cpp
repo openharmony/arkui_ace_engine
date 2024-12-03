277ee88bfffa374da7acf8e4b42861179157cf99
朱维峰 <zhuweifeng1@huawei.com>
2024-12-03 03:18:25 +0000

    update test/unittest/core/pattern/rich_editor/rich_editor_pattern_test_ng.cpp.
    Signed-off-by: 朱维峰 <zhuweifeng1@huawei.com>

--- test/unittest/core/pattern/rich_editor/rich_editor_pattern_test_ng.cpp
+++ test/unittest/core/pattern/rich_editor/rich_editor_pattern_test_ng.cpp
@@ -14,6 +14,14 @@
  */
 
 #include "test/unittest/core/pattern/rich_editor/rich_editor_common_test_ng.h"
+#include "test/mock/core/common/mock_udmf.h"
+#include "test/mock/core/render/mock_paragraph.h"
+#include "test/mock/core/pipeline/mock_pipeline_context.h"
+#include "test/mock/core/common/mock_theme_manager.h"
+#include "test/mock/core/common/mock_container.h"
+#include "test/mock/base/mock_task_executor.h"
+#include "core/components_ng/pattern/rich_editor/rich_editor_theme.h"
+#include "core/components_ng/pattern/rich_editor/rich_editor_model_ng.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -33,6 +41,7 @@ int32_t testNumber2 = 2;
 int32_t testNumber3 = 3;
 int32_t testNumber4 = 4;
 int32_t testNumber5 = 5;
+int32_t callBack1 = 0;
 } // namespace
 
 class RichEditorPatternTestNg : public RichEditorCommonTestNg {
