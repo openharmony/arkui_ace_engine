3b8bb311159f9a0732b0a306b64974756957fb9b
朱维峰 <zhuweifeng1@huawei.com>
2024-12-03 03:22:18 +0000

    update test/unittest/core/pattern/rich_editor/rich_editor_styled_string_test_ng.cpp.
    Signed-off-by: 朱维峰 <zhuweifeng1@huawei.com>

--- test/unittest/core/pattern/rich_editor/rich_editor_styled_string_test_ng.cpp
+++ test/unittest/core/pattern/rich_editor/rich_editor_styled_string_test_ng.cpp
@@ -14,6 +14,11 @@
  */
 
 #include "test/unittest/core/pattern/rich_editor/rich_editor_common_test_ng.h"
+#include "test/mock/core/render/mock_paragraph.h"
+#include "test/mock/core/pipeline/mock_pipeline_context.h"
+#include "test/mock/core/common/mock_container.h"
+#include "test/mock/base/mock_task_executor.h"
+#include "core/components_ng/pattern/rich_editor/rich_editor_model_ng.h"
 
 using namespace testing;
 using namespace testing::ext;
