04b82c0f8cadb8ebd347cc0d03bc5b6e65e8bed9
朱维峰 <zhuweifeng1@huawei.com>
2024-12-03 03:01:21 +0000

    update test/unittest/core/pattern/rich_editor/rich_editor_accessibility_test_ng.cpp.
    Signed-off-by: 朱维峰 <zhuweifeng1@huawei.com>

--- test/unittest/core/pattern/rich_editor/rich_editor_accessibility_test_ng.cpp
+++ test/unittest/core/pattern/rich_editor/rich_editor_accessibility_test_ng.cpp
@@ -12,6 +12,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#define private public
+#define protected public
+
+#include "test/mock/base/mock_task_executor.h"
+#include "test/mock/core/common/mock_container.h"
+#include "test/mock/core/pipeline/mock_pipeline_context.h"
+#include "test/mock/core/render/mock_paragraph.h"
 #include "test/unittest/core/pattern/rich_editor/rich_editor_common_test_ng.h"
 
 
