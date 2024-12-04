eb53b016c96f9e47d83f3f27a3e5604033e7992b
Sergey Dudkin <dudkin.sergey@huawei.com>
2024-12-04 15:30:17 +0300

    Implement RichEditor base accessor
    Signed-off-by: Sergey Dudkin <dudkin.sergey@huawei.com>
    Change-Id: I3950e09a23adf8854d97bd7a13d320f75d13c1dc

--- frameworks/core/interfaces/native/implementation/rich_editor_base_controller_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/rich_editor_base_controller_accessor.cpp
@@ -14,15 +14,73 @@
  */
 
 #include "core/components_ng/base/frame_node.h"
+#include "core/interfaces/native/implementation/rich_editor_styled_string_controller_peer_impl.h"
 #include "core/interfaces/native/utility/converter.h"
+#include "core/interfaces/native/utility/converter2.h"
+#include "core/interfaces/native/utility/reverse_converter.h"
+#include "rich_editor_base_controller_peer_impl.h"
 #include "arkoala_api_generated.h"
 
-struct RichEditorBaseControllerPeer {};
+namespace OHOS::Ace::NG::Converter {
+template<> TextStyle Convert(const Ark_RichEditorTextStyle& src);
+template<> OHOS::Ace::FontStyle Convert(const Ark_FontStyle& src);
+template<> Array_ShadowOptions Convert(const Ark_ShadowOptions& src)
+{
+    Array_ShadowOptions ret;
+    ret.array = const_cast<Ark_ShadowOptions*>(&src);
+    ret.length = 1;
+    return ret;
+}
+template<>
+std::vector<Shadow> Convert(const Ark_Union_ShadowOptions_Array_ShadowOptions& src)
+{
+    std::vector<Shadow> ret;
+    auto value = Converter::OptConvert<Array_ShadowOptions>(src);
+    for (int idx = 0; value && idx < value->length; idx++) {
+        if (auto shadow = Converter::OptConvert<Shadow>(value->array[idx]); shadow) {
+            ret.push_back(shadow.value());
+        }
+    }
+    return ret;
+}
+template<>
+struct UpdateSpanStyle Convert(const Ark_RichEditorTextStyle& src)
+{
+    UpdateSpanStyle ret;
+    ret.updateTextColor = Converter::OptConvert<Color>(src.fontColor);
+    if (auto sizeOpt = Converter::OptConvert<Dimension>(src.fontSize); sizeOpt) {
+        ret.updateFontSize = sizeOpt.value();
+    }
+    ret.updateItalicFontStyle = Converter::OptConvert<OHOS::Ace::FontStyle>(src.fontStyle);
+    ret.updateFontWeight = Converter::OptConvert<FontWeight>(src.fontWeight);
+    auto fontFamilyStr = Converter::OptConvert<std::string>(src.fontFamily);
+    if (fontFamilyStr && fontFamilyStr->length() > 0) {
+        ret.updateFontFamily->push_back(fontFamilyStr.value());
+    }
+    ret.updateTextDecoration = Converter::OptConvert<TextDecoration>(src.decoration.value.type);
+    ret.updateTextDecorationColor = Converter::OptConvert<Color>(src.decoration.value.color);
+    ret.updateTextDecorationStyle = Converter::OptConvert<TextDecorationStyle>(src.decoration.value.style);
+    ret.updateTextShadows = Converter::OptConvert<std::vector<Shadow>>(src.textShadow);
+
+    if (auto heightOpt = Converter::OptConvert<Dimension>(src.lineHeight); heightOpt) {
+        ret.updateLineHeight = heightOpt.value();
+    }
+    if (auto letterSpacingOpt = Converter::OptConvert<Dimension>(src.letterSpacing); letterSpacingOpt) {
+        ret.updateLetterSpacing = letterSpacingOpt.value();
+    }
+    return ret;
+}
+}
+
+struct RichEditorBaseControllerPeer : public OHOS::Ace::NG::GeneratedModifier::RichEditorBaseControllerPeerImpl {};
 
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace RichEditorBaseControllerAccessor {
 void DestroyPeerImpl(RichEditorBaseControllerPeer* peer)
 {
+    if (peer) {
+        delete peer;
+    }
 }
 RichEditorBaseControllerPeer* CtorImpl()
 {
@@ -34,43 +92,72 @@ Ark_NativePointer GetFinalizerImpl()
 }
 Ark_Int32 GetCaretOffsetImpl(RichEditorBaseControllerPeer* peer)
 {
-    return 0;
+    CHECK_NULL_RETURN(peer, 0);
+    auto result = peer->GetCaretOffset();
+    return Converter::ArkValue<Ark_Int32>(result);
 }
 Ark_Boolean SetCaretOffsetImpl(RichEditorBaseControllerPeer* peer,
                                const Ark_Number* offset)
 {
-    return 0;
+    CHECK_NULL_RETURN(peer, false);
+    CHECK_NULL_RETURN(offset, false);
+    int32_t caretOffset = Converter::Convert<int32_t>(*offset);
+    bool result = peer->SetCaretOffset(caretOffset);
+    return Converter::ArkValue<Ark_Boolean>(result);
 }
 void CloseSelectionMenuImpl(RichEditorBaseControllerPeer* peer)
 {
+    CHECK_NULL_VOID(peer);
+    peer->CloseSelectionMenu();
 }
 Ark_NativePointer GetTypingStyleImpl(RichEditorBaseControllerPeer* peer)
 {
-    return 0;
+    CHECK_NULL_RETURN(peer, nullptr);
+    LOGW("RichEditorBaseControllerAccessor:: GetTypingStyleImpl is not implemented");
+    return nullptr;
 }
 void SetTypingStyleImpl(RichEditorBaseControllerPeer* peer,
                         const Ark_RichEditorTextStyle* value)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(value);
+    auto textStyle = Converter::OptConvert<TextStyle>(*value);
+    auto typingStyle = Converter::OptConvert<struct UpdateSpanStyle>(*value);
+    peer->SetTypingStyle(typingStyle, textStyle);
 }
 void SetSelectionImpl(RichEditorBaseControllerPeer* peer,
                       const Ark_Number* selectionStart,
                       const Ark_Number* selectionEnd,
                       const Opt_SelectionOptions* options)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(selectionStart);
+    CHECK_NULL_VOID(selectionEnd);
+    CHECK_NULL_VOID(options);
+    int32_t start = Converter::Convert<int32_t>(*selectionStart);
+    int32_t end = Converter::Convert<int32_t>(*selectionEnd);
+    auto optOptions = Converter::OptConvert<SelectionOptions>(*options);
+    peer->SetSelection(start, end, optOptions, start < end);
 }
 Ark_Boolean IsEditingImpl(RichEditorBaseControllerPeer* peer)
 {
-    return 0;
+    CHECK_NULL_RETURN(peer, false);
+    bool result = peer->IsEditing();
+    return Converter::ArkValue<Ark_Boolean>(result);
 }
 void StopEditingImpl(RichEditorBaseControllerPeer* peer)
 {
+    CHECK_NULL_VOID(peer);
+    peer->StopEditing();
 }
 Ark_NativePointer GetLayoutManagerImpl(RichEditorBaseControllerPeer* peer)
 {
+    LOGW("RichEditorBaseControllerAccessor:: GetLayoutManagerImpl is not implemented");
     return 0;
 }
 Ark_NativePointer GetPreviewTextImpl(RichEditorBaseControllerPeer* peer)
 {
+    LOGW("RichEditorBaseControllerAccessor:: GetPreviewTextImpl is not implemented");
     return 0;
 }
 } // RichEditorBaseControllerAccessor
---
+++ test/unittest/capi/accessors/richeditor_base_controller_accessor_test.cpp
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "richeditor_accessor_test.h"
+#include "core/interfaces/native/implementation/rich_editor_base_controller_peer_impl.h"
+#include "accessor_test_base.h"
+#include "node_api.h"
+#include "core/components_ng/pattern/text/span/span_string.h"
+#include "core/interfaces/native/utility/converter.h"
+#include "core/interfaces/native/utility/reverse_converter.h"
+#include "gmock/gmock.h"
+
+namespace OHOS::Ace {
+bool operator==(const std::optional<OHOS::Ace::SelectionOptions>& lhs,
+                const std::optional<OHOS::Ace::SelectionOptions>& rhs)
+{
+    if (!lhs.has_value() || !rhs.has_value()) {
+        return false;
+    }
+    return lhs->menuPolicy == rhs->menuPolicy;
+}
+bool operator==(const std::optional<OHOS::Ace::UpdateSpanStyle>& lhs,
+                const std::optional<OHOS::Ace::UpdateSpanStyle>& rhs)
+{
+    if (!lhs.has_value() || !rhs.has_value()) {
+        return false;
+    }
+    bool textColorEq = lhs->updateTextColor && rhs->updateTextColor &&
+        lhs->updateTextColor.value() == rhs->updateTextColor.value();
+    bool fontSizeEq = lhs->updateFontSize && rhs->updateFontSize &&
+        lhs->updateFontSize.value() == rhs->updateFontSize.value();
+    bool italicFontStyleEq = lhs->updateItalicFontStyle && rhs->updateItalicFontStyle &&
+        lhs->updateItalicFontStyle.value() == rhs->updateItalicFontStyle.value();
+    bool fontWeightEq = lhs->updateFontWeight && rhs->updateFontWeight &&
+        lhs->updateFontWeight.value() == rhs->updateFontWeight.value();
+    return textColorEq && fontSizeEq && italicFontStyleEq && fontWeightEq;
+}
+} // OHOS::Ace
+
+static constexpr int32_t TEST_IDX = 0;
+static constexpr int32_t TEST_IDX_2 = 10;
+static constexpr int32_t TEST_START = 1;
+static constexpr int32_t TEST_END = 3;
+static const std::vector<std::string> FONT_FAMILIES = { "serif" };
+static constexpr double FONT_SIZE = 10.0;
+static constexpr OHOS::Ace::FontWeight FONT_WEIGHT = OHOS::Ace::FontWeight::NORMAL;
+static constexpr OHOS::Ace::FontStyle FONT_STYLE = OHOS::Ace::FontStyle::NORMAL;
+static const OHOS::Ace::Color FONT_COLOUR = OHOS::Ace::Color::RED;
+
+namespace OHOS::Ace::NG {
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace {
+class MockRichEditorBaseController : public RichEditorBaseController {
+public:
+    MockRichEditorBaseController() = default;
+    ~MockRichEditorBaseController() override = default;
+    MOCK_METHOD(int32_t, GetCaretOffset, ());
+    MOCK_METHOD(bool, SetCaretOffset, (int32_t));
+    MOCK_METHOD(void, CloseSelectionMenu, ());
+    MOCK_METHOD(bool, IsEditing, ());
+    MOCK_METHOD(void, StopEditing, ());
+    MOCK_METHOD(void, SetSelection, (int32_t, int32_t, const std::optional<SelectionOptions>&, bool));
+    MOCK_METHOD(void, SetTypingStyle, (std::optional<struct UpdateSpanStyle>, std::optional<TextStyle>));
+};
+} // namespace
+
+class RichEditorBaseControllerAccessorTest : public AccessorTestBase<GENERATED_ArkUIRichEditorBaseControllerAccessor,
+    &GENERATED_ArkUIAccessors::getRichEditorBaseControllerAccessor, RichEditorBaseControllerPeer> {
+public:
+    void SetUp(void) override
+    {
+        AccessorTestBase::SetUp();
+
+        mockRichEditorController_ = new MockRichEditorBaseController();
+        mockRichEditorControllerKeeper_ = AceType::Claim(mockRichEditorController_);
+        ASSERT_NE(mockRichEditorControllerKeeper_, nullptr);
+
+        auto peerImpl = reinterpret_cast<GeneratedModifier::RichEditorBaseControllerPeerImpl *>(peer_);
+        ASSERT_NE(peerImpl, nullptr);
+        peerImpl->AddTargetController(mockRichEditorControllerKeeper_);
+
+        ASSERT_NE(mockRichEditorController_, nullptr);
+    }
+
+    void TearDown() override
+    {
+        mockRichEditorControllerKeeper_ = nullptr;
+        mockRichEditorController_ = nullptr;
+    }
+
+    MockRichEditorBaseController *mockRichEditorController_ = nullptr;
+    RefPtr<MockRichEditorBaseController> mockRichEditorControllerKeeper_ = nullptr;
+};
+
+/**
+ * @tc.name: getCaretOffsetTest
+ * @tc.desc: Check the functionality of getCaretOffset
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorBaseControllerAccessorTest, getCaretOffsetTest, TestSize.Level1)
+{
+    ASSERT_NE(accessor_->getCaretOffset, nullptr);
+
+    EXPECT_CALL(*mockRichEditorController_, GetCaretOffset()).Times(1);
+    Ark_Int32 idx = accessor_->getCaretOffset(peer_);
+    ASSERT_EQ(idx, TEST_IDX);
+}
+
+/**
+ * @tc.name: setCaretOffsetTest
+ * @tc.desc: Check the functionality of setCaretOffset
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorBaseControllerAccessorTest, setCaretOffsetTest, TestSize.Level1)
+{
+    ASSERT_NE(accessor_->getCaretOffset, nullptr);
+
+    EXPECT_CALL(*mockRichEditorController_, SetCaretOffset(TEST_IDX_2)).Times(1);
+    Ark_Number offset = Converter::ArkValue<Ark_Number>(TEST_IDX_2);
+    bool result = accessor_->setCaretOffset(peer_, &offset);
+    ASSERT_FALSE(result);
+}
+
+/**
+ * @tc.name: closeSelectionMenuTest
+ * @tc.desc: Check the functionality of closeSelectionMenu
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorBaseControllerAccessorTest, closeSelectionMenuTest, TestSize.Level1)
+{
+    ASSERT_NE(accessor_->closeSelectionMenu, nullptr);
+
+    EXPECT_CALL(*mockRichEditorController_, CloseSelectionMenu()).Times(1);
+    accessor_->closeSelectionMenu(peer_);
+}
+
+/**
+ * @tc.name: isEditingTest
+ * @tc.desc: Check the functionality of isEditing
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorBaseControllerAccessorTest, isEditingTest, TestSize.Level1)
+{
+    ASSERT_NE(accessor_->isEditing, nullptr);
+
+    EXPECT_CALL(*mockRichEditorController_, IsEditing()).Times(1);
+    bool result = accessor_->isEditing(peer_);
+    ASSERT_FALSE(result);
+}
+
+/**
+ * @tc.name: stopEditingTest
+ * @tc.desc: Check the functionality of stopEditing
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorBaseControllerAccessorTest, stopEditingTest, TestSize.Level1)
+{
+    ASSERT_NE(accessor_->stopEditing, nullptr);
+
+    EXPECT_CALL(*mockRichEditorController_, StopEditing()).Times(1);
+    accessor_->stopEditing(peer_);
+}
+
+/**
+ * @tc.name: setSelectionTest
+ * @tc.desc: Check the functionality of setSelection
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorBaseControllerAccessorTest, setSelectionTest, TestSize.Level1)
+{
+    ASSERT_NE(accessor_->setSelection, nullptr);
+
+    SelectionOptions opts;
+    opts.menuPolicy = MenuPolicy::SHOW;
+    std::optional<SelectionOptions> checkOpts = opts;
+    EXPECT_CALL(*mockRichEditorController_, SetSelection(TEST_START, TEST_END, checkOpts, true)).Times(1);
+    auto start = Converter::ArkValue<Ark_Number>(TEST_START);
+    auto end = Converter::ArkValue<Ark_Number>(TEST_END);
+    Opt_SelectionOptions optsOpt;
+    optsOpt.value.menuPolicy.value = Ark_MenuPolicy::ARK_MENU_POLICY_SHOW;
+    accessor_->setSelection(peer_, &start, &end, &optsOpt);
+}
+
+/**
+ * @tc.name: setTypingStyleTest
+ * @tc.desc: Check the functionality of setTypingStyle
+ * @tc.type: FUNC
+ */
+HWTEST_F(RichEditorBaseControllerAccessorTest, setTypingStyleTest, TestSize.Level1)
+{
+    ASSERT_NE(accessor_->setTypingStyle, nullptr);
+    std::optional<struct UpdateSpanStyle> spanStyle = UpdateSpanStyle();
+    spanStyle->updateTextColor = FONT_COLOUR;
+    spanStyle->updateFontSize = CalcDimension(FONT_SIZE);
+    spanStyle->updateItalicFontStyle = FONT_STYLE;
+    spanStyle->updateFontWeight = FONT_WEIGHT;
+    std::optional<TextStyle> textStyle = TextStyle(FONT_FAMILIES, FONT_SIZE, FONT_WEIGHT, FONT_STYLE, FONT_COLOUR);
+    EXPECT_CALL(*mockRichEditorController_, SetTypingStyle(spanStyle, textStyle)).Times(1);
+    auto value = Converter::ArkValue<Ark_RichEditorTextStyle>(*textStyle);
+    accessor_->setTypingStyle(peer_, &value);
+}
+
+} // namespace OHOS::Ace::NG
