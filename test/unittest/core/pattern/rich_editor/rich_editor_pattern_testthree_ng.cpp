5428dc8b57468feaf13a87f4765f00c8ba053a1e
朱维峰 <zhuweifeng1@huawei.com>
2024-12-04 08:59:36 +0000

    update test/unittest/core/pattern/rich_editor/rich_editor_pattern_testthree_ng.cpp.
    Signed-off-by: 朱维峰 <zhuweifeng1@huawei.com>

--- test/unittest/core/pattern/rich_editor/rich_editor_pattern_testthree_ng.cpp
+++ test/unittest/core/pattern/rich_editor/rich_editor_pattern_testthree_ng.cpp
@@ -13,9 +13,9 @@
  * limitations under the License.
  */
 
+#include "test/unittest/core/pattern/rich_editor/rich_editor_common_test_ng.h"
 #include "test/mock/base/mock_task_executor.h"
 #include "test/mock/core/common/mock_clipboard.h"
-#include "test/unittest/core/pattern/rich_editor/rich_editor_common_test_ng.h"
 #include "core/components_ng/pattern/text_field/text_field_manager.h"
 #include "test/mock/core/render/mock_paragraph.h"
 #include "test/mock/core/pipeline/mock_pipeline_context.h"
