c3a4304c3eedfa910421f2414376b43bb572ebdd
zhouchaobo <zhouchaobo@huawei.com>
2024-12-04 10:06:33 +0800

    cherrypick:mouse localLocation bug fix
    Signed-off-by: zhouchaobo <zhouchaobo@huawei.com>
    Change-Id: Id0ff0ddfdadcc5674e05208d332fa9c60e30f7c5

--- frameworks/core/event/mouse_event.h
+++ frameworks/core/event/mouse_event.h
@@ -441,31 +441,7 @@ public:
         onMouseCallback_ = onMouseCallback;
     }
 
-    bool HandleMouseEvent(const MouseEvent& event)
-    {
-        if (!onMouseCallback_) {
-            return false;
-        }
-        MouseInfo info;
-        info.SetPointerEvent(event.pointerEvent);
-        info.SetButton(event.button);
-        info.SetAction(event.action);
-        info.SetPullAction(event.pullAction);
-        info.SetGlobalLocation(event.GetOffset());
-        Offset localLocation = Offset(
-            event.GetOffset().GetX() - coordinateOffset_.GetX(), event.GetOffset().GetY() - coordinateOffset_.GetY());
-        info.SetLocalLocation(localLocation);
-        info.SetScreenLocation(event.GetScreenOffset());
-        info.SetTimeStamp(event.time);
-        info.SetDeviceId(event.deviceId);
-        info.SetTargetDisplayId(event.targetDisplayId);
-        info.SetSourceDevice(event.sourceType);
-        info.SetSourceTool(event.sourceTool);
-        info.SetTarget(GetEventTarget().value_or(EventTarget()));
-        info.SetPressedKeyCodes(event.pressedKeyCodes_);
-        onMouseCallback_(info);
-        return info.IsStopPropagation();
-    }
+    bool HandleMouseEvent(const MouseEvent& event);
 
     bool DispatchEvent(const TouchEvent& point) override
     {
