edfd5bf59774e9f90d23bc1218d93606f16aabed
voronov.vadim <voronov.vadim@huawei-partners.com>
2024-12-06 18:58:50 +0300

    s_developer_canvas_gradient review update
    Signed-off-by: voronov.vadim <voronov.vadim@huawei-partners.com>
    Change-Id: I9757a9c68fe14a9edcdc780bf1f3fd3ccaaf2f3f

--- test/unittest/capi/accessors/canvas_gradient_accessor_test.cpp
+++ test/unittest/capi/accessors/canvas_gradient_accessor_test.cpp
@@ -16,9 +16,9 @@
 #include "gmock/gmock.h"
 
 #include "accessor_test_base.h"
+#include "frameworks/base/utils/utils.h"
 #include "node_api.h"
 #include "core/interfaces/native/utility/reverse_converter.h"
-
 #include "core/interfaces/native/implementation/canvas_gradient_peer.h"
 
 namespace OHOS::Ace::NG {
@@ -27,57 +27,32 @@ using namespace testing::ext;
 using ::testing::NiceMock;
 
 namespace {
-struct MockCanvasGradientPeer : public CanvasGradientPeer {
-    MockCanvasGradientPeer() = default;
-    ~MockCanvasGradientPeer() override = default;
-    MOCK_METHOD(void, AddColorStop, (const float&, const Color&), (override));
-};
-std::vector < std::tuple<float, std::string, float, Color>> GRADIENT_TEST_PLAN = {
-    { 0.50f, "#FFFFFFFF", 0.5f, Color::WHITE },
-    { 12.34f, "#FF000000", 12.34f, Color::BLACK },
-    { -56.73f, "#FF0000FF", -56.73f, Color::BLUE },
-    { 0.50f, "invalid color", -1.0f, Color::TRANSPARENT },
-    { 0.00f, "#FF0000FF", -1.0f, Color::TRANSPARENT },
-    { 0.50f, "", -1.0f, Color::TRANSPARENT },
-    { 0.00f, "", -1.0f, Color::TRANSPARENT },
+const auto DEFAULT_COLORS_SIZE = 2;
+std::vector < std::tuple<float, std::string, double, Color>> gradientTestPlan = {
+    { 0.50f, "#FFFFFFFF", 0.5, Color::WHITE },
+    { 12.34f, "#FF000000", 0.00, Color::TRANSPARENT },
+    { -56.73f, "#FF0000FF", 0.00, Color::TRANSPARENT },
+    { 0.50f, "invalid color", 0.00, Color::TRANSPARENT },
+    { 0.50f, "", 0.00, Color::TRANSPARENT },
 };
 } // namespace
 
-class CanvasGradientAccessorTest : public testing::Test {
+class CanvasGradientAccessorTest : public AccessorTestBase<GENERATED_ArkUICanvasGradientAccessor,
+    &GENERATED_ArkUIAccessors::getCanvasGradientAccessor, CanvasGradientPeer> {
 public:
-    static void SetUpTestCase()
+    void SetUp(void) override
     {
-        ASSERT_NE(accessor_, nullptr);
-        ASSERT_NE(accessor_->ctor, nullptr);
-        finalyzer_ = reinterpret_cast<void (*)(MockCanvasGradientPeer *)>(accessor_->getFinalizer());
-        ASSERT_NE(accessor_->getFinalizer, nullptr);
+        AccessorTestBase::SetUp();
+        canvasGradient_ = std::make_shared<OHOS::Ace::Gradient>();
+        ASSERT_NE(canvasGradient_, nullptr);
+        ASSERT_NE(peer_, nullptr);
+        peer_->SetGradient(canvasGradient_);
     }
-    virtual void SetUp()
+    void TearDown() override
     {
-        ASSERT_NE(finalyzer_, nullptr);
+        canvasGradient_ = nullptr;
     }
-    virtual void TearDown()
-    {
-        FinalizePeer();
-    }
-    void FinalizePeer()
-    {
-        if (peer_) {
-            ASSERT_NE(finalyzer_, nullptr);
-            finalyzer_(peer_);
-            peer_ = nullptr;
-        }
-    }
-private:
-    inline static const GENERATED_ArkUIFullNodeAPI *fullAPI_
-        = reinterpret_cast<const GENERATED_ArkUIFullNodeAPI *>(
-            GENERATED_GetArkAnyAPI(GENERATED_Ark_APIVariantKind::GENERATED_FULL, GENERATED_ARKUI_FULL_API_VERSION)
-        );
-public:
-    inline static const GENERATED_ArkUICanvasGradientAccessor *accessor_ =
-        fullAPI_->getAccessors()->getCanvasGradientAccessor();
-    inline static void (*finalyzer_)(MockCanvasGradientPeer *) = nullptr;
-    MockCanvasGradientPeer *peer_ = nullptr;
+    std::shared_ptr<OHOS::Ace::Gradient> canvasGradient_ = nullptr;
 };
 
 /**
@@ -88,24 +63,18 @@ public:
 HWTEST_F(CanvasGradientAccessorTest, addColorStopTest, TestSize.Level1)
 {
     ASSERT_NE(accessor_->addColorStop, nullptr);
-    NiceMock<MockCanvasGradientPeer>*  peer_ = new NiceMock<MockCanvasGradientPeer>();
-    ASSERT_NE(peer_, nullptr);
-
-    for (const auto& [offset, color, expectedOffset, expectedColor] : GRADIENT_TEST_PLAN) {
+    std::shared_ptr<OHOS::Ace::Gradient> gradient = peer_->GetGradient();
+    for (const auto& [offset, color, expectedOffset, expectedColor] : gradientTestPlan) {
         Ark_Number arkOffset = Converter::ArkValue<Ark_Number>(offset);
         Ark_String arkColor = Converter::ArkValue<Ark_String>(color);
-
-        EXPECT_CALL(*peer_, AddColorStop(expectedOffset, expectedColor)).Times(1);
-        if (NearZero(offset) && color.length() == 0) {
-            accessor_->addColorStop(peer_, nullptr, nullptr);
-        } else if (NearZero(offset)) {
-            accessor_->addColorStop(peer_, nullptr, &arkColor);
-        } else if (color.length() == 0) {
-            accessor_->addColorStop(peer_, &arkOffset, nullptr);
-        } else {
-            accessor_->addColorStop(peer_, &arkOffset, &arkColor);
+        gradient->ClearColors();
+        accessor_->addColorStop(peer_, &arkOffset, &arkColor);
+        auto colors = gradient->GetColors();
+        EXPECT_EQ(colors.size(), DEFAULT_COLORS_SIZE);
+        for (const auto& color : gradient->GetColors()) {
+            EXPECT_TRUE(LessOrEqualCustomPrecision(color.GetDimension().Value(), expectedOffset));
+            EXPECT_EQ(color.GetColor().ToString(), expectedColor.ToString());
         }
     }
-    delete(peer_);
 }
 } // namespace OHOS::Ace::NG
