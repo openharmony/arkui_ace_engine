2a9ab675695182828520c1e87be7c7387c31a4f2
wangweiyuan <wangweiyuan2@huawei.com>
2024-12-07 11:58:42 +0800

    支持未成功发起拖拽情况下的end状态回调
    Signed-off-by: wangweiyuan <wangweiyuan2@huawei.com>

--- frameworks/core/components_ng/event/gesture_event_hub.cpp
+++ frameworks/core/components_ng/event/gesture_event_hub.cpp
@@ -916,6 +916,7 @@ void GestureEventHub::OnDragStart(const GestureEvent& info, const RefPtr<Pipelin
     auto eventRet = dragEvent->GetResult();
     if (eventRet == DragRet::DRAG_FAIL || eventRet == DragRet::DRAG_CANCEL) {
         TAG_LOGI(AceLogTag::ACE_DRAG, "Drag result is %{public}d, stop dragging.", eventRet);
+        FireCustomerOnDragEnd(pipeline, eventHub);
         if (info.GetInputEventType() == InputEventType::MOUSE_BUTTON) {
             SetMouseDragMonitorState(false);
         }
@@ -1157,6 +1158,7 @@ void GestureEventHub::OnDragStart(const GestureEvent& info, const RefPtr<Pipelin
             SubwindowManager::GetInstance()->HidePreviewNG();
             overlayManager->RemovePixelMap();
         }
+        FireCustomerOnDragEnd(pipeline, eventHub);
         TAG_LOGW(AceLogTag::ACE_DRAG, "Start drag failed, return value is %{public}d", ret);
         return;
     }
