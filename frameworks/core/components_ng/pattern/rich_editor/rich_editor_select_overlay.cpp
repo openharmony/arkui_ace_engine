c585323de0d7dc6176f0d3c3e025b036187e3b0a
zzc0127 <zhangzecong@huawei.com>
2024-12-09 03:32:48 +0000

    update frameworks/core/components_ng/pattern/rich_editor/rich_editor_s… （cherry picked commit from <gitee.com//openharmony/arkui_ace_engine/commit/0517afc5aea799543ba7baefe8db9d0b388420fe> Signed-off-by: zzc0127 <zhangzecong@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_select_overlay.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_select_overlay.cpp
@@ -405,17 +405,21 @@ void RichEditorSelectOverlay::OnCloseOverlay(OptionMenuType menuType, CloseReaso
     IF_TRUE(needResetSelection, pattern->ResetSelection());
     IF_TRUE(isHoldByOther, pattern->CloseSelectOverlay());
     if (isBackPressed) {
-        if (pattern->IsEditing()) {
-            TAG_LOGI(AceLogTag::ACE_RICH_TEXT, "only show caret for edit state");
-            pattern->isCursorAlwaysDisplayed_ = false;
-            pattern->StartTwinkling();
-        }
+        IF_TRUE((info && info->isSingleHandle), pattern->OnBackPressed());
+        ResumeTwinkling();
     }
 }
 
 void RichEditorSelectOverlay::OnHandleGlobalTouchEvent(SourceType sourceType, TouchType touchType, bool touchInside)
 {
     BaseTextSelectOverlay::OnHandleGlobalTouchEvent(sourceType, touchType);
+    CHECK_NULL_VOID(IsMouseClickDown(sourceType, touchType) || IsTouchUp(sourceType, touchType));
+    if (IsSingleHandle()) {
+        CloseOverlay(false, CloseReason::CLOSE_REASON_CLICK_OUTSIDE);
+        ResumeTwinkling();
+    } else {
+        HideMenu();
+    }
 }
 
 void RichEditorSelectOverlay::OnHandleLevelModeChanged(HandleLevelMode mode)
@@ -533,6 +537,15 @@ void RichEditorSelectOverlay::SwitchCaretState()
         pattern->isCursorAlwaysDisplayed_ = false;
         pattern->StartTwinkling();
     }
+}
+
+void RichEditorSelectOverlay::ResumeTwinkling()
+{
+    auto pattern = GetPattern<RichEditorPattern>();
+    CHECK_NULL_VOID(pattern && pattern->IsEditing());
+    TAG_LOGI(AceLogTag::ACE_RICH_TEXT, "only show caret for edit state");
+    pattern->isCursorAlwaysDisplayed_ = false;
+    pattern->StartTwinkling();
 }
 
 void RichEditorSelectOverlay::OnHandleIsHidden()
