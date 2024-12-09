43ff9515b3994ee205a8ad2fab4cab3f9f66a5be
15904609267 <linhao54@huawei.com>
2024-12-09 13:37:50 +0800

    点击菜单项关闭，aboutToDisappear会回调两次
    Signed-off-by: 15904609267 <linhao54@huawei.com>

--- frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
+++ frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
@@ -1204,7 +1204,7 @@ void OverlayManager::PopMenuAnimation(const RefPtr<FrameNode>& menu, bool showPr
     auto wrapperPattern = menu->GetPattern<MenuWrapperPattern>();
     CHECK_NULL_VOID(wrapperPattern);
 
-    if (wrapperPattern->GetMenuStatus() == MenuStatus::ON_HIDE_ANIMATION) {
+    if (wrapperPattern->IsHide()) {
         return;
     }
 
