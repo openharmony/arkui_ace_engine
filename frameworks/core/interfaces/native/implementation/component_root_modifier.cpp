52d23ada694ded45da70ffdbab4f05ec604f56b8
Andrey Spirin <spirin.andrey@huawei-partners.com>
2024-12-17 15:40:09 +0300

    update root_modifier implementaions for feature branch
    Signed-off-by: Andrey Spirin <spirin.andrey@huawei-partners.com>
    Change-Id: I0e50fd9d88d859a45ebf8dd73d5143c5db219df1

--- frameworks/core/interfaces/native/implementation/component_root_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/component_root_modifier.cpp
@@ -14,6 +14,7 @@
  */
 
 #include "core/components_ng/base/frame_node.h"
+#include "core/components_ng/pattern/common_view/common_view_model_ng.h"
 #include "core/interfaces/native/utility/converter.h"
 #include "arkoala_api_generated.h"
 
@@ -22,7 +23,12 @@ namespace ComponentRootModifier {
 Ark_NativePointer ConstructImpl(Ark_Int32 id,
                                 Ark_Int32 flags)
 {
-    return nullptr;
+    auto frameNode = CommonViewModelNG::CreateFrameNode(id);
+    CHECK_NULL_RETURN(frameNode, nullptr);
+    frameNode->GetLayoutProperty()->UpdateAlignment(Alignment::TOP_LEFT);
+    frameNode->IncRefCount();
+    TAG_LOGD(AceLogTag::ACE_NATIVE_NODE, "createComponentRootNode: frameNode %{public}p", AceType::RawPtr(frameNode));
+    return AceType::RawPtr(frameNode);
 }
 } // ComponentRootModifier
 const GENERATED_ArkUIComponentRootModifier* GetComponentRootModifier()
