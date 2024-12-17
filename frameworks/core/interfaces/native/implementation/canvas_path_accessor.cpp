40642c4c8896f919be74a77dbce803b6263066e8
kovalevsergey <kovalev.sergey5@huawei-partners.com>
2024-12-17 15:49:10 +0300

    modifier update
    Signed-off-by: kovalevsergey <kovalev.sergey5@huawei-partners.com>
    Change-Id: I0aea9f8e354ce42d8070df2ae22b8c4ede11e3f3

--- frameworks/core/interfaces/native/implementation/canvas_path_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/canvas_path_accessor.cpp
@@ -16,7 +16,28 @@
 #include "core/components_ng/base/frame_node.h"
 #include "core/interfaces/native/utility/converter.h"
 #include "arkoala_api_generated.h"
-#include "canvas_path_peer.h"
+#include "canvas_path_accessor_peer_impl.h"
+
+namespace OHOS::Ace::NG {
+    struct Ark_Helper_Rect {
+    Ark_Number x;
+    Ark_Number y;
+    Ark_Number w;
+    Ark_Number h;
+};
+namespace Converter {
+template<>
+Rect Convert(const Ark_Helper_Rect& src)
+{
+    return Rect(
+        static_cast<double>(Converter::Convert<float>(src.x)),
+        static_cast<double>(Converter::Convert<float>(src.y)),
+        static_cast<double>(Converter::Convert<float>(src.w)),
+        static_cast<double>(Converter::Convert<float>(src.h))
+    );
+}
+} // namespace OHOS::Ace::NG::Converter
+} // namespace OHOS::Ace::NG
 
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace CanvasPathAccessor {
@@ -42,6 +63,22 @@ void ArcImpl(CanvasPathPeer* peer,
              const Ark_Number* endAngle,
              const Opt_Boolean* counterclockwise)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetHandler());
+    CHECK_NULL_VOID(x);
+    CHECK_NULL_VOID(y);
+    CHECK_NULL_VOID(radius);
+    CHECK_NULL_VOID(startAngle);
+    CHECK_NULL_VOID(endAngle);
+    auto clockwise = Converter::OptConvert<bool>(*counterclockwise);
+    ArcParam params;
+    params.x = Converter::Convert<float>(*x);
+    params.y = Converter::Convert<float>(*y);
+    params.radius = Converter::Convert<float>(*radius);
+    params.startAngle = Converter::Convert<float>(*startAngle);
+    params.endAngle = Converter::Convert<float>(*endAngle);
+    params.anticlockwise = clockwise ? clockwise.value() : false;
+    peer->GetHandler()->Arc(params);
 }
 void ArcToImpl(CanvasPathPeer* peer,
                const Ark_Number* x1,
@@ -50,6 +87,20 @@ void ArcToImpl(CanvasPathPeer* peer,
                const Ark_Number* y2,
                const Ark_Number* radius)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetHandler());
+    CHECK_NULL_VOID(x1);
+    CHECK_NULL_VOID(y1);
+    CHECK_NULL_VOID(x2);
+    CHECK_NULL_VOID(y2);
+    CHECK_NULL_VOID(radius);
+    ArcToParam params;
+    params.x1 = Converter::Convert<float>(*x1);
+    params.y1 = Converter::Convert<float>(*y1);
+    params.x2 = Converter::Convert<float>(*x2);
+    params.y2 = Converter::Convert<float>(*y2);
+    params.radius = Converter::Convert<float>(*radius);
+    peer->GetHandler()->ArcTo(params);
 }
 void BezierCurveToImpl(CanvasPathPeer* peer,
                        const Ark_Number* cp1x,
@@ -59,9 +110,26 @@ void BezierCurveToImpl(CanvasPathPeer* peer,
                        const Ark_Number* x,
                        const Ark_Number* y)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetHandler());
+    CHECK_NULL_VOID(cp1x);
+    CHECK_NULL_VOID(cp1y);
+    CHECK_NULL_VOID(cp2x);
+    CHECK_NULL_VOID(cp2y);
+    CHECK_NULL_VOID(x);
+    CHECK_NULL_VOID(y);
+    BezierCurveParam  params;
+    params.cp1x = Converter::Convert<float>(*cp1x);
+    params.cp1y = Converter::Convert<float>(*cp1y);
+    params.cp2x = Converter::Convert<float>(*cp2x);
+    params.cp2y = Converter::Convert<float>(*cp2y);
+    params.x = Converter::Convert<float>(*x);
+    params.y = Converter::Convert<float>(*y);
+    peer->GetHandler()->BezierCurveTo(params);
 }
 void ClosePathImpl(CanvasPathPeer* peer)
 {
+    peer->GetHandler()->ClosePath();
 }
 void EllipseImpl(CanvasPathPeer* peer,
                  const Ark_Number* x,
@@ -73,16 +141,46 @@ void EllipseImpl(CanvasPathPeer* peer,
                  const Ark_Number* endAngle,
                  const Opt_Boolean* counterclockwise)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetHandler());
+    CHECK_NULL_VOID(x);
+    CHECK_NULL_VOID(y);
+    CHECK_NULL_VOID(radiusX);
+    CHECK_NULL_VOID(radiusY);
+    CHECK_NULL_VOID(rotation);
+    CHECK_NULL_VOID(startAngle);
+    CHECK_NULL_VOID(endAngle);
+    CHECK_NULL_VOID(counterclockwise);
+    auto clockwise = Converter::OptConvert<bool>(*counterclockwise);
+    EllipseParam params;
+    params.x = Converter::Convert<float>(*x);
+    params.y = Converter::Convert<float>(*y);
+    params.radiusX = Converter::Convert<float>(*radiusX);
+    params.radiusY = Converter::Convert<float>(*radiusY);
+    params.rotation = Converter::Convert<float>(*rotation);
+    params.startAngle = Converter::Convert<float>(*startAngle);
+    params.endAngle = Converter::Convert<float>(*endAngle);
+    params.anticlockwise = clockwise ? clockwise.value() : false;
+    peer->GetHandler()->Ellipse(params);
 }
 void LineToImpl(CanvasPathPeer* peer,
                 const Ark_Number* x,
                 const Ark_Number* y)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetHandler());
+    CHECK_NULL_VOID(x);
+    CHECK_NULL_VOID(y);
+    peer->GetHandler()->LineTo(Converter::Convert<float>(*x), Converter::Convert<float>(*y));
 }
 void MoveToImpl(CanvasPathPeer* peer,
                 const Ark_Number* x,
                 const Ark_Number* y)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(x);
+    CHECK_NULL_VOID(y);
+    peer->GetHandler()->MoveTo(Converter::Convert<float>(*x), Converter::Convert<float>(*y));
 }
 void QuadraticCurveToImpl(CanvasPathPeer* peer,
                           const Ark_Number* cpx,
@@ -90,6 +188,18 @@ void QuadraticCurveToImpl(CanvasPathPeer* peer,
                           const Ark_Number* x,
                           const Ark_Number* y)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetHandler());
+    CHECK_NULL_VOID(cpx);
+    CHECK_NULL_VOID(cpy);
+    CHECK_NULL_VOID(x);
+    CHECK_NULL_VOID(y);
+    QuadraticCurveParam params;
+    params.cpx = Converter::Convert<float>(*cpx);
+    params.cpy = Converter::Convert<float>(*cpy);
+    params.x = Converter::Convert<float>(*x);
+    params.y = Converter::Convert<float>(*y);
+    peer->GetHandler()->QuadraticCurveTo(params);
 }
 void RectImpl(CanvasPathPeer* peer,
               const Ark_Number* x,
@@ -97,6 +207,20 @@ void RectImpl(CanvasPathPeer* peer,
               const Ark_Number* w,
               const Ark_Number* h)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetHandler());
+    CHECK_NULL_VOID(x);
+    CHECK_NULL_VOID(y);
+    CHECK_NULL_VOID(w);
+    CHECK_NULL_VOID(h);
+    auto arkRect = Ark_Helper_Rect {
+        .x = *x,
+        .y = *y,
+        .w = *w,
+        .h = *h
+    };
+    auto rect = Converter::Convert<Rect>(arkRect);
+    peer->GetHandler()->AddRect(rect);
 }
 } // CanvasPathAccessor
 const GENERATED_ArkUICanvasPathAccessor* GetCanvasPathAccessor()
@@ -117,5 +241,4 @@ const GENERATED_ArkUICanvasPathAccessor* GetCanvasPathAccessor()
     };
     return &CanvasPathAccessorImpl;
 }
-
 }
