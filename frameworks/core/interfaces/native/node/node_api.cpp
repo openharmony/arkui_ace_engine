a7efb51ebde377633957e8b624a4483ce56bd8e5
Caoruihong <crh.cao@huawei.com>
2024-12-20 18:09:43 +0800

    chore: ensure all fields are explicitly initialized to enhance code stability
            modified:   frameworks/core/interfaces/native/node/alphabet_indexer_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/badge_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/blank_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/button_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/calendar_picker_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/checkboxgroup_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/column_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/column_split_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/common_shape_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/counter_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/data_panel_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/divider_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/flex_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/form_component_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/frame_node_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/gauge_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/grid_col_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/grid_item_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/grid_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/grid_row_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/hyperlink_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/image_animator_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/line_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/marquee_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/menu_item_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/menu_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/nav_destination_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/nav_router_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/navigation_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/navigator_modifier.cpp
            modified:   frameworks/core/interfaces/native/node/node_api.cpp
            modified:   frameworks/core/interfaces/native/node/node_checkbox_modifier.cpp
    
    Signed-off-by: Caoruihong <crh.cao@huawei.com>

--- frameworks/core/interfaces/native/node/node_api.cpp
+++ frameworks/core/interfaces/native/node/node_api.cpp
@@ -133,16 +133,37 @@ void SetSupportedUIState(ArkUINodeHandle node, ArkUI_Int64 state)
 namespace NodeModifier {
 const ArkUIStateModifier* GetUIStateModifier()
 {
-    static const ArkUIStateModifier modifier = { GetUIState, SetSupportedUIState };
+    constexpr auto lineBegin = __LINE__; // don't move this line
+    static const ArkUIStateModifier modifier = {
+        .getUIState = GetUIState,
+        .setSupportedUIState = SetSupportedUIState,
+    };
+    constexpr auto lineEnd = __LINE__; // don't move this line
+    constexpr auto ifdefOverhead = 4; // don't modify this line
+    constexpr auto overHeadLines = 3; // don't modify this line
+    constexpr auto blankLines = 0; // modify this line accordingly
+    constexpr auto ifdefs = 0; // modify this line accordingly
+    constexpr auto initializedFieldLines = lineEnd - lineBegin - ifdefs * ifdefOverhead - overHeadLines - blankLines;
+    static_assert(initializedFieldLines == sizeof(modifier) / sizeof(void*),
+        "ensure all fields are explicitly initialized");
     return &modifier;
 }
 
 const CJUIStateModifier* GetCJUIStateModifier()
 {
+    constexpr auto lineBegin = __LINE__; // don't move this line
     static const CJUIStateModifier modifier = {
-        GetUIState,
-        SetSupportedUIState
+        .getUIState = GetUIState,
+        .setSupportedUIState = SetSupportedUIState,
     };
+    constexpr auto lineEnd = __LINE__; // don't move this line
+    constexpr auto ifdefOverhead = 4; // don't modify this line
+    constexpr auto overHeadLines = 3; // don't modify this line
+    constexpr auto blankLines = 0; // modify this line accordingly
+    constexpr auto ifdefs = 0; // modify this line accordingly
+    constexpr auto initializedFieldLines = lineEnd - lineBegin - ifdefs * ifdefOverhead - overHeadLines - blankLines;
+    static_assert(initializedFieldLines == sizeof(modifier) / sizeof(void*),
+        "ensure all fields are explicitly initialized");
     return &modifier;
 }
 } // namespace NodeModifier
