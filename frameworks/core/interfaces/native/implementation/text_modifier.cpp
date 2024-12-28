cc39babb5bfaab2e59f9b44f768a708aea1a1590
Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>
2024-12-28 13:21:26 +0300

    Resolved PR comments
    Signed-off-by: Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>

--- frameworks/core/interfaces/native/implementation/text_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/text_modifier.cpp
@@ -144,23 +144,27 @@ TextResponseType Convert(const Ark_TextResponseType& src)
 template<>
 SelectMenuParam Convert(const Ark_SelectionMenuOptions& src)
 {
-    SelectMenuParam selectMenuParam;
-    selectMenuParam.onAppear = [&src](int32_t start, int32_t end) {
-        auto optOnAppear = Converter::OptConvert<MenuOnAppearCallback>(src.onAppear);
-        if (optOnAppear.has_value()) {
-            Ark_Number arkStart = Converter::ArkValue<Ark_Number>(start);
-            Ark_Number arkEnd = Converter::ArkValue<Ark_Number>(end);
-            auto onAppearShared = std::make_shared<MenuOnAppearCallback>(optOnAppear.value());
-            CallbackHelper(*onAppearShared).Invoke(arkStart, arkEnd);
-        }
-    };
-    selectMenuParam.onDisappear = [&src]() {
-        auto optOnDisappear = Converter::OptConvert<Callback_Void>(src.onDisappear);
-        if (optOnDisappear.has_value()) {
-            auto onDisappearShared = std::make_shared<Callback_Void>(optOnDisappear.value());
-            CallbackHelper(*onDisappearShared).Invoke();
-        }
-    };
+    SelectMenuParam selectMenuParam = {.onAppear = [](int32_t start, int32_t end) {}, .onDisappear = []() {}};
+    auto optOnAppear = Converter::OptConvert<MenuOnAppearCallback>(src.onAppear);
+    if (optOnAppear.has_value()) {
+        selectMenuParam.onAppear = [&optOnAppear](int32_t start, int32_t end) {
+            if (optOnAppear.has_value()) {
+                Ark_Number arkStart = Converter::ArkValue<Ark_Number>(start);
+                Ark_Number arkEnd = Converter::ArkValue<Ark_Number>(end);
+                auto onAppearShared = std::make_shared<MenuOnAppearCallback>(optOnAppear.value());
+                CallbackHelper(*onAppearShared).Invoke(arkStart, arkEnd);
+            }
+        };
+    }
+    auto optOnDisappear = Converter::OptConvert<Callback_Void>(src.onDisappear);
+    if (optOnDisappear.has_value()) {
+        selectMenuParam.onDisappear = [&optOnDisappear]() {
+            if (optOnDisappear.has_value()) {
+                auto onDisappearShared = std::make_shared<Callback_Void>(optOnDisappear.value());
+                CallbackHelper(*onDisappearShared).Invoke();
+            }
+        };
+    }
     return selectMenuParam;
 }
 }
