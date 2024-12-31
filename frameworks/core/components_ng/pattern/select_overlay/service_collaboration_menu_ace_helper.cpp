c08f0bf04e7a9189df8986375fb75bd56d3f7057
xuyue <xuyue51@huawei.com>
2024-12-31 06:37:44 +0000

    update service_collaboration_menu_ace_helper.cpp.
    Signed-off-by: xuyue <xuyue51@huawei.com>

--- frameworks/core/components_ng/pattern/select_overlay/service_collaboration_menu_ace_helper.cpp
+++ frameworks/core/components_ng/pattern/select_overlay/service_collaboration_menu_ace_helper.cpp
@@ -760,7 +760,7 @@ void ServiceCollaborationAceCallback::RemovePopupNode()
     CHECK_NULL_VOID(info_->pattern.Upgrade()->GetHost());
     auto pattern = AceType::DynamicCast<RichEditorPattern>(info_->pattern.Upgrade());
     CHECK_NULL_VOID(pattern);
-    pattern->RegisiterCaretChangeListener(nullptr);
+    pattern->RegisterCaretChangeListener(nullptr);
     auto targetId = info_->pattern.Upgrade()->GetHost()->GetId();
     auto popupInfo = overlay->GetPopupInfo(targetId);
     popupInfo.markNeedUpdate = true;
@@ -821,7 +821,7 @@ int32_t ServiceCollaborationAceCallback::OnEvent(uint32_t code, uint32_t eventId
                 callback->info_ = nullptr;
             }
         };
-        pattern->RegisiterCaretChangeListener(std::move(func));
+        pattern->RegisterCaretChangeListener(std::move(func));
         auto row = CreateCustomPopUpNode(category, "");
         CHECK_NULL_RETURN(row, -1);
         ViewAbstract::BindPopup(popupParam, info_->pattern.Upgrade()->GetHost(), row);
@@ -851,11 +851,7 @@ int32_t ServiceCollaborationAceCallback::OnEvent(uint32_t code, uint32_t eventId
     CHECK_NULL_RETURN(toastPipeline, -1);
     auto overlay = toastPipeline->GetOverlayManager();
     CHECK_NULL_RETURN(overlay, -1);
-    auto toastInfo = NG::ToastInfo { .message = category,
-        .duration = TOAST_DURATION,
-        .alignment = -1
-    };
-    overlay->ShowToast(toastInfo, nullptr);
+    overlay->ShowToast({ .message = category, .duration = TOAST_DURATION, .alignment = -1 }, nullptr);
     info_ = nullptr;
     return 0;
 }
