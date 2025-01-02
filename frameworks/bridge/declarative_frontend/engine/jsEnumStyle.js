864e99de96cd822bf4ed912e219ff099a3eeeefe
wangtao <wangtao487@huawei.com>
2025-01-02 16:44:41 +0800

    Text BindSelectionMenu 支持菜单显隐回调，新增默认类型支持
    Signed-off-by: wangtao <wangtao487@huawei.com>
    Change-Id: I53c6bfd27d872c3456abf445973f691fa0518949

--- frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
+++ frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
@@ -3370,6 +3370,7 @@ let TextSpanType;
   TextSpanType[TextSpanType.TEXT = 0] = 'TEXT';
   TextSpanType[TextSpanType.IMAGE = 1] = 'IMAGE';
   TextSpanType[TextSpanType.MIXED = 2] = 'MIXED';
+  TextSpanType[TextSpanType.DEFAULT = 3] = 'DEFAULT';
 })(TextSpanType || (TextSpanType = {}));
 
 let TextResponseType;
@@ -3377,6 +3378,7 @@ let TextResponseType;
   TextResponseType[TextResponseType.RIGHT_CLICK = 0] = 'RIGHT_CLICK';
   TextResponseType[TextResponseType.LONG_PRESS = 1] = 'LONG_PRESS';
   TextResponseType[TextResponseType.SELECT = 2] = 'SELECT';
+  TextResponseType[TextResponseType.DEFAULT = 3] = 'DEFAULT';
 })(TextResponseType || (TextResponseType = {}));
 
 let MarqueeState;
--- frameworks/bridge/declarative_frontend/jsview/js_text.h
+++ frameworks/bridge/declarative_frontend/jsview/js_text.h
@@ -77,6 +77,8 @@ public:
     static void SetOnTextSelectionChange(const JSCallbackInfo& info);
     static void ParseMenuParam(
         const JSCallbackInfo& info, const JSRef<JSObject>& menuOptions, NG::SelectMenuParam& menuParam);
+    static std::function<void(int32_t, int32_t)> ParseMenuCallback(const WeakPtr<NG::FrameNode>& frameNode,
+        const JSRef<JSObject>& menuOptions, const JSCallbackInfo& info, const std::string& name);
     static void JsClip(const JSCallbackInfo& info);
     static void SetFontFeature(const JSCallbackInfo &info);
     static void SetForegroundColor(const JSCallbackInfo& info);
