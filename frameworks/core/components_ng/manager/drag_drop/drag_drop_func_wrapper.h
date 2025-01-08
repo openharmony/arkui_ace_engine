9c7c86e144852f00f1defc868e52a4ff14cedadf
zenix_zxy <zhengxingyou1@huawei.com>
2025-01-08 14:10:41 +0800

    new fwk func add
    Signed-off-by: zenix_zxy <zhengxingyou1@huawei.com>
    Change-Id: Iffa4583e6afcf0eb5ff29d71db6e42655300f4b4

--- frameworks/core/components_ng/manager/drag_drop/drag_drop_func_wrapper.h
+++ frameworks/core/components_ng/manager/drag_drop/drag_drop_func_wrapper.h
@@ -65,6 +65,11 @@ public:
         const RefPtr<FrameNode>& targetNode, const RefPtr<FrameNode>& frameNode, float width, float height);
     static OffsetF GetPointRelativeToMainWindow(const Point& point);
     static void HandleOnDragEvent(std::shared_ptr<OHOS::Ace::NG::ArkUIInteralDragAction> dragAction);
+    static bool IsSelectedItemNode(const RefPtr<UINode>& uiNode);
+    static bool IsBelongToMultiItemNode(const RefPtr<FrameNode>& frameNode);
+    static bool CheckIsNeedGather(const RefPtr<FrameNode>& frameNode);
+    static RefPtr<FrameNode> FindItemParentNode(const RefPtr<FrameNode>& frameNode);
+    static RefPtr<PixelMap> GetGatherNodePreviewPixelMap(const RefPtr<FrameNode>& frameNode);
 
 private:
     static void GetPointerEventAction(const TouchEvent& touchPoint, DragPointerEvent& event);
