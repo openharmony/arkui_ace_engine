82b0bc0216395d8e54a6cf264d825ee60eeccafa
liuwei <liuwei846@h-partners.com>
2025-01-09 10:27:38 +0800

    ArcScroll删除无用的代码
    Signed-off-by: liuwei <liuwei846@h-partners.com>

--- frameworks/core/components_ng/pattern/arc_scroll/inner/arc_scroll_bar.cpp
+++ frameworks/core/components_ng/pattern/arc_scroll/inner/arc_scroll_bar.cpp
@@ -18,7 +18,6 @@
 namespace OHOS::Ace::NG {
 namespace {
 inline constexpr double HALF = 0.5;
-constexpr int32_t BAR_SHRINK_DELAY_DURATION = 2000; // 2000ms
 } // namespace
 
 bool ArcScrollBar::InBarTouchRegion(const Point& point) const
@@ -161,24 +160,6 @@ float ArcScrollBar::CalcPatternOffset(float scrollBarOffset) const
     auto mainSize = GetViewPortSize().Height();
     auto offset = -scrollBarOffset * (GetEstimatedHeigh() - mainSize) / (GetBarRegionSize() - activeRectLength);
     return offset;
-}
-
-void ArcScrollBar::ScheduleShrinkDelayTask()
-{
-    if (GetDisplayMode() != DisplayMode::AUTO) {
-        shrinkDelayTask_.Cancel();
-        auto context = PipelineContext::GetCurrentContext();
-        CHECK_NULL_VOID(context);
-        auto taskExecutor = context->GetTaskExecutor();
-        CHECK_NULL_VOID(taskExecutor);
-        shrinkDelayTask_.Reset([weak = WeakClaim(this)] {
-            auto scrollBar = weak.Upgrade();
-            CHECK_NULL_VOID(scrollBar);
-            scrollBar->PlayScrollBarShrinkAnimation();
-        });
-        taskExecutor->PostDelayedTask(shrinkDelayTask_, TaskExecutor::TaskType::UI, BAR_SHRINK_DELAY_DURATION,
-            "ArkUIScrollBarInnerShrinkAnimation");
-    }
 }
 
 void ArcScrollBar::CalcReservedHeight()
