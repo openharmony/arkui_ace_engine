88401405f81501fdc2ad046eecc15668f7ab76b1
AnBetter <wangguan27@huawei.com>
2025-01-09 22:18:04 +0800

    Progress组件支持CAPI补充用例及代码优化
    Signed-off-by: AnBetter <wangguan27@huawei.com>

--- frameworks/core/interfaces/arkoala/arkoala_api.h
+++ frameworks/core/interfaces/arkoala/arkoala_api.h
@@ -1469,6 +1469,13 @@ struct ArkUIAccessibilityValue {
     ArkUIOptionalCharPtr text;
 };
 
+struct ArkUIProgressLinearStyleOption {
+    bool scanEffectEnable;
+    bool smoothEffectEnable;
+    float strokeWidth;
+    float strokeRadius;
+};
+
 struct ArkUITranslateOption {
     ArkUI_Float32 x;
     ArkUI_Float32 y;
@@ -3353,6 +3360,7 @@ struct ArkUIProgressModifier {
     void (*setProgressInitialize)(
         ArkUINodeHandle node, ArkUI_Float32 value, ArkUI_Float32 total, ArkUI_Int32 progressStyle);
     void (*resetProgressInitialize)(ArkUINodeHandle node);
+    void (*getProgressLinearStyle)(ArkUINodeHandle node, ArkUIProgressLinearStyleOption& option);
 };
 
 struct ArkUIPluginModifier {
