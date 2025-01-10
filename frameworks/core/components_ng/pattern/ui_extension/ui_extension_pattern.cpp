60ee2250be969f92036d80f75fc1a8bbe309dd63
zhanghangkai@huawei.com <zhanghangkai@huawei.com>
2025-01-10 19:18:09 +0800

    修复三折叠M态下windowscene下UEC的无障碍布局获取问题
    Signed-off-by: zhanghangkai@huawei.com <zhanghangkai@huawei.com>
    Change-Id: I8c6e50480dce5d1111da2c3860952a164fd58bf9

--- frameworks/core/components_ng/pattern/ui_extension/ui_extension_pattern.cpp
+++ frameworks/core/components_ng/pattern/ui_extension/ui_extension_pattern.cpp
@@ -1069,7 +1069,7 @@ void UIExtensionPattern::DispatchDisplayArea(bool isForce)
     CHECK_NULL_VOID(sessionWrapper_);
     auto host = GetHost();
     CHECK_NULL_VOID(host);
-    auto [displayOffset, err] = host->GetPaintRectGlobalOffsetWithTranslate();
+    auto [displayOffset, err] = host->GetPaintRectGlobalOffsetWithTranslate(false, true);
     auto geometryNode = host->GetGeometryNode();
     CHECK_NULL_VOID(geometryNode);
     auto renderContext = host->GetRenderContext();
