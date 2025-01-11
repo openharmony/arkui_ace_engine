98690c45aef3d0c768393ed22d3a855804a1f01e
dongzhipeng <dongzhipeng5@huawei.com>
2025-01-12 01:59:23 +0800

    修改tdd
    Signed-off-by: dongzhipeng <dongzhipeng5@huawei.com>

--- frameworks/core/components_ng/pattern/security_component/security_component_model_ng.cpp
+++ frameworks/core/components_ng/pattern/security_component/security_component_model_ng.cpp
@@ -107,7 +107,7 @@ RefPtr<FrameNode> SecurityComponentModelNG::InitChild(const std::string& tag, in
             SetInvisibleBackgroundButton(buttonNode);
         }
         frameNode->AddChild(buttonNode);
-        if (style.symbolIcon != static_cast<uint32_t>(SecurityComponentIconStyle::ICON_NULL)) {
+        if (style.symbolIcon && style.symbolIcon != static_cast<uint32_t>(SecurityComponentIconStyle::ICON_NULL)) {
             auto symbolIcon = FrameNode::CreateFrameNode(
                 V2::SYMBOL_ETS_TAG, ElementRegister::GetInstance()->MakeUniqueId(), AceType::MakeRefPtr<TextPattern>());
             SetDefaultSymbolIconStyle(symbolIcon, style.symbolIcon, isButtonVisible);
