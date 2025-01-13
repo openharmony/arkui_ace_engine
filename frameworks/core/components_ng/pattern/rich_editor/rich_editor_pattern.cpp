6a69564b7d413ae46771a58c080644aa912362ed
zyx0121 <zhangyingxiang2@huawei.com>
2025-01-13 11:01:31 +0800

    修复预上屏范围内鼠标点击不能切换光标
    Signed-off-by: zyx0121 <zhangyingxiang2@huawei.com>

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -7055,6 +7055,7 @@ bool RichEditorPattern::IsScrollBarPressed(const MouseInfo& info)
 void RichEditorPattern::HandleMouseLeftButtonMove(const MouseInfo& info)
 {
     ACE_SCOPED_TRACE("RichEditorHandleMouseLeftButtonMove");
+    CHECK_NULL_VOID(!IsPreviewTextInputting());
     if (blockPress_) {
         ACE_SCOPED_TRACE("RichEditorUpdateDragBoxes");
         dragBoxes_ = GetTextBoxes();
@@ -7116,7 +7117,8 @@ void RichEditorPattern::HandleMouseLeftButtonPress(const MouseInfo& info)
     auto frameNodeRange = GetSpanRangeByLocalOffset(info.GetLocalLocation());
     bool frameNodeSelected = textSelector_.ContainsRange(frameNodeRange);
     bool pressFrameNode = !frameNodeSelected && InRangeRect(info.GetGlobalLocation(), frameNodeRange);
-    if (IsScrollBarPressed(info) || BetweenSelectedPosition(info.GetGlobalLocation()) || pressFrameNode) {
+    if (IsPreviewTextInputting() || IsScrollBarPressed(info) || BetweenSelectedPosition(info.GetGlobalLocation()) ||
+        pressFrameNode) {
         blockPress_ = true;
         return;
     }
@@ -7165,6 +7167,7 @@ void RichEditorPattern::HandleMouseLeftButtonRelease(const MouseInfo& info)
 {
     blockPress_ = false;
     leftMousePress_ = false;
+    CHECK_NULL_VOID(!IsPreviewTextInputting());
     auto oldMouseStatus = mouseStatus_;
     mouseStatus_ = MouseStatus::RELEASED;
     isMouseSelect_ = false;
@@ -7202,7 +7205,6 @@ void RichEditorPattern::HandleMouseLeftButtonRelease(const MouseInfo& info)
 
 void RichEditorPattern::HandleMouseLeftButton(const MouseInfo& info)
 {
-    CHECK_NULL_VOID(!IsPreviewTextInputting());
     if (info.GetAction() == MouseAction::MOVE) {
         HandleMouseLeftButtonMove(info);
     } else if (info.GetAction() == MouseAction::PRESS) {
