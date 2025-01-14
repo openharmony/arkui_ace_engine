24adaa314a90dfbeb5b865e324ebc961e9957186
dongzhipeng <dongzhipeng5@huawei.com>
2025-01-14 12:37:50 +0800

    tdd Signed-off-by: dongzhipeng <dongzhipeng5@huawei.com>

--- test/mock/core/pipeline/mock_pipeline_context.cpp
+++ test/mock/core/pipeline/mock_pipeline_context.cpp
@@ -27,6 +27,7 @@
 #include "core/components_ng/pattern/stage/stage_pattern.h"
 #include "core/pipeline/pipeline_base.h"
 #include "core/pipeline_ng/pipeline_context.h"
+#include "test/mock/base/mock_task_executor.h"
 
 namespace OHOS::Ace {
 
