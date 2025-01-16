09f29d5209f94101a1f74e34e7e16f5ee4d2f86b
胡清云 <huqingyun1@huawei.com>
2025-01-16 16:06:32 +0800

    onAreaChange监听适配Edges、LocalizedEdges方式的position属性
    Signed-off-by: 胡清云 <huqingyun1@huawei.com>

--- frameworks/core/components_ng/render/adapter/rosen_render_context.h
+++ frameworks/core/components_ng/render/adapter/rosen_render_context.h
@@ -432,6 +432,9 @@ public:
     void UpdateWindowBlur() override;
     void MarkUiFirstNode(bool isUiFirstNode) override;
 
+    OffsetF GetRectOffsetWithPositionEdges(
+        const EdgesParam& positionEdges, float widthPercentReference, float heightPercentReference) override;
+
 protected:
     void OnBackgroundImageUpdate(const ImageSourceInfo& src) override;
     void OnBackgroundImageRepeatUpdate(const ImageRepeat& imageRepeat) override;
@@ -583,8 +586,6 @@ protected:
         const Dimension& parentPaddingTop, float widthPercentReference, float heightPercentReference);
     OffsetF GetRectOffsetWithOffsetEdges(
         const EdgesParam& offsetEdges, float widthPercentReference, float heightPercentReference);
-    OffsetF GetRectOffsetWithPositionEdges(
-        const EdgesParam& positionEdges, float widthPercentReference, float heightPercentReference);
 
     void InitEventClickEffect();
     RefPtr<Curve> UpdatePlayAnimationValue(const ClickEffectLevel& level, float& scaleValue);
