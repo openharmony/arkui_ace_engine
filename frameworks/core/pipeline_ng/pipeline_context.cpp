f90afa9d5a4404fe295f29d124b8930d882b9691
yihao_lin <linyihao4@huawei.com>
2025-01-17 14:51:59 +0800

    【挑单】焦点上报获焦原因
    Signed-off-by: yihao_lin <linyihao4@huawei.com>
    Change-Id: Id9fa1561890b9fb08e2d321a9460aa3ef429f6da

--- frameworks/core/pipeline_ng/pipeline_context.cpp
+++ frameworks/core/pipeline_ng/pipeline_context.cpp
@@ -4683,7 +4683,8 @@ void PipelineContext::FlushFrameCallback(uint64_t nanoTimestamp)
 
 void PipelineContext::RegisterFocusCallback()
 {
-    focusManager_->AddFocusListener([](const WeakPtr<FocusHub>& last, const RefPtr<FocusHub>& current) {
+    focusManager_->AddFocusListener([](const WeakPtr<FocusHub>& last, const RefPtr<FocusHub>& current,
+        FocusReason focusReason) {
         CHECK_NULL_VOID(current);
         auto node = current->GetFrameNode();
         CHECK_NULL_VOID(node);
