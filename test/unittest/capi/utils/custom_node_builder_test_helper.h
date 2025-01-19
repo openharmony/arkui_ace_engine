b45cece942b6c369d7c85cb4ac3f47942a52bcff
Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>
2025-01-20 00:16:29 +0300

    Fixed CI
    Signed-off-by: Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>

--- test/unittest/capi/utils/custom_node_builder_test_helper.h
+++ test/unittest/capi/utils/custom_node_builder_test_helper.h
@@ -11,10 +11,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+*/
 
- #ifndef CUSTOM_NODE_BUILDER_TEST_HELPER_H
- #define CUSTOM_NODE_BUILDER_TEST_HELPER_H
+#ifndef CUSTOM_NODE_BUILDER_TEST_HELPER_H
+#define CUSTOM_NODE_BUILDER_TEST_HELPER_H
+
 
 #include <utility>
 #include "gtest/gtest.h"
@@ -83,3 +84,4 @@ private:
     int callbackCounter_ = 0;
 };
 }
+#endif // CUSTOM_NODE_BUILDER_TEST_HELPER_H
