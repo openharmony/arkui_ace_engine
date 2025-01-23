f43cb39e90ceeb7d9def55f479fbd7931351b69b
zhanghang <zhanghang160@huawei-partners.com>
2025-01-23 10:32:17 +0800

    fix:solve slider mode change bug
    Signed-off-by: zhanghang <zhanghang160@huawei-partners.com>

--- frameworks/core/components_ng/pattern/slider/slider_pattern.h
+++ frameworks/core/components_ng/pattern/slider/slider_pattern.h
@@ -165,6 +165,10 @@ public:
 
     void SetSliderValue(double value, int32_t mode);
     void InitAccessibilityVirtualNodeTask();
+    void SetIsAccessibilityOn(bool value)
+    {
+        isAccessibilityOn_ = value;
+    }
     void PlayHapticFeedback(bool isShowSteps, float step, float oldValue);
     
 #ifdef SUPPORT_DIGITAL_CROWN
@@ -335,6 +339,7 @@ private:
     void SetStepPointAccessibilityVirtualNode(
         const RefPtr<FrameNode>& pointNode, const SizeF& size, const PointF& point, const std::string& txt);
     void SendAccessibilityValueEvent(int32_t mode);
+    void ClearSliderVirtualNode();
     void InitOrRefreshSlipFactor();
     RefPtr<PanEvent> CreatePanEvent();
 
@@ -412,6 +417,8 @@ private:
     RefPtr<FrameNode> imageFrameNode_;
     std::function<void(bool)> isFocusActiveUpdateEvent_;
     bool isFocusActive_ = false;
+    SliderModel::SliderMode sliderMode_ = SliderModel::SliderMode::OUTSET;
+    bool isAccessibilityOn_ = AceApplicationInfo::GetInstance().IsAccessibilityEnabled();
 
     std::shared_ptr<AccessibilitySAObserverCallback> accessibilitySAObserverCallback_;
     RefPtr<FrameNode> parentAccessibilityNode_;
