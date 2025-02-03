960ab768dea1e70f68418f990b40f9ed6e785c64
Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>
2025-02-03 10:29:46 +0300

    Tests implementation finished
    Signed-off-by: Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>

--- test/unittest/capi/modifiers/image_modifier_test2.cpp
+++ test/unittest/capi/modifiers/image_modifier_test2.cpp
@@ -22,6 +22,8 @@
 #include "point_light_test.h"
 #include "generated/type_helpers.h"
 #include "arkoala_api_generated.h"
+#include "test/unittest/capi/stubs/ace_pixelmap_stub.h"
+#include "core/components/image/image_theme.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -31,87 +33,121 @@ using namespace OHOS::Ace::NG::PointLight;
 namespace OHOS::Ace::NG {
 namespace Converter {
 
-}
 } // OHOS::Ace::NG::Converter
 
 namespace  {
-    
-    // const auto ATTRIBUTE_FILL_COLOR_NAME = "fillColor"; 
-    // const auto ATTRIBUTE_FILL_COLOR_DEFAULT_VALUE = "#FF000000";
+    const auto ATTRIBUTE_ALT_NAME = "alt";
+    const auto ATTRIBUTE_ALT_DEFAULT_VALUE = "";
+
+    const std::string CHECK_RESOURCE_THEME_STR("www.example.test/image_source.png");
+    const std::string CHECK_RESOURCE_LOCAL_STR("path/to/image/image_source.png");
+    const std::string IMAGE_RESOURCE_THEME_KEY = "image_source";
+    const auto IMAGE_RES_ID = 555;
 
 } // namespace
 
-class ImageModifierTest : public ModifierTestBase<GENERATED_ArkUIImageModifier,
+class ImageModifierTest2 : public ModifierTestBase<GENERATED_ArkUIImageModifier,
     &GENERATED_ArkUINodeModifiers::getImageModifier,
     GENERATED_ARKUI_IMAGE> {
 public:
     static void SetUpTestCase()
     {
         ModifierTestBase::SetUpTestCase();
-        for (auto&& res : Fixtures::resourceInitTable) {
-            AddResource(std::get<0>(res), std::get<2>(res)); // 2 - index of resource
-            AddResource(std::get<1>(res), std::get<2>(res)); // 2 - index of resource
-        }
-
-        for (auto& [id, strid, res] : resourceInitTable) {
-            AddResource(id, res);
-            AddResource(strid, res);
-        }
-        fullAPI_->setArkUIEventsAPI(&EventsTracker::eventsApiImpl);
+        auto themeStyle = SetupThemeStyle(THEME_PATTERN_IMAGE);
+        SetupTheme<ImageTheme>();
+        AddResource(IMAGE_RESOURCE_THEME_KEY, CHECK_RESOURCE_THEME_STR);
+        AddResource(IMAGE_RES_ID, CHECK_RESOURCE_THEME_STR);
     }
 };
-// /*
-//  * @tc.name: setFillColorTestDefaultValues
-//  * @tc.desc: Check functionality of ImageModifier.setFillColor
-//  * @tc.type: FUNC
-//  */
-// HWTEST_F(ImageModifierTest, setFillColorTestDefaultValues, TestSize.Level1)
-// {
-//     std::unique_ptr<JsonValue> jsonValue = GetJsonValue(node_);
-//     std::string resultStr;
 
-//     resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_FILL_COLOR_NAME);
-//     EXPECT_EQ(resultStr, ATTRIBUTE_FILL_COLOR_DEFAULT_VALUE);
-// }
+/*
+ * @tc.name: setAltTestDefaultValues
+ * @tc.desc: Check functionality of ImageModifier.setAlt
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageModifierTest2, setAltTestDefaultValues, TestSize.Level1)
+{
+    auto jsonValue = GetJsonValue(node_);
+    auto resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_ALT_NAME);
+    EXPECT_EQ(resultStr, ATTRIBUTE_ALT_DEFAULT_VALUE);
+}
 
-// /*
-//  * @tc.name: setFillColorTestValidValues
-//  * @tc.desc: Check functionality of ImageModifier.setFillColor
-//  * @tc.type: FUNC
-//  */
-// HWTEST_F(ImageModifierTest, setFillColorTestValidValues, TestSize.Level1)
-// {
-//     auto checkValue = [this](const std::string& input, const Ark_ResourceColor& value, const std::string& expectedStr) {
-//         modifier_->setFillColor(node_, &value);
-//         auto jsonValue = GetJsonValue(node_);
-//         auto resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_FILL_COLOR_NAME);
-//         EXPECT_EQ(resultStr, expectedStr) << "Passed value is: " << input;
-//     };
+/*
+ * @tc.name: setAlt_ArkStringUnion_Test
+ * @tc.desc: Check functionality of ImageModifier.setAlt
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageModifierTest2, setAlt_ArkStringUnion_Test, TestSize.Level1)
+{
+    ASSERT_NE(modifier_->setAlt, nullptr);
+    auto frameNode = reinterpret_cast<FrameNode*>(node_);
+    ASSERT_NE(frameNode, nullptr);
 
-//     for (auto&& value : Fixtures::testFixtureColorsStrValidValues) {
-//         checkValue(std::get<0>(value), ArkUnion<Ark_ResourceColor, Ark_String>(std::get<1>(value)),
-//             std::get<2>(value));
-//     }
-//     for (auto&& value : Fixtures::testFixtureColorsStrInvalidValues) {
-//         checkValue(std::get<0>(value), ArkUnion<Ark_ResourceColor,
-//             Ark_String>(std::get<1>(value)), OPACITY_COLOR);
-//     }
-//     for (auto&& value : Fixtures::testFixtureColorsNumValidValues) {
-//         checkValue(std::get<0>(value), ArkUnion<Ark_ResourceColor, Ark_Number>(std::get<1>(value)),
-//             std::get<2>(value));
-//     }
-//     for (auto&& value : Fixtures::testFixtureColorsResValidValues) {
-//         checkValue(std::get<0>(value), ArkUnion<Ark_ResourceColor, Ark_Resource>(std::get<1>(value)),
-//             std::get<2>(value));
-//     }
-//     for (auto&& value : Fixtures::testFixtureColorsEnumValidValues) {
-//         checkValue(std::get<0>(value), ArkUnion<Ark_ResourceColor, Ark_Color>(std::get<1>(value)),
-//             std::get<2>(value));
-//     }
-//     for (auto&& value : Fixtures::testFixtureColorsEnumInvalidValues) {
-//         checkValue(std::get<0>(value), ArkUnion<Ark_ResourceColor, Ark_Color>(std::get<1>(value)),
-//             ATTRIBUTE_FILL_COLOR_DEFAULT_VALUE);
-//     }
-// }
+    std::string expectedStr = CHECK_RESOURCE_LOCAL_STR;
+    auto inputStr = Converter::ArkUnion<Ark_Union_String_Resource_PixelMap, Ark_String>(
+        Converter::ArkValue<Ark_String>(expectedStr));
+    modifier_->setAlt(frameNode, &inputStr);
+    auto fullJson = GetJsonValue(node_);
+    auto resultStr = GetAttrValue<std::string>(fullJson, ATTRIBUTE_ALT_NAME);
+    EXPECT_EQ(resultStr, expectedStr);
+}
+
+/*
+ * @tc.name: setAlt_ArkResourceUnion_Test
+ * @tc.desc: Check functionality of ImageModifier.setAlt
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageModifierTest2, setAlt_ArkResourceUnion_Test, TestSize.Level1)
+{
+    ASSERT_NE(modifier_->setAlt, nullptr);
+    auto frameNode = reinterpret_cast<FrameNode*>(node_);
+    ASSERT_NE(frameNode, nullptr);
+
+    std::string expectedStr = CHECK_RESOURCE_THEME_STR;
+    std::vector<std::string> paramsVector = {IMAGE_RESOURCE_THEME_KEY};
+    Converter::ArkArrayHolder<Array_String> paramsStringArrayHolder(paramsVector);
+    Array_String paramsArkArrayValues = paramsStringArrayHolder.ArkValue();
+    Ark_Resource expectedArkResource = {
+        .bundleName = Converter::ArkValue<Ark_String>("testBundle"),
+        .moduleName = Converter::ArkValue<Ark_String>("testModule"),
+        .id = Converter::ArkValue<Ark_Number>(IMAGE_RES_ID),
+        .params = Converter::ArkValue<Opt_Array_String>(paramsArkArrayValues),
+        .type = Converter::ArkValue<Opt_Number>(10003/*ResourceType::STRING*/)
+    };
+    auto inputArkResource = Converter::ArkUnion<Ark_Union_String_Resource_PixelMap, Ark_Resource>(expectedArkResource);
+    modifier_->setAlt(frameNode, &inputArkResource);
+    auto fullJson = GetJsonValue(node_);
+    auto resultStr = GetAttrValue<std::string>(fullJson, ATTRIBUTE_ALT_NAME);
+    EXPECT_EQ(resultStr, expectedStr);
+}
+
+/*
+ * @tc.name: setAlt_PixelMapUnion_Test
+ * @tc.desc: Check functionality of ImageModifier.setAlt
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageModifierTest2, setAlt_PixelMapUnion_Test, TestSize.Level1)
+{
+    ASSERT_NE(modifier_->setAlt, nullptr);
+    auto frameNode = reinterpret_cast<FrameNode*>(node_);
+    ASSERT_NE(frameNode, nullptr);
+
+    Ace::RefPtr<Ace::PixelMap> expectedPixelMapRefPtr = AceType::MakeRefPtr<Ace::PixelMapStub>();
+    PixelMapPeer pixelMapPeer;
+    pixelMapPeer.pixelMap = expectedPixelMapRefPtr;
+    Ark_PixelMap expectedPixelMap{.ptr = reinterpret_cast<Ark_NativePointer>(&pixelMapPeer)};
+    auto inputArkPixelMap = Converter::ArkUnion<Ark_Union_String_Resource_PixelMap, Ark_PixelMap>(expectedPixelMap);
+    modifier_->setAlt(frameNode, &inputArkPixelMap);
+
+    auto imageLayoutProperty = frameNode->GetLayoutPropertyPtr<ImageLayoutProperty>();
+    ASSERT_NE(imageLayoutProperty, nullptr);
+
+    std::optional<ImageSourceInfo> alt = imageLayoutProperty->GetAlt();
+    EXPECT_TRUE(alt.has_value());
+    EXPECT_TRUE(alt->IsPixmap());
+
+    auto resultPixelMap = alt->GetPixmap();
+    EXPECT_EQ(resultPixelMap.GetRawPtr(), expectedPixelMapRefPtr.GetRawPtr());
+}
 
 } // namespace OHOS::Ace::NG
