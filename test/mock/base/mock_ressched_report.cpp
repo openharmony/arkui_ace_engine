163139fb77d50d74b81da870a4f020c1c251d2e9
dujingcheng <dujingcheng@huawei.com>
2025-02-06 21:24:27 +0800

    resreport
    Signed-off-by: dujingcheng <dujingcheng@huawei.com>
    Change-Id: Ia16bf9e7829fd8914589f2a2f1d5b66fcc8eb55f

--- test/mock/base/mock_ressched_report.cpp
+++ test/mock/base/mock_ressched_report.cpp
@@ -32,7 +32,7 @@ void ResSchedReport::ResSchedDataReport(
     uint32_t resType, int32_t value, const std::unordered_map<std::string, std::string>& payload)
 {}
 
-void ResSchedReport::OnTouchEvent(const TouchEvent& touchEvent) {}
+void ResSchedReport::OnTouchEvent(const TouchEvent& touchEvent, const ReportConfig& config) {}
 
 ResSchedReportScope::ResSchedReportScope(
     const std::string& name, const std::unordered_map<std::string, std::string>& param)
