fa402bb44a4a7a34f3a757fd7a9bb19414695b24
tuzhilkin.ivan <tuzhilkin.ivan@huawei.com>
2025-02-06 17:01:07 +0300

    SubmitEventAccessor - Fix review comments
    Signed-off-by: tuzhilkin.ivan <tuzhilkin.ivan@huawei.com>
    Change-Id: I4291c3f05ec7684d9ede07bba7e5a125a0351540

--- frameworks/core/interfaces/native/implementation/submit_event_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/submit_event_accessor.cpp
@@ -34,11 +34,13 @@ Ark_NativePointer GetFinalizerImpl()
 void KeepEditableStateImpl(SubmitEventPeer* peer)
 {
     CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetEventInfo());
     peer->GetEventInfo()->SetKeepEditable(true);
 }
 void GetTextImpl(SubmitEventPeer* peer)
 {
     CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetEventInfo());
     auto text = peer->GetEventInfo()->GetText();
     LOGE("Arkoala method SubmitEventAccessor.GetTextImpl has incorrect return type");
 }
@@ -46,6 +48,7 @@ void SetTextImpl(SubmitEventPeer* peer,
                  const Ark_String* text)
 {
     CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(peer->GetEventInfo());
     auto string = Converter::Convert<std::u16string>(*text);
     peer->GetEventInfo()->SetText(string);
 }
