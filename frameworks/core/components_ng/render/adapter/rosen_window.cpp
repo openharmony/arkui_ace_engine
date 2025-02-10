310adc8acef5c69208e67cd105dc931e6221e724
fangzhiyuan <fangzhiyuan5@huawei.com>
2025-02-10 13:09:43 +0000

    回退 'Pull Request !54883 : root未初始化时拦截vsync请求'

--- frameworks/core/components_ng/render/adapter/rosen_window.cpp
+++ frameworks/core/components_ng/render/adapter/rosen_window.cpp
@@ -135,7 +135,6 @@ void RosenWindow::RequestFrame()
     CHECK_NULL_VOID(onShow_);
     CHECK_RUN_ON(UI);
     CHECK_NULL_VOID(!isRequestVsync_);
-    CHECK_NULL_VOID(isRootInitialize_);
     auto taskExecutor = taskExecutor_.Upgrade();
     if (rsWindow_) {
         isRequestVsync_ = true;
