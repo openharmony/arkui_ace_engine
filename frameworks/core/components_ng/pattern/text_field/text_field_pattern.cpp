9e0f977aa4b5ca4f853044b4846e5373c7721f9b
zhoukechen <zhoukechen2@huawei.com>
2025-02-10 16:38:13 +0800

    fix 横竖屏切换坐标拿的不对
    Signed-off-by: zhoukechen <zhoukechen2@huawei.com>
    Change-Id: I7fa0ce8f257789fe46b3a7449065ac4fa82ae477

--- frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
+++ frameworks/core/components_ng/pattern/text_field/text_field_pattern.cpp
@@ -8762,7 +8762,7 @@ void TextFieldPattern::OnWindowSizeChanged(int32_t width, int32_t height, Window
                 }
             }
         },
-        TaskExecutor::TaskType::UI, "ArkUITextFieldOnWindowSizeChangedRotation", PriorityType::VIP);
+        TaskExecutor::TaskType::UI, "ArkUITextFieldOnWindowSizeChangedRotation");
 }
 
 void TextFieldPattern::PasswordResponseKeyEvent()
