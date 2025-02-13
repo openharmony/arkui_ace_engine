3bd7f3d9b979b71174379367c1403a9b8997b5d7
evstigneevroman_9cd1 <evstigneev.roman1@huawei-partners.com>
2025-02-13 16:57:04 +0300

    full implement modifier and unittests
    Signed-off-by: evstigneevroman_9cd1 <evstigneev.roman1@huawei-partners.com>
    Change-Id: I9efb39e246a52c323691cd38f45a0b315026f8cf

--- test/unittest/capi/modifiers/media_cached_image_modifier_test.cpp
+++ test/unittest/capi/modifiers/media_cached_image_modifier_test.cpp
@@ -18,6 +18,7 @@
 
 #include "modifier_test_base.h"
 #include "modifiers_test_utils.h"
+#include "core/interfaces/native/implementation/pixel_map_peer.h"
 #include "core/interfaces/native/utility/converter.h"
 #include "core/interfaces/native/utility/reverse_converter.h"
 #include "core/components_ng/pattern/image/image_event_hub.h"
@@ -26,39 +27,16 @@
 
 using namespace testing;
 using namespace testing::ext;
+using namespace OHOS::Ace::NG::Converter;
 
 namespace OHOS::Ace::NG {
-/* 
-namespace Converter {
-template<>
-LoadImageFailEvent Convert(const Ark_ImageError& info)
-{
-    auto width = Convert<float>(info.componentWidth);
-    auto height = Convert<float>(info.componentHeight);
-    auto error = Convert<std::string>(info.message);
-    LoadImageFailEvent event(width, height, error);
-    return event;
-}
-} // OHOS::Ace::NG::Converter
+namespace {
+    const auto ATTRIBUTE_SRC_NAME("src");
+    const auto ATTRIBUTE_SRC_DEFAULT_VALUE("");
 
-namespace  {
-    const auto ATTRIBUTE_FILL_COLOR_NAME = "fillColor";
-    const auto ATTRIBUTE_FILL_COLOR_DEFAULT_VALUE = "#FF000000";
-    const auto OPACITY_COLOR = "#FF000000";
-    const auto ATTRIBUTE_AUTO_RESIZE_NAME = "autoResize";
-    const auto ATTRIBUTE_AUTO_RESIZE_DEFAULT_VALUE = "false";
-
-    struct EventsTracker {
-        static inline GENERATED_ArkUIImageEventsReceiver getImageEventsReceiver {};
-
-        static inline const GENERATED_ArkUIEventsAPI eventsApiImpl = {
-            .getImageEventsReceiver = [] () -> const GENERATED_ArkUIImageEventsReceiver* {
-                return &getImageEventsReceiver;
-            }
-        };
-    }; // EventsTracker
+    using imageOptions = Ark_Union_PixelMap_ResourceStr_DrawableDescriptor_ASTCResource;
 } // namespace
- */
+
 class MediaCachedImageModifierTest : public ModifierTestBase<GENERATED_ArkUIMediaCachedImageModifier,
     &GENERATED_ArkUINodeModifiers::getMediaCachedImageModifier,
     GENERATED_ARKUI_MEDIA_CACHED_IMAGE> {
@@ -72,27 +50,177 @@ public:
         }
         // fullAPI_->setArkUIEventsAPI(&EventsTracker::eventsApiImpl);
     }
+
+    RefPtr<PixelMap> CreatePixelMap(std::string& src);
 };
+
+RefPtr<PixelMap> MediaCachedImageModifierTest::CreatePixelMap(std::string& src)
+{
+    auto voidChar = src.data();
+    void* voidPtr = static_cast<void*>(voidChar);
+    RefPtr<PixelMap> pixelMap = PixelMap::CreatePixelMap(voidPtr);
+    return pixelMap;
+}
+
+/*
+ * @tc.name: setImageOptions0TestDefaultValues
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(MediaCachedImageModifierTest, setMediaCachedImageOptionsTestDefaultValues, TestSize.Level1)
+{
+    std::unique_ptr<JsonValue> jsonValue = GetJsonValue(node_);
+    std::string resultStr;
+
+    resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_SRC_NAME);
+    EXPECT_EQ(resultStr, ATTRIBUTE_SRC_DEFAULT_VALUE) << "Default value for attribute 'src'";
+
+    auto frameNode = reinterpret_cast<FrameNode*>(node_);
+    ASSERT_TRUE(frameNode);
+    auto layoutProperty = frameNode->GetLayoutProperty<ImageLayoutProperty>();
+    ASSERT_TRUE(layoutProperty);
+    auto sourceInfo = layoutProperty->GetImageSourceInfo();
+    ASSERT_TRUE(sourceInfo);
+    ASSERT_FALSE(sourceInfo->GetPixmap());
+}
+
 /*
  * @tc.name: setMediaCachedImageOptionsTest
  * @tc.desc: Check functionality of MediaCachedImageInterface.setMediaCachedImageOptions
  * @tc.type: FUNC
  */
-HWTEST_F(MediaCachedImageModifierTest, setMediaCachedImageOptionsTest, TestSize.Level1)
+HWTEST_F(MediaCachedImageModifierTest, setMediaCachedImageOptionsTestPixelMap, TestSize.Level1)
 {
-    ASSERT_NE(modifier_->setMediaCachedImageOptions, nullptr);
-
-    const std::string expectedSource = "sourceData";
-    const std::string expectedModule = {};
+    auto frameNode = reinterpret_cast<FrameNode*>(node_);
+    ASSERT_TRUE(frameNode);
+    std::string imagesSrc = "test";
+    RefPtr<PixelMap> pixelMap = CreatePixelMap(imagesSrc);
+    PixelMapPeer pixelMapPeer;
+    pixelMapPeer.pixelMap = pixelMap;
+    Ark_Materialized px { .ptr = &pixelMapPeer };
 
     auto arkValue =
-        Converter::ArkUnion<Ark_Union_PixelMap_ResourceStr_DrawableDescriptor_ASTCResource, Ark_ResourceStr>(
-            Converter::ArkUnion<Ark_ResourceStr, Ark_String>(expectedSource)
-        );
+        ArkUnion<imageOptions, Ark_PixelMap>(px);
     modifier_->setMediaCachedImageOptions(node_, &arkValue);
 
     std::unique_ptr<JsonValue> jsonValue = GetJsonValue(node_);
-    EXPECT_EQ(GetAttrValue<std::string>(jsonValue, "rawSrc"), expectedSource);
-    EXPECT_EQ(GetAttrValue<std::string>(jsonValue, "moduleName"), expectedModule);
+    auto layoutProperty = frameNode->GetLayoutProperty<ImageLayoutProperty>();
+    ASSERT_TRUE(layoutProperty);
+    auto sourceInfo = layoutProperty->GetImageSourceInfo();
+    ASSERT_TRUE(sourceInfo);
+    EXPECT_EQ(sourceInfo->GetPixmap().GetRawPtr(), pixelMap.GetRawPtr());
+}
+
+/*
+ * @tc.name: setMediaCachedImageOptionsTest
+ * @tc.desc: Check functionality of MediaCachedImageInterface.setMediaCachedImageOptions
+ * @tc.type: FUNC
+ */
+HWTEST_F(MediaCachedImageModifierTest, setMediaCachedImageOptionsTestResouce, TestSize.Level1)
+{
+    ASSERT_NE(modifier_->setMediaCachedImageOptions, nullptr);
+    imageOptions initValueSrc;
+
+    auto checkValue = [this, &initValueSrc](const std::string& input,
+            const std::string& expectedStr, const imageOptions& value) {
+            imageOptions inputValueSrc = initValueSrc;
+        // Re-create node for 'options' attribute
+        auto node = CreateNode();
+        inputValueSrc = value;
+        modifier_->setMediaCachedImageOptions(node, &inputValueSrc);
+        auto jsonValue = GetJsonValue(node);
+        auto resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_SRC_NAME);
+        DisposeNode(node);
+        EXPECT_EQ(resultStr, expectedStr) <<
+        "Input value is: " << input << ", method: setMediaCachedImageOptions, attribute: src";
+    };
+
+    for (auto& [input, value, expected] : Fixtures::testFixtureStringValidValues) {
+        checkValue(input, expected,
+            ArkUnion<imageOptions,
+                Ark_ResourceStr>(
+                ArkUnion<Ark_ResourceStr, Ark_String>(value)));
+    }
+    for (auto& [input, value, expected] : Fixtures::testFixtureStringResValidValues) {
+        checkValue(input, expected,
+            ArkUnion<imageOptions, Ark_ResourceStr>(
+                ArkUnion<Ark_ResourceStr, Ark_Resource>(value)));
+    }
+}
+
+/*
+ * @tc.name: setMediaCachedImageOptionsTest
+ * @tc.desc: Check functionality of MediaCachedImageInterface.setMediaCachedImageOptions
+ * @tc.type: FUNC
+ */
+HWTEST_F(MediaCachedImageModifierTest, DISABLED_setMediaCachedImageOptionsTestDrawableDescriptor, TestSize.Level1)
+{
+    // not supported yet
+}
+
+/*
+ * @tc.name: setMediaCachedImageOptionsTest
+ * @tc.desc: Check functionality of MediaCachedImageInterface.setMediaCachedImageOptions
+ * @tc.type: FUNC
+ */
+HWTEST_F(MediaCachedImageModifierTest, setMediaCachedImageOptionsTestASTCResource, TestSize.Level1)
+{
+    auto frameNode = reinterpret_cast<FrameNode*>(node_);
+    ASSERT_TRUE(frameNode);
+    ConvContext ctx;
+    std::vector<std::string> vecSrc { "test" };
+    Ark_ASTCResource resource {
+        .column = ArkValue<Ark_Number>(frameNode->GetId()),
+        .sources = ArkValue<Array_String>(vecSrc, &ctx)
+    };
+    auto arkValue =
+        ArkUnion<imageOptions, Ark_ASTCResource>(resource);
+    modifier_->setMediaCachedImageOptions(node_, &arkValue);
+
+    std::unique_ptr<JsonValue> jsonValue = GetJsonValue(node_);
+    std::string resultStr;
+
+    resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_SRC_NAME);
+    EXPECT_EQ(resultStr, ATTRIBUTE_SRC_DEFAULT_VALUE) << "Default value for attribute 'src'";
+
+    auto layoutProperty = frameNode->GetLayoutProperty<ImageLayoutProperty>();
+    ASSERT_TRUE(layoutProperty);
+    auto sourceInfo = layoutProperty->GetImageSourceInfo();
+    ASSERT_TRUE(sourceInfo);
+    ASSERT_FALSE(sourceInfo->GetPixmap());
+}
+
+/*
+ * @tc.name: setMediaCachedImageOptionsTestSrcInvalidValues
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(MediaCachedImageModifierTest, setMediaCachedImageOptionsTestSrcInvalidValues, TestSize.Level1)
+{
+    imageOptions initValueSrc;
+
+    // Initial setup
+    initValueSrc = ArkUnion<imageOptions, Ark_ResourceStr>(
+        ArkUnion<Ark_ResourceStr, Ark_String>(std::get<1>(Fixtures::testFixtureStringValidValues[0])));
+
+    auto checkValue = [this, &initValueSrc](
+                          const std::string& input, const imageOptions& value) {
+                            imageOptions inputValueSrc = initValueSrc;
+
+        // Re-create node for 'options' attribute
+        auto node = CreateNode();
+        inputValueSrc = value;
+        modifier_->setMediaCachedImageOptions(node, &inputValueSrc);
+        auto jsonValue = GetJsonValue(node);
+        auto resultStr = GetAttrValue<std::string>(jsonValue, ATTRIBUTE_SRC_NAME);
+        DisposeNode(node);
+        EXPECT_EQ(resultStr, ATTRIBUTE_SRC_DEFAULT_VALUE) <<
+            "Input value is: " << input << ", method: setMediaCachedImageOptions, attribute: src";
+    };
+
+    // Check invalid union
+    checkValue("invalid union", ArkUnion<imageOptions, Ark_Empty>(nullptr));
+    // Check invalid union
+    checkValue("invalid union", ArkUnion<imageOptions, Ark_Empty>(nullptr));
 }
 } // namespace OHOS::Ace::NG
