dac666ca55036b258013de2b29605125ea296b59
liyongzhuang <liyongzhuang1@huawei.com>
2025-02-17 19:55:35 +0800

    提供HTTPS证书错误校验接口能力
    Signed-off-by: liyongzhuang <liyongzhuang1@huawei.com>

--- frameworks/core/components/web/resource/web_client_impl.h
+++ frameworks/core/components/web/resource/web_client_impl.h
@@ -303,6 +303,9 @@ public:
     void GetVisibleRectToWeb(int& visibleX, int& visibleY, int& visibleWidth, int& visibleHeight) override;
 
     void OnScrollStart(const float x, const float y) override;
+    
+    bool OnSslErrorRequestByJSV2(std::shared_ptr<NWeb::NWebJSSslErrorResult> result,
+        OHOS::NWeb::SslError error, const std::vector<std::string>& certChainData) override;
 private:
     std::weak_ptr<OHOS::NWeb::NWeb> webviewWeak_;
     WeakPtr<WebDelegate> webDelegate_;
