c8775c2ccaee90e060da3784fb9c1f49848f5031
yusong <yusong9@huawei.com>
2025-02-19 15:13:36 +0800

    > 增加最大化分屏切换动画
    Signed-off-by: yusong <yusong9@huawei.com>

--- frameworks/core/pipeline_ng/pipeline_context.cpp
+++ frameworks/core/pipeline_ng/pipeline_context.cpp
@@ -1661,7 +1661,7 @@ void PipelineContext::StartFullToMultWindowAnimation(int32_t width, int32_t heig
 void PipelineContext::StartSplitWindowAnimation(int32_t width, int32_t height, WindowSizeChangeReason type,
     const std::shared_ptr<Rosen::RSTransaction>& rsTransaction)
 {
-    TAG_LOGI(AceLogTag::ACE_ANIMATION,
+    TAG_LOGI(AceLogTag::ACE_WINDOW_SCENE,
         "Root node start split window animation, type = %{public}d, width = %{public}d, height = %{public}d", type,
         width, height);
 #ifdef ENABLE_ROSEN_BACKEND
