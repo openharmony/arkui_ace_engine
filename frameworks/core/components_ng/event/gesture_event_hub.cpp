0ce9a25e169fff49d6b602ee8b1d2f56cbe67df8
zenix_zxy <zhengxingyou1@huawei.com>
2025-02-20 20:24:17 +0800

    fix damping reset
    Signed-off-by: zenix_zxy <zhengxingyou1@huawei.com>
    Change-Id: I0c90cf0525e252143c7c45f40150abefaee6198e

--- frameworks/core/components_ng/event/gesture_event_hub.cpp
+++ frameworks/core/components_ng/event/gesture_event_hub.cpp
@@ -1023,6 +1023,7 @@ void GestureEventHub::OnDragStart(const GestureEvent& info, const RefPtr<Pipelin
     bool hasContextMenu = focusHub == nullptr
                               ? false : focusHub->FindContextMenuOnKeyEvent(OnKeyEventType::CONTEXT_MENU);
     bool isBindMenuPreview = GetPreviewMode() != MenuPreviewMode::NONE;
+    dragDropManager->ResetContextMenuDragPosition();
     if (IsNeedSwitchToSubWindow() || isMenuShow) {
         imageNode = overlayManager->GetPixelMapContentNode();
         DragEventActuator::CreatePreviewNode(frameNode, imageNode, defaultPixelMapScale);
@@ -1039,8 +1040,6 @@ void GestureEventHub::OnDragStart(const GestureEvent& info, const RefPtr<Pipelin
                 previewOffset = previewDragMovePosition + originPoint;
                 DragEventActuator::UpdatePreviewPositionAndScale(imageNode, previewOffset);
             }
-
-            dragDropManager->ResetContextMenuDragPosition();
         }
 
         auto frameTag = frameNode->GetTag();
