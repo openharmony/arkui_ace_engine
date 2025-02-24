88bde7c25ee657349a0e0824e087002fc182147b
wangyihui <wangyihui16@huawei.com>
2025-02-24 14:21:12 +0800

    【Menu】修复路由跳转后蒙层异常
    Signed-off-by: wangyihui <wangyihui16@huawei.com>
    Change-Id: I53ad6f7e794365cb083ed4a96bc12b052aa5b37e

--- frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
+++ frameworks/core/components_ng/pattern/overlay/overlay_manager.cpp
@@ -6303,7 +6303,10 @@ void OverlayManager::RemoveFilterAnimation()
         return;
     }
     auto filterNode = filterColumnNodeWeak_.Upgrade();
-    CHECK_NULL_VOID(filterNode);
+    if (!filterNode) {
+        hasFilter_ = false;
+        return;
+    }
     auto filterContext = filterNode->GetRenderContext();
     CHECK_NULL_VOID(filterContext);
     auto pipelineContext = PipelineContext::GetCurrentContext();
