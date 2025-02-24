e76ca6df61fefc2c1b83d89b0de5071f710f78a1
zhaoyong <zhaoyong70@huawei.com>
2025-02-24 19:13:40 +0800

    fix dialog accessibility bug
    Signed-off-by: zhaoyong <zhaoyong70@huawei.com>

--- frameworks/core/components_ng/pattern/dialog/dialog_view.cpp
+++ frameworks/core/components_ng/pattern/dialog/dialog_view.cpp
@@ -110,6 +110,7 @@ RefPtr<FrameNode> DialogView::CreateDialogNode(
     if (dialogLayoutProp->GetIsScenceBoardDialog().value_or(false)) {
         dialogContext->UpdateBackgroundColor(param.maskColor.value_or(dialogTheme->GetMaskColorEnd()));
     }
+    SetDialogAccessibilityHoverConsume(dialog);
     // set onCancel callback
     auto hub = dialog->GetEventHub<DialogEventHub>();
     CHECK_NULL_RETURN(hub, dialog);
@@ -144,6 +145,26 @@ std::string DialogView::GetDialogTag(const DialogProperties& param)
             break;
     }
     return tag;
+}
+
+void DialogView::SetDialogAccessibilityHoverConsume(const RefPtr<FrameNode>& dialog)
+{
+    auto dialogAccessibilityProperty = dialog->GetAccessibilityProperty<DialogAccessibilityProperty>();
+    CHECK_NULL_VOID(dialogAccessibilityProperty);
+    dialogAccessibilityProperty->SetAccessibilityHoverConsume(
+        [weak = AceType::WeakClaim(AceType::RawPtr(dialog))](const NG::PointF& point) {
+            auto dialogNode = weak.Upgrade();
+            CHECK_NULL_RETURN(dialogNode, true);
+            auto dialogLayoutProp = dialogNode->GetLayoutProperty<DialogLayoutProperty>();
+            CHECK_NULL_RETURN(dialogLayoutProp, true);
+            auto pattern = dialogNode->GetPattern<DialogPattern>();
+            CHECK_NULL_RETURN(pattern, true);
+            if (!dialogLayoutProp->GetIsModal().value_or(true) ||
+                (dialogLayoutProp->GetShowInSubWindowValue(false) && !pattern->IsUIExtensionSubWindow())) {
+                return false;
+            }
+            return true;
+        });
 }
 
 } // namespace OHOS::Ace::NG
