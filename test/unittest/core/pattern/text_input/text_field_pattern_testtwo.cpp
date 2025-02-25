3111cd72e7d33201d587825f45b2f187e3e12a53
wangweiyuan <wangweiyuan2@huawei.com>
2025-02-25 21:11:59 +0800

    fix code review errs 2
    Signed-off-by: wangweiyuan <wangweiyuan2@huawei.com>

--- test/unittest/core/pattern/text_input/text_field_pattern_testtwo.cpp
+++ test/unittest/core/pattern/text_input/text_field_pattern_testtwo.cpp
@@ -20,7 +20,7 @@
 #include "test/mock/core/common/mock_theme_manager.h"
 #include "test/mock/core/common/mock_udmf.h"
 #include "test/mock/core/render/mock_paragraph.h"
-
+#include "core/components_ng/pattern/text/span/span_string.h"
 #include "core/components_ng/pattern/select_overlay/select_overlay_pattern.h"
 
 namespace OHOS::Ace::NG {
