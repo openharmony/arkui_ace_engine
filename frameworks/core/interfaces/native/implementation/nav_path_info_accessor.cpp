0ae6d34b698a205ce945920f7b55a23bf29cbeae
gleb_skroba <skroba.gleb1@huawei.com>
2025-02-26 00:35:53 +0300

    NavPathInfo - implementation for the materialized object
    Signed-off-by: gleb_skroba <skroba.gleb1@huawei.com>
    Change-Id: I0c1c644074f8129eb5411fd41d38813adc25d5c9

--- frameworks/core/interfaces/native/implementation/nav_path_info_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/nav_path_info_accessor.cpp
@@ -15,19 +15,70 @@
 
 #include "core/components_ng/base/frame_node.h"
 #include "core/interfaces/native/utility/converter.h"
+#include "core/interfaces/native/utility/reverse_converter.h"
+#include "nav_path_info_peer_impl.h"
 #include "arkoala_api_generated.h"
+
+namespace Nav = OHOS::Ace::NG::GeneratedModifier::NavigationContext;
+
+namespace OHOS::Ace::NG::Converter {
+void AssignArkValue(Ark_CustomObject& dst, const Nav::ExternalData& src)
+{
+    if (src) {
+        dst = src->data_;
+    }
+}
+void AssignArkValue(Ark_NavPathInfo& dst, const Nav::PathInfo& src)
+{
+    auto peer = new NavPathInfoPeer();
+    CHECK_NULL_VOID(peer);
+    peer->data = src;
+}
+
+template<>
+Nav::ExternalData Convert(const Ark_CustomObject& src)
+{
+    return Referenced::MakeRefPtr<Nav::ExternalDataKeeper>(src);
+}
+
+template<>
+Nav::PathInfo Convert(const Ark_NavPathInfo& src)
+{
+    return src ? src->data : Nav::PathInfo();
+}
+} // namespace OHOS::Ace::NG::Converter
+
+using namespace OHOS::Ace::NG::Converter;
 
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace NavPathInfoAccessor {
 void DestroyPeerImpl(Ark_NavPathInfo peer)
 {
+    delete peer;
 }
 Ark_NavPathInfo CtorImpl(const Ark_String* name,
                          const Ark_CustomObject* param,
                          const Opt_Callback_PopInfo_Void* onPop,
                          const Opt_Boolean* isEntry)
 {
-    return {};
+    CHECK_NULL_RETURN(name, nullptr);
+    auto peer = new NavPathInfoPeer();
+    CHECK_NULL_RETURN(peer, nullptr);
+    peer->data.name_ = Convert<std::string>(*name);
+    if (param) {
+        peer->data.param_ = Convert<Nav::ExternalData>(*param);
+    }
+    if (onPop) {
+        if (auto onPopOpt = OptConvert<Callback_PopInfo_Void>(*onPop); onPopOpt) {
+            peer->data.onPop_ = CallbackHelper(*onPopOpt);
+        }
+    }
+    if (isEntry) {
+        if (auto isEntryOpt = OptConvert<bool>(*isEntry); isEntryOpt) {
+            peer->data.isEntry_ = *isEntryOpt;
+        }
+    }
+    return peer;
 }
 Ark_NativePointer GetFinalizerImpl()
 {
@@ -35,11 +86,15 @@ Ark_NativePointer GetFinalizerImpl()
 }
 Ark_String GetNameImpl(Ark_NavPathInfo peer)
 {
-    return {};
+    CHECK_NULL_RETURN(peer, {});
+    return ArkValue<Ark_String>(peer->data.name_, Converter::FC);
 }
 void SetNameImpl(Ark_NavPathInfo peer,
                  const Ark_String* name)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(name);
+    peer->data.name_ = Convert<std::string>(*name);
 }
 Ark_CustomObject GetParamImpl(Ark_NavPathInfo peer)
 {
@@ -48,22 +103,32 @@ Ark_CustomObject GetParamImpl(Ark_NavPathInfo peer)
 void SetParamImpl(Ark_NavPathInfo peer,
                   const Ark_CustomObject* param)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(param);
+    peer->data.param_ = Convert<Nav::ExternalData>(*param);
 }
 Callback_PopInfo_Void GetOnPopImpl(Ark_NavPathInfo peer)
 {
-    return {};
+    CHECK_NULL_RETURN(peer, Callback_PopInfo_Void());
+    return peer->data.onPop_.GetCallback();
 }
 void SetOnPopImpl(Ark_NavPathInfo peer,
                   const Callback_PopInfo_Void* onPop)
 {
+    CHECK_NULL_VOID(peer);
+    CHECK_NULL_VOID(onPop);
+    peer->data.onPop_ = CallbackHelper(*onPop);
 }
 Ark_Boolean GetIsEntryImpl(Ark_NavPathInfo peer)
 {
-    return {};
+    CHECK_NULL_RETURN(peer, {});
+    return ArkValue<Ark_Boolean>(peer->data.isEntry_);
 }
 void SetIsEntryImpl(Ark_NavPathInfo peer,
                     Ark_Boolean isEntry)
 {
+    CHECK_NULL_VOID(peer);
+    peer->data.isEntry_ = Convert<bool>(isEntry);
 }
 } // NavPathInfoAccessor
 const GENERATED_ArkUINavPathInfoAccessor* GetNavPathInfoAccessor()
