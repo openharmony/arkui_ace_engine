9a75f13bb68c1fa5dddb7b2c54a1de0c65f96f12
Ilya Erokhin <ilya.erokhin@huawei.com>
2025-03-05 14:28:34 +0300

    ConsoleMessageAccessor: getMessage, getSourceId, getMessageLevel implementation and UTs
    Signed-off-by: Ilya Erokhin <ilya.erokhin@huawei.com>
    Change-Id: Iaa11a6bd31b39ae91fc2f88ff19638093c7c2e1d

--- frameworks/core/interfaces/native/utility/reverse_converter.h
+++ frameworks/core/interfaces/native/utility/reverse_converter.h
@@ -197,6 +197,7 @@ namespace OHOS::Ace::NG::Converter {
     void AssignArkValue(Ark_ListItemStyle& dst, const V2::ListItemStyle& src);
     void AssignArkValue(Ark_LocationButtonOnClickResult& dst, const SecurityComponentHandleResult& src);
     void AssignArkValue(Ark_MenuPolicy& dst, const MenuPolicy& src);
+    void AssignArkValue(Ark_MessageLevel& dst, const MessageLevel& src);
     void AssignArkValue(Ark_MouseAction& dst, const MouseAction& src);
     void AssignArkValue(Ark_MouseButton& dst, const MouseButton& src);
     void AssignArkValue(Ark_NativeEmbedInfo& dst, const EmbedInfo& src);
