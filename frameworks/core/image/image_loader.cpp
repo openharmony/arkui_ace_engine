35e2b171a242191b37a3ec4c46fd2c4085a715e4
hw_wyx <wuyinxiao@huawei.com>
2025-03-05 16:12:22 +0800

    fixFDLeak5.0.3
    Signed-off-by: hw_wyx <wuyinxiao@huawei.com>
    Change-Id: Ia8179cab07c902f1fe6141bd8c4555f40df42b96

--- frameworks/core/image/image_loader.cpp
+++ frameworks/core/image/image_loader.cpp
@@ -712,6 +712,7 @@ std::string DecodedDataProviderImageLoader::GetThumbnailOrientation(const ImageS
 
     // check image orientation
     auto imageSrc = ImageSource::Create(fd);
+    close(fd);
     CHECK_NULL_RETURN(imageSrc, "");
     std::string orientation = imageSrc->GetProperty("Orientation");
     return orientation;
@@ -887,6 +888,7 @@ std::string AstcImageLoader::GetThumbnailOrientation(const ImageSourceInfo& src)
     CHECK_NULL_RETURN(fd >= 0, "");
 
     auto imageSrc = ImageSource::Create(fd);
+    close(fd);
     CHECK_NULL_RETURN(imageSrc, "");
     std::string orientation = imageSrc->GetProperty("Orientation");
     return orientation;
