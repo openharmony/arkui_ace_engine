b0fb5f3b243d1757cb02ab64ae9ef1d47f8776e9
Ilya Erokhin <ilya.erokhin@huawei.com>
2025-03-06 11:13:10 +0300

    PermissionRequestAccessor: getOrigin, getAccessibleResource implementation and UTs
    Signed-off-by: Ilya Erokhin <ilya.erokhin@huawei.com>
    Change-Id: I2b778556890d07dc2af2233191079c4d8af1b4f2

--- test/unittest/capi/accessors/permission_request_accessor_test.cpp
+++ test/unittest/capi/accessors/permission_request_accessor_test.cpp
@@ -63,29 +63,35 @@ HWTEST_F(PermissionRequestAccessorTest, denyTest, TestSize.Level1)
 }
 
 /**
- * @tc.name: DISABLED_getOriginTest
+ * @tc.name: getOriginTest
  * @tc.desc:
  * @tc.type: FUNC
  */
-HWTEST_F(PermissionRequestAccessorTest, DISABLED_getOriginTest, TestSize.Level1)
+HWTEST_F(PermissionRequestAccessorTest, getOriginTest, TestSize.Level1)
 {
     ASSERT_NE(accessor_->getOrigin, nullptr);
 
-    EXPECT_CALL(*mockHandler_, GetOrigin()).Times(1);
-    accessor_->getOrigin(peer_);
+    std::string origin = "origin";
+    EXPECT_CALL(*mockHandler_, GetOrigin()).Times(1).WillOnce(Return(origin));
+    EXPECT_EQ(Converter::Convert<std::string>(accessor_->getOrigin(peer_)), origin);
+    EXPECT_EQ(Converter::Convert<std::string>(accessor_->getOrigin(nullptr)), "");
 }
 
 /**
- * @tc.name: DISABLED_getAccessibleResourceTest
+ * @tc.name: getAccessibleResourceTest
  * @tc.desc:
  * @tc.type: FUNC
  */
-HWTEST_F(PermissionRequestAccessorTest, DISABLED_getAccessibleResourceTest, TestSize.Level1)
+HWTEST_F(PermissionRequestAccessorTest, getAccessibleResourceTest, TestSize.Level1)
 {
     ASSERT_NE(accessor_->getAccessibleResource, nullptr);
 
-    EXPECT_CALL(*mockHandler_, GetResources()).Times(1);
-    accessor_->getAccessibleResource(peer_);
+    std::vector<std::string> resources { "item1", "item2" };
+    EXPECT_CALL(*mockHandler_, GetResources()).Times(1).WillOnce(Return(resources));
+    EXPECT_EQ(Converter::Convert<std::vector<std::string>>(accessor_->getAccessibleResource(peer_)), resources);
+
+    std::vector<std::string> empty;
+    EXPECT_EQ(Converter::Convert<std::vector<std::string>>(accessor_->getAccessibleResource(nullptr)), empty);
 }
 
 /**
