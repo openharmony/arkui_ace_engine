25cd8d170376af09bbaac52d98b2586f18ddb05a
kovalevsergey <kovalev.sergey5@huawei-partners.com>
2025-03-10 10:26:41 +0300

    modifier update
    Signed-off-by: kovalevsergey <kovalev.sergey5@huawei-partners.com>
    Change-Id: I6634ee1fa9d465e0e6b19313b3649d6bb499f860

--- frameworks/core/interfaces/native/implementation/long_press_gesture_event_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/long_press_gesture_event_accessor.cpp
@@ -15,6 +15,7 @@
 
 #include "core/components_ng/base/frame_node.h"
 #include "core/interfaces/native/utility/converter.h"
+#include "core/interfaces/native/utility/reverse_converter.h"
 #include "core/interfaces/native/implementation/long_press_gesture_event_peer.h"
 
 namespace OHOS::Ace::NG::GeneratedModifier {
@@ -33,11 +34,19 @@ Ark_NativePointer GetFinalizerImpl()
 }
 Ark_Boolean GetRepeatImpl(Ark_LongPressGestureEvent peer)
 {
-    return {};
+    const auto errValue = Converter::ArkValue<Ark_Boolean>(false);
+    CHECK_NULL_RETURN(peer, errValue);
+    auto event = peer->GetEventInfo();
+    CHECK_NULL_RETURN(event, errValue);
+    return event->GetRepeat();
 }
 void SetRepeatImpl(Ark_LongPressGestureEvent peer,
                    Ark_Boolean repeat)
 {
+    CHECK_NULL_VOID(peer);
+    auto event = peer->GetEventInfo();
+    CHECK_NULL_VOID(event);
+    event->SetRepeat(Converter::Convert<bool>(repeat));
 }
 } // LongPressGestureEventAccessor
 const GENERATED_ArkUILongPressGestureEventAccessor* GetLongPressGestureEventAccessor()
