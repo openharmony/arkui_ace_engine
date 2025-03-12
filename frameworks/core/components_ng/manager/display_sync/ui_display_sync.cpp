afa03ea1cb94496719943f3e495a1deacaa49690
鲁保良 <lubaoliang1@h-partners.com>
2025-03-12 11:39:14 +0800

    1
    Signed-off-by: 鲁保良 <lubaoliang1@h-partners.com>

--- frameworks/core/components_ng/manager/display_sync/ui_display_sync.cpp
+++ frameworks/core/components_ng/manager/display_sync/ui_display_sync.cpp
@@ -76,9 +76,9 @@ void UIDisplaySync::OnFrame()
                      GetId(), static_cast<int32_t>(uiObjectType_), data_->timestamp_, data_->targetTimestamp_,
                      data_->rateRange_->min_, data_->rateRange_->max_, data_->rateRange_->preferred_,
                      drawFPS_, sourceVsyncRate_, data_->rate_, data_->noSkip_);
-    TAG_LOGD(AceLogTag::ACE_DISPLAY_SYNC, "Type[%{public}d] FrameRateRange[%{public}d, %{public}d, %{public}d],"
-        " DrawFPS[%{public}d] VSyncRate[%{public}d] Rate[%{public}d] noSkip[%{public}d]",
-        static_cast<int32_t>(uiObjectType_), data_->rateRange_->min_, data_->rateRange_->max_,
+    TAG_LOGD(AceLogTag::ACE_DISPLAY_SYNC, "Id[%{public}" PRIu64 "] Type[%{public}d] FrameRateRange[%{public}d, "
+        "%{public}d, %{public}d], DrawFPS[%{public}d] VSyncRate[%{public}d] Rate[%{public}d] noSkip[%{public}d]",
+        GetId(), static_cast<int32_t>(uiObjectType_), data_->rateRange_->min_, data_->rateRange_->max_,
         data_->rateRange_->preferred_, drawFPS_, sourceVsyncRate_, data_->rate_, data_->noSkip_);
     if (data_->noSkip_ && data_->onFrame_) {
         data_->onFrame_();
