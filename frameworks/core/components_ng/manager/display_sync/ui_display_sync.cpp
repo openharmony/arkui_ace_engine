8c339b059e170dc1584bc4182bd28091221b6ae4
鲁保良 <lubaoliang1@h-partners.com>
2025-03-12 10:22:11 +0800

    Test GetCurrentContextSafelyWithCheck
    Signed-off-by: 鲁保良 <lubaoliang1@h-partners.com>

--- frameworks/core/components_ng/manager/display_sync/ui_display_sync.cpp
+++ frameworks/core/components_ng/manager/display_sync/ui_display_sync.cpp
@@ -106,9 +106,9 @@ void UIDisplaySync::AddToPipeline(WeakPtr<PipelineBase>& pipelineContext)
 
     auto context = pipelineContext.Upgrade();
     if (!context) {
-        context = PipelineBase::GetCurrentContextSafely();
+        context = PipelineBase::GetCurrentContextSafelyWithCheck();
         if (!context) {
-            TAG_LOGE(AceLogTag::ACE_DISPLAY_SYNC, "[DisplaySync] CurrentContext is nullptr.");
+            TAG_LOGE(AceLogTag::ACE_DISPLAY_SYNC, "[DisplaySync] AddToPipeline CurrentContext is nullptr.");
             return;
         }
         TAG_LOGD(AceLogTag::ACE_DISPLAY_SYNC, "[DisplaySync] Add to current context safely.");
@@ -126,7 +126,7 @@ void UIDisplaySync::DelFromPipeline(WeakPtr<PipelineBase>& pipelineContext)
 {
     auto context = GetCurrentContext();
     if (!context) {
-        TAG_LOGE(AceLogTag::ACE_DISPLAY_SYNC, "[DisplaySync] CurrentContext is nullptr.");
+        TAG_LOGE(AceLogTag::ACE_DISPLAY_SYNC, "[DisplaySync] DelFromPipeline CurrentContext is nullptr.");
         return;
     }
 
