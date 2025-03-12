655f5273563eff55b3a55b58df41ffb02537d6d8
zgit2021 <zhaowenqiang14@huawei.com>
2025-03-12 13:03:29 +0800

    remove the web request focus on touch
    Signed-off-by: zgit2021 <zhaowenqiang14@huawei.com>
    Change-Id: Ifeec492808a4e893dc4b91cc0e3f418ad63fbf93

--- frameworks/core/components_ng/pattern/web/web_pattern.cpp
+++ frameworks/core/components_ng/pattern/web/web_pattern.cpp
@@ -3762,9 +3762,6 @@ void WebPattern::HandleTouchDown(const TouchEventInfo& info, bool fromOverlay)
             imageAnalyzerManager_->UpdateOverlayTouchInfo(touchPoint.x, touchPoint.y, TouchType::DOWN);
         }
     }
-    if (!touchInfos.empty() && !GetNativeEmbedModeEnabledValue(false)) {
-        WebRequestFocus();
-    }
 }
 
 void WebPattern::HandleTouchUp(const TouchEventInfo& info, bool fromOverlay)
@@ -6815,11 +6812,6 @@ void WebPattern::UnregisterWebComponentClickCallback()
     webComponentClickCallback_ = nullptr;
     textBlurAccessibilityEnable_ = false;
     SetAccessibilityState(false);
-}
-
-void WebPattern::RequestFocus()
-{
-    WebRequestFocus();
 }
 
 bool WebPattern::IsCurrentFocus()
