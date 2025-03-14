d532fd552d19a91650cc3d20794465bea73bcde1
kangshihui <kangshihui@huawei.com>
2025-03-14 19:47:19 +0800

    修复文本组件子窗文本菜单在热切换主题或语言后点击失效 Signed-off-by:kangshihui<kangshihui@huawei.com>
    Change-Id: I855534925d6bcb4886f773e36b0e516e9ce22f97

--- frameworks/core/components_ng/pattern/select_overlay/select_overlay_pattern.cpp
+++ frameworks/core/components_ng/pattern/select_overlay/select_overlay_pattern.cpp
@@ -832,7 +832,7 @@ void SelectOverlayPattern::OnDpiConfigurationUpdate()
 {
     auto host = DynamicCast<SelectOverlayNode>(GetHost());
     CHECK_NULL_VOID(host);
-    host->UpdateToolBar(true, true);
+    host->UpdateToolBarFromMainWindow(true, true);
 }
 
 void SelectOverlayPattern::SwitchHandleToOverlayMode(bool afterRender)
@@ -865,13 +865,13 @@ void SelectOverlayPattern::OnColorConfigurationUpdate()
     auto host = DynamicCast<SelectOverlayNode>(GetHost());
     CHECK_NULL_VOID(host);
     host->UpdateSelectMenuBg();
-    host->UpdateToolBar(true, true);
+    host->UpdateToolBarFromMainWindow(true, true);
 }
 
 void SelectOverlayPattern::OnLanguageConfigurationUpdate()
 {
     auto host = DynamicCast<SelectOverlayNode>(GetHost());
     CHECK_NULL_VOID(host);
-    host->UpdateToolBar(true, true);
+    host->UpdateToolBarFromMainWindow(true, true);
 }
 } // namespace OHOS::Ace::NG
