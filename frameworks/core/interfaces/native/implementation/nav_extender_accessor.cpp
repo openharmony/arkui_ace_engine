a3f696fda30c13f21bbd5a2989e244f99d4c81aa
Nikolay Pisanov <Pisanov.Nikolay@huawei.com>
2025-03-14 15:07:06 +0300

    Generated C API v124using IDLize: 2.1.0, SDK: weekly_20250224
    Signed-off-by: Nikolay Pisanov <Pisanov.Nikolay@huawei.com>
    Change-Id: I15cd2df5558b0bfc6f7174f6fc110958052ce1cf

--- frameworks/core/interfaces/native/implementation/nav_extender_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/nav_extender_accessor.cpp
@@ -23,11 +23,48 @@ void SetUpdateStackCallbackImpl(Ark_NavPathStack peer,
                                 const NavExtender_OnUpdateStack* callback)
 {
 }
+void SyncStackImpl(Ark_NavPathStack peer)
+{
+}
+Ark_Boolean CheckNeedCreateImpl(Ark_NativePointer navigation,
+                                Ark_NavPathStack stack)
+{
+    auto frameNode = reinterpret_cast<FrameNode *>(node);
+    CHECK_NULL_VOID(frameNode);
+    //auto convValue = Converter::Convert<type>(navigation);
+    //auto convValue = Converter::OptConvert<type>(navigation); // for enums
+    //undefinedModelNG::SetCheckNeedCreate(frameNode, convValue);
+    return {};
+}
+Ark_NativePointer NavigationCreateImpl(Ark_Int32 peer,
+                                       Ark_Int32 flag)
+{
+    return {};
+}
+void SetNavigationOptionsImpl(Ark_NativePointer navigation,
+                              Ark_NavPathStack stack)
+{
+    auto frameNode = reinterpret_cast<FrameNode *>(node);
+    CHECK_NULL_VOID(frameNode);
+    //auto convValue = Converter::Convert<type>(navigation);
+    //auto convValue = Converter::OptConvert<type>(navigation); // for enums
+    //undefinedModelNG::SetSetNavigationOptions(frameNode, convValue);
+}
+void SetNavDestinationNodeImpl(Ark_NavPathStack peer,
+                               Ark_Int32 index,
+                               Ark_NativePointer node)
+{
+}
 } // NavExtenderAccessor
 const GENERATED_ArkUINavExtenderAccessor* GetNavExtenderAccessor()
 {
     static const GENERATED_ArkUINavExtenderAccessor NavExtenderAccessorImpl {
         NavExtenderAccessor::SetUpdateStackCallbackImpl,
+        NavExtenderAccessor::SyncStackImpl,
+        NavExtenderAccessor::CheckNeedCreateImpl,
+        NavExtenderAccessor::NavigationCreateImpl,
+        NavExtenderAccessor::SetNavigationOptionsImpl,
+        NavExtenderAccessor::SetNavDestinationNodeImpl,
     };
     return &NavExtenderAccessorImpl;
 }
