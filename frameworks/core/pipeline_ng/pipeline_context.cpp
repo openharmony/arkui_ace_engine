26a85af1e935f54e51882fb65d2085301b3611d4
刘永凯 <liuyongkai2@huawei-partners.com>
2025-03-17 22:24:51 +0800

    support detecting container instance run in multi-thread issues
    Signed-off-by: 刘永凯 <liuyongkai2@huawei-partners.com>

--- frameworks/core/pipeline_ng/pipeline_context.cpp
+++ frameworks/core/pipeline_ng/pipeline_context.cpp
@@ -5910,7 +5910,7 @@ void PipelineContext::FlushMouseEventForHover()
         windowSizeChangeReason_ == WindowSizeChangeReason::MOVE) {
         return;
     }
-    CHECK_RUN_ON(TaskExecutor::TaskType::UI);
+    CHECK_RUN_ON(UI);
     auto container = Container::Current();
     CHECK_NULL_VOID(container);
     MouseEvent event;
