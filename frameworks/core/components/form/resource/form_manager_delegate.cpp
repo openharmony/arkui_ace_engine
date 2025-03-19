345dc3272ea3d53b7a15d7570030990c1ec63f91
笑笑你的牙 <zhangsaiyang1@h-partners.com>
2025-03-19 12:08:25 +0000

    wantCahce单独加变量锁
    Signed-off-by: 笑笑你的牙 <zhangsaiyang1@h-partners.com>

--- frameworks/core/components/form/resource/form_manager_delegate.cpp
+++ frameworks/core/components/form/resource/form_manager_delegate.cpp
@@ -687,13 +687,15 @@ void FormManagerDelegate::DispatchPointerEvent(const
     if (pointerEvent->GetPointerAction() == OHOS::MMI::PointerEvent::POINTER_ACTION_DOWN) {
         TAG_LOGI(AceLogTag::ACE_FORM, "dispatch down event to renderer");
     }
+    
+    bool disablePanGesture;
     {
         std::lock_guard<std::mutex> wantCacheLock(wantCacheMutex_);
-        auto disablePanGesture = wantCache_.GetBoolParam(OHOS::AppExecFwk::Constants::FORM_DISABLE_GESTURE_KEY, false);
-        if (!disablePanGesture) {
-            formRendererDispatcher_->DispatchPointerEvent(pointerEvent, serializedGesture);
-            return;
-        }
+        disablePanGesture = wantCache_.GetBoolParam(OHOS::AppExecFwk::Constants::FORM_DISABLE_GESTURE_KEY, false);
+    }
+    if (!disablePanGesture) {
+        formRendererDispatcher_->DispatchPointerEvent(pointerEvent, serializedGesture);
+        return;
     }
 
     // pan gesture disabled, not dispatch move event, not concat serialized gesture
