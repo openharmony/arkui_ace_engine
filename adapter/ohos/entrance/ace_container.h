302347b385be685095eaf8c0aec44c66841510a9
ConvivialYu <yujiale3@huawei.com>
2025-03-20 14:46:23 +0800

    fix AceContainer RemoveOnConfigurationChange
    Signed-off-by: ConvivialYu <yujiale3@huawei.com>

--- adapter/ohos/entrance/ace_container.h
+++ adapter/ohos/entrance/ace_container.h
@@ -95,7 +95,7 @@ struct SingleHandTransform {
     SingleHandTransform() = default;
     SingleHandTransform(float x, float y, float scaleX, float scaleY)
         : x_(x), y_(y), scaleX_(scaleX), scaleY_(scaleY) {}
- 
+
     float x_ = 0.0f;
     float y_ = 0.0f;
     float scaleX_ = 1.0f;
@@ -598,7 +598,7 @@ public:
 
     void RemoveOnConfigurationChange(int32_t instanceId)
     {
-        configurationChangedCallbacks_.erase(instanceId_);
+        configurationChangedCallbacks_.erase(instanceId);
     }
 
     void HotReload() override;
