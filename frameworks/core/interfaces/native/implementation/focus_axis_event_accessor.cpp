19fdda02161dbcf560ac413201098fdfb60e3b8a
evstigneevroman_9cd1 <evstigneev.roman1@huawei-partners.com>
2025-03-26 12:38:02 +0300

    implement FocusAxisEventAccessor and tests
    Signed-off-by: evstigneevroman_9cd1 <evstigneev.roman1@huawei-partners.com>
    Change-Id: Ibd2613416655fb667afbf4e1f3e70a9c198862c1

--- frameworks/core/interfaces/native/implementation/focus_axis_event_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/focus_axis_event_accessor.cpp
@@ -12,19 +12,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "core/interfaces/native/implementation/focus_axis_event_accessor.h"
 
-#include "core/components_ng/base/frame_node.h"
-#include "core/interfaces/native/utility/converter.h"
-#include "arkoala_api_generated.h"
+namespace OHOS::Ace::NG {
+namespace Converter {
+void AssignArkValue(Ark_AxisModel& dst, const AxisModel& src)
+{
+    switch (src) {
+        case AxisModel::ABS_X: dst = ARK_AXIS_MODEL_ABS_X; break;
+        case AxisModel::ABS_Y: dst = ARK_AXIS_MODEL_ABS_Y; break;
+        case AxisModel::ABS_Z: dst = ARK_AXIS_MODEL_ABS_Z; break;
+        case AxisModel::ABS_RZ: dst = ARK_AXIS_MODEL_ABS_RZ; break;
+        case AxisModel::ABS_GAS: dst = ARK_AXIS_MODEL_ABS_GAS; break;
+        case AxisModel::ABS_BRAKE: dst = ARK_AXIS_MODEL_ABS_BRAKE; break;
+        case AxisModel::ABS_HAT0X: dst = ARK_AXIS_MODEL_ABS_HAT0X; break;
+        case AxisModel::ABS_HAT0Y: dst = ARK_AXIS_MODEL_ABS_HAT0Y; break;
+        default: {
+            dst = INVALID_ENUM_VAL<Ark_AxisModel>;
+            LOGE("Unexpected enum value in AxisModel: %{public}d", src);
+        }
+    }
+}
+} // namespace Converter
+} // namespace OHOS::Ace::NG
+
 
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace FocusAxisEventAccessor {
 void DestroyPeerImpl(Ark_FocusAxisEvent peer)
 {
+    PeerUtils::DestroyPeer(peer);
 }
 Ark_FocusAxisEvent CtorImpl()
 {
-    return nullptr;
+    return PeerUtils::CreatePeer<FocusAxisEventPeer>();
 }
 Ark_NativePointer GetFinalizerImpl()
 {
@@ -32,19 +53,29 @@ Ark_NativePointer GetFinalizerImpl()
 }
 Map_AxisModel_Number GetAxisMapImpl(Ark_FocusAxisEvent peer)
 {
-    return {};
+    CHECK_NULL_RETURN(peer && peer->GetEventInfo(), {});
+    auto eventInfo = peer->GetEventInfo();
+    return Converter::ArkValue<Map_AxisModel_Number>(getAxisMapFromInfo(*eventInfo), Converter::FC);
 }
 void SetAxisMapImpl(Ark_FocusAxisEvent peer,
                     const Map_AxisModel_Number* axisMap)
 {
+    LOGW("ARKOALA KeyEventAccessor::SetAxisMapImpl doesn't have sense.");
 }
 Callback_Void GetStopPropagationImpl(Ark_FocusAxisEvent peer)
 {
-    return {};
+    CHECK_NULL_RETURN(peer, {});
+    auto callback = CallbackKeeper::DefineReverseCallback<Callback_Void>([peer]() {
+        FocusAxisEventInfo* info = peer->GetEventInfo();
+        CHECK_NULL_VOID(info);
+        info->SetStopPropagation(true);
+    });
+    return callback;
 }
 void SetStopPropagationImpl(Ark_FocusAxisEvent peer,
                             const Callback_Void* stopPropagation)
 {
+    LOGE("FocusAxisEventAccessor::SetStopPropagationImpl we can only GET stopPropagation callback");
 }
 } // FocusAxisEventAccessor
 const GENERATED_ArkUIFocusAxisEventAccessor* GetFocusAxisEventAccessor()
