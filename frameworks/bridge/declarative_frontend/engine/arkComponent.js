08a86796c69c6b493ce223d92274c0800f9eff39
Zhang-Dong-hui <zhangdonghui5@huawei.com>
2025-03-27 10:35:25 +0800

    list item group initializes with headerComponent and footerComponent
    Signed-off-by: Zhang-Dong-hui <zhangdonghui5@huawei.com>
    Change-Id: Ifab5d4e082cfaae7f69a65be539dc4945ba80c9b

--- frameworks/bridge/declarative_frontend/engine/arkComponent.js
+++ frameworks/bridge/declarative_frontend/engine/arkComponent.js
@@ -30391,7 +30391,8 @@ class ListItemGroupInitializeModifier extends ModifierWithKey {
     if (reset) {
       getUINativeModule().listItemGroup.resetListItemGroupInitialize(node);
     } else {
-      getUINativeModule().listItemGroup.setListItemGroupInitialize(node, this.value?.space, this.value?.style);
+      getUINativeModule().listItemGroup.setListItemGroupInitialize(node, this.value?.space, this.value?.style,
+        this.value?.headerComponent, this.value?.footerComponent);
     }
   }
 }
