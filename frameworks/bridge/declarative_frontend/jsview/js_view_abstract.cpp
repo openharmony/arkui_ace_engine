eb66d0d39861fa35144408d4d1282cbc925bf053
zhanghang <zhanghang160@huawei-partners.com>
2025-04-02 22:13:56 +0800

    Feat:提供onAccessibilityActionIntercept回调
    Signed-off-by: zhanghang <zhanghang160@huawei-partners.com>

--- frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
+++ frameworks/bridge/declarative_frontend/jsview/js_view_abstract.cpp
@@ -7123,6 +7123,8 @@ void JSViewAbstract::JSBind(BindingTarget globalObj)
                                           &JSViewAbstract::JsAccessibilityScrollTriggerable);
     JSClass<JSViewAbstract>::StaticMethod("accessibilityFocusDrawLevel",
                                           &JSViewAbstract::JsAccessibilityFocusDrawLevel);
+    JSClass<JSViewAbstract>::StaticMethod("onAccessibilityActionIntercept",
+                                          &JSViewAbstract::JsOnAccessibilityActionIntercept);
 
     JSClass<JSViewAbstract>::StaticMethod("alignRules", &JSViewAbstract::JsAlignRules);
     JSClass<JSViewAbstract>::StaticMethod("chainMode", &JSViewAbstract::JsChainMode);
