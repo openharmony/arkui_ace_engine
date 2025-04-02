b10cda520e4a235ee5b62d887af1363168b80fd4
Zhang-Dong-hui <zhangdonghui5@huawei.com>
2025-04-02 16:12:09 +0800

    change the way of getting pipeline context
    Signed-off-by: Zhang-Dong-hui <zhangdonghui5@huawei.com>
    Change-Id: I7178c8fd0ea44e464183b28afaf9d5461243fc98

--- frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
+++ frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
@@ -2665,7 +2665,7 @@ void ScrollablePattern::OnStatusBarClick()
         return;
     }
 
-    auto pipeline = GetContext();
+    auto pipeline = PipelineContext::GetCurrentContextSafelyWithCheck();
     CHECK_NULL_VOID(pipeline);
     auto host = GetHost();
     CHECK_NULL_VOID(host);
