7d0874f3077d243141fdb533c14db8858c895189
Zhang-Dong-hui <zhangdonghui5@huawei.com>
2025-04-05 17:07:17 +0800

    get default value from theme
    Signed-off-by: Zhang-Dong-hui <zhangdonghui5@huawei.com>

--- frameworks/core/components_ng/pattern/indexer/indexer_paint_property.h
+++ frameworks/core/components_ng/pattern/indexer/indexer_paint_property.h
@@ -76,20 +76,21 @@ public:
         if (filter.IsFastFilter()) {
             return;
         }
-        json->PutExtAttr("selectedBackgroundColor",
-            propSelectedBackgroundColor_.value_or(Color::WHITE).ColorToString().c_str(), filter);
-        json->PutExtAttr("popupBackground",
-            propPopupBackground_.value_or(Color::WHITE).ColorToString().c_str(), filter);
         auto pipeline = PipelineContext::GetCurrentContext();
         CHECK_NULL_VOID(pipeline);
         auto indexerTheme = pipeline->GetTheme<IndexerTheme>();
         CHECK_NULL_VOID(indexerTheme);
+        json->PutExtAttr("selectedBackgroundColor",
+            propSelectedBackgroundColor_.value_or(indexerTheme->GetSeclectedBackgroundColor()).ColorToString().c_str(),
+            filter);
+        json->PutExtAttr("popupBackground",
+            propPopupBackground_.value_or(indexerTheme->GetPopupBackgroundColor()).ColorToString().c_str(), filter);
         json->PutExtAttr("popupSelectedColor", propPopupSelectedColor_.
             value_or(indexerTheme->GetPopupSelectedTextColor()).ColorToString().c_str(), filter);
         json->PutExtAttr("popupUnselectedColor", propPopupUnselectedColor_.
             value_or(indexerTheme->GetPopupUnselectedTextColor()).ColorToString().c_str(), filter);
         json->PutExtAttr("popupItemBackground", propPopupItemBackground_.
-            value_or(indexerTheme->GetPopupBackgroundColor()).ColorToString().c_str(), filter);
+            value_or(indexerTheme->GetPopupUnclickedBgAreaColor()).ColorToString().c_str(), filter);
         if (propPopupBorderRadius_.has_value()) {
             json->PutExtAttr("popupBorderRadius", propPopupBorderRadius_.value().ToString().c_str(), filter);
         } else {
