87d5ba159e063a6dd540b2fd55d77d17efb8847d
Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>
2025-04-09 10:09:01 +0300

    Fixed review comments
    Signed-off-by: Mikhail V. Lobakh <lobah.mikhail@huawei-partners.com>

--- frameworks/core/interfaces/native/implementation/common_method_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/common_method_modifier.cpp
@@ -3717,8 +3717,9 @@ void AccessibilityDescription1Impl(Ark_NativePointer node,
     auto frameNode = reinterpret_cast<FrameNode *>(node);
     CHECK_NULL_VOID(frameNode);
     CHECK_NULL_VOID(value);
-    auto optValue = value ? Converter::OptConvert<std::string>(*value) : std::nullopt;
-    ViewAbstractModelNG::SetAccessibilityDescription(frameNode, optValue.value_or(""));
+    auto optValue = Converter::OptConvert<std::string>(*value);
+    CHECK_EQUAL_VOID(optValue.has_value(), false);
+    ViewAbstractModelNG::SetAccessibilityDescription(frameNode, optValue.value());
 }
 void AccessibilityLevelImpl(Ark_NativePointer node,
                             const Ark_String* value)
