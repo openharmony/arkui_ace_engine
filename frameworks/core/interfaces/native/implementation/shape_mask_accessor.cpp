702be9d417780c773ba471064a9cf74d626634f3
voronov.vadim <voronov.vadim@huawei-partners.com>
2025-04-09 14:32:27 +0300

    s_shape_mask_part1 init
    Signed-off-by: voronov.vadim <voronov.vadim@huawei-partners.com>

--- frameworks/core/interfaces/native/implementation/shape_mask_accessor.cpp
+++ frameworks/core/interfaces/native/implementation/shape_mask_accessor.cpp
@@ -15,16 +15,89 @@
 
 #include "core/components_ng/base/frame_node.h"
 #include "core/interfaces/native/utility/converter.h"
+#include "core/interfaces/native/utility/peer_utils.h"
+#include "shape_mask_peer.h"
 #include "arkoala_api_generated.h"
 
+namespace OHOS::Ace::NG {
+namespace Converter {
+template<>
+RefPtr<Ace::ShapeRect> Convert(const Ark_Rect& src)
+{
+    auto dst = AceType::MakeRefPtr<Ace::ShapeRect>();
+    float left = Converter::Convert<float>(src.left);
+    float top = Converter::Convert<float>(src.top);
+    float right = Converter::Convert<float>(src.right);
+    float bottom = Converter::Convert<float>(src.bottom);
+    auto width = Dimension(right - left);
+    auto height = Dimension(bottom - top);
+    auto deltaX = Dimension(left);
+    auto deltaY = Dimension(top);
+    auto position = DimensionOffset(deltaX, deltaY);
+    dst->SetWidth(width);
+    dst->SetHeight(height);
+    dst->SetPosition(position);
+    return dst;
+}
+template<>
+Ace::Radius Convert(const Ark_Vector2& src)
+{
+    Ace::Radius dst;
+    auto x = Converter::Convert<float>(src.x);
+    auto y = Converter::Convert<float>(src.y);
+    dst.SetX(Dimension(x));
+    dst.SetY(Dimension(y));
+    return dst;
+}
+template<>
+Ace::Corner Convert(const Ark_CornerRadius& src)
+{
+    return Ace::Corner {
+        .topLeftRadius = Converter::Convert<Ace::Radius>(src.topLeft),
+        .topRightRadius = Converter::Convert<Ace::Radius>(src.topRight),
+        .bottomLeftRadius = Converter::Convert<Ace::Radius>(src.bottomLeft),
+        .bottomRightRadius = Converter::Convert<Ace::Radius>(src.bottomRight),
+    };
+}
+template<>
+RefPtr<Ace::ShapeRect> Convert(const Ark_RoundRect& src)
+{
+    auto dst = Converter::Convert<RefPtr<Ace::ShapeRect>>(src.rect);
+    auto corners = Converter::Convert<Ace::Corner>(src.corners);
+    dst->SetTopLeftRadius(corners.topLeftRadius);
+    dst->SetTopRightRadius(corners.topRightRadius);
+    dst->SetBottomLeftRadius(corners.bottomLeftRadius);
+    dst->SetBottomRightRadius(corners.bottomRightRadius);
+    return dst;
+}
+template<>
+RefPtr<Ace::Circle> Convert(const Ark_Circle& src)
+{
+    auto dst = AceType::MakeRefPtr<Ace::Circle>();
+    auto centerX = Converter::Convert<float>(src.centerX);
+    auto centerY = Converter::Convert<float>(src.centerY);
+    auto radius = Converter::Convert<float>(src.radius);
+    auto position = DimensionOffset(Dimension(centerX), Dimension(centerY));
+    dst->SetPosition(position);
+    dst->SetRadius(Dimension(radius));
+    return dst;
+}
+template<>
+std::string Convert(const Ark_CommandPath& src)
+{
+    return Converter::Convert<std::string>(src.commands);
+}
+} // namespace OHOS::Ace::NG::Converter
+} // namespace OHOS::Ace::NG
 namespace OHOS::Ace::NG::GeneratedModifier {
 namespace ShapeMaskAccessor {
 void DestroyPeerImpl(Ark_ShapeMask peer)
 {
+    PeerUtils::DestroyPeer(peer);
 }
 Ark_ShapeMask CtorImpl()
 {
-    return nullptr;
+    return PeerUtils::CreatePeer<ShapeMaskPeer>();
 }
 Ark_NativePointer GetFinalizerImpl()
 {
@@ -33,22 +106,37 @@ Ark_NativePointer GetFinalizerImpl()
 void SetRectShapeImpl(Ark_ShapeMask peer,
                       const Ark_Rect* rect)
 {
+    if (peer && rect) {
+        peer->SetRectShape(Converter::Convert<RefPtr<Ace::ShapeRect>>(*rect));
+    }
 }
 void SetRoundRectShapeImpl(Ark_ShapeMask peer,
                            const Ark_RoundRect* roundRect)
 {
+    if (peer && roundRect) {
+        peer->SetRoundRectShape(Converter::Convert<RefPtr<Ace::ShapeRect>>(*roundRect));
+    }
 }
 void SetCircleShapeImpl(Ark_ShapeMask peer,
                         const Ark_Circle* circle)
 {
+    if (peer && circle) {
+        peer->SetCircleShape(Converter::Convert<RefPtr<Ace::Circle>>(*circle));
+    }
 }
 void SetOvalShapeImpl(Ark_ShapeMask peer,
                       const Ark_Rect* oval)
 {
+    if (peer && oval) {
+        peer->SetOvalShape(Converter::Convert<RefPtr<Ace::ShapeRect>>(*oval));
+    }
 }
 void SetCommandPathImpl(Ark_ShapeMask peer,
                         const Ark_CommandPath* path)
 {
+    if (peer && path) {
+        peer->SetCommandPath(Converter::Convert<std::string>(*path));
+    }
 }
 Ark_Number GetFillColorImpl(Ark_ShapeMask peer)
 {
