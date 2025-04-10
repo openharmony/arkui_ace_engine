9b3e90a573ec2691b773c60e174afbf0c75f44fd
coolfeng <zhangshengfeng6@huawei.com>
2025-04-10 11:43:32 +0800

    嵌套滚动方向锁定
    Signed-off-by: coolfeng <zhangshengfeng6@huawei.com>

--- frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
+++ frameworks/core/components_ng/pattern/scrollable/scrollable_pattern.cpp
@@ -488,17 +488,16 @@ bool ScrollablePattern::CoordinateWithNavigation(double& offset, int32_t source,
 
 void ScrollablePattern::SetUiDvsyncSwitch(bool on)
 {
-    TAG_LOGI(AceLogTag::ACE_WEB, "WebPattern::SetUiDvsyncSwitch ");
     auto context = GetContext();
     CHECK_NULL_VOID(context);
     bool WebNestedScrollExisted = GetWebNestedScrollExisted();
     if (on && inScrollingStatus_ && !WebNestedScrollExisted) {
-        TAG_LOGI(AceLogTag::ACE_WEB, "WebPattern::SetUiDvsyncSwitch SetUiDvsyncSwitch true");
+        TAG_LOGI(AceLogTag::ACE_SCROLLABLE, "ScrollablePattern::SetUiDvsyncSwitch SetUiDvsyncSwitch true");
         inScrollingStatus_ = false;
         context->SetUiDvsyncSwitch(true);
         switchOnStatus_ = true;
     } else if (!on && switchOnStatus_) {
-        TAG_LOGI(AceLogTag::ACE_WEB, "WebPattern::SetUiDvsyncSwitch SetUiDvsyncSwitch false");
+        TAG_LOGI(AceLogTag::ACE_SCROLLABLE, "ScrollablePattern::SetUiDvsyncSwitch SetUiDvsyncSwitch false");
         context->SetUiDvsyncSwitch(false);
         switchOnStatus_ = false;
     }
