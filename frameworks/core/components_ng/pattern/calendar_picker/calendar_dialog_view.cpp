31f786cc6965777779983b6bdc5e1d26df8ea62a
zhanghang <zhanghang160@huawei-partners.com>
2025-04-14 10:18:15 +0800

    update code for report code.(calendarpicker)
    Signed-off-by: zhanghang <zhanghang160@huawei-partners.com>

--- frameworks/core/components_ng/pattern/calendar_picker/calendar_dialog_view.cpp
+++ frameworks/core/components_ng/pattern/calendar_picker/calendar_dialog_view.cpp
@@ -1278,15 +1278,17 @@ bool CalendarDialogView::ReportChangeEvent(const RefPtr<FrameNode>& frameNode, c
 bool CalendarDialogView::ReportChangeEvent(int32_t nodeId, const std::string& compName,
     const std::string& eventName, const PickerDate& pickerDate)
 {
-#if !defined(PREVIEW) && !defined(ACE_UNITTEST) && defined(OHOS_PLATFORM)
-    auto value = InspectorJsonUtil::Create();
+    auto params = JsonUtil::Create();
+    CHECK_NULL_RETURN(params, false);
+    params->Put("year", static_cast<int32_t>(pickerDate.GetYear()));
+    params->Put("month", static_cast<int32_t>(pickerDate.GetMonth()));
+    params->Put("day", static_cast<int32_t>(pickerDate.GetDay()));
+    auto value = JsonUtil::Create();
     CHECK_NULL_RETURN(value, false);
+    value->Put("nodeId", nodeId);
     value->Put(compName.c_str(), eventName.c_str());
-    value->Put("year", pickerDate.GetYear());
-    value->Put("month", pickerDate.GetMonth());
-    value->Put("day", pickerDate.GetDay());
-    UiSessionManager::GetInstance()->ReportComponentChangeEvent(nodeId, "event", value);
-#endif
+    value->Put("params", params);
+    UiSessionManager::GetInstance()->ReportComponentChangeEvent("event", value->ToString());
     return true;
 }
 
