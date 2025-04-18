0a01862a50ad9a4299e3892aa5cc78d3f514501e
quchongchong <quchongchong@huawei.com>
2025-04-18 15:40:22 +0800

    UEC蒙层优化
    Change-Id: I676ba72e98640e662ee0bded8870af37b73346ac
    Signed-off-by: quchongchong <quchongchong@huawei.com>

--- test/unittest/core/pattern/dialog/dialog_pattern_test_ng.cpp
+++ test/unittest/core/pattern/dialog/dialog_pattern_test_ng.cpp
@@ -899,11 +899,6 @@ HWTEST_F(DialogPatternAdditionalTestNg, DialogPatternAdditionalTestNgAddExtraMas
     props.isModal = false;
     pattern->AddExtraMaskNode(props);
     EXPECT_EQ(totalChildCount, frameNode->GetTotalChildCount());
-
-    pattern->isUIExtensionSubWindow_ = true;
-    props.isModal = true;
-    pattern->AddExtraMaskNode(props);
-    EXPECT_EQ(totalChildCount + 1, frameNode->GetTotalChildCount());
 }
 
 /**
