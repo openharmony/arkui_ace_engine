82854b58d436b5125067e3ebb98e3491efad672a
liukaii <liukai240@huawei.com>
2025-04-21 15:05:03 +0800

    ss2
    Signed-off-by: liukaii <liukai240@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/jsi/nativeModule/arkts_native_common_bridge.cpp
+++ frameworks/bridge/declarative_frontend/engine/jsi/nativeModule/arkts_native_common_bridge.cpp
@@ -5643,7 +5643,7 @@ ArkUINativeModuleValue CommonBridge::SetDragPreview(ArkUIRuntimeCallInfo* runtim
 #endif
             if (pixmap) {
                 auto pixelMapSharedPtr = pixmap->GetPixelMapSharedPtr();
-                dragPreview.pixelMap = &pixelMapSharedPtr;
+                dragPreview.pixelMap = static_cast<void*>(&pixelMapSharedPtr);
             }
         }
     }
