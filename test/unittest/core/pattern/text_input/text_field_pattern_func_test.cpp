9a18a151ac900b8c3663d422fac5025e989f3377
xiangyuan <xiangyuan6@huawei.com>
2025-04-23 13:42:27 +0800

    优化TextField创建耗时
    Signed-off-by: xiangyuan <xiangyuan6@huawei.com>

--- test/unittest/core/pattern/text_input/text_field_pattern_func_test.cpp
+++ test/unittest/core/pattern/text_input/text_field_pattern_func_test.cpp
@@ -2044,6 +2044,7 @@ HWTEST_F(TextFieldPatternFuncTest, TextFieldSelectOverlay001, TestSize.Level1)
     pattern->selectOverlay_->OnOverlayClick(event, true);
     pattern->selectOverlay_->isSingleHandle_ = true;
     pattern->selectOverlay_->OnOverlayClick(event, true);
+    pattern->multipleClickRecognizer_ = pattern->GetOrCreateMultipleClickRecognizer();
     pattern->multipleClickRecognizer_->clickCountTask_.Reset([] {});
     pattern->selectOverlay_->OnOverlayClick(event, true);
     pattern->multipleClickRecognizer_->lastClickPosition_ = Offset(0.0f, 0.0f);
