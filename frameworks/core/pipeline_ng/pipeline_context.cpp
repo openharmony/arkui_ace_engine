b35a445858b8c7a48d915846bcb1c7456c5ef203
lly920173971 <920173971@qq.com>
2025-04-23 07:00:03 +0000

    update frameworks/core/pipeline_ng/pipeline_context.cpp.
    Signed-off-by: lly920173971 <920173971@qq.com>

--- frameworks/core/pipeline_ng/pipeline_context.cpp
+++ frameworks/core/pipeline_ng/pipeline_context.cpp
@@ -1613,7 +1613,7 @@ void PipelineContext::StartWindowSizeChangeAnimate(int32_t width, int32_t height
             break;
         }
         case WindowSizeChangeReason::MAXIMIZE_IN_IMPLICT: {
-            MaximizeToImmersiveAnimation(width, height, type, rsTransaction);
+            MaximizeInImplictAnimation(width, height, type, rsTransaction);
             break;
         }
         case WindowSizeChangeReason::ROTATION: {
@@ -1786,7 +1786,7 @@ void PipelineContext::StartSplitWindowAnimation(int32_t width, int32_t height, W
 #endif
 }
 
-void PipelineContext::MaximizeToImmersiveAnimation(int32_t width, int32_t height, WindowSizeChangeReason type,
+void PipelineContext::MaximizeInImplictAnimation(int32_t width, int32_t height, WindowSizeChangeReason type,
     const std::shared_ptr<Rosen::RSTransaction>& rsTransaction)
 {
     TAG_LOGI(AceLogTag::ACE_WINDOW_SCENE,
