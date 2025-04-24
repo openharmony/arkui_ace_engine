368439f09c654becd3b5c0db46843cd128ccb160
lly920173971 <920173971@qq.com>
2025-04-24 06:21:36 +0000

    update frameworks/core/common/window_animation_config.h.
    Signed-off-by: lly920173971 <920173971@qq.com>

--- frameworks/core/common/window_animation_config.h
+++ frameworks/core/common/window_animation_config.h
@@ -50,7 +50,7 @@ enum class WindowSizeChangeReason : uint32_t {
     SPLIT_DRAG,
     SPLIT_DRAG_END,
     RESIZE_BY_LIMIT,
-    MAXIMIZE_IN_IMPLICT,
+    MAXIMIZE_IN_IMPLICT = 32,
     END,
 };
 } // namespace OHOS::Ace
