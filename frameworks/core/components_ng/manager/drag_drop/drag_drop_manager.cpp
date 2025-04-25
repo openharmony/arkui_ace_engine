2fc203f9da21f8df1018ec3ca673766b41bf8ae8
wangzhihao <wangzhihao42@huawei.com>
2025-04-25 23:10:25 +0800

    优化角标显示逻辑
    Signed-off-by: wangzhihao <wangzhihao42@huawei.com>

--- frameworks/core/components_ng/manager/drag_drop/drag_drop_manager.cpp
+++ frameworks/core/components_ng/manager/drag_drop/drag_drop_manager.cpp
@@ -1015,10 +1015,7 @@ void DragDropManager::OnDragMove(const DragPointerEvent& pointerEvent, const std
             FireOnDragEvent(preTargetFrameNode_, pointerEvent, DragEventType::LEAVE, extraInfo);
             preTargetFrameNode_ = nullptr;
         }
-
-        if (!isMouseDragged_ || isDragWindowShow_) {
-            UpdateDragStyle(DragCursorStyleCore::MOVE, pointerEvent.pointerEventId);
-        }
+        UpdateDragStyle(DragCursorStyleCore::MOVE, pointerEvent.pointerEventId);
         return;
     }
     HandleOnDragMove(pointerEvent, extraInfo, dragFrameNode);
@@ -1623,9 +1620,6 @@ void DragDropManager::FireOnDragEvent(
     FireOnEditableTextComponent(frameNode, type);
     FireOnDragEventWithDragType(eventHub, type, event, extraParams);
 
-    if (isMouseDragged_ && !isDragWindowShow_) {
-        return;
-    }
     UpdateDragCursorStyle(frameNode, event, pointerEvent.pointerEventId);
 }
 
