8b552f1ca968c5c9d8aac7465f9bf279ff95fdb2
chenkexin <chenkexin13@h-partners.com>
2025-04-25 10:09:30 +0800

    【平板PC软件】【兼容模式】支持强制分栏动态切换
    Signed-off-by: chenkexin <chenkexin13@h-partners.com>

--- frameworks/core/components_ng/pattern/stage/stage_manager.h
+++ frameworks/core/components_ng/pattern/stage/stage_manager.h
@@ -77,7 +77,7 @@ public:
 
     void OnForceSplitConfigUpdated ()
     {
-        TAG_LOGI(AceLogTag::ACE_ROUTER, "OnForceSplitConfigUpdated")
+        TAG_LOGI(AceLogTag::ACE_ROUTER, "OnForceSplitConfigUpdated");
         stagePattern_->OnForceSplitConfigUpdated();
     }
 
