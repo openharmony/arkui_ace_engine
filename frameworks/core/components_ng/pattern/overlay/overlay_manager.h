33aee039760916e7ce8bb748bcaba93be67fdf70
zhaoyong <zhaoyong70@huawei.com>
2025-04-27 23:25:55 +0800

    fix dialog masknode bug
    Signed-off-by: zhaoyong <zhaoyong70@huawei.com>

--- frameworks/core/components_ng/pattern/overlay/overlay_manager.h
+++ frameworks/core/components_ng/pattern/overlay/overlay_manager.h
@@ -284,6 +284,8 @@ public:
      */
     bool RemoveOverlay(bool isBackPressed, bool isPageRouter = false);
     bool RemoveDialog(const RefPtr<FrameNode>& overlay, bool isBackPressed, bool isPageRouter = false);
+    bool RemoveDialogWithContent(
+        const RefPtr<FrameNode>& overlay, const DialogProperties& props, bool isBackPressed, bool isPageRouter = false);
     bool RemoveBubble(const RefPtr<FrameNode>& overlay);
     bool RemoveMenu(const RefPtr<FrameNode>& overlay);
     bool RemoveDragPreview(const RefPtr<FrameNode>& overlay);
