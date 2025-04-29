40a4bcd8790830e0aaf7ce97805d003c24b8c12c
liang_qiyuan <liangqiyuan@huawei.com>
2025-04-29 17:54:25 +0800

    5.1.0
    Signed-off-by: liang_qiyuan <liangqiyuan@huawei.com>

--- frameworks/core/components_ng/pattern/dialog/dialog_pattern.h
+++ frameworks/core/components_ng/pattern/dialog/dialog_pattern.h
@@ -69,7 +69,7 @@ public:
 
     bool ShouldDismiss() const
     {
-        if (onWillDismiss_) {
+        if (onWillDismiss_ && !isDialogDisposed_) {
             return true;
         }
         return false;
@@ -90,7 +90,7 @@ public:
 
     void CallOnWillDismiss(const int32_t reason, const int32_t instanceId)
     {
-        if (onWillDismiss_) {
+        if (onWillDismiss_ && !isDialogDisposed_) {
             onWillDismiss_(reason, instanceId);
         }
     }
@@ -311,6 +311,11 @@ public:
         return true;
     }
 
+    void SetIsDialogDisposed(bool isDialogDisposed)
+    {
+        isDialogDisposed_ = isDialogDisposed;
+    }
+
     bool IsShowInFreeMultiWindow();
     bool IsShowInFloatingWindow();
     void AddExtraMaskNode(const DialogProperties& props);
@@ -436,6 +441,7 @@ private:
     std::function<void()> onWillDisappearCallback_ = nullptr;
     std::unordered_map<DialogContentNode, RefPtr<FrameNode>> contentNodeMap_;
     bool isUIExtensionSubWindow_ = false;
+    bool isDialogDisposed_ = false;
     RectF hostWindowRect_;
 };
 } // namespace OHOS::Ace::NG
