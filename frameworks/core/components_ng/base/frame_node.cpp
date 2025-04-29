5e234e467449ceac6b25c205f0e0c1f3f623a88e
zjucx <chengxiang4@huawei.com>
2025-04-29 11:02:56 +0800

    fix mem leak
    Signed-off-by: zjucx <chengxiang4@huawei.com>

--- frameworks/core/components_ng/base/frame_node.cpp
+++ frameworks/core/components_ng/base/frame_node.cpp
@@ -4048,7 +4048,7 @@ bool FrameNode::OnRemoveFromParent(bool allowTransition)
 {
     // the node set isInDestroying state when destroying in pop animation
     // when in isInDestroying state node should not DetachFromMainTree preventing pop page from being white
-    if (IsDestroyingState()) {
+    if (IsDestroyingState() && GetContext() && !GetContext()->IsDestroyed()) {
         return false;
     }
     // kick out transition animation if needed, wont re-entry if already detached.
