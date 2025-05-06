e73df5fd54bb55866cf52d440091a5488501d789
hongzexuan <hongzexuan@huawei.com>
2025-05-06 17:16:16 +0800

    fix scroll snap
    Signed-off-by: hongzexuan <hongzexuan@huawei.com>
    Change-Id: I8d7a9a1a4b92c122801223b39b3a17cdd55f293c

--- frameworks/core/components_ng/pattern/scrollable/scrollable.cpp
+++ frameworks/core/components_ng/pattern/scrollable/scrollable.cpp
@@ -1450,7 +1450,7 @@ double Scrollable::CalcNextStep(double position, double mainDelta)
         return nextStep_.value();
     }
     if (LessOrEqual(std::abs(mainDelta), SCROLL_SNAP_MIN_STEP)) {
-        nextStep_ = Positive(mainDelta) ? SCROLL_SNAP_MIN_STEP : -SCROLL_SNAP_MIN_STEP;
+        nextStep_ = Positive(finalDelta) ? SCROLL_SNAP_MIN_STEP : -SCROLL_SNAP_MIN_STEP;
         mainDelta = nextStep_.value();
     }
     return mainDelta;
