41b7b13e4e56387ea29a41a44f0bc74410c495b3
李肖葛 <lixiaoge1@huawei.com>
2025-05-08 17:23:19 +0800

    卡片点击新增维测日志、
    Signed-off-by: 李肖葛 <lixiaoge1@huawei.com>

--- interfaces/inner_api/form_render/src/form_renderer_dispatcher_impl.cpp
+++ interfaces/inner_api/form_render/src/form_renderer_dispatcher_impl.cpp
@@ -65,6 +65,7 @@ void FormRendererDispatcherImpl::DispatchPointerEvent(
 
         std::shared_ptr<FormSerializedResultData> serializedResultData = std::make_shared<FormSerializedResultData>();
         auto callback = [serializedResultData]() {
+            HILOG_INFO("dowm event process callback");
             std::unique_lock<std::mutex> lock(serializedResultData->mtx);
             serializedResultData->cv.notify_all();
         };
