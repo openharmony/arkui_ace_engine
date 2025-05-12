adc91092e19b42c901f8b21c8ff411610e8fd27a
Tianer Zhou <zhoutianer@huawei.com>
2025-05-12 16:13:53 +0800

    refactor synchronizer
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: Ic4d4d90f5ec3bce0d8910f7e0dfce35f74c5b790

--- frameworks/core/components_ng/layout/section/layout_synchronizer.cpp
+++ frameworks/core/components_ng/layout/section/layout_synchronizer.cpp
@@ -16,39 +16,39 @@
 
 #include "core/components_ng/layout/section/staggered_fill_algorithm.h"
 #include "frameworks/core/components_ng/base/frame_node.h"
-#include "frameworks/core/components_ng/pattern/list/list_layout_property.h"
 #include "frameworks/core/components_ng/pattern/list/list_pattern.h"
-#include "frameworks/core/components_ng/pattern/swiper/swiper_layout_property.h"
 #include "frameworks/core/components_ng/pattern/swiper/swiper_pattern.h"
-#include "frameworks/core/components_ng/pattern/waterflow/water_flow_layout_property.h"
 #include "frameworks/core/components_ng/pattern/waterflow/water_flow_pattern.h"
-namespace OHOS::Ace::NG {
 
+namespace OHOS::Ace::NG {
 void LayoutSynchronizer::Sync(const RefPtr<LayoutProperty>& props, StaggeredFillAlgorithm& fillAlgo)
 {
-    if (auto p = AceType::DynamicCast<WaterFlowLayoutProperty>(props); p) {
-        SyncWaterFlow(p, fillAlgo);
-    } else if (auto p = AceType::DynamicCast<ListLayoutProperty>(props); p) {
-    } else if (auto p = AceType::DynamicCast<SwiperLayoutProperty>(props); p) {
+    const auto truth = GetLayoutTruth(props);
+
+    if (truth.startIdx != fillAlgo.StartIdx() || truth.endIdx != fillAlgo.EndIdx()) {
+        LOGW("Item ranges are out of sync: component range = %d - %d, fill range = %d - %d", truth.startIdx,
+            truth.endIdx, fillAlgo.StartIdx().value_or(-1), fillAlgo.EndIdx().value_or(-1));
+    } else if (truth.startPos != fillAlgo.StartPos()) {
+        float diff = truth.startPos - fillAlgo.StartPos().value_or(0.0f);
+        fillAlgo.OnSlidingOffsetUpdate(diff);
+        LOGI("LayoutSynchronizer adjusted offset in FillAlgorithm by %f", diff);
     }
 }
 
-void LayoutSynchronizer::SyncWaterFlow(const RefPtr<WaterFlowLayoutProperty>& props, StaggeredFillAlgorithm& fillAlgo)
+LayoutSynchronizer::LayoutState LayoutSynchronizer::GetLayoutTruth(const RefPtr<LayoutProperty>& props)
 {
     auto host = props->GetHost();
-    CHECK_NULL_VOID(host);
-    auto pattern = host->GetPattern<WaterFlowPattern>();
-    CHECK_NULL_VOID(pattern);
-    int32_t startIdx = pattern->GetBeginIndex();
-    int32_t endIdx = pattern->GetEndIndex();
-    float startPos = pattern->GetStoredOffset() + pattern->GetPendingDelta();
-    if (startIdx != fillAlgo.StartIdx() || endIdx != fillAlgo.EndIdx()) {
-        LOGW("WaterFlow item ranges are out of sync: component range = %d - %d, fill range = %d - %d", startIdx, endIdx,
-            fillAlgo.StartIdx().value_or(-1), fillAlgo.EndIdx().value_or(-1));
-    } else if (startPos != fillAlgo.StartPos()) {
-        float diff = startPos - fillAlgo.StartPos().value_or(0.0f);
-        fillAlgo.OnSlidingOffsetUpdate(diff);
-        LOGI("synchronizer adjusted WaterFlow offset by %f", diff);
+    CHECK_NULL_RETURN(host, {});
+    if (auto waterFlow = host->GetPattern<WaterFlowPattern>(); waterFlow) {
+        return LayoutState { waterFlow->GetStoredOffset() + waterFlow->GetPendingDelta(), waterFlow->GetBeginIndex(),
+            waterFlow->GetEndIndex() };
     }
+    if (auto list = host->GetPattern<ListPattern>(); list) {
+        return LayoutState { list->GetContentStartOffset() - list->GetCurrentDelta(), list->GetStartIndex(),
+            list->GetEndIndex() };
+    }
+    if (auto swiper = host->GetPattern<SwiperPattern>(); swiper) {
+    }
+    return {};
 }
 } // namespace OHOS::Ace::NG
