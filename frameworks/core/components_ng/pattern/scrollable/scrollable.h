9cf02e7b8cb9917f8d4263a8a07f8309d87ff4e1
gongwq <gongweiqi4@h-partners.com>
2025-05-15 02:23:22 +0000

    修改告警问题
    Signed-off-by: gongwq <gongweiqi4@h-partners.com>

--- frameworks/core/components_ng/pattern/scrollable/scrollable.h
+++ frameworks/core/components_ng/pattern/scrollable/scrollable.h
@@ -57,12 +57,12 @@ struct ScrollResult {
 };
 
 struct SlidInfo {
-    double gestureVelocity;
-    double velocityScale;
-    double gain;
-    double maxFlingVelocity;
-    double slipFactor;
-    double friction;
+    double gestureVelocity {0};
+    double velocityScale {0};
+    double gain {0};
+    double maxFlingVelocity {0};
+    double slipFactor {0};
+    double friction {0};
 };
 
 using ScrollEventCallback = std::function<void()>;
