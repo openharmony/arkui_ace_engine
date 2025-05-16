0dcbae01364b3e9fe7cbf762cfcf3597e161031a
zhanghang <zhanghang160@huawei-partners.com>
2025-05-16 11:15:02 +0800

    可见可说支持sa发送点击响应事件
    Signed-off-by: zhanghang <zhanghang160@huawei-partners.com>

--- frameworks/core/components_ng/pattern/pattern.cpp
+++ frameworks/core/components_ng/pattern/pattern.cpp
@@ -16,6 +16,34 @@
 #include "core/components_ng/pattern/pattern.h"
  
 namespace OHOS::Ace::NG {
+int32_t Pattern::OnRecvCommand(const std::string& command)
+{
+    auto json = JsonUtil::ParseJsonString(command);
+    if (!json || !json->IsValid() || !json->IsObject()) {
+        return RET_FAILED;
+    }
+    auto event = json->GetString("cmd");
+    if (event.compare("click") == 0) {
+        auto host = GetHost();
+        CHECK_NULL_RETURN(host, RET_FAILED);
+        auto eventHub = host->GetOrCreateEventHub<EventHub>();
+        CHECK_NULL_RETURN(eventHub, RET_FAILED);
+        if (!eventHub->IsEnabled()) {
+            return RET_FAILED;
+        }
+        auto gestureHub = host->GetOrCreateGestureEventHub();
+        CHECK_NULL_RETURN(gestureHub, RET_FAILED);
+        auto clickEventFunc = gestureHub->GetClickEvent();
+        CHECK_NULL_RETURN(clickEventFunc, RET_FAILED);
+        GestureEvent info;
+        clickEventFunc(info);
+        return RET_SUCCESS;
+    } else {
+        return OnInjectionEvent(command);
+    }
+    return RET_FAILED;
+}
+
 void Pattern::OnColorModeChange(uint32_t colorMode)
 {
     if (resourceMgr_) {
