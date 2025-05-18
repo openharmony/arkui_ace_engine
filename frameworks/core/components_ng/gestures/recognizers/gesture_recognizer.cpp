b6dc34c72851fa83d9dd19fa574f6a4bd7d79666
刘永凯 <liuyongkai2@huawei-partners.com>
2025-05-18 17:45:15 +0800

    修复轴拖拽问题
    Signed-off-by: 刘永凯 <liuyongkai2@huawei-partners.com>

--- frameworks/core/components_ng/gestures/recognizers/gesture_recognizer.cpp
+++ frameworks/core/components_ng/gestures/recognizers/gesture_recognizer.cpp
@@ -147,7 +147,8 @@ void NGGestureRecognizer::HandleTouchDown(const TouchEvent& point)
     deviceType_ = point.sourceType;
     deviceTool_ = point.sourceTool;
     inputEventType_ = (deviceType_ == SourceType::MOUSE) ? InputEventType::MOUSE_BUTTON : InputEventType::TOUCH_SCREEN;
-
+    originInputEventType_ =
+        (point.originInputEventType == InputEventType::AXIS) ? InputEventType::AXIS : inputEventType_;
     auto result = AboutToAddCurrentFingers(point);
     if (result) {
         HandleTouchDownEvent(point);
@@ -195,6 +196,7 @@ bool NGGestureRecognizer::HandleEvent(const AxisEvent& event)
             deviceType_ = event.sourceType;
             deviceTool_ = event.sourceTool;
             inputEventType_ = InputEventType::AXIS;
+            originInputEventType_ = InputEventType::AXIS;
             HandleTouchDownEvent(event);
             break;
         case AxisAction::UPDATE:
@@ -235,6 +237,8 @@ void NGGestureRecognizer::HandleBridgeModeEvent(const TouchEvent& point)
             } else {
                 inputEventType_ = InputEventType::TOUCH_SCREEN;
             }
+            originInputEventType_ =
+                (point.originInputEventType == InputEventType::AXIS) ? InputEventType::AXIS : inputEventType_;
             auto result = AboutToAddCurrentFingers(point);
             if (result) {
                 HandleTouchDownEvent(point);
@@ -284,6 +288,7 @@ void NGGestureRecognizer::HandleBridgeModeEvent(const AxisEvent& event)
             deviceType_ = event.sourceType;
             deviceTool_ = event.sourceTool;
             inputEventType_ = InputEventType::AXIS;
+            originInputEventType_ = InputEventType::AXIS;
             HandleTouchDownEvent(event);
             break;
         case AxisAction::UPDATE:
