d2d2706baf8e1a2e4ae35337c5cd54396c9fb4bc
zhanghang160@huawei-partners.com <zhanghang160@huawei-partners.com>
2025-05-24 21:44:26 +0800

    fromHtml extend supported tags
    Signed-off-by: zhanghang160@huawei-partners.com <zhanghang160@huawei-partners.com>

--- test/unittest/adapter/ohos/capability/html_to_spanstring_test.cpp
+++ test/unittest/adapter/ohos/capability/html_to_spanstring_test.cpp
@@ -2253,4 +2253,72 @@ HWTEST_F(HtmlConvertTestNg, HtmlConvertTestSuperscriptText, TestSize.Level1)
     EXPECT_EQ((*it)->fontStyle->GetSuperscript().value(), OHOS::Ace::SuperscriptStyle::SUPERSCRIPT);
 }
 
+/**
+ * @tc.name: HtmlTagsConversionTest
+ * @tc.desc: Verify the conversion of HTML tags <a>, <b>, <i>, <u>, <del>, <br>, <strong>, <s>, and <em> to SpanItems.
+ * @tc.level: 1
+ */
+HWTEST_F(HtmlConvertTestNg, HtmlTagsConversionTest, TestSize.Level1)
+{
+    /**
+     * @tc.steps1: Create an HTML string with various tags and convert it to SpanString.
+     * @tc.expected: The tags should be correctly converted to SpanItems with appropriate styles.
+     */
+    const std::string html = "<html>"
+                             "<body>"
+                             "<a href=\"https://example.com\">Link</a>"
+                             "<b>Bold Text</b>"
+                             "<i>Italic Text</i>"
+                             "<u>Underlined Text</u>"
+                             "<del>Deleted Text</del>"
+                             "<br>"
+                             "<strong>Strong Text</strong>"
+                             "<s>Strikethrough Text</s>"
+                             "<em>Emphasized Text</em>"
+                             "</body>"
+                             "</html>";
+
+    HtmlToSpan toSpan;
+    auto dstSpan = toSpan.ToSpanString(html);
+    EXPECT_NE(dstSpan, nullptr);
+
+    std::list<RefPtr<NG::SpanItem>> items = dstSpan->GetSpanItems();
+    EXPECT_EQ(items.size(), 9);
+
+    auto it = items.begin();
+
+    // Verify <b> tag
+    ++it;
+    EXPECT_TRUE((*it)->fontStyle->GetFontWeight().has_value());
+    EXPECT_EQ((*it)->fontStyle->GetFontWeight().value(), FontWeight::BOLD);
+
+    // Verify <i> tag
+    ++it;
+    EXPECT_EQ((*it)->fontStyle->GetItalicFontStyle().value(), Ace::FontStyle::ITALIC);
+
+    // Verify <u> tag
+    ++it;
+    EXPECT_EQ((*it)->fontStyle->GetTextDecorationFirst(), TextDecoration::UNDERLINE);
+
+    // Verify <del> tag
+    ++it;
+    EXPECT_EQ((*it)->fontStyle->GetTextDecorationFirst(), TextDecoration::LINE_THROUGH);
+
+    // Verify <br> tag (no specific style to check)
+    ++it;
+
+    // Verify <strong> tag
+    ++it;
+    EXPECT_TRUE((*it)->fontStyle->GetFontWeight().has_value());
+    EXPECT_EQ((*it)->fontStyle->GetFontWeight().value(), FontWeight::BOLD);
+
+    // Verify <s> tag
+    ++it;
+    EXPECT_EQ((*it)->fontStyle->GetTextDecorationFirst(), TextDecoration::LINE_THROUGH);
+
+    // Verify <em> tag
+    ++it;
+    EXPECT_EQ((*it)->fontStyle->GetItalicFontStyle().value(), Ace::FontStyle::ITALIC);
+}
+
 } // namespace OHOS::Ace::NG
