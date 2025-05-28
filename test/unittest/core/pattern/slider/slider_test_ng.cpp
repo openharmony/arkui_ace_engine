6d20fee472189c290d9f043d192cc5eab86b96bb
liang_qiyuan <liangqiyuan@huawei.com>
2025-05-28 20:22:30 +0800

    add tdd
    Signed-off-by: liang_qiyuan <liangqiyuan@huawei.com>

--- test/unittest/core/pattern/slider/slider_test_ng.cpp
+++ test/unittest/core/pattern/slider/slider_test_ng.cpp
@@ -847,26 +847,26 @@ HWTEST_F(SliderTestNg, SliderTestNg012, TestSize.Level1)
  * @tc.desc: Test Slider Trace Color
  * @tc.type: FUNC
  */
- HWTEST_F(SliderTestNg, SliderTestNg013, TestSize.Level1)
- {
-     /**
-      * @tc.steps: step1. create slider and set the properties, and then get frameNode.
-      */
-     SliderModelNG sliderModelNG;
-     sliderModelNG.Create(VALUE, STEP, MIN, MAX);
-     sliderModelNG.SetTrackBackgroundColor(SliderModelNG::CreateSolidGradient(TEST_COLOR));
-     auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
-     EXPECT_NE(frameNode, nullptr);
- 
-     /**
-      * @tc.steps: step2. get the properties of all settings.
-      * @tc.expected: step2. check whether the properties is correct.
-      */
-     auto sliderPaintProperty = frameNode->GetPaintProperty<SliderPaintProperty>();
-     EXPECT_NE(sliderPaintProperty, nullptr);
-     EXPECT_EQ(true, sliderPaintProperty->GetTrackBackgroundColor().has_value());
-     EXPECT_EQ(sliderPaintProperty->GetTrackBackgroundColor().value(), SliderModelNG::CreateSolidGradient(TEST_COLOR));
- }
+HWTEST_F(SliderTestNg, SliderTestNg013, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create slider and set the properties, and then get frameNode.
+     */
+    SliderModelNG sliderModelNG;
+    sliderModelNG.Create(VALUE, STEP, MIN, MAX);
+    sliderModelNG.SetTrackBackgroundColor(SliderModelNG::CreateSolidGradient(TEST_COLOR));
+    auto frameNode = AceType::DynamicCast<FrameNode>(ViewStackProcessor::GetInstance()->Finish());
+    EXPECT_NE(frameNode, nullptr);
+
+    /**
+     * @tc.steps: step2. get the properties of all settings.
+     * @tc.expected: step2. check whether the properties is correct.
+     */
+    auto sliderPaintProperty = frameNode->GetPaintProperty<SliderPaintProperty>();
+    EXPECT_NE(sliderPaintProperty, nullptr);
+    EXPECT_EQ(true, sliderPaintProperty->GetTrackBackgroundColor().has_value());
+    EXPECT_EQ(sliderPaintProperty->GetTrackBackgroundColor().value(), SliderModelNG::CreateSolidGradient(TEST_COLOR));
+}
 
 /**
  * @tc.name: SliderLayoutAlgorithm001
