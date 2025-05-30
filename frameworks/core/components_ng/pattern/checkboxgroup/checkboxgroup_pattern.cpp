8fa26f7e1287510070b5d085384abd05c5251e0f
liang_qiyuan <liangqiyuan@huawei.com>
2025-05-30 14:29:58 +0800

    checkbox
    Signed-off-by: liang_qiyuan <liangqiyuan@huawei.com>

--- frameworks/core/components_ng/pattern/checkboxgroup/checkboxgroup_pattern.cpp
+++ frameworks/core/components_ng/pattern/checkboxgroup/checkboxgroup_pattern.cpp
@@ -46,7 +46,11 @@ void CheckBoxGroupPattern::OnDetachFromFrameNode(FrameNode* frameNode)
     CHECK_NULL_VOID(frameNode);
     auto groupManager = GetGroupManager();
     CHECK_NULL_VOID(groupManager);
-    groupManager->RemoveCheckBoxGroup(GetGroupNameWithNavId(), frameNode->GetId());
+    auto eventHub = frameNode->GetEventHub<CheckBoxGroupEventHub>();
+    CHECK_NULL_VOID(eventHub);
+    auto group = currentNavId_.has_value() ? eventHub->GetGroupName() + currentNavId_.value()
+                                           : eventHub->GetGroupName() + groupManager->GetLastNavId();
+    groupManager->RemoveCheckBoxGroup(group, frameNode->GetId());
 }
 
 void CheckBoxGroupPattern::OnModifyDone()
