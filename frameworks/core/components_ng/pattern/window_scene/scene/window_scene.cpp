27ab06ed0124de3887ab31f0f52b49a0b265f5df
yecong2332 <yecong5@h-partners.com>
2025-05-30 10:22:01 +0800

    修复多任务子窗可见性
    Signed-off-by: yecong2332 <yecong5@h-partners.com>

--- frameworks/core/components_ng/pattern/window_scene/scene/window_scene.cpp
+++ frameworks/core/components_ng/pattern/window_scene/scene/window_scene.cpp
@@ -153,14 +153,15 @@ void WindowScene::OnAttachToMainTree()
     if (IsMainWindow()) {
         return;
     }
+    auto surfaceNode = session_->GetSurfaceNode();
+    CHECK_NULL_VOID(surfaceNode);
+    TAG_LOGI(AceLogTag::ACE_WINDOW_SCENE,
+        "OnAttachToMainTree id:%{public}d, nodeId:%{public}d, type:%{public}d, name:%{public}s",
+        session_->GetPersistentId(), host->GetId(), session_->GetWindowType(), windowName.c_str());
     if (IsMainSessionRecent()) {
-        TAG_LOGI(AceLogTag::ACE_WINDOW_SCENE,
-            "OnAttachToMainTree id:%{public}d, nodeId:%{public}d, type:%{public}d, name:%{public}s",
-            session_->GetPersistentId(), host->GetId(), session_->GetWindowType(), windowName.c_str());
-        auto surfaceNode = session_->GetSurfaceNode();
-        if (surfaceNode) {
-            surfaceNode->SetVisible(false);
-        }
+        surfaceNode->SetVisible(false);
+    } else {
+        surfaceNode->SetVisible(true);
     }
 }
 
@@ -572,6 +573,7 @@ void WindowScene::OnActivation()
         } else if (self->snapshotWindow_) {
             self->session_->SetEnableAddSnapshot(true);
             self->DisposeSnapshotAndBlankWindow();
+            self->SetSubSessionVisible();
         }
     };
 
