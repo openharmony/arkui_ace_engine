d9650c8c226b311955b9e89762db151cd574c689
qianli22 <qianli22@huawei.com>
2025-06-03 20:25:50 +0800

    异步任务动画需请求vsync
    Signed-off-by: qianli22 <qianli22@huawei.com>

--- adapter/ohos/entrance/ui_content_impl.cpp
+++ adapter/ohos/entrance/ui_content_impl.cpp
@@ -3226,13 +3226,18 @@ void UIContentImpl::ExecKeyFrameCachedAnimateAction()
 
     if (cachedAnimateFlag_.load()) {
         const uint32_t delay = 50;
+        auto pipeline = AceType::DynamicCast<NG::PipelineContext>(container->GetPipelineContext());
+        CHECK_NULL_VOID(pipeline);
         auto task = [cachedConfig = cachedConfig_, cachedReason = cachedReason_,
             cachedRsTransaction = cachedRsTransaction_, cachedAvoidAreas = cachedAvoidAreas_,
-            UICONTENT_IMPL_HELPER(content)] () {
+            weak = WeakPtr<NG::PipelineContext>(pipeline), UICONTENT_IMPL_HELPER(content)] () {
+            auto pipeline = weak.Upgrade();
+            CHECK_NULL_VOID(pipeline);
             UICONTENT_IMPL_HELPER_GUARD(content, return);
             TAG_LOGD(AceLogTag::ACE_WINDOW, "exec keyframe cache");
             UICONTENT_IMPL_PTR(content)->UpdateViewportConfig(cachedConfig, cachedReason,
                 cachedRsTransaction, cachedAvoidAreas);
+            pipeline->RequestFrame();
         };
         taskExecutor->PostDelayedTask(task, TaskExecutor::TaskType::UI, delay,
             "ArkUIExecKeyFrameCachedAnimateTask", PriorityType::HIGH);
