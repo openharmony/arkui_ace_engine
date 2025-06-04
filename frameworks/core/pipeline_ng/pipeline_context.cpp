f310bdfc4e848c069e562687d4f85f6aa7a3e35c
zhoukechen <zhoukechen2@huawei.com>
2025-06-04 15:32:16 +0800

    去除pipelinecontext摧毁关闭键盘的逻辑
    Signed-off-by: zhoukechen <zhoukechen2@huawei.com>
    Change-Id: Ia2a9aca9dd3fa6b2b9667eb7b2cc490ab7684448

--- frameworks/core/pipeline_ng/pipeline_context.cpp
+++ frameworks/core/pipeline_ng/pipeline_context.cpp
@@ -4561,9 +4561,6 @@ void PipelineContext::Destroy()
     dirtyFocusScope_.Reset();
     needRenderNode_.clear();
     dirtyRequestFocusNode_.Reset();
-    if (textFieldManager_ && textFieldManager_->GetImeShow()) {
-        InputMethodManager::GetInstance()->CloseKeyboardInPipelineDestroy();
-    }
     auto formEventMgr = this->GetFormEventManager();
     if (formEventMgr) {
         formEventMgr->ClearEtsCardTouchEventCallback();
