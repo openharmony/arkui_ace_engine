a1cbe31455beb7c1c8252c22b8b71d8eddd2e172
Tianer Zhou <zhoutianer@huawei.com>
2025-06-05 19:29:09 +0800

    maxSize on reusable pool
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>
    Change-Id: I629c101bfc81450bdfd616d780d42eec1dcb98e1

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ReusablePool.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ReusablePool.ts
@@ -13,14 +13,15 @@
  * limitations under the License.
  */
 
-import { Disposable } from "@koalaui/runtime";
+import { Disposable, scheduleCallback } from "@koalaui/runtime";
 
 export class ReusablePool implements Disposable {
-    private cache: Disposable[];
-    disposed: boolean = false;
+    private _maxSize: number = Number.POSITIVE_INFINITY
+    private _cache: Disposable[] = []
+    private _disposed: boolean = false;
 
-    constructor() {
-        this.cache = [];
+    get disposed(): boolean {
+        return this._disposed
     }
 
     /**
@@ -28,23 +29,34 @@ export class ReusablePool implements Disposable {
      */
     get(): Disposable | undefined {
         if (this.disposed) return undefined;
-        return this.cache.shift();
+        return this._cache.shift();
     }
 
     /**
      * Adds an item to the pool
      */
     put(value: Disposable): void {
-        if (this.disposed) return;
-        this.cache.push(value);
+        if (this.disposed || this._cache.length >= this._maxSize) return;
+        this._cache.push(value);
     }
 
     dispose(): void {
         if (this.disposed) return;
-        this.disposed = true;
-        for (const value of this.cache) {
+        this._disposed = true;
+        for (const value of this._cache) {
             value.dispose();
         }
-        this.cache = [];
+        this._cache = [];
+    }
+
+    setMaxSize(value: number) {
+        this._maxSize = value
+        if (this._cache.length > this._maxSize) {
+            const removed = this._cache.splice(this._maxSize);
+            scheduleCallback(() => {
+                for (const value of removed)
+                    value.dispose();
+            })
+        }
     }
 }
