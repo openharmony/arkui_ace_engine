083e2d8b31a44c79e90b1bae67bd1d6d751dd274
DESKTOP-H8KLN8I\lisitao <lisitao3@huawei.com>
2025-06-05 09:46:21 +0800

    [L] add modifiy 修改Signed-off-by:lisitaolisitao3@huawei.com
    Signed-off-by: DESKTOP-H8KLN8I\lisitao <lisitao3@huawei.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/external/arkts/index.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/external/arkts/index.ts
@@ -17,6 +17,7 @@ import image from "@ohos.multimedia.image"
 import _Want from "@ohos.app.ability.Want"
 import webview from "@ohos.web.webview"
 import common from "@ohos.app.ability.common"
+import pointer from '@ohos.multimodalInput.pointer'
 
 export * from "@ohos.base"
 
@@ -24,3 +25,4 @@ export type PixelMap = image.PixelMap
 export type AbilityWant = _Want
 export type WebviewController = webview.WebviewController
 export type Context = common.Context
+export type PointerStyle = pointer.PointerStyle
