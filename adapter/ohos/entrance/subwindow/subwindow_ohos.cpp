f13220e59ca96e0e56a68e60031ce11276593ad2
姚明 <yaoming14@huawei.com>
2025-06-10 16:03:02 +0800

    创建子窗优化
    Signed-off-by: 姚明 <yaoming14@huawei.com>

--- adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
+++ adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
@@ -268,7 +268,7 @@ void SubwindowOhos::InitContainer()
         sptr<OHOS::Rosen::Window> parentWindow = parentContainer->GetUIWindow(parentContainerId_);
         CHECK_NULL_VOID(parentWindow);
         parentWindow_ = parentWindow;
-        auto windowType = parentWindow->GetType();
+        auto parentWindowType = parentWindow->GetType();
         std::string windowTag = "";
         bool isAppSubwindow = false;
         bool needFollowScreen = false;
@@ -290,18 +290,18 @@ void SubwindowOhos::InitContainer()
             windowName = "ARK_APP_SUBWINDOW_" + windowTag + parentWindowName + std::to_string(windowId_);
             Rosen::WindowType subwindowType = Rosen::WindowType::WINDOW_TYPE_APP_SUB_WINDOW;
             OHOS::Rosen::WMError ret = OHOS::Rosen::Window::GetAndVerifyWindowTypeForArkUI(parentWindowId, windowName,
-                windowType, subwindowType);
-            TAG_LOGI(AceLogTag::ACE_SUB_WINDOW, "get and verify window type, ret= %{public}d,"
-                "windowType= %{public}d, subwindowType= %{public}d", static_cast<int32_t>(ret),
-                static_cast<int32_t>(windowType), static_cast<int32_t>(subwindowType)); 
+                parentWindowType, subwindowType);
+            TAG_LOGI(AceLogTag::ACE_SUB_WINDOW, "get and verify window type, ret = %{public}d,"
+                " parentWindowType = %{public}d, subwindowType = %{public}d", static_cast<int32_t>(ret),
+                static_cast<int32_t>(parentWindowType), static_cast<int32_t>(subwindowType));
             if (ret != OHOS::Rosen::WMError::WM_OK) {
                 return;
             }
             windowOption->SetWindowType(subwindowType);
             needFollowScreen = parentContainer->IsSceneBoardWindow() ||
-                windowType >= Rosen::WindowType::SYSTEM_WINDOW_BASE;
+                parentWindowType >= Rosen::WindowType::SYSTEM_WINDOW_BASE;
             isAppSubwindow = subwindowType == Rosen::WindowType::WINDOW_TYPE_APP_SUB_WINDOW;
-            bool isUIExt = windowType == Rosen::WindowType::WINDOW_TYPE_UI_EXTENSION;
+            bool isUIExt = parentWindowType == Rosen::WindowType::WINDOW_TYPE_UI_EXTENSION;
             if (isAppSubwindow || subwindowType == Rosen::WindowType::WINDOW_TYPE_SYSTEM_SUB_WINDOW) {
                 windowOption->SetParentId(isUIExt ? parentPipeline->GetFocusWindowId() : parentWindowId);
             }
@@ -329,7 +329,7 @@ void SubwindowOhos::InitContainer()
         SetUIExtensionSubwindowFlag(windowOption, isAppSubwindow, parentWindow);
         windowOption->SetDisplayId(displayId);
         OHOS::Rosen::WMError ret;
-        window_ = OHOS::Rosen::Window::Create(, windowOption, parentWindow->GetContext(), ret);
+        window_ = OHOS::Rosen::Window::Create(windowName, windowOption, parentWindow->GetContext(), ret);
         if (!window_ || ret != OHOS::Rosen::WMError::WM_OK) {
             SetIsRosenWindowCreate(false);
             TAG_LOGW(AceLogTag::ACE_SUB_WINDOW, "Window create failed, errCode is %{public}d", ret);
