67036002fb948c28f0f4081e966b2d96b545e835
zhanghang <zhanghang160@huawei-partners.com>
2025-06-11 11:33:09 +0800

    feat: bindtips支持跟随鼠标tdd
    Signed-off-by: zhanghang <zhanghang160@huawei-partners.com>
    Change-Id: I379250b46cbceec43ba0432f555d28d7d50bebf3

--- adapter/ohos/entrance/ace_container.h
+++ adapter/ohos/entrance/ace_container.h
@@ -817,6 +817,14 @@ public:
         return uiWindow_->GetFreeMultiWindowModeEnabledState();
     }
 
+    Rect GetGlobalScaledRect() const override
+    {
+        CHECK_NULL_RETURN(uiWindow_, Rect());
+        Rosen::Rect rect{};
+        uiWindow_->GetGlobalScaledRect(rect);
+        return Rect(rect.posX_, rect.posY_, rect.width_, rect.height_);
+    }
+
     bool IsWaterfallWindow() const override
     {
         CHECK_NULL_RETURN(uiWindow_, false);
