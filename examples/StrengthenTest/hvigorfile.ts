4c1c38f2c0d3f08e9e8ffbe1aeffd2ae30b002b0
junhg <wujun5543473@163.com>
2025-06-17 21:04:11 +0800

    强基L0
    Signed-off-by: junhg <wujun5543473@163.com>

---
+++ examples/StrengthenTest/hvigorfile.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+  system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+}
