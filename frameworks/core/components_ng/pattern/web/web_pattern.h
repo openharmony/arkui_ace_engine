ec63b38e136994832b38da4dc1466f8c1e94f3e2
zhushengle <zhushengle@huawei.com>
2025-06-17 12:28:38 +0800

    fix: ArkUI强基回合解耦合Web组建
    Signed-off-by: zhushengle <zhushengle@huawei.com>
    Change-Id: I9b5496d31616e18c1ea439c7491a5ec79effbe0b

--- frameworks/core/components_ng/pattern/web/web_pattern.h
+++ frameworks/core/components_ng/pattern/web/web_pattern.h
@@ -236,17 +236,6 @@ public:
         }
     }
 
-    void SetWebSrcStatic(const std::string& webSrc)
-    {
-        if (webSrc_ != webSrc) {
-            webSrc_ = webSrc;
-            OnWebSrcUpdate();
-        }
-        if (webPaintProperty_) {
-            webPaintProperty_->SetWebPaintData(webSrc);
-        }
-    }
-
     const std::optional<std::string>& GetWebSrc() const
     {
         return webSrc_;
