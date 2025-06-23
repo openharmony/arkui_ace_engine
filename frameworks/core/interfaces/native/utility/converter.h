7a8016b823f773e8c1e7f63f6e2ac80fe151b0a6
liyou <liyou44@huawei.com>
2025-06-23 09:33:53 +0800

    sync form 0328:interop/src/arkts: switch to fast intrinsics (fix crash, include comments fix, chinese garbled)
    Signed-off-by: liyou <liyou44@huawei.com>

--- frameworks/core/interfaces/native/utility/converter.h
+++ frameworks/core/interfaces/native/utility/converter.h
@@ -271,10 +271,7 @@ namespace Converter {
     }
 
     template<>
-    inline std::string Convert(const Ark_String& src)
-    {
-        return (src.chars != nullptr) ? std::string(src.chars, src.length) : "";
-    }
+    std::string Convert(const Ark_String& src);
 
     template<>
     inline std::string Convert(const Ark_Buffer& src)
@@ -352,8 +349,7 @@ namespace Converter {
     template<>
     inline ImageSourceInfo Convert(const Ark_String& value)
     {
-        auto str = value.chars ? value.chars : "";
-        return ImageSourceInfo(str);
+        return ImageSourceInfo(Convert<std::string>(value));
     }
 
     template<>
