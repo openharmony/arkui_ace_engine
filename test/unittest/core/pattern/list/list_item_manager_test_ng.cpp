7453cb4fc6b8bf845a7a68855279f6610171025f
rongShao-Z <zhangrongjie4@huawei.com>
2025-06-23 19:47:21 +0800

    List修改
    Signed-off-by: rongShao-Z <zhangrongjie4@huawei.com>

--- test/unittest/core/pattern/list/list_item_manager_test_ng.cpp
+++ test/unittest/core/pattern/list/list_item_manager_test_ng.cpp
@@ -110,6 +110,11 @@ HWTEST_F(ListItemManagerTestNg, HandleScrollCallback001, TestSize.Level1)
     listItemDragManager->HandleScrollCallback();
     EXPECT_FALSE(listItemDragManager->scrolling_);
 }
+/**
+ * @tc.name: HandleScrollCallback002
+ * @tc.desc: Test ListItemDragManager HandleScrollCallback
+ * @tc.type: FUNC
+ */
 HWTEST_F(ListItemManagerTestNg, HandleScrollCallback002, TestSize.Level1)
 {
     RefPtr<ListPattern> listPattern = AceType::MakeRefPtr<ListPattern>();
@@ -120,13 +125,21 @@ HWTEST_F(ListItemManagerTestNg, HandleScrollCallback002, TestSize.Level1)
     ASSERT_NE(lazyForEachNode, nullptr);
     auto listItemDragManager = AceType::MakeRefPtr<ListItemDragManager>(host, lazyForEachNode);
 
+    GestureEvent info;
+    listItemDragManager->forEachNode_ = lazyForEachNode;
+    listItemDragManager->dragState_ = ListItemDragState::DRAGGING;
+    listItemDragManager->HandleOnItemDragStart(info);
+    info.SetOffsetX(0.0);
+    info.SetOffsetY(42.0);
+    info.SetGlobalPoint(Point(0, 360.f));
+    
+    listItemDragManager->HandleOnItemDragUpdate(info);
     listItemDragManager->scrolling_ = false;
+    listItemDragManager->HandleScrollCallback();
     auto frameRect = host->geometryNode_->GetMarginFrameRect();
     auto from = listItemDragManager->GetIndex();
     auto paddingOffset = listItemDragManager->GetParentPaddingOffset();
-    auto to = listItemDragManager->ScaleNearItem(from, frameRect, listItemDragManager->realOffset_
-        - frameRect.GetOffset() + paddingOffset);
-    listItemDragManager->HandleScrollCallback();
+    auto to = listItemDragManager->ScaleNearItem(from, frameRect, listItemDragManager->realOffset_ - frameRect.GetOffset() + paddingOffset);
     EXPECT_EQ(from, to);
 }
 /**
