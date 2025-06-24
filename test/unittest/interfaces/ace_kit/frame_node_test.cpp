3f2f1156d701bfe226b8e73336b6192ada29b8ab
lancer <haoshuo1@huawei.com>
2025-06-24 12:35:07 +0800

    fix tdd Signed-off-by: haoshuo <haoshuo1@huawei.com>

--- test/unittest/interfaces/ace_kit/frame_node_test.cpp
+++ test/unittest/interfaces/ace_kit/frame_node_test.cpp
@@ -208,6 +208,6 @@ HWTEST_F(FrameNodeTest, FrameNodeTestTest007, TestSize.Level1)
 
     EXPECT_EQ(frameNode->GetParentHandle(), nullptr);
     frameNodeParent->AddChild(frameNode);
-    EXPECT_NE(frameNode->GetParentHandle(), nullptr);
+    EXPECT_FALSE(frameNode->GetParentHandle() == nullptr);
 }
 } // namespace OHOS::Ace
