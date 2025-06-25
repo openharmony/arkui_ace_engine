fc738b8e457c038b0286b74ec04358e7c69be97b
b30045777 <baoguoqiang3@huawei.com>
2025-06-25 20:39:36 +0800

    【RichEditor】修复告警问题
    Signed-off-by: b30045777 <baoguoqiang3@huawei.com>
    Change-Id: I5dcfd9388c8b2c142baffab2f9bed5566661857a

--- frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
+++ frameworks/core/components_ng/pattern/rich_editor/rich_editor_pattern.cpp
@@ -4901,7 +4901,7 @@ void RichEditorPattern::HandleOnDragInsertStyledString(const RefPtr<SpanString>&
         AfterStyledStringChange(record);
     }
     StartTwinkling();
-    auto host = GetHost();
+    auto host = GetContentHost();
     CHECK_NULL_VOID(host);
     host->MarkDirtyNode(PROPERTY_UPDATE_MEASURE);
 }
@@ -6010,7 +6010,7 @@ void RichEditorPattern::DeleteSelectionOrPreviewText(
     }
     if (isSelector) {
         DeleteByRange(record, textSelector_.GetTextStart(), textSelector_.GetTextEnd());
-        if (!shouldCommitInput) {
+        if (!shouldCommitInput && record) {
             previewInputRecord_.deleteText = record->deleteText;
             previewInputRecord_.beforeCaretPosition = rangeStart.start;
         }
