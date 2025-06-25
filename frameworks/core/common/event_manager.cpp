5ab16c4d71dc2c3d6e30298a1480ecc4004e2ae9
刘永凯 <liuyongkai2@huawei-partners.com>
2025-06-25 21:48:15 +0800

    函数优化
    Signed-off-by: 刘永凯 <liuyongkai2@huawei-partners.com>

--- frameworks/core/common/event_manager.cpp
+++ frameworks/core/common/event_manager.cpp
@@ -979,19 +979,18 @@ void EventManager::LogTouchTestRecognizerStates(int32_t touchEventId)
 
 void EventManager::DispatchTouchEventAndCheck(const TouchEvent& event, bool sendOnTouch)
 {
-    TouchEvent point = event;
-    const auto iter = touchTestResults_.find(point.id);
+    const auto iter = touchTestResults_.find(event.id);
     bool hasFailRecognizer = false;
     bool allDone = false;
-    if (point.type == TouchType::DOWN) {
-        hasFailRecognizer = refereeNG_->HasFailRecognizer(point.id);
+    if (event.type == TouchType::DOWN) {
+        hasFailRecognizer = refereeNG_->HasFailRecognizer(event.id);
         allDone = refereeNG_->QueryAllDone();
     }
-    DispatchTouchEventToTouchTestResult(point, iter->second, sendOnTouch);
-    if (!allDone && point.type == TouchType::DOWN && !hasFailRecognizer &&
-        refereeNG_->HasFailRecognizer(point.id) && downFingerIds_.size() <= 1) {
+    DispatchTouchEventToTouchTestResult(event, iter->second, sendOnTouch);
+    if (!allDone && event.type == TouchType::DOWN && !hasFailRecognizer &&
+        refereeNG_->HasFailRecognizer(event.id) && downFingerIds_.size() <= 1) {
             refereeNG_->ForceCleanGestureRefereeState();
-            DispatchTouchEventToTouchTestResult(point, iter->second, false);
+            DispatchTouchEventToTouchTestResult(event, iter->second, false);
         }
 }
 
@@ -1084,7 +1083,7 @@ void EventManager::NotifyDragTouchEventListener(const TouchEvent& touchEvent)
     }
 }
 
-void EventManager::DispatchTouchEventToTouchTestResult(TouchEvent touchEvent,
+void EventManager::DispatchTouchEventToTouchTestResult(const TouchEvent& touchEvent,
     TouchTestResult touchTestResult, bool sendOnTouch)
 {
     bool isStopTouchEvent = false;
