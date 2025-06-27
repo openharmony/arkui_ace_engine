06559183bba83aff089edfa73a7bcb851a7e7378
sun-xinyan <sunxinyan2@huawei.com>
2025-06-27 16:50:41 +0800

    revert navigation file change
    Signed-off-by: sun-xinyan <sunxinyan2@huawei.com>

--- frameworks/core/components_ng/pattern/navigation/navigation_model_ng.h
+++ frameworks/core/components_ng/pattern/navigation/navigation_model_ng.h
@@ -81,29 +81,25 @@ public:
 
     static RefPtr<FrameNode> CreateFrameNode(int32_t nodeId);
     static void SetNavigationStack(FrameNode* frameNode);
-    static void SetNavigationStack(FrameNode* frameNode, const RefPtr<NG::NavigationStack>& navigationStack);
-    static void SetHideToolBar(FrameNode* frameNode, bool hideToolBar, bool animated = false);
+    static void SetHideToolBar(FrameNode* frameNode, bool hideToolBar, bool animated);
     static void SetEnableModeChangeAnimation(FrameNode* frameNode, bool isEnable);
     static void SetMinContentWidth(FrameNode* frameNode, const Dimension& value);
     static void SetMinNavBarWidth(FrameNode* frameNode, const Dimension& value);
     static void SetMaxNavBarWidth(FrameNode* frameNode, const Dimension& value);
     static void SetNavBarWidth(FrameNode* frameNode, const Dimension& value);
-    static void SetNavBarPosition(FrameNode* frameNode, const std::optional<NG::NavBarPosition>& mode);
-    static void SetUsrNavigationMode(FrameNode* frameNode, const std::optional<NavigationMode>& mode);
+    static void SetNavBarPosition(FrameNode* frameNode, NG::NavBarPosition mode);
+    static void SetUsrNavigationMode(FrameNode* frameNode, NavigationMode mode);
     static void SetBackButtonIcon(FrameNode* frameNode, const std::function<void(WeakPtr<NG::FrameNode>)>& symbolApply,
         const std::string& src, const ImageOption& imageOption, RefPtr<PixelMap>& pixMap);
-    static void SetBackButtonIcon(FrameNode* frameNode, const std::function<void(WeakPtr<NG::FrameNode>)>& symbolApply,
-        const ImageSourceInfo& imageSourceInfo, const ImageOption& imageOption, RefPtr<PixelMap>& pixMap);
     static void SetHideNavBar(FrameNode* frameNode, bool hideNavBar);
-    static void SetHideTitleBar(FrameNode* frameNode, bool hideTitleBar, bool animated = false);
+    static void SetHideTitleBar(FrameNode* frameNode, bool hideTitleBar, bool animated);
     static void SetSubtitle(FrameNode* frameNode, const std::string& subtitle);
     static void SetHideBackButton(FrameNode* frameNode, bool hideBackButton);
-    static void SetTitleMode(FrameNode* frameNode, const std::optional<NG::NavigationTitleMode>& mode);
     static void SetTitleMode(FrameNode* frameNode, NG::NavigationTitleMode mode);
     static void SetOnNavBarStateChange(FrameNode* frameNode, std::function<void(bool)>&& onNavBarStateChange);
     static void SetOnNavigationModeChange(FrameNode* frameNode,
         std::function<void(NG::NavigationMode)>&& onModeChange);
-    static void SetRecoverable(FrameNode* frameNode, const std::optional<bool>& recoverable);
+    static void SetRecoverable(FrameNode* frameNode, bool recoverable);
     static void SetEnableDragBar(FrameNode* frameNode, bool enableDragBar);
     static void SetEnableToolBarAdaptation(FrameNode* frameNode, bool enable);
 
@@ -128,7 +124,6 @@ public:
     static void SetOnCoordScrollEndAction(FrameNode* frameNode, std::function<void()>&& onCoordScrollEnd);
     static void SetSystemBarStyle(FrameNode* frameNode, const Color& contentColor);
     static bool IsDoubleBindBlock(const RefPtr<NavigationPattern>& navigationPattern);
-    static void SetSubTitle(FrameNode* frameNode, const std::string& subTitle);
 
 private:
     bool CreateNavBarNodeIfNeeded(const RefPtr<NavigationGroupNode>& navigationGroupNode);
