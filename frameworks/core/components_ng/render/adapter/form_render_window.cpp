98b5cf3be533d0acffd40925c3ed37e7834ba4ff
ouyanglihao <ouyanglihao1@huawei.com>
2025-07-01 15:40:34 +0800

    rs并行化帧率适配
    Signed-off-by: ouyanglihao <ouyanglihao1@huawei.com>

--- frameworks/core/components_ng/render/adapter/form_render_window.cpp
+++ frameworks/core/components_ng/render/adapter/form_render_window.cpp
@@ -199,7 +199,8 @@ void FormRenderWindow::FlushFrameRate(int32_t rate, int32_t animatorExpectedFram
     decltype(frameRateData_) frameRateData{rate, animatorExpectedFrameRate, rateType};
     if (frameRateData_ != frameRateData) {
         frameRateData_ = frameRateData;
-        frameRateLinker_->UpdateFrameRateRange({0, RANGE_MAX_REFRESHRATE, rate, rateType}, animatorExpectedFrameRate);
+        auto rsUIContext = rsUIDirector ? rsUIDirector_->GetRSUIContext() : nullptr;
+        frameRateLinker_->UpdateFrameRateRange({0, RANGE_MAX_REFRESHRATE, rate, rateType}, animatorExpectedFrameRate, rsUIContext);
     }
 #endif
 }
