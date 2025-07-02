65294f272cc44bd08a51d81190d98e1f0f139631
SQ <sunqi57@huawei.com>
2025-07-02 10:23:17 +0800

    复Swiper组件分组时选中导航点在尾部异常的问题
    Signed-off-by: SQ <sunqi57@huawei.com>
    Change-Id: I3b9c13646a93057e0b54f6adf4ae75d21b89e92c

--- frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
+++ frameworks/core/components_ng/pattern/swiper/swiper_pattern.cpp
@@ -4158,6 +4158,9 @@ RefPtr<Curve> SwiperPattern::GetIndicatorHeadCurve()
 
 void SwiperPattern::PlayIndicatorTranslateAnimation(float translate, std::optional<int32_t> nextIndex)
 {
+    if (NearZero(translate)) {
+        return;
+    }
     if (!stopIndicatorAnimation_) {
         stopIndicatorAnimation_ = true;
         return;
