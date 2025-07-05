7117a692c70985de4b8757adfaa55e66b9bf7261
mayaolll <mayao17@huawei.com>
2025-07-05 19:03:18 +0800

    fix bug in fa mode load-content
    Signed-off-by: mayaolll <mayao17@huawei.com>

--- frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
+++ frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
@@ -2108,7 +2108,8 @@ bool JsiDeclarativeEngine::LoadNamedRouterSource(const std::string& routeNameOrU
         } else {
             auto container = Container::GetContainer(instanceId_);
             CHECK_NULL_RETURN(container, false);
-            bundleName = container->GetBundleName();
+            bundleName = container->IsUseStageModel() ?
+                container->GetBundleName() : AceApplicationInfo::GetInstance().GetPackageName();
             moduleName = container->GetModuleName();
         }
 #else
