751aab7351e4227e6c2011d423b76f259381ae67
c30053238 <chenxiaoxue11@huawei.com>
2025-07-09 22:42:53 +0800

    sheettype
    Signed-off-by: c30053238 <chenxiaoxue11@huawei.com>

--- frameworks/core/components_ng/pattern/overlay/sheet_presentation_pattern.h
+++ frameworks/core/components_ng/pattern/overlay/sheet_presentation_pattern.h
@@ -1164,6 +1164,8 @@ private:
     std::string DrawClipPathTop(const SizeF&, const BorderRadiusProperty&);
     std::string DrawClipPathLeft(const SizeF&, const BorderRadiusProperty&);
     std::string DrawClipPathRight(const SizeF&, const BorderRadiusProperty&);
+
+    SheetType GetSheetTypeFromSheetManager() const;
     
     uint32_t broadcastPreDetentsIndex_ = 0;
     SheetAccessibilityDetents sheetDetents_ = SheetAccessibilityDetents::HIGH;
