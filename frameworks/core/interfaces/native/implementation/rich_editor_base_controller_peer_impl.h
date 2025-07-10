1c4cf482ee33013a071c93e52a327b4968b4d660
liuhui <liuhui160@huawei.com>
2025-07-10 09:26:24 +0800

    同步代码
    Signed-off-by: liuhui <liuhui160@huawei.com>

--- frameworks/core/interfaces/native/implementation/rich_editor_base_controller_peer_impl.h
+++ frameworks/core/interfaces/native/implementation/rich_editor_base_controller_peer_impl.h
@@ -78,7 +78,6 @@ public:
         const std::optional<SelectionOptions>& options, bool isForward) override
     {
         if (auto controller = handler_.Upgrade(); controller) {
-            controller->SetSelection(selectionStart, selectionEnd, options, isForward);
         }
     }
 
