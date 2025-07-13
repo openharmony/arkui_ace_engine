60780138edc9ad912d91f72d0ca5593b8162d580
卢宇豪 <luyuhao6@huawei.com>
2025-07-13 21:27:39 +0800

    change1
    Signed-off-by: 卢宇豪 <luyuhao6@huawei.com>

--- frameworks/core/components_ng/pattern/window_scene/scene/window_scene.cpp
+++ frameworks/core/components_ng/pattern/window_scene/scene/window_scene.cpp
@@ -197,7 +197,7 @@ void WindowScene::OnAttachToFrameNode()
         surfaceNode->SetBoundsChangedCallback(boundsChangedCallback_);
         SetSubWindowBufferAvailableCallback(surfaceNode);
         Rosen::SceneSessionManager::GetInstance().NotifyOnAttachToFrameNode(
-        session_->GetPersistentId(), session_->GetSessionInfo().bundleName_, surfaceNode->GetId());
+            session_->GetPersistentId(), session_->GetSessionInfo().bundleName_, surfaceNode->GetId());
         TAG_LOGI(AceLogTag::ACE_WINDOW_SCENE,
             "OnAttachToFrameNode id: %{public}d, node id: %{public}d, type: %{public}d, name: %{public}s",
             session_->GetPersistentId(), host->GetId(), session_->GetWindowType(), session_->GetWindowName().c_str());
