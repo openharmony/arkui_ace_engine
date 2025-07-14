20b52d6a5b062eca6b664fcb955fe56c3acb88ab
houguobiao <houguobiao@huawei.com>
2025-07-14 14:24:11 +0800

    修复Dialog相关生命周期接口实现与定义不一致问题
    Signed-off-by: houguobiao <houguobiao@huawei.com>
    Change-Id: Idbe846cfd403f92c5019e4136704b81dcd6c735c

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/component/actionSheet.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/component/actionSheet.ts
@@ -139,10 +139,10 @@ export interface ActionSheetOptions {
     shadow?: ShadowOptions | ShadowStyle;
     enableHoverMode?: boolean;
     hoverModeArea?: HoverModeAreaType;
-    onDidAppear?: (() => void);
-    onDidDisappear?: (() => void);
-    onWillAppear?: (() => void);
-    onWillDisappear?: (() => void);
+    onDidAppear?: ((data: undefined) => void);
+    onDidDisappear?: ((data: undefined) => void);
+    onWillAppear?: ((data: undefined) => void);
+    onWillDisappear?: ((data: undefined) => void);
     levelMode?: LevelMode;
     levelUniqueId?: number;
     immersiveMode?: ImmersiveMode;
--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/component/alertDialog.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/component/alertDialog.ts
@@ -128,10 +128,10 @@ export interface AlertDialogParam {
     textStyle?: TextStyle_alert_dialog;
     enableHoverMode?: boolean;
     hoverModeArea?: HoverModeAreaType;
-    onDidAppear?: (() => void);
-    onDidDisappear?: (() => void);
-    onWillAppear?: (() => void);
-    onWillDisappear?: (() => void);
+    onDidAppear?: ((data: undefined) => void);
+    onDidDisappear?: ((data: undefined) => void);
+    onWillAppear?: ((data: undefined) => void);
+    onWillDisappear?: ((data: undefined) => void);
     levelMode?: LevelMode;
     levelUniqueId?: number;
     immersiveMode?: ImmersiveMode;
