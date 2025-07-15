b46e80b7978183da8df1bd75210027529f44a08e
tomkl123 <wangyansong11@huawei.com>
2025-07-15 20:34:25 +0800

    修复了cacheCount show设置为true的情况下，会布局到-1行的bug
    Signed-off-by: tomkl123 <wangyansong11@huawei.com>
    Change-Id: I36d722717f60a9d4cf618af4cf2094ea22e3ba1e

--- frameworks/core/components_ng/pattern/grid/irregular/grid_irregular_filler.cpp
+++ frameworks/core/components_ng/pattern/grid/irregular/grid_irregular_filler.cpp
@@ -65,6 +65,9 @@ Result GridIrregularFiller::Fill(const FillParameters& params, float targetLen, 
 
 void GridIrregularFiller::FillToTarget(const FillParameters& params, int32_t targetIdx, int32_t startingLine)
 {
+    if (startingLine < 0) {
+        startingLine = 0;
+    }
     if (targetIdx >= info_->GetChildrenCount()) {
         targetIdx = info_->GetChildrenCount() - 1;
     }
