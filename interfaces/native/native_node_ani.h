c0d77354f83406758b1710aac9badb30465f9422
sunbees <sunqinjia@huawei.com>
2025-07-16 11:50:59 +0800

    add OH_ArkUI_GetNodeContentFromAniValue
    Signed-off-by: sunbees <sunqinjia@huawei.com>

--- interfaces/native/native_node_ani.h
+++ interfaces/native/native_node_ani.h
@@ -49,6 +49,19 @@ int32_t OH_ArkUI_NativeModule_GetNodeHandleFromAniValue(ani_env* env, ani_object
 int32_t OH_ArkUI_NativeModule_GetContextFromAniValue(ani_env* env, ani_object value, ArkUI_ContextHandle* context);
 
 /**
+ * @brief 获取ArkTS侧创建的NodeContent对象映射到native侧的ArkUI_NodeContentHandle。
+ *
+ * @param env ani的环境指针。
+ * @param nodeContent ArkTS侧创建的NodeContent对象。
+ * @param content ArkUI_NodeContentHandle指针。
+ * @return 0 - 成功。
+ *         401 - 函数参数异常。
+ * @since 20
+ */
+int32_t OH_ArkUI_NativeModule_GetNodeContentFromAniValue(
+    ani_env* env, ani_object nodeContent, ArkUI_NodeContentHandle* content);
+
+/**
  * @brief Obtains the parameters of the NavDestination component where the node is located.
  *
  * @param node The node.
