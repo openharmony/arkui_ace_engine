32f013460bf3c5a64859426a5b6de9703544fc92
jiyujia926 <jiyujia3@huawei.com>
2025-07-18 14:10:53 +0800

    several bug fix
    Signed-off-by: jiyujia926 <jiyujia3@huawei.com>

--- frameworks/bridge/arkts_frontend/koala_projects/incremental/runtime/src/states/State.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/incremental/runtime/src/states/State.ts
@@ -244,9 +244,9 @@ interface ManagedScope extends Disposable, Dependency, ReadonlyTreeNode {
     invalidateOnReuse(): void
 }
 
-class StateImpl<Value> implements Observable, ManagedState, MutableState<Value> {
+export class StateImpl<Value> implements Observable, ManagedState, MutableState<Value> {
     protected manager: StateManagerImpl | undefined = undefined
-    private dependencies: Dependencies | undefined = undefined
+    public dependencies: Dependencies | undefined = undefined
     protected snapshot: Value
     protected myModified = false
     protected myUpdated = true
