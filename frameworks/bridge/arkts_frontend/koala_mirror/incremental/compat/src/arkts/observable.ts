a71035d54e505f8db7d2d859a09597301c26c253
wuxiesaber <wuxie@huawei.com>
2025-07-18 17:38:05 +0800

    adjust number2int in map and set
    Issue: https://gitee.com/openharmony/arkcompiler_runtime_core/issues/ICN2J6
    Signed-off-by: wuxiesaber <wuxie@huawei.com>
    Change-Id: Ib36ebf17a202cf4a18b0ffbedc5eb82dde11b133

--- frameworks/bridge/arkts_frontend/koala_mirror/incremental/compat/src/arkts/observable.ts
+++ frameworks/bridge/arkts_frontend/koala_mirror/incremental/compat/src/arkts/observable.ts
@@ -558,7 +558,7 @@ class ObservableMap<T, V> extends Map<T, V> {
         return ObservableHandler.find(this)
     }
 
-    override get size(): number {
+    override get size(): int {
         this.handler?.onAccess()
         return super.size
     }
@@ -662,7 +662,7 @@ class ObservableSet<T> extends Set<T> {
         return new Set<T>(this.elements.keys()).toString()
     }
 
-    override get size(): number {
+    override get size(): int {
         this.handler?.onAccess()
         return this.elements.size
     }
