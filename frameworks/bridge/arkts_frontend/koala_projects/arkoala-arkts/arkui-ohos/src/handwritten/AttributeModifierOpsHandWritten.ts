151a1e085f83cd4c369b1ca2421d6686d641f7e5
wjnRance <wanjining@h-partners.com>
2025-07-21 10:30:20 +0800

    挑单0702
    Signed-off-by: wjnRance <wanjining@h-partners.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/handwritten/AttributeModifierOpsHandWritten.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/handwritten/AttributeModifierOpsHandWritten.ts
@@ -16,10 +16,12 @@
 import { ArkCommonMethodComponent, AttributeModifier, CommonMethod } from '../component/common';
 import { ArkButtonComponent, ButtonAttribute } from '../component/button'
 import { ArkTextComponent, TextAttribute, ArkTextPeer, TextOptions } from '../component/text'
+import { ArkSymbolGlyphComponent, SymbolGlyphAttribute, ArkSymbolGlyphPeer } from '../component/symbolglyph'
 import { applyAttributeModifierBase, applyCommonModifier } from "./modifiers/ArkCommonModifier";
 import { CommonModifier } from '../CommonModifier';
 import { TextModifier } from '../TextModifier';
 import { ButtonModifier } from "../ButtonModifier";
+import { SymbolGlyphModifier } from '../SymbolGlyphModifier';
 import { Resource } from "global.resource"
 import { runtimeType, RuntimeType } from "@koalaui/interop"
 export function hookButtonAttributeModifier(component: ArkButtonComponent, modifier: AttributeModifier<ButtonAttribute> | AttributeModifier<CommonMethod> | undefined): void {
@@ -103,4 +105,45 @@ export function hookButtonAttributeModifier(component: ArkButtonComponent, modif
         return initComponent;
     };
     applyAttributeModifierBase(modifier as Object as AttributeModifier<TextAttribute>, attributeSet, constructParam, updaterReceiver, component.getPeer());
+}
+export function hookSymbolGlyphAttributeModifier(component: ArkSymbolGlyphComponent,
+    modifier: AttributeModifier<SymbolGlyphAttribute> | AttributeModifier<CommonMethod> | undefined): void {
+    if (modifier === undefined) {
+        return;
+    }
+    let isCommonModifier: boolean = modifier instanceof CommonModifier;
+    if (isCommonModifier) {
+        applyCommonModifier(component.getPeer(), modifier as Object as AttributeModifier<CommonMethod>);
+        return;
+    }
+    let attributeSet = (): SymbolGlyphModifier => {
+        let isSymbolGlyphModifier: boolean = modifier instanceof SymbolGlyphModifier;
+        let initModifier = component.getPeer()._attributeSet ? component.getPeer()._attributeSet! : new SymbolGlyphModifier();
+        if (isSymbolGlyphModifier) {
+            let symbolGlyphModifier = modifier as object as SymbolGlyphModifier;
+            initModifier.mergeModifier(symbolGlyphModifier);
+            component.getPeer()._attributeSet = initModifier;
+            return initModifier;
+        } else {
+            component.getPeer()._attributeSet = initModifier;
+            return initModifier;
+        }
+    }
+    let constructParam = (component: ArkCommonMethodComponent, ...params: FixedArray<Object>): void => {
+        if (params.length > 1) {
+            throw new Error('more than 1 parameters')
+        }
+        let value_casted: Resource | undefined = undefined
+        if (params.length >= 0) {
+            value_casted = params[0] as Resource
+        }
+        let symbolGlyphPeer: ArkSymbolGlyphPeer = component.getPeer() as Object as ArkSymbolGlyphPeer;
+        symbolGlyphPeer.setSymbolGlyphOptionsAttribute(value_casted)
+    };
+    let updaterReceiver = (): ArkSymbolGlyphComponent => {
+        let componentNew: ArkSymbolGlyphComponent = new ArkSymbolGlyphComponent();
+        componentNew.setPeer(component.getPeer());
+        return componentNew;
+    };
+    applyAttributeModifierBase(modifier as Object as AttributeModifier<SymbolGlyphAttribute>, attributeSet, constructParam, updaterReceiver, component.getPeer());
 }
