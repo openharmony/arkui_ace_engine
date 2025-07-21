16eb1c8bde818906638b9496ab8510309a239543
徐钦锋 <xuqinfeng1@h-partners.com>
2025-07-21 22:56:34 +0800

    0328分支picker、gauge部分代码合入0702
    Signed-off-by: 徐钦锋 <xuqinfeng1@h-partners.com>

--- frameworks/core/interfaces/native/implementation/gauge_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/gauge_modifier.cpp
@@ -274,6 +274,13 @@ void DescriptionImpl(Ark_NativePointer node,
 void TrackShadowImpl(Ark_NativePointer node,
                      const Opt_GaugeShadowOptions* value)
 {
+    auto frameNode = reinterpret_cast<FrameNode *>(node);
+    CHECK_NULL_VOID(frameNode);
+    auto convValue = value ? Converter::OptConvert<GaugeShadowOptions>(*value) : std::nullopt;
+    GaugeShadowOptions defaultOptions;
+    defaultOptions.isShadowVisible = false;
+    auto shadow = convValue.value_or(defaultOptions);
+    GaugeModelNG::SetShadowOptions(frameNode, shadow);
 }
 void IndicatorImpl(Ark_NativePointer node,
                    const Opt_GaugeIndicatorOptions* value)
@@ -309,6 +316,8 @@ void ContentModifierImpl(Ark_NativePointer node,
 {
     auto frameNode = reinterpret_cast<FrameNode *>(node);
     CHECK_NULL_VOID(frameNode);
+    //auto convValue = value ? Converter::OptConvert<type>(*value) : std::nullopt;
+    //GaugeModelNG::SetContentModifier(frameNode, convValue);
 }
 } // GaugeAttributeModifier
 const GENERATED_ArkUIGaugeModifier* GetGaugeModifier()
