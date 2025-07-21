6f5bf53fa4db490ac5e6cafc97a8807510adfa52
wangchensu <wangchensu@huawei.com>
2025-07-21 14:49:21 +0800

    add ComponentContentBase type
    Signed-off-by: wangchensu <wangchensu@huawei.com>
    Change-Id: I19bbf9698c23f0bdf905a7a9fc5e94601be196c2

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ComponentContent.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ComponentContent.ts
@@ -22,7 +22,11 @@ import { BuilderNode, BuildOptions, voidBuilderFunc, TBuilderFunc } from "./Buil
 import { Content } from "./Content"
 import { KPointer, pointer } from "@koalaui/interop"
 
-export class ComponentContent<T = undefined> implements Content {
+export interface ComponentContentBase extends Content {
+
+}
+
+export class ComponentContent<T = undefined> implements ComponentContentBase {
     // the name of "builderNode_" is used in ace_engine/interfaces/native/node/native_node_ani.cpp.
     private builderNode_: BuilderNode<T>;
     private parentWeak_?: WeakRef<FrameNode>;
