21a8c27a4fc7366f6677402e53aece8783be9fb0
yalixie <yali.xie@huawei-partners.com>
2025-07-28 12:55:53 +0300

    Update koala_mirror
    Signed-off-by: yalixie <yali.xie@huawei-partners.com>

--- frameworks/bridge/arkts_frontend/koala_mirror/ui2abc/libarkts/src/Es2pandaNativeModule.ts
+++ frameworks/bridge/arkts_frontend/koala_mirror/ui2abc/libarkts/src/Es2pandaNativeModule.ts
@@ -185,18 +185,18 @@ export class Es2pandaNativeModule {
     _CreateDiagnosticKind(context: KNativePointer, message: string, type: Es2pandaPluginDiagnosticType): KNativePointer {
         throw new Error("Not implemented")
     }
-    _CreateDiagnosticInfo(context: KNativePointer, kind: KNativePointer, args: string[], argc: number): KNativePointer {
+    _CreateDiagnosticInfo(context: KNativePointer, kind: KNativePointer, args: string[], argc: number, pos: KNativePointer): KNativePointer {
         throw new Error("Not implemented")
     }
     _CreateSuggestionInfo(context: KNativePointer, kind: KNativePointer, args: string[],
-        argc: number, substitutionCode: string): KNativePointer {
+        argc: number, substitutionCode: string, range: KNativePointer): KNativePointer {
         throw new Error("Not implemented")
     }
     _LogDiagnostic(context: KNativePointer, kind: KNativePointer, argv: string[], argc: number, pos: KNativePointer): void {
         throw new Error("Not implemented")
     }
     _LogDiagnosticWithSuggestion(context: KNativePointer, diagnosticInfo: KNativePointer,
-        suggestionInfo?: KNativePointer, range?: KNativePointer): void {
+        suggestionInfo?: KNativePointer): void {
         throw new Error("Not implemented")
     }
     _SetUpSoPath(soPath: string): void {
--- frameworks/bridge/arkts_frontend/koala_mirror/ui2abc/libarkts/src/wrapper-compat/arkts-api/peers/SuggestionInfo.ts
+++ frameworks/bridge/arkts_frontend/koala_mirror/ui2abc/libarkts/src/wrapper-compat/arkts-api/peers/SuggestionInfo.ts
@@ -18,15 +18,16 @@ import { global } from '../static/global';
 import { KNativePointer } from '@koalaui/interop';
 import { DiagnosticKind } from './DiagnosticKind';
 import { passStringArray } from '../utilities/private';
+import { SourceRange } from './SourceRange';
 
 export class SuggestionInfo extends ArktsObject {
   constructor(peer: KNativePointer) {
     super(peer);
   }
 
-  static create(kind: DiagnosticKind, substitutionCode: string, ...args: string[]): SuggestionInfo {
+  static create(kind: DiagnosticKind, substitutionCode: string, range: SourceRange, ...args: string[]): SuggestionInfo {
     return new SuggestionInfo(
-      global.es2panda._CreateSuggestionInfo(global.context, kind.peer, passStringArray(args), args.length, substitutionCode)
+      global.es2panda._CreateSuggestionInfo(global.context, kind.peer, passStringArray(args), args.length, substitutionCode, range.peer)
     );
   }
 
