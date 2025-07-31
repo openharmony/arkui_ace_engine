0ff77f969d756bc5385e8e6433b8e4c75e15e7f8
wangchensu <wangchensu@huawei.com>
2025-07-31 15:06:25 +0800

    fix the bug about UAF in TypedrameNode
    Signed-off-by: wangchensu <wangchensu@huawei.com>
    Change-Id: I0c83e4aa7e8d6f2bb745a20965b25df92fd6764f

--- frameworks/bridge/declarative_frontend/ark_node/src/frame_node.ts
+++ frameworks/bridge/declarative_frontend/ark_node/src/frame_node.ts
@@ -836,9 +836,16 @@ class TypedFrameNode<T extends ArkComponent> extends FrameNode {
     this.attrCreator_ = attrCreator;
   }
 
-  dispose(){
-    super.dispose();
+  dispose() {
+    this.isDisposed_ = true;
+    if (this.nodePtr_) {
+      getUINativeModule().frameNode.fireArkUIObjectLifecycleCallback(new WeakRef(this), 'FrameNode', this.getNodeType() || 'FrameNode', this.nodePtr_);
+    }
+    FrameNodeFinalizationRegisterProxy.ElementIdToOwningFrameNode_.delete(this._nodeId);
+    this._nodeId = -1;
     this._nativeRef?.dispose();
+    this._nativeRef = null;
+    this.nodePtr_ = null;
   }
 
   initialize(...args: Object[]): T {
