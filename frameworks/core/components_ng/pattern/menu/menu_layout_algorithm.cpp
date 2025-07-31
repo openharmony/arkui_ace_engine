53b5e870954e7fb558bd97b3ae40c3bd5d2f011f
sunziyuan <sunziyuan1@h-partners.com>
2025-07-31 21:40:07 +0800

    fix sub menu position in UIExtension
    Signed-off-by: sunziyuan <sunziyuan1@h-partners.com>
    Change-Id: Ie9d7a5fc2c1ff5a3259f5e5a9382517cf675d8b7

--- frameworks/core/components_ng/pattern/menu/menu_layout_algorithm.cpp
+++ frameworks/core/components_ng/pattern/menu/menu_layout_algorithm.cpp
@@ -804,7 +804,7 @@ void MenuLayoutAlgorithm::Measure(LayoutWrapper* layoutWrapper)
     auto menuLayoutProperty = AceType::DynamicCast<MenuLayoutProperty>(layoutWrapper->GetLayoutProperty());
     CHECK_NULL_VOID(menuLayoutProperty);
     auto isContextMenu = menuPattern->IsContextMenu();
-    auto isShowInSubWindow = menuLayoutProperty->GetShowInSubWindowValue(true) || isContextMenu;
+    auto isShowInSubWindow = menuLayoutProperty->GetShowInSubWindowValue(true) || isContextMenu || (containerId >= MIN_SUBCONTAINER_ID);
     InitCanExpandCurrentWindow(isShowInSubWindow, layoutWrapper);
     Initialize(layoutWrapper);
     if (!targetTag_.empty()) {
