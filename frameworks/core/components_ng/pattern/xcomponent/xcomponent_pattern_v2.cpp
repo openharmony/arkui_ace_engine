0a62abcb62ab2c10fca32423c084f472cbaf0584
WangJiazhen <wangjiazhen4@huawei.com>
2025-08-01 15:54:31 +0800

    clear warning
    Signed-off-by: WangJiazhen <wangjiazhen4@huawei.com>

--- frameworks/core/components_ng/pattern/xcomponent/xcomponent_pattern_v2.cpp
+++ frameworks/core/components_ng/pattern/xcomponent/xcomponent_pattern_v2.cpp
@@ -230,6 +230,7 @@ void XComponentPatternV2::OnDetachFromMainTree()
 
 void XComponentPatternV2::OnDetachFromFrameNode(FrameNode* frameNode)
 {
+    CHECK_NULL_VOID(frameNode);
     UpdateUsesSuperMethod();
     if (usesSuperMethod_) {
         XComponentPattern::OnDetachFromFrameNode(frameNode);
