5ef9af87cceaa06ba665a80e780624121a70bbb7
SprucEovo <lichenwei2@huawei.com>
2025-08-02 20:35:11 +0800

    补充TDD
    Signed-off-by: SprucEovo <lichenwei2@huawei.com>

--- test/unittest/core/pattern/web/ani/web_model_static_test.cpp
+++ test/unittest/core/pattern/web/ani/web_model_static_test.cpp
@@ -109,7 +109,6 @@ HWTEST_F(WebModelStaticTest, CreateFrameNode001, TestSize.Level1)
     WebModelStatic::NotifyPopupWindowResultStatic(1, true);
     WebModelStatic::SetBlurOnKeyboardHideMode(AccessibilityManager::RawPtr(frameNode), keyBoardMode);
     WebModelStatic::SetPopup(AccessibilityManager::RawPtr(frameNode), true, 0);
-    WebModelStatic::SetPopup(nullptr, false, 0);
     keyBoardMode = BlurOnKeyboardHideMode::BLUR;
     WebModelStatic::SetBlurOnKeyboardHideMode(AccessibilityManager::RawPtr(frameNode), keyBoardMode);
     auto webPatternStatic = ViewStackProcessor::GetInstance()->GetMainFrameNodePattern<WebPatternStatic>();
