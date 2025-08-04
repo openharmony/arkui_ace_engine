15f5a7abf41e028328b4003cb281e1058fea67ec
lushi1202 <lushi@huawei.com>
2025-08-04 22:25:05 +0800

    add uisession report router page change ut.
    Signed-off-by: lushi1202 <lushi@huawei.com>
    Change-Id: If6102b1a940a6659a7d0dbb284a1d026c423bc3f

--- test/mock/adapter/mock_uisession_manager.h
+++ test/mock/adapter/mock_uisession_manager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,17 +13,20 @@
  * limitations under the License.
  */
 
+#ifndef TEST_MOCK_ADAPTER_MOCK_UI_SESSION_MANAGER_H
+#define TEST_MOCK_ADAPTER_MOCK_UI_SESSION_MANAGER_H
+
 #include "interfaces/inner_api/ui_session/ui_session_manager.h"
 
-#include "mock_uisession_manager.h"
+#include "gmock/gmock.h"
 
 namespace OHOS::Ace {
-std::mutex UiSessionManager::mutex_;
-std::shared_mutex UiSessionManager::reportObjectMutex_;
+class MockUiSessionManager : public UiSessionManager {
+public:
+    MockUiSessionManager() = default;
+    ~MockUiSessionManager() override = default;
 
-UiSessionManager* UiSessionManager::GetInstance()
-{
-    static MockUiSessionManager instance;
-    return &instance;
+    MOCK_METHOD2(OnRouterChange, void(const std::string& path, const std::string& event));
+};
 }
-} // namespace OHOS::Ace
+#endif
