5bae0d48594b606692d62a7386a5ca13ea0e73c1
Ilya Erokhin <ilya.erokhin@huawei.com>
2025-08-07 12:34:59 +0300

    Add koala_mirror (based on eb28aac)
    Signed-off-by: Ilya Erokhin <ilya.erokhin@huawei.com>
    Change-Id: I3fff8fa2fd7e48a2443aa4211a682cbef05d2a74

--- frameworks/bridge/arkts_frontend/koala_mirror/arkoala-arkts/arkui/src/stateManagement/decorator.ts
+++ frameworks/bridge/arkts_frontend/koala_mirror/arkoala-arkts/arkui/src/stateManagement/decorator.ts
@@ -14,11 +14,14 @@
  */
 
 import { ObserveSingleton } from './base/observeSingleton';
-import { int32 } from '@koalaui/common';
+import { int32, observableProxy, propDeepCopy } from "@koalaui/common"
 import { __StateMgmtFactoryImpl } from './base/stateMgmtFactory';
 import { ExtendableComponent } from '../component/extendableComponent';
 import { IBindingSource, ITrackedDecoratorRef } from './base/mutableStateMeta';
 import { IComputedDecoratorRef } from './decoratorImpl/decoratorComputed';
+import { mutableState, scheduleCallback, MutableState, GlobalStateManager, ObservableClassV2 } from "@koalaui/runtime"
+import { AppStorage, LocalStorage } from "../Storage"
+import { SubscribedAbstractProperty } from "../ArkState"
 
 export interface IDecoratedVariable {
     readonly varName: string;
@@ -256,3 +259,312 @@ export interface IMonitorValue<T> {
 export type MonitorValueCallback = () => Any;
 export type MonitorCallback = (m: IMonitor) => void;
 export type ComputeCallback<T> = () => T;
+
+class StatableHolder<Value> {
+    private state: MutableState<Value> | undefined = undefined
+    private readonly stateManager = GlobalStateManager.instance
+
+    get value(): Value {
+        return this.state!.value!
+    }
+
+    set value(value: Value) {
+        if (this.state) {
+            this.state!.value = observableProxy<Value>(value)
+        } else {
+            this.state = this.mutableState(value)
+        }
+    }
+
+    dispose() {
+        if (this.state) {
+            this.state!.dispose()
+            this.state = undefined
+        }
+    }
+
+    private mutableState<Value>(value: Value): MutableState<Value> {
+        return this.stateManager.mutableState(observableProxy<Value>(value), true)
+    }
+}
+
+class PlainValueHolder<Value> {
+    private plainValue: Value | undefined = undefined
+    private state: StatableHolder<Value> | undefined = undefined
+
+    constructor(value: Value | undefined) {
+        if (value != undefined) {
+            this.setValue(value)
+        }
+    }
+
+    get value(): Value {
+        return this.state ? this.state!.value! : this.plainValue!
+    }
+
+    set value(value: Value) {
+        this.setValue(value)
+    }
+
+    dispose() {
+        this.state?.dispose()
+    }
+
+    private setValue(value: Value) {
+        if (this.isStatable(value)) {
+            this.setStateValue(value)
+            this.plainValue = undefined
+        } else {
+            this.plainValue = value
+            this.dispose()
+        }
+    }
+
+    private setStateValue(value: Value) {
+        if (!this.state) {
+            this.state = new StatableHolder<Value>()
+        }
+        this.state!.value = value
+    }
+
+    private isStatable(value: Value | undefined): boolean {
+        return value instanceof ObservableClassV2
+    }
+}
+
+export class PlainStructProperty<Value> implements SubscribedAbstractProperty<Value> {
+    private name: string
+    private readonly holder: PlainValueHolder<Value>
+
+    constructor(name: string, value?: Value) {
+        this.name = name
+        this.holder = new PlainValueHolder<Value>(value)
+    }
+
+    init(value: Value | undefined): void {
+        if (value != undefined)
+            this.holder.value = value
+    }
+
+    info(): string {
+        return this.name
+    }
+
+    get(): Value {
+        return this.holder.value!
+    }
+
+    set(value: Value): void {
+        this.holder.value = value
+    }
+
+    subscribe(listener: () => void): void {
+    }
+
+    unsubscribe(listener: () => void): void {
+    }
+
+    aboutToBeDeleted(): void {
+        this.holder.dispose()
+    }
+}
+
+export class BuilderParamDecoratorProperty<Value> extends PlainStructProperty<Value> {
+    constructor(name: string, value?: Value) {
+        super(name, value)
+    }
+}
+
+export class LinkDecoratorProperty<Value> implements SubscribedAbstractProperty<Value> {
+    private readonly name: string
+    private readonly watch: (() => void) | undefined
+    private property: SubscribedAbstractProperty<Value> | undefined = undefined
+
+    constructor(name: string, watch?: () => void) {
+        this.name = name
+        this.watch = watch
+    }
+
+    linkTo(maybeProperty: SubscribedAbstractProperty<Value> | Value | undefined): void {
+        if (!maybeProperty) throw new Error(`${this.name} must be linked with another property`)
+        const property = maybeProperty! // Improve: this is to workaround Any considered non-nulish
+        if (!(property instanceof SubscribedAbstractProperty<Value>)) throw new Error('Property must be passed, got')
+        if (this.property) throw new Error(`${this.name} is already linked with some property`)
+        this.property = property
+        if (this.watch) property.subscribe(this.watch!)
+    }
+
+    info(): string {
+        return this.name
+    }
+
+    get(): Value {
+        return this.property!.get()
+    }
+
+    set(value: Value): void {
+        this.property!.set(value)
+    }
+
+    subscribe(listener: () => void): void {
+        this.property!.unsubscribe(listener)
+    }
+
+    unsubscribe(listener: () => void): void {
+        this.property!.unsubscribe(listener)
+    }
+
+    aboutToBeDeleted(): void {
+        if (this.watch) this.property!.unsubscribe(this.watch!)
+    }
+}
+
+export class StateDecoratorProperty<Value> implements SubscribedAbstractProperty<Value> {
+    private name: string
+    private state: MutableState<Value> | undefined = undefined
+    private listeners: Set<() => void> | undefined = undefined
+
+    constructor(name: string, listener?: () => void) {
+        this.name = name
+        if (listener) this.subscribe(listener)
+    }
+    init(value?: Value, initial?: Value): void {
+        this.state = mutableState(observableProxy<Value>(value ?? (initial as Value)))
+    }
+    info(): string {
+        return this.name
+    }
+    get(): Value {
+        return this.state!.value
+    }
+    set(value: Value): void {
+        this.state!.value = observableProxy(value)
+        this.listeners?.forEach(notify)
+    }
+    subscribe(listener: () => void): void {
+        if (!this.listeners) this.listeners = new Set<() => void>()
+        this.listeners?.add(listener)
+    }
+    unsubscribe(listener: () => void): void {
+        this.listeners?.delete(listener)
+    }
+    aboutToBeDeleted(): void {
+        this.listeners?.clear()
+    }
+}
+
+function notify(listener: () => void) {
+    listener()
+}
+
+export class PropDecoratorProperty<Value> extends StateDecoratorProperty<Value> {
+    /*
+        _modified and _value needed for changes to be observable instantly, on the same recomputation value is being changed
+    */
+    private _modified = false
+    private _value?: Value
+
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    init(value?: Value, initial?: Value): void {
+        super.init(value ? this.deepCopyOnUpdate(value) : undefined, initial)
+    }
+    get(): Value {
+        let value = super.get() // subscribe
+        if (this._modified) value = this._value as Value
+        return value
+    }
+    update(value?: Value): void {
+        this._modified = false
+        this._value = undefined
+        const scope = GlobalStateManager.instance.scope<void>(0, 1)
+        const parameter = scope.param(0, value)
+        if (scope.unchanged) {
+            scope.cached
+            return
+        }
+        value = parameter.value // subscribe to update
+        if (value != undefined) {
+            const copy = this.deepCopyOnUpdate(value)
+            this._modified = true
+            this._value = copy
+            scheduleCallback(() => { this.set(copy) })
+        }
+        scope.recache()
+    }
+    protected deepCopyOnUpdate(value: Value): Value {
+        return observableProxy(propDeepCopy(value))
+    }
+}
+
+export class ObjectLinkDecoratorProperty<Value> extends PropDecoratorProperty<Value> {
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    protected deepCopyOnUpdate(value: Value): Value {
+        return value
+    }
+}
+
+export class ProvideDecoratorProperty<Value> extends StateDecoratorProperty<Value> {
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    provide(provideKey?: string): void {
+        GlobalStateManager.instance.namedState<SubscribedAbstractProperty<Value>>(provideKey ?? this.info(), () => this)
+    }
+    checkOverrides(provideKey?: string): void {
+        const actualProvideKey = provideKey ?? this.info()
+        const state = GlobalStateManager.instance.stateBy<Value>(actualProvideKey, false)
+        if (state) {
+            throw new Error(`Variable "${actualProvideKey}" was already defined on the current page. ` +
+                `Use @Provide({allowOverride: "${actualProvideKey}"}) for explicit override or choose another variable name`)
+        }
+    }
+}
+
+export class ConsumeDecoratorProperty<Value> extends LinkDecoratorProperty<Value> {
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    init(provideKey?: string): void {
+        this.linkTo(GlobalStateManager.instance.valueBy<SubscribedAbstractProperty<Value>>(provideKey ?? this.info()))
+    }
+}
+
+export class StorageLinkDecoratorProperty<Value> extends LinkDecoratorProperty<Value> {
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    init(value: Value, storageKey?: string): void {
+        this.linkTo(AppStorage.setAndLink<Value>(storageKey ?? this.info(), value))
+    }
+}
+
+export class LocalStorageLinkDecoratorProperty<Value> extends LinkDecoratorProperty<Value> {
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    init(value: Value, storage: LocalStorage, storageKey?: string): void {
+        this.linkTo(storage.setAndLink<Value>(storageKey ?? this.info(), value))
+    }
+}
+
+export class StoragePropDecoratorProperty<Value> extends LinkDecoratorProperty<Value>  {
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    init(value: Value, storageKey?: string): void {
+        this.linkTo(AppStorage.setAndProp<Value>(storageKey ?? this.info(), value))
+    }
+}
+
+export class LocalStoragePropDecoratorProperty<Value> extends LinkDecoratorProperty<Value> {
+    constructor(name: string, listener?: () => void) {
+        super(name, listener)
+    }
+    init(value: Value, storage: LocalStorage, storageKey?: string): void {
+        this.linkTo(storage.setAndProp<Value>(storageKey ?? this.info(), value))
+    }
+}
