21aede9dd07bb10f37eec1a471ee6504c2889dca
luoxi <luoxi18@huawei.com>
2025-08-11 19:41:40 +0800

    swiper modifier
    Signed-off-by: luoxi <luoxi18@huawei.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/SwiperModifier.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/SwiperModifier.ts
@@ -13,9 +13,6 @@
  * limitations under the License.
  */
 
-
-// WARNING! THIS FILE IS AUTO-GENERATED, DO NOT MAKE CHANGES, THEY WILL BE LOST ON NEXT GENERATION!
-
 import { AttributeModifier, CommonMethod, Bindable, ICurve } from "arkui/component/common"
 import { AttributeUpdaterFlag, CommonMethodModifier } from './CommonMethodModifier';
 import { IndicatorComponentController } from "arkui/component/indicatorcomponent"
