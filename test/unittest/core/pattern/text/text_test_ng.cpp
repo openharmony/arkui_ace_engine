ecec8d842cf7d8f2ef249fcd84b6e3d4b0a677f9
lijuan124 <lijuan124@h-partners.com>
2025-08-12 17:45:26 +0800

    text自适应字号对接引擎新接口
    Signed-off-by: lijuan124 <lijuan124@h-partners.com>

--- test/unittest/core/pattern/text/text_test_ng.cpp
+++ test/unittest/core/pattern/text/text_test_ng.cpp
@@ -2261,7 +2261,7 @@ HWTEST_F(TextTestNg, DidExceedMaxLines001, TestSize.Level1)
 {
     auto paragraph = MockParagraph::GetOrCreateMockParagraph();
     EXPECT_CALL(*paragraph, Layout);
-    EXPECT_CALL(*paragraph, DidExceedMaxLines).WillOnce(Return(true));
+    EXPECT_CALL(*paragraph, DidExceedMaxLinesInner).WillOnce(Return(true));
     /**
      * @tc.steps: step1. create textFrameNode.
      */
@@ -2315,7 +2315,7 @@ HWTEST_F(TextTestNg, DidExceedMaxLines001, TestSize.Level1)
 HWTEST_F(TextTestNg, DidExceedMaxLines002, TestSize.Level1)
 {
     auto paragraph = MockParagraph::GetOrCreateMockParagraph();
-    EXPECT_CALL(*paragraph, DidExceedMaxLines).WillOnce(Return(true));
+    EXPECT_CALL(*paragraph, DidExceedMaxLinesInner).WillOnce(Return(true));
     /**
      * @tc.steps: step1. create textFrameNode.
      */
