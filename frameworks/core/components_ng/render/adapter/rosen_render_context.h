ea17a0dc4bb6994bffa3491196e887fd2c761dd7
wangweiyuan <wangweiyuan2@huawei-partners.com>
2025-08-20 13:17:25 +0800

    effectLayer
    Signed-off-by: wangweiyuan <wangweiyuan2@huawei-partners.com>

--- frameworks/core/components_ng/render/adapter/rosen_render_context.h
+++ frameworks/core/components_ng/render/adapter/rosen_render_context.h
@@ -82,6 +82,9 @@ public:
 
     void InitContext(bool isRoot, const std::optional<ContextParam>& param, bool isLayoutNode) override;
 
+    void CreateNodeByType(
+        const ContextParam& param, bool isTextureExportNode, std::shared_ptr<Rosen::RSUIContext>& rsContext);
+
     void SyncGeometryFrame(const RectF& paintRect);
 
     void SyncGeometryProperties(GeometryNode* geometryNode, bool isRound = true, uint8_t flag = 0) override;
--- test/unittest/core/rosen/rosen_render_context_test.cpp
+++ test/unittest/core/rosen/rosen_render_context_test.cpp
@@ -775,6 +775,11 @@ HWTEST_F(RosenRenderContextTest, RosenRenderContextTest032, TestSize.Level1)
     rosenRenderContext->InitContext(false, contextParamValue8);
     EXPECT_EQ(rosenRenderContext->rsNode_ != nullptr, true);
     rosenRenderContext->rsNode_ = nullptr;
+    contextParam.type = RenderContext::ContextType::COMPOSITE_COMPONENT;
+    std::optional<RenderContext::ContextParam> contextParamValue10 = std::make_optional(contextParam);
+    rosenRenderContext->InitContext(false, contextParamValue10);
+    EXPECT_EQ(rosenRenderContext->rsNode_ != nullptr, true);
+    rosenRenderContext->rsNode_ = nullptr;
     contextParam.type = RenderContext::ContextType::EXTERNAL;
     std::optional<RenderContext::ContextParam> contextParamValue9 = std::make_optional(contextParam);
     rosenRenderContext->InitContext(false, contextParamValue9);
