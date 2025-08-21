9519c65d82d0dfd00b256c591f413916215d5da7
zhiqizhou <zhouzhiqi7@huawei.com>
2025-08-21 14:58:17 +0800

    修复自定义组件单帧展开时的依赖收集问题
    Signed-off-by: zhiqizhou <zhouzhiqi7@huawei.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ArkComponentRoot.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ArkComponentRoot.ts
@@ -49,8 +49,8 @@ export function ArkComponentRoot(
                 ObserveSingleton.instance.applyTaskDelayMutableStateChange(() => {
                     component.aboutToAppear()
                 })
-                return mutableState(true)
-            }, (_: MutableState<boolean> | undefined) =>
+                return true;
+            }, (_: boolean | undefined) =>
                 scheduleCallback(() => {
                     component.aboutToDisappear()
                 })
