efd193c8ff47671189deb83c5b0f281b78093217
刘永凯 <liuyongkai2@huawei-partners.com>
2025-08-25 11:57:57 +0800

    ace migrate from master to 0702
    Signed-off-by: 刘永凯 <liuyongkai2@huawei-partners.com>

--- frameworks/core/interfaces/native/implementation/text_area_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/text_area_modifier.cpp
@@ -32,7 +32,7 @@ namespace TextAreaModifier {
 Ark_NativePointer ConstructImpl(Ark_Int32 id,
                                 Ark_Int32 flags)
 {
-    auto frameNode = TextFieldModelNG::CreateFrameNode(id, u"", u"", true);
+    auto frameNode = TextFieldModelStatic::CreateTextAreaNode(id, u"", u"");
     CHECK_NULL_RETURN(frameNode, nullptr);
     frameNode->IncRefCount();
     return AceType::RawPtr(frameNode);
