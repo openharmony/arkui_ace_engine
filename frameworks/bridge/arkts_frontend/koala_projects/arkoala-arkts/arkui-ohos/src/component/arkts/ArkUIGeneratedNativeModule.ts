518e10a4ca44aba409c591b681f954644b958218
chenguanhao <chenguanhao2@huawei.com>
2025-08-26 20:50:43 +0800

    number rectify
    Signed-off-by: chenguanhao <chenguanhao2@huawei.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/component/arkts/ArkUIGeneratedNativeModule.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/component/arkts/ArkUIGeneratedNativeModule.ts
@@ -6802,7 +6802,7 @@ export class ArkUIGeneratedNativeModule {
     @ani.unsafe.Direct
     native static _WebContextMenuResult_selectAll(ptr: KPointer): void
     @ani.unsafe.Quick
-    native static _ConsoleMessage_ctor(message: KStringPtr, sourceId: KStringPtr, lineNumber: number, messageLevel: KInt): KPointer
+    native static _ConsoleMessage_ctor(message: KStringPtr, sourceId: KStringPtr, lineNumber: int32, messageLevel: KInt): KPointer
     @ani.unsafe.Direct
     native static _ConsoleMessage_getFinalizer(): KPointer
     @ani.unsafe.Quick
@@ -6810,7 +6810,7 @@ export class ArkUIGeneratedNativeModule {
     @ani.unsafe.Quick
     native static _ConsoleMessage_getSourceId(ptr: KPointer): string
     @ani.unsafe.Direct
-    native static _ConsoleMessage_getLineNumber(ptr: KPointer): number
+    native static _ConsoleMessage_getLineNumber(ptr: KPointer): int32
     @ani.unsafe.Direct
     native static _ConsoleMessage_getMessageLevel(ptr: KPointer): int32
     @ani.unsafe.Direct
@@ -6846,7 +6846,7 @@ export class ArkUIGeneratedNativeModule {
     @ani.unsafe.Quick
     native static _WebResourceResponse_getResponseHeader(ptr: KPointer): KInteropReturnBuffer
     @ani.unsafe.Direct
-    native static _WebResourceResponse_getResponseCode(ptr: KPointer): number
+    native static _WebResourceResponse_getResponseCode(ptr: KPointer): int32
     @ani.unsafe.Direct
     native static _WebResourceResponse_setResponseData(ptr: KPointer, thisArray: KSerializerBuffer, thisLength: int32): void
     @ani.unsafe.Quick
@@ -6858,7 +6858,7 @@ export class ArkUIGeneratedNativeModule {
     @ani.unsafe.Direct
     native static _WebResourceResponse_setResponseHeader(ptr: KPointer, thisArray: KSerializerBuffer, thisLength: int32): void
     @ani.unsafe.Direct
-    native static _WebResourceResponse_setResponseCode(ptr: KPointer, code: number): void
+    native static _WebResourceResponse_setResponseCode(ptr: KPointer, code: int32): void
     @ani.unsafe.Direct
     native static _WebResourceResponse_setResponseIsReady(ptr: KPointer, IsReady: KInt): void
     @ani.unsafe.Direct
@@ -6870,7 +6870,7 @@ export class ArkUIGeneratedNativeModule {
     @ani.unsafe.Quick
     native static _WebResourceError_getErrorInfo(ptr: KPointer): string
     @ani.unsafe.Direct
-    native static _WebResourceError_getErrorCode(ptr: KPointer): number
+    native static _WebResourceError_getErrorCode(ptr: KPointer): int32
     @ani.unsafe.Direct
     native static _JsGeolocation_ctor(): KPointer
     @ani.unsafe.Direct
--- frameworks/bridge/arkts_frontend/koala_projects/arkoala/framework/native/src/generated/dummy_impl.cc
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala/framework/native/src/generated/dummy_impl.cc
@@ -41359,7 +41359,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
     }
     Ark_ConsoleMessage CtorImpl(const Ark_String* message,
                                 const Ark_String* sourceId,
-                                const Ark_Number* lineNumber,
+                                const Ark_Int32* lineNumber,
                                 Ark_MessageLevel messageLevel)
     {
         if (!needGroupedLog(1))
@@ -41407,7 +41407,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
         appendGroupedLog(1, out);
         return {};
     }
-    Ark_Number GetLineNumberImpl(Ark_ConsoleMessage peer)
+    Ark_Int32 GetLineNumberImpl(Ark_ConsoleMessage peer)
     {
         if (!needGroupedLog(1))
             return {42};
@@ -41557,7 +41557,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
         appendGroupedLog(1, out);
         return {};
     }
-    Opt_Union_String_Number_Buffer_Resource GetResponseDataExImpl(Ark_WebResourceResponse peer)
+    Opt_Union_String_Int32_Buffer_Resource GetResponseDataExImpl(Ark_WebResourceResponse peer)
     {
         if (!needGroupedLog(1))
             return {};
@@ -41607,7 +41607,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
         appendGroupedLog(1, out);
         return {};
     }
-    Ark_Number GetResponseCodeImpl(Ark_WebResourceResponse peer)
+    Ark_Int32 GetResponseCodeImpl(Ark_WebResourceResponse peer)
     {
         if (!needGroupedLog(1))
             return {42};
@@ -41618,7 +41618,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
         return {42};
     }
     void SetResponseDataImpl(Ark_WebResourceResponse peer,
-                             const Ark_Union_String_Number_Resource_Buffer* data)
+                             const Ark_Union_String_Int32_Resource_Buffer* data)
     {
         if (!needGroupedLog(1))
             return;
@@ -41668,7 +41668,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
         appendGroupedLog(1, out);
     }
     void SetResponseCodeImpl(Ark_WebResourceResponse peer,
-                             const Ark_Number* code)
+                             const Ark_Int32* code)
     {
         if (!needGroupedLog(1))
             return;
@@ -41737,7 +41737,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
         appendGroupedLog(1, out);
         return {};
     }
-    Ark_Number GetErrorCodeImpl(Ark_WebResourceError peer)
+    Ark_Int32 GetErrorCodeImpl(Ark_WebResourceError peer)
     {
         if (!needGroupedLog(1))
             return {42};
