0306a48cbcabb143f35cf3ab7198e55b4fef09a7
Tianer Zhou <ezhoureal@gmail.com>
2025-09-01 11:36:56 +0800

    add copyright header
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>

--- test/benchmark/test_regression_detector.py
+++ test/benchmark/test_regression_detector.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+
+# Copyright (c) 2025 Huawei Device Co., Ltd.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Test script for the regression detector with mock data.
 This allows testing the regression detection logic without needing actual benchmark executables.
@@ -8,14 +22,13 @@ import json
 import os
 import tempfile
 import sys
-from pathlib import Path
 
 # Add the current directory to path to import regression_detector
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from regression_detector import (
-    BenchmarkRunner, RegressionAnalyzer, ReportGenerator, 
-    BenchmarkResult, NormalizedBenchmarkResult, RegressionReport
+    RegressionAnalyzer, ReportGenerator, 
+    BenchmarkResult, RegressionReport
 )
 import time
 
