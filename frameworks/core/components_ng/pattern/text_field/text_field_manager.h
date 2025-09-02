6cee549b251209689a059850edeac1ac3c0a9ac7
刘永凯 <liuyongkai2@huawei-partners.com>
2025-09-02 10:01:32 +0800

    drunk差异6.0
    Signed-off-by: 刘永凯 <liuyongkai2@huawei-partners.com>

--- frameworks/core/components_ng/pattern/text_field/text_field_manager.h
+++ frameworks/core/components_ng/pattern/text_field/text_field_manager.h
@@ -53,6 +53,7 @@ public:
     ~TextFieldManagerNG() override;
 
     void SetClickPosition(const Offset& position) override;
+
     const Offset& GetClickPosition() override
     {
         return position_;
@@ -249,6 +250,16 @@ public:
     {
         return clickPositionOffset_;
     }
+    
+    void AddAvoidKeyboardCallback(int32_t id, bool isCustomKeyboard, const std::function<void()>&& callback);
+
+    void RemoveAvoidKeyboardCallback(int32_t id)
+    {
+        avoidCustomKeyboardCallbacks_.erase(id);
+        avoidSystemKeyboardCallbacks_.erase(id);
+    }
+
+    void OnAfterAvoidKeyboard(bool isCustomKeyboard);
 
     RefPtr<FrameNode> FindScrollableOfFocusedTextField(const RefPtr<FrameNode>& textField);
     void AddTextFieldInfo(const TextFieldInfo& textFieldInfo);
@@ -275,16 +286,6 @@ public:
     {
         return isImeAttached_;
     }
-
-    void AddAvoidKeyboardCallback(int32_t id, bool isCustomKeyboard, const std::function<void()>&& callback);
-
-    void RemoveAvoidKeyboardCallback(int32_t id)
-    {
-        avoidCustomKeyboardCallbacks_.erase(id);
-        avoidSystemKeyboardCallbacks_.erase(id);
-    }
-
-    void OnAfterAvoidKeyboard(bool isCustomKeyboard);
 
     int32_t GetContextTriggerAvoidTaskOrientation() const
     {
