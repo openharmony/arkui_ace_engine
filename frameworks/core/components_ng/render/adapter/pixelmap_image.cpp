c8a625c2b5ca53d2e5dddc9fdea4520c5bb055c2
huang-zhongyan <huangzhongyan2@h-partners.com>
2025-09-06 17:28:46 +0800

    style 行宽120限制
    Signed-off-by: huang-zhongyan <huangzhongyan2@h-partners.com>
    Change-Id: Iae5aa7ac510f5ee8ba15f5531aeade8064347eae

--- frameworks/core/components_ng/render/adapter/pixelmap_image.cpp
+++ frameworks/core/components_ng/render/adapter/pixelmap_image.cpp
@@ -337,7 +337,8 @@ void PixelMapImage::DrawToRSCanvas(
         return;
     }
     ACE_SCOPED_TRACE("DrawToRSCanvas %s-%f-%f-%d-%d-%d", dfxConfig.ToStringWithSrc().c_str(),
-        dfxConfig.GetFrameSizeWidth(), dfxConfig.GetFrameSizeHeight(), pixmap->GetWidth(), pixmap->GetHeight(), dfxConfig.GetAutoResize());
+        dfxConfig.GetFrameSizeWidth(), dfxConfig.GetFrameSizeHeight(),
+        pixmap->GetWidth(), pixmap->GetHeight(), dfxConfig.GetAutoResize());
     const auto& config = GetPaintConfig();
     RSBrush brush;
     RSSamplingOptions options;
