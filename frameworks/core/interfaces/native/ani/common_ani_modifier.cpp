0632f50996c524a8fa1d553ee1f4b0d8eab7a127
fangzhiyuan <fangzhiyuan5@huawei.com>
2025-09-10 09:02:49 +0000

    未使用代码删除
    Signed-off-by: fangzhiyuan <fangzhiyuan5@huawei.com>

--- frameworks/core/interfaces/native/ani/common_ani_modifier.cpp
+++ frameworks/core/interfaces/native/ani/common_ani_modifier.cpp
@@ -832,7 +832,6 @@ void SetThemeScopeId(ani_env* env, ani_int themeScopeId)
 void CreateAndBindTheme(ani_env* env, ani_int themeScopeId, ani_int themeId,
     const std::vector<Ark_ResourceColor>& colorArray, ani_int colorMode, void* func)
 {
-    int32_t themeScopeIdValue = static_cast<int32_t>(themeScopeId);
     int32_t colorModeValue = static_cast<int32_t>(colorMode);
 
     std::vector<uint32_t> colors;
