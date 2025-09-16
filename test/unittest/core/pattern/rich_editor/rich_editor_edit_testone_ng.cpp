7ba635ee091eec2861b7d4122a3076aa929ff579
tzcurtain <zhoukechen2@huawei.com>
2025-09-16 11:47:36 +0800

    textfield/richeditor消费escape按键事件
    Signed-off-by: tzcurtain <zhoukechen2@huawei.com>

--- test/unittest/core/pattern/rich_editor/rich_editor_edit_testone_ng.cpp
+++ test/unittest/core/pattern/rich_editor/rich_editor_edit_testone_ng.cpp
@@ -639,7 +639,7 @@ HWTEST_F(RichEditorEditTestOneNg, HandleOnEscape001, TestSize.Level1)
     ASSERT_NE(richEditorNode_, nullptr);
     auto richEditorPattern = richEditorNode_->GetPattern<RichEditorPattern>();
     ASSERT_NE(richEditorPattern, nullptr);
-    EXPECT_FALSE(richEditorPattern->HandleOnEscape());
+    EXPECT_TRUE(richEditorPattern->HandleOnEscape());
 }
 
 /**
