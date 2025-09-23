2f0e8f386c8015a4db2e6ae580301e2d8dca5d75
song-song-song <songqifan7@huawei.com>
2025-09-23 17:45:19 +0800

    columnsplit适配
    Signed-off-by: song-song-song <songqifan7@huawei.com>

--- frameworks/core/components_ng/pattern/linear_split/linear_split_model_ng_static.h
+++ frameworks/core/components_ng/pattern/linear_split/linear_split_model_ng_static.h
@@ -25,7 +25,7 @@ class  ACE_EXPORT LinearSplitModelNGStatic : public OHOS::Ace::LinearSplitModel 
 public:
     static RefPtr<FrameNode> CreateFrameNode(int32_t nodeId, SplitType splitType);
     static void SetDivider(FrameNode* frameNode, NG::SplitType splitType,
-        const std::optional<ItemDivider>& optDivider);
+        const std::optional<ColumnSplitDivider>& optDivider);
 };
 } // namespace OHOS::Ace::NG
 
