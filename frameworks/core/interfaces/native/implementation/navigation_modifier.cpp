17b82386e91fa0882ae9f3bfc80e01acba93508e
guozejun <guozejun@huawei.com>
2025-09-25 17:05:12 +0800

    Fix compile
    Signed-off-by: guozejun <guozejun@huawei.com>
    Change-Id: I774db50d67d4ec74822bde86d1b0b26494139b74

--- frameworks/core/interfaces/native/implementation/navigation_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/navigation_modifier.cpp
@@ -708,7 +708,6 @@ const GENERATED_ArkUINavigationModifier* GetNavigationModifier()
         NavigationAttributeModifier::SetOnTitleModeChangeImpl,
         NavigationAttributeModifier::SetOnNavBarStateChangeImpl,
         NavigationAttributeModifier::SetOnNavigationModeChangeImpl,
-        NavigationAttributeModifier::SetNavDestinationImpl,
         NavigationAttributeModifier::SetCustomNavContentTransitionImpl,
         NavigationAttributeModifier::SetSystemBarStyleImpl,
         NavigationAttributeModifier::SetRecoverableImpl,
