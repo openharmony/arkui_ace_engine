cde44df2f7b47b37e2c4305284bc2b2a43ab416f
ohci1 <ohci1@noreply.gitcode.com>
2025-09-26 14:48:26 +0800

    fix the bug of redundant operations for branch delete / create
    Signed-off-by: jiyujia926 <jiyujia3@huawei.com>
    
    
    revert merge request: fix the bug of redundant operations for branch delete / create
    
    6a193109a27c97d1d0bf0a57b9872a1f5c435efd
    
    This revert merge request !72971

--- frameworks/core/components_ng/syntax/arkoala_condition_scope.cpp
+++ frameworks/core/components_ng/syntax/arkoala_condition_scope.cpp
@@ -25,13 +25,4 @@ constexpr char CONDITION_SCOPE_TAG[] = "ConditionScope";
 
 ConditionScopeNode::ConditionScopeNode(int32_t nodeId) : UINode(CONDITION_SCOPE_TAG, nodeId) {}
 
-void ConditionScopeNode::AddChild(
-    const RefPtr<UINode>& child, int32_t slot, bool silently, bool addDefaultTransition, bool addModalUiextension)
-{
-    CHECK_NULL_VOID(child);
-    UINode::AddChild(child, slot, silently, addDefaultTransition, addModalUiextension);
-    // for geometryTransition, let all reused children call UpdateGeometryTransition.
-    LayoutProperty::UpdateAllGeometryTransition(child);
-}
-
 } // namespace OHOS::Ace::NG
