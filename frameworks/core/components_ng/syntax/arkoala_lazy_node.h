638749b40dcc5164933eefedcaed805dba247ea8
liubihao <liubihao1@huawei.com>
2025-10-11 16:44:07 +0800

    [syntax1.2] feat: lazyforeach build all children in non-scroller.
    Signed-off-by: liubihao <liubihao1@huawei.com>

--- frameworks/core/components_ng/syntax/arkoala_lazy_node.h
+++ frameworks/core/components_ng/syntax/arkoala_lazy_node.h
@@ -96,6 +96,8 @@ public:
 
     void SetJSViewActive(bool active = true, bool isLazyForEachNode = false, bool isReuse = false) override;
 
+    void BuildAllChildren();
+
     // used for drag move operation.
     void SetOnMove(std::function<void(int32_t, int32_t)>&& onMove);
     void SetOnMoveFromTo(std::function<void(int32_t, int32_t)>&& onMoveFromTo);
