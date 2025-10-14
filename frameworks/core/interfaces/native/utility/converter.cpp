27df7bafba8a14a4f3627286b8468f0502db6502
zhourenfeng <zhourenfeng@h-partners.com>
2025-10-14 10:16:59 +0800

    fix some convert bug
    Signed-off-by: zhourenfeng <zhourenfeng@h-partners.com>

--- frameworks/core/interfaces/native/utility/converter.cpp
+++ frameworks/core/interfaces/native/utility/converter.cpp
@@ -2124,7 +2124,7 @@ template std::optional<Dimension> OptConvertFromArkNumStrRes<Ark_Length, Ark_Num
 
 std::optional<Dimension> OptConvertFromArkLength(const Ark_Length& src, DimensionUnit defaultUnit)
 {
-    std::optional<Dimension> dimension;
+    std::optional<Dimension> dimension = std::nullopt;
     Converter::VisitUnion(src,
         [&dimension](const Ark_Number& value) {
             dimension = Converter::Convert<Dimension>(value);
@@ -2135,11 +2135,7 @@ std::optional<Dimension> OptConvertFromArkLength(const Ark_Length& src, Dimensio
         [&dimension, defaultUnit](const Ark_Resource& value) {
             dimension = OptConvertFromArkLengthResource(value, defaultUnit);
         },
-        [&dimension]() {
-            dimension = Dimension();
-        });
-
-    dimension->SetUnit(defaultUnit);
+        [&dimension]() {});
     return dimension;
 }
 
