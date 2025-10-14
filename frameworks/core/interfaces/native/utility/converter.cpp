a22413f844d7c36b03a4ff9ad5825f37272b10b4
刘永凯 <liuyongkai2@huawei-partners.com>
2025-10-14 22:31:38 +0800

    overlay_master
    Signed-off-by: 刘永凯 <liuyongkai2@huawei-partners.com>

--- frameworks/core/interfaces/native/utility/converter.cpp
+++ frameworks/core/interfaces/native/utility/converter.cpp
@@ -1583,6 +1583,7 @@ template<>
 OverlayOptions Convert(const Ark_OverlayOptions& src)
 {
     OverlayOptions dst;
+    dst.align = Alignment::TOP_LEFT;
     auto align = Converter::OptConvert<Alignment>(src.align);
     if (align) {
         dst.align = align.value();
