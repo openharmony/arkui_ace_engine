2024f797c3862a2575839539a9be2fb7b9d813b9
chijunwen <chijunwen@h-partners.com>
2025-10-20 17:53:43 +0800

    fix common markAnchor undefined
    Signed-off-by: chijunwen <chijunwen@h-partners.com>
    Change-Id: I7a0d60eea3a1ce25b1f96ea7cb329a696da6a2cb

--- frameworks/core/interfaces/native/implementation/common_method_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/common_method_modifier.cpp
@@ -3588,7 +3588,7 @@ void SetMarkAnchorImpl(Ark_NativePointer node,
         }
         ViewAbstractModelStatic::MarkAnchor(frameNode, anchorOpt->first);
     } else {
-        ViewAbstractModelStatic::MarkAnchor(frameNode, std::nullopt);
+        ViewAbstractModelStatic::MarkAnchor(frameNode, OffsetT<Dimension>(0.0_vp, 0.0_vp));
     }
     ViewAbstractModelStatic::ResetMarkAnchorStart(frameNode);
 }
