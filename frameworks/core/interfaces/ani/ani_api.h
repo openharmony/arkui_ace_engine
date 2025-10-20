92ada68e111c2426f644d695d92b1da8eaf833a1
liyou <liyou63@h-partners.com>
2025-10-20 19:52:51 +0800

    add trace between applyOptionsFinish and applyAttributesFinish for the attributes set measurement
    Signed-off-by: liyou <liyou63@h-partners.com>
    Change-Id: I1b8ec353f58c092cb00aa1187e757cb60126def4

--- frameworks/core/interfaces/ani/ani_api.h
+++ frameworks/core/interfaces/ani/ani_api.h
@@ -697,6 +697,7 @@ struct ArkUIAniCanvasModifier {
 };
 
 struct ArkUIAniTraceModifier {
+    bool (*getAttributeSetTraceEnabled)();
     void (*traceBegin)(const std::string& traceName);
     void (*traceEnd)();
     void (*asyncTraceBegin)(const std::string& traceName, int taskId);
