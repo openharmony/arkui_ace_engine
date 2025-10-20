ae061907dd57a23295ab93016acece519ba1bae6
zany_pink <zhangyi580@huawei.com>
2025-10-20 20:44:29 +0800

    fix apllySync
    Signed-off-by: zany_pink <zhangyi580@huawei.com>

--- frameworks/bridge/declarative_frontend/state_mgmt/src/lib/v2/v2_change_observation.ts
+++ frameworks/bridge/declarative_frontend/state_mgmt/src/lib/v2/v2_change_observation.ts
@@ -1423,6 +1423,11 @@ class ObserveV2 {
       throw new BusinessError(FUNC_CALLED_IN_COMPUTED_ILLEGAL, message);
     }
 
+    if (this.applySyncRunningCount_) {
+      stateMgmtConsole.warn('UIUtils.applySync will be skipped when called within another UIUtils.applySync. The inner UIUtils.applySync will return undefined');
+      return undefined;
+    }
+
     this.applySyncRunningCount_++;
 
     const names = [
