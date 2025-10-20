8adaf95a22ef78387cfd3fe04bb688f4643d1a80
openharmony_ci <openharmony_ci@noreply.gitcode.com>
2025-10-20 18:17:16 +0800

    键盘避让接口去除蓝黄隔离
    Signed-off-by: WendongPang <pangwd_winston@163.com>
    
    
    cherry-pick merge request: 键盘避让接口去除蓝黄隔离
    
    344b242e31d68642d56b005c1de03412cd87bd5f
    
    This cherry-pick merge request !73322

--- adapter/ohos/entrance/ui_content_impl.h
+++ adapter/ohos/entrance/ui_content_impl.h
@@ -520,7 +520,6 @@ private:
     int32_t instanceId_ = -1;
     int32_t lastRotation = -1;
     OHOS::sptr<OHOS::Rosen::IWindowDragListener> dragWindowListener_ = nullptr;
-    OHOS::sptr<OHOS::Rosen::IOccupiedAreaChangeListener> occupiedAreaChangeListener_ = nullptr;
     OHOS::sptr<OHOS::Rosen::IAvoidAreaChangedListener> avoidAreaChangedListener_ = nullptr;
     OHOS::sptr<OHOS::Rosen::IWaterfallModeChangeListener> waterfallModeChangeListener_ = nullptr;
     OHOS::sptr<OHOS::Rosen::IWindowRectChangeListener> windowRectChangeListener_ = nullptr;
