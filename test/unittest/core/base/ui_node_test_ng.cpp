81349846e2e890163e755e5eeed8958ea4e349f7
happy <wangxueli8@huawei.com>
2025-10-22 15:43:05 +0800

    补充ui_node_test_ng用例
    Signed-off-by: happy <wangxueli8@huawei.com>
    Change-Id: Ibc6b8bacf4e2fd6f420d7933d81bdd3605c19371

--- test/unittest/core/base/ui_node_test_ng.cpp
+++ test/unittest/core/base/ui_node_test_ng.cpp
@@ -38,65 +38,12 @@
 #include "core/pipeline_ng/pipeline_context.h"
 #include "core/components_ng/base/view_abstract.h"
 #include "core/components_ng/pattern/navigation/navigation_pattern.h"
+#include "test/unittest/core/base/ui_node_test_ng.h"
 
 using namespace testing;
 using namespace testing::ext;
 
 namespace OHOS::Ace::NG {
-namespace {
-const RefPtr<FrameNode> TEN0 = FrameNode::CreateFrameNode("zero", 10, AceType::MakeRefPtr<Pattern>(), true);
-const RefPtr<FrameNode> ZERO = FrameNode::CreateFrameNode("zero", 0, AceType::MakeRefPtr<Pattern>(), true);
-const RefPtr<FrameNode> ONE = FrameNode::CreateFrameNode("one", 1, AceType::MakeRefPtr<Pattern>(), true);
-const RefPtr<FrameNode> TWO = FrameNode::CreateFrameNode("two", 2, AceType::MakeRefPtr<Pattern>());
-const RefPtr<FrameNode> THREE = FrameNode::CreateFrameNode("three", 3, AceType::MakeRefPtr<Pattern>());
-const RefPtr<FrameNode> FOUR = FrameNode::CreateFrameNode("four", 4, AceType::MakeRefPtr<Pattern>());
-const RefPtr<FrameNode> FIVE = FrameNode::CreateFrameNode("five", 5, AceType::MakeRefPtr<Pattern>());
-const RefPtr<FrameNode> F_ONE = FrameNode::CreateFrameNode("one", 5, AceType::MakeRefPtr<Pattern>());
-const int32_t TEST_ID_ONE = 21;
-const int32_t TEST_ID_TWO = 22;
-} // namespace
-
-class TestNode : public UINode {
-    DECLARE_ACE_TYPE(TestNode, UINode);
-
-public:
-    static RefPtr<TestNode> CreateTestNode(int32_t nodeId)
-    {
-        auto spanNode = MakeRefPtr<TestNode>(nodeId);
-        return spanNode;
-    }
-
-    bool IsAtomicNode() const override
-    {
-        return true;
-    }
-
-    explicit TestNode(int32_t nodeId) : UINode("TestNode", nodeId) {}
-
-    HitTestResult TouchTest(const PointF& globalPoint, const PointF& parentLocalPoint, const PointF& parentRevertPoint,
-        TouchRestrict& touchRestrict, TouchTestResult& result, int32_t touchId, ResponseLinkResult& responseLinkResult,
-        bool isDispatch = false) override
-    {
-        return hitTestResult_;
-    }
-
-    HitTestResult MouseTest(const PointF& globalPoint, const PointF& parentLocalPoint, MouseTestResult& onMouseResult,
-        MouseTestResult& onHoverResult, RefPtr<FrameNode>& hoverNode) override
-    {
-        return hitTestResult_;
-    }
-
-    HitTestResult AxisTest(const PointF &globalPoint, const PointF &parentLocalPoint, const PointF &parentRevertPoint,
-        TouchRestrict &touchRestrict, AxisTestResult &axisResult) override
-    {
-        return hitTestResult_;
-    }
-
-    ~TestNode() override = default;
-
-private:
-    HitTestResult hitTestResult_;
-};
 
 class UINodeTestNg : public testing::Test {
 public:
