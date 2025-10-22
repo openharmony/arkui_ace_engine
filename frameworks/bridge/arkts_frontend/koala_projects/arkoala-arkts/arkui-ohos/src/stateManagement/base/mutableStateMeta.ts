e946c64c5a6c6f96dfb1138169e1bc0fa65b17ce
jiyujia926 <jiyujia3@huawei.com>
2025-10-22 11:48:41 +0800

    fix makeObserved
    Signed-off-by: jiyujia926 <jiyujia3@huawei.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/stateManagement/base/mutableStateMeta.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/stateManagement/base/mutableStateMeta.ts
@@ -59,6 +59,7 @@ export class MutableStateMeta extends MutableStateMetaBase implements IMutableSt
     enableDynamicCompatible: boolean = false;
     dynamicAddRefFunc?: () => void;
     dynamicFireChangeFunc?: () => void;
+    private hasFired: boolean;
 
     constructor(info: string, metaDependency?: MutableState<int32>) {
         super(info);
@@ -66,6 +67,7 @@ export class MutableStateMeta extends MutableStateMetaBase implements IMutableSt
         this.bindingRefs_ = new Set<WeakRef<ITrackedDecoratorRef>>();
         this.weakThis = new WeakRef<IBindingSource>(this);
         this.metaValue = 0;
+        this.hasFired = false;
     }
 
     public registerDynamicHookFunc(addRef: () => void, fireChange: () => void) {
@@ -75,15 +77,21 @@ export class MutableStateMeta extends MutableStateMetaBase implements IMutableSt
     }
 
     public addRef(): void {
+        const renderingComponent = ObserveSingleton.instance.renderingComponent;
+        if (renderingComponent <= ObserveSingleton.RenderingComponent) {
+            return;
+        }
+        // >= RenderingMonitor means Monitor/Computed/PersistentStorage
         if (
-            ObserveSingleton.instance.renderingComponent === ObserveSingleton.RenderingMonitor ||
-            ObserveSingleton.instance.renderingComponent === ObserveSingleton.RenderingComputed ||
-            ObserveSingleton.instance.renderingComponent === ObserveSingleton.RenderingPersistentStorage
+            renderingComponent >= ObserveSingleton.RenderingMonitor
         ) {
             this.bindingRefs_.add(ObserveSingleton.instance.renderingComponentRef!.weakThis);
             ObserveSingleton.instance.renderingComponentRef!.reverseBindings.add(this.weakThis);
         } else {
             this.__metaDependency!.value;
+        }
+        if (this.hasFired) {
+            this.hasFired = false;
         }
         if (this.enableDynamicCompatible) {
             this.dynamicAddRefFunc?.();
@@ -104,8 +112,9 @@ export class MutableStateMeta extends MutableStateMetaBase implements IMutableSt
                 }
             });
         }
-        if (this.shouldFireChange()) {
+        if (!this.hasFired && this.shouldFireChange()) {
             ObserveSingleton.instance.changeMutableState(this);
+            this.hasFired = true;
             if (StateUpdateLoop.canRequestFrame) {
                 ArkUIAniModule._CustomNode_RequestFrame();
                 StateUpdateLoop.canRequestFrame = false;
