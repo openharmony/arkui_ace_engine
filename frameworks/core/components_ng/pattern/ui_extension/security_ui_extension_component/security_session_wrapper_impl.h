e452fb47aba9a1a0fcd01829d351494457b3d9cb
hanjiajie2000 <hanjiajie1@h-partners.com>
2025-10-24 12:05:31 +0800

    UEC问题修复迁移SUEC
    Signed-off-by: hanjiajie2000 <hanjiajie1@h-partners.com>

--- frameworks/core/components_ng/pattern/ui_extension/security_ui_extension_component/security_session_wrapper_impl.h
+++ frameworks/core/components_ng/pattern/ui_extension/security_ui_extension_component/security_session_wrapper_impl.h
@@ -102,6 +102,7 @@ public:
     uint32_t GetReasonDump() const override;
     void NotifyUieDump(const std::vector<std::string>& params, std::vector<std::string>& info) override;
     int32_t GetInstanceIdFromHost() const;
+    int32_t GetInstanceId() const;
     bool SendBusinessDataSyncReply(UIContentBusinessCode code, const AAFwk::Want& data, AAFwk::Want& reply,
         RSSubsystemId subSystemId = RSSubsystemId::ARKUI_UIEXT) override;
     bool SendBusinessData(UIContentBusinessCode code, const AAFwk::Want& data, BusinessDataSendType type,
