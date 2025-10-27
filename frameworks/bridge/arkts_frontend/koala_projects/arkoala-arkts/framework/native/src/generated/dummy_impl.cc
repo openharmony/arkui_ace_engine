bd64709f99cd1cf79a130c3e5180409169889688
liuyongkai2 <liuyongkai2@huawei-partners.com>
2025-10-27 14:21:29 +0800

    onKeyEvent
    Signed-off-by: liuyongkai2 <liuyongkai2@huawei-partners.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/framework/native/src/generated/dummy_impl.cc
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/framework/native/src/generated/dummy_impl.cc
@@ -2993,7 +2993,7 @@ namespace OHOS::Ace::NG::GeneratedModifier {
         appendGroupedLog(1, out);
     }
     void SetOnKeyEventImpl(Ark_NativePointer node,
-                           const Opt_Callback_KeyEvent_Void* value)
+                           const Opt_Callback_KeyEvent_Boolean* value)
     {
         if (!needGroupedLog(1))
         {
