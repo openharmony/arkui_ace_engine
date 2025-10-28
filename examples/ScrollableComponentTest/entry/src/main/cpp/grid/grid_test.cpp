2868a4fee5d776c8cd53d70e352131efdaf3b37d
13588035024 <yeyinglong@hisilicon.com>
2025-10-28 20:48:17 +0800

    新增sample代码
    Signed-off-by: 13588035024 <yeyinglong@hisilicon.com>

--- examples/ScrollableComponentTest/entry/src/main/cpp/grid/grid_test.cpp
+++ examples/ScrollableComponentTest/entry/src/main/cpp/grid/grid_test.cpp
@@ -1045,7 +1045,8 @@ napi_value GridTest::CreateNativeNode(napi_env env, napi_callback_info info)
     auto scroll = AssembleFinalLayout();
     std::string id(xComponentID);
     if (OH_NativeXComponent_AttachNativeRootNode(
-            PluginManager::GetInstance()->GetNativeXComponent(id), scroll->GetComponent()) == INVALID_PARAM) {
+            PluginManager::GetInstance()->GetNativeXComponent(id), 
+            scroll->GetComponent == INVALID_PARAM)) {
         OH_LOG_Print(
             LOG_APP, LOG_ERROR, LOG_PRINT_DOMAIN, "GridTest", "OH_NativeXComponent_AttachNativeRootNode failed");
     }
