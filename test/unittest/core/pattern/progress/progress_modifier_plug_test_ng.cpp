4bfdc135616e2285535826c3eccdcd6cbb61995e
zhanghang <zhanghang160@huawei-partners.com>
2025-11-05 09:50:44 +0800

    progress change isolation version
    Signed-off-by: zhanghang <zhanghang160@huawei-partners.com>

--- test/unittest/core/pattern/progress/progress_modifier_plug_test_ng.cpp
+++ test/unittest/core/pattern/progress/progress_modifier_plug_test_ng.cpp
@@ -100,6 +100,71 @@ HWTEST_F(ProgressModifierPlugTestNg, CapsuleProgressModifier001, TestSize.Level1
 }
 
 /**
+ * @tc.name: CapsuleProgressModifier002
+ * @tc.desc: Test the gradient color of the capsule progress.
+ * @tc.type: FUNC
+ */
+HWTEST_F(ProgressModifierPlugTestNg, CapsuleProgressModifier002, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. Create ProgressModifier and set ProgressModifier property.
+     * @tc.expected: step1. Check the ProgressModifier property value.
+     */
+    Gradient gradient;
+    GradientColor gradientColorEnd;
+    GradientColor gradientColorStart;
+    gradientColorEnd.SetLinearColor(LinearColor(Color::WHITE));
+    gradientColorStart.SetLinearColor(LinearColor(Color::BLACK));
+    gradientColorEnd.SetDimension(Dimension(0.0));
+    gradient.AddColor(gradientColorEnd);
+    gradientColorStart.SetDimension(Dimension(1.0));
+    gradient.AddColor(gradientColorStart);
+
+    auto pipeline = PipelineBase::GetCurrentContext();
+    pipeline->taskExecutor_ = AceType::MakeRefPtr<MockTaskExecutor>();
+    auto progressModifier = AceType::MakeRefPtr<ProgressModifier>(frameNode_);
+    progressModifier->SetVisible(true);
+    progressModifier->SetProgressType(PROGRESS_TYPE_CAPSULE);
+    LinearColor linearColor;
+    progressModifier->SetColor(linearColor);
+    progressModifier->SetBackgroundColor(linearColor);
+    progressModifier->SetRingProgressColor(gradient);
+    OffsetF offset(0, 0);
+    progressModifier->SetContentOffset(offset);
+
+    Testing::MockCanvas canvas;
+    DrawingContext context { canvas, CONTEXT_WIDTH, CONTEXT_HEIGHT };
+    EXPECT_CALL(canvas, AttachBrush(_)).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, AttachPen(_)).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, DetachPen()).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, Restore()).Times(AtLeast(1));
+    EXPECT_CALL(canvas, DetachBrush()).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, DrawRoundRect(_)).Times(AtLeast(1));
+    EXPECT_CALL(canvas, DrawPath(_)).Times(AtLeast(1));
+    EXPECT_CALL(canvas, Save()).Times(AtLeast(1));
+    EXPECT_CALL(canvas, ClipPath(_, _, _)).Times(AtLeast(1));
+    EXPECT_CALL(canvas, DrawRect(_)).Times(AtLeast(1));
+
+    /**
+     * @tc.steps: step2. Set different properties, call function onDraw.
+     * @tc.expected: step2. Set the properties success.
+     */
+    SizeF progressContentSize(200.0, 100.0);
+    progressModifier->SetContentSize(progressContentSize);
+    progressModifier->SetValue(20.0);
+    progressModifier->PaintCapsuleWithGradient(context.canvas, progressModifier->offset_->Get(),
+        progressModifier->contentSize_->Get(), progressModifier->capsuleBorderRadius_->Get());
+    SizeF progressContentSize1(100.0, 200.0);
+    progressModifier->SetContentSize(progressContentSize1);
+    progressModifier->PaintVerticalCapsuleWithGradient(context.canvas, progressModifier->offset_->Get(),
+        progressModifier->contentSize_->Get(), progressModifier->capsuleBorderRadius_->Get());
+    auto gradientColors = progressModifier->ringProgressColors_->Get().GetGradient().GetColors();
+    EXPECT_EQ(gradientColors.size(), 2);
+    EXPECT_EQ(gradientColors[0].GetLinearColor(), LinearColor(Color::WHITE));
+    EXPECT_EQ(gradientColors[1].GetLinearColor(), LinearColor(Color::BLACK));
+}
+
+/**
  * @tc.name: LinearProgressModifier001
  * @tc.desc: Test the gradient color of the linear progress.
  * @tc.type: FUNC
@@ -170,6 +235,67 @@ HWTEST_F(ProgressModifierPlugTestNg, LinearProgressModifier001, TestSize.Level1)
     progressModifier->SetValue(20.0);
     progressModifier->onDraw(context);
     EXPECT_NE(progressModifier->sweepingDate_, 0.0f);
+    auto gradientColors = progressModifier->ringProgressColors_->Get().GetGradient().GetColors();
+    EXPECT_EQ(gradientColors.size(), 2);
+    EXPECT_EQ(gradientColors[0].GetLinearColor(), LinearColor(Color::WHITE));
+    EXPECT_EQ(gradientColors[1].GetLinearColor(), LinearColor(Color::BLACK));
+}
+
+/**
+ * @tc.name: LinearProgressModifier002
+ * @tc.desc: Test the gradient color of the linear progress.
+ * @tc.type: FUNC
+ */
+HWTEST_F(ProgressModifierPlugTestNg, LinearProgressModifier002, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. Create ProgressModifier and set ProgressModifier property.
+     * @tc.expected: step1. Check the ProgressModifier property value.
+     */
+    Gradient gradient;
+    GradientColor gradientColorEnd;
+    GradientColor gradientColorStart;
+    gradientColorEnd.SetLinearColor(LinearColor(Color::WHITE));
+    gradientColorStart.SetLinearColor(LinearColor(Color::BLACK));
+    gradientColorEnd.SetDimension(Dimension(0.0));
+    gradient.AddColor(gradientColorEnd);
+    gradientColorStart.SetDimension(Dimension(1.0));
+    gradient.AddColor(gradientColorStart);
+
+    auto pipeline = PipelineBase::GetCurrentContext();
+    pipeline->taskExecutor_ = AceType::MakeRefPtr<MockTaskExecutor>();
+    auto progressModifier = AceType::MakeRefPtr<ProgressModifier>(frameNode_);
+    progressModifier->SetVisible(true);
+    progressModifier->SetProgressType(PROGRESS_TYPE_LINEAR);
+    LinearColor linearColor;
+    progressModifier->SetColor(linearColor);
+    progressModifier->SetBackgroundColor(linearColor);
+    progressModifier->SetRingProgressColor(gradient);
+    OffsetF offset(0, 0);
+    progressModifier->SetContentOffset(offset);
+
+    Testing::MockCanvas canvas;
+    DrawingContext context { canvas, CONTEXT_WIDTH, CONTEXT_HEIGHT };
+    EXPECT_CALL(canvas, AttachBrush(_)).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, AttachPen(_)).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, DetachPen()).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, Restore()).Times(AtLeast(1));
+    EXPECT_CALL(canvas, DetachBrush()).WillRepeatedly(ReturnRef(canvas));
+    EXPECT_CALL(canvas, DrawRoundRect(_)).Times(AtLeast(1));
+    EXPECT_CALL(canvas, DrawPath(_)).Times(AtLeast(1));
+    EXPECT_CALL(canvas, Save()).Times(AtLeast(1));
+    EXPECT_CALL(canvas, ClipPath(_, _, _)).Times(AtLeast(1));
+    EXPECT_CALL(canvas, DrawRect(_)).Times(AtLeast(1));
+
+    /**
+     * @tc.steps: step2. Set different properties, call function onDraw.
+     * @tc.expected: step2. Set the properties success.
+     */
+    SizeF progressContentSize(200.0, 100.0);
+    progressModifier->SetContentSize(progressContentSize);
+    progressModifier->SetValue(20.0);
+    progressModifier->PaintLinearWithGradient(
+        context.canvas, progressModifier->offset_->Get(), progressModifier->contentSize_->Get());
     auto gradientColors = progressModifier->ringProgressColors_->Get().GetGradient().GetColors();
     EXPECT_EQ(gradientColors.size(), 2);
     EXPECT_EQ(gradientColors[0].GetLinearColor(), LinearColor(Color::WHITE));
