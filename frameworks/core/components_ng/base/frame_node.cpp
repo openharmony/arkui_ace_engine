57f40bc7f678775415dadf2297892fc046b96466
zenix_zxy <zhengxingyou1@huawei.com>
2025-11-06 21:56:07 +0800

    fixoverlaymkdirty
    Signed-off-by: zenix_zxy <zhengxingyou1@huawei.com>
    Change-Id: I668d5555cc1a158605acd52461646d82873eeef5

--- frameworks/core/components_ng/base/frame_node.cpp
+++ frameworks/core/components_ng/base/frame_node.cpp
@@ -5742,13 +5742,7 @@ void FrameNode::LayoutOverlay()
     auto childLayoutProperty = overlayNode_->GetLayoutProperty();
     CHECK_NULL_VOID(childLayoutProperty);
     childLayoutProperty->GetOverlayOffset(offsetX, offsetY);
-    auto renderContext = GetRenderContext();
-    CHECK_NULL_VOID(renderContext);
-    auto options = renderContext->GetOverlayTextValue(NG::OverlayOptions());
-    auto direction = options.direction;
-    if (direction == TextDirection::AUTO) {
-        direction = AceApplicationInfo::GetInstance().IsRightToLeft() ? TextDirection::RTL : TextDirection::LTR;
-    }
+    auto direction = childLayoutProperty->GetNonAutoLayoutDirection();
     if (direction == TextDirection::RTL) {
         offsetX = -offsetX;
     }
@@ -5761,8 +5755,6 @@ void FrameNode::LayoutOverlay()
     auto translate = Alignment::GetAlignPositionWithDirection(size, childSize, align, direction) + offset;
     overlayNode_->GetGeometryNode()->SetMarginFrameOffset(translate);
     overlayNode_->Layout();
-    overlayNode_->SetActive(true);
-    overlayNode_->MarkDirtyNode(PROPERTY_UPDATE_MEASURE);
 }
 
 void FrameNode::DoRemoveChildInRenderTree(uint32_t index, bool isAll)
