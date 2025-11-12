447e66495c44c36e74259f0e7101db992c0bc10b
g00500048 <ganzhiwei2@huawei.com>
2025-11-12 11:28:33 +0800

    修复arkts1.2单位不正确问题
    Signed-off-by: g00500048 <ganzhiwei2@huawei.com>

--- frameworks/core/interfaces/native/implementation/span_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/span_modifier.cpp
@@ -138,8 +138,8 @@ void SetLetterSpacingImpl(Ark_NativePointer node,
                           const Opt_Union_F64_String* value)
 {
     auto frameNode = reinterpret_cast<FrameNode *>(node);
-    CHECK_NULL_VOID(frameNode);
-    auto convValue = Converter::OptConvertPtr<Dimension>(value);
+    CHECK_NULL_VOID(frameNode && value);
+    auto convValue = Converter::OptConvertFromArkNumStrRes<Opt_Union_F64_String, Ark_Float64>(*value);
     Validator::ValidateNonPercent(convValue);
     SpanModelStatic::SetLetterSpacing(frameNode, convValue);
 }
@@ -156,7 +156,7 @@ void SetLineHeightImpl(Ark_NativePointer node,
 {
     auto frameNode = reinterpret_cast<FrameNode *>(node);
     CHECK_NULL_VOID(frameNode);
-    auto convValue = Converter::OptConvertPtr<Dimension>(value);
+    auto convValue = Converter::OptConvertFromArkNumStrRes<Opt_Length, Ark_Float64>(*value);
     Validator::ValidateNonNegative(convValue);
     SpanModelStatic::SetLineHeight(frameNode, convValue);
 }
