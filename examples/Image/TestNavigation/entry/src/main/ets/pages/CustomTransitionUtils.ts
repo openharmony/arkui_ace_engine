d950c17ac692dd10d4f3c1648dd3d707e4e426f9
silverbullet1412 <zhaoquanzhe1@h-partners.com>
2025-11-19 17:58:43 +0800

    增加OAT信息
    Signed-off-by: silverbullet1412 <zhaoquanzhe1@h-partners.com>

--- examples/Image/TestNavigation/entry/src/main/ets/pages/CustomTransitionUtils.ts
+++ examples/Image/TestNavigation/entry/src/main/ets/pages/CustomTransitionUtils.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 // src/main/pages/CustomTransitionUtils.ts 工具类，用来管理所有页面的自定义动画参数注册和获取等
 // 自定义接口，用来保存某个页面相关的转场动画回调和参数
 export interface AnimateCallback {
