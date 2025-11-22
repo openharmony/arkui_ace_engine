477840b337c7d33577ddfa82d8b5281c31072ced
sunziyuan <sunziyuan1@h-partners.com>
2025-11-22 17:40:35 +0800

    fix review
    Signed-off-by: sunziyuan <sunziyuan1@h-partners.com>
    Change-Id: I9b5ea962bc28e43754172e28f65dd9ed3a3e68f3

--- adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
+++ adapter/ohos/entrance/subwindow/subwindow_ohos.cpp
@@ -447,8 +447,12 @@ void SubwindowOhos::InitContainer()
     Ace::Platform::UIEnvCallback callback = nullptr;
     // set view
     Platform::AceContainer::SetView(aceView, density, width, height, window_, callback);
-    // ArkTS1.2 need set subwindow vsyncListener because subwindow does not enter into ArktsFrontend::RunPage
-    SetSubWindowVsyncListener(parentPipeline, container->GetPipelineContext());
+    // ArkTS static need set subwindow vsyncListener because subwindow does not enter into ArktsFrontend::RunPage
+    if (parentPipeline->GetFrontendType() == FrontendType::ARK_TS
+        || parentPipeline->GetFrontendType() == FrontendType::DYNAMIC_HYBRID_STATIC
+        || parentPipeline->GetFrontendType() == FrontendType::STATIC_HYBRID_DYNAMIC) {
+        SetSubWindowVsyncListener(parentPipeline, container->GetPipelineContext());
+    }
     Platform::AceViewOhos::SurfaceChanged(aceView, width, height, config.Orientation());
 
     auto uiContentImpl = reinterpret_cast<UIContentImpl*>(window_->GetUIContent());
