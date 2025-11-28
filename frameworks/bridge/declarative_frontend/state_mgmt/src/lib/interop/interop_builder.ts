087fbf7f607537fcbf17fceca8c69d55954db946
hwx1119949 <huyisuo@huawei.com>
2025-11-28 15:09:30 +0800

    通用告警处理
    Signed-off-by: hwx1119949 <huyisuo@huawei.com>

--- frameworks/bridge/declarative_frontend/state_mgmt/src/lib/interop/interop_builder.ts
+++ frameworks/bridge/declarative_frontend/state_mgmt/src/lib/interop/interop_builder.ts
@@ -79,7 +79,8 @@ function createDynamicUpdatableBuilder(
 
 function enableCompatibleObservedV2ForStatic(value: Object, 
   createFunc: Function, recordFunc: Function, updateFunc: Function): void {
-  value['__staticCompatibleFunc__'] = [createFunc, recordFunc, updateFunc]
+  const str = '__staticCompatibleFunc__';
+  value[str] = [createFunc, recordFunc, updateFunc];
 }
 
 function createCompatibleStateMetaForStaticObservedV2(): [()=>void, ()=>void] {
@@ -90,13 +91,15 @@ function createCompatibleStateMetaForStaticObservedV2(): [()=>void, ()=>void] {
 }
 
 function isDynamicBuilderProxy(value: Object): boolean {
-    return !!(value && value['__builder_param_get_target']);
+    const str = '__builder_param_get_target';
+    return !!(value && value[str]);
 }
 
 function getBuilderParamProxyEntries(value: Object): any[] {
     const res: any[] = [];
     if (isDynamicBuilderProxy(value)) {
-        const raw = value['__builder_param_get_target'];
+        const str = '__builder_param_get_target';
+        const raw = value[str];
         if (raw instanceof Map) {
             const entries = Array.from(raw.entries());
             entries.forEach((entry)=>{
