89656d0efe3aaa8bca75b8f52d5fb5787dbe72c0
pensLong <pengsilong@huawei-partners.com>
2025-11-29 20:04:21 +0800

    fix waterflow rowsGap columnGap undefined
    Signed-off-by: pensLong <pengsilong@huawei-partners.com>

--- frameworks/core/components_ng/pattern/waterflow/water_flow_model_static.cpp
+++ frameworks/core/components_ng/pattern/waterflow/water_flow_model_static.cpp
@@ -180,6 +180,9 @@ void WaterFlowModelStatic::SetColumnsGap(FrameNode* frameNode, const std::option
     } else {
         ACE_RESET_NODE_LAYOUT_PROPERTY_WITH_FLAG(
             WaterFlowLayoutProperty, ColumnsGap, PROPERTY_UPDATE_MEASURE, frameNode);
+        auto layout = frameNode->GetLayoutPropertyPtr<WaterFlowLayoutProperty>();
+        CHECK_NULL_VOID(layout);
+        layout->OnColumnsGapUpdate(Dimension(0.0));
     }
 }
 
@@ -189,9 +192,10 @@ void WaterFlowModelStatic::SetRowsGap(FrameNode* frameNode, const std::optional<
     if (value) {
         ACE_UPDATE_NODE_LAYOUT_PROPERTY(WaterFlowLayoutProperty, RowsGap, value.value(), frameNode);
     } else {
+        ACE_RESET_NODE_LAYOUT_PROPERTY_WITH_FLAG(
+            WaterFlowLayoutProperty, RowsGap, PROPERTY_UPDATE_MEASURE, frameNode);
         auto layout = frameNode->GetLayoutPropertyPtr<WaterFlowLayoutProperty>();
         CHECK_NULL_VOID(layout);
-        layout->ResetRowsGap();
         layout->OnRowsGapUpdate(Dimension(0.0));
     }
 }
