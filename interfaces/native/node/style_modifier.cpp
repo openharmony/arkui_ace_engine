d5670c4170e2c95d0b1d6e51f70b65a0aa91b5af
胡清云 <huqingyun1@huawei.com>
2025-12-03 11:26:50 +0800

    修复NODE_RESPONSE_REGION_LIST NODE_RESPONSE_REGION 传递参数大小超过20个，get方法会有crash的问题
    Signed-off-by: 胡清云 <huqingyun1@huawei.com>

--- interfaces/native/node/style_modifier.cpp
+++ interfaces/native/node/style_modifier.cpp
@@ -3082,6 +3082,7 @@ const ArkUI_AttributeItem* GetResponseRegion(ArkUI_NodeHandle node)
     ArkUI_Float32 values[32];
     auto valueSize =
         GetFullImpl()->getNodeModifiers()->getCommonModifier()->getResponseRegion(node->uiNodeHandle, &values);
+    valueSize = valueSize > MAX_ATTRIBUTE_ITEM_LEN ? MAX_ATTRIBUTE_ITEM_LEN : valueSize;
     for (int i = 0; i < valueSize; i++) {
         g_numberValues[i].f32 = values[i];
     }
@@ -3146,6 +3147,7 @@ const ArkUI_AttributeItem* GetResponseRegionList(ArkUI_NodeHandle node)
         g_attributeItem.size = NUM_5;
         return &g_attributeItem;
     }
+    regionSize = regionSize > NUM_4 ? NUM_4 : regionSize;
     int32_t toolType[regionSize];
     float valueArray[regionSize * NUM_4];
     GetFullImpl()->getNodeModifiers()->getCommonModifier()->getResponseRegionList(
