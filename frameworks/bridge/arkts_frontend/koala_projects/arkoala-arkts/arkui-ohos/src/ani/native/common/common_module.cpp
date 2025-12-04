8c3438d72cd51ba480b1e0255fbe691d1ceef962
zjut.wujinhui@qq.com <zjut.wujinhui@qq.com>
2025-12-04 15:01:17 +0800

    bug fix frameNode
    Signed-off-by: zjut.wujinhui@qq.com <zjut.wujinhui@qq.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ani/native/common/common_module.cpp
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ani/native/common/common_module.cpp
@@ -503,6 +503,7 @@ std::function<void(NG::LayoutConstraintF& layoutConstraint)> ConvertFunOnMeasure
         CHECK_NULL_VOID(vm);
         CHECK_NULL_VOID(callbackAni);
         ani_env* env = nullptr;
+        GetAniEnv(vm, &env);
         CHECK_NULL_VOID(env);
         auto layoutConstraintAni = CreateLayoutConstraintF(env, constraint);
         if (!layoutConstraintAni) {
@@ -592,6 +593,7 @@ std::function<void(NG::DrawingContext& drawingContext)> ConvertFnObjDrawCallback
         CHECK_NULL_VOID(vm);
         CHECK_NULL_VOID(callbackAni);
         ani_env* env = nullptr;
+        GetAniEnv(vm, &env);
         CHECK_NULL_VOID(env);
         auto drawingContext = CreateDrawingContext(env, context);
         if (!drawingContext) {
