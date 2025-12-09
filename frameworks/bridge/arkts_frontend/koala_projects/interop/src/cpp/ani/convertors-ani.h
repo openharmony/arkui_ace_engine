66196de7f324f5a8f2e6ba820654ae2daa495194
kolegovilya <kolegov.ilya@huawei.com>
2025-12-09 17:33:55 +0300

    Fix code checks
    Signed-off-by: kolegovilya <kolegov.ilya@huawei.com>

--- frameworks/bridge/arkts_frontend/koala_projects/interop/src/cpp/ani/convertors-ani.h
+++ frameworks/bridge/arkts_frontend/koala_projects/interop/src/cpp/ani/convertors-ani.h
@@ -36,15 +36,14 @@ do {                                                                            
 }                                                                                                \
 while (0)
 
-#define CHECK_RETHROW_ERROR(env, result)    \
-    do {                                    \
-        ani_boolean isError = false;        \
-        env->ExistUnhandledError(&isError); \
-        if (isError) {                      \
-            return result;                  \
-        }                                   \
-    } while (0)
-
+#define CHECK_RETHROW_ERROR(env, result) \
+do {                                     \
+  ani_boolean isError = false;           \
+  env->ExistUnhandledError(&isError);    \
+  if (isError) {                         \
+      return result;                     \
+  }                                      \
+} while (0)
 
 template<class T>
 struct InteropTypeConverter {
@@ -192,15 +191,15 @@ struct InteropTypeConverter<KInteropReturnBuffer> {
     using InteropType = ani_fixedarray_byte;
     static inline KInteropReturnBuffer convertFrom(ani_env* env, InteropType value) = delete;
     static inline InteropType convertTo(ani_env* env, KInteropReturnBuffer value) {
-        CHECK_RETHROW_ERROR(env, nullptr);
-        ani_fixedarray_byte result = nullptr;
-        ani_boolean errorExist;
-        env->ExistUnhandledError(&errorExist);
-        if (!errorExist) {
-            CHECK_ANI_FATAL(env->FixedArray_New_Byte(value.length, &result));
-            CHECK_ANI_FATAL(
-                env->FixedArray_SetRegion_Byte(result, 0, value.length, reinterpret_cast<const ani_byte*>(value.data)));
-        }
+      CHECK_RETHROW_ERROR(env, nullptr);
+      ani_fixedarray_byte result = nullptr;
+      ani_boolean errorExist;
+      env->ExistUnhandledError(&errorExist);
+      if (!errorExist) {
+          CHECK_ANI_FATAL(env->FixedArray_New_Byte(value.length, &result));
+          CHECK_ANI_FATAL(
+              env->FixedArray_SetRegion_Byte(result, 0, value.length, reinterpret_cast<const ani_byte*>(value.data)));
+      }
       value.dispose(value.data, value.length);
       return result;
     };
