914ae45e39c66c388541f18100e422ad68d309ae
yixiqin <yixiqin@huawei.com>
2025-12-11 11:50:02 +0800

    Repeat pre_insert
    Signed-off-by: yixiqin <yixiqin@huawei.com>

--- frameworks/core/components_ng/syntax/arkoala_lazy_node.cpp
+++ frameworks/core/components_ng/syntax/arkoala_lazy_node.cpp
@@ -243,10 +243,14 @@ void ArkoalaLazyNode::OnDataChange(int32_t changeIndex, int32_t count, Notificat
         RequestSyncTree();
     }
 
-    auto parent = GetParent();
+    auto parent = GetParentFrameNode();
     int64_t accessibilityId = GetAccessibilityId();
     if (parent) {
-        parent->NotifyChange(changeIndex, count, accessibilityId, type);
+        if (isRepeat_ && parent->GetHostTag() == V2::LIST_ETS_TAG) {
+            parent->NotifyChange(changeIndex, count, accessibilityId, NotificationType::START_AND_END_CHANGE_POSITION);
+        } else {
+            parent->NotifyChange(changeIndex, count, accessibilityId, type);
+        }
         MarkNeedSyncRenderTree(true);
         MarkNeedFrameFlushDirty(PROPERTY_UPDATE_MEASURE_SELF_AND_PARENT);
     }
