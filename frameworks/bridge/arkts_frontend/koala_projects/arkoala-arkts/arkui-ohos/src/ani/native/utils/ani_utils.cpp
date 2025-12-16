2218b8143f77e9f87e767e9cac9af63492221cd4
yuanlin94 <linyuan25@h-partners.com>
2025-12-16 21:05:28 +0800

    截图支持广色域
    Signed-off-by: yuanlin94 <linyuan25@h-partners.com>

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ani/native/utils/ani_utils.cpp
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/ani/native/utils/ani_utils.cpp
@@ -236,7 +236,8 @@ std::optional<ani_string> AniUtils::StdStringToANIString(ani_env *env, std::stri
     return result_string;
 }
 
-ani_object WrapBusinessError(ani_env* env, const char *msg)
+// Do not use this function directly to create a BusinessError object.
+static ani_object WrapBusinessError(ani_env* env, const char *msg)
 {
     ani_class cls {};
     ani_method method {};
