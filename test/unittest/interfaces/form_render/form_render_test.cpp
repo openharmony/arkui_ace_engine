f67ee6ef3806fcc00961d3c733e564704024d3e8
luyangyi <luyangyi2@huawei.com>
2025-12-17 21:32:17 +0800

    update: 更新文件 form_render_test.cpp
    Signed-off-by: luyangyi <luyangyi2@huawei.com>

--- test/unittest/interfaces/form_render/form_render_test.cpp
+++ test/unittest/interfaces/form_render/form_render_test.cpp
@@ -1051,7 +1051,8 @@ HWTEST_F(FormRenderTest, FormRenderTest_SetUiContentParams_001, TestSize.Level0)
     formRenderer->SetUiContentParams(renderWant);
     formRenderer->uiContent_ = UIContent::Create(nullptr, nullptr);
     EXPECT_TRUE(formRenderer->uiContent_);
-    renderWant.SetParam(OHOS::AppExecFwk::Constants::PARAM_FORM_TRANSPARENCY_KEY, "#FFFFFFFF");
+    std::string transparentColor = "#FFFFFFFF";
+    renderWant.SetParam(OHOS::AppExecFwk::Constants::PARAM_FORM_TRANSPARENCY_KEY, transparentColor);
     renderWant.SetParam(OHOS::AppExecFwk::Constants::PARAM_FORM_ENABLE_BLUR_BACKGROUND_KEY, true);
     formRenderer->SetUiContentParams(renderWant);
 }
