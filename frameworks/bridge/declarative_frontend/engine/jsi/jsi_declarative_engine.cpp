5de6e89aaa79acb6bfe63b00c9dcdc710c187b9a
zhangsaiyang1 <zhangsaiyang1@h-partners.com>
2025-12-22 14:23:36 +0800

    调用SetAutoCloseFd接口,防止fd未释放
    Signed-off-by: zhangsaiyang1 <zhangsaiyang1@h-partners.com>

--- frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
+++ frameworks/bridge/declarative_frontend/engine/jsi/jsi_declarative_engine.cpp
@@ -1814,6 +1814,7 @@ bool JsiDeclarativeEngine::ExecuteCardAbc(const std::string& fileName, int64_t c
                 TAG_LOGE(AceLogTag::ACE_FORM, "null data");
                 return false;
             }
+            extractor->SetAutoCloseFd(true);
             data->SetAutoReleaseMem(true);
             if (arkRuntime->IsStaticOrInvalidFile(data->GetDataPtr(), data->GetDataLen())) {
                 return false;
