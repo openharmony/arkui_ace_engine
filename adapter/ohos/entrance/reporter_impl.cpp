b6573f14be7e41ebb188841536bfb98190276118
yuanlin94 <linyuan25@h-partners.com>
2025-12-28 13:05:23 +0800

    1228 delete inspector message log
    Signed-off-by: yuanlin94 <linyuan25@h-partners.com>

--- adapter/ohos/entrance/reporter_impl.cpp
+++ adapter/ohos/entrance/reporter_impl.cpp
@@ -76,9 +76,6 @@ void ReporterImpl::HandleInspectorReporting(const JsonReport& report) const
     if (value->IsNull()) {
         return;
     }
-    if (SystemProperties::GetDebugEnabled()) {
-        TAG_LOGD(AceLogTag::ACE_UIEVENT, "Inspector JsonString %{public}s", value->ToString().c_str());
-    }
     LayoutInspector::SendMessage(value->ToString());
 }
 
@@ -171,7 +168,6 @@ void ReporterImpl::HandleInputEventInspectorReporting(const KeyEvent& event) con
     }
     // Block invalid keyCode 65535 (KEYCODE_INVALID) in certain scenarios to avoid subsequent exceptions
     if (static_cast<int32_t>(event.code) == KEYCODE_INVALID) {
-        TAG_LOGE(AceLogTag::ACE_UIEVENT, "Report insepector message error, keyCode is %{public}d", KEYCODE_INVALID);
         return;
     }
     NG::KeyJsonReport keyReport;
