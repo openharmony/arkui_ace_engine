8b8e62c45f8133d54c6288ec27548121796ab7a0
chen-yu5 <417887319@qq.com>
2025-12-29 19:06:01 +0800

    update: 更新文件 ui_content_impl.cpp commit
    Signed-off-by: chen-yu5 <417887319@qq.com>

--- adapter/ohos/entrance/ui_content_impl.cpp
+++ adapter/ohos/entrance/ui_content_impl.cpp
@@ -3656,7 +3656,7 @@ void KeyboardAvoid(OHOS::Rosen::WindowSizeChangeReason reason, int32_t instanceI
         double textFieldHeight = info->textFieldHeight_;
         textFieldPositionY -= curWindow.Top();
         pipelineContext->OnVirtualKeyboardAreaChange(keyboardRect, textFieldPositionY, textFieldHeight);
-    }
+    };
 
     if (taskExecutor->WillRunOnCurrentThread(TaskExecutor::TaskType::UI)) {
         task();
