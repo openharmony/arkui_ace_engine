6cf58071d9a532fd4645cd82236c85cc468e73e8
seaside_wu1 <wuhaibin5@huawei.com>
2026-01-04 12:06:43 +0800

    remove getLocalStateMgmr from StateMgmtTool
    Signed-off-by: liwenzhen3 <liwenzhen3@huawei.com>
    
    
    revert merge request: set the stateMgr same with CustComponent when lazy build
    
    70170c6db9fb0b9417c19eaa402b01957ddc5902
    
    This revert merge request !78474

--- frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/stateManagement/base/observeSingleton.ts
+++ frameworks/bridge/arkts_frontend/koala_projects/arkoala-arkts/arkui-ohos/src/stateManagement/base/observeSingleton.ts
@@ -21,7 +21,6 @@ import { NullableObject } from './types';
 import { MonitorFunctionDecorator, MonitorValueInternal } from '../decoratorImpl/decoratorMonitor';
 import { ComputedDecoratedVariable, IComputedDecoratorRef } from '../decoratorImpl/decoratorComputed';
 import { PersistenceV2Impl } from '../storage/persistenceV2';
-import { GlobalStateManager } from '@koalaui/runtime';
 
 type TaskType<T> = () => T;
 
@@ -65,7 +64,7 @@ export class ObserveSingleton implements IObserve {
 
     get renderingId(): RenderIdType | undefined {
         const id =
-            GlobalStateManager.instance.currentScope?.id ?? ObserveSingleton.InvalidRenderId;
+            StateMgmtTool.getGlobalStateManager().currentScope?.id ?? ObserveSingleton.InvalidRenderId;
         return id;
     }
     set renderingId(value: RenderIdType | undefined) {
