045af2db28698c27acf01c2a35e02cfa9ce41b2c
wangzilin <wangzilin17@huawei.com>
2026-01-09 17:41:50 +0800

    fix tdd
    Signed-off-by: wangzilin <wangzilin17@huawei.com>

--- test/unittest/core/pattern/window_scene/window_scene_test.cpp
+++ test/unittest/core/pattern/window_scene/window_scene_test.cpp
@@ -49,7 +49,6 @@ const std::string ABILITY_NAME = "MainAbility";
 const std::string BUNDLE_NAME = "com.example.helloworld";
 const std::string MODULE_NAME = "entry";
 const uint32_t WAIT_SYNC_IN_NS = 200000;
-constexpr uint32_t STARTING_WINDOW_TIMEOUT_NS = 10000000000;
 } // namespace
 
 class WindowSceneTest : public testing::Test {
@@ -688,40 +687,6 @@ HWTEST_F(WindowSceneTest, HideStartingWindowDefault01, TestSize.Level1)
      * @tc.steps: step3. Test and check
      */
     ASSERT_EQ(windowScene->session_->GetHidingStartingWindow(), false);
-}
-
-/**
- * @tc.name: HideStartingWindow
- * @tc.desc: HideStartingWindow
- * @tc.type: FUNC
- */
-HWTEST_F(WindowSceneTest, HideStartingWindowDefault02, TestSize.Level1)
-{
-    /**
-     * @tc.steps: step1. Create windowScene.
-     */
-    Rosen::SessionInfo sessionInfo = {
-        .abilityName_ = ABILITY_NAME,
-        .bundleName_ = BUNDLE_NAME,
-        .moduleName_ = MODULE_NAME,
-        .startWindowType_ = Rosen::StartWindowType::DEFAULT,
-    };
-    auto session = ssm_->RequestSceneSession(sessionInfo);
-    ASSERT_NE(session, nullptr);
-    auto windowScene = AceType::MakeRefPtr<WindowScene>(session);
-    ASSERT_NE(windowScene, nullptr);
-
-    /**
-     * @tc.steps: step2. Set default.
-     */
-    windowScene->HideStartingWindow();
-
-    /**
-     * @tc.steps: step3. Test and check
-     */
-    ASSERT_EQ(windowScene->session_->GetHidingStartingWindow(), true);
-    windowScene->startingWindow_ = nullptr;
-    usleep(STARTING_WINDOW_TIMEOUT_NS);
 }
 
 /**
