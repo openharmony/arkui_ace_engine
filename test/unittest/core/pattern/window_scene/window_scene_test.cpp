646b9cfc193e5d67cfca14763db349a582b3e3ec
wangzilin <wangzilin17@huawei.com>
2026-01-09 16:59:38 +0800

    hide start window tdd
    Signed-off-by: wangzilin <wangzilin17@huawei.com>

--- test/unittest/core/pattern/window_scene/window_scene_test.cpp
+++ test/unittest/core/pattern/window_scene/window_scene_test.cpp
@@ -658,11 +658,11 @@ HWTEST_F(WindowSceneTest, HideStartingWindowInvisible, TestSize.Level1)
 }
 
 /**
- * @tc.name: HideStartingWindow
- * @tc.desc: HideStartingWindow
+ * @tc.name: HideStartingWindow01
+ * @tc.desc: HideStartingWindow01
  * @tc.type: FUNC
  */
-HWTEST_F(WindowSceneTest, HideStartingWindowDefalut, TestSize.Level1)
+HWTEST_F(WindowSceneTest, HideStartingWindowDefault01, TestSize.Level1)
 {
     /**
      * @tc.steps: step1. Create windowScene.
@@ -687,6 +687,38 @@ HWTEST_F(WindowSceneTest, HideStartingWindowDefalut, TestSize.Level1)
      * @tc.steps: step3. Test and check
      */
     ASSERT_EQ(windowScene->session_->GetHidingStartingWindow(), false);
+}
+
+/**
+ * @tc.name: HideStartingWindow
+ * @tc.desc: HideStartingWindow
+ * @tc.type: FUNC
+ */
+HWTEST_F(WindowSceneTest, HideStartingWindowDefault02, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. Create windowScene.
+     */
+    Rosen::SessionInfo sessionInfo = {
+        .abilityName_ = ABILITY_NAME,
+        .bundleName_ = BUNDLE_NAME,
+        .moduleName_ = MODULE_NAME,
+        .startWindowType_ = Rosen::StartWindowType::DEFAULT,
+    };
+    auto session = ssm_->RequestSceneSession(sessionInfo);
+    ASSERT_NE(session, nullptr);
+    auto windowScene = AceType::MakeRefPtr<WindowScene>(session);
+    ASSERT_NE(windowScene, nullptr);
+
+    /**
+     * @tc.steps: step2. Set default.
+     */
+    windowScene->HideStartingWindow();
+
+    /**
+     * @tc.steps: step3. Test and check
+     */
+    ASSERT_EQ(windowScene->session_->GetHidingStartingWindow(), true);
 }
 
 /**
