25120fdeee900950db36bf264ee4ced783a66365
zzc19990127 <zhangzecong@huawei.com>
2026-01-20 17:11:01 +0800

    update modifier
    Signed-off-by: zzc19990127 <zhangzecong@huawei.com>

--- frameworks/core/interfaces/native/implementation/rich_editor_modifier.cpp
+++ frameworks/core/interfaces/native/implementation/rich_editor_modifier.cpp
@@ -890,10 +890,10 @@ void SetCustomKeyboardImpl(Ark_NativePointer node,
     if (value->value.selector == SELECTOR_CUSTOM_BUILDER) {
         const CustomNodeBuilder& builder = value->value.value0;
         CallbackHelper(builder).BuildAsync([frameNode, supportAvoidance](const RefPtr<UINode>& uiNode) {
-        auto builder = [uiNode]() {
-            NG::ViewStackProcessor::GetInstance()->Push(uiNode);
-        };
-        RichEditorModelStatic::SetCustomKeyboard(frameNode, std::move(builder), supportAvoidance);
+            auto customNodeBuilder = [uiNode]() {
+                NG::ViewStackProcessor::GetInstance()->Push(uiNode);
+            };
+            RichEditorModelStatic::SetCustomKeyboard(frameNode, std::move(customNodeBuilder), supportAvoidance);
         }, node);
     } else if (value->value.selector == SELECTOR_COMPONENT_CONTENT) {
         const Ark_ComponentContentBase& arkContent = value->value.value1;
