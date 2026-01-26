6c26b1688bccdfae0b7d06d8b81d1047d025abb0
wangyang2022 <wangyang412@huawei.com>
2026-01-26 07:20:58 +0000

    补充TDD Signed-off-by: wangyang2022 <wangyang412@huawei.com> Change-Id: I7298cfbee71840d1d60f51ec5e4f3ea3feb8c6f6

--- test/unittest/core/common/statistic_event/statistic_event_reporter_test.cpp
+++ test/unittest/core/common/statistic_event/statistic_event_reporter_test.cpp
@@ -114,4 +114,240 @@ HWTEST_F(StatisticEventReporterTest, ForceReportStatisticEvents001, TestSize.Lev
     reporter->ForceReportStatisticEvents();
     EXPECT_EQ(reporter->totalEventCount_, 0);
 }
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test ConvertToEvent with all event types
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, ConvertToEvent002, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    // Test CALL_SET_CACHE_RANGE
+    auto event1 = reporter->ConvertToEvent(StatisticEventType::CALL_SET_CACHE_RANGE);
+    EXPECT_EQ(event1.eventName, "List");
+    EXPECT_EQ(event1.subEventName, "CALL_SET_CACHE_RANGE");
+
+    // Test SEARCH_ONDIDINSERT
+    auto event2 = reporter->ConvertToEvent(StatisticEventType::SEARCH_ONDIDINSERT);
+    EXPECT_EQ(event2.eventName, "SEARCH");
+    EXPECT_EQ(event2.subEventName, "ONDIDINSERT");
+
+    // Test SEARCH_ONWILLDELETE
+    auto event3 = reporter->ConvertToEvent(StatisticEventType::SEARCH_ONWILLDELETE);
+    EXPECT_EQ(event3.eventName, "SEARCH");
+    EXPECT_EQ(event3.subEventName, "ONWILLDELETE");
+
+    // Test SEARCH_ONDIDDELETE
+    auto event4 = reporter->ConvertToEvent(StatisticEventType::SEARCH_ONDIDDELETE);
+    EXPECT_EQ(event4.eventName, "SEARCH");
+    EXPECT_EQ(event4.subEventName, "ONDIDDELETE");
+
+    // Test CLICK_AI_MENU_PHONE_NUMBER
+    auto event5 = reporter->ConvertToEvent(StatisticEventType::CLICK_AI_MENU_PHONE_NUMBER);
+    EXPECT_EQ(event5.eventName, "CLICK_AI_MENU");
+    EXPECT_EQ(event5.subEventName, "PHONE_NUMBER");
+
+    // Test CLICK_AI_MENU_URL
+    auto event6 = reporter->ConvertToEvent(StatisticEventType::CLICK_AI_MENU_URL);
+    EXPECT_EQ(event6.eventName, "CLICK_AI_MENU");
+    EXPECT_EQ(event6.subEventName, "URL");
+
+    // Test CLICK_AI_MENU_EMAIL
+    auto event7 = reporter->ConvertToEvent(StatisticEventType::CLICK_AI_MENU_EMAIL);
+    EXPECT_EQ(event7.eventName, "CLICK_AI_MENU");
+    EXPECT_EQ(event7.subEventName, "EMAIL");
+
+    // Test CLICK_AI_MENU_ADDRESS
+    auto event8 = reporter->ConvertToEvent(StatisticEventType::CLICK_AI_MENU_ADDRESS);
+    EXPECT_EQ(event8.eventName, "CLICK_AI_MENU");
+    EXPECT_EQ(event8.subEventName, "ADDRESS");
+
+    // Test CLICK_AI_MENU_DATE_TIME
+    auto event9 = reporter->ConvertToEvent(StatisticEventType::CLICK_AI_MENU_DATE_TIME);
+    EXPECT_EQ(event9.eventName, "CLICK_AI_MENU");
+    EXPECT_EQ(event9.subEventName, "DATE_TIME");
+
+    // Test CLICK_AI_MENU_ASK_CELIA
+    auto event10 = reporter->ConvertToEvent(StatisticEventType::CLICK_AI_MENU_ASK_CELIA);
+    EXPECT_EQ(event10.eventName, "CLICK_AI_MENU");
+    EXPECT_EQ(event10.subEventName, "ASK_CELIA");
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test ConvertToEvent with invalid event type
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, ConvertToEvent003, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    // Test with an invalid event type (larger than the last valid enum value)
+    auto event = reporter->ConvertToEvent(static_cast<StatisticEventType>(999));
+    EXPECT_EQ(event.eventName, "");
+    EXPECT_EQ(event.subEventName, "");
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test SendEvent with existing event
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, SendEvent002, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    // Send event first time
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 1);
+    EXPECT_EQ(reporter->totalEventCount_, 1);
+
+    // Send same event again - should increment eventCount
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 1);
+    EXPECT_EQ(reporter->statisitcEventMap_[StatisticEventType::FA_APP_START].eventCount, 2);
+    EXPECT_EQ(reporter->totalEventCount_, 2);
+
+    // Send same event third time
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 1);
+    EXPECT_EQ(reporter->statisitcEventMap_[StatisticEventType::FA_APP_START].eventCount, 3);
+    EXPECT_EQ(reporter->totalEventCount_, 3);
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test SendEvent with multiple different events
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, SendEvent003, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+    reporter->SendEvent(StatisticEventType::CALL_SET_CACHE_RANGE);
+    reporter->SendEvent(StatisticEventType::CLICK_AI_MENU_PHONE_NUMBER);
+
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 3);
+    EXPECT_EQ(reporter->totalEventCount_, 3);
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test TryReportStatisticEvents with null pipeline
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, TryReportStatisticEvents002, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    reporter->totalEventCount_ = MAX_PENDING_EVENT_COUNT;
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+
+    // Should not crash with null pipeline
+    reporter->TryReportStatisticEvents(nullptr);
+    EXPECT_EQ(reporter->totalEventCount_, MAX_PENDING_EVENT_COUNT + 1);
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test TryReportStatisticEvents with threshold not reached
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, TryReportStatisticEvents003, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    // Set totalEventCount_ below MAX_PENDING_EVENT_COUNT
+    reporter->totalEventCount_ = MAX_PENDING_EVENT_COUNT - 1;
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+
+    reporter->TryReportStatisticEvents(nullptr);
+    // Event count should remain unchanged
+    EXPECT_EQ(reporter->totalEventCount_, MAX_PENDING_EVENT_COUNT);
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test ForceReportStatisticEvents with empty map
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, ForceReportStatisticEvents002, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    // statisitcEventMap_ should be empty initially
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 0);
+
+    // ForceReportStatisticEvents should handle empty map gracefully
+    reporter->ForceReportStatisticEvents();
+    EXPECT_EQ(reporter->totalEventCount_, 0);
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 0);
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test ForceReportStatisticEvents with multiple events
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, ForceReportStatisticEvents003, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+    reporter->SendEvent(StatisticEventType::CALL_SET_CACHE_RANGE);
+    reporter->SendEvent(StatisticEventType::SEARCH_ONDIDINSERT);
+
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 3);
+    EXPECT_EQ(reporter->totalEventCount_, 3);
+
+    reporter->ForceReportStatisticEvents();
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 0);
+    EXPECT_EQ(reporter->totalEventCount_, 0);
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test reporter initial state
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, InitialState001, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    // Check initial state
+    EXPECT_EQ(reporter->totalEventCount_, 0);
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 0);
+}
+
+/**
+ * @tc.name: StatisticEventReporterTest
+ * @tc.desc: Test multiple calls to ForceReportStatisticEvents
+ * @tc.type: FUNC
+ */
+HWTEST_F(StatisticEventReporterTest, ForceReportStatisticEvents004, TestSize.Level1)
+{
+    auto reporter = std::make_shared<StatisticEventReporter>();
+    ASSERT_TRUE(reporter != nullptr);
+
+    reporter->SendEvent(StatisticEventType::FA_APP_START);
+    reporter->ForceReportStatisticEvents();
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 0);
+
+    // Call ForceReportStatisticEvents again on empty map
+    reporter->ForceReportStatisticEvents();
+    EXPECT_EQ(reporter->statisitcEventMap_.size(), 0);
+    EXPECT_EQ(reporter->totalEventCount_, 0);
+}
 } // namespace OHOS::Ace
