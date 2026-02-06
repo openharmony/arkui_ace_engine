0a137a63d3a95fce2ffd22d2e691afe0e2fa72c5
sunziyuan <sunziyuan1@h-partners.com>
2026-02-06 17:27:45 +0800

    fixCheckboxTestLevel18
    Signed-off-by: sunziyuan <sunziyuan1@h-partners.com>
    Change-Id: Ie709f5d09ca3ddfdc354604cbcc1ead3a8de16bd

--- frameworks/core/components_ng/pattern/checkboxgroup/bridge/checkboxgroup_dynamic_modifier.cpp
+++ frameworks/core/components_ng/pattern/checkboxgroup/bridge/checkboxgroup_dynamic_modifier.cpp
@@ -126,12 +126,14 @@ void ResetCheckboxGroupUnSelectedColor(ArkUINodeHandle node)
 {
     auto* frameNode = GetFrameNode(node);
     CHECK_NULL_VOID(frameNode);
-    auto context = frameNode->GetContext();
-    CHECK_NULL_VOID(context);
-    auto themeManager = context->GetThemeManager();
-    CHECK_NULL_VOID(themeManager);
-    auto checkBoxTheme = themeManager->GetTheme<CheckboxTheme>();
     if (node) {
+        auto context = frameNode->GetContext();
+        CHECK_NULL_VOID(context);
+        auto themeManager = context->GetThemeManager();
+        CHECK_NULL_VOID(themeManager);
+        auto scopeId = frameNode->GetThemeScopeId();
+        auto checkBoxTheme = SystemProperties::ConfigChangePerform() ? themeManager->GetTheme<CheckboxTheme>(scopeId)
+            : themeManager->GetTheme<CheckboxTheme>();
         CheckBoxGroupModelNG::SetUnSelectedColor(frameNode, checkBoxTheme->GetInactiveColor());
     } else {
         CheckBoxGroupModelNG::ResetUnSelectedColor(frameNode);
