00035960a69153d5885729fad39b6cefcfec2d5f
haiwenxiang <yinweikai@h-partners.com>
2026-02-06 10:12:06 +0800

    add tdd
    Signed-off-by: haiwenxiang <yinweikai@h-partners.com>

---
+++ test/unittest/core/accessibility/js_accessibility_manager_test_two.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ #include "gmock/gmock.h"
+ #include "gtest/gtest.h"
+ 
+ #define private public
+ #define protected public
+ 
+#include "adapter/ohos/osal/js_accessibility_manager.h"
+
+using namespace OHOS::Accessibility;
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Ace {
+} // namespace OHOS::Ace
+
+namespace OHOS::Ace::NG {
+namespace {
+class MockFrameNode : public FrameNode {
+    DECLARE_ACE_TYPE(MockFrameNode, FrameNode);
+
+public:
+    MockFrameNode(const std::string& tag, int32_t nodeId, const RefPtr<Pattern>& pattern)
+        : FrameNode(tag, nodeId, pattern)
+    {}
+
+    ~MockFrameNode() override = default;
+};
+} // namespace
+} // namespace OHOS::Ace::NG
