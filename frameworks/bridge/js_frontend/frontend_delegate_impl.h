54d6d95039b4d4ebc07f35c92a6766be65811ce5
liuyongkai2 <liuyongkai2@huawei-partners.com>
2026-02-10 16:51:22 +0800

    修复旋转表冠事件并发问题
    Signed-off-by: liuyongkai2 <liuyongkai2@huawei-partners.com>

--- frameworks/bridge/js_frontend/frontend_delegate_impl.h
+++ frameworks/bridge/js_frontend/frontend_delegate_impl.h
@@ -434,6 +434,8 @@ private:
 
     void GetAssetFromI18n(const std::string& fileFullPath, std::unique_ptr<JsonValue>& data);
 
+    bool OnMonitorForCrownEvents(const std::string& callbackId, const std::string& args);
+
     std::atomic<uint64_t> pageIdPool_ = 0;
     int32_t callbackCnt_ = 0;
     int32_t pageId_ = -1;
