8f86abc77e3a97bfc0bdd39029d7d4e43b28cc66
chendongqi <chendongqi3@h-partners.com>
2026-03-04 16:42:58 +0800

    维吾尔语绘制角标问题修复
    Signed-off-by: chendongqi <chendongqi3@h-partners.com>

--- frameworks/core/components_ng/base/frame_node.cpp
+++ frameworks/core/components_ng/base/frame_node.cpp
@@ -6034,6 +6034,11 @@ void FrameNode::SyncGeometryNode(bool needSyncRsNode, const DirtySwapConfig& con
 
     UpdateBackground();
 
+    // update corner mark position on size change.
+    if (config.frameSizeChange && cornerMarkNode_) {
+        CornerMark::UpdateCornerMarkNodePosition(Claim(this));
+    }
+
     // update focus state
     UpdateFocusState();
 
