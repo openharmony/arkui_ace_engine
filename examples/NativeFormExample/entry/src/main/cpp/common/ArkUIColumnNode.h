82ba209b5cca35af76be058d91b490b3622b1fc9
liyi0309 <liyi58@huawei.com>
2026-03-13 17:39:48 +0800

    NDK表单示例代码补充2 Signed-off-by: liyi0309 <liyi58@huawei.com>

--- examples/NativeFormExample/entry/src/main/cpp/common/ArkUIColumnNode.h
+++ examples/NativeFormExample/entry/src/main/cpp/common/ArkUIColumnNode.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2026 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License")
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -12,9 +12,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef MYAPPLICATION_ARKUICOLUMNNODE_H
+#define MYAPPLICATION_ARKUICOLUMNNODE_H
 
-import localUnitTest from './LocalUnit.test';
+#include "ArkUINode.h"
 
-export default function testsuite() {
-  localUnitTest();
-}
+#include <string>
+
+namespace NativeModule {
+class ArkUIColumnNode : public ArkUINode {
+public:
+    ArkUIColumnNode()
+        : ArkUINode((NativeModuleInstance::GetInstance()->GetNativeNodeAPI())->createNode(ARKUI_NODE_COLUMN)) {}
+};
+} // namespace NativeModule
+#endif //MYAPPLICATION_ARKUICOLUMNNODE_H
