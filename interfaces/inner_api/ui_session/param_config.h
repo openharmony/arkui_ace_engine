209d87aa33674cf6ad5f04e0b4677cac3fdfd2e8
Sunjiamei <sunjiamei001@chinasoftinc.com>
2026-03-17 15:51:21 +0800

    video控件支持操控响应与事件上报
    Signed-off-by: Sunjiamei <sunjiamei001@chinasoftinc.com>
    Change-Id: I02619e5f3d2d6b74c20345530d2cf312474a0152

--- interfaces/inner_api/ui_session/param_config.h
+++ interfaces/inner_api/ui_session/param_config.h
@@ -70,6 +70,7 @@ enum ComponentEventType : uint32_t {
     COMPONENT_EVENT_MENU = 1 << 12,             // menu  event
     COMPONENT_EVENT_SHEET_PRESENTATION = 1 << 13, // sheet presentation event
     COMPONENT_EVENT_MODAL_PRESENTATION = 1 << 14, // modal presentation event
+    COMPONENT_EVENT_VIDEO = 1 << 15,           // video event
 
     COMPONENT_EVENT_ALL = static_cast<uint32_t>(-1),  // all events
 };
