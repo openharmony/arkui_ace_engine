02efe87c96667e14f4c56dcbfe6987c30a323352
chenjiafeng10 <chenjiafeng10@h-partners.com>
2026-03-25 11:59:01 +0800

    Revert "!82562 merge date_0323_base_master_imageDfx into master" Signed-off-by: chenjiafeng <chenjiafeng10@h-partners.com> This reverts commit 7614121d01a30cb6b39659083ff7bf486e79b478, reversing changes made to bfa0909d1c00b37b692e582e620edcd25a548735.

--- test/unittest/core/pattern/image/image_base.h
+++ test/unittest/core/pattern/image/image_base.h
@@ -55,7 +55,6 @@
 #include "core/components_ng/pattern/image/image_model_static.h"
 #include "core/components_ng/pattern/image/image_paint_method.h"
 #include "core/components_ng/pattern/image/image_pattern.h"
-#include "core/components_ng/pattern/image/image_layout_algorithm.h"
 #include "core/components_v2/inspector/inspector_constants.h"
 #include "core/event/mouse_event.h"
 #include "core/image/image_source_info.h"
