b46f9aed15445309c48e6fa74b64ab56d1bda4c1
zenix_zxy <zhengxingyou1@huawei.com>
2026-04-19 20:48:14 +0800

    fix: overlay empty content negative width + direction-aware offset
    Source fix in overlay_modifier.h:
    - GetParagraph: filter empty content to avoid skia FLT_MIN-FLT_MAX
    - GetParagraphSize: defensive validation for negative/abnormal width
    - Add GetOverlayOffsetWithDirection public method for alignment+direction
    
    Caller simplification in rosen_render_context.cpp:
    - Deduplicate if/else modifier creation
    - Use GetOverlayOffsetWithDirection instead of GetOverlayOffset
    
    Add TDD tests for offset calculation and empty content scenarios.
    
    Closes: #75104
    Co-Authored-By: Agent
    Signed-off-by: zenix_zxy <zhengxingyou1@huawei.com>

---
+++ test/unittest/core/render/overlay_modifier_test_ng.cpp
@@ -0,0 +1,268 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include "base/geometry/ng/offset_t.h"
+#include "base/geometry/ng/size_t.h"
+#define private public
+#define protected public
+#include "test/mock/frameworks/core/pipeline/mock_pipeline_context.h"
+#undef private
+#undef protected
+
+#include "core/components/common/properties/alignment.h"
+#include "core/components_ng/property/overlay_property.h"
+#include "core/components_ng/render/adapter/overlay_modifier.h"
+#include "core/common/ace_application_info.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Ace {
+
+namespace {
+constexpr float TEST_WIDTH = 400.0f;
+constexpr float TEST_HEIGHT = 300.0f;
+constexpr float TEST_CHILD_WIDTH = 200.0f;
+constexpr float TEST_CHILD_HEIGHT = 100.0f;
+} // namespace
+
+class OverlayModifierTestNg : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+};
+
+void OverlayModifierTestNg::SetUpTestCase()
+{
+    NG::MockPipelineContext::SetUp();
+}
+
+void OverlayModifierTestNg::TearDownTestCase()
+{
+    NG::MockPipelineContext::TearDownTestCase();
+}
+
+/**
+ * @tc.name: GetOverlayOffsetWithDirection001
+ * @tc.desc: Test GetOverlayOffsetWithDirection with TOP_LEFT alignment and PX offset
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetOverlayOffsetWithDirection001, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "test";
+    overlayOptions.x = Dimension(10.0f, DimensionUnit::PX);
+    overlayOptions.y = Dimension(20.0f, DimensionUnit::PX);
+    overlayOptions.align = Alignment::TOP_LEFT;
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    NG::SizeF parentSize(TEST_WIDTH, TEST_HEIGHT);
+    NG::SizeF childSize(TEST_CHILD_WIDTH, TEST_CHILD_HEIGHT);
+
+    auto offset = modifier->GetOverlayOffsetWithDirection(parentSize, childSize);
+
+    EXPECT_FLOAT_EQ(offset.GetX(), 10.0f);
+    EXPECT_FLOAT_EQ(offset.GetY(), 20.0f);
+}
+
+/**
+ * @tc.name: GetOverlayOffsetWithDirection002
+ * @tc.desc: Test GetOverlayOffsetWithDirection with CENTER alignment
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetOverlayOffsetWithDirection002, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "test";
+    overlayOptions.x = Dimension(0.0f, DimensionUnit::PX);
+    overlayOptions.y = Dimension(0.0f, DimensionUnit::PX);
+    overlayOptions.align = Alignment::CENTER;
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    NG::SizeF parentSize(TEST_WIDTH, TEST_HEIGHT);
+    NG::SizeF childSize(TEST_CHILD_WIDTH, TEST_CHILD_HEIGHT);
+
+    auto offset = modifier->GetOverlayOffsetWithDirection(parentSize, childSize);
+
+    float expectedX = (TEST_WIDTH - TEST_CHILD_WIDTH) / 2.0f;
+    float expectedY = (TEST_HEIGHT - TEST_CHILD_HEIGHT) / 2.0f;
+
+    EXPECT_FLOAT_EQ(offset.GetX(), expectedX);
+    EXPECT_FLOAT_EQ(offset.GetY(), expectedY);
+}
+
+/**
+ * @tc.name: GetOverlayOffsetWithDirection003
+ * @tc.desc: Test GetOverlayOffsetWithDirection with RTL direction
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetOverlayOffsetWithDirection003, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "test";
+    overlayOptions.x = Dimension(10.0f, DimensionUnit::PX);
+    overlayOptions.y = Dimension(20.0f, DimensionUnit::PX);
+    overlayOptions.align = Alignment::TOP_LEFT;
+    overlayOptions.direction = TextDirection::RTL;
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    NG::SizeF parentSize(TEST_WIDTH, TEST_HEIGHT);
+    NG::SizeF childSize(TEST_CHILD_WIDTH, TEST_CHILD_HEIGHT);
+
+    auto offset = modifier->GetOverlayOffsetWithDirection(parentSize, childSize);
+
+    float expectedX = -10.0f + (TEST_WIDTH - TEST_CHILD_WIDTH);
+    float expectedY = 20.0f;
+
+    EXPECT_FLOAT_EQ(offset.GetX(), expectedX);
+    EXPECT_FLOAT_EQ(offset.GetY(), expectedY);
+}
+
+/**
+ * @tc.name: GetOverlayOffsetWithDirection004
+ * @tc.desc: Test GetOverlayOffsetWithDirection with BOTTOM_RIGHT alignment
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetOverlayOffsetWithDirection004, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "test";
+    overlayOptions.x = Dimension(15.0f, DimensionUnit::PX);
+    overlayOptions.y = Dimension(25.0f, DimensionUnit::PX);
+    overlayOptions.align = Alignment::BOTTOM_RIGHT;
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    NG::SizeF parentSize(TEST_WIDTH, TEST_HEIGHT);
+    NG::SizeF childSize(TEST_CHILD_WIDTH, TEST_CHILD_HEIGHT);
+
+    auto offset = modifier->GetOverlayOffsetWithDirection(parentSize, childSize);
+
+    float expectedX = (TEST_WIDTH - TEST_CHILD_WIDTH) + 15.0f;
+    float expectedY = (TEST_HEIGHT - TEST_CHILD_HEIGHT) + 25.0f;
+
+    EXPECT_FLOAT_EQ(offset.GetX(), expectedX);
+    EXPECT_FLOAT_EQ(offset.GetY(), expectedY);
+}
+
+/**
+ * @tc.name: GetOverlayOffsetWithDirection005
+ * @tc.desc: Test GetOverlayOffsetWithDirection with VP units
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetOverlayOffsetWithDirection005, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "test";
+    overlayOptions.x = Dimension(10.0f, DimensionUnit::VP);
+    overlayOptions.y = Dimension(20.0f, DimensionUnit::VP);
+    overlayOptions.align = Alignment::TOP_LEFT;
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    NG::SizeF parentSize(TEST_WIDTH, TEST_HEIGHT);
+    NG::SizeF childSize(TEST_CHILD_WIDTH, TEST_CHILD_HEIGHT);
+
+    auto offset = modifier->GetOverlayOffsetWithDirection(parentSize, childSize);
+
+    EXPECT_GE(offset.GetX(), 0.0f);
+    EXPECT_GE(offset.GetY(), 0.0f);
+}
+
+/**
+ * @tc.name: GetOverlayOffsetWithDirection006
+ * @tc.desc: Test GetOverlayOffsetWithDirection with RTL and CENTER alignment
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetOverlayOffsetWithDirection006, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "test";
+    overlayOptions.x = Dimension(10.0f, DimensionUnit::PX);
+    overlayOptions.y = Dimension(15.0f, DimensionUnit::PX);
+    overlayOptions.align = Alignment::CENTER;
+    overlayOptions.direction = TextDirection::RTL;
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    NG::SizeF parentSize(TEST_WIDTH, TEST_HEIGHT);
+    NG::SizeF childSize(TEST_CHILD_WIDTH, TEST_CHILD_HEIGHT);
+
+    auto offset = modifier->GetOverlayOffsetWithDirection(parentSize, childSize);
+
+    float expectedX = (TEST_WIDTH - TEST_CHILD_WIDTH) / 2.0f - 10.0f;
+    float expectedY = (TEST_HEIGHT - TEST_CHILD_HEIGHT) / 2.0f + 15.0f;
+
+    EXPECT_FLOAT_EQ(offset.GetX(), expectedX);
+    EXPECT_FLOAT_EQ(offset.GetY(), expectedY);
+}
+
+/**
+ * @tc.name: GetOverlayOffsetWithDirection007
+ * @tc.desc: Test GetOverlayOffsetWithDirection returns zero offset when property is null
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetOverlayOffsetWithDirection007, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+
+    NG::SizeF parentSize(TEST_WIDTH, TEST_HEIGHT);
+    NG::SizeF childSize(TEST_CHILD_WIDTH, TEST_CHILD_HEIGHT);
+
+    auto offset = modifier->GetOverlayOffsetWithDirection(parentSize, childSize);
+
+    EXPECT_FLOAT_EQ(offset.GetX(), 0.0f);
+    EXPECT_FLOAT_EQ(offset.GetY(), 0.0f);
+}
+
+/**
+ * @tc.name: GetParagraphEmptyContent001
+ * @tc.desc: Test GetParagraph returns nullptr for empty content
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetParagraphEmptyContent001, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "";
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    auto paragraph = modifier->GetParagraph(400.0);
+    EXPECT_EQ(paragraph, nullptr);
+}
+
+/**
+ * @tc.name: GetParagraphSizeEmptyContent001
+ * @tc.desc: Test GetParagraphSize returns empty SizeF for empty content
+ * @tc.type: FUNC
+ */
+HWTEST_F(OverlayModifierTestNg, GetParagraphSizeEmptyContent001, TestSize.Level1)
+{
+    auto modifier = std::make_shared<NG::OverlayTextModifier>();
+    NG::OverlayOptions overlayOptions;
+    overlayOptions.content = "";
+    modifier->SetCustomData(NG::OverlayTextData(overlayOptions));
+
+    auto size = modifier->GetParagraphSize(400.0);
+    EXPECT_FLOAT_EQ(size.Width(), 0.0f);
+    EXPECT_FLOAT_EQ(size.Height(), 0.0f);
+}
+
+} // namespace OHOS::Ace
--- test/unittest/core/rosen/rosen_render_context_test_new.cpp
+++ test/unittest/core/rosen/rosen_render_context_test_new.cpp
@@ -1341,4 +1341,38 @@ HWTEST_F(RosenRenderContextTest, RosenRenderContextTestNew050, TestSize.Level1)
     rosenRenderContext->UpdateFrontBlurRadius(0.0_px);
     EXPECT_EQ(rosenRenderContext->GetFrontBlurRadius().value(), 0.0_px);
 }
+
+/**
+ * @tc.name: RosenRenderContextTestNew051
+ * @tc.desc: PaintOverlayText should refresh draw region when overlay content is cleared.
+ * @tc.type: FUNC
+ */
+HWTEST_F(RosenRenderContextTest, RosenRenderContextTestNew051, TestSize.Level1)
+{
+    constexpr uint32_t DRAW_REGION_OVERLAY_TEXT_INDEX = 4;
+    constexpr float PAINT_WIDTH = 200.0f;
+    constexpr float PAINT_HEIGHT = 100.0f;
+    auto frameNode =
+        FrameNode::GetOrCreateFrameNode("frame", -1, []() { return AceType::MakeRefPtr<PagePattern>(nullptr); });
+    RefPtr<RosenRenderContext> rosenRenderContext = InitRosenRenderContext(frameNode);
+    rosenRenderContext->paintRect_ = RectF(0.0f, 0.0f, PAINT_WIDTH, PAINT_HEIGHT);
+
+    OverlayOptions overlay;
+    overlay.content = "overlay-text";
+    overlay.align = Alignment::TOP_LEFT;
+    overlay.x = Dimension(10.0_px);
+    overlay.y = Dimension(5.0_px);
+    rosenRenderContext->UpdateOverlayText(overlay);
+    ASSERT_NE(rosenRenderContext->overlayTextModifier_, nullptr);
+    ASSERT_NE(rosenRenderContext->drawRegionRects_[DRAW_REGION_OVERLAY_TEXT_INDEX], nullptr);
+
+    auto expectedClearRect = std::make_shared<Rosen::RectF>(0.0f, 0.0f, PAINT_WIDTH, PAINT_HEIGHT);
+    EXPECT_FALSE(*rosenRenderContext->drawRegionRects_[DRAW_REGION_OVERLAY_TEXT_INDEX] == *expectedClearRect);
+
+    overlay.content = "";
+    rosenRenderContext->UpdateOverlayText(overlay);
+    ASSERT_NE(rosenRenderContext->overlayTextModifier_, nullptr);
+    ASSERT_NE(rosenRenderContext->drawRegionRects_[DRAW_REGION_OVERLAY_TEXT_INDEX], nullptr);
+    EXPECT_TRUE(*rosenRenderContext->drawRegionRects_[DRAW_REGION_OVERLAY_TEXT_INDEX] == *expectedClearRect);
+}
 } // namespace OHOS::Ace::NG
