77fcb525e0bbfedef8ef6c211ca52091d554d722
quguiren <quguiren1@huawei-partners.com>
2026-04-22 21:38:11 +0800

    add cross platform xc snapshot
    Signed-off-by: quguiren <quguiren1@huawei-partners.com>

--- frameworks/core/components_ng/pattern/video/resource/ext_surface.h
+++ frameworks/core/components_ng/pattern/video/resource/ext_surface.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -54,6 +54,7 @@ public:
 #ifdef RENDER_EXTRACT_SUPPORTED
     void SetSurfaceRotation(bool isLock);
     void SetSurfaceRect(float positionX, float positionY, float width, float height);
+    bool SurfaceCapture(uintptr_t pointerVal, int32_t width, int32_t height);
 #endif
 
 private:
