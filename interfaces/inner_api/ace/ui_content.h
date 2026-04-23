12555c599142ab169b13a8c3e3408d6e808c0959
wujinhui <wujinhui7@huawei.com>
2026-04-24 01:30:01 +0800

    merge
    Signed-off-by: wujinhui <wujinhui7@huawei.com>

--- interfaces/inner_api/ace/ui_content.h
+++ interfaces/inner_api/ace/ui_content.h
@@ -140,7 +140,6 @@ public:
     static const std::string& GetUICorrectionConfig();
     static bool SetUICorrectionConfig(const std::string& configStr);
     virtual ~UIContent() = default;
-    virtual OHOS::Rosen::Window* GetUIContentWindow() { return nullptr; };
 
     // UI content life-cycles
     virtual UIContentErrorCode Initialize(OHOS::Rosen::Window* window, const std::string& url, napi_value storage) = 0;
@@ -155,7 +154,8 @@ public:
                                                 napi_value storage) = 0;
     virtual void InitializeByName(OHOS::Rosen::Window *window,
         const std::string &name, napi_value storage, uint32_t focusWindowId) {};
-    virtual void InitializeDynamic(const DynamicInitialConfig& config) {};
+    virtual void InitializeDynamic(
+        const DynamicInitialConfig& config, sptr<IRemoteObject> connectToRender = nullptr) {};
 
     // UIExtensionAbility initialize for focusWindow ID
     virtual void Initialize(
@@ -246,8 +246,7 @@ public:
 
     // ArkTS Form
     virtual void PreInitializeForm(OHOS::Rosen::Window* window, const std::string& url, napi_value storage,
-        sptr<IRemoteObject> connectToRender) = 0;
-    virtual void PreInitializeForm(OHOS::Rosen::Window* window, const std::string& url, napi_value storage) {}
+        sptr<IRemoteObject> connectToRender = nullptr) = 0;
     virtual void PreInitializeFormAni(OHOS::Rosen::Window* window, const std::string& url, ani_object storage) {};
     virtual void RunFormPage() = 0;
     virtual std::shared_ptr<Rosen::RSSurfaceNode> GetFormRootNode() = 0;
