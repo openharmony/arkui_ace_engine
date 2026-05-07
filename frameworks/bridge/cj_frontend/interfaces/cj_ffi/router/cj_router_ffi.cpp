07c656ed5bbf738887debb20209adf8d69acc455
yuzhuohao <yuzhuohao@noreply.gitcode.com>
2026-05-07 14:14:37 +0800

    fix: fix router push when showInSubwindow is true.
    Signed-off-by: yuzhuohao <1171326722@qq.com>

--- frameworks/bridge/cj_frontend/interfaces/cj_ffi/router/cj_router_ffi.cpp
+++ frameworks/bridge/cj_frontend/interfaces/cj_ffi/router/cj_router_ffi.cpp
@@ -17,19 +17,43 @@
 
 #include "cj_lambda.h"
 
+#include "base/subwindow/subwindow_manager.h"
 #include "bridge/cj_frontend/frontend/cj_frontend_abstract.h"
+#include "core/common/ace_engine.h"
+#include "core/common/container_scope.h"
 
 using namespace OHOS::Ace;
 using namespace OHOS::Ace::Framework;
 
+namespace {
+int32_t GetParentContainerIdIfNeeded()
+{
+    auto container = Container::Current();
+    if (!container) {
+        return -1;
+    }
+    if (container->IsSubContainer()) {
+        return SubwindowManager::GetInstance()->GetParentContainerId(Container::CurrentId());
+    }
+    return Container::CurrentId();
+}
+}
+
 extern "C" {
 void FfiOHOSAceFrameworkRouterPush(const char* url, const char* param)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterPush fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterPush fail, no valid container");
         return;
     }
-    auto frontend = Utils::GetCurrentFrontend();
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterPush fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
@@ -39,11 +63,18 @@ void FfiOHOSAceFrameworkRouterPush(const char* url, const char* param)
 
 void FfiOHOSAceFrameworkRouterBack(const char* url, const char* param)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterBack fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterBack fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterBack fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
@@ -53,11 +84,18 @@ void FfiOHOSAceFrameworkRouterBack(const char* url, const char* param)
 
 void FfiOHOSAceFrameworkRouterReplace(const char* url, const char* param, int32_t modeValue)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterBack fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterReplace fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterReplace fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
@@ -68,27 +106,39 @@ void FfiOHOSAceFrameworkRouterReplace(const char* url, const char* param, int32_
 
 ExternalString FfiOHOSAceFrameworkRouterGetParams()
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterGetParams fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterGetParams fail, no valid container");
         return {};
     }
-
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterGetParams fail, can not get container");
+        return {};
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return {};
     }
-
     return Utils::MallocCString(frontend->GetParams());
 }
 
 void FfiOHOSAceFrameworkRouterBackIndex(int32_t index, const char* param)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterBackIndex fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterBackIndex fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterBackIndex fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
@@ -98,11 +148,18 @@ void FfiOHOSAceFrameworkRouterBackIndex(int32_t index, const char* param)
 
 void FfiOHOSAceFrameworkRouterPushUrl(const char* url, const char* param, int32_t modeValue, RouterCallback callbackRef)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterPushUrl fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterPushUrl fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterPushUrl fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
@@ -111,14 +168,22 @@ void FfiOHOSAceFrameworkRouterPushUrl(const char* url, const char* param, int32_
     auto callback = [ffiGetErrcode = CJLambda::Create(callbackRef)](int32_t errorCode) { ffiGetErrcode(errorCode); };
     frontend->PushPageWithCallback(url, param, mode, std::move(callback));
 }
+
 void FfiOHOSAceFrameworkRouterReplaceUrl(
     const char* url, const char* param, int32_t modeValue, RouterCallback callbackRef)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterReplaceUrl fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterReplaceUrl fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterReplaceUrl fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
@@ -127,52 +192,84 @@ void FfiOHOSAceFrameworkRouterReplaceUrl(
     auto callback = [ffiGetErrcode = CJLambda::Create(callbackRef)](int32_t errorCode) { ffiGetErrcode(errorCode); };
     frontend->ReplacePageWithCallback(url, param, mode, std::move(callback));
 }
+
 void FfiOHOSAceFrameworkRouterClear()
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterClear fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterClear fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterClear fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
     }
     frontend->Clear();
 }
+
 int32_t FfiOHOSAceFrameworkRouterGetLength()
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterGetLength fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterGetLength fail, no valid container");
         return 0;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterGetLength fail, can not get container");
+        return 0;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return 0;
     }
     return frontend->GetLength();
 }
+
 void FfiOHOSAceFrameworkRouterSetHideAlertBeforeBackPage()
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("SetHideAlertBeforeBackPage fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("SetHideAlertBeforeBackPage fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("SetHideAlertBeforeBackPage fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
     }
     frontend->SetHideAlertBeforeBackPage();
 }
+
 void FfiOHOSAceFrameworkRouterSetShowAlertBeforeBackPage(const char* msg, RouterCallback callbackRef)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("SetShowAlertBeforeBackPage fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("SetShowAlertBeforeBackPage fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("SetShowAlertBeforeBackPage fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
@@ -180,40 +277,64 @@ void FfiOHOSAceFrameworkRouterSetShowAlertBeforeBackPage(const char* msg, Router
     auto callback = [ffiGetErrcode = CJLambda::Create(callbackRef)](int32_t errorCode) { ffiGetErrcode(errorCode); };
     frontend->SetShowAlertBeforeBackPage(msg, std::move(callback));
 }
+
 void FfiOHOSAceFrameworkRouterGetState(OHOS::Ace::CJPageRouterAbstract::RouterState* info)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterGetState fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterGetState fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterGetState fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
     }
     frontend->GetState(info);
 }
+
 void FfiOHOSAceFrameworkRouterGetStateByIndex(OHOS::Ace::CJPageRouterAbstract::RouterState* info)
 {
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterGetStateByIndex fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterGetStateByIndex fail, no valid container");
         return;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterGetStateByIndex fail, can not get container");
+        return;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return;
     }
     frontend->GetStateByIndex(info);
 }
+
 OHOS::Ace::CJPageRouterAbstract::RouterStateList FfiOHOSAceFrameworkRouterGetStateByUrl(const char* url)
 {
     OHOS::Ace::CJPageRouterAbstract::RouterStateList ret;
-    if (Container::CurrentId() < 0) {
-        LOGE("RouterGetStateByUrl fail, no current container");
+    auto containerId = GetParentContainerIdIfNeeded();
+    if (containerId < 0) {
+        LOGE("RouterGetStateByUrl fail, no valid container");
         return ret;
     }
-    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(Utils::GetCurrentFrontend());
+    ContainerScope scope(containerId);
+    auto container = AceEngine::Get().GetContainer(containerId);
+    if (!container) {
+        LOGE("RouterGetStateByUrl fail, can not get container");
+        return ret;
+    }
+    auto frontend = AceType::DynamicCast<CJFrontendAbstract>(container->GetFrontend());
     if (!frontend) {
         LOGE("can not get frontend.");
         return ret;
