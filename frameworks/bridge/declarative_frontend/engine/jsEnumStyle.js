a53dfeabf60f36d8cc26753756d8fa0db26dcc51
dlh11234 <dailihuan@h-partners.com>
2026-05-12 09:34:30 +0800

    整改EdgeEffect枚举值类型 Signed-off-by: dailihuan <dailihuan@h-partners.com> Co-Authored-By: Agent
    Change-Id: Ib3e30cd054198e4cd11d1a001aafc2b8c8f0eee8

--- frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
+++ frameworks/bridge/declarative_frontend/engine/jsEnumStyle.js
@@ -567,7 +567,7 @@ let BarState;
   BarState[BarState.On = 2] = 'On';
 })(BarState || (BarState = {}));
 
-var EdgeEffect;
+let EdgeEffect;
 (function (EdgeEffect) {
   EdgeEffect[EdgeEffect.Spring = 0] = 'Spring';
   EdgeEffect[EdgeEffect.Fade = 1] = 'Fade';
@@ -1128,13 +1128,6 @@ let SwiperDisplayMode;
   SwiperDisplayMode[SwiperDisplayMode.STRETCH = 0] = 'STRETCH';
   SwiperDisplayMode[SwiperDisplayMode.AUTO_LINEAR = 1] = 'AUTO_LINEAR';
 })(SwiperDisplayMode || (SwiperDisplayMode = {}));
-
-var EdgeEffect;
-(function (EdgeEffect) {
-  EdgeEffect[EdgeEffect.Spring = 0] = 'Spring';
-  EdgeEffect[EdgeEffect.Fade = 1] = 'Fade';
-  EdgeEffect[EdgeEffect.None = 2] = 'None';
-})(EdgeEffect || (EdgeEffect = {}));
 
 let DatePickerType;
 (function (DatePickerType) {
