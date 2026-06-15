9ee854340d81ac1ac0ce7b0533da32028680ad62
guzhaoyang <guzhaoyang@h-partners.com>
2026-06-15 11:11:15 +0800

    删除arkweb-app-data-dir
    Signed-off-by: guzhaoyang <guzhaoyang@h-partners.com>

--- frameworks/core/components/web/resource/web_delegate.cpp
+++ frameworks/core/components/web/resource/web_delegate.cpp
@@ -3134,7 +3134,6 @@ void WebDelegate::InitWebViewWithWindow()
                 std::make_shared<OHOS::NWeb::NWebEngineInitArgsImpl>();
             std::string app_path = GetDataPath();
             if (!app_path.empty()) {
-                initArgs->AddArg(std::string("--arkweb-app-data-dir=").append(app_path));
                 initArgs->AddArg(std::string("--user-data-dir=").append(app_path));
             }
 
@@ -3668,7 +3667,6 @@ void WebDelegate::InitWebViewWithSurface()
             CHECK_NULL_VOID(delegate);
             std::shared_ptr<OHOS::NWeb::NWebEngineInitArgsImpl> initArgs =
                 std::make_shared<OHOS::NWeb::NWebEngineInitArgsImpl>();
-            initArgs->AddArg(std::string("--arkweb-app-data-dir=").append(delegate->bundleDataPath_));
             initArgs->AddArg(std::string("--user-data-dir=").append(delegate->bundleDataPath_));
             initArgs->AddArg(std::string("--bundle-installation-dir=").append(delegate->bundlePath_));
             initArgs->AddArg(std::string("--lang=").append(AceApplicationInfo::GetInstance().GetLanguage() +
