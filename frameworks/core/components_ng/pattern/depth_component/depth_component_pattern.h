b5fe7139ed782f87d6f48b70a2af46088f17272e
houguobiao <houguobiao@huawei.com>
2026-06-27 21:14:02 +0800

    修复DepthComponent组件2D和3D资源无法动态切换的问题
    Signed-off-by: houguobiao <houguobiao@huawei.com>
    Change-Id: Ie6fbccd953716bcbcb8cebcc5240bd6d082d831d

--- frameworks/core/components_ng/pattern/depth_component/depth_component_pattern.h
+++ frameworks/core/components_ng/pattern/depth_component/depth_component_pattern.h
@@ -190,6 +190,7 @@ private:
     void RemoveBackgroundImageNode();
     bool IsCameraChange();
     void OnPaint3D();
+    void FinishBackgroundSwitch();
 
 #if defined(ENABLE_ROSEN_BACKEND) && !defined(ACE_UNITTEST)
     void LoadDepthMap();
@@ -237,6 +238,8 @@ private:
     float lastRender3DScale_ = 1.0f;
     ImageSourceInfo depthMap_;
     std::optional<int32_t> backgroundImageId_;
+    bool pendingCleanupImage_ = false;
+    bool pendingCleanupGltf_ = false;
 
 #if defined(KIT_3D_ENABLE) && !defined(PREVIEW)
     BASE_NS::shared_ptr<Render3D::IMrtDepthAdapter> mrtDepthAdapter_;
