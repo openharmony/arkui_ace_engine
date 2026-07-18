5986f83f83e5a2eb3b568063fc85458096551391
zhang1596479078 <zhangdonghui5@huawei.com>
2026-07-18 16:51:50 +0800

    adapt fontscale with env
    Signed-off-by: zhang1596479078 <zhangdonghui5@huawei.com>

--- test/mock/frameworks/core/pipeline/mock_pipeline_context.cpp
+++ test/mock/frameworks/core/pipeline/mock_pipeline_context.cpp
@@ -1802,7 +1802,7 @@ std::optional<float> NG::PipelineContext::ResolveFontScaleFromEnv(const RefPtr<F
 
 float NG::PipelineContext::GetFontScaleFromEnv(const RefPtr<FrameNode>& host)
 {
-    return 1.0f;
+    return fontScale_;
 }
 
 std::optional<TextDirection> NG::PipelineContext::ResolveDirectionFromEnv(const RefPtr<FrameNode>& host)
