3b4e44bae569f94645fe9f9f422197bbeddc02ba
Lichtschein <linhu5@huawei.com>
2026-07-30 11:50:13 +0800

    fix diff final
    Signed-off-by: Lichtschein <linhu5@huawei.com>

--- frameworks/bridge/declarative_frontend/ark_component/src/ArkRow.ts
+++ frameworks/bridge/declarative_frontend/ark_component/src/ArkRow.ts
@@ -14,9 +14,6 @@
  */
 
 class RowAlignItemsModifier extends ModifierWithKey<number> {
-  constructor(value: number) {
-    super(value);
-  }
   static identity: Symbol = Symbol('rowAlignItems');
   applyPeer(node: KNode, reset: boolean): void {
     if (reset) {
@@ -31,9 +28,6 @@ class RowAlignItemsModifier extends ModifierWithKey<number> {
 }
 
 class RowJustifyContentlModifier extends ModifierWithKey<number> {
-  constructor(value: number) {
-    super(value);
-  }
   static identity: Symbol = Symbol('rowJustifyContent');
   applyPeer(node: KNode, reset: boolean): void {
     if (reset) {
