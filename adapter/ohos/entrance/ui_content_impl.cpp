2ddaea7046d4ecfc399a0c6173ae16f6d931232c
ryan5630 <chenran45@huawei.com>
2026-08-07 12:05:34 +0800

    Enable smartlayout withoutwhitelist
    Signed-off-by: ryan5630 <chenran45@huawei.com>

--- adapter/ohos/entrance/ui_content_impl.cpp
+++ adapter/ohos/entrance/ui_content_impl.cpp
@@ -1877,7 +1877,6 @@ UIContentErrorCode UIContentImpl::CommonInitializeForm(OHOS::Rosen::Window* wind
         frontend->SetIsBundle(false);
         container->SetBundleName(bundleName_);
         container->SetModuleName(moduleName_);
-        FeatureParamManager::GetInstance().UICorrectionParamParseEntryForForm(bundleName_);
     } else {
         errorCode = Platform::AceContainer::SetViewNew(aceView, density, 0, 0, window_);
         CHECK_ERROR_CODE_RETURN(errorCode);
