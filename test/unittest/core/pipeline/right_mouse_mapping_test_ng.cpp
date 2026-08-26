429607a07bdca3b8f2a94da9e6651c6993925115
Leo_Mei <meizhiyuan1@hisilicon.com>
2026-08-26 09:02:02 +0800

    fix: add NOLINT to suppress G.NAM.03-CPP for #define private/protected
    Signed-off-by: Leo_Mei <meizhiyuan1@hisilicon.com>

--- test/unittest/core/pipeline/right_mouse_mapping_test_ng.cpp
+++ test/unittest/core/pipeline/right_mouse_mapping_test_ng.cpp
@@ -19,8 +19,8 @@
 #include <string>
 #include <vector>
 
-#define private public
-#define protected public
+#define private public // NOLINT
+#define protected public // NOLINT
 #include "base/utils/feature_manager.h"
 #include "core/common/event_manager.h"
 #include "core/event/event_info_convertor.h"
